eb8071676098f9ae2538550ac9e32204f2d4f126
r12a <ishida@w3.org>
2017-07-18 09:20:37 +0100

    fontlists, add cherokee and n'ko

---
+++ fontlists/cherokee.js
@@ -0,0 +1,26 @@
+ var fontSelection = '<optgroup label="Webfonts">\
+      <option value="Noto Sans Cherokee WF">Noto Sans Cherokee WF</option>\
+      </optgroup>\
+      <optgroup label="Mac fonts">\
+      <option value="Plantagenet Cherokee">Plantagenet Cherokee</option>\
+      <option value="Noteworthy">Noteworthy</option>\
+      </optgroup>\
+      <optgroup label="Windows fonts">\
+      <option value="Plantagenet Cherokee">Plantagenet Cherokee</option>\
+      <option value="Gadugi">Gadugi</option>\
+      </optgroup>\
+      <optgroup label="Google fonts">\
+      <option value="Noto Sans Cherokee WF">Noto Sans Cherokee WF</option>\
+      </optgroup>\
+      <optgroup label="Other fonts">\
+      <option value="Aboriginal Sans">Aboriginal Sans</option>\
+      <option value="Aboriginal Serif">Aboriginal Serif</option>\
+      <option value="CherokeeLSU">CherokeeLSU</option>\
+      <option value="Marin">Marin</option>\
+      </optgroup>\
+      <optgroup label="General fonts">\
+      <option value="Code2000">Code2000</option>\
+      <option value="Arial Unicode MS">Arial Unicode MS</option>\
+      <option value="sans-serif">sans-serif</option>\
+      <option value="serif">serif</option>\
+      </optgroup>'
