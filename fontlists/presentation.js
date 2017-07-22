5005500b7adecd4a930c50927d64c8357d5696f8
r12a <ishida@w3.org>
2017-07-22 18:29:43 +0100

    fontlists/presentation.js new

---
+++ fontlists/presentation.js
@@ -0,0 +1,29 @@
+ var fontSelection = '     <option value="0">Select...</option>\
+             <optgroup label="Web font">\
+            <option value="Arian AMU WF">Arian AMU WF</option>\
+            <option value="Noto Sans Armenian WF">Noto Sans Armenian WF</option>\
+            </optgroup>\
+            \
+           <optgroup label="Mac fonts ">\
+            <option value="Mshtakan">Mshtakan</option>\
+            </optgroup>\
+            \
+            <optgroup label="Windows fonts ">\
+            <option value="Sylfaen">Sylfaen</option>\
+            </optgroup>\
+            \
+            <optgroup label="Google fonts ">\
+            <option value="Noto Serif Armenian">Noto Serif Armenian</option>\
+            <option value="Noto Sans Armenian">Noto Sans Armenian</option>\
+            </optgroup>\
+            \
+            <optgroup label="Other fonts ">\
+            <option value="Arian AMU">Arian AMU</option>\
+            </optgroup>\
+            \
+            <optgroup label="General fonts ">\
+            <option value="Arial Unicode MS">Arial Unicode MS</option>\
+            <option value="Code2000">Code2000</option>\
+            <option value="sans-serif">sans-serif</option>\
+            <option value="serif">serif</option>\
+            </optgroup>'
