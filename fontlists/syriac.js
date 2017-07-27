a504e7f99a39061ab6247b62c25fa649acd04d23
r12a <ishida@w3.org>
2017-07-27 19:14:53 +0100

    syriac fonts

---
+++ fontlists/syriac.js
@@ -0,0 +1,31 @@
+/*jshint multistr: true */
+ var fontSelection = '<option value="0">Select...</option>\
+            <optgroup label="Web fonts ">\
+            <option value="Noto Sans Syriac Eastern WF">Noto Sans Syriac Eastern WF</option>\
+            <option value="Noto Sans Syriac Estrangela WF">Noto Sans Syriac Estrangela WF</option>\
+            <option value="Noto Sans Syriac Western WF">Noto Sans Syriac Western WF</option>\
+            </optgroup>\
+            \
+            <optgroup label="Windows fonts ">\
+            <option value="Estrangelo Edessa">Estrangelo Edessa</option>\
+            </optgroup>\
+            \
+            <optgroup label="Google fonts ">\
+            <option value="Noto Sans Syriac Eastern">Noto Sans Syriac Eastern</option>\
+            <option value="Noto Sans Syriac Estrangela">Noto Sans Syriac Estrangela</option>\
+            <option value="Noto Sans Syriac Western">Noto Sans Syriac Western</option>\
+            </optgroup>\
+            \
+            <optgroup label="Other fonts ">\
+            <option value="Estrangelo Antioch">Estrangelo Antioch</option>\
+            <option value="Serto Jerusalem">Serto Jerusalem</option>\
+            </optgroup>\
+            \
+            <optgroup label="General fonts ">\
+            <option value="Arial Unicode MS">Arial Unicode MS</option>\
+            <option value="Code2000">Code2000</option>\
+            <option value="DejaVu Sans">DejaVu Sans</option>\
+            <option value="sans-serif">sans-serif</option>\
+            <option value="serif">serif</option>\
+            </optgroup>\
+        '
