b2026d34d9622dfdc01f554d9d97b6f5daeac819
r12a <ishida@w3.org>
2017-08-15 20:50:07 +0100

    gujarati fonts

---
+++ fontlists/gujarati.js
@@ -0,0 +1,32 @@
+ var fontSelection = '<optgroup label="Web font ">\
+        <option value="Noto Sans Gujarati WF">Noto Sans Gujarati WF</option>\
+        </optgroup>\
+        \
+		<optgroup label="Mac fonts ">\
+        <option value="Gujarati MT">Gujarati MT</option>\
+        <option value="Gujarati Sangam MN">Gujarati Sangam MN</option>\
+        </optgroup>\
+        \
+		<optgroup label="Windows fonts ">\
+        <option value="Shruti">Shruti</option>\
+        <option value="Nirmala UI">Nirmala UI</option>\
+        </optgroup>\
+        \
+		<optgroup label="Google fonts ">\
+        <option value="Noto Sans Gujarati">Noto Sans Gujarati</option>\
+        <option value="Noto Serif Gujarati">Noto Serif Gujarati</option>\
+		</optgroup>\
+        \
+		<optgroup label="Other fonts ">\
+        <option value="Aakar">Aakar</option>\
+        <option value="Lohit Gujarati">Lohit Gujarati</option>\
+        <option value="RaghuGujarati">RaghuGujarati</option>\
+        <option value="Rekha">Rekha</option>\
+		</optgroup>\
+        \
+		<optgroup label="General fonts ">\
+        <option value="Code2000">Code2000</option>\
+        <option value="Arial Unicode MS">Arial Unicode MS</option>\
+        <option value="sans-serif">sans-serif</option>\
+        <option value="serif">serif</option>\
+    	</optgroup>'
