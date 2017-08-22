295de084743aaa5332eb4b5907e8ed55842c95f2
r12a <ishida@w3.org>
2017-08-22 11:13:02 +0100

    fontlists/javanese.js new

---
+++ fontlists/javanese.js
@@ -0,0 +1,14 @@
+ var fontSelection = '     <option value="0">Select...</option>\
+             <optgroup label="Web font">\
+            <option value="Noto Sans Javanese WF">Noto Sans Javanese WF</option>\
+            </optgroup>\
+            \
+            <optgroup label="Google fonts ">\
+            <option value="Noto Sans Javanese">Noto Sans Javanese</option>\
+            </optgroup>\
+            \
+            <optgroup label="Other fonts ">\
+            <option value="Tuladha Jejeg">Tuladha Jejeg</option>\
+            <option value="Ki Sakabawa">Ki Sakabawa</option>\
+            <option value="Yogyakarta">Yogyakarta</option>\
+            </optgroup>'
