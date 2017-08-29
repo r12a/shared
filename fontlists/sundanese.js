c96f02561e682b2af5d97630a862806579084bef
r12a <ishida@w3.org>
2017-08-29 10:49:27 +0100

    sundanese fonts

---
+++ fontlists/sundanese.js
@@ -0,0 +1,14 @@
+ var fontSelection = '     <option value="0">Select...</option>\
+             <optgroup label="Web font">\
+            <option value="Sundanese Unicode 2013 WF">Sundanese Unicode 2013 WF</option>\
+            <option value="Noto Sans Sundanese WF">Noto Sans Sundanese WF</option>\
+            </optgroup>\
+            \
+            <optgroup label="Google fonts ">\
+            <option value="Noto Sans Sundanese">Noto Sans Sundanese</option>\
+            </optgroup>\
+            \
+            <optgroup label="Other fonts ">\
+            <option value="Sundanese Unicode 2013">Sundanese Unicode 2013</option>\
+            <option value="Sundanese Unicode">Sundanese Unicode</option>\
+            </optgroup>'
