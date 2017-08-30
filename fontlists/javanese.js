371a79dcd8ffebf4b7f8c8f9d11b5750da141791
r12a <ishida@w3.org>
2017-08-30 20:39:16 +0100

    fontlists/javanese, converted to multiline string

--- fontlists/javanese.js
+++ fontlists/javanese.js
@@ -1,14 +1,14 @@
- var fontSelection = '     <option value="0">Select...</option>\
-             <optgroup label="Web font">\
-            <option value="Noto Sans Javanese WF">Noto Sans Javanese WF</option>\
-            </optgroup>\
-            \
-            <optgroup label="Google fonts ">\
-            <option value="Noto Sans Javanese">Noto Sans Javanese</option>\
-            </optgroup>\
-            \
-            <optgroup label="Other fonts ">\
-            <option value="Yogyakarta">Yogyakarta</option>\
-            <option value="Yogya">Yogya</option>\
-            <option value="Tuladha Jejeg">Tuladha Jejeg</option>\
-            </optgroup>'
+ var fontSelection = `<option value="0">Select...</option>
+             <optgroup label="Web font">
+            <option value="Noto Sans Javanese WF">Noto Sans Javanese WF</option>
+            </optgroup>
+            
+            <optgroup label="Google fonts ">
+            <option value="Noto Sans Javanese">Noto Sans Javanese</option>
+            </optgroup>
+            
+            <optgroup label="Other fonts ">
+            <option value="Yogyakarta">Yogyakarta</option>
+            <option value="Yogya">Yogya</option>
+            <option value="Tuladha Jejeg">Tuladha Jejeg</option>
+            </optgroup>`
