1c489cf19aabd89a110ae02d66f95eb3ecbb9cf4
r12a <ishida@w3.org>
2017-08-31 09:40:09 +0100

    add Norbert's fonts to fontlists

--- fontlists/javanese.js
+++ fontlists/javanese.js
@@ -3,12 +3,16 @@
             <option value="Noto Sans Javanese WF">Noto Sans Javanese WF</option>
             </optgroup>
             
+            <optgroup label="Windows fonts ">
+            <option value="Javanese Text">Javanese Text</option>
+            </optgroup>
+            
             <optgroup label="Google fonts ">
             <option value="Noto Sans Javanese">Noto Sans Javanese</option>
             </optgroup>
             
             <optgroup label="Other fonts ">
             <option value="Yogyakarta">Yogyakarta</option>
-            <option value="Yogya">Yogya</option>
             <option value="Tuladha Jejeg">Tuladha Jejeg</option>
+            <option value="Yogya">Yogya</option>
             </optgroup>`
--- fontlists/sundanese.js
+++ fontlists/sundanese.js
@@ -1,14 +1,15 @@
- var fontSelection = '     <option value="0">Select...</option>\
-             <optgroup label="Web font">\
-            <option value="Sundanese Unicode 2013 WF">Sundanese Unicode 2013 WF</option>\
-            <option value="Noto Sans Sundanese WF">Noto Sans Sundanese WF</option>\
-            </optgroup>\
-            \
-            <optgroup label="Google fonts ">\
-            <option value="Noto Sans Sundanese">Noto Sans Sundanese</option>\
-            </optgroup>\
-            \
-            <optgroup label="Other fonts ">\
-            <option value="Sundanese Unicode 2013">Sundanese Unicode 2013</option>\
-            <option value="Sundanese Unicode">Sundanese Unicode</option>\
-            </optgroup>'
+ var fontSelection = `<option value="0">Select...</option>
+             <optgroup label="Web fonts">
+            <option value="Sundanese Unicode 2013 WF">Sundanese Unicode 2013 WF</option>
+            <option value="Noto Sans Sundanese WF">Noto Sans Sundanese WF</option>
+            </optgroup>
+
+            <optgroup label="Google fonts">
+            <option value="Noto Sans Sundanese">Noto Sans Sundanese</option>
+            </optgroup>
+
+            <optgroup label="Other fonts">
+            <option value="Sundanese Unicode 2013">Sundanese Unicode 2013</option>
+            <option value="Sundanese Unicode">Sundanese Unicode</option>
+            <option value="Bandung">Bandung</option>
+            </optgroup>`
