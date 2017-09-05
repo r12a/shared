834803820191e7a11651e626e36d6df17f2aff5f
r12a <ishida@w3.org>
2017-09-05 21:04:04 +0100

    buginese fonts

---
+++ fontlists/buginese.js
@@ -0,0 +1,17 @@
+ var fontSelection = `<option value="0">Select...</option>
+             <optgroup label="Web fonts">
+            <option value="Noto Sans Buginese WF">Noto Sans Buginese WF</option>
+            </optgroup>
+
+            <optgroup label="Microsoft fonts ">
+            <option value="Leelawadee UI">Leelawadee UI</option>
+            </optgroup>
+
+            <optgroup label="Google fonts ">
+            <option value="Noto Sans Buginese">Noto Sans Buginese</option>
+            </optgroup>
+
+            <optgroup label="Other fonts ">
+            <option value="Saweri">Saweri</option>
+            <option value="Makassar">Makassar</option>
+            </optgroup>`
