f437c4cf420b25b5b0d49cedf6383ccf79730f73
r12a <ishida@w3.org>
2017-09-07 13:52:29 +0100

    mongliang, updated webfont and fontlist

--- fontlists/mongoliang.js
+++ fontlists/mongoliang.js
@@ -1,1 +1,4 @@
- var fontSelection = '<option value="Graphetic prototype WF">Graphetic prototype WF</option><option value="Graphetic prototype 20170906">Graphetic prototype 20170906</option><option value="Menk Qagan Tig">Menk Qagan Tig</option>'
+ var fontSelection = `<option value="Graphetic prototype WF">Graphetic prototype WF</option>
+ 					<option value="Graphetic prototype 20170907">Graphetic prototype 20170907</option>
+ 					<option value="Graphetic prototype 20170906">Graphetic prototype 20170906</option>
+ 					<option value="Menk Qagan Tig">Menk Qagan Tig</option>`
