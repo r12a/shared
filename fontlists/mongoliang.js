8749607d618003a43c983090c33760ab4e01cbcc
r12a <ishida@w3.org>
2017-09-17 06:59:22 +0100

    mongoliang, updated webfont

--- fontlists/mongoliang.js
+++ fontlists/mongoliang.js
@@ -1,4 +1,5 @@
  var fontSelection = `<option value="Graphetic prototype WF">Graphetic prototype WF</option>
+ 					<option value="Graphetic prototype 20170916">Graphetic prototype 20170916</option>
  					<option value="Graphetic prototype 20170912">Graphetic prototype 20170912</option>
  					<option value="Graphetic prototype 20170907">Graphetic prototype 20170907</option>
  					<option value="Graphetic prototype 20170906">Graphetic prototype 20170906</option>
