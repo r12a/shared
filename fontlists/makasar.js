37f070ac9cd3fa8fe0a29acc99d1afd468f88717
r12a <ishida@w3.org>
2017-10-20 10:02:29 +0100

    makasar fontlist

---
+++ fontlists/makasar.js
@@ -0,0 +1,8 @@
+ var fontSelection = `<option value="0">Select...</option>
+             <optgroup label="Web fonts">
+            <option value="MakasarGraphite WF">MakasarGraphite WF</option>
+            </optgroup>
+
+            <optgroup label="Other fonts ">
+            <option value="MakasarGraphite">MakasarGraphite</option>
+            </optgroup>`
