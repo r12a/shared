92e61e5cffec9dfc1c2e9f9330ef42d43ce9c66e
r12a <ishida@w3.org>
2017-12-30 17:41:07 +0000

    Mandaic fonts

---
+++ fontlists/mandaic.js
@@ -0,0 +1,8 @@
+ var fontSelection = `<option value="0">Select...</option>
+             <optgroup label="Web fonts">
+            <option value="Mandaic WF">Mandaic WF</option>
+            </optgroup>
+
+            <optgroup label="Other fonts ">
+            <option value="Noto Sans Mandaic">Noto Sans Mandaic</option>
+            </optgroup>`
