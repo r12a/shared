7b8bff7153e23c45b111ebf3a1065270ed8622e4
r12a <ishida@w3.org>
2018-01-24 09:27:54 +0000

    fontlists/sinhala.js

---
+++ fontlists/sinhala.js
@@ -0,0 +1,33 @@
+ var fontSelection = `<optgroup label="Web font ">
+        <option value="Noto Serif Sinhala WF">Noto Serif Sinhala WF</option>
+        </optgroup>
+        
+		<optgroup label="Mac fonts ">
+        <option value="Sinhala MN">Sinhala MN</option>
+        <option value="Sinhala Sangam MN">Sinhala Sangam MN</option>
+        </optgroup>
+        
+		<optgroup label="Windows fonts ">
+        <option value="Iskoola Pota">Iskoola Pota</option>
+        <option value="Nirmala UI">Nirmala UI</option>
+        </optgroup>
+        
+		<optgroup label="Google fonts ">
+        <option value="Noto Serif Sinhala">Noto Serif Sinhala</option>
+        <option value="Noto Sans Sinhala">Noto Sans Sinhala</option>
+		</optgroup>
+        
+		<optgroup label="Other fonts ">
+        <option value="Dinamina">Dinamina</option>
+        <option value="DinaminaUniWeb">DinaminaUniWeb</option>
+        <option value="Malithi Web">Malithi Web</option>
+        <option value="Potha">Potha</option>
+		</optgroup>
+        
+		<optgroup label="General fonts ">
+        <option value="Code2000">Code2000</option>
+        <option value="FreeSans">FreeSans</option>
+        <option value="Arial Unicode MS">Arial Unicode MS</option>
+        <option value="sans-serif">sans-serif</option>
+        <option value="serif">serif</option>
+    	</optgroup>`
