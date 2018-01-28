2ff5275e447b992a5fa588d7a291c790a2ffb074
r12a <ishida@w3.org>
2018-01-28 20:29:25 +0000

    fontlists/tifinagh.js, added SIL font

--- fontlists/tifinagh.js
+++ fontlists/tifinagh.js
@@ -1,28 +1,36 @@
- var fontSelection = '<optgroup label="Webfont">\
-        <option value="Noto Sans Tifinagh WF">Noto Sans Tifinagh WF</option>\
-        </optgroup>\
-        <option value="0">Select...</option>\
-		   <optgroup label="Windows fonts">\
-           <option value="Ebrima">Ebrima</option>\
-           </optgroup>\
-			\
-		   <optgroup label="Other fonts"> \
-            <option value="Hapax Berbère">Hapax Berbère</option>\
-			<option value="Tamalout Noufuss Unicode">Tamalout Noufuss Unicode</option>\
-            <option value="Tamalout Standard Unicode">Tamalout Standard Unicode</option>\
-            <option value="Tamzwart Noufuss Unicode">Tamzwart Noufuss Unicode</option>\
-            <option value="Tamzwart Standard Unicode">Tamzwart Standard Unicode</option>\
-            <option value="Tassafout Noufuss Unicode">Tassafout Noufuss Unicode</option>\
-            <option value="Tassafout Standard Unicode">Tassafout Standard Unicode</option>\
-            <option value="Tazdayt Noufuss Unicode">Tazdayt Noufuss Unicode</option>\
-            <option value="Tazirit Unicode">Tazirit Unicode</option>\
-            <option value="Teddus Noufuss Unicode">Teddus Noufuss Unicode</option>\
-            <option value="Teddus Standard Unicode">Teddus Standard Unicode</option>\
-            <option value="Tifinaghe-Ircam Unicode">Tifinaghe-Ircam Unicode</option>\
-            </optgroup>\
-            \
-		   <optgroup label="General fonts"> \
-            <option value="Code2000">Code2000</option>\
-            <option value="sans-serif">sans-serif</option>\
-            <option value="serif">serif</option>\
-            </optgroup>'
+ var fontSelection = `<optgroup label="Webfont">
+        <option value="Noto Sans Tifinagh WF">Noto Sans Tifinagh WF</option>
+        </optgroup>
+
+		   <optgroup label="Windows fonts">
+           <option value="Ebrima">Ebrima</option>
+           </optgroup>
+			
+		   <optgroup label="Google fonts">
+           <option value="Noto Sans Tifinagh">Noto Sans Tifinagh</option>
+           </optgroup>
+			
+		   <optgroup label="SIL fonts">
+           <option value="Tagmukay">Tagmukay</option>
+           </optgroup>
+			
+		   <optgroup label="Other fonts"> 
+            <option value="Hapax Berbère">Hapax Berbère</option>
+			<option value="Tamalout Noufuss Unicode">Tamalout Noufuss Unicode</option>
+            <option value="Tamalout Standard Unicode">Tamalout Standard Unicode</option>
+            <option value="Tamzwart Noufuss Unicode">Tamzwart Noufuss Unicode</option>
+            <option value="Tamzwart Standard Unicode">Tamzwart Standard Unicode</option>
+            <option value="Tassafout Noufuss Unicode">Tassafout Noufuss Unicode</option>
+            <option value="Tassafout Standard Unicode">Tassafout Standard Unicode</option>
+            <option value="Tazdayt Noufuss Unicode">Tazdayt Noufuss Unicode</option>
+            <option value="Tazirit Unicode">Tazirit Unicode</option>
+            <option value="Teddus Noufuss Unicode">Teddus Noufuss Unicode</option>
+            <option value="Teddus Standard Unicode">Teddus Standard Unicode</option>
+            <option value="Tifinaghe-Ircam Unicode">Tifinaghe-Ircam Unicode</option>
+            </optgroup>
+            
+		   <optgroup label="General fonts"> 
+            <option value="Code2000">Code2000</option>
+            <option value="sans-serif">sans-serif</option>
+            <option value="serif">serif</option>
+            </optgroup>`
