49ff55b1af156a343a7f8da3b14995782da058da
r12a <ishida@w3.org>
2018-04-01 13:11:38 +0100

    fontlists/mongolian: Update format and add Oyun Qagan Tig font

--- fontlists/mongolian.js
+++ fontlists/mongolian.js
@@ -1,12 +1,30 @@
- var fontSelection = '<option value="Mongolian Universal White">Mongolian Universal White</option>\
-<option value="Mongolian White">Mongolian White</option>\
-<option value="Mongolian Writing">Mongolian Writing</option>\
-<option value="Mongolian Baiti">Mongolian Baiti</option>\
-<option value="Noto Sans Mongolian">Noto Sans Mongolian</option>\
-<option value="Noto Sans Mongolian WF">Noto Sans Mongolian WF</option>\
-<option value="Mongolian Title">Mongolian Title</option>\
-<option value="Mongolian Art">Mongolian Art</option>\
-<option value="MongolianScript">MongolianScript</option>\
-<option value="FZMWBTOT_Unicode">FZMWBTOT_Unicode</option>\
-<option value="sans-serif">sans-serif</option>\
-<option value="serif">serif</option>'
+ var fontSelection = `    <option value="0">Select...</option>
+             <optgroup label="Web font">
+            <option value="Noto Sans Mongolian WF">Noto Sans Mongolian WF</option>
+            </optgroup>
+            
+            <optgroup label="Windows fonts ">
+            <option value="Mongolian Baiti">Mongolian Baiti</option>
+            </optgroup>
+            
+            <optgroup label="Google fonts ">
+            <option value="Noto Sans Mongolian">Noto Sans Mongolian</option>
+            </optgroup>
+            
+            <optgroup label="Other fonts ">
+            <option value="Mongolian Universal White">Mongolian Universal White</option>
+			<option value="Mongolian White">Mongolian White</option>
+			<option value="Mongolian Writing">Mongolian Writing</option>
+ 			<option value="Oyun Qagan Tig">Oyun Qagan Tig</option>
+ 			<option value="Mongolian Title">Mongolian Title</option>
+			<option value="Mongolian Art">Mongolian Art</option>
+			<option value="MongolianScript">MongolianScript</option>
+			<option value="FZMWBTOT_Unicode">FZMWBTOT_Unicode</option>
+           </optgroup>
+            
+            <optgroup label="General fonts ">
+            <option value="Arial Unicode MS">Arial Unicode MS</option>
+            <option value="Code2000">Code2000</option>
+            <option value="sans-serif">sans-serif</option>
+            <option value="serif">serif</option>
+            </optgroup>`
