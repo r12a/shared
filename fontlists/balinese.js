2798f0b2048b3337e92f7d8c680d0360b82b34e3
r12a <ishida@w3.org>
2018-04-17 10:48:11 +0100

    Replace Notos Sans Balinese with Noto Serif Balinese

--- fontlists/balinese.js
+++ fontlists/balinese.js
@@ -1,10 +1,11 @@
  var fontSelection = `<option value="0">Select...</option>
              <optgroup label="Web fonts">
             <option value="Aksara Bali WF">Aksara Bali WF</option>
-            <option value="Noto Sans Balinese WF">Noto Sans Balinese WF</option>
+            <option value="Noto Serif Balinese WF">Noto Serif Balinese WF</option>
             </optgroup>
 
             <optgroup label="Google fonts ">
+            <option value="Noto Serif Balinese">Noto Serif Balinese</option>
             <option value="Noto Sans Balinese">Noto Sans Balinese</option>
             </optgroup>
 
