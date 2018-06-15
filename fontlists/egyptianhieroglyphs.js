1c62a1ba1ba5033e0268d31129821ad5d24f88a5
r12a <ishida@w3.org>
2018-06-15 10:17:59 +0100

    egyptianhieroglyphs: Added fonts

--- fontlists/egyptianhieroglyphs.js
+++ fontlists/egyptianhieroglyphs.js
@@ -1,14 +1,19 @@
- var fontSelection = '<optgroup label="Webfonts ">\
-        <option value="Noto Sans Egyptian Hieroglyphs WF">Noto Sans Egyptian Hieroglyphs WF</option>\
-		</optgroup>\
-        \
-		<optgroup label="Google fonts ">\
-        <option value="Noto Sans Egyptian Hieroglyphs">Noto Sans Egyptian Hieroglyphs</option>\
-		</optgroup>\
-        \
-		<optgroup label="Other fonts ">\
-        <option value="Aegyptus">Aegyptus</option>\
-        <option value="Gardiner">Gardiner</option>\
-        <option value="New Gardiner">New Gardiner</option>\
-        <option value="Nilus">Nilus</option>\
-    	</optgroup>'
+ var fontSelection = `<optgroup label="Webfonts ">
+        <option value="Noto Sans Egyptian Hieroglyphs WF">Noto Sans Egyptian Hieroglyphs WF</option>
+		</optgroup>
+        
+		<optgroup label="Google fonts ">
+        <option value="Noto Sans Egyptian Hieroglyphs">Noto Sans Egyptian Hieroglyphs</option>
+		</optgroup>
+        
+		<optgroup label="Other fonts ">
+        <option value="Aegyptus">Aegyptus</option>
+        <option value="Abydos">Abydos</option>
+        <option value="Gardiner">Gardiner</option>
+        <option value="JSesh font">JSesh font</option>
+        <option value="New Gardiner">New Gardiner</option>
+        <option value="Nilus">Nilus</option>
+        <option value="Aaron UMdC Alpha">Aaron UMdC Alpha</option>
+        <option value="Aaron UMdC Phonetic Alpha">Aaron UMdC Phonetic Alpha</option>
+        <option value="Aaron Basic RTL Alpha">Aaron Basic RTL Alpha</option>
+    	</optgroup>`
