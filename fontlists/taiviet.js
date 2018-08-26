f0f6ffbe4cdd88e1e9cd7a074f12f899ee02ab69
r12a <ishida@w3.org>
2018-08-26 17:45:28 +0100

    fontlists, + tai viet

---
+++ fontlists/taiviet.js
@@ -0,0 +1,20 @@
+ var fontSelection =`<optgroup label="Webfonts">
+         <option value="Tai Heritage Pro WF">Tai Heritage Pro WF</option>
+       <option value="Noto Sans Tai Viet WF">Noto Sans Tai Viet WF</option>
+        </optgroup>
+        
+        <optgroup label="Google fonts">
+        <option value="Noto Sans Tai Viet">Noto Sans Tai Viet</option>
+        </optgroup>
+		
+        <optgroup label="SIL fonts">
+        <option value="Tai Heritage Pro">Tai Heritage Pro</option>
+        </optgroup>
+        
+        <optgroup label="General fonts">
+        <option value="Arial Unicode MS">Arial Unicode MS</option>
+        <option value="Code2000">Code2000</option>
+        <option value="Microsoft Sans Serif">Microsoft Sans Serif</option>
+        <option value="sans-serif">sans-serif</option>
+        <option value="serif">serif</option>
+        </optgroup>`
