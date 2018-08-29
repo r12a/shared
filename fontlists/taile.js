1f8bf7dbf4fd3e31ca3dc09d62e723f593899232
r12a <ishida@w3.org>
2018-08-29 09:44:22 +0100

    fontlists, + tai le

---
+++ fontlists/taile.js
@@ -0,0 +1,23 @@
+ var fontSelection =`<optgroup label="Webfonts">
+       <option value="Noto Sans New Tai Le WF">Noto Sans Tai Le WF</option>
+        </optgroup>
+        
+        <optgroup label="Google fonts">
+        <option value="Noto Sans Tai Le">Noto Sans Tai Le</option>
+        </optgroup>
+		
+        <optgroup label="Microsoft fonts">
+        <option value="Microsoft Tai Le">Microsoft Tai Le</option>
+        </optgroup>
+		
+        <optgroup label="SIL fonts">
+        <option value="Tharlon">Tharlon</option>
+        </optgroup>
+		
+        <optgroup label="General fonts">
+        <option value="Arial Unicode MS">Arial Unicode MS</option>
+        <option value="Code2000">Code2000</option>
+        <option value="Microsoft Sans Serif">Microsoft Sans Serif</option>
+        <option value="sans-serif">sans-serif</option>
+        <option value="serif">serif</option>
+        </optgroup>`
