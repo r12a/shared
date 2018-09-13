cc1c7bee3036164d2051d884d2466672fb7f9173
r12a <ishida@w3.org>
2018-09-13 02:38:37 -0700

    fontlists, + taikuen.js

---
+++ fontlists/taikhuen.js
@@ -0,0 +1,22 @@
+ var fontSelection =`<optgroup label="Webfonts">
+        <option value="A Tai Tham KH New WF">A Tai Tham KH New WF</option>
+         <option value="Lanna Alif WF">Lanna Alif WF</option>
+       <option value="Noto Sans Tai Tham WF">Noto Sans Tai Tham WF</option>
+        </optgroup>
+        
+        <optgroup label="Google fonts">
+        <option value="Noto Sans Tai Tham">Noto Sans Tai Tham</option>
+        </optgroup>
+		
+        <optgroup label="Other fonts">
+        <option value="A Tai Tham KH New">A Tai Tham KH New</option>
+        <option value="Lanna Alif">Lanna Alif</option>
+        </optgroup>
+        
+        <optgroup label="General fonts">
+        <option value="Arial Unicode MS">Arial Unicode MS</option>
+        <option value="Code2000">Code2000</option>
+        <option value="Microsoft Sans Serif">Microsoft Sans Serif</option>
+        <option value="sans-serif">sans-serif</option>
+        <option value="serif">serif</option>
+        </optgroup>`
