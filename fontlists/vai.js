9e289d15e6efee348cf4efa715543451956b5fbe
r12a <ishida@w3.org>
2018-11-25 18:18:45 +0000

    vai fontlist

---
+++ fontlists/vai.js
@@ -0,0 +1,17 @@
+ var fontSelection = `<option value="0">Select...</option>
+             <optgroup label="Web font">
+            <option value="Noto Sans Vai WF">Noto Sans Vai WF</option>
+            </optgroup>
+            
+            <optgroup label="Windows fonts ">
+            <option value="Ebrima">Ebrima</option>
+            </optgroup>
+            
+            <optgroup label="Google fonts ">
+            <option value="Noto Sans Vai">Noto Sans Vai</option>
+            </optgroup>
+            
+            <optgroup label="Other fonts ">
+            <option value="Dukor">Dukor</option>
+            <option value="Wakor">Wakor</option>
+            </optgroup>`
