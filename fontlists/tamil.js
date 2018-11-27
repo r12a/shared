feadcb62f5e315c1861f219114d07adc53cce2b3
r12a <ishida@w3.org>
2018-11-27 11:34:46 +0000

    fontlist tamil + fonts

--- fontlists/tamil.js
+++ fontlists/tamil.js
@@ -1,22 +1,35 @@
- var fontSelection = '<optgroup label="Webfont">\
-        <option value="Noto Sans Tamil WF">Noto Sans Tamil WF</option>\
-        </optgroup>\
-        <optgroup label="Mac fonts">\
-        <option value="InaiMathi">InaiMathi</option>\
-        <option value="Tamil MN">Tamil MN</option>\
-        <option value="Tamil Sangam MN">Tamil Sangam MN</option>\
-        </optgroup>\
-        \
-        <optgroup label="Windows fonts">\
-        <option value="Latha">Latha</option>\
-        <option value="Vijaya">Vijaya</option>\
-        </optgroup>\
-        \
-        <optgroup label="Google fonts">\
-        <option value="Droid Sans Tamil">Droid Sans Tamil</option>\
-        <option value="Karla Tamil Inclined">Karla Tamil Inclined</option>\
-        <option value="Karla Tamil Upright">Karla Tamil Upright</option>\
-        <option value="Lohit Tamil">Lohit Tamil</option>\
-        <option value="Noto Sans Tamil">Noto Sans Tamil</option>\
-        </optgroup>\
-        '
+ var fontSelection = `<optgroup label="Webfont">
+        <option value="Noto Sans Tamil WF">Noto Sans Tamil WF</option>
+        </optgroup>
+        
+        <optgroup label="Mac fonts">
+        <option value="InaiMathi">InaiMathi</option>
+        <option value="Tamil MN">Tamil MN</option>
+        <option value="Tamil Sangam MN">Tamil Sangam MN</option>
+        </optgroup>
+        
+        <optgroup label="Windows fonts">
+        <option value="Latha">Latha</option>
+        <option value="Vijaya">Vijaya</option>
+        </optgroup>
+        
+        <optgroup label="Google fonts">
+        <option value="Noto Sans Tamil">Noto Sans Tamil</option>
+        <option value="Noto Serif Tamil">Noto Serif Tamil</option>
+        </optgroup>
+
+        <optgroup label="Other fonts ">
+         <option value="Droid Sans Tamil">Droid Sans Tamil</option>
+        <option value="Karla Tamil Inclined">Karla Tamil Inclined</option>
+        <option value="Karla Tamil Upright">Karla Tamil Upright</option>
+        <option value="Lohit Tamil">Lohit Tamil</option>
+       <option value="Maduram">Maduram</option>
+        </optgroup>
+        
+      <optgroup label="General fonts">
+        <option value="Arial Unicode MS">Arial Unicode MS</option>
+        <option value="Code2000">Code2000</option>
+        <option value="Microsoft Sans Serif">Microsoft Sans Serif</option>
+        <option value="sans-serif">sans-serif</option>
+        <option value="serif">serif</option>
+        </optgroup>`
