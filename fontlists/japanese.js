6c08515a7a9f353d28a86c825884be2ade177668
r12a <ishida@w3.org>
2019-01-04 08:53:19 +0000

    + japanese & samaritan

---
+++ fontlists/japanese.js
@@ -0,0 +1,22 @@
+ var fontSelection = `<option value="0">Select...</option>
+            <optgroup label="Windows fonts ">
+            <option value="Meiryo">Meiryo</option>
+            <option value="MS Gothic">MS Gothic</option>
+            <option value="MS Mincho">MS Mincho</option>
+            </optgroup>
+            
+            <optgroup label="Mac fonts ">
+            <option value="Hiragino Kaku Gothic Pro">Hiragino Kaku Gothic Pro</option>
+            <option value="Hiragino Maru Gothic Pro">Hiragino Maru Gothic Pro</option>
+            <option value="Osaka">Osaka</option>
+            <option value="Hiragino Mincho Pro">Hiragino Mincho Pro</option>
+          </optgroup>
+            
+            <optgroup label="Google fonts ">
+            <option value="Noto Sans CJK JP">Noto Sans CJK JP</option>
+            <option value="Noto Serif CJK JP">Noto Serif CJK JP</option>
+            </optgroup>
+            
+            <optgroup label="Other fonts ">
+            <option value="Arial Unicode MS">Arial Unicode MS</option>
+            </optgroup>`
---
+++ fontlists/samaritan.js
@@ -0,0 +1,8 @@
+ var fontSelection = `<option value="0">Select...</option>
+             <optgroup label="Web fonts">
+            <option value="Samaritan WF">Samaritan WF</option>
+            </optgroup>
+
+            <optgroup label="Google fonts ">
+            <option value="Noto Sans Samaritan">Noto Sans Samaritan</option>
+            </optgroup>`
