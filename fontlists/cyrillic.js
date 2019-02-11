ec80a8d794767498e25fca42e06e8979c9629016
r12a <ishida@w3.org>
2019-02-11 20:08:42 +0000

    fontlist cyrillic, Expand list of fonts

--- fontlists/cyrillic.js
+++ fontlists/cyrillic.js
@@ -1,8 +1,24 @@
  var fontSelection = `
- 		<option value="Doulos SIL">Doulos SIL WF</option>
+        <optgroup label="Google fonts ">
  		<option value="Noto Serif">Noto Serif</option>
+ 		<option value="Noto Sans">Noto Sans</option>
+        </optgroup>
+            
+        <optgroup label="SIL fonts ">
+        <option value="Gentium Plus">Gentium Plus</option>
         <option value="Gentium">Gentium</option>
-        <option value="Doulos SIL">Doulos SIL</option>
-        <option value="Times New Roman">Times New Roman</option>
+ 		<option value="Doulos SIL">Doulos SIL WF</option>
+        </optgroup>
+            
+        <optgroup label="Other fonts ">
+        <option value="Brill">Brill</option>
+        </optgroup>
+            
+        <optgroup label="General fonts ">
         <option value="Arial">Arial</option>
+        <option value="Arial Unicode MS">Arial Unicode MS</option>
+        <option value="Times New Roman">Times New Roman</option>
+        <option value="sans-serif">sans-serif</option>
+        <option value="serif">serif</option>
+        </optgroup>
 `
