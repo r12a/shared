af2347db011ba68d494eaadb5b4781e03d6205f2
r12a <ishida@w3.org>
2019-03-10 14:04:28 +0000

    fontlist, +georgian

---
+++ fontlists/georgian.js
@@ -0,0 +1,29 @@
+ var fontSelection = '     <option value="0">Select...</option>\
+             <optgroup label="Web font">\
+            <option value="Noto Sans Georgian WF">Noto Sans Georgian WF</option>\
+            </optgroup>\
+            \
+           <optgroup label="Mac fonts ">\
+            <option value="Menlo">Menlo</option>\
+            </optgroup>\
+            \
+            <optgroup label="Windows fonts ">\
+            <option value="Sylfaen">Sylfaen</option>\
+            </optgroup>\
+            \
+            <optgroup label="Google fonts ">\
+            <option value="Noto Serif Georgian">Noto Serif Georgian</option>\
+            <option value="Noto Sans Georgian">Noto Sans Georgian</option>\
+            </optgroup>\
+            \
+            <optgroup label="Other fonts ">\
+            <option value="BPG Classic 99U">BPG Classic 99U</option>\
+            <option value="BPG Paata Khutsuri U">BPG Paata Khutsuri U</option>\
+            </optgroup>\
+            \
+            <optgroup label="General fonts ">\
+            <option value="Arial Unicode MS">Arial Unicode MS</option>\
+            <option value="Code2000">Code2000</option>\
+            <option value="sans-serif">sans-serif</option>\
+            <option value="serif">serif</option>\
+            </optgroup>'
