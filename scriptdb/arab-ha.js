f316f6fd2b9b7c636e839b983632259a70715aef
r12a <ishida@w3.org>
2019-07-18 10:34:53 +0100

    scriptdb + arab-ha

---
+++ scriptdb/arab-ha.js
@@ -0,0 +1,109 @@
+var spreadsheet = `
+ٻ		b	ɓ	ɓ		ɓ	implosive	hafs orthography	ٻ ٻٻٻ			U+067B: ARABIC LETTER BEEH
+ݑ		b	ɓ̇	ɓ		ɓ	implosive	warsh orthography	ݑ ݑݑݑ			U+0751: ARABIC LETTER BEH WITH DOT BELOW AND THREE DOTS ABOVE
+ط		d	ɗ	ɗ t͡sʼ		ɗ ts	implosive/ejective		ط ططط			U+0637: ARABIC LETTER TAH
+ڟ		t	ʦ	t͡sʼ		ts	ejective		ڟ ڟڟڟ			U+069F: ARABIC LETTER TAH WITH THREE DOTS ABOVE
+ق		k	ƙ	kʼ		ƙ	ejective	hafs orthography	ق ققق			U+0642: ARABIC LETTER QAF
+ࢼ		k	ƙ̇	kʼ		ƙ	ejective	warsh orthography	ࢼ ࢼࢼࢼ			U+08BC: ARABIC LETTER AFRICAN QAF
+												
+												
+ب		¶b	b	b (ɓ)		b ɓ	plosive/implosive		ب ببب			U+0628: ARABIC LETTER BEH
+ت		¶t	t	t		t	plosive		ت تتت			U+062A: ARABIC LETTER TEH
+د		¶d	d	d (ɗ)		d ɗ	plosive/implosive		د ـد			U+062F: ARABIC LETTER DAL
+ث		t	ʧ	t͡ʃ		c	plosive		ث ثثث			U+062B: ARABIC LETTER THEH
+ك		¶k	k	k (kʼ)		k (ƙ)	plosive/ejective	hafs orthography	ك ككك			U+0643: ARABIC LETTER KAF
+ک		k	k	k		k	plosive/ejective	warsh orthography	ک ککک			U+06A9: ARABIC LETTER KEHEH
+غ		g	ɡ	ɡ		g	plosive		غ غغغ			U+063A: ARABIC LETTER GHAIN
+ع		}	ʔ	ʔ		ʼ	glottal stop		ع ععع			U+0639: ARABIC LETTER AIN
+												
+												
+ف		f	f	ɸ		f	fricative	hafs orthography	ف ففف			U+0641: ARABIC LETTER FEH
+ࢻ		f	ḟ	ɸ		f	fricative	warsh orthography	ࢻ ࢻࢻࢻ			U+08BB: ARABIC LETTER AFRICAN FEH
+پ		f	f̄	(ɸ)		f	fricative	non-standard	پ پپ			U+067E: ARABIC LETTER PEH
+س		s	s	s		s	fricative		س سسس			U+0633: ARABIC LETTER SEEN
+ص		s	s̄	s		s	fricative	non-standard	ص صصص			U+0635: ARABIC LETTER SAD
+ز		z	z	z		z	fricative		ز ـز			U+0632: ARABIC LETTER ZAIN
+ذ		z	ż	z		z	fricative	non-standard	ذ ـذ			U+0630: ARABIC LETTER THAL
+ظ		z	z̄	z		z	fricative	non-standard	ظ ظظظ			U+0638: ARABIC LETTER ZAH
+ش		s	ʃ	ʃ		sh	fricative		ش ششش			U+0634: ARABIC LETTER SHEEN
+ج		d	ʒ	ʒ d͡ʒ		j	plosive/affricate		ج ججج			U+062C: ARABIC LETTER JEEM
+ه		h¶	h	h		h	fricative		ه ههه			U+0647: ARABIC LETTER HEH																		
+ح		h¶	ħ	h		h	fricative		ح ححح			U+062D: ARABIC LETTER HAH																		
+												
+م		m	m	m		m	nasal		م ممم			U+0645: ARABIC LETTER MEEM
+ن		n	n	n		n	nasal	hafs orthography	ن ننن			U+0646: ARABIC LETTER NOON
+ࢽ		n	ṅ	n		n	nasal	warsh orthography	ࢽ ࢽࢽࢽ			U+08BD: ARABIC LETTER AFRICAN NOON
+												
+و		w¶	w	w		w	approximant/vowel		و ـو			U+0648: ARABIC LETTER WAW
+ل		l	l	l		l	approximant		ل للل			U+0644: ARABIC LETTER LAM
+ض		l	ḻ	l		l	approximant	non-standard				U+0636: ARABIC LETTER DAD
+ر		r	r	r ɽ		r	trill/flap		ر ـر			U+0631: ARABIC LETTER REH
+ی		y¶	y	j		y	approximant/vowel		ی ییی			U+06CC: ARABIC LETTER FARSI YEH
+ۑ		y	ƴ	j̰		ƴ ʼy	creaky approximant	hafs orthography	ۑ ۑۑۑ			U+06D1: ARABIC LETTER YEH WITH THREE DOTS BELOW
+ؿ		y	ƴ̇	j̰		ƴ ʼy	creaky approximant	warsh orthography	ؿ ؿؿؿ			U+063F: ARABIC LETTER FARSI YEH WITH THREE DOTS ABOVE
+												
+												
+ِ		i	i	i		i	vowel					U+0650: ARABIC KASRA​
+إِ	s	i	i	i		i	vowel					U+0625 U+0650: ARABIC LETTER ALEF WITH HAMZA BELOW, KASRA
+ِى	s	i	ī	iː		i	vowel		ـِىِى			U+0650 U+0649: ARABIC KASRA, LETTER ALEF MAKSURA​
+ىِٕ	s	i	ī	iː		i	vowel		ىِٕ ىِٕـ			U+0649 U+0650 U+0655: ARABIC LETTER ALEF MAKSURA, KASRA, HAMZA BELOW
+ُ		u	u	u o		o u	vowel					U+064F: ARABIC DAMMA​
+ُو	s	u	ū	uː oː		o u	vowel		ُو ـُو			U+064F U+0648: ARABIC DAMMA, LETTER WAW​
+ٜ		e	e	e		e	vowel					U+065C: ARABIC VOWEL SIGN DOT BELOW​
+ىٰٜ	s	e	ē	eː		e	vowel		ىٰٜ ىٰٜـ			U+0649 U+0670 U+065C: ARABIC LETTER ALEF MAKSURA, LETTER SUPERSCRIPT ALEF, VOWEL SIGN DOT BELOW
+ٰٜ	s	e	ē	eː		e	vowel					U+0670 U+065C: ARABIC LETTER SUPERSCRIPT ALEF, VOWEL SIGN DOT BELOW​
+ٰٜى	s	e	ē	eː		e	vowel		ـٰٜى			U+0670 U+065C U+0649: ARABIC LETTER SUPERSCRIPT ALEF, VOWEL SIGN DOT BELOW, LETTER ALEF MAKSURA​
+ُواْ	s	o	ō	oː		o	vowel		ـُواْ			U+064F U+0648 U+0627 U+0652: ARABIC DAMMA, LETTER WAW, LETTER ALEF, SUKUN​
+َ		a¶	a	a		a	vowel					U+064E: ARABIC FATHA​
+أَ	s	a	a	a		a	vowel					U+0623 U+064E: ARABIC LETTER ALEF WITH HAMZA ABOVE, FATHA
+َا	s	a	ā	aː		a	vowel		َا ـَا			U+064E U+0627: ARABIC FATHA, LETTER ALEF​
+												
+												
+ا		a	ā	aː			vowel		ا ـا			U+0627: ARABIC LETTER ALEF
+ى		i	ī	iː			vowel		ى ىىى			U+0649: ARABIC LETTER ALEF MAKSURA
+أ	dc	a	a͑	a	\u{627}\u{654}		vowel		أ ـأ			U+0623: ARABIC LETTER ALEF WITH HAMZA ABOVE
+إ	dc-	a	ɑ̜	ʔi	\u{627}\u{655}		consonant+vowel		إ ـإ			U+0625: ARABIC LETTER ALEF WITH HAMZA BELOW
+آ	dc-	a	ɑ̄	ʔaʔ ʔaː	\u{627}\u{653}		vowel		آ ـآ			U+0622: ARABIC LETTER ALEF WITH MADDA ABOVE
+ٱ	-	a	ɑ̃	a			consonant+vowel	infrequent	ٱ ـٱ			U+0671: ARABIC LETTER ALEF WASLA
+												
+												
+ٰ	o	[	̍				diacritic	used in long e ٰٜ				U+0670: ARABIC LETTER SUPERSCRIPT ALEF​
+ٔ	o	[	ʿ	ʾ			diacritic	used with أ				U+0654: ARABIC HAMZA ABOVE​
+ّ		&	˖				gemination mark					U+0651: ARABIC SHADDA​
+ْ		*	͓				vowel absence marker					U+0652: ARABIC SUKUN​
+
+
+`
+
+
+
+
+var cols = {
+"class": 1,
+"key":2,
+"transLoc": 3,
+"ipaLoc": 4,
+"equiv": 5,
+"transckey": 0,
+"transcription": 6,
+"typeLoc": 7,
+"statusLoc": 8,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 9,
+"numLoc": 0,
+"nameLoc": 10,
+"nnameLoc": 11,
+"ucsName": 12,
+
+"othertranscriptions": [[6, 'Latin']]
+}
+
+
+
