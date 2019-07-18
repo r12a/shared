a5ec3e3b90763e6a38f8a10248f8e772b32d9110
r12a <ishida@w3.org>
2019-07-18 10:34:24 +0100

    + ajami fontlist

---
+++ fontlists/ajami.js
@@ -0,0 +1,76 @@
+ var fontSelection = `<option value="0">Select...</option>
+            <optgroup label="Web fonts ">
+            <option value="Alkalami WF">Akalami WF</option>
+            <option value="Alkalami Light WF">Alkalami Light WF</option>
+            <option value="Scheherazade WF">Scheherazade WF</option>
+            <option value="Noto Naskh Arabic WF">Noto Naskh Arabic WF</option>
+            </optgroup>
+            
+            <optgroup label="Mac fonts ">
+            <option value="Al Bayan">Al Bayan</option>
+            <option value="Al Nile">Al Nile</option>
+            <option value="Al Tarikh">Al Tarikh</option>
+            <option value="Baghdad">Baghdad</option>
+            <option value="Beirut">Beirut</option>
+            <option value="Damascus">Damascus</option>
+            <option value="Diwan Kufi">Diwan Kufi</option>
+            <option value="Diwan Thuluth">Diwan Thuluth</option>
+            <option value="Decotype Naskh">Decotype Naskh</option>
+            <option value="Farah">Farah</option>
+            <option value="Farisi">Farisi</option>
+            <option value="Geeza Pro">Geeza Pro</option>
+            <option value="KufiStandardGK">KufiStandardGK</option>
+            <option value="Mishafi">Mishafi</option>
+            <option value="Mishafi Gold">Mishafi Gold</option>
+            <option value="Muna">Muna</option>
+            <option value="Myriad Arabic">Myriad Arabic</option>
+            <option value="Nadeem">Nadeem</option>
+            <option value="Sana">Sana</option>
+            <option value="Waseem">Waseem</option>
+            </optgroup>
+            
+            <optgroup label="Windows fonts ">
+            <option value="Arabic Typesetting">Arabic Typesetting</option>
+            <option value="Aldhabi">Aldhabi</option>
+            <option value="Andalus">Andalus</option>
+            <option value="Microsoft Uighur">Microsoft Uighur </option>
+            <option value="Sakkal Majalla">Sakkal Majalla</option>
+            <option value="Simplified Arabic">Simplified Arabic</option>
+            <option value="Simplified Arabic Fixed">Simplified Arabic Fixed</option>
+            </optgroup>
+            
+            <optgroup label="Google fonts ">
+            <option value="Scheherazade">Scheherazade</option>
+            <option value="Noto Naskh Arabic">Noto Naskh Arabic</option>
+            </optgroup>
+            
+            <optgroup label="SIL fonts ">
+            <option value="Alkalami">Akalami</option>
+            <option value="Alkalami Light">Alkalami Light</option>
+            <option value="Scheherazade">Scheherazade</option>
+            </optgroup>
+            
+            <optgroup label="Other fonts ">
+            <option value="Amiri">Amiri</option>
+            <option value="Amiri Quran">Amiri Quran</option>
+            <option value="Arabic Transparent">Arabic Transparent</option>
+            <option value="Awami Nastaliq">Awami Nastaliq</option>
+            <option value="Farsi Simple">Farsi Simple</option>
+            <option value="Koodak">Koodak</option>
+            <option value="Koodak">Koodak</option>
+            <option value="Nazli">Nazli</option>
+            <option value="PakType Tehreer">PakType Tehreer</option>
+            </optgroup>
+            
+            <optgroup label="General fonts ">
+            <option value="Arial">Arial</option>
+            <option value="Arial Unicode MS">Arial Unicode MS</option>
+            <option value="Code2000">Code2000</option>
+            <option value="Courier New">Courier New</option>
+            <option value="DejaVu Sans">DejaVu Sans</option>
+            <option value="Tahoma">Tahoma</option>
+            <option value="Times New Roman">Times New Roman</option>
+            <option value="sans-serif">sans-serif</option>
+            <option value="serif">serif</option>
+            </optgroup>
+       `
