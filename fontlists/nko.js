b1aea3b2dc3281423f59f6e750af987971ea2765
r12a <ishida@w3.org>
2019-07-26 08:54:32 +0100

    nko fontlist, Add Conakry

--- fontlists/nko.js
+++ fontlists/nko.js
@@ -1,15 +1,16 @@
- var fontSelection = 'option value="0">Select...</option>\
-			<optgroup label="Web font">\
- 							<option value="Noto Sans NKo WF">Noto Sans NKo WF</option>\
-            </optgroup>\
-            </optgroup>\
-            <optgroup label="Windows fonts">\
- 							<option value="Ebrima">Ebrima</option>\
-           </optgroup>\
-			<optgroup label="Google fonts">\
- 							<option value="Noto Serif NKo">Noto Serif NKo</option>\
-            </optgroup>\
-			<optgroup label="General fonts">\
-							<option value="sans-serif">sans-serif</option>\
-							<option value="serif">serif</option>\
-                            </optgroup>'
+ var fontSelection = `option value="0">Select...</option>
+			<optgroup label="Web font">
+ 							<option value="Noto Sans NKo WF">Noto Sans NKo WF</option>
+            </optgroup>
+            </optgroup>
+            <optgroup label="Windows fonts">
+ 							<option value="Ebrima">Ebrima</option>
+           </optgroup>
+			<optgroup label="Google fonts">
+ 							<option value="Noto Serif NKo">Noto Serif NKo</option>
+            </optgroup>
+			<optgroup label="General fonts">
+							<option value="Conakry">Conakry</option>
+							<option value="sans-serif">sans-serif</option>
+							<option value="serif">serif</option>
+                            </optgroup>`
