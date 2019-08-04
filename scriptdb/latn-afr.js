f4e8156d767e30fd018d04b325fd77ce204eeba5
r12a <ishida@w3.org>
2019-08-04 09:31:05 +0100

    + scriptdb/latn-afr

---
+++ scriptdb/latn-afr.js
@@ -0,0 +1,214 @@
+var spreadsheet = `
+b		¶b	b		consonant					U+0062: LATIN SMALL LETTER B
+ɓ		b	ɓ		consonant					U+0253: LATIN SMALL LETTER B WITH HOOK
+c		¶c	c		consonant					U+0063: LATIN SMALL LETTER C
+ƈ		c	ƈ		consonant					U+0188: LATIN SMALL LETTER C WITH HOOK
+d		¶d	d		consonant					U+0064: LATIN SMALL LETTER D
+ɖ		d	ɖ		consonant					U+0256: LATIN SMALL LETTER D WITH TAIL
+ɗ		d	ɗ		consonant					U+0257: LATIN SMALL LETTER D WITH HOOK
+đ		d	đ		consonant					U+0111: LATIN SMALL LETTER D WITH STROKE
+ð		d	ð		consonant					U+00F0: LATIN SMALL LETTER ETH
+f		¶f	f		consonant					U+0066: LATIN SMALL LETTER F
+ƒ		f	ƒ		consonant					U+0192: LATIN SMALL LETTER F WITH HOOK
+g		¶g	g		consonant					U+0067: LATIN SMALL LETTER G
+ɠ		g	ɠ		consonant					U+0260: LATIN SMALL LETTER G WITH HOOK
+h		¶h	h		consonant					U+0068: LATIN SMALL LETTER H
+ɦ		h	ɦ		consonant					U+0266: LATIN SMALL LETTER H WITH HOOK
+j		¶j	j		consonant					U+006A: LATIN SMALL LETTER J
+ɟ		j	ɟ		consonant					U+025F: LATIN SMALL LETTER DOTLESS J WITH STROKE
+k		¶k	k		consonant					U+006B: LATIN SMALL LETTER K
+ƙ		k	ƙ		consonant					U+0199: LATIN SMALL LETTER K WITH HOOK
+l		¶l	l		consonant					U+006C: LATIN SMALL LETTER L
+λ		l	λ		consonant					U+03BB: GREEK SMALL LETTER LAMDA
+m		m	m		consonant					U+006D: LATIN SMALL LETTER M
+n		¶n	n		consonant					U+006E: LATIN SMALL LETTER N
+ɴ		n	ɴ		consonant					U+0274: LATIN LETTER SMALL CAPITAL N
+ŋ		n	ŋ		consonant					U+014B: LATIN SMALL LETTER ENG
+ɲ		n	ɲ		consonant					U+0272: LATIN SMALL LETTER N WITH LEFT HOOK
+ǌ		n	ǌ		consonant					U+01CC: LATIN SMALL LETTER NJ
+p		¶p	p		consonant					U+0070: LATIN SMALL LETTER P
+ƥ		p	ƥ		consonant					U+01A5: LATIN SMALL LETTER P WITH HOOK
+q		¶q	q		consonant					U+0071: LATIN SMALL LETTER Q
+ꝗ		q	ꝗ		consonant					U+A757: LATIN SMALL LETTER Q WITH STROKE THROUGH DESCENDER
+r		¶r	r		consonant					U+0072: LATIN SMALL LETTER R
+ɍ		r	ɍ		consonant					U+024D: LATIN SMALL LETTER R WITH STROKE
+ɽ		r	ɽ		consonant					U+027D: LATIN SMALL LETTER R WITH TAIL
+s		¶s	s		consonant					U+0073: LATIN SMALL LETTER S
+ʃ		s	ʃ		consonant					U+0283: LATIN SMALL LETTER ESH
+t		¶t	t		consonant					U+0074: LATIN SMALL LETTER T
+ƭ		t	ƭ		consonant					U+01AD: LATIN SMALL LETTER T WITH HOOK
+ʈ		t	ʈ		consonant					U+0288: LATIN SMALL LETTER T WITH RETROFLEX HOOK
+v		¶v	v		consonant					U+0076: LATIN SMALL LETTER V
+ʋ		v	ʋ		consonant					U+028B: LATIN SMALL LETTER V WITH HOOK
+w		¶w	w		consonant					U+0077: LATIN SMALL LETTER W
+ω		w	ω		consonant					U+03C9: GREEK SMALL LETTER OMEGA
+ʷ		w	ʷ		consonant					U+02B7: MODIFIER LETTER SMALL W
+x		x	x		consonant					U+0078: LATIN SMALL LETTER X
+y		¶y	y		consonant					U+0079: LATIN SMALL LETTER Y
+ƴ		y	ƴ		consonant					U+01B4: LATIN SMALL LETTER Y WITH HOOK
+z		¶z	z		consonant					U+007A: LATIN SMALL LETTER Z
+ʒ		z	ʒ		consonant					U+0292: LATIN SMALL LETTER EZH
+ƹ		z	ƹ		consonant					U+01B9: LATIN SMALL LETTER EZH REVERSED
+ɣ		y	ɣ		consonant					U+0263: LATIN SMALL LETTER GAMMA
+θ		q	θ		consonant					U+03B8: GREEK SMALL LETTER THETA
+ʔ		}	ʔ		consonant					U+0294: LATIN LETTER GLOTTAL STOP
+ǀ		|	ǀ		consonant					U+01C0: LATIN LETTER DENTAL CLICK
+ǁ		|	ǁ		consonant					U+01C1: LATIN LETTER LATERAL CLICK
+ǂ		|	ǂ		consonant					U+01C2: LATIN LETTER ALVEOLAR CLICK
+ǃ		|	ǃ		consonant					U+01C3: LATIN LETTER RETROFLEX CLICK
+ꞌ		'	ꞌ		consonant					U+A78C: LATIN SMALL LETTER SALTILLO
+ʼ		'	ʼ		consonant					U+02BC: MODIFIER LETTER APOSTROPHE
+										
+										
+B		¶B	B		uppercase consonant					U+0042: LATIN CAPITAL LETTER B
+Ɓ		B	Ɓ		uppercase consonant					U+0181: LATIN CAPITAL LETTER B WITH HOOK
+C		¶C	C		uppercase consonant					U+0043: LATIN CAPITAL LETTER C
+Ƈ		C	Ƈ		uppercase consonant					U+0187: LATIN CAPITAL LETTER C WITH HOOK
+D		¶D	D		uppercase consonant					U+0044: LATIN CAPITAL LETTER D
+Ɖ		D	Ɖ		uppercase consonant					U+0189: LATIN CAPITAL LETTER AFRICAN D
+Ɗ		D	Ɗ		uppercase consonant					U+018A: LATIN CAPITAL LETTER D WITH HOOK
+Đ		D	Đ		uppercase consonant					U+0110: LATIN CAPITAL LETTER D WITH STROKE
+Ð		D	Ð		uppercase consonant					U+00D0: LATIN CAPITAL LETTER ETH
+F		¶F	F		uppercase consonant					U+0046: LATIN CAPITAL LETTER F
+Ƒ		F	Ƒ		uppercase consonant					U+0191: LATIN CAPITAL LETTER F WITH HOOK
+G		¶G	G		uppercase consonant					U+0047: LATIN CAPITAL LETTER G
+Ɠ		G	Ɠ		uppercase consonant					U+0193: LATIN CAPITAL LETTER G WITH HOOK
+H		¶H	H		uppercase consonant					U+0048: LATIN CAPITAL LETTER H
+Ɦ		H	Ɦ		uppercase consonant					U+A7AA: LATIN CAPITAL LETTER H WITH HOOK
+J		¶J	J		uppercase consonant					U+004A: LATIN CAPITAL LETTER J
+ɟ		J	ɟ		uppercase consonant					U+025F: LATIN SMALL LETTER DOTLESS J WITH STROKE
+K		¶K	K		uppercase consonant					U+004B: LATIN CAPITAL LETTER K
+Ƙ		K	Ƙ		uppercase consonant					U+0198: LATIN CAPITAL LETTER K WITH HOOK
+L		¶L	L		uppercase consonant					U+004C: LATIN CAPITAL LETTER L
+Λ		L	Λ		uppercase consonant					U+039B: GREEK CAPITAL LETTER LAMDA
+M		M	M		uppercase consonant					U+004D: LATIN CAPITAL LETTER M
+N		¶N	N		uppercase consonant					U+004E: LATIN CAPITAL LETTER N
+ɴ		N	ɴ		uppercase consonant					U+0274: LATIN LETTER SMALL CAPITAL N
+Ŋ		N	Ŋ		uppercase consonant					U+014A: LATIN CAPITAL LETTER ENG
+Ɲ		N	Ɲ		uppercase consonant					U+019D: LATIN CAPITAL LETTER N WITH LEFT HOOK
+Ǌ		N	Ǌ		uppercase consonant					U+01CA: LATIN CAPITAL LETTER NJ
+P		¶P	P		uppercase consonant					U+0050: LATIN CAPITAL LETTER P
+Ƥ		P	Ƥ		uppercase consonant					U+01A4: LATIN CAPITAL LETTER P WITH HOOK
+Q		¶Q	Q		uppercase consonant					U+0051: LATIN CAPITAL LETTER Q
+Ꝗ		Q	Ꝗ		uppercase consonant					U+A756: LATIN CAPITAL LETTER Q WITH STROKE THROUGH DESCENDER
+R		¶R	R		uppercase consonant					U+0052: LATIN CAPITAL LETTER R
+Ɍ		R	Ɍ		uppercase consonant					U+024C: LATIN CAPITAL LETTER R WITH STROKE
+Ɽ		R	Ɽ		uppercase consonant					U+2C64: LATIN CAPITAL LETTER R WITH TAIL
+S		¶S	S		uppercase consonant					U+0053: LATIN CAPITAL LETTER S
+Ʃ		S	Ʃ		uppercase consonant					U+01A9: LATIN CAPITAL LETTER ESH
+T		¶T	T		uppercase consonant					U+0054: LATIN CAPITAL LETTER T
+Ƭ		T	Ƭ		uppercase consonant					U+01AC: LATIN CAPITAL LETTER T WITH HOOK
+Ʈ		T	Ʈ		uppercase consonant					U+01AE: LATIN CAPITAL LETTER T WITH RETROFLEX HOOK
+V		¶V	V		uppercase consonant					U+0056: LATIN CAPITAL LETTER V
+Ʋ		V	Ʋ		uppercase consonant					U+01B2: LATIN CAPITAL LETTER V WITH HOOK
+W		¶W	W		uppercase consonant					U+0057: LATIN CAPITAL LETTER W
+Ω		W	Ω		uppercase consonant					U+03A9: GREEK CAPITAL LETTER OMEGA
+X		W	X		uppercase consonant					U+0058: LATIN CAPITAL LETTER X
+Y		X	Y		uppercase consonant					U+0059: LATIN CAPITAL LETTER Y
+Ƴ		¶Y	Ƴ		uppercase consonant					U+01B3: LATIN CAPITAL LETTER Y WITH HOOK
+Z		Y	Z		uppercase consonant					U+005A: LATIN CAPITAL LETTER Z
+Ʒ	s	¶Z	Ʒ		uppercase consonant					U+01B7: LATIN CAPITAL LETTER EZH
+Ƹ		Z	Ƹ		uppercase consonant					U+01B8: LATIN CAPITAL LETTER EZH REVERSED
+Ɣ		Z	Ɣ		uppercase consonant					U+0194: LATIN CAPITAL LETTER GAMMA
+Θ		Q	Θ		uppercase consonant					U+0398: GREEK CAPITAL LETTER THETA
+Ꞌ		'	Ꞌ		uppercase consonant					U+A78B: LATIN CAPITAL LETTER SALTILLO
+										
+										
+										
+										
+a		a	a		vowel					U+0061: LATIN SMALL LETTER A
+ɑ		a	ɑ		vowel					U+0251: LATIN SMALL LETTER ALPHA
+ʌ		a	ʌ		vowel					U+028C: LATIN SMALL LETTER TURNED V
+e		e	e		vowel					U+0065: LATIN SMALL LETTER E
+ə		e	ə		vowel					U+0259: LATIN SMALL LETTER SCHWA
+ǝ		e	ǝ		vowel					U+01DD: LATIN SMALL LETTER TURNED E
+ɛ		e	ɛ		vowel					U+025B: LATIN SMALL LETTER OPEN E
+i		i	i		vowel					U+0069: LATIN SMALL LETTER I
+ɨ		i	ɨ		vowel					U+0268: LATIN SMALL LETTER I WITH STROKE
+ɩ		i	ɩ		vowel					U+0269: LATIN SMALL LETTER IOTA
+ɪ		i	ɪ		vowel					U+026A: LATIN LETTER SMALL CAPITAL I
+o		o	o		vowel					U+006F: LATIN SMALL LETTER O
+ø		o	ø		vowel					U+00F8: LATIN SMALL LETTER O WITH STROKE
+ɔ		o	ɔ		vowel					U+0254: LATIN SMALL LETTER OPEN O
+œ		o	œ		vowel					U+0153: LATIN SMALL LIGATURE OE
+u		u	u		vowel					U+0075: LATIN SMALL LETTER U
+ʉ		u	ʉ		vowel					U+0289: LATIN SMALL LETTER U BAR
+ʊ		u	ʊ		vowel					U+028A: LATIN SMALL LETTER UPSILON
+										
+A		A	A		uppercase vowel					U+0041: LATIN CAPITAL LETTER A
+Ɑ		A	Ɑ		uppercase vowel					U+2C6D: LATIN CAPITAL LETTER ALPHA
+Ʌ		A	Ʌ		uppercase vowel					U+0245: LATIN CAPITAL LETTER TURNED V
+E		E	E		uppercase vowel					U+0045: LATIN CAPITAL LETTER E
+Ə		E	Ə		uppercase vowel					U+018F: LATIN CAPITAL LETTER SCHWA
+Ǝ		E	Ǝ		uppercase vowel					U+018E: LATIN CAPITAL LETTER REVERSED E
+Ɛ		E	Ɛ		uppercase vowel					U+0190: LATIN CAPITAL LETTER OPEN E
+I		I	I		uppercase vowel					U+0049: LATIN CAPITAL LETTER I
+Ɨ		I	Ɨ		uppercase vowel					U+0197: LATIN CAPITAL LETTER I WITH STROKE
+Ɩ		I	Ɩ		uppercase vowel					U+0196: LATIN CAPITAL LETTER IOTA
+Ɪ		I	Ɪ		uppercase vowel					U+A7AE: LATIN CAPITAL LETTER SMALL CAPITAL I
+O		O	O		uppercase vowel					U+004F: LATIN CAPITAL LETTER O
+Ø		O	Ø		uppercase vowel					U+00D8: LATIN CAPITAL LETTER O WITH STROKE
+Ɔ		O	Ɔ		uppercase vowel					U+0186: LATIN CAPITAL LETTER OPEN O
+Œ		O	Œ		uppercase vowel					U+0152: LATIN CAPITAL LIGATURE OE
+U		U	U		uppercase vowel					U+0055: LATIN CAPITAL LETTER U
+Ʉ		U	Ʉ		uppercase vowel					U+0244: LATIN CAPITAL LETTER U BAR
+Ʊ		U	Ʊ		uppercase vowel					U+01B1: LATIN CAPITAL LETTER UPSILON
+										
+										
+										
+̧		]	̧		diacritic					U+0327: COMBINING CEDILLA​
+̱		]	̱		diacritic					U+0331: COMBINING MACRON BELOW​
+̣		]	̣		diacritic					U+0323: COMBINING DOT BELOW​
+̭		]	̭		diacritic					U+032D: COMBINING CIRCUMFLEX ACCENT BELOW​
+̀		[	̀		diacritic					U+0300: COMBINING GRAVE ACCENT​
+́		[	́		diacritic					U+0301: COMBINING ACUTE ACCENT​
+̂		[	̂		diacritic					U+0302: COMBINING CIRCUMFLEX ACCENT​
+̌		[	̌		diacritic					U+030C: COMBINING CARON​
+̃		[	̃		diacritic					U+0303: COMBINING TILDE​
+̄		[	̄		diacritic					U+0304: COMBINING MACRON​
+̇		[	̇		diacritic					U+0307: COMBINING DOT ABOVE​
+̈		[	̈		diacritic					U+0308: COMBINING DIAERESIS​
+										
+										
+										
+		;								
+		,								
+«	-	_	«		quotation mark					U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK
+»	-		»		quotation mark					U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK
+’		'	’		apostrophe					U+2019: RIGHT SINGLE QUOTATION MARK
+
+
+`
+
+
+
+
+var cols = {
+"class": 1,
+"key":2,
+"transLoc": 3,
+"ipaLoc": 4,
+"transckey": 0,
+"transcription": 0,
+"typeLoc": 5,
+"statusLoc": 6,
+"equiv": 7,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nameLoc": 8,
+"nnameLoc": 9,
+"ucsName": 10,
+
+//"othertranscriptions": [[6, 'ISO 15919']]
+}
+
+
+
