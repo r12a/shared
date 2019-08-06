7defba95f1d55a6f437d4bad105f30563b264d32
r12a <ishida@w3.org>
2019-08-06 20:44:45 +0100

    fontlist + adlam

---
+++ fontlists/adlam.js
@@ -0,0 +1,19 @@
+ var fontSelection = `<option value="0">Select...</option>
+            <optgroup label="Web fonts ">
+            <option value="Noto Sans Adlam WF">Noto Sans Adlam WF</option>
+            <option value="Noto Sans Adlam Unjoined WF">Noto Sans Adlam Unjoined WF</option>
+            </optgroup>
+            
+            <optgroup label="Windows fonts ">
+            <option value="Ebrima">Ebrima</option>
+            </optgroup>
+            
+            <optgroup label="Google fonts ">
+            <option value="Noto Sans Adlam">Noto Sans Adlam</option>
+            <option value="Noto Sans Adlam Unjoined">Noto Sans Adlam Unjoined</option>
+            </optgroup>
+            
+            <optgroup label="Other fonts ">
+            <option value="Kigelia">Kigelia</option>
+            </optgroup>
+       `
