5edb4d5ab612f2d59a33133fa073fab82a662850
r12a <ishida@w3.org>
2019-08-08 18:43:04 +0100

    fontlists + african

---
+++ fontlists/african.js
@@ -0,0 +1,45 @@
+ var fontSelection = `
+ 	  <optgroup label="Webfont">
+      			<option value="Adelle Sans EXT Light WF">Adelle Sans EXT Light WF</option>
+      			<option value="Gentium Plus WF">Gentium Plus WF</option>
+      			<option value="Doulos SIL WF">Doulos SIL WF</option>
+      </optgroup>
+      <optgroup label="SIL fonts">
+                <option value="Gentium Plus">Gentium Plus</option>
+                <option value="Doulos SIL">Doulos SIL</option>
+                <option value="Charis SIL">Charis SIL</option>
+                <option value="Gentium">Gentium</option>
+                <option value="Gentium Plus">Gentium Plus</option>
+                <option value="GentiumAlt">GentiumAlt</option>
+     </optgroup>
+      <optgroup label="Wide coverage fonts">
+                <option value="Symbola">Symbola</option>
+                <option value="EversonMono">EversonMono</option>
+                <option value="Quivira">Quivira</option>
+                <option value="Unifont">Unifont</option>
+                <option value="Brill">Brill</option>
+     </optgroup>
+      <optgroup label="Other fonts">
+       			<option value="Adelle Sans EXT">Adelle Sans EXT</option>
+      			<option value="Adelle Sans EXT Light">Adelle Sans EXT Light</option>
+               <option value="Berling Antiqua">Berling Antiqua</option>
+                <option value="Cardo">Cardo</option>
+                <option value="Chrysanthi Unicode">Chrysanthi Unicode</option>
+                <option value="DejaVu Sans">DejaVu Sans </option>
+                <option value="DejaVu Serif">DejaVu Serif</option>
+                <option value="Dialekt Uni">Dialekt Uni</option>
+                <option value="Free Serif">Free Serif</option>
+                <option value="Frutiger Linotype">Frutiger Linotype</option>
+                <option value="Gandhari Unicode">Gandhari Unicode</option>
+                <option value="Hindsight Unicode">Hindsight Unicode</option>
+                <option value="IPA LS Uni">IPA LS Uni</option>
+                <option value="Lucida Sans">Lucida Sans</option>
+                <option value="Lucida Sans Unicode">Lucida Sans Unicode</option>
+                <option value="Thryomanes">Thryomanes</option>
+                <option value="TITUS Cyberbit Basic">TITUS Cyberbit Basic</option>
+                <option value="Arial Unicode MS">Arial Unicode MS</option>
+                <option value="Code2000">Code2000</option>
+                <option value="sans-serif">sans-serif</option>
+                <option value="serif">serif</option>
+      </optgroup>
+      `
