dc69e4ebc5c4a0a610e0f14af917d4969f33b92c
r12a <ishida@w3.org>
2020-01-12 14:36:03 +0000

    scriptdb & fontlists, tibetan -> tibt

---
+++ scriptdb/tibt.js
@@ -0,0 +1,315 @@
+var spreadsheet = `
+ཀ		k	k	kʰ	consonant				U+0F40: TIBETAN LETTER KA	tibetan
+ཁ		k	kʰ	kʰ	consonant				U+0F41: TIBETAN LETTER KHA	tibetan
+ག		g	g	k kʰ -g	consonant				U+0F42: TIBETAN LETTER GA	tibetan
+ང		n	ŋ	ŋ	consonant				U+0F44: TIBETAN LETTER NGA	tibetan
+ཅ		t	ʧ	ʧ	consonant				U+0F45: TIBETAN LETTER CA	tibetan
+ཆ		t	ʧʰ	ʧʰ	consonant				U+0F46: TIBETAN LETTER CHA	tibetan
+ཇ		d	ʤ	ʤ	consonant				U+0F47: TIBETAN LETTER JA	tibetan
+ཉ		n	ɲ	ɲ	consonant				U+0F49: TIBETAN LETTER NYA	tibetan
+ཏ		t¶	t	t	consonant				U+0F4F: TIBETAN LETTER TA	tibetan
+ཐ		t	tʰ	tʰ	consonant				U+0F50: TIBETAN LETTER THA	tibetan
+ད		d¶	d	d	consonant				U+0F51: TIBETAN LETTER DA	tibetan
+ན		n¶	n	n	consonant				U+0F53: TIBETAN LETTER NA	tibetan
+པ		p	p	p	consonant				U+0F54: TIBETAN LETTER PA	tibetan
+ཕ		p	pʰ	pʰ	consonant				U+0F55: TIBETAN LETTER PHA	tibetan
+བ		b	b	b	consonant				U+0F56: TIBETAN LETTER BA	tibetan
+མ		m	m	m	consonant				U+0F58: TIBETAN LETTER MA	tibetan
+ཙ		t	ʦ	ʦ	consonant				U+0F59: TIBETAN LETTER TSA	tibetan
+ཚ		t	ʦʰ	ʦʰ	consonant				U+0F5A: TIBETAN LETTER TSHA	tibetan
+ཛ		d	ʣ	ʣ	consonant				U+0F5B: TIBETAN LETTER DZA	tibetan
+ཝ		w	w	w	consonant				U+0F5D: TIBETAN LETTER WA	tibetan
+ཞ		z	ʒ	ʒ	consonant				U+0F5E: TIBETAN LETTER ZHA	tibetan
+ཟ		z	z	z	consonant				U+0F5F: TIBETAN LETTER ZA	tibetan
+འ		a	à	a	consonant		'a-chung		U+0F60: TIBETAN LETTER -A	tibetan
+ཡ		y	y	j	consonant				U+0F61: TIBETAN LETTER YA	tibetan
+ར		r	r	r	consonant				U+0F62: TIBETAN LETTER RA	tibetan
+ལ		l	l	l	consonant				U+0F63: TIBETAN LETTER LA	tibetan
+ཤ		s	ʃ	ʃ	consonant				U+0F64: TIBETAN LETTER SHA	tibetan
+ས		s	s	s	consonant				U+0F66: TIBETAN LETTER SA	tibetan
+ཧ		h	h	h	consonant				U+0F67: TIBETAN LETTER HA	tibetan
+ཨ		a	ạ	a	consonant				U+0F68: TIBETAN LETTER A	tibetan
+										
+										
+ྐ		K	k̰	kʰ	subjoined consonant				U+0F90: TIBETAN SUBJOINED LETTER KA​	tibetan
+ྑ		K	k̰ʰ	kʰ	subjoined consonant				U+0F91: TIBETAN SUBJOINED LETTER KHA​	tibetan
+ྒ		G	g̰	k kʰ -g	subjoined consonant				U+0F92: TIBETAN SUBJOINED LETTER GA​	tibetan
+ྔ		N	ŋ̰	ŋ	subjoined consonant				U+0F94: TIBETAN SUBJOINED LETTER NGA​	tibetan
+ྕ		T	ʧ̰	ʧ	subjoined consonant				U+0F95: TIBETAN SUBJOINED LETTER CA​	tibetan
+ྖ		T	ʧ̰ʰ	ʧʰ	subjoined consonant				U+0F96: TIBETAN SUBJOINED LETTER CHA​	tibetan
+ྗ		D	ʤ̰	ʤ	subjoined consonant				U+0F97: TIBETAN SUBJOINED LETTER JA​	tibetan
+ྙ		N	ɲ̰	ɲ	subjoined consonant				U+0F99: TIBETAN SUBJOINED LETTER NYA​	tibetan
+ྟ		T¶	t̰	t	subjoined consonant				U+0F9F: TIBETAN SUBJOINED LETTER TA​	tibetan
+ྠ		T	t̰ʰ	tʰ	subjoined consonant				U+0FA0: TIBETAN SUBJOINED LETTER THA​	tibetan
+ྡ		D¶	d̰	d	subjoined consonant				U+0FA1: TIBETAN SUBJOINED LETTER DA​	tibetan
+ྣ		N¶	n̰	n	subjoined consonant				U+0FA3: TIBETAN SUBJOINED LETTER NA​	tibetan
+ྤ		P	p̰	p	subjoined consonant				U+0FA4: TIBETAN SUBJOINED LETTER PA​	tibetan
+ྥ		P	p̰ʰ	pʰ	subjoined consonant				U+0FA5: TIBETAN SUBJOINED LETTER PHA​	tibetan
+ྦ		B	b̰	b	subjoined consonant				U+0FA6: TIBETAN SUBJOINED LETTER BA​	tibetan
+ྨ		M	m̰	m	subjoined consonant				U+0FA8: TIBETAN SUBJOINED LETTER MA​	tibetan
+ྩ		T	ʦ̰	ʦ	subjoined consonant				U+0FA9: TIBETAN SUBJOINED LETTER TSA​	tibetan
+ྪ		T	ʦ̰ʰ	ʦʰ	subjoined consonant				U+0FAA: TIBETAN SUBJOINED LETTER TSHA​	tibetan
+ྫ		D¶	ʣ̰	ʣ	subjoined consonant				U+0FAB: TIBETAN SUBJOINED LETTER DZA​	tibetan
+ྭ		W	w̰	w	subjoined consonant		wazur/wa-ta (wa-btags)		U+0FAD: TIBETAN SUBJOINED LETTER WA​	tibetan
+ྮ		Z	ʒ̰	ʒ	subjoined consonant				U+0FAE: TIBETAN SUBJOINED LETTER ZHA​	tibetan
+ྯ		Z	z̰	z	subjoined consonant				U+0FAF: TIBETAN SUBJOINED LETTER ZA​	tibetan
+ྰ		A	à̰	a	subjoined consonant				U+0FB0: TIBETAN SUBJOINED LETTER -A​	tibetan
+ྱ		Y	y̰	j	subjoined consonant		ya-ta (ya-btags)		U+0FB1: TIBETAN SUBJOINED LETTER YA​	tibetan
+ྲ		R	r̰	r	subjoined consonant		ra-ta (ra-btags)		U+0FB2: TIBETAN SUBJOINED LETTER RA​	tibetan
+ླ		L	l̰	l	subjoined consonant				U+0FB3: TIBETAN SUBJOINED LETTER LA​	tibetan
+ྵ		S	ʃ̰	ʃ	subjoined consonant				U+0FB5: TIBETAN SUBJOINED LETTER SSA​	tibetan
+ྶ		S	s̰	s	subjoined consonant				U+0FB6: TIBETAN SUBJOINED LETTER SA​	tibetan
+ྷ		H	h̰	h	subjoined consonant				U+0FB7: TIBETAN SUBJOINED LETTER HA​	tibetan
+ྸ		A	ạ̰	a	subjoined consonant				U+0FB8: TIBETAN SUBJOINED LETTER A​	tibetan
+										
+										
+ཫ		Q	q	q	balti consonant				U+0F6B: TIBETAN LETTER KKA	tibetan
+ཬ		R	ɽ	ɽ	balti consonant				U+0F6C: TIBETAN LETTER RRA	tibetan
+ཊ		T	ʈ	ʈ	sanskrit consonant				U+0F4A: TIBETAN LETTER TTA	tibetan
+ཋ		T	ʈʰ	ʈʰ	sanskrit consonant				U+0F4B: TIBETAN LETTER TTHA	tibetan
+ཌ		D	ɖ	ɖ	sanskrit consonant				U+0F4C: TIBETAN LETTER DDA	tibetan
+ཎ		N	ɳ	ɳ	sanskrit consonant				U+0F4E: TIBETAN LETTER NNA	tibetan
+\u0F43		£	gʰ	gʰ	precomposed sanskrit consonant				U+0F43: TIBETAN LETTER GHA	tibetan
+\u0F4D		£	ɖʰ	ɖʰ	precomposed sanskrit consonant				U+0F4D: TIBETAN LETTER DDHA	tibetan
+\u0F52		£	dʰ	d̪ʱ	precomposed sanskrit consonant				U+0F52: TIBETAN LETTER DHA	tibetan
+\u0F57		£	bʰ	bʰ	precomposed sanskrit consonant				U+0F57: TIBETAN LETTER BHA	tibetan
+\u0F5C		£	ʣʰ	ʣʰ	precomposed sanskrit consonant				U+0F5C: TIBETAN LETTER DZHA	tibetan
+\u0F69		£	kˢ	kʂ	precomposed sanskrit consonant				U+0F69: TIBETAN LETTER KSSA	tibetan
+ཥ		S	ʂ	ʂ	sanskrit consonant				U+0F65: TIBETAN LETTER SSA	tibetan
+ཪ		R	r̄		fixed form variant				U+0F6A: TIBETAN LETTER FIXED-FORM RA	tibetan
+										
+										
+				q	balti consonant					
+				ɽ	balti consonant					
+ྚ		T	ʈ̰	ʈ	sanskrit consonant				U+0F9A: TIBETAN SUBJOINED LETTER TTA​	tibetan
+ྛ		T	ʈ̰ʰ	ʈʰ	sanskrit consonant				U+0F9B: TIBETAN SUBJOINED LETTER TTHA​	tibetan
+ྜ		D	ɖ̰	ɖ	sanskrit consonant				U+0F9C: TIBETAN SUBJOINED LETTER DDA​	tibetan
+ྞ		N	ɳ̰	ɳ	sanskrit consonant				U+0F9E: TIBETAN SUBJOINED LETTER NNA​	tibetan
+\u0F93		£	g̰ʰ	gʰ	precomposed sanskrit consonant				U+0F93: TIBETAN SUBJOINED LETTER GHA​	tibetan
+\u0F9D		£	ɖ̰ʰ	ɖʰ	precomposed sanskrit consonant				U+0F9D: TIBETAN SUBJOINED LETTER DDHA​	tibetan
+\u0FA2		£	d̰ʰ	d̪ʱ	precomposed sanskrit consonant				U+0FA2: TIBETAN SUBJOINED LETTER DHA​	tibetan
+\u0FA7		£	b̰ʰ	bʰ	precomposed sanskrit consonant				U+0FA7: TIBETAN SUBJOINED LETTER BHA​	tibetan
+\u0FAC		£	ʣ̰ʰ	ʣʰ	precomposed sanskrit consonant				U+0FAC: TIBETAN SUBJOINED LETTER DZHA​	tibetan
+\u0FB9		£	k̰ˢ	kʂ	precomposed sanskrit consonant				U+0FB9: TIBETAN SUBJOINED LETTER KSSA​	tibetan
+ྴ		S	ʂ̰	ʂ	sanskrit consonant				U+0FB4: TIBETAN SUBJOINED LETTER SHA​	tibetan
+ྼ		R	r̰̄		fixed form variant				U+0FBC: TIBETAN SUBJOINED LETTER FIXED-FORM RA​	tibetan
+ྺ		W	w̰̄		fixed form variant				U+0FBA: TIBETAN SUBJOINED LETTER FIXED-FORM WA​	tibetan
+ྻ		Y	ȳ̰		fixed form variant				U+0FBB: TIBETAN SUBJOINED LETTER FIXED-FORM YA​	tibetan
+										
+										
+ི		i	i	i	vowel				U+0F72: TIBETAN VOWEL SIGN I​	tibetan
+ུ		u	u	u	vowel				U+0F74: TIBETAN VOWEL SIGN U​	tibetan
+ེ		e	e	e	vowel				U+0F7A: TIBETAN VOWEL SIGN E​	tibetan
+ོ		o	o	o	vowel				U+0F7C: TIBETAN VOWEL SIGN O​	tibetan
+										
+										
+ཱ		A	à	ā	vowel lengthening mark				U+0F71: TIBETAN VOWEL SIGN AA​	tibetan
+\u0F73		£	ī	ī	precomposed sanskrit vowel	deprecated			U+0F73: TIBETAN VOWEL SIGN II​	tibetan
+\u0F75		£	ū	ū	precomposed sanskrit vowel	deprecated			U+0F75: TIBETAN VOWEL SIGN UU​	tibetan
+\u0F76		£	r̥	ṛ	precomposed sanskrit vowel	deprecated			U+0F76: TIBETAN VOWEL SIGN VOCALIC R​	tibetan
+\u0F78		£	l̥	ḷ	precomposed sanskrit vowel	deprecated			U+0F78: TIBETAN VOWEL SIGN VOCALIC L​	tibetan
+ཻ		E	ē	ai	sanskrit vowel				U+0F7B: TIBETAN VOWEL SIGN EE​	tibetan
+ཽ		O	ō	au	sanskrit vowel				U+0F7D: TIBETAN VOWEL SIGN OO​	tibetan
+ྀ		I	ï	i	sanskrit vowel				U+0F80: TIBETAN VOWEL SIGN REVERSED I​	tibetan
+ཾ		M	m̽	ṃ	sanskrit vowel		ngaro		U+0F7E: TIBETAN SIGN RJES SU NGA RO​	tibetan
+ྃ		M	m̽̇	ṃ	sanskrit vowel				U+0F83: TIBETAN SIGN SNA LDAN​	tibetan
+ཿ		H	h̽	ḥ	sanskrit vowel		nam-chay		U+0F7F: TIBETAN SIGN RNAM BCAD​	tibetan
+										
+										
+\u0F77		£	r̥̄	"ṝ
+"	precomposed sanskrit vowel	strongly deprecated			U+0F77: TIBETAN VOWEL SIGN VOCALIC RR​	tibetan
+\u0F79		£	l̥̄	ḹ	precomposed sanskrit vowel	strongly deprecated			U+0F79: TIBETAN VOWEL SIGN VOCALIC LL​	tibetan
+\u0F81		£	ï̄	ī	precomposed sanskrit vowel	deprecated			U+0F81: TIBETAN VOWEL SIGN REVERSED II​	tibetan
+										
+										
+										
+༠		0	0		digit				U+0F20: TIBETAN DIGIT ZERO	tibetan
+༡		1	1		digit				U+0F21: TIBETAN DIGIT ONE	tibetan
+༢		2	2		digit				U+0F22: TIBETAN DIGIT TWO	tibetan
+༣		3	3		digit				U+0F23: TIBETAN DIGIT THREE	tibetan
+༤		4	4		digit				U+0F24: TIBETAN DIGIT FOUR	tibetan
+༥		5	5		digit				U+0F25: TIBETAN DIGIT FIVE	tibetan
+༦		6	6		digit				U+0F26: TIBETAN DIGIT SIX	tibetan
+༧		7	7		digit				U+0F27: TIBETAN DIGIT SEVEN	tibetan
+༨		8	8		digit				U+0F28: TIBETAN DIGIT EIGHT	tibetan
+༩		9	9		digit				U+0F29: TIBETAN DIGIT NINE	tibetan
+										
+༳			0.5		digit minus half				U+0F33: TIBETAN DIGIT HALF ZERO	tibetan
+༪			1.5		digit minus half				U+0F2A: TIBETAN DIGIT HALF ONE	tibetan
+༫			2.5		digit minus half				U+0F2B: TIBETAN DIGIT HALF TWO	tibetan
+༬			3.5		digit minus half				U+0F2C: TIBETAN DIGIT HALF THREE	tibetan
+༭			4.5		digit minus half				U+0F2D: TIBETAN DIGIT HALF FOUR	tibetan
+༮			5.5		digit minus half				U+0F2E: TIBETAN DIGIT HALF FIVE	tibetan
+༯			6.5		digit minus half				U+0F2F: TIBETAN DIGIT HALF SIX	tibetan
+༰			7.5		digit minus half				U+0F30: TIBETAN DIGIT HALF SEVEN	tibetan
+༱			8.5		digit minus half				U+0F31: TIBETAN DIGIT HALF EIGHT	tibetan
+༲			9.5		digit minus half				U+0F32: TIBETAN DIGIT HALF NINE	tibetan
+										
+										
+ༀ					symbol				U+0F00: TIBETAN SYLLABLE OM	tibetan
+༓					symbol				U+0F13: TIBETAN MARK CARET -DZUD RTAGS ME LONG CAN	tibetan
+࿄					symbol				U+0FC4: TIBETAN SYMBOL DRIL BU	tibetan
+࿅					symbol				U+0FC5: TIBETAN SYMBOL RDO RJE	tibetan
+࿆					combining symbol				U+0FC6: TIBETAN SYMBOL PADMA GDAN​	tibetan
+࿇					symbol				U+0FC7: TIBETAN SYMBOL RDO RJE RGYA GRAM	tibetan
+࿈					symbol				U+0FC8: TIBETAN SYMBOL PHUR PA	tibetan
+࿉					symbol				U+0FC9: TIBETAN SYMBOL NOR BU	tibetan
+࿊					symbol				U+0FCA: TIBETAN SYMBOL NOR BU NYIS -KHYIL	tibetan
+࿋					symbol				U+0FCB: TIBETAN SYMBOL NOR BU GSUM -KHYIL	tibetan
+࿌					svasti sign				U+0FCC: TIBETAN SYMBOL NOR BU BZHI -KHYIL	tibetan
+࿕					svasti sign		gyung drung nang -khor		U+0FD5: RIGHT-FACING SVASTI SIGN	tibetan
+࿖					svasti sign		gyung drung phyi -khor		U+0FD6: LEFT-FACING SVASTI SIGN	tibetan
+࿗					svasti sign		gyung drung nang -khor bzhi mig can		U+0FD7: RIGHT-FACING SVASTI SIGN WITH DOTS	tibetan
+࿘					svasti sign		gyung drung phyi -khor bzhi mig can		U+0FD8: LEFT-FACING SVASTI SIGN WITH DOTS	tibetan
+										
+										
+										
+༁					head mark				U+0F01: TIBETAN MARK GTER YIG MGO TRUNCATED A	tibetan
+༂					head mark				U+0F02: TIBETAN MARK GTER YIG MGO -UM RNAM BCAD MA	tibetan
+༃					head mark				U+0F03: TIBETAN MARK GTER YIG MGO -UM GTER TSHEG MA	tibetan
+༄		#	ᵗ>		head mark				U+0F04: TIBETAN MARK INITIAL YIG MGO MDUN MA	tibetan
+༅		#	>		head mark				U+0F05: TIBETAN MARK CLOSING YIG MGO SGAB MA	tibetan
+༆					head mark				U+0F06: TIBETAN MARK CARET YIG MGO PHUR SHAD MA	tibetan
+༇					head mark				U+0F07: TIBETAN MARK YIG MGO TSHEG SHAD MA	tibetan
+࿓					head mark		da nying yik go dun ma		U+0FD3: TIBETAN MARK INITIAL BRDA RNYING YIG MGO MDUN MA	tibetan
+࿔					head mark		da nying yik go kab ma		U+0FD4: TIBETAN MARK CLOSING BRDA RNYING YIG MGO SGAB MA	tibetan
+										
+										
+ྈ					transliteration head letter				U+0F88: TIBETAN SIGN LCE TSA CAN	tibetan
+ྉ					transliteration head letter				U+0F89: TIBETAN SIGN MCHU CAN	tibetan
+ྊ					transliteration head letter				U+0F8A: TIBETAN SIGN GRU CAN RGYINGS	tibetan
+ྋ					transliteration head letter				U+0F8B: TIBETAN SIGN GRU MED RGYINGS	tibetan
+ྌ					subjoined sign				U+0F8C: TIBETAN SIGN INVERTED MCHU CAN	tibetan
+										
+										
+ྍ					subjoined sign				U+0F8D: TIBETAN SUBJOINED SIGN LCE TSA CAN​	tibetan
+ྎ					subjoined sign				U+0F8E: TIBETAN SUBJOINED SIGN MCHU CAN​	tibetan
+ྏ					subjoined sign				U+0F8F: TIBETAN SUBJOINED SIGN INVERTED MCHU CAN​	tibetan
+										
+										
+༉		[	•		list enumerator				U+0F09: TIBETAN MARK BSKUR YIG MGO	tibetan
+༊					punctuation				U+0F0A: TIBETAN MARK BKA- SHOG YIG MGO	tibetan
+་		]	␣		primary break delimiter		tsek (tsheg)		U+0F0B: TIBETAN MARK INTERSYLLABIC TSHEG	tibetan
+༌		'	¯		punctuation				U+0F0C: TIBETAN MARK DELIMITER TSHEG BSTAR	tibetan
+།		|	|		full stop after text		shay (shad)		U+0F0D: TIBETAN MARK SHAD	tibetan
+༎		|	‖		full stop after topic		shay (shad)		U+0F0E: TIBETAN MARK NYIS SHAD	tibetan
+༈		|	—		topic/subtopic separator				U+0F08: TIBETAN MARK SBRUL SHAD	tibetan
+༏					punctuation				U+0F0F: TIBETAN MARK TSHEG SHAD	tibetan
+༐					punctuation				U+0F10: TIBETAN MARK NYIS TSHEG SHAD	tibetan
+༑					punctuation				U+0F11: TIBETAN MARK RIN CHEN SPUNGS SHAD	tibetan
+༒					punctuation				U+0F12: TIBETAN MARK RGYA GRAM SHAD	tibetan
+༔					~comma		ter tsek		U+0F14: TIBETAN MARK GTER TSHEG	tibetan
+༺		(	[		left bracket				U+0F3A: TIBETAN MARK GUG RTAGS GYON	tibetan
+༻		)	]		right bracket				U+0F3B: TIBETAN MARK GUG RTAGS GYAS	tibetan
+༼		(	{		left bracket				U+0F3C: TIBETAN MARK ANG KHANG GYON	tibetan
+༽		)	}		right bracket				U+0F3D: TIBETAN MARK ANG KHANG GYAS	tibetan
+྅					punctuation				U+0F85: TIBETAN MARK PALUTA	tibetan
+࿐					punctuation				U+0FD0: TIBETAN MARK BSKA- SHOG GI MGO RGYAN	tibetan
+࿑					punctuation				U+0FD1: TIBETAN MARK MNYAM YIG GI MGO RGYAN	tibetan
+࿒					punctuation		tsek		U+0FD2: TIBETAN MARK NYIS TSHEG	tibetan
+࿙					punctuation				U+0FD9: TIBETAN MARK LEADING MCHAN RTAGS	tibetan
+࿚					punctuation				U+0FDA: TIBETAN MARK TRAILING MCHAN RTAGS	tibetan
+										
+										
+༴		&	˖		repetition sign		du tag (bsdus-rtags)	བསྡུས་རྟགས	U+0F34: TIBETAN MARK BSDUS RTAGS	tibetan
+༵					emphasis sign				U+0F35: TIBETAN MARK NGAS BZUNG NYI ZLA​	tibetan
+༶					annotation sign				U+0F36: TIBETAN MARK CARET -DZUD RTAGS BZHI MIG CAN	tibetan
+༷					emphasis sign				U+0F37: TIBETAN MARK NGAS BZUNG SGOR RTAGS​	tibetan
+༸					sign				U+0F38: TIBETAN MARK CHE MGO	tibetan
+༹		/	̣		sound extension/lenition mark		tsa-'phru		U+0F39: TIBETAN MARK TSA -PHRU​	tibetan
+ྂ					sign				U+0F82: TIBETAN SIGN NYI ZLA NAA DA​	tibetan
+ྃ					sign				U+0F83: TIBETAN SIGN SNA LDAN​	tibetan
+྆					sign				U+0F86: TIBETAN SIGN LCI RTAGS​	tibetan
+྇					sign				U+0F87: TIBETAN SIGN YANG RTAGS​	tibetan
+྾					sign				U+0FBE: TIBETAN KU RU KHA	tibetan
+྿					sign				U+0FBF: TIBETAN KU RU KHA BZHI MIG CAN	tibetan
+࿀					sign				U+0FC0: TIBETAN CANTILLATION SIGN HEAVY BEAT	tibetan
+࿁					sign				U+0FC1: TIBETAN CANTILLATION SIGN LIGHT BEAT	tibetan
+࿂					sign				U+0FC2: TIBETAN CANTILLATION SIGN CANG TE-U	tibetan
+࿃					sign				U+0FC3: TIBETAN CANTILLATION SIGN SBUB -CHAL	tibetan
+										
+										
+྄		*	͓		virama		srog med		U+0F84: TIBETAN MARK HALANTA​	tibetan
+྅		:	ː		paluta				U+0F85: TIBETAN MARK PALUTA	tibetan
+										
+										
+										
+										
+༕					astrological symbol				U+0F15: TIBETAN LOGOTYPE SIGN CHAD RTAGS	tibetan
+༖					astrological symbol				U+0F16: TIBETAN LOGOTYPE SIGN LHAG RTAGS	tibetan
+༗					astrological symbol				U+0F17: TIBETAN ASTROLOGICAL SIGN SGRA GCAN -CHAR RTAGS	tibetan
+༚					astrological symbol				U+0F1A: TIBETAN SIGN RDEL DKAR GCIG	tibetan
+༛					astrological symbol				U+0F1B: TIBETAN SIGN RDEL DKAR GNYIS	tibetan
+༜					astrological symbol				U+0F1C: TIBETAN SIGN RDEL DKAR GSUM	tibetan
+༝					astrological symbol				U+0F1D: TIBETAN SIGN RDEL NAG GCIG	tibetan
+༞					astrological symbol				U+0F1E: TIBETAN SIGN RDEL NAG GNYIS	tibetan
+༟					astrological symbol				U+0F1F: TIBETAN SIGN RDEL DKAR RDEL NAG	tibetan
+༾					astrological symbol				U+0F3E: TIBETAN SIGN YAR TSHES​	tibetan
+༿					astrological symbol				U+0F3F: TIBETAN SIGN MAR TSHES​	tibetan
+࿎					astrological symbol				U+0FCE: TIBETAN SIGN RDEL NAG RDEL DKAR	tibetan
+࿏					astrological symbol				U+0FCF: TIBETAN SIGN RDEL NAG GSUM	tibetan
+										
+										
+										
+										
+གྷ	s	G	gh̰	gʰ	sanskrit consonant				U+0F42 U+0FB7: TIBETAN LETTER GA, SUBJOINED LETTER HA	
+ཌྷ	s	D	ɖh̰	ɖʰ	sanskrit consonant				U+0F4C U+0FB7: TIBETAN LETTER DDA, SUBJOINED LETTER HA	
+དྷ	s	D	dh̰	d̪ʱ	sanskrit consonant				U+0F51 U+0FB7: TIBETAN LETTER DA, SUBJOINED LETTER HA	
+བྷ	s	B	bh̰	bʰ	sanskrit consonant				U+0F56 U+0FB7: TIBETAN LETTER BA, SUBJOINED LETTER HA	
+ཛྷ	s	D	ʣh̰	ʣʰ	sanskrit consonant				U+0F5B U+0FB7: TIBETAN LETTER DZA, SUBJOINED LETTER HA	
+ཀྵ	s	K	kʃ̰	kʂ	sanskrit consonant				U+0F40 U+0FB5: TIBETAN LETTER KA, SUBJOINED LETTER SSA	
+										
+										
+ྒྷ	s	G	g̰h̰	gʰ	sanskrit consonant				U+0F92 U+0FB7: TIBETAN SUBJOINED LETTER GA, SUBJOINED LETTER HA​	
+ྜྷ	s	D	ɖ̰h̰	ɖʰ	sanskrit consonant				U+0F9C U+0FB7: TIBETAN SUBJOINED LETTER DDA, SUBJOINED LETTER HA​	
+ྡྷ	s	D	d̰h̰	d̪ʱ	sanskrit consonant				U+0FA1 U+0FB7: TIBETAN SUBJOINED LETTER DA, SUBJOINED LETTER HA​	
+ྦྷ	s	B	b̰h̰	bʰ	sanskrit consonant				U+0FA6 U+0FB7: TIBETAN SUBJOINED LETTER BA, SUBJOINED LETTER HA​	
+ྫྷ	s	D	ʣ̰h̰	ʣʰ	sanskrit consonant				U+0FAB U+0FB7: TIBETAN SUBJOINED LETTER DZA, SUBJOINED LETTER HA​	
+ྐྵ	s	K	k̰ʃ̰	kʂ	sanskrit consonant				U+0F90 U+0FB5: TIBETAN SUBJOINED LETTER KA, SUBJOINED LETTER SSA​	
+										
+										
+ཕ༹	s	f	pʰ̣						U+0F55 U+0F39: TIBETAN LETTER PHA, MARK TSA -PHRU	
+བ༹	s	v	ḅ						U+0F56 U+0F39: TIBETAN LETTER BA, MARK TSA -PHRU	
+										
+										
+ཱི	s	I	ài	ī	sanskrit vowel				U+0F71 U+0F72: TIBETAN VOWEL SIGN AA, VOWEL SIGN I​	
+ཱུ	s	U	àu	ū	sanskrit vowel				U+0F71 U+0F74: TIBETAN VOWEL SIGN AA, VOWEL SIGN U​	
+ྲྀ	s	R	r̰ï	ṛ	sanskrit vowel				U+0FB2 U+0F80: TIBETAN SUBJOINED LETTER RA, VOWEL SIGN REVERSED I​	
+ྲཱྀ	s	R	r̰àï	"ṝ
+"	sanskrit vowel				U+0FB2 U+0F71 U+0F80: TIBETAN SUBJOINED LETTER RA, VOWEL SIGN AA, VOWEL SIGN REVERSED I​	
+ླྀ	s	L	l̰ï	ḷ	sanskrit vowel				U+0FB3 U+0F80: TIBETAN SUBJOINED LETTER LA, VOWEL SIGN REVERSED I​	
+ླཱྀ	s	L	l̰àï	ḹ	sanskrit vowel				U+0FB3 U+0F71 U+0F80: TIBETAN SUBJOINED LETTER LA, VOWEL SIGN AA, VOWEL SIGN REVERSED I​	
+ཱྀ	s	I	àï	ī	sanskrit vowel				U+0F71 U+0F80: TIBETAN VOWEL SIGN AA, VOWEL SIGN REVERSED I​	
+
+
+`
+
+
+
+var cols = {
+"class": 1,
+"key":2,
+"transLoc": 3,
+"ipaLoc": 4,
+"transckey": 0,
+"transcription":0,
+"typeLoc": 5,
+"statusLoc": 6,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nameLoc": 7,
+"nnameLoc": 8,
+"ucsName": 9,
+"block": 10,
+
+//"othertranscriptions": [[6, 'ISO 9985']]
+}
+
