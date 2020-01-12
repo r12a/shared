ccaddaf15d349f03d89401d412e5d2971901fbfa
r12a <ishida@w3.org>
2020-01-12 09:23:39 +0000

    scriptdb georgian -> geor

---
+++ fontlists/geor.js
@@ -0,0 +1,34 @@
+ var fontSelection = `<option value="0">Select...</option>
+            <optgroup label="Web font">
+            <option value="BGP 2017 DejaVu Serif WF">BGP 2017 DejaVu Serif WF</option>
+            <option value="BGP 2017 DejaVu Sans WF">BGP 2017 DejaVu Sans WF</option>
+            <option value="Noto Serif Georgian WF">Noto Serif Georgian WF</option>
+            <option value="Noto Sans Georgian WF">Noto Sans Georgian WF</option>
+            </optgroup>
+            
+            <optgroup label="Mac fonts ">
+            <option value="Menlo">Menlo</option>
+            </optgroup>
+            
+            <optgroup label="Windows fonts ">
+            <option value="Sylfaen">Sylfaen</option>
+            </optgroup>
+            
+            <optgroup label="Google fonts ">
+            <option value="Noto Serif Georgian">Noto Serif Georgian</option>
+            <option value="Noto Sans Georgian">Noto Sans Georgian</option>
+            </optgroup>
+            
+            <optgroup label="Other fonts ">
+            <option value="BGP 2017 DejaVu Serif">BGP 2017 DejaVu Serif</option>
+            <option value="BGP 2017 DejaVu Sans">BGP 2017 DejaVu Sans</option>
+            <option value="BPG Classic 99U">BPG Classic 99U</option>
+            <option value="BPG Paata Khutsuri U">BPG Paata Khutsuri U</option>
+            </optgroup>
+            
+            <optgroup label="General fonts ">
+            <option value="Arial Unicode MS">Arial Unicode MS</option>
+            <option value="Code2000">Code2000</option>
+            <option value="sans-serif">sans-serif</option>
+            <option value="serif">serif</option>
+            </optgroup>`
