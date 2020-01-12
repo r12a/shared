f252ca99fd503b6f4a66150477c82a4d95e30d7b
r12a <ishida@w3.org>
2020-01-12 09:28:38 +0000

    scriptdb, new & renamed georgian files georg-ka -> geor-ka: for use with all georgian picker & script summary + geor-ka: for use with Georgian picker & character notes + geok-ka: for use with Khutsuri picker & character notes + geor-other: for use with character notes - georgian

---
+++ scriptdb/geok-ka.js
@@ -0,0 +1,123 @@
+var spreadsheet = `
+Ⴀ		A	A̱	ɑ	A	a	a	a	a	asomtravuli letter		ⴀ		1	ani	U+10A0: GEORGIAN CAPITAL LETTER AN	georgian
+Ⴁ		B	Ḇ	b	B	b	b	b	b	asomtravuli letter		ⴁ		2	bani	U+10A1: GEORGIAN CAPITAL LETTER BAN	georgian
+Ⴂ		G	G̱	ɡ	G	g	g	g	g	asomtravuli letter		ⴂ		3	gani	U+10A2: GEORGIAN CAPITAL LETTER GAN	georgian
+Ⴃ		D	Ḏ	d	D	d	d	d	d	asomtravuli letter		ⴃ		4	doni	U+10A3: GEORGIAN CAPITAL LETTER DON	georgian
+Ⴄ		E	Ɛ̱	ɛ	R	e	e	e	e	asomtravuli letter		ⴄ		5	eni	U+10A4: GEORGIAN CAPITAL LETTER EN	georgian
+Ⴅ		V	V̱	v	V	v	v	v	v	asomtravuli letter		ⴅ		6	vini	U+10A5: GEORGIAN CAPITAL LETTER VIN	georgian
+Ⴆ		Z	Ẕ	z	Z	z	z	z	z	asomtravuli letter		ⴆ		7	zeni	U+10A6: GEORGIAN CAPITAL LETTER ZEN	georgian
+Ⴇ		T	Ṯ	t⁽ʰ⁾	T	t	t'	t'	t	asomtravuli letter		ⴇ		9	tani	U+10A7: GEORGIAN CAPITAL LETTER TAN	georgian
+Ⴈ		I	I̱	i	I	i	i	i	i	asomtravuli letter		ⴈ		10	ini	U+10A8: GEORGIAN CAPITAL LETTER IN	georgian
+Ⴉ		K	Ḵ̇	kʼ	K	k'	k	k	ǩ	asomtravuli letter		ⴉ		20	k'ani	U+10A9: GEORGIAN CAPITAL LETTER KAN	georgian
+Ⴊ		L	Ḻ	l	L	l	l	l	l	asomtravuli letter		ⴊ		30	lasi	U+10AA: GEORGIAN CAPITAL LETTER LAS	georgian
+Ⴋ		M	M̱	m	M	m	m	m	m	asomtravuli letter		ⴋ		40	mani	U+10AB: GEORGIAN CAPITAL LETTER MAN	georgian
+Ⴌ		N	Ṉ	n	N	n	n	n	n	asomtravuli letter		ⴌ		50	nari	U+10AC: GEORGIAN CAPITAL LETTER NAR	georgian
+Ⴍ		O	Ɔ̱	ɔ	O	o	o	o	o	asomtravuli letter		ⴍ		70	oni	U+10AD: GEORGIAN CAPITAL LETTER ON	georgian
+Ⴎ		P	Ṗ̱	pʼ	P	p'	p	p	ṗ	asomtravuli letter		ⴎ		80	p'ari	U+10AE: GEORGIAN CAPITAL LETTER PAR	georgian
+Ⴏ		Z	Ʒ̱	ʒ	Z	zh	ž	zh	j	asomtravuli letter		ⴏ		90	zhani	U+10AF: GEORGIAN CAPITAL LETTER ZHAR	georgian
+Ⴐ		R	Ṟ	r	R	r	r	r	r	asomtravuli letter		ⴐ		100	rae	U+10B0: GEORGIAN CAPITAL LETTER RAE	georgian
+Ⴑ		S	S̱	s	S	s	s	s	s	asomtravuli letter		ⴑ		200	sani	U+10B1: GEORGIAN CAPITAL LETTER SAN	georgian
+Ⴒ		T	Ṯ̇	tʼ	T	t'	t	t	ť	asomtravuli letter		ⴒ		300	t'ari	U+10B2: GEORGIAN CAPITAL LETTER TAR	georgian
+Ⴓ		Y	U̱	u	U	u	u	u	u	asomtravuli letter		ⴓ		400	uni	U+10B3: GEORGIAN CAPITAL LETTER UN	georgian
+Ⴔ		P	P̱ʰ	p⁽ʰ⁾	P	p	p'	p'	p	asomtravuli letter		ⴔ		500	pari	U+10B4: GEORGIAN CAPITAL LETTER PHAR	georgian
+Ⴕ		K	Ḵʰ	k⁽ʰ⁾	K	k	k'	k'	k	asomtravuli letter		ⴕ		600	kani	U+10B5: GEORGIAN CAPITAL LETTER KHAR	georgian
+Ⴖ		Y	Ɣ̱	ɣ	G	gh	ḡ	gh	ğ	asomtravuli letter		ⴖ		700	ghani	U+10B6: GEORGIAN CAPITAL LETTER GHAN	georgian
+Ⴗ		Q	Q̱̇	qʼ	Q	q'	q	q	q	asomtravuli letter		ⴗ		800	q'ari	U+10B7: GEORGIAN CAPITAL LETTER QAR	georgian
+Ⴘ		S	Ʃ̱	ʃ	S	sh	š	sh	ş	asomtravuli letter		ⴘ		900	shini	U+10B8: GEORGIAN CAPITAL LETTER SHIN	georgian
+Ⴙ		C	Č̱ʰ	tʃ⁽ʰ⁾	C	ch	č'	ch'	ç	asomtravuli letter		ⴙ		1000	chini	U+10B9: GEORGIAN CAPITAL LETTER CHIN	georgian
+Ⴚ		C	C̱ʰ	ts⁽ʰ⁾	T	ts	c’	ts'	ts	asomtravuli letter		ⴚ		2000	tsani	U+10BA: GEORGIAN CAPITAL LETTER CAN	georgian
+Ⴛ		D	Ǳ̱	dz	D	dz	j	dz	ž	asomtravuli letter		ⴛ		3000	dzili	U+10BB: GEORGIAN CAPITAL LETTER JIL	georgian
+Ⴜ		C	Ċ̱	tsʼ	T	ts'	c	ts	ts’	asomtravuli letter		ⴜ		4000	ts'ili	U+10BC: GEORGIAN CAPITAL LETTER CIL	georgian
+Ⴝ		C	Č̱̇	tʃʼ	C	ch'	č	ch	ç̌	asomtravuli letter		ⴝ		5000	ch'ari	U+10BD: GEORGIAN CAPITAL LETTER CHAR	georgian
+Ⴞ		X	Χ̱	χ	K	kh	x	kh	x	asomtravuli letter		ⴞ		6000	khani	U+10BE: GEORGIAN CAPITAL LETTER XAN	georgian
+Ⴟ		D	Ǆ̱	dʒ	J	j	ǰ	j	c	asomtravuli letter		ⴟ		8000	jani	U+10BF: GEORGIAN CAPITAL LETTER JHAN	georgian
+Ⴠ		H	H̱	h	H	h	h	h	h	asomtravuli letter		ⴠ		9000	hae	U+10C0: GEORGIAN CAPITAL LETTER HAE	georgian
+Ⴡ		E	E̱	eɪ			ē	ey		asomtravuli letter		ⴡ		8	he	U+10C1: GEORGIAN CAPITAL LETTER HE	georgian
+Ⴢ		J	J̱	je			y	j	y	asomtravuli letter		ⴢ		60	hie	U+10C2: GEORGIAN CAPITAL LETTER HIE	georgian
+Ⴣ		U	W̱	uɪ			w			asomtravuli letter		ⴣ		400	vie	U+10C3: GEORGIAN CAPITAL LETTER WE	georgian
+Ⴤ		Q	Q̱ʰ	q⁽ʰ⁾			ẖ	q'		asomtravuli letter		ⴤ		7000	qari, hari	U+10C4: GEORGIAN CAPITAL LETTER HAR	georgian
+Ⴥ		O	O̱	oː			ō			asomtravuli letter	archaic	ⴥ		10000	hoe	U+10C5: GEORGIAN CAPITAL LETTER HOE	georgian
+																	
+																	
+ⴀ		a	a̱	ɑ	a	a	a	a	a	nuskhuri letter			Ⴀ	1	ani	U+2D00: GEORGIAN SMALL LETTER AN	georgian
+ⴁ		b	ḇ	b	b	b	b	b	b	nuskhuri letter			Ⴁ	2	bani	U+2D01: GEORGIAN SMALL LETTER BAN	georgian
+ⴂ		g	g̱	ɡ	g	g	g	g	g	nuskhuri letter			Ⴂ	3	gani	U+2D02: GEORGIAN SMALL LETTER GAN	georgian
+ⴃ		d	ḏ	d	d	d	d	d	d	nuskhuri letter			Ⴃ	4	doni	U+2D03: GEORGIAN SMALL LETTER DON	georgian
+ⴄ		e	ɛ̱	ɛ	e	e	e	e	e	nuskhuri letter			Ⴄ	5	eni	U+2D04: GEORGIAN SMALL LETTER EN	georgian
+ⴅ		v	v̱	v	v	v	v	v	v	nuskhuri letter			Ⴅ	6	vini	U+2D05: GEORGIAN SMALL LETTER VIN	georgian
+ⴆ		z	ẕ	z	z	z	z	z	z	nuskhuri letter			Ⴆ	7	zeni	U+2D06: GEORGIAN SMALL LETTER ZEN	georgian
+ⴇ		t	ṯ	t⁽ʰ⁾	t	t	t'	t'	t	nuskhuri letter			Ⴇ	9	tani	U+2D07: GEORGIAN SMALL LETTER TAN	georgian
+ⴈ		j	i̱	i	i	i	i	i	i	nuskhuri letter			Ⴈ	10	ini	U+2D08: GEORGIAN SMALL LETTER IN	georgian
+ⴉ		k	ḵ̇	kʼ	k	k'	k	k	ǩ	nuskhuri letter			Ⴉ	20	k'ani	U+2D09: GEORGIAN SMALL LETTER KAN	georgian
+ⴊ		l	ḻ	l	l	l	l	l	l	nuskhuri letter			Ⴊ	30	lasi	U+2D0A: GEORGIAN SMALL LETTER LAS	georgian
+ⴋ		m	m̱	m	m	m	m	m	m	nuskhuri letter			Ⴋ	40	mani	U+2D0B: GEORGIAN SMALL LETTER MAN	georgian
+ⴌ		n	ṉ	n	n	n	n	n	n	nuskhuri letter			Ⴌ	50	nari	U+2D0C: GEORGIAN SMALL LETTER NAR	georgian
+ⴍ		o	ɔ̱	ɔ	o	o	o	o	o	nuskhuri letter			Ⴍ	70	oni	U+2D0D: GEORGIAN SMALL LETTER ON	georgian
+ⴎ		p	ṗ̱	pʼ	p	p'	p	p	ṗ	nuskhuri letter			Ⴎ	80	p'ari	U+2D0E: GEORGIAN SMALL LETTER PAR	georgian
+ⴏ		z	ʒ̱	ʒ	z	zh	ž	zh	j	nuskhuri letter			Ⴏ	90	zhani	U+2D0F: GEORGIAN SMALL LETTER ZHAR	georgian
+ⴐ		r	ṟ	r	r	r	r	r	r	nuskhuri letter			Ⴐ	100	rae	U+2D10: GEORGIAN SMALL LETTER RAE	georgian
+ⴑ		s	s̱	s	s	s	s	s	s	nuskhuri letter			Ⴑ	200	sani	U+2D11: GEORGIAN SMALL LETTER SAN	georgian
+ⴒ		t	ṯ̇	tʼ	t	t'	t	t	ť	nuskhuri letter			Ⴒ	300	t'ari	U+2D12: GEORGIAN SMALL LETTER TAR	georgian
+ⴓ		y	u̱	u	u	u	u	u	u	nuskhuri letter			Ⴓ	400	uni	U+2D13: GEORGIAN SMALL LETTER UN	georgian
+ⴔ		p	p̱ʰ	p⁽ʰ⁾	o	p	p'	p'	p	nuskhuri letter			Ⴔ	500	pari	U+2D14: GEORGIAN SMALL LETTER PHAR	georgian
+ⴕ		k	ḵʰ	k⁽ʰ⁾	k	k	k'	k'	k	nuskhuri letter			Ⴕ	600	kani	U+2D15: GEORGIAN SMALL LETTER KHAR	georgian
+ⴖ		y	ɣ̱	ɣ	g	gh	ḡ	gh	ğ	nuskhuri letter			Ⴖ	700	ghani	U+2D16: GEORGIAN SMALL LETTER GHAN	georgian
+ⴗ		g	q̱̇	qʼ	q	q'	q	q	q	nuskhuri letter			Ⴗ	800	q'ari	U+2D17: GEORGIAN SMALL LETTER QAR	georgian
+ⴘ		s	ʃ̱	ʃ	s	sh	š	sh	ş	nuskhuri letter			Ⴘ	900	shini	U+2D18: GEORGIAN SMALL LETTER SHIN	georgian
+ⴙ		c	č̱ʰ	tʃ⁽ʰ⁾	c	ch	č'	ch'	ç	nuskhuri letter			Ⴙ	1000	chini	U+2D19: GEORGIAN SMALL LETTER CHIN	georgian
+ⴚ		c	c̱ʰ	ts⁽ʰ⁾	t	ts	c’	ts'	ts	nuskhuri letter			Ⴚ	2000	tsani	U+2D1A: GEORGIAN SMALL LETTER CAN	georgian
+ⴛ		d	ʣ̱	dz	d	dz	j	dz	ž	nuskhuri letter			Ⴛ	3000	dzili	U+2D1B: GEORGIAN SMALL LETTER JIL	georgian
+ⴜ		c	ċ̱	tsʼ	t	ts'	c	ts	ts’	nuskhuri letter			Ⴜ	4000	ts'ili	U+2D1C: GEORGIAN SMALL LETTER CIL	georgian
+ⴝ		c	č̱̇	tʃʼ	c	ch'	č	ch	ç̌	nuskhuri letter			Ⴝ	5000	ch'ari	U+2D1D: GEORGIAN SMALL LETTER CHAR	georgian
+ⴞ		x	χ̱	χ	k	kh	x	kh	x	nuskhuri letter			Ⴞ	6000	khani	U+2D1E: GEORGIAN SMALL LETTER XAN	georgian
+ⴟ		d	ʤ̱	dʒ	j	j	ǰ	j	c	nuskhuri letter			Ⴟ	8000	jani	U+2D1F: GEORGIAN SMALL LETTER JHAN	georgian
+ⴠ		h	ẖ	h	h	h	h	h	h	nuskhuri letter			Ⴠ	9000	hae	U+2D20: GEORGIAN SMALL LETTER HAE	georgian
+ⴡ		e	e̱	eɪ			ē	ey		nuskhuri letter			Ⴡ	8	he	U+2D21: GEORGIAN SMALL LETTER HE	georgian
+ⴢ		j	j̱	je			y	j	y	nuskhuri letter			Ⴢ	60	hie	U+2D22: GEORGIAN SMALL LETTER HIE	georgian
+ⴣ		u	w̱	uɪ			w			nuskhuri letter			Ⴣ	400	vie	U+2D23: GEORGIAN SMALL LETTER WE	georgian
+ⴤ		g	q̱ʰ	q⁽ʰ⁾			ẖ	q'		nuskhuri letter			Ⴤ	7000	qari, hari	U+2D24: GEORGIAN SMALL LETTER HAR	georgian
+ⴥ		o	o̱	oː			ō			nuskhuri letter	archaic		Ⴥ	10000	hoe	U+2D25: GEORGIAN SMALL LETTER HOE	georgian																	
+																	
+																	
+჻			⁋							paragraph separator						U+10FB: GEORGIAN PARAGRAPH SEPARATOR	georgian																
+
+
+`
+
+
+
+var cols = {
+"class": 1,
+"key":2,
+"transLoc": 3,
+"ipaLoc": 4,
+"transckey": 5,
+"transcription":6,
+"typeLoc": 10,
+"statusLoc": 11,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc": 12,
+"uc": 13,
+"meaning":0,
+"shape": 0,
+"numLoc": 14,
+"nameLoc": 15,
+"nnameLoc": 0,
+"ucsName": 16,
+"block": 17,
+
+"othertranscriptions": [[6, 'National'], [9, 'ISO']]
+}
+
+
+
+
+
+
+
+
+
---
+++ scriptdb/geor-other.js
@@ -0,0 +1,101 @@
+var spreadsheet = `
+Ⴀ	A	A̱	A	a	a æ	asomtravuli letter		ani	a	a	a	1	ⴀ		Svan	georgian
+Ⴡ	E	E̱			eː	asomtravuli letter		he	ē	ey		8	ⴡ		Svan	georgian
+Ⴢ	J	J̱			j	asomtravuli letter		hie	y	j	y	60	ⴢ		Svan, Mingrelian, Laz	georgian
+Ⴍ	O	Ɔ̱	O	o	œ	asomtravuli letter		oni	o	o	o	70	ⴍ		Svan	georgian
+Ⴣ	U	W̱			w	asomtravuli letter		vie	w			400	ⴣ		Svan	georgian
+Ⴓ	Y	U̱	U	u	y	asomtravuli letter		uni	u	u	u	400	ⴓ		Svan, Mingrelian, Laz	georgian
+Ⴤ	Q	Q̱ʰ			q⁽ʰ⁾	asomtravuli letter		qari, hari	ẖ	q'		7000	ⴤ		Svan	georgian
+Ⴧ	E	Ə̱			ə	asomtravuli letter		yn, schva					ⴧ		Svan, Mingrelian	georgian
+Ⴭ	E	Ə̱̇			ə	asomtravuli letter	archaic	aen					ⴭ		Ossetian	georgian
+																
+																
+																
+ⴀ	a	a̱	a	a	a æ	nuskhuri letter		ani	a	a	a	1		Ⴀ	Svan	georgian
+ⴡ	e	e̱			eː	nuskhuri letter		he	ē	ey		8		Ⴡ	Svan	georgian
+ⴢ	j	j̱			j	nuskhuri letter		hie	y	j	y	60		Ⴢ	Svan, Mingrelian, Laz	georgian
+ⴍ	o	ɔ̱	o	o	œ	nuskhuri letter		oni	o	o	o	70		Ⴍ	Svan	georgian
+ⴣ	u	w̱			w	nuskhuri letter		vie	w			400		Ⴣ	Svan	georgian
+ⴓ	y	u̱	u	u	y	nuskhuri letter		uni	u	u	u	400		Ⴓ	Svan, Mingrelian, Laz	georgian
+ⴤ	g	q̱ʰ			q⁽ʰ⁾	nuskhuri letter		qari, hari	ẖ	q'		7000		Ⴤ	Svan	georgian
+ⴧ	e	ə̱			ə	nuskhuri letter		yn, schva						Ⴧ	Svan, Mingrelian	georgian
+ⴭ	e	ə̱̇			ə	nuskhuri letter	archaic	aen						Ⴭ	Ossetian	georgian
+																
+																
+																
+																
+ა	a	a	a	a	a æ	mkhedruli letter		ani	a	a	a	1		Ა	Svan	georgian
+ჱ	e	e			eː	mkhedruli letter		he	ē	ey		8		Ჱ	Svan	georgian
+ჲ	j	j			j	mkhedruli letter		hie	y	j	y	60		Ჲ	Svan, Mingrelian, Laz	georgian
+ო	o	ɔ	o	o	ɔ œ	mkhedruli letter		oni	o	o	o	70		Ო	Svan	georgian
+ჳ	u	w			w	mkhedruli letter		vie	w			400		Ჳ	Svan	georgian
+უ	y	u	u	u	u y	mkhedruli letter		uni	u	u	u	400		Უ	Svan, Mingrelian, Laz	georgian
+ჷ	e	ə			ə	mkhedruli letter		yn, schva						Ჷ	Svan, Mingrelian	georgian
+ჸ	}	ʔ			ʔ	mkhedruli letter		elif						Ჸ	Svan, Mingrelian	georgian
+ჴ	q	qʰ			q⁽ʰ⁾	mkhedruli letter		qari, hari	ẖ	q'		7000		Ჴ	Svan	georgian
+ჶ	f	f			f	mkhedruli letter		fi	f		f			Ჶ	Laz	georgian
+ჹ	g	ɢ			ɢ	mkhedruli letter	archaic	turned gani						Ჹ	Dagenstan	georgian
+ჺ	{	ʕ			ʕ	mkhedruli letter	archaic	aini						Ჺ	Bats	georgian
+ჼ	n	ᴺ			̃	mkhedruli letter	archaic	modifier nar							Bats	georgian
+ჽ	e	ə̇			ə	mkhedruli letter	archaic	aen						Ჽ	Ossetian	georgian
+ჾ	l	ᴸ			̴	mkhedruli hard sign	archaic	hard sign						Ჾ	Abkhaz	georgian
+ჿ	w	ʷ			ʷ	mkhedruli labial sign	archaic	labial sign						Ჿ	Abkhaz	georgian
+																
+																
+																
+Ა	A	A	A	a	a æ	mtavruli letter		ani	a	a	a	1	ა		Svan	georgian
+Ჱ	E	E			eː	mtavruli letter		he	ē	ey		8	ჱ		Svan	georgian
+Ჲ	J	J			j	mtavruli letter		hie	y	j	y	60	ჲ		Svan, Mingrelian, Laz	georgian
+Ო	O	Ɔ	O	o	œ	mtavruli letter		oni	o	o	o	70	ო		Svan	georgian
+Ჳ	U	W			w	mtavruli letter		vie	w			400	ჳ		Svan	georgian
+Უ	Y	U	U	u	y	mtavruli letter		uni	u	u	u	400	უ		Svan, Mingrelian, Laz	georgian
+Ჷ	E	Ə			ə	mtavruli letter		yn, schva					ჷ		Svan, Mingrelian	georgian
+Ჸ	}	ʔ			ʔ	mtavruli letter		elif					ჸ		Svan, Mingrelian	georgian
+Ჴ	Q	Qʰ			q⁽ʰ⁾	mtavruli letter		qari, hari	ẖ	q'		7000	ჴ		Svan	georgian
+Ჶ	F	F			f	mtavruli letter		fi	f		f		ჶ		Laz	georgian
+Ჹ	G	ɢ			ɢ	mtavruli letter	archaic	turned gani					ჹ		Dagenstan	georgian
+Ჺ	{	ʕ			ʕ	mtavruli letter	archaic	aini					ჺ		Bats	georgian
+Ჽ	E	Ə̇			ə	mtavruli letter	archaic	aen					ჽ		Ossetian	georgian
+Ჾ	L	ᴸ̄			̴	mtavruli hard sign	archaic	hard sign					ჾ		Abkhaz	georgian
+Ჿ	W	ʷ̄			ʷ	mtavruli labial sign	archaic	labial sign					ჿ		Abkhaz	georgian
+																
+																
+																
+																
+჻		⁋				paragraph separator										georgian
+
+
+`
+
+var cols = {
+"transLoc": 2,
+"ipaLoc": 5,
+"typeLoc": 6,
+"nameLoc": 8,
+"nnameLoc": 0,
+"numLoc": 12,
+"statusLoc": 7,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"numLoc":11,
+"meaning":0,
+"shape":0,
+"lc":13,
+"uc":14,
+"transcription":4,
+"transcription2":9,
+
+"othertranscriptions": [[9, 'ISO']]
+}
+
+
+
+
+
+
+
+
+
