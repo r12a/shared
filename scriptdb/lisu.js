2c74b6cccd34c2b43f8136458243d78e2320c1c0
r12a <ishida@w3.org>
2020-01-14 05:09:53 +0000

    Adding block column

--- scriptdb/lisu.js
+++ scriptdb/lisu.js
@@ -1,66 +1,68 @@
 var spreadsheet = `
-ꓐ		b	b	b	b	b	consonant		U+A4D0: LISU LETTER BA
-ꓭ		y	ɣ	ɣ	y	ɣ	consonant/vowel		U+A4ED: LISU LETTER GHA
-ꓚ		t	ʨ	ʨ	t	ʨ	consonant		U+A4DA: LISU LETTER CA
-ꓛ		t	ʨʰ	ʨʰ	t	ʨʰ	consonant		U+A4DB: LISU LETTER CHA
-ꓓ		d	d	d	d	d	consonant		U+A4D3: LISU LETTER DA
-ꓝ		t	ʦ	ʦ	t	ʦ	consonant		U+A4DD: LISU LETTER TSA
-ꓞ		t	ʦʰ	ʦʰ	t	ʦʰ	consonant		U+A4DE: LISU LETTER TSHA
-ꓖ		g	ɡ	ɡ	g	ɡ	consonant		U+A4D6: LISU LETTER GA
-ꓨ		h	ɦ	ɦ	h	ɦ	consonant		U+A4E8: LISU LETTER HHA
-ꓧ		x	x	x	x	x	consonant		U+A4E7: LISU LETTER XA
-ꓙ		d	ʥ	ʥ	d	ʥ	consonant		U+A4D9: LISU LETTER JA
-ꓩ		f	f	f	f	f	consonant		U+A4E9: LISU LETTER FA
-ꓗ		k	k	k	k	k	consonant		U+A4D7: LISU LETTER KA
-ꓘ		k	kʰ	kʰ	k	kʰ	consonant		U+A4D8: LISU LETTER KHA
-ꓡ		l	l	l	l	l	consonant		U+A4E1: LISU LETTER LA
-ꓟ		m	m	m	m	m	consonant		U+A4DF: LISU LETTER MA
-ꓠ		n	n	n	n	n	consonant		U+A4E0: LISU LETTER NA
-ꓑ		p	p	p	p	p	consonant		U+A4D1: LISU LETTER PA
-ꓒ		p	pʰ	pʰ	p	pʰ	consonant		U+A4D2: LISU LETTER PHA
-ꓣ		z	ʒ	ʒ	z	ʒ	consonant		U+A4E3: LISU LETTER ZHA
-ꓤ		z	z	z	z	z	consonant		U+A4E4: LISU LETTER ZA
-ꓢ		s	s	s	s	s	consonant		U+A4E2: LISU LETTER SA
-ꓔ		t	t	t	t	t	consonant		U+A4D4: LISU LETTER TA
-ꓕ		t	tʰ	tʰ	t	tʰ	consonant		U+A4D5: LISU LETTER THA
-ꓪ		w	w	w	w	w	consonant/vowel		U+A4EA: LISU LETTER WA
-ꓥ		n	ŋ	ŋ	n	ŋ	consonant		U+A4E5: LISU LETTER NGA
-ꓦ		h	h	h	h	h	consonant		U+A4E6: LISU LETTER HA
-ꓫ		c	ɕ	ɕ	c	ɕ	consonant		U+A4EB: LISU LETTER SHA
-ꓬ		z	ʑ	ʑ	z	ʑ	consonant/vowel		U+A4EC: LISU LETTER YA
-ꓜ		d	ʣ	ʣ	d	ʣ	consonant		U+A4DC: LISU LETTER DZA
-									
-									
-ꓮ		a	ɑ	ɑ	a	ɑ	vowel		U+A4EE: LISU LETTER A
-ꓯ		e	ɛ	ɛ	e	ɛ	vowel		U+A4EF: LISU LETTER AE
-ꓷ		e	ə	ə	e	ə	vowel		U+A4F7: LISU LETTER OE
-ꓰ		e	e	e	e	e	vowel		U+A4F0: LISU LETTER E
-ꓱ		o	ø	ø	o	ø	vowel		U+A4F1: LISU LETTER EU
-ꓲ		i	i	i	i	i	vowel		U+A4F2: LISU LETTER I
-ꓶ		u	ɯ	ɯ	u	ɯ	vowel		U+A4F6: LISU LETTER UH
-ꓳ		o	o	o	o	o	vowel		U+A4F3: LISU LETTER O
-ꓴ		u	u	u	u	u	vowel		U+A4F4: LISU LETTER U
-ꓵ		y	y	y	y	y	vowel		U+A4F5: LISU LETTER UE
-ˍ		a	ä		a	ä	vowel		U+02CD: MODIFIER LETTER LOW MACRON
-ʼ		n	ñ		n	ñ	vowel		U+02BC: MODIFIER LETTER APOSTROPHE
-									
-									
-ꓸ		^	¹	˥˥	^	˥˥	pitch 55 tone mark		U+A4F8: LISU LETTER TONE MYA TI
-ꓹ		^	²	˧˥	^	˧˥	pitch 35 tone mark		U+A4F9: LISU LETTER TONE NA PO
-ꓺ		^	³	˦˦	^	˦˦	pitch 44 tone mark		U+A4FA: LISU LETTER TONE MYA CYA
-ꓻ		^	⁴	˧˧	^	˧˧	pitch 33 tone mark		U+A4FB: LISU LETTER TONE MYA BO
-ꓼ		^	⁵	˦˨	^	˦˨	pitch 42 tone mark		U+A4FC: LISU LETTER TONE MYA NA
-ꓽ		^	⁶	˧˩	^	˧˩	pitch 31 tone mark		U+A4FD: LISU LETTER TONE MYA JEU
-									
-									
-꓾		,	,		,	,	comma		U+A4FE: LISU PUNCTUATION COMMA
-꓿		.	.		.	.	full stop		U+A4FF: LISU PUNCTUATION FULL STOP
-《		(	<		(	<	bracket		U+300A: LEFT DOUBLE ANGLE BRACKET
-》		)	>		)	>	bracket		U+300B: RIGHT DOUBLE ANGLE BRACKET
-									
-									
-									
-ꓹꓼ	s								U+A4F9 U+A4FC: LISU LETTER TONE NA PO, LISU LETTER TONE MYA NA
+ꓐ		b	b	b	b	b	consonant		U+A4D0: LISU LETTER BA	lisu
+ꓭ		y	ɣ	ɣ	y	ɣ	consonant/vowel		U+A4ED: LISU LETTER GHA	lisu
+ꓚ		t	ʨ	ʨ	t	ʨ	consonant		U+A4DA: LISU LETTER CA	lisu
+ꓛ		t	ʨʰ	ʨʰ	t	ʨʰ	consonant		U+A4DB: LISU LETTER CHA	lisu
+ꓓ		d	d	d	d	d	consonant		U+A4D3: LISU LETTER DA	lisu
+ꓝ		t	ʦ	ʦ	t	ʦ	consonant		U+A4DD: LISU LETTER TSA	lisu
+ꓞ		t	ʦʰ	ʦʰ	t	ʦʰ	consonant		U+A4DE: LISU LETTER TSHA	lisu
+ꓖ		g	ɡ	ɡ	g	ɡ	consonant		U+A4D6: LISU LETTER GA	lisu
+ꓨ		h	ɦ	ɦ	h	ɦ	consonant		U+A4E8: LISU LETTER HHA	lisu
+ꓧ		x	x	x	x	x	consonant		U+A4E7: LISU LETTER XA	lisu
+ꓙ		d	ʥ	ʥ	d	ʥ	consonant		U+A4D9: LISU LETTER JA	lisu
+ꓩ		f	f	f	f	f	consonant		U+A4E9: LISU LETTER FA	lisu
+ꓗ		k	k	k	k	k	consonant		U+A4D7: LISU LETTER KA	lisu
+ꓘ		k	kʰ	kʰ	k	kʰ	consonant		U+A4D8: LISU LETTER KHA	lisu
+ꓡ		l	l	l	l	l	consonant		U+A4E1: LISU LETTER LA	lisu
+ꓟ		m	m	m	m	m	consonant		U+A4DF: LISU LETTER MA	lisu
+ꓠ		n	n	n	n	n	consonant		U+A4E0: LISU LETTER NA	lisu
+ꓑ		p	p	p	p	p	consonant		U+A4D1: LISU LETTER PA	lisu
+ꓒ		p	pʰ	pʰ	p	pʰ	consonant		U+A4D2: LISU LETTER PHA	lisu
+ꓣ		z	ʒ	ʒ	z	ʒ	consonant		U+A4E3: LISU LETTER ZHA	lisu
+ꓤ		z	z	z	z	z	consonant		U+A4E4: LISU LETTER ZA	lisu
+ꓢ		s	s	s	s	s	consonant		U+A4E2: LISU LETTER SA	lisu
+ꓔ		t	t	t	t	t	consonant		U+A4D4: LISU LETTER TA	lisu
+ꓕ		t	tʰ	tʰ	t	tʰ	consonant		U+A4D5: LISU LETTER THA	lisu
+ꓪ		w	w	w	w	w	consonant/vowel		U+A4EA: LISU LETTER WA	lisu
+ꓥ		n	ŋ	ŋ	n	ŋ	consonant		U+A4E5: LISU LETTER NGA	lisu
+ꓦ		h	h	h	h	h	consonant		U+A4E6: LISU LETTER HA	lisu
+ꓫ		c	ɕ	ɕ	c	ɕ	consonant		U+A4EB: LISU LETTER SHA	lisu
+ꓬ		z	ʑ	ʑ	z	ʑ	consonant/vowel		U+A4EC: LISU LETTER YA	lisu
+ꓜ		d	ʣ	ʣ	d	ʣ	consonant		U+A4DC: LISU LETTER DZA	lisu
+										
+										
+ꓮ		a	ɑ	ɑ	a	ɑ	vowel		U+A4EE: LISU LETTER A	lisu
+ꓯ		e	ɛ	ɛ	e	ɛ	vowel		U+A4EF: LISU LETTER AE	lisu
+ꓷ		e	ə	ə	e	ə	vowel		U+A4F7: LISU LETTER OE	lisu
+ꓰ		e	e	e	e	e	vowel		U+A4F0: LISU LETTER E	lisu
+ꓱ		o	ø	ø	o	ø	vowel		U+A4F1: LISU LETTER EU	lisu
+ꓲ		i	i	i	i	i	vowel		U+A4F2: LISU LETTER I	lisu
+ꓶ		u	ɯ	ɯ	u	ɯ	vowel		U+A4F6: LISU LETTER UH	lisu
+ꓳ		o	o	o	o	o	vowel		U+A4F3: LISU LETTER O	lisu
+ꓴ		u	u	u	u	u	vowel		U+A4F4: LISU LETTER U	lisu
+ꓵ		y	y	y	y	y	vowel		U+A4F5: LISU LETTER UE	lisu
+ˍ		a	ä		a	ä	vowel		U+02CD: MODIFIER LETTER LOW MACRON	
+ʼ		n	ñ		n	ñ	vowel		U+02BC: MODIFIER LETTER APOSTROPHE	
+										
+										
+ꓸ		^	¹	˥˥	^	˥˥	pitch 55 tone mark		U+A4F8: LISU LETTER TONE MYA TI	lisu
+ꓹ		^	²	˧˥	^	˧˥	pitch 35 tone mark		U+A4F9: LISU LETTER TONE NA PO	lisu
+ꓺ		^	³	˦˦	^	˦˦	pitch 44 tone mark		U+A4FA: LISU LETTER TONE MYA CYA	lisu
+ꓻ		^	⁴	˧˧	^	˧˧	pitch 33 tone mark		U+A4FB: LISU LETTER TONE MYA BO	lisu
+ꓼ		^	⁵	˦˨	^	˦˨	pitch 42 tone mark		U+A4FC: LISU LETTER TONE MYA NA	lisu
+ꓽ		^	⁶	˧˩	^	˧˩	pitch 31 tone mark		U+A4FD: LISU LETTER TONE MYA JEU	lisu
+										
+										
+꓾		,	,		,	,	comma		U+A4FE: LISU PUNCTUATION COMMA	lisu
+꓿		.	.		.	.	full stop		U+A4FF: LISU PUNCTUATION FULL STOP	lisu
+《		(	<		(	<	bracket		U+300A: LEFT DOUBLE ANGLE BRACKET	
+》		)	>		)	>	bracket		U+300B: RIGHT DOUBLE ANGLE BRACKET	
+										
+										
+										
+ꓹꓼ	s		²⁵						U+A4F9 U+A4FC: LISU LETTER TONE NA PO, LISU LETTER TONE MYA NA	
+
+
 `
 
 
@@ -87,6 +89,7 @@ var cols = {
 "nameLoc": 0,
 "nnameLoc": 0,
 "ucsName": 9,
+"block": 10,
 
 //"othertranscriptions": [[6, 'ISO 9985']]
 }
