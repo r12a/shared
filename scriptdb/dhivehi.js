f3f9333e56b0d804d8c3694f4151b4f6bae42f95
r12a <ishida@w3.org>
2020-01-14 09:31:28 +0000

    Adding block column

--- scriptdb/dhivehi.js
+++ scriptdb/dhivehi.js
@@ -1,62 +1,63 @@
 var spreadsheet = `
-ހ		h	h	h	h	h	consonant		haa	ހާ	U+0780: THAANA LETTER HAA
-ށ		s	ʂ	ʂ	s	sh	consonant		shaviyana	ށަވިޔަނަ	U+0781: THAANA LETTER SHAVIYANI
-ނ		n	n	n	n	n	consonant		noonu	ނޫނު	U+0782: THAANA LETTER NOONU
-ރ		r	r	r	r	r	consonant		raa	ރާ	U+0783: THAANA LETTER RAA
-ބ		b	b	b	b	b	consonant		baa	ބާ	U+0784: THAANA LETTER BAA
-ޅ		l	ɭ	ɭ	l	lh	consonant		lhaviyani	ޅަވިޔަނި	U+0785: THAANA LETTER LHAVIYANI
-ކ		k	k	k	k	k	consonant		kaafu	ކާފު	U+0786: THAANA LETTER KAAFU
-އ		{	ʿ	∅	[		vowel support		alifu	އަލިފު	U+0787: THAANA LETTER ALIFU
-ވ		v	v	v	v	v	consonant		vaavu	ވާވު	U+0788: THAANA LETTER VAAVU
-މ		m	m	m	m	m	consonant		meemu	މީމު	U+0789: THAANA LETTER MEEMU
-ފ		f	f	f	f	f	consonant		faafu	ފާފު	U+078A: THAANA LETTER FAAFU
-ދ		d	d	d	d	dh	consonant		dhaalu	ދާލު	U+078B: THAANA LETTER DHAALU
-ތ		t	t	t	t	th	consonant		thaa	ތާ	U+078C: THAANA LETTER THAA
-ލ		l	l	l	l	l	consonant		laamu	ލާމު	U+078D: THAANA LETTER LAAMU
-ގ		g	g	ɡ	g	g	consonant		gaafu	ގާފު	U+078E: THAANA LETTER GAAFU
-ޏ		n	ɲ	ɲ	g	gn	consonant		gnaviyani	ޏަވިޔަނި	U+078F: THAANA LETTER GNAVIYANI
-ސ		s	s	s	s	s	consonant		seenu	ސީނު	U+0790: THAANA LETTER SEENU
-ޑ		d	ɖ	ɖ	d	d	consonant		daviyani	ޑަވިޔަނި	U+0791: THAANA LETTER DAVIYANI
-ޒ		z	z	z	z	z	consonant		zaviyani	ޒަވިޔަނި	U+0792: THAANA LETTER ZAVIYANI
-ޓ		t	ʈ	ʈ	t	t	consonant		taviyani	ޓަވިޔަނި	U+0793: THAANA LETTER TAVIYANI
-ޔ		y	y	j	y	y	consonant		yaviyani	ޔަވިޔަނި	U+0794: THAANA LETTER YAA
-ޕ		p	p	p	p	p	consonant		paviyani	ޕަވިޔަނި	U+0795: THAANA LETTER PAVIYANI
-ޖ		j	ɟ	ɟ	j	j	consonant		javiyani	ޖަވިޔަނި	U+0796: THAANA LETTER JAVIYANI
-ޗ		c	c	c	c	ch	consonant		chaviyani	ޗަވިޔަނި	U+0797: THAANA LETTER CHAVIYANI
-											
-											
-ޘ		Q	θ	ث	T	ṯ	arabic consonant		ttaa	ޘާ	U+0798: THAANA LETTER TTAA
-ޙ		H	ħ	ح	H	ḥ	arabic consonant		hhaa	ޙާ	U+0799: THAANA LETTER HHAA
-ޚ		X	x	خ	H	ḫ	arabic consonant		khaa	ޚާ	U+079A: THAANA LETTER KHAA
-ޛ		D	ð	ذ	D	ḏ	arabic consonant		thaalu	ޛާލު	U+079B: THAANA LETTER THAALU
-ޜ		Z	ʒ	ʒ	£		arabic consonant		zaa	ޜާ	U+079C: THAANA LETTER ZAA
-ޝ		S	ʃ	ش	S	š	arabic consonant		sheenu	ޝީނު	U+079D: THAANA LETTER SHEENU
-ޞ		S	s̴	ص	S	ṣ	arabic consonant		saadhu	ޞާޑު	U+079E: THAANA LETTER SAADHU
-ޟ		D	d̴	ض	D	d̤	arabic consonant		daadhu	ޟާޑު	U+079F: THAANA LETTER DAADHU
-ޠ		T	t̴	ط	T	t̤	arabic consonant		to	ޓޮ	U+07A0: THAANA LETTER TO
-ޡ		Z	z̴	ظ	£		arabic consonant		zo	ޡޮ	U+07A1: THAANA LETTER ZO
-ޢ		{	ʕ	ع	{	ʻ	arabic consonant		aïnu	ޢައިނު	U+07A2: THAANA LETTER AINU
-ޣ		Y	ɣ	غ	G	ǵ	arabic consonant		ghaïnu	ޣައިނު	U+07A3: THAANA LETTER GHAINU
-ޤ		Q	q	ق	Q	q	arabic consonant		qaafu	ޤާފު	U+07A4: THAANA LETTER QAAFU
-ޥ		W	w	و	W	w	arabic consonant		waavu	ޥާވު	U+07A5: THAANA LETTER WAAVU
-ޱ		N	ɳ	ɳ	N	ṇ	arabic consonant		naviyani	ޱަވިޔަނަ	U+07B1: THAANA LETTER NAA
-											
-											
-ަ		e	ə	ə	a	a	vowel		aba fili	އަބަ ފިލި	U+07A6: THAANA ABAFILI​
-ާ		e	ə̄	əː	a	aa	vowel		aabaa fili	އާބާ ފިލި	U+07A7: THAANA AABAAFILI​
-ި		i	i	i	i	i	vowel		ibi fili	އިބި ފިލި	U+07A8: THAANA IBIFILI​
-ީ		i	ī	iː	e	ee	vowel		eebee fili	އީބީ ފިލި	U+07A9: THAANA EEBEEFILI​
-ު		u	u	u	u	u	vowel		ubu fili	އުބު ފިލި	U+07AA: THAANA UBUFILI​
-ޫ		u	ū	uː	u	oo	vowel		ooboo fili	އޫބޫ ފިލި	U+07AB: THAANA OOBOOFILI​
-ެ		e	e	e	e	e	vowel		ebe fili	އެބެެ ފިލި	U+07AC: THAANA EBEFILI​
-ޭ		e	ē	eː	e	ey	vowel		eybey fili	އޭބޭ ފިލި	U+07AD: THAANA EYBEYFILI​
-ޮ		o	o	ɔ	o	oo	vowel		obo fili	އޮބޮ ފިލި	U+07AE: THAANA OBOFILI​
-ޯ		o	ō	ɔː	o	oa	vowel		oaboa fili	އޯބޯ ފިލި	U+07AF: THAANA OABOAFILI​
-ް		*	͓	∅	[		vowel killer		sukun	ސުކުނ	U+07B0: THAANA SUKUN​
-											
-											
-،		,	,		,	,	arabic comma				U+060C: ARABIC COMMA
-؛		;	;		;	;	arabic semicolon				U+061B: ARABIC SEMICOLON
+ހ		h	h	h	h	h	consonant		haa	ހާ	U+0780: THAANA LETTER HAA	thaana
+ށ		s	ʂ	ʂ	s	sh	consonant		shaviyana	ށަވިޔަނަ	U+0781: THAANA LETTER SHAVIYANI	thaana
+ނ		n	n	n	n	n	consonant		noonu	ނޫނު	U+0782: THAANA LETTER NOONU	thaana
+ރ		r	r	r	r	r	consonant		raa	ރާ	U+0783: THAANA LETTER RAA	thaana
+ބ		b	b	b	b	b	consonant		baa	ބާ	U+0784: THAANA LETTER BAA	thaana
+ޅ		l	ɭ	ɭ	l	lh	consonant		lhaviyani	ޅަވިޔަނި	U+0785: THAANA LETTER LHAVIYANI	thaana
+ކ		k	k	k	k	k	consonant		kaafu	ކާފު	U+0786: THAANA LETTER KAAFU	thaana
+އ		{	ʿ	∅	[		vowel support		alifu	އަލިފު	U+0787: THAANA LETTER ALIFU	thaana
+ވ		v	v	v	v	v	consonant		vaavu	ވާވު	U+0788: THAANA LETTER VAAVU	thaana
+މ		m	m	m	m	m	consonant		meemu	މީމު	U+0789: THAANA LETTER MEEMU	thaana
+ފ		f	f	f	f	f	consonant		faafu	ފާފު	U+078A: THAANA LETTER FAAFU	thaana
+ދ		d	d	d	d	dh	consonant		dhaalu	ދާލު	U+078B: THAANA LETTER DHAALU	thaana
+ތ		t	t	t	t	th	consonant		thaa	ތާ	U+078C: THAANA LETTER THAA	thaana
+ލ		l	l	l	l	l	consonant		laamu	ލާމު	U+078D: THAANA LETTER LAAMU	thaana
+ގ		g	g	ɡ	g	g	consonant		gaafu	ގާފު	U+078E: THAANA LETTER GAAFU	thaana
+ޏ		n	ɲ	ɲ	g	gn	consonant		gnaviyani	ޏަވިޔަނި	U+078F: THAANA LETTER GNAVIYANI	thaana
+ސ		s	s	s	s	s	consonant		seenu	ސީނު	U+0790: THAANA LETTER SEENU	thaana
+ޑ		d	ɖ	ɖ	d	d	consonant		daviyani	ޑަވިޔަނި	U+0791: THAANA LETTER DAVIYANI	thaana
+ޒ		z	z	z	z	z	consonant		zaviyani	ޒަވިޔަނި	U+0792: THAANA LETTER ZAVIYANI	thaana
+ޓ		t	ʈ	ʈ	t	t	consonant		taviyani	ޓަވިޔަނި	U+0793: THAANA LETTER TAVIYANI	thaana
+ޔ		y	y	j	y	y	consonant		yaviyani	ޔަވިޔަނި	U+0794: THAANA LETTER YAA	thaana
+ޕ		p	p	p	p	p	consonant		paviyani	ޕަވިޔަނި	U+0795: THAANA LETTER PAVIYANI	thaana
+ޖ		j	ɟ	ɟ	j	j	consonant		javiyani	ޖަވިޔަނި	U+0796: THAANA LETTER JAVIYANI	thaana
+ޗ		c	c	c	c	ch	consonant		chaviyani	ޗަވިޔަނި	U+0797: THAANA LETTER CHAVIYANI	thaana
+												
+												
+ޘ		Q	θ	ث	T	ṯ	arabic consonant		ttaa	ޘާ	U+0798: THAANA LETTER TTAA	thaana
+ޙ		H	ħ	ح	H	ḥ	arabic consonant		hhaa	ޙާ	U+0799: THAANA LETTER HHAA	thaana
+ޚ		X	x	خ	H	ḫ	arabic consonant		khaa	ޚާ	U+079A: THAANA LETTER KHAA	thaana
+ޛ		D	ð	ذ	D	ḏ	arabic consonant		thaalu	ޛާލު	U+079B: THAANA LETTER THAALU	thaana
+ޜ		Z	ʒ	ʒ	£		arabic consonant		zaa	ޜާ	U+079C: THAANA LETTER ZAA	thaana
+ޝ		S	ʃ	ش	S	š	arabic consonant		sheenu	ޝީނު	U+079D: THAANA LETTER SHEENU	thaana
+ޞ		S	s̴	ص	S	ṣ	arabic consonant		saadhu	ޞާޑު	U+079E: THAANA LETTER SAADHU	thaana
+ޟ		D	d̴	ض	D	d̤	arabic consonant		daadhu	ޟާޑު	U+079F: THAANA LETTER DAADHU	thaana
+ޠ		T	t̴	ط	T	t̤	arabic consonant		to	ޓޮ	U+07A0: THAANA LETTER TO	thaana
+ޡ		Z	z̴	ظ	£		arabic consonant		zo	ޡޮ	U+07A1: THAANA LETTER ZO	thaana
+ޢ		{	ʕ	ع	{	ʻ	arabic consonant		aïnu	ޢައިނު	U+07A2: THAANA LETTER AINU	thaana
+ޣ		Y	ɣ	غ	G	ǵ	arabic consonant		ghaïnu	ޣައިނު	U+07A3: THAANA LETTER GHAINU	thaana
+ޤ		Q	q	ق	Q	q	arabic consonant		qaafu	ޤާފު	U+07A4: THAANA LETTER QAAFU	thaana
+ޥ		W	w	و	W	w	arabic consonant		waavu	ޥާވު	U+07A5: THAANA LETTER WAAVU	thaana
+ޱ		N	ɳ	ɳ	N	ṇ	arabic consonant		naviyani	ޱަވިޔަނަ	U+07B1: THAANA LETTER NAA	thaana
+												
+												
+ަ		e	ə	ə	a	a	vowel		aba fili	އަބަ ފިލި	U+07A6: THAANA ABAFILI​	thaana
+ާ		e	ə̄	əː	a	aa	vowel		aabaa fili	އާބާ ފިލި	U+07A7: THAANA AABAAFILI​	thaana
+ި		i	i	i	i	i	vowel		ibi fili	އިބި ފިލި	U+07A8: THAANA IBIFILI​	thaana
+ީ		i	ī	iː	e	ee	vowel		eebee fili	އީބީ ފިލި	U+07A9: THAANA EEBEEFILI​	thaana
+ު		u	u	u	u	u	vowel		ubu fili	އުބު ފިލި	U+07AA: THAANA UBUFILI​	thaana
+ޫ		u	ū	uː	u	oo	vowel		ooboo fili	އޫބޫ ފިލި	U+07AB: THAANA OOBOOFILI​	thaana
+ެ		e	e	e	e	e	vowel		ebe fili	އެބެެ ފިލި	U+07AC: THAANA EBEFILI​	thaana
+ޭ		e	ē	eː	e	ey	vowel		eybey fili	އޭބޭ ފިލި	U+07AD: THAANA EYBEYFILI​	thaana
+ޮ		o	o	ɔ	o	oo	vowel		obo fili	އޮބޮ ފިލި	U+07AE: THAANA OBOFILI​	thaana
+ޯ		o	ō	ɔː	o	oa	vowel		oaboa fili	އޯބޯ ފިލި	U+07AF: THAANA OABOAFILI​	thaana
+ް		*	͓	∅	[		vowel killer		sukun	ސުކުނ	U+07B0: THAANA SUKUN​	thaana
+												
+												
+،		,	,		,	,	arabic comma				U+060C: ARABIC COMMA	arabic
+؛		;	;		;	;	arabic semicolon				U+061B: ARABIC SEMICOLON	arabic
+
 
 `
 
@@ -84,6 +85,7 @@ var cols = {
 "nameLoc": 9,
 "nnameLoc": 10,
 "ucsName": 11,
+"block": 12,
 
 "othertranscriptions": [[6, 'Maldivian Latin']]
 }
