bbabd000cbcea9c8bf5418df41c1ef9532825159
r12a <ishida@w3.org>
2020-01-14 07:36:36 +0000

    Adding block column

--- scriptdb/gujarati.js
+++ scriptdb/gujarati.js
@@ -1,111 +1,114 @@
 var spreadsheet = `
-ક		k	k	k	k	consonant					U+0A95: GUJARATI LETTER KA
-ખ		K	kʰ	kʰ	kh	consonant					U+0A96: GUJARATI LETTER KHA
-ગ		g	g	g	g	consonant					U+0A97: GUJARATI LETTER GA
-ઘ		G	gʰ	gʰ	gh	consonant					U+0A98: GUJARATI LETTER GHA
-ઙ		N	ŋ	ŋ	ṅ	consonant					U+0A99: GUJARATI LETTER NGA
-ચ		c	c	ʧ	c	consonant					U+0A9A: GUJARATI LETTER CA
-છ		c	cʰ	ʧʰ	ch	consonant					U+0A9B: GUJARATI LETTER CHA
-જ		d	ʤ	ʤ	j	consonant					U+0A9C: GUJARATI LETTER JA
-ઝ		d	ʤʰ	ʤʰ	jh	consonant					U+0A9D: GUJARATI LETTER JHA
-ઞ		n	ɲ	ɲ	ñ	consonant					U+0A9E: GUJARATI LETTER NYA
-ટ		t	ʈ	ʈ	ṭ	consonant					U+0A9F: GUJARATI LETTER TTA
-ઠ		T	ʈʰ	ʈʰ	ṭh	consonant					U+0AA0: GUJARATI LETTER TTHA
-ડ		d	ɖ	ɖ	ḍ	consonant					U+0AA1: GUJARATI LETTER DDA
-ઢ		D	ɖʰ	ɖʰ	ḍh	consonant					U+0AA2: GUJARATI LETTER DDHA
-ણ		n	ɳ	ɳ	ṇ	consonant					U+0AA3: GUJARATI LETTER NNA
-ત		t¶	t	t	t	consonant					U+0AA4: GUJARATI LETTER TA
-થ		T¶	tʰ	tʰ	th	consonant					U+0AA5: GUJARATI LETTER THA
-દ		d¶	d	d	d	consonant					U+0AA6: GUJARATI LETTER DA
-ધ		D¶	dʰ	dʰ	dh	consonant					U+0AA7: GUJARATI LETTER DHA
-ન		n¶	n	n	n	consonant					U+0AA8: GUJARATI LETTER NA
-પ		p	p	p	p	consonant					U+0AAA: GUJARATI LETTER PA
-ફ		f	f	f	ph	consonant					U+0AAB: GUJARATI LETTER PHA
-બ		b	b	b	b	consonant					U+0AAC: GUJARATI LETTER BA
-ભ		B	bʰ	bʰ	bh	consonant					U+0AAD: GUJARATI LETTER BHA
-મ		m	m	m	m	consonant					U+0AAE: GUJARATI LETTER MA
-ય		y	y	j	y	consonant					U+0AAF: GUJARATI LETTER YA
-ર		r	r	r	r	consonant					U+0AB0: GUJARATI LETTER RA
-લ		l	l	l	l	consonant					U+0AB2: GUJARATI LETTER LA
-ળ		l	ɭ	ɭ	ḷ	consonant					U+0AB3: GUJARATI LETTER LLA
-વ		v	ʋ	w ʋ	v	consonant					U+0AB5: GUJARATI LETTER VA
-શ		c	ɕ	ɕ ʃ	ś	consonant					U+0AB6: GUJARATI LETTER SHA
-ષ		s	ʃ	ʃ	ṣ	consonant					U+0AB7: GUJARATI LETTER SSA
-સ		s¶	s	s	s	consonant					U+0AB8: GUJARATI LETTER SA
-હ		h	h	ɦ	h	consonant					U+0AB9: GUJARATI LETTER HA
-											
-											
-											
-ઁ					m̐						U+0A81: GUJARATI SIGN CANDRABINDU​
-ં		[	˜	ä	ṁ	vowel nasalisation				anusvār	U+0A82: GUJARATI SIGN ANUSVARA​
-ઃ		h	h̽	ɨ	ḥ	visarga				visarga	U+0A83: GUJARATI SIGN VISARGA​
-											
-											
-ૃ		R	r̥	ru	r̥	dependent vocalic			ઋ		U+0AC3: GUJARATI VOWEL SIGN VOCALIC R​
-ૄ		R	r̥̄		r̥̄	dependent vocalic			ૠ		U+0AC4: GUJARATI VOWEL SIGN VOCALIC RR​
-ઋ		R	r̥̣	ru	r̥	independent vocalic		ૃ			U+0A8B: GUJARATI LETTER VOCALIC R
-ૠ		R	r̥̣̄		r̥̄	independent vocalic		ૄ			U+0AE0: GUJARATI LETTER VOCALIC RR
-ૢ		L	l̥		l̥	dependent vocalic			ઌ		U+0AE2: GUJARATI VOWEL SIGN VOCALIC L​
-ૣ		L	l̥̄		l̥̄	dependent vocalic			ૡ		U+0AE3: GUJARATI VOWEL SIGN VOCALIC LL​
-ઌ		L	l̥̣		l̥	independent vocalic		ૢ			U+0A8C: GUJARATI LETTER VOCALIC L
-ૡ		L	l̥̣̄		l̥̄	independent vocalic		ૣ			U+0AE1: GUJARATI LETTER VOCALIC LL
-											
-											
-અ		E	ə̣	ə	a	independent vowel					U+0A85: GUJARATI LETTER A
-આ		A	ạ̄	ɑ	ā	independent vowel		ા			U+0A86: GUJARATI LETTER AA
-ઇ		I	ị	i	i	independent vowel		િ			U+0A87: GUJARATI LETTER I
-ઈ		I	ị̄	i	ī	independent vowel		ી			U+0A88: GUJARATI LETTER II
-ઉ		U	ụ	u	u	independent vowel		ુ			U+0A89: GUJARATI LETTER U
-ઊ		U	ụ̄	u	ū	independent vowel		ૂ			U+0A8A: GUJARATI LETTER UU
-એ		E	ẹ	e ɛ	ē	independent vowel		ે			U+0A8F: GUJARATI LETTER E
-ઐ		E	ə̣ʲ	əj	ai	independent vowel		ૈ			U+0A90: GUJARATI LETTER AI
-ઓ		O	ọ	o ɔ	ō	independent vowel		ો			U+0A93: GUJARATI LETTER O
-ઔ		E	ə̣ʷ	əʋ	au	independent vowel		ૌ			U+0A94: GUJARATI LETTER AU
-ઍ		A	æ̣	æ	ê	independent vowel		ૅ			U+0A8D: GUJARATI VOWEL CANDRA E
-ઑ		O	ɔ̣	ɔ	ô	independent vowel		ૉ			U+0A91: GUJARATI VOWEL CANDRA O
-											
-											
-ા		e	ə	ɑ	ā	vowel sign			આ	kāno	U+0ABE: GUJARATI VOWEL SIGN AA​
-િ		i	i	i	i	vowel sign			ઇ	hrasva-ajju	U+0ABF: GUJARATI VOWEL SIGN I​
-ી		i	ī	i	ī	vowel sign			ઈ	dīrgha-ajju	U+0AC0: GUJARATI VOWEL SIGN II​
-ુ		u	u	u	u	vowel sign			ઉ	hrasva-varaṛũ	U+0AC1: GUJARATI VOWEL SIGN U​
-ૂ		u	ū	u	ū	vowel sign			ઊ	dīrgha-varaṛũ	U+0AC2: GUJARATI VOWEL SIGN UU​
-ે		e	e	e ɛ	ē	vowel sign			એ	ek mātra	U+0AC7: GUJARATI VOWEL SIGN E​
-ૈ		e	əʲ	əj	ai	vowel sign			ઐ	be mātra	U+0AC8: GUJARATI VOWEL SIGN AI​
-ો		o	o	o ɔ	ō	vowel sign			ઓ	kāno ek mātra	U+0ACB: GUJARATI VOWEL SIGN O​
-ૌ		e	əʷ	əʋ	au	vowel sign			ઔ	kāno be mātra	U+0ACC: GUJARATI VOWEL SIGN AU​
-ૅ		a	æ	æ	ê	vowel sign			ઍ		U+0AC5: GUJARATI VOWEL SIGN CANDRA E​
-ૉ		o	ɔ	ɔ	ô	vowel sign			ઑ		U+0AC9: GUJARATI VOWEL SIGN CANDRA O​
-											
-											
-ૐ											U+0AD0: GUJARATI OM
-૱											U+0AF1: GUJARATI RUPEE SIGN
-											
-											
-્		*	͓			virama					U+0ACD: GUJARATI SIGN VIRAMA​
-઼		[	̣			nukta					U+0ABC: GUJARATI SIGN NUKTA​
-											
-											
-૦		0	0			digit				mīṇḍu or shunya	U+0AE6: GUJARATI DIGIT ZERO
-૧		1	1			digit				ekado or ek	U+0AE7: GUJARATI DIGIT ONE
-૨		2	2			digit				bagado or bay	U+0AE8: GUJARATI DIGIT TWO
-૩		3	3			digit				tragado or tran	U+0AE9: GUJARATI DIGIT THREE
-૪		4	4			digit				chogado or chaar	U+0AEA: GUJARATI DIGIT FOUR
-૫		5	5			digit				pāchado or paanch	U+0AEB: GUJARATI DIGIT FIVE
-૬		6	6			digit				chagado or chah	U+0AEC: GUJARATI DIGIT SIX
-૭		7	7			digit				sātado or sāt	U+0AED: GUJARATI DIGIT SEVEN
-૮		8	8			digit				āṭhado or āanth	U+0AEE: GUJARATI DIGIT EIGHT
-૯		9	9			digit				navado or nav	U+0AEF: GUJARATI DIGIT NINE
-											
-											
-।		.	.	,							U+0964: DEVANAGARI DANDA
-॥		[	¶	.							U+0965: DEVANAGARI DOUBLE DANDA
-૰											U+0AF0: GUJARATI ABBREVIATION SIGN
-											
-											
-											
-ક્ષ		k	k͓ʃ	kʃ		consonant					U+0A95 U+0ACD U+0AB7: GUJARATI LETTER KA, SIGN VIRAMA, LETTER SSA
-જ્ઞ		j	ʤ͓ɲ	ɡn		consonant					U+0A9C U+0ACD U+0A9E: GUJARATI LETTER JA, SIGN VIRAMA, LETTER NYA
+ક		k	k	k	k	consonant					U+0A95: GUJARATI LETTER KA	gujarati
+ખ		K	kʰ	kʰ	kh	consonant					U+0A96: GUJARATI LETTER KHA	gujarati
+ગ		g	g	ɡ	g	consonant					U+0A97: GUJARATI LETTER GA	gujarati
+ઘ		G	gʰ	ɡʰ	gh	consonant					U+0A98: GUJARATI LETTER GHA	gujarati
+ઙ		N	ŋ	ŋ	ṅ	consonant					U+0A99: GUJARATI LETTER NGA	gujarati
+ચ		c	c	ʧ	c	consonant					U+0A9A: GUJARATI LETTER CA	gujarati
+છ		c	cʰ	ʧʰ	ch	consonant					U+0A9B: GUJARATI LETTER CHA	gujarati
+જ		d	ʤ	ʤ	j	consonant					U+0A9C: GUJARATI LETTER JA	gujarati
+ઝ		d	ʤʰ	ʤʰ	jh	consonant					U+0A9D: GUJARATI LETTER JHA	gujarati
+ઞ		n	ɲ	ɲ	ñ	consonant					U+0A9E: GUJARATI LETTER NYA	gujarati
+ટ		t	ʈ	ʈ	ṭ	consonant					U+0A9F: GUJARATI LETTER TTA	gujarati
+ઠ		T	ʈʰ	ʈʰ	ṭh	consonant					U+0AA0: GUJARATI LETTER TTHA	gujarati
+ડ		d	ɖ	ɖ	ḍ	consonant					U+0AA1: GUJARATI LETTER DDA	gujarati
+ઢ		D	ɖʰ	ɖʰ	ḍh	consonant					U+0AA2: GUJARATI LETTER DDHA	gujarati
+ણ		n	ɳ	ɳ	ṇ	consonant					U+0AA3: GUJARATI LETTER NNA	gujarati
+ત		t¶	t	t	t	consonant					U+0AA4: GUJARATI LETTER TA	gujarati
+થ		T¶	tʰ	tʰ	th	consonant					U+0AA5: GUJARATI LETTER THA	gujarati
+દ		d¶	d	d	d	consonant					U+0AA6: GUJARATI LETTER DA	gujarati
+ધ		D¶	dʰ	dʰ	dh	consonant					U+0AA7: GUJARATI LETTER DHA	gujarati
+ન		n¶	n	n	n	consonant					U+0AA8: GUJARATI LETTER NA	gujarati
+પ		p	p	p	p	consonant					U+0AAA: GUJARATI LETTER PA	gujarati
+ફ		f	f	f	ph	consonant					U+0AAB: GUJARATI LETTER PHA	gujarati
+બ		b	b	b	b	consonant					U+0AAC: GUJARATI LETTER BA	gujarati
+ભ		B	bʰ	bʰ	bh	consonant					U+0AAD: GUJARATI LETTER BHA	gujarati
+મ		m	m	m	m	consonant					U+0AAE: GUJARATI LETTER MA	gujarati
+ય		y	y	j	y	consonant					U+0AAF: GUJARATI LETTER YA	gujarati
+ર		r	r	r	r	consonant					U+0AB0: GUJARATI LETTER RA	gujarati
+લ		l	l	l	l	consonant					U+0AB2: GUJARATI LETTER LA	gujarati
+ળ		l	ɭ	ɭ	ḷ	consonant					U+0AB3: GUJARATI LETTER LLA	gujarati
+વ		v	ʋ	w ʋ	v	consonant					U+0AB5: GUJARATI LETTER VA	gujarati
+શ		c	ɕ	ɕ ʃ	ś	consonant					U+0AB6: GUJARATI LETTER SHA	gujarati
+ષ		s	ʃ	ʃ	ṣ	consonant					U+0AB7: GUJARATI LETTER SSA	gujarati
+સ		s¶	s	s	s	consonant					U+0AB8: GUJARATI LETTER SA	gujarati
+હ		h	h	ɦ	h	consonant					U+0AB9: GUJARATI LETTER HA	gujarati
+												
+												
+												
+ઁ					m̐						U+0A81: GUJARATI SIGN CANDRABINDU​	gujarati
+ં		[	˜	ä	ṁ	vowel nasalisation				anusvār	U+0A82: GUJARATI SIGN ANUSVARA​	gujarati
+ઃ		h	h̽	ɨ	ḥ	visarga				visarga	U+0A83: GUJARATI SIGN VISARGA​	gujarati
+												
+												
+ૃ		R	r̥	ru	r̥	dependent vocalic			ઋ		U+0AC3: GUJARATI VOWEL SIGN VOCALIC R​	gujarati
+ૄ		R	r̥̄		r̥̄	dependent vocalic			ૠ		U+0AC4: GUJARATI VOWEL SIGN VOCALIC RR​	gujarati
+ઋ		R	r̥̣	ru	r̥	independent vocalic		ૃ			U+0A8B: GUJARATI LETTER VOCALIC R	gujarati
+ૠ		R	r̥̣̄		r̥̄	independent vocalic		ૄ			U+0AE0: GUJARATI LETTER VOCALIC RR	gujarati
+ૢ		L	l̥		l̥	dependent vocalic			ઌ		U+0AE2: GUJARATI VOWEL SIGN VOCALIC L​	gujarati
+ૣ		L	l̥̄		l̥̄	dependent vocalic			ૡ		U+0AE3: GUJARATI VOWEL SIGN VOCALIC LL​	gujarati
+ઌ		L	l̥̣		l̥	independent vocalic		ૢ			U+0A8C: GUJARATI LETTER VOCALIC L	gujarati
+ૡ		L	l̥̣̄		l̥̄	independent vocalic		ૣ			U+0AE1: GUJARATI LETTER VOCALIC LL	gujarati
+												
+												
+અ		E	ə̣	ə	a	independent vowel					U+0A85: GUJARATI LETTER A	gujarati
+આ		A	ạ̄	ɑ	ā	independent vowel		ા			U+0A86: GUJARATI LETTER AA	gujarati
+ઇ		I	ị	i	i	independent vowel		િ			U+0A87: GUJARATI LETTER I	gujarati
+ઈ		I	ị̄	i	ī	independent vowel		ી			U+0A88: GUJARATI LETTER II	gujarati
+ઉ		U	ụ	u	u	independent vowel		ુ			U+0A89: GUJARATI LETTER U	gujarati
+ઊ		U	ụ̄	u	ū	independent vowel		ૂ			U+0A8A: GUJARATI LETTER UU	gujarati
+એ		E	ẹ	e ɛ	ē	independent vowel		ે			U+0A8F: GUJARATI LETTER E	gujarati
+ઐ		E	ə̣ʲ	əj	ai	independent vowel		ૈ			U+0A90: GUJARATI LETTER AI	gujarati
+ઓ		O	ọ	o ɔ	ō	independent vowel		ો			U+0A93: GUJARATI LETTER O	gujarati
+ઔ		E	ə̣ʷ	əʋ	au	independent vowel		ૌ			U+0A94: GUJARATI LETTER AU	gujarati
+ઍ		A	æ̣	æ	ê	independent vowel		ૅ			U+0A8D: GUJARATI VOWEL CANDRA E	gujarati
+ઑ		O	ɔ̣	ɔ	ô	independent vowel		ૉ			U+0A91: GUJARATI VOWEL CANDRA O	gujarati
+												
+												
+ા		e	ə	ɑ	ā	vowel sign			આ	kāno	U+0ABE: GUJARATI VOWEL SIGN AA​	gujarati
+િ		i	i	i	i	vowel sign			ઇ	hrasva-ajju	U+0ABF: GUJARATI VOWEL SIGN I​	gujarati
+ી		i	ī	i	ī	vowel sign			ઈ	dīrgha-ajju	U+0AC0: GUJARATI VOWEL SIGN II​	gujarati
+ુ		u	u	u	u	vowel sign			ઉ	hrasva-varaṛũ	U+0AC1: GUJARATI VOWEL SIGN U​	gujarati
+ૂ		u	ū	u	ū	vowel sign			ઊ	dīrgha-varaṛũ	U+0AC2: GUJARATI VOWEL SIGN UU​	gujarati
+ે		e	e	e ɛ	ē	vowel sign			એ	ek mātra	U+0AC7: GUJARATI VOWEL SIGN E​	gujarati
+ૈ		e	əʲ	əj	ai	vowel sign			ઐ	be mātra	U+0AC8: GUJARATI VOWEL SIGN AI​	gujarati
+ો		o	o	o ɔ	ō	vowel sign			ઓ	kāno ek mātra	U+0ACB: GUJARATI VOWEL SIGN O​	gujarati
+ૌ		e	əʷ	əʋ	au	vowel sign			ઔ	kāno be mātra	U+0ACC: GUJARATI VOWEL SIGN AU​	gujarati
+ૅ		a	æ	æ	ê	vowel sign			ઍ		U+0AC5: GUJARATI VOWEL SIGN CANDRA E​	gujarati
+ૉ		o	ɔ	ɔ	ô	vowel sign			ઑ		U+0AC9: GUJARATI VOWEL SIGN CANDRA O​	gujarati
+												
+												
+ૐ											U+0AD0: GUJARATI OM	gujarati
+૱											U+0AF1: GUJARATI RUPEE SIGN	gujarati
+												
+												
+્		*	͓			virama					U+0ACD: GUJARATI SIGN VIRAMA​	gujarati
+઼		[	̣			nukta					U+0ABC: GUJARATI SIGN NUKTA​	gujarati
+												
+												
+૦		0	0			digit				mīṇḍu or shunya	U+0AE6: GUJARATI DIGIT ZERO	gujarati
+૧		1	1			digit				ekado or ek	U+0AE7: GUJARATI DIGIT ONE	gujarati
+૨		2	2			digit				bagado or bay	U+0AE8: GUJARATI DIGIT TWO	gujarati
+૩		3	3			digit				tragado or tran	U+0AE9: GUJARATI DIGIT THREE	gujarati
+૪		4	4			digit				chogado or chaar	U+0AEA: GUJARATI DIGIT FOUR	gujarati
+૫		5	5			digit				pāchado or paanch	U+0AEB: GUJARATI DIGIT FIVE	gujarati
+૬		6	6			digit				chagado or chah	U+0AEC: GUJARATI DIGIT SIX	gujarati
+૭		7	7			digit				sātado or sāt	U+0AED: GUJARATI DIGIT SEVEN	gujarati
+૮		8	8			digit				āṭhado or āanth	U+0AEE: GUJARATI DIGIT EIGHT	gujarati
+૯		9	9			digit				navado or nav	U+0AEF: GUJARATI DIGIT NINE	gujarati
+												
+												
+।		.	.	,							U+0964: DEVANAGARI DANDA	devanagari
+॥		[	¶	.							U+0965: DEVANAGARI DOUBLE DANDA	devanagari
+૰											U+0AF0: GUJARATI ABBREVIATION SIGN	gujarati
+												
+												
+												
+ક્ષ		k	k͓ʃ	kʃ		consonant					U+0A95 U+0ACD U+0AB7: GUJARATI LETTER KA, SIGN VIRAMA, LETTER SSA	
+જ્ઞ		j	ʤ͓ɲ	ɡn		consonant					U+0A9C U+0ACD U+0A9E: GUJARATI LETTER JA, SIGN VIRAMA, LETTER NYA	
+
+
+
 `
 
 
@@ -133,6 +136,7 @@ var cols = {
 "nameLoc": 10,
 "nnameLoc": 0,
 "ucsName": 11,
+"block": 12,
 
 "othertranscriptions": [[5, 'ISO 19515']]
 }
