2135ab62c05fac1ffb8d7b41d437fd4985f51acd
r12a <ishida@w3.org>
2020-01-15 20:49:20 +0000

    Adding block column

--- scriptdb/newtailue.js
+++ scriptdb/newtailue.js
@@ -1,151 +1,154 @@
 var spreadsheet = `
-ᦂ		k	k	k	high tone consonant		ᦅ				U+1982: NEW TAI LUE LETTER HIGH KA
-ᦅ		k	ḵ	k	low tone consonant			ᦂ			U+1985: NEW TAI LUE LETTER LOW KA
-ᦦ		k	kʷ	kʷ	high tone consonant		ᦨ				U+19A6: NEW TAI LUE LETTER HIGH KVA
-ᦨ		k	ḵʷ	kʷ	low tone consonant			ᦦ			U+19A8: NEW TAI LUE LETTER LOW KVA
-ᦄ		n	ŋ	ŋa	high tone consonant		ᦇ				U+1984: NEW TAI LUE LETTER HIGH NGA
-ᦇ		n	ŋ̱	ŋa	low tone consonant			ᦄ			U+1987: NEW TAI LUE LETTER LOW NGA
-											
-ᦈ		t	ʦ	ʦ ʨ	high tone consonant		ᦋ				U+1988: NEW TAI LUE LETTER HIGH TSA
-ᦋ		t	ʦ̱	ʦ ʨ	low tone consonant			ᦈ			U+198B: NEW TAI LUE LETTER LOW TSA
-											
-ᦎ		t	t	t	high tone consonant		ᦑ				U+198E: NEW TAI LUE LETTER HIGH TA
-ᦑ		t	ṯ	t	low tone consonant			ᦎ			U+1991: NEW TAI LUE LETTER LOW TA
-ᦏ		t	tʰ	tʰ	high tone consonant		ᦒ				U+198F: NEW TAI LUE LETTER HIGH THA
-ᦒ		t	ṯʰ	tʰ	low tone consonant			ᦏ			U+1992: NEW TAI LUE LETTER LOW THA
-ᦡ		d	d	d	high tone consonant		ᦤ				U+19A1: NEW TAI LUE LETTER HIGH DA
-ᦤ		d	ḏ	d	low tone consonant			ᦡ			U+19A4: NEW TAI LUE LETTER LOW DA
-ᦐ		n	n	n	high tone consonant		ᦓ				U+1990: NEW TAI LUE LETTER HIGH NA
-ᦓ		n	ṉ	n	low tone consonant			ᦐ			U+1993: NEW TAI LUE LETTER LOW NA
-											
-ᦔ		p	p	p	high tone consonant		ᦗ				U+1994: NEW TAI LUE LETTER HIGH PA
-ᦗ		p	p̱	p	low tone consonant			ᦔ			U+1997: NEW TAI LUE LETTER LOW PA
-ᦕ		p	pʰ	pʰ	high tone consonant		ᦘ				U+1995: NEW TAI LUE LETTER HIGH PHA
-ᦘ		p	p̱ʰ	pʰ	low tone consonant			ᦕ			U+1998: NEW TAI LUE LETTER LOW PHA
-ᦢ		b	b	b	high tone consonant		ᦥ				U+19A2: NEW TAI LUE LETTER HIGH BA
-ᦥ		b	ḇ	b	low tone consonant			ᦢ			U+19A5: NEW TAI LUE LETTER LOW BA
-ᦖ		m	m	m	high tone consonant		ᦙ				U+1996: NEW TAI LUE LETTER HIGH MA
-ᦙ		m	m̱	m	low tone consonant			ᦖ			U+1999: NEW TAI LUE LETTER LOW MA
-											
-ᦜ		l	l	l	high tone consonant		ᦟ				U+199C: NEW TAI LUE LETTER HIGH LA
-ᦟ		l	ḻ	l	low tone consonant			ᦜ			U+199F: NEW TAI LUE LETTER LOW LA
-ᦊ		y	y	j	high tone consonant		ᦍ				U+198A: NEW TAI LUE LETTER HIGH YA
-ᦍ		y	y̱	j	low tone consonant			ᦊ			U+198D: NEW TAI LUE LETTER LOW YA
-ᦛ		w	w	w	high tone consonant		ᦞ				U+199B: NEW TAI LUE LETTER HIGH VA
-ᦞ		w	w̱	w	low tone consonant			ᦛ			U+199E: NEW TAI LUE LETTER LOW VA
-											
-ᦠ		h	h	h	high tone consonant		ᦣ				U+19A0: NEW TAI LUE LETTER HIGH HA
-ᦣ		h	ẖ	h	low tone consonant			ᦠ			U+19A3: NEW TAI LUE LETTER LOW HA
-ᦃ		x	x	x	high tone consonant		ᦆ				U+1983: NEW TAI LUE LETTER HIGH XA
-ᦆ		x	x̱	x	low tone consonant			ᦃ			U+1986: NEW TAI LUE LETTER LOW XA
-ᦧ		x	xʷ	xʷ	high tone consonant		ᦩ				U+19A7: NEW TAI LUE LETTER HIGH XVA
-ᦩ		x	x̱ʷ	xʷ	low tone consonant			ᦧ			U+19A9: NEW TAI LUE LETTER LOW XVA
-ᦉ		s	s	s ɕ	high tone consonant		ᦌ				U+1989: NEW TAI LUE LETTER HIGH SA
-ᦌ		s	s̱	s ɕ	low tone consonant			ᦉ			U+198C: NEW TAI LUE LETTER LOW SA
-ᦪ		s	sʷ	sʷ	high tone consonant		ᦫ				U+19AA: NEW TAI LUE LETTER HIGH SUA
-ᦫ		s	s̱ʷ	sʷ	low tone consonant			ᦪ			U+19AB: NEW TAI LUE LETTER LOW SUA
-ᦚ		f	f	f	high tone consonant		ᦝ				U+199A: NEW TAI LUE LETTER HIGH FA
-ᦝ		f	f̱	f	low tone consonant			ᦚ			U+199D: NEW TAI LUE LETTER LOW FA
-											
-ᦀ		a	ạ	ʔ	high tone consonant		ᦁ				U+1980: NEW TAI LUE LETTER HIGH QA
-ᦁ		a	ạ̱	ʔ	low tone consonant			ᦀ			U+1981: NEW TAI LUE LETTER LOW QA
-											
-											
-											
-ᧅ		K	k̽	k	final consonant						U+19C5: NEW TAI LUE LETTER FINAL K
-ᧂ		N	ŋ̽	ŋ	final consonant						U+19C2: NEW TAI LUE LETTER FINAL NG
-ᧆ		T	t̽	t	final consonant						U+19C6: NEW TAI LUE LETTER FINAL D
-ᧃ		N	n̽	n	final consonant						U+19C3: NEW TAI LUE LETTER FINAL N
-ᧇ		P	p̽	p	final consonant						U+19C7: NEW TAI LUE LETTER FINAL B
-ᧄ		M	m̽	m	final consonant						U+19C4: NEW TAI LUE LETTER FINAL M
-ᧁ		W	w̽	u̯	final consonant						U+19C1: NEW TAI LUE LETTER FINAL V
-											
-											
-											
-᧞		L	lˀ	lɛʔ	ligature						U+19DE: NEW TAI LUE SIGN LAE
-᧟		L	lʷ	lɛu	ligature						U+19DF: NEW TAI LUE SIGN LAEV
-											
-											
-											
-ᦺ		a	a͕ʲ	ai̯	dependent vowel						U+19BA: NEW TAI LUE VOWEL SIGN AY
-ᦵ		e	e͕	eː	dependent vowel						U+19B5: NEW TAI LUE VOWEL SIGN E
-ᦶ		e	ɛ͕	ɛ	dependent vowel						U+19B6: NEW TAI LUE VOWEL SIGN AE
-ᦷ		o	o͕	oː	dependent vowel						U+19B7: NEW TAI LUE VOWEL SIGN O
-											
-											
-ᦱ		a	ā	aː	dependent vowel						U+19B1: NEW TAI LUE VOWEL SIGN AA
-ᦲ		i	i	i	dependent vowel						U+19B2: NEW TAI LUE VOWEL SIGN II
-ᦹ		u	ɯ	ɯ	dependent vowel						U+19B9: NEW TAI LUE VOWEL SIGN UE
-ᦳ		u	u	uʔ	dependent vowel						U+19B3: NEW TAI LUE VOWEL SIGN U
-ᦴ		u	ū	u	dependent vowel						U+19B4: NEW TAI LUE VOWEL SIGN UU
-ᦸ		o	ɔ	ɔ	dependent vowel						U+19B8: NEW TAI LUE VOWEL SIGN OA
-ᦰ		}	ʔ	ʔ	vowel shortener						U+19B0: NEW TAI LUE VOWEL SIGN VOWEL SHORTENER
-											
-ᦻ		a	aʲ	aːi̯	dipthong						U+19BB: NEW TAI LUE VOWEL SIGN AAY
-ᦿ		u	ɯʲ	ɯi̯	dipthong						U+19BF: NEW TAI LUE VOWEL SIGN UEY
-ᦼ		u	uʲ	ui̯	dipthong						U+19BC: NEW TAI LUE VOWEL SIGN UY
-ᦽ		o	oʲ	oi̯	dipthong						U+19BD: NEW TAI LUE VOWEL SIGN OY
-ᦾ		o	ɔʲ	ɔi̯	dipthong						U+19BE: NEW TAI LUE VOWEL SIGN OAY
-ᧀ		e	əʲ	əi̯	dipthong						U+19C0: NEW TAI LUE VOWEL SIGN IY
-											
-											
-											
-ᧈ		^	¹		tone mark						U+19C8: NEW TAI LUE TONE MARK-1
-ᧉ		^	²		tone mark						U+19C9: NEW TAI LUE TONE MARK-2
-											
-											
-											
-											
-											
-											
-											
-											
-᧐		0	0		digit					nɯŋ	U+19D0: NEW TAI LUE DIGIT ZERO
-᧑		1	1		digit					sɔ́ŋ	U+19D1: NEW TAI LUE DIGIT ONE
-᧒		2	2		digit					sám	U+19D2: NEW TAI LUE DIGIT TWO
-᧓		3	3		digit					si᷄ː	U+19D3: NEW TAI LUE DIGIT THREE
-᧔		4	4		digit					ha᷅ː	U+19D4: NEW TAI LUE DIGIT FOUR
-᧕		5	5		digit					hók	U+19D5: NEW TAI LUE DIGIT FIVE
-᧖		6	6		digit					ʨét	U+19D6: NEW TAI LUE DIGIT SIX
-᧗		7	7		digit					pɛ᷄t	U+19D7: NEW TAI LUE DIGIT SEVEN
-᧘		8	8		digit					ka᷅w	U+19D8: NEW TAI LUE DIGIT EIGHT
-᧙		9	9		digit					síp	U+19D9: NEW TAI LUE DIGIT NINE
-											
-᧚		1	1̣		alternative digit						U+19DA: NEW TAI LUE THAM DIGIT ONE
-											
-											
-											
-											
-											
-											
-〈		(	<		fullwidth bracket						U+3008: LEFT ANGLE BRACKET
-〉		)	>		fullwidth bracket						U+3009: RIGHT ANGLE BRACKET
-《		(	{		fullwidth bracket						U+300A: LEFT DOUBLE ANGLE BRACKET
-》		)	}		fullwidth bracket						U+300B: RIGHT DOUBLE ANGLE BRACKET
-（		(	(		fullwidth bracket						U+FF08: FULLWIDTH LEFT PARENTHESIS
-）		)	)		fullwidth bracket						U+FF09: FULLWIDTH RIGHT PARENTHESIS
-！		!	!		fullwidth bracket						U+FF01: FULLWIDTH EXCLAMATION MARK
-？		?	?								U+FF1F: FULLWIDTH QUESTION MARK
-：		:	:								U+FF1A: FULLWIDTH COLON
-；		;	;								U+FF1B: FULLWIDTH SEMICOLON
-。		.	.								U+3002: IDEOGRAPHIC FULL STOP
-、		,	,								U+3001: IDEOGRAPHIC COMMA
-，		,	,								U+FF0C: FULLWIDTH COMMA
-．		.	.								U+FF0E: FULLWIDTH FULL STOP
-											
-											
-											
-											
-											
-											
--ᦲᦰ	s	i	-iʔ	iʔ							U+19B2 U+19B0: NEW TAI LUE VOWEL SIGN II, VOWEL SIGN VOWEL SHORTENER
--ᦹᦰ	s	e	-ɯʔ	eʔ							U+19B9 U+19B0: NEW TAI LUE VOWEL SIGN UE, VOWEL SIGN VOWEL SHORTENER
--ᦸᦰ	s	e	-ɔʔ	ɛʔ							U+19B8 U+19B0: NEW TAI LUE VOWEL SIGN OA, VOWEL SIGN VOWEL SHORTENER
-ᦵ-ᦰ	s	o	e͕-ʔ	oʔ							U+19B5 U+19B0: NEW TAI LUE VOWEL SIGN E, VOWEL SIGN VOWEL SHORTENER
-ᦵ-ᦲ	s	o	e͕-i	ɔʔ							U+19B5 U+19B2: NEW TAI LUE VOWEL SIGN E, VOWEL SIGN II
-ᦶ-ᦰ	s	e	ɛ͕-ʔ	əʔ							U+19B6 U+19B0: NEW TAI LUE VOWEL SIGN AE, VOWEL SIGN VOWEL SHORTENER
-ᦷ-ᦰ	s	e	o͕-ʔ	əː							U+19B7 U+19B0: NEW TAI LUE VOWEL SIGN O, VOWEL SIGN VOWEL SHORTENER
-ᦵ-ᦲᦰ	s	u	e͕-iʔ	ɯ							U+19B5 U+19B2 U+19B0: NEW TAI LUE VOWEL SIGN E, VOWEL SIGN II, VOWEL SIGN VOWEL SHORTENER
+ᦂ		k	k	k	high tone consonant		ᦅ				U+1982: NEW TAI LUE LETTER HIGH KA	newtailue
+ᦅ		k	ḵ	k	low tone consonant			ᦂ			U+1985: NEW TAI LUE LETTER LOW KA	newtailue
+ᦦ		k	kʷ	kʷ	high tone consonant		ᦨ				U+19A6: NEW TAI LUE LETTER HIGH KVA	newtailue
+ᦨ		k	ḵʷ	kʷ	low tone consonant			ᦦ			U+19A8: NEW TAI LUE LETTER LOW KVA	newtailue
+ᦄ		n	ŋ	ŋa	high tone consonant		ᦇ				U+1984: NEW TAI LUE LETTER HIGH NGA	newtailue
+ᦇ		n	ŋ̱	ŋa	low tone consonant			ᦄ			U+1987: NEW TAI LUE LETTER LOW NGA	newtailue
+												
+ᦈ		t	ʦ	ʦ ʨ	high tone consonant		ᦋ				U+1988: NEW TAI LUE LETTER HIGH TSA	newtailue
+ᦋ		t	ʦ̱	ʦ ʨ	low tone consonant			ᦈ			U+198B: NEW TAI LUE LETTER LOW TSA	newtailue
+												
+ᦎ		t	t	t	high tone consonant		ᦑ				U+198E: NEW TAI LUE LETTER HIGH TA	newtailue
+ᦑ		t	ṯ	t	low tone consonant			ᦎ			U+1991: NEW TAI LUE LETTER LOW TA	newtailue
+ᦏ		t	tʰ	tʰ	high tone consonant		ᦒ				U+198F: NEW TAI LUE LETTER HIGH THA	newtailue
+ᦒ		t	ṯʰ	tʰ	low tone consonant			ᦏ			U+1992: NEW TAI LUE LETTER LOW THA	newtailue
+ᦡ		d	d	d	high tone consonant		ᦤ				U+19A1: NEW TAI LUE LETTER HIGH DA	newtailue
+ᦤ		d	ḏ	d	low tone consonant			ᦡ			U+19A4: NEW TAI LUE LETTER LOW DA	newtailue
+ᦐ		n	n	n	high tone consonant		ᦓ				U+1990: NEW TAI LUE LETTER HIGH NA	newtailue
+ᦓ		n	ṉ	n	low tone consonant			ᦐ			U+1993: NEW TAI LUE LETTER LOW NA	newtailue
+												
+ᦔ		p	p	p	high tone consonant		ᦗ				U+1994: NEW TAI LUE LETTER HIGH PA	newtailue
+ᦗ		p	p̱	p	low tone consonant			ᦔ			U+1997: NEW TAI LUE LETTER LOW PA	newtailue
+ᦕ		p	pʰ	pʰ	high tone consonant		ᦘ				U+1995: NEW TAI LUE LETTER HIGH PHA	newtailue
+ᦘ		p	p̱ʰ	pʰ	low tone consonant			ᦕ			U+1998: NEW TAI LUE LETTER LOW PHA	newtailue
+ᦢ		b	b	b	high tone consonant		ᦥ				U+19A2: NEW TAI LUE LETTER HIGH BA	newtailue
+ᦥ		b	ḇ	b	low tone consonant			ᦢ			U+19A5: NEW TAI LUE LETTER LOW BA	newtailue
+ᦖ		m	m	m	high tone consonant		ᦙ				U+1996: NEW TAI LUE LETTER HIGH MA	newtailue
+ᦙ		m	m̱	m	low tone consonant			ᦖ			U+1999: NEW TAI LUE LETTER LOW MA	newtailue
+												
+ᦜ		l	l	l	high tone consonant		ᦟ				U+199C: NEW TAI LUE LETTER HIGH LA	newtailue
+ᦟ		l	ḻ	l	low tone consonant			ᦜ			U+199F: NEW TAI LUE LETTER LOW LA	newtailue
+ᦊ		y	y	j	high tone consonant		ᦍ				U+198A: NEW TAI LUE LETTER HIGH YA	newtailue
+ᦍ		y	y̱	j	low tone consonant			ᦊ			U+198D: NEW TAI LUE LETTER LOW YA	newtailue
+ᦛ		w	w	w	high tone consonant		ᦞ				U+199B: NEW TAI LUE LETTER HIGH VA	newtailue
+ᦞ		w	w̱	w	low tone consonant			ᦛ			U+199E: NEW TAI LUE LETTER LOW VA	newtailue
+												
+ᦠ		h	h	h	high tone consonant		ᦣ				U+19A0: NEW TAI LUE LETTER HIGH HA	newtailue
+ᦣ		h	ẖ	h	low tone consonant			ᦠ			U+19A3: NEW TAI LUE LETTER LOW HA	newtailue
+ᦃ		x	x	x	high tone consonant		ᦆ				U+1983: NEW TAI LUE LETTER HIGH XA	newtailue
+ᦆ		x	x̱	x	low tone consonant			ᦃ			U+1986: NEW TAI LUE LETTER LOW XA	newtailue
+ᦧ		x	xʷ	xʷ	high tone consonant		ᦩ				U+19A7: NEW TAI LUE LETTER HIGH XVA	newtailue
+ᦩ		x	x̱ʷ	xʷ	low tone consonant			ᦧ			U+19A9: NEW TAI LUE LETTER LOW XVA	newtailue
+ᦉ		s	s	s ɕ	high tone consonant		ᦌ				U+1989: NEW TAI LUE LETTER HIGH SA	newtailue
+ᦌ		s	s̱	s ɕ	low tone consonant			ᦉ			U+198C: NEW TAI LUE LETTER LOW SA	newtailue
+ᦪ		s	sʷ	sʷ	high tone consonant		ᦫ				U+19AA: NEW TAI LUE LETTER HIGH SUA	newtailue
+ᦫ		s	s̱ʷ	sʷ	low tone consonant			ᦪ			U+19AB: NEW TAI LUE LETTER LOW SUA	newtailue
+ᦚ		f	f	f	high tone consonant		ᦝ				U+199A: NEW TAI LUE LETTER HIGH FA	newtailue
+ᦝ		f	f̱	f	low tone consonant			ᦚ			U+199D: NEW TAI LUE LETTER LOW FA	newtailue
+												
+ᦀ		a	ạ	ʔ	high tone consonant		ᦁ				U+1980: NEW TAI LUE LETTER HIGH QA	newtailue
+ᦁ		a	ạ̱	ʔ	low tone consonant			ᦀ			U+1981: NEW TAI LUE LETTER LOW QA	newtailue
+												
+												
+												
+ᧅ		K	k̽	k	final consonant						U+19C5: NEW TAI LUE LETTER FINAL K	newtailue
+ᧂ		N	ŋ̽	ŋ	final consonant						U+19C2: NEW TAI LUE LETTER FINAL NG	newtailue
+ᧆ		T	t̽	t	final consonant						U+19C6: NEW TAI LUE LETTER FINAL D	newtailue
+ᧃ		N	n̽	n	final consonant						U+19C3: NEW TAI LUE LETTER FINAL N	newtailue
+ᧇ		P	p̽	p	final consonant						U+19C7: NEW TAI LUE LETTER FINAL B	newtailue
+ᧄ		M	m̽	m	final consonant						U+19C4: NEW TAI LUE LETTER FINAL M	newtailue
+ᧁ		W	w̽	u̯	final consonant						U+19C1: NEW TAI LUE LETTER FINAL V	newtailue
+												
+												
+												
+᧞		L	lˀ	lɛʔ	ligature						U+19DE: NEW TAI LUE SIGN LAE	newtailue
+᧟		L	lʷ	lɛu	ligature						U+19DF: NEW TAI LUE SIGN LAEV	newtailue
+												
+												
+												
+ᦺ		a	a͕ʲ	ai̯	dependent vowel						U+19BA: NEW TAI LUE VOWEL SIGN AY	newtailue
+ᦵ		e	e͕	eː	dependent vowel						U+19B5: NEW TAI LUE VOWEL SIGN E	newtailue
+ᦶ		e	ɛ͕	ɛ	dependent vowel						U+19B6: NEW TAI LUE VOWEL SIGN AE	newtailue
+ᦷ		o	o͕	oː	dependent vowel						U+19B7: NEW TAI LUE VOWEL SIGN O	newtailue
+												
+												
+ᦱ		a	ā	aː	dependent vowel						U+19B1: NEW TAI LUE VOWEL SIGN AA	newtailue
+ᦲ		i	i	i	dependent vowel						U+19B2: NEW TAI LUE VOWEL SIGN II	newtailue
+ᦹ		u	ɯ	ɯ	dependent vowel						U+19B9: NEW TAI LUE VOWEL SIGN UE	newtailue
+ᦳ		u	u	u	dependent vowel						U+19B3: NEW TAI LUE VOWEL SIGN U	newtailue
+ᦴ		u	ū	uː	dependent vowel						U+19B4: NEW TAI LUE VOWEL SIGN UU	newtailue
+ᦸ		o	ɔ	ɔ	dependent vowel						U+19B8: NEW TAI LUE VOWEL SIGN OA	newtailue
+ᦰ		}	ʔ	ʔ	vowel shortener						U+19B0: NEW TAI LUE VOWEL SIGN VOWEL SHORTENER	newtailue
+												
+ᦻ		a	aʲ	aːi̯	dipthong						U+19BB: NEW TAI LUE VOWEL SIGN AAY	newtailue
+ᦿ		u	ɯʲ	ɯi̯	dipthong						U+19BF: NEW TAI LUE VOWEL SIGN UEY	newtailue
+ᦼ		u	uʲ	ui̯	dipthong						U+19BC: NEW TAI LUE VOWEL SIGN UY	newtailue
+ᦽ		o	oʲ	oi̯	dipthong						U+19BD: NEW TAI LUE VOWEL SIGN OY	newtailue
+ᦾ		o	ɔʲ	ɔi̯	dipthong						U+19BE: NEW TAI LUE VOWEL SIGN OAY	newtailue
+ᧀ		e	əʲ	əi̯	dipthong						U+19C0: NEW TAI LUE VOWEL SIGN IY	newtailue
+												
+												
+												
+ᧈ		^	¹		tone mark						U+19C8: NEW TAI LUE TONE MARK-1	newtailue
+ᧉ		^	²		tone mark						U+19C9: NEW TAI LUE TONE MARK-2	newtailue
+												
+												
+												
+												
+												
+												
+												
+												
+᧐		0	0		digit					nɯŋ	U+19D0: NEW TAI LUE DIGIT ZERO	newtailue
+᧑		1	1		digit					sɔ́ŋ	U+19D1: NEW TAI LUE DIGIT ONE	newtailue
+᧒		2	2		digit					sám	U+19D2: NEW TAI LUE DIGIT TWO	newtailue
+᧓		3	3		digit					si᷄ː	U+19D3: NEW TAI LUE DIGIT THREE	newtailue
+᧔		4	4		digit					ha᷅ː	U+19D4: NEW TAI LUE DIGIT FOUR	newtailue
+᧕		5	5		digit					hók	U+19D5: NEW TAI LUE DIGIT FIVE	newtailue
+᧖		6	6		digit					ʨét	U+19D6: NEW TAI LUE DIGIT SIX	newtailue
+᧗		7	7		digit					pɛ᷄t	U+19D7: NEW TAI LUE DIGIT SEVEN	newtailue
+᧘		8	8		digit					ka᷅w	U+19D8: NEW TAI LUE DIGIT EIGHT	newtailue
+᧙		9	9		digit					síp	U+19D9: NEW TAI LUE DIGIT NINE	newtailue
+												
+᧚		1	1̣		alternative digit						U+19DA: NEW TAI LUE THAM DIGIT ONE	newtailue
+												
+												
+												
+												
+												
+												
+〈		(	<		fullwidth bracket						U+3008: LEFT ANGLE BRACKET	
+〉		)	>		fullwidth bracket						U+3009: RIGHT ANGLE BRACKET	
+《		(	{		fullwidth bracket						U+300A: LEFT DOUBLE ANGLE BRACKET	
+》		)	}		fullwidth bracket						U+300B: RIGHT DOUBLE ANGLE BRACKET	
+（		(	(		fullwidth bracket						U+FF08: FULLWIDTH LEFT PARENTHESIS	
+）		)	)		fullwidth bracket						U+FF09: FULLWIDTH RIGHT PARENTHESIS	
+！		!	!		fullwidth bracket						U+FF01: FULLWIDTH EXCLAMATION MARK	
+？		?	?								U+FF1F: FULLWIDTH QUESTION MARK	
+：		:	:								U+FF1A: FULLWIDTH COLON	
+；		;	;								U+FF1B: FULLWIDTH SEMICOLON	
+。		.	.								U+3002: IDEOGRAPHIC FULL STOP	
+、		,	,								U+3001: IDEOGRAPHIC COMMA	
+，		,	,								U+FF0C: FULLWIDTH COMMA	
+．		.	.								U+FF0E: FULLWIDTH FULL STOP	
+												
+												
+												
+												
+												
+												
+-ᦲᦰ	s	i	-iʔ	iʔ							U+19B2 U+19B0: NEW TAI LUE VOWEL SIGN II, VOWEL SIGN VOWEL SHORTENER	
+-ᦹᦰ	s	e	-ɯʔ	eʔ							U+19B9 U+19B0: NEW TAI LUE VOWEL SIGN UE, VOWEL SIGN VOWEL SHORTENER	
+-ᦸᦰ	s	e	-ɔʔ	ɛʔ							U+19B8 U+19B0: NEW TAI LUE VOWEL SIGN OA, VOWEL SIGN VOWEL SHORTENER	
+ᦵ-ᦰ	s	o	e͕-ʔ	oʔ							U+19B5 U+19B0: NEW TAI LUE VOWEL SIGN E, VOWEL SIGN VOWEL SHORTENER	
+ᦵ-ᦲ	s	o	e͕-i	ɔʔ							U+19B5 U+19B2: NEW TAI LUE VOWEL SIGN E, VOWEL SIGN II	
+ᦶ-ᦰ	s	e	ɛ͕-ʔ	əʔ							U+19B6 U+19B0: NEW TAI LUE VOWEL SIGN AE, VOWEL SIGN VOWEL SHORTENER	
+ᦷ-ᦰ	s	e	o͕-ʔ	əː							U+19B7 U+19B0: NEW TAI LUE VOWEL SIGN O, VOWEL SIGN VOWEL SHORTENER	
+ᦵ-ᦲᦰ	s	u	e͕-iʔ	ɯ							U+19B5 U+19B2 U+19B0: NEW TAI LUE VOWEL SIGN E, VOWEL SIGN II, VOWEL SIGN VOWEL SHORTENER	
+												
+ᦵ-ᦲ	s	o	e͕-i	ɔʔ								
+
 
 
 `
@@ -172,6 +175,7 @@ var cols = {
 "nameLoc": 9,
 "nnameLoc": 10,
 "ucsName": 11,
+"block": 12,
 
 //"othertranscriptions": [[6, 'ISO 9985']]
 }
