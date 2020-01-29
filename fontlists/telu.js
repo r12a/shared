f37c07b9a08dcd309fe95635431cc323cc3a089a
r12a <ishida@w3.org>
2020-01-29 09:04:43 +0000

    + telugu stuff

---
+++ fontlists/telu.js
@@ -0,0 +1,36 @@
+ var fontSelection = `<optgroup label="Webfont">
+        <option value="Noto Sans Telugu WF">Noto Sans Telugu WF</option>
+        </optgroup>
+        
+        <optgroup label="Mac fonts">
+        <option value="Kohinoor Telugu">Kohinoor Telugu</option>
+        <option value="Telugu MN">Telugu MN</option>
+        <option value="Telugu Sangam MN">Telugu Sangam MN</option>
+        </optgroup>
+        
+        <optgroup label="Windows fonts">
+        <option value="Gautami">Gautami</option>
+        <option value="Vani">Vani</option>
+        <option value="Nirmala UI">Nirmala UI</option>
+        </optgroup>
+        
+        <optgroup label="Google fonts">
+        <option value="Noto Sans Telugu">Noto Sans Telugu</option>
+        <option value="Noto Serif Telugu">Noto Serif Telugu</option>
+        </optgroup>
+
+        <!--optgroup label="Other fonts ">
+        <option value="Droid Sans Tamil">Droid Sans Tamil</option>
+        <option value="Karla Tamil Inclined">Karla Tamil Inclined</option>
+        <option value="Karla Tamil Upright">Karla Tamil Upright</option>
+        <option value="Lohit Tamil">Lohit Tamil</option>
+        <option value="Maduram">Maduram</option>
+        </optgroup-->
+        
+      <optgroup label="General fonts">
+        <option value="Arial Unicode MS">Arial Unicode MS</option>
+        <option value="Code2000">Code2000</option>
+        <option value="Microsoft Sans Serif">Microsoft Sans Serif</option>
+        <option value="sans-serif">sans-serif</option>
+        <option value="serif">serif</option>
+        </optgroup>`
---
+++ scriptdb/telu.js
@@ -0,0 +1,178 @@
+var spreadsheet = `
+ప		p¶	p	p		p	consonant				్ప			U+0C2A: TELUGU LETTER PA	telugu
+బ		b	b	b		b	consonant				్బ			U+0C2C: TELUGU LETTER BA	telugu
+ఫ		P	pʰ	pʰ		ph	consonant				్ఫ			U+0C2B: TELUGU LETTER PHA	telugu
+భ		B	bʰ	bʰ		bh	consonant				్భ			U+0C2D: TELUGU LETTER BHA	telugu
+త		t	t	t		t	consonant				్త			U+0C24: TELUGU LETTER TA	telugu
+ద		d	d	d		d	consonant				్ద			U+0C26: TELUGU LETTER DA	telugu
+థ		T	tʰ	tʰ		th	consonant				్థ			U+0C25: TELUGU LETTER THA	telugu
+ధ		D	dʰ	dʰ		dh	consonant				్ధ			U+0C27: TELUGU LETTER DHA	telugu
+ట		t	ʈ	ʈ		ṭ	consonant				్ట			U+0C1F: TELUGU LETTER TTA	telugu
+డ		d	ɖ	ɖ		ḍ	consonant				్డ			U+0C21: TELUGU LETTER DDA	telugu
+ఠ		T	ʈʰ	ʈʰ		ṭh	consonant				్ఠ			U+0C20: TELUGU LETTER TTHA	telugu
+ఢ		D	ɖʰ	ɖʰ		ḍh	consonant				్ఢ			U+0C22: TELUGU LETTER DDHA	telugu
+క		k	k	k		k	consonant				్క			U+0C15: TELUGU LETTER KA	telugu
+గ		ɡ	g	ɡ		g	consonant				్గ			U+0C17: TELUGU LETTER GA	telugu
+ఖ		K	kʰ	kʰ		kh	consonant				్ఖ			U+0C16: TELUGU LETTER KHA	telugu
+ఘ		G	gʰ	ɡʰ		gh	consonant				్ఘ			U+0C18: TELUGU LETTER GHA	telugu
+చ		c	c	t͡ʃ		c	consonant				్చ			U+0C1A: TELUGU LETTER CA	telugu
+జ		j	ɟ	d͡ʒ		j	consonant				్జ			U+0C1C: TELUGU LETTER JA	telugu
+ఛ		C	cʰ	t͡ʃʰ		ch	consonant				్ఛ			U+0C1B: TELUGU LETTER CHA	telugu
+ఝ		J	ɟʰ	d͡ʒʰ		jh	consonant				్ఝ			U+0C1D: TELUGU LETTER JHA	telugu
+స		s	s	s		s	consonant				్స			U+0C38: TELUGU LETTER SA	telugu
+శ		s	ʃ	ʃ		ś	consonant				్శ			U+0C36: TELUGU LETTER SHA	telugu
+ష		s	ʂ	ʂ		ṣ	consonant				్ష			U+0C37: TELUGU LETTER SSA	telugu
+హ		h	h	h		h	consonant				్హ			U+0C39: TELUGU LETTER HA	telugu
+మ		m	m	m		m	consonant				్మ			U+0C2E: TELUGU LETTER MA	telugu
+న		n	n	n		n	consonant				్న			U+0C28: TELUGU LETTER NA	telugu
+ఞ		n	ɲ	ɲ		ñ	consonant				్ఞ			U+0C1E: TELUGU LETTER NYA	telugu
+ణ		n	ɳ	ɳ		ṇ	consonant				్ణ			U+0C23: TELUGU LETTER NNA	telugu
+ఙ		n	ŋ	ŋ		ṅ	consonant				్ఙ			U+0C19: TELUGU LETTER NGA	telugu
+వ		v	ʋ	ʋ		v	consonant				్వ			U+0C35: TELUGU LETTER VA	telugu
+ర		r	r	r		r	consonant				్ర			U+0C30: TELUGU LETTER RA	telugu
+ఱ		r	ɽ	ɽ		ṛ	consonant				్ఱ			U+0C31: TELUGU LETTER RRA	telugu
+ల		l	l	l		l	consonant				్ల			U+0C32: TELUGU LETTER LA	telugu
+ళ		l	ɭ	ɭ		ḷ	consonant				్ళ			U+0C33: TELUGU LETTER LLA	telugu
+య		y	y	j		y	consonant				్య			U+0C2F: TELUGU LETTER YA	telugu
+															
+															
+ఽ							avagraha	archaic			్ఽ			U+0C3D: TELUGU SIGN AVAGRAHA	telugu
+ౘ							consonant	archaic			్ౘ			U+0C58: TELUGU LETTER TSA	telugu
+ౙ							consonant	archaic			్ౙ			U+0C59: TELUGU LETTER DZA	telugu
+ౚ							consonant	archaic			్ౚ			U+0C5A: TELUGU LETTER RRRA	telugu
+															
+															
+															
+ి		i	i	i		ē	vowel sign			ఇ				U+0C3F: TELUGU VOWEL SIGN I​	telugu
+ీ		i	ī	iː		ī	vowel sign			ఈ				U+0C40: TELUGU VOWEL SIGN II​	telugu
+ు		u	u	u		u	vowel sign			ఉ				U+0C41: TELUGU VOWEL SIGN U​	telugu
+ూ		u	ū	uː		ū	vowel sign			ఊ				U+0C42: TELUGU VOWEL SIGN UU​	telugu
+ె		e	e	e		e	vowel sign			ఎ				U+0C46: TELUGU VOWEL SIGN E​	telugu
+ే		e	ē	eː		ē	vowel sign			ఏ				U+0C47: TELUGU VOWEL SIGN EE​	telugu
+ొ		o	o	o		o	vowel sign			ఒ				U+0C4A: TELUGU VOWEL SIGN O​	telugu
+ో		o	ō	oː		ō	vowel sign			ఓ				U+0C4B: TELUGU VOWEL SIGN OO​	telugu
+ా		a	ā	aː		a	vowel sign			ఆ				U+0C3E: TELUGU VOWEL SIGN AA​	telugu
+															
+ై	d	a	aʲ	aj		ai	diphthong vowel sign			ఐ				U+0C48: TELUGU VOWEL SIGN AI​	telugu
+ౌ		a	aʷ	aw		au	diphthong vowel sign			ఔ				U+0C4C: TELUGU VOWEL SIGN AU​	telugu
+															
+\u{C46}\u{C56}	s			aj			decomposed ai	NFD only						U+0C46 U+0C56: TELUGU VOWEL SIGN E, AI LENGTH MARK​	
+															
+:		:	:												
+ౕ	x	:	ː				length mark	not used						U+0C55: TELUGU LENGTH MARK​	telugu
+ౖ		:	ˉ				length mark	use in NFD only						U+0C56: TELUGU AI LENGTH MARK​	telugu
+															
+															
+															
+															
+															
+ఇ		I	ị	i		i	independent vowel		ి					U+0C07: TELUGU LETTER I	telugu
+ఈ		I	ị̄	iː		ī	independent vowel		ీ					U+0C08: TELUGU LETTER II	telugu
+ఉ		U	ụ	u		u	independent vowel		ు					U+0C09: TELUGU LETTER U	telugu
+ఊ		U	ụ̄	uː		ū	independent vowel		ూ					U+0C0A: TELUGU LETTER UU	telugu
+ఎ		E	ẹ	eː		e	independent vowel		ె					U+0C0E: TELUGU LETTER E	telugu
+ఏ		E	ẹ̄	eː		ē	independent vowel		ే					U+0C0F: TELUGU LETTER EE	telugu
+ఒ		O	ọ	o		o	independent vowel		ొ					U+0C12: TELUGU LETTER O	telugu
+ఓ		O	ọ̄	oː		ō	independent vowel		ో					U+0C13: TELUGU LETTER OO	telugu
+అ		A	ạ	a		a	independent vowel							U+0C05: TELUGU LETTER A	telugu
+ఆ		A	ạ̄	aː		ā	independent vowel		ా					U+0C06: TELUGU LETTER AA	telugu
+															
+ఐ		A	ạʲ	aj		ai	independent diphthong		ై					U+0C10: TELUGU LETTER AI	telugu
+ఔ		A	ạʷ	aw		aw	independent diphthong		ౌ					U+0C14: TELUGU LETTER AU	telugu
+															
+															
+															
+ృ			r̥	ru		r̥	vocalic vowel sign			ఋ				U+0C43: TELUGU VOWEL SIGN VOCALIC R​	telugu
+ౢ			r̥̄	ruː		r̥̄	vocalic vowel sign	for Sanskrit		ౠ				U+0C62: TELUGU VOWEL SIGN VOCALIC L​	telugu
+ౄ			l̥	alu		l̥	vocalic vowel sign	for Sanskrit		ఌ				U+0C44: TELUGU VOWEL SIGN VOCALIC RR​	telugu
+ౣ			l̥̄	alu		l̥̄	vocalic vowel sign	for Sanskrit		ౡ				U+0C63: TELUGU VOWEL SIGN VOCALIC LL​	telugu
+															
+ఋ			r̥̣	ru		r̥	independent vocalic		ృ					U+0C0B: TELUGU LETTER VOCALIC R	telugu
+ౠ			r̥̣̄	ruː		r̥̄	independent vocalic	for Sanskrit	ౢ					U+0C60: TELUGU LETTER VOCALIC RR	telugu
+ఌ			l̥̣	alu		l̥	independent vocalic	for Sanskrit	ౄ					U+0C0C: TELUGU LETTER VOCALIC L	telugu
+ౡ			l̥̣̄	alu:		l̥̄	independent vocalic	for Sanskrit	ౣ					U+0C61: TELUGU LETTER VOCALIC LL	telugu
+															
+															
+															
+౦		±	0̣		0	0	digit	infrequent						U+0C66: TELUGU DIGIT ZERO	telugu
+౧		±	1̣		1	1	digit	infrequent						U+0C67: TELUGU DIGIT ONE	telugu
+౨		±	2̣		2	2	digit	infrequent						U+0C68: TELUGU DIGIT TWO	telugu
+౩		±	3̣		3	3	digit	infrequent						U+0C69: TELUGU DIGIT THREE	telugu
+౪		±	4̣		4	4	digit	infrequent						U+0C6A: TELUGU DIGIT FOUR	telugu
+౫		±	5̣		5	5	digit	infrequent						U+0C6B: TELUGU DIGIT FIVE	telugu
+౬		±	6̣		6	6	digit	infrequent						U+0C6C: TELUGU DIGIT SIX	telugu
+౭		±	7̣		7	7	digit	infrequent						U+0C6D: TELUGU DIGIT SEVEN	telugu
+౮		±	8̣		8	8	digit	infrequent						U+0C6E: TELUGU DIGIT EIGHT	telugu
+౯		±	9̣		9	9	digit	infrequent						U+0C6F: TELUGU DIGIT NINE	telugu
+															
+౸		$	0⁴				fraction	archaic						U+0C78: TELUGU FRACTION DIGIT ZERO FOR ODD POWERS OF FOUR	telugu
+౹		$	1⁴				fraction	archaic						U+0C79: TELUGU FRACTION DIGIT ONE FOR ODD POWERS OF FOUR	telugu
+౺		$	2⁴				fraction	archaic						U+0C7A: TELUGU FRACTION DIGIT TWO FOR ODD POWERS OF FOUR	telugu
+౻		$	3⁴				fraction	archaic						U+0C7B: TELUGU FRACTION DIGIT THREE FOR ODD POWERS OF FOUR	telugu
+౼		$	0⁶				fraction	archaic						U+0C7C: TELUGU FRACTION DIGIT ONE FOR EVEN POWERS OF FOUR	telugu
+౽		$	1⁶				fraction	archaic						U+0C7D: TELUGU FRACTION DIGIT TWO FOR EVEN POWERS OF FOUR	telugu
+౾		$	2⁶				fraction	archaic						U+0C7E: TELUGU FRACTION DIGIT THREE FOR EVEN POWERS OF FOUR	telugu
+౿		$	3⁶				fraction	archaic						U+0C7F: TELUGU SIGN TUUMU	telugu
+															
+															
+₹		#	¤				currency symbol							U+20B9 INDIAN RUPEE SIGN	currencysymbols
+౷														U+0C77: TELUGU SIGN SIDDHAM	telugu
+															
+															
+ం		m	m̽	m n ŋ		ṁ	anusvara							U+0C02: TELUGU SIGN ANUSVARA​	telugu
+ః		h	h̽	ha		ḥ	visarga							U+0C03: TELUGU SIGN VISARGA​	telugu
+్		*	͓		*		virama							U+0C4D: TELUGU SIGN VIRAMA​	telugu
+															
+ఀ		[	˜				candrabindu	Sanskrit only						U+0C00: TELUGU SIGN COMBINING CANDRABINDU ABOVE​	telugu
+ఁ		[	˓				candrabindu marker	annotation						U+0C01: TELUGU SIGN CANDRABINDU​	telugu
+ఄ														U+0C04: TELUGU SIGN COMBINING ANUSVARA ABOVE​	telugu
+															
+															
+।		|	|		.		punctuation	infrequent						U+0964: DEVANAGARI DANDA	devanagari
+॥		|	‖		|		punctuation	infrequent						U+0965: DEVANAGARI DOUBLE DANDA	devanagari
+															
+															
+															
+															
+															
+\u200C		=	ₓ				zero-width non-joiner							U+200C: ZERO WIDTH NON-JOINER	punctuation
+\u200D		=	₊				zero-width joiner							U+200D: ZERO WIDTH JOINER	punctuation
+															
+“		<	“				quotation mark							U+201C: LEFT DOUBLE QUOTATION MARK	punctuation
+”		>	”				quotation mark							U+201D: RIGHT DOUBLE QUOTATION MARK	punctuation
+‘		<	‘				quotation mark							U+2018: LEFT SINGLE QUOTATION MARK	punctuation
+’		>	’				quotation mark							U+2019: RIGHT SINGLE QUOTATION MARK	punctuation
+
+
+`
+
+
+
+var cols = {
+"class": 1,
+"key":2,
+"transLoc": 3,
+"ipaLoc": 4,
+"transckey": 5,
+"transcription":6,
+"typeLoc": 7,
+"statusLoc": 8,
+"dvowel": 9,
+"ivowel": 10,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 11,
+"numLoc": 0,
+"nameLoc": 12,
+"nnameLoc": 13,
+"ucsName": 14,
+"block": 15,
+
+"othertranscriptions": [[6, 'ISO 15919']]
+}
+
+
