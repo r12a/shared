04335a54a726df083d7db881b312cd82db731510
r12a <ishida@w3.org>
2020-01-30 13:00:21 +0000

    + scriptdb/punctuation.js

---
+++ scriptdb/punctuation.js
@@ -0,0 +1,359 @@
+var spreadsheet = `
+\u2000					spaces		U+2000 EN QUAD	punctuation
+\u2001					spaces		U+2001 EM QUAD	punctuation
+\u2002					spaces		U+2002 EN SPACE	punctuation
+\u2003					spaces		U+2003 EM SPACE	punctuation
+\u2004					spaces		U+2004 THREE-PER-EM SPACE	punctuation
+\u2005					spaces		U+2005 FOUR-PER-EM SPACE	punctuation
+\u2006					spaces		U+2006 SIX-PER-EM SPACE	punctuation
+\u2007					spaces		U+2007 FIGURE SPACE	punctuation
+\u2008					spaces		U+2008 PUNCTUATION SPACE	punctuation
+\u2009					spaces		U+2009 THIN SPACE	punctuation
+\u200A					spaces		U+200A HAIR SPACE	punctuation
+								
+\u200B					format characters		U+200B: ZERO WIDTH SPACE	punctuation
+\u200C					format characters		U+200C: ZERO WIDTH NON-JOINER	punctuation
+\u200D					format characters		U+200D: ZERO WIDTH JOINER	punctuation
+\u200E					format characters		U+200E: LEFT-TO-RIGHT MARK	punctuation
+\u200F					format characters		U+200F: RIGHT-TO-LEFT MARK	punctuation
+								
+‐					dashes		U+2010: HYPHEN	punctuation
+‑					dashes		U+2011: NON-BREAKING HYPHEN	punctuation
+‒					dashes		U+2012: FIGURE DASH	punctuation
+–					dashes		U+2013: EN DASH	punctuation
+—					dashes		U+2014: EM DASH	punctuation
+―					dashes		U+2015: HORIZONTAL BAR	punctuation
+								
+‖					general punctuation		U+2016: DOUBLE VERTICAL LINE	punctuation
+‗					general punctuation		U+2017: DOUBLE LOW LINE	punctuation
+								
+‘					quotation marks & apostrophe		U+2018: LEFT SINGLE QUOTATION MARK	punctuation
+’					quotation marks & apostrophe		U+2019: RIGHT SINGLE QUOTATION MARK	punctuation
+‚					quotation marks & apostrophe		U+201A: SINGLE LOW-9 QUOTATION MARK	punctuation
+‛					quotation marks & apostrophe		U+201B: SINGLE HIGH-REVERSED-9 QUOTATION MARK	punctuation
+“					quotation marks & apostrophe		U+201C: LEFT DOUBLE QUOTATION MARK	punctuation
+”					quotation marks & apostrophe		U+201D: RIGHT DOUBLE QUOTATION MARK	punctuation
+„					quotation marks & apostrophe		U+201E: DOUBLE LOW-9 QUOTATION MARK	punctuation
+‟					quotation marks & apostrophe		U+201F: DOUBLE HIGH-REVERSED-9 QUOTATION MARK	punctuation
+								
+†					general punctuation		U+2020: DAGGER	punctuation
+‡					general punctuation		U+2021: DOUBLE DAGGER	punctuation
+•					general punctuation		U+2022: BULLET	punctuation
+‣					general punctuation		U+2023: TRIANGULAR BULLET	punctuation
+․					general punctuation		U+2024: ONE DOT LEADER	punctuation
+‥					general punctuation		U+2025: TWO DOT LEADER	punctuation
+…					general punctuation		U+2026: HORIZONTAL ELLIPSIS	punctuation
+‧					general punctuation		U+2027: HYPHENATION POINT	punctuation
+								
+\u2028					format characters		U+2028 LINE SEPARATOR	punctuation
+\u2029					format characters		U+2029 PARAGRAPH SEPARATOR	punctuation
+\u202A					format characters		U+202A: LEFT-TO-RIGHT EMBEDDING	punctuation
+\u202B					format characters		U+202B: RIGHT-TO-LEFT EMBEDDING	punctuation
+\u202C					format characters		U+202C: POP DIRECTIONAL FORMATTING	punctuation
+\u202D					format characters		U+202D: LEFT-TO-RIGHT OVERRIDE	punctuation
+\u202E					format characters		U+202E: RIGHT-TO-LEFT OVERRIDE	punctuation
+								
+\u202F					space		U+202F NARROW NO-BREAK SPACE	punctuation
+								
+‰					general punctuation		U+2030: PER MILLE SIGN	punctuation
+‱					general punctuation		U+2031: PER TEN THOUSAND SIGN	punctuation
+′					general punctuation		U+2032: PRIME	punctuation
+″					general punctuation		U+2033: DOUBLE PRIME	punctuation
+‴					general punctuation		U+2034: TRIPLE PRIME	punctuation
+‵					general punctuation		U+2035: REVERSED PRIME	punctuation
+‶					general punctuation		U+2036: REVERSED DOUBLE PRIME	punctuation
+‷					general punctuation		U+2037: REVERSED TRIPLE PRIME	punctuation
+‸					general punctuation		U+2038: CARET	punctuation
+								
+‹					quotation marks		U+2039: SINGLE LEFT-POINTING ANGLE QUOTATION MARK	punctuation
+›					quotation marks		U+203A: SINGLE RIGHT-POINTING ANGLE QUOTATION MARK	punctuation
+								
+※					general punctuation		U+203B: REFERENCE MARK	punctuation
+								
+‼					double punctuation for vertical text		U+203C: DOUBLE EXCLAMATION MARK	punctuation
+								
+‽					general punctuation		U+203D: INTERROBANG	punctuation
+‾					general punctuation		U+203E: OVERLINE	punctuation
+‿					general punctuation		U+203F: UNDERTIE	punctuation
+⁀					general punctuation		U+2040: CHARACTER TIE	punctuation
+⁁					general punctuation		U+2041: CARET INSERTION POINT	punctuation
+⁂					general punctuation		U+2042: ASTERISM	punctuation
+⁃					general punctuation		U+2043: HYPHEN BULLET	punctuation
+⁄					general punctuation		U+2044: FRACTION SLASH	punctuation
+								
+⁅					brackets		U+2045: LEFT SQUARE BRACKET WITH QUILL	punctuation
+⁆					brackets		U+2046: RIGHT SQUARE BRACKET WITH QUILL	punctuation
+								
+⁇					double punctuation for vertical text		U+2047: DOUBLE QUESTION MARK	punctuation
+⁈					double punctuation for vertical text		U+2048: QUESTION EXCLAMATION MARK	punctuation
+⁉					double punctuation for vertical text		U+2049: EXCLAMATION QUESTION MARK	punctuation
+								
+⁊					general punctuation		U+204A: TIRONIAN SIGN ET	punctuation
+⁋					general punctuation		U+204B: REVERSED PILCROW SIGN	punctuation
+⁌					general punctuation		U+204C: BLACK LEFTWARDS BULLET	punctuation
+⁍					general punctuation		U+204D: BLACK RIGHTWARDS BULLET	punctuation
+⁎					general punctuation		U+204E: LOW ASTERISK	punctuation
+⁏					general punctuation		U+204F: REVERSED SEMICOLON	punctuation
+⁐					general punctuation		U+2050: CLOSE UP	punctuation
+⁑					general punctuation		U+2051: TWO ASTERISKS ALIGNED VERTICALLY	punctuation
+⁒					general punctuation		U+2052: COMMERCIAL MINUS SIGN	punctuation
+⁓					general punctuation		U+2053: SWUNG DASH	punctuation
+⁔					general punctuation		U+2054: INVERTED UNDERTIE	punctuation
+⁕					general punctuation		U+2055: FLOWER PUNCTUATION MARK	punctuation
+								
+⁖					archaic punctuation	archaic	U+2056: THREE DOT PUNCTUATION	punctuation
+								
+⁗					general punctuation		U+2057: QUADRUPLE PRIME	punctuation
+								
+⁘					punctuation	archaic	U+2058: FOUR DOT PUNCTUATION	punctuation
+⁙					punctuation	archaic	U+2059: FIVE DOT PUNCTUATION	punctuation
+⁚					punctuation	archaic	U+205A: TWO DOT PUNCTUATION	punctuation
+⁛					punctuation	archaic	U+205B: FOUR DOT MARK	punctuation
+⁜					punctuation	archaic	U+205C: DOTTED CROSS	punctuation
+⁝					punctuation	archaic	U+205D: TRICOLON	punctuation
+⁞					punctuation	archaic	U+205E: VERTICAL FOUR DOTS	punctuation
+								
+\u205F					space		U+205F MEDIUM MATHEMATICAL SPACE	punctuation
+								
+\u2060					format character		U+2060: WORD JOINER	punctuation
+								
+\u2061					invisible operators		U+2061: FUNCTION APPLICATION	punctuation
+\u2062					invisible operators		U+2062: INVISIBLE TIMES	punctuation
+\u2063					invisible operators		U+2063: INVISIBLE SEPARATOR	punctuation
+\u2064					invisible operators		U+2064: INVISIBLE PLUS	punctuation
+								
+\u2066					format characters		U+2066: LEFT-TO-RIGHT ISOLATE	punctuation
+\u2067					format characters		U+2067: RIGHT-TO-LEFT ISOLATE	punctuation
+\u2068					format characters		U+2068: FIRST STRONG ISOLATE	punctuation
+\u2069					format characters		U+2069: POP DIRECTIONAL ISOLATE	punctuation
+								
+\u206A					punctuation	deprecated	U+206A: INHIBIT SYMMETRIC SWAPPING	punctuation
+\u206B					punctuation	deprecated	U+206B: ACTIVATE SYMMETRIC SWAPPING	punctuation
+\u206C					punctuation	deprecated	U+206C: INHIBIT ARABIC FORM SHAPING	punctuation
+\u206D					punctuation	deprecated	U+206D: ACTIVATE ARABIC FORM SHAPING	punctuation
+\u206E					punctuation	deprecated	U+206E: NATIONAL DIGIT SHAPES	punctuation
+\u206F					punctuation	deprecated	U+206F: NOMINAL DIGIT SHAPES	punctuation
+								
+								
+								
+								
+								
+⸀					new testament editorial symbols		U+2E00: RIGHT ANGLE SUBSTITUTION MARKER	punctuation
+⸁					new testament editorial symbols		U+2E01: RIGHT ANGLE DOTTED SUBSTITUTION MARKER	punctuation
+⸂					new testament editorial symbols		U+2E02: LEFT SUBSTITUTION BRACKET	punctuation
+⸃					new testament editorial symbols		U+2E03: RIGHT SUBSTITUTION BRACKET	punctuation
+⸄					new testament editorial symbols		U+2E04: LEFT DOTTED SUBSTITUTION BRACKET	punctuation
+⸅					new testament editorial symbols		U+2E05: RIGHT DOTTED SUBSTITUTION BRACKET	punctuation
+⸆					new testament editorial symbols		U+2E06: RAISED INTERPOLATION MARKER	punctuation
+⸇					new testament editorial symbols		U+2E07: RAISED DOTTED INTERPOLATION MARKER	punctuation
+⸈					new testament editorial symbols		U+2E08: DOTTED TRANSPOSITION MARKER	punctuation
+⸉					new testament editorial symbols		U+2E09: LEFT TRANSPOSITION BRACKET	punctuation
+⸊					new testament editorial symbols		U+2E0A: RIGHT TRANSPOSITION BRACKET	punctuation
+⸋					new testament editorial symbols		U+2E0B: RAISED SQUARE	punctuation
+⸌					new testament editorial symbols		U+2E0C: LEFT RAISED OMISSION BRACKET	punctuation
+⸍					new testament editorial symbols		U+2E0D: RIGHT RAISED OMISSION BRACKET	punctuation
+								
+⸎					ancient greek textual symbols		U+2E0E: EDITORIAL CORONIS	punctuation
+⸏					ancient greek textual symbols		U+2E0F: PARAGRAPHOS	punctuation
+⸐					ancient greek textual symbols		U+2E10: FORKED PARAGRAPHOS	punctuation
+⸑					ancient greek textual symbols		U+2E11: REVERSED FORKED PARAGRAPHOS	punctuation
+⸒					ancient greek textual symbols		U+2E12: HYPODIASTOLE	punctuation
+⸓					ancient greek textual symbols		U+2E13: DOTTED OBELOS	punctuation
+⸔					ancient greek textual symbols		U+2E14: DOWNWARDS ANCORA	punctuation
+⸕					ancient greek textual symbols		U+2E15: UPWARDS ANCORA	punctuation
+⸖					ancient greek textual symbols		U+2E16: DOTTED RIGHT-POINTING ANGLE	punctuation
+								
+⸗					ancient near-eastern linguistic symbol		U+2E17: DOUBLE OBLIQUE HYPHEN	punctuation
+								
+⸘					general punctuation		U+2E18: INVERTED INTERROBANG	punctuation
+⸙					general punctuation		U+2E19: PALM BRANCH	punctuation
+								
+⸚					dictionary punctuation		U+2E1A: HYPHEN WITH DIAERESIS	punctuation
+⸛					dictionary punctuation		U+2E1B: TILDE WITH RING ABOVE	punctuation
+								
+⸜					brackets		U+2E1C: LEFT LOW PARAPHRASE BRACKET	punctuation
+⸝					brackets		U+2E1D: RIGHT LOW PARAPHRASE BRACKET	punctuation
+								
+⸞					dictionary punctuation		U+2E1E: TILDE WITH DOT ABOVE	punctuation
+⸟					dictionary punctuation		U+2E1F: TILDE WITH DOT BELOW	punctuation
+								
+⸠					brackets		U+2E20: LEFT VERTICAL BAR WITH QUILL	punctuation
+⸡					brackets		U+2E21: RIGHT VERTICAL BAR WITH QUILL	punctuation
+								
+⸢					half-brackets		U+2E22: TOP LEFT HALF BRACKET	punctuation
+⸣					half-brackets		U+2E23: TOP RIGHT HALF BRACKET	punctuation
+⸤					half-brackets		U+2E24: BOTTOM LEFT HALF BRACKET	punctuation
+⸥					half-brackets		U+2E25: BOTTOM RIGHT HALF BRACKET	punctuation
+								
+⸦					brackets		U+2E26: LEFT SIDEWAYS U BRACKET	punctuation
+⸧					brackets		U+2E27: RIGHT SIDEWAYS U BRACKET	punctuation
+⸨					brackets		U+2E28: LEFT DOUBLE PARENTHESIS	punctuation
+⸩					brackets		U+2E29: RIGHT DOUBLE PARENTHESIS	punctuation
+								
+⸪					historic punctuation	archaic	U+2E2A: TWO DOTS OVER ONE DOT PUNCTUATION	punctuation
+⸫					historic punctuation	archaic	U+2E2B: ONE DOT OVER TWO DOTS PUNCTUATION	punctuation
+⸬					historic punctuation	archaic	U+2E2C: SQUARED FOUR DOT PUNCTUATION	punctuation
+⸭					historic punctuation	archaic	U+2E2D: FIVE DOT MARK	punctuation
+⸮					historic punctuation	archaic	U+2E2E: REVERSED QUESTION MARK	punctuation
+ⸯ					historic punctuation	archaic	U+2E2F: VERTICAL TILDE	punctuation
+⸰					historic punctuation	archaic	U+2E30: RING POINT	punctuation
+⸱					historic punctuation	archaic	U+2E31: WORD SEPARATOR MIDDLE DOT	punctuation
+								
+⸲					palaeotype transliteration symbol		U+2E32: TURNED COMMA	punctuation
+								
+⸳					historic punctuation	archaic	U+2E33: RAISED DOT	punctuation
+⸴					historic punctuation	archaic	U+2E34: RAISED COMMA	punctuation
+								
+⸵					palaeotype transliteration symbols		U+2E35: TURNED SEMICOLON	punctuation
+⸶					palaeotype transliteration symbols		U+2E36: DAGGER WITH LEFT GUARD	punctuation
+⸷					palaeotype transliteration symbols		U+2E37: DAGGER WITH RIGHT GUARD	punctuation
+⸸					palaeotype transliteration symbols		U+2E38: TURNED DAGGER	punctuation
+⸹					palaeotype transliteration symbols		U+2E39: TOP HALF SECTION SIGN	punctuation
+								
+⸺					dashes		U+2E3A: TWO-EM DASH	punctuation
+⸻					dashes		U+2E3B: THREE-EM DASH	punctuation
+								
+⸼					alternate forms of punctuation		U+2E3C: STENOGRAPHIC FULL STOP	punctuation
+⸽					alternate forms of punctuation		U+2E3D: VERTICAL SIX DOTS	punctuation
+⸾					alternate forms of punctuation		U+2E3E: WIGGLY VERTICAL LINE	punctuation
+								
+⸿					historic punctuation	archaic	U+2E3F: CAPITULUM	punctuation
+								
+⹀					double hyphen		U+2E40: DOUBLE HYPHEN	punctuation
+								
+⹁					reversed punctuation		U+2E41: REVERSED COMMA	punctuation
+⹂					reversed punctuation		U+2E42: DOUBLE LOW-REVERSED-9 QUOTATION MARK	punctuation
+								
+⹃					miscellaneous punctuation		U+2E43: DASH WITH LEFT UPTURN	punctuation
+⹄					miscellaneous punctuation		U+2E44: DOUBLE SUSPENSION MARK	punctuation
+								
+⹅					typicon punctuation		U+2E45: INVERTED LOW KAVYKA	punctuation
+⹆					typicon punctuation		U+2E46: INVERTED LOW KAVYKA WITH KAVYKA ABOVE	punctuation
+⹇					typicon punctuation		U+2E47: LOW KAVYKA	punctuation
+⹈					typicon punctuation		U+2E48: LOW KAVYKA WITH DOT	punctuation
+⹉					typicon punctuation		U+2E49: DOUBLE STACKED COMMA	punctuation
+								
+⹊					historic punctuation	archaic	U+2E4A: DOTTED SOLIDUS	punctuation
+⹋					historic punctuation	archaic	U+2E4B: TRIPLE DAGGER	punctuation
+⹌					historic punctuation	archaic	U+2E4C: MEDIEVAL COMMA	punctuation
+⹍					historic punctuation	archaic	U+2E4D: PARAGRAPHUS MARK	punctuation
+⹎					historic punctuation	archaic	U+2E4E: PUNCTUS ELEVATUS MARK	punctuation
+⹏					historic punctuation	archaic	U+2E4F: CORNISH VERSE DIVIDER	punctuation
+⹐					historic punctuation	archaic	U+2E50: CROSS PATTY WITH RIGHT CROSSBAR	punctuation
+⹑					historic punctuation	archaic	U+2E51: CROSS PATTY WITH LEFT CROSSBAR	punctuation
+⹒					historic punctuation	archaic	U+2E52: TIRONIAN SIGN CAPITAL ET	punctuation
+								
+								
+								
+								
+								
+\u3000					CJK symbols & punctuation		U+3000 IDEOGRAPHIC SPACE	punctuation
+、					CJK symbols & punctuation		U+3001: IDEOGRAPHIC COMMA	punctuation
+。					CJK symbols & punctuation		U+3002: IDEOGRAPHIC FULL STOP	punctuation
+〃					CJK symbols & punctuation		U+3003: DITTO MARK	punctuation
+〄					CJK symbols & punctuation		U+3004: JAPANESE INDUSTRIAL STANDARD SYMBOL	punctuation
+々					CJK symbols & punctuation		U+3005: IDEOGRAPHIC ITERATION MARK	punctuation
+〆					CJK symbols & punctuation		U+3006: IDEOGRAPHIC CLOSING MARK	punctuation
+〇					CJK symbols & punctuation		U+3007: IDEOGRAPHIC NUMBER ZERO	punctuation
+								
+〈					CJK angle brackets		U+3008: LEFT ANGLE BRACKET	punctuation
+〉					CJK angle brackets		U+3009: RIGHT ANGLE BRACKET	punctuation
+《					CJK angle brackets		U+300A: LEFT DOUBLE ANGLE BRACKET	punctuation
+》					CJK angle brackets		U+300B: RIGHT DOUBLE ANGLE BRACKET	punctuation
+								
+「					CJK corner brackets		U+300C: LEFT CORNER BRACKET	punctuation
+」					CJK corner brackets		U+300D: RIGHT CORNER BRACKET	punctuation
+『					CJK corner brackets		U+300E: LEFT WHITE CORNER BRACKET	punctuation
+』					CJK corner brackets		U+300F: RIGHT WHITE CORNER BRACKET	punctuation
+								
+【					CJK brackets		U+3010: LEFT BLACK LENTICULAR BRACKET	punctuation
+】					CJK brackets		U+3011: RIGHT BLACK LENTICULAR BRACKET	punctuation
+								
+〒					CJK symbols		U+3012: POSTAL MARK	punctuation
+〓					CJK symbols		U+3013: GETA MARK	punctuation
+								
+〔					CJK brackets		U+3014: LEFT TORTOISE SHELL BRACKET	punctuation
+〕					CJK brackets		U+3015: RIGHT TORTOISE SHELL BRACKET	punctuation
+〖					CJK brackets		U+3016: LEFT WHITE LENTICULAR BRACKET	punctuation
+〗					CJK brackets		U+3017: RIGHT WHITE LENTICULAR BRACKET	punctuation
+〘					CJK brackets		U+3018: LEFT WHITE TORTOISE SHELL BRACKET	punctuation
+〙					CJK brackets		U+3019: RIGHT WHITE TORTOISE SHELL BRACKET	punctuation
+〚					CJK brackets		U+301A: LEFT WHITE SQUARE BRACKET	punctuation
+〛					CJK brackets		U+301B: RIGHT WHITE SQUARE BRACKET	punctuation
+								
+〜					CJK punctuation		U+301C: WAVE DASH	punctuation
+〝					CJK punctuation		U+301D: REVERSED DOUBLE PRIME QUOTATION MARK	punctuation
+〞					CJK punctuation		U+301E: DOUBLE PRIME QUOTATION MARK	punctuation
+〟					CJK punctuation		U+301F: LOW DOUBLE PRIME QUOTATION MARK	punctuation
+								
+〠					CJK symbol		U+3020: POSTAL MARK FACE	punctuation
+								
+〡					suzhou numerals		U+3021: HANGZHOU NUMERAL ONE	punctuation
+〢					suzhou numerals		U+3022: HANGZHOU NUMERAL TWO	punctuation
+〣					suzhou numerals		U+3023: HANGZHOU NUMERAL THREE	punctuation
+〤					suzhou numerals		U+3024: HANGZHOU NUMERAL FOUR	punctuation
+〥					suzhou numerals		U+3025: HANGZHOU NUMERAL FIVE	punctuation
+〦					suzhou numerals		U+3026: HANGZHOU NUMERAL SIX	punctuation
+〧					suzhou numerals		U+3027: HANGZHOU NUMERAL SEVEN	punctuation
+〨					suzhou numerals		U+3028: HANGZHOU NUMERAL EIGHT	punctuation
+〩					suzhou numerals		U+3029: HANGZHOU NUMERAL NINE	punctuation
+								
+〪					combining tone marks		U+302A: IDEOGRAPHIC LEVEL TONE MARK​	punctuation
+〫					combining tone marks		U+302B: IDEOGRAPHIC RISING TONE MARK​	punctuation
+〬					combining tone marks		U+302C: IDEOGRAPHIC DEPARTING TONE MARK​	punctuation
+〭					combining tone marks		U+302D: IDEOGRAPHIC ENTERING TONE MARK​	punctuation
+〮					combining tone marks		U+302E: HANGUL SINGLE DOT TONE MARK​	punctuation
+〯					combining tone marks		U+302F: HANGUL DOUBLE DOT TONE MARK​	punctuation
+								
+〰					other CJK punctuation		U+3030: WAVY DASH	punctuation
+								
+〱					kana repeat marks		U+3031: VERTICAL KANA REPEAT MARK	punctuation
+〲					kana repeat marks		U+3032: VERTICAL KANA REPEAT WITH VOICED SOUND MARK	punctuation
+〳					kana repeat marks		U+3033: VERTICAL KANA REPEAT MARK UPPER HALF	punctuation
+〴					kana repeat marks		U+3034: VERTICAL KANA REPEAT WITH VOICED SOUND MARK UPPER HALF	punctuation
+〵					kana repeat marks		U+3035: VERTICAL KANA REPEAT MARK LOWER HALF	punctuation
+								
+〶					other CJK symbols		U+3036: CIRCLED POSTAL MARK	punctuation
+〷					other CJK symbols		U+3037: IDEOGRAPHIC TELEGRAPH LINE FEED SEPARATOR SYMBOL	punctuation
+								
+〸					additional suzhou numerals		U+3038: HANGZHOU NUMERAL TEN	punctuation
+〹					additional suzhou numerals		U+3039: HANGZHOU NUMERAL TWENTY	punctuation
+〺					additional suzhou numerals		U+303A: HANGZHOU NUMERAL THIRTY	punctuation
+								
+〻					other CJK punctuation		U+303B: VERTICAL IDEOGRAPHIC ITERATION MARK	punctuation
+〼					other CJK punctuation		U+303C: MASU MARK	punctuation
+〽					other CJK punctuation		U+303D: PART ALTERNATION MARK	punctuation
+								
+〾					special CJK indicators		U+303E: IDEOGRAPHIC VARIATION INDICATOR	punctuation
+〿					special CJK indicators		U+303F: IDEOGRAPHIC HALF FILL SPACE	punctuation
+
+
+`
+
+
+var cols = {
+"class": 1,
+"key":2,
+"transLoc": 0,
+"ipaLoc": 0,
+"transckey": 0,
+"transcription":0,
+"typeLoc": 5,
+"statusLoc": 6,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nameLoc": 10,
+"nnameLoc": 11,
+"ucsName": 7,
+"block": 8,
+
+//"othertranscriptions": [[5, 'ISO 19515']]
+}
