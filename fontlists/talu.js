fd0faa7bc982d92e4cc47283a32c3ce890f1ec58
r12a <ishida@w3.org>
2020-02-03 09:08:16 +0000

    fontlists, newtailue -> talu

---
+++ fontlists/talu.js
@@ -0,0 +1,20 @@
+ var fontSelection =`<optgroup label="Webfonts">
+       <option value="Noto Sans New Tai Lue WF">Noto Sans New Tai Lue WF</option>
+        </optgroup>
+        
+        <optgroup label="Google fonts">
+        <option value="Noto Sans New Tai Lue">Noto Sans New Tai Lue</option>
+        </optgroup>
+		
+        <optgroup label="SIL fonts">
+        <option value="Dai Banna SIL Book">Dai Banna SIL Book</option>
+        <option value="Dai Banna SIL Light">Dai Banna SIL Light</option>
+        </optgroup>
+		
+        <optgroup label="General fonts">
+        <option value="Arial Unicode MS">Arial Unicode MS</option>
+        <option value="Code2000">Code2000</option>
+        <option value="Microsoft Sans Serif">Microsoft Sans Serif</option>
+        <option value="sans-serif">sans-serif</option>
+        <option value="serif">serif</option>
+        </optgroup>`
