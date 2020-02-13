1ca763251cfe412bb919b6c53b07eb8502b5e11e
r12a <ishida@w3.org>
2020-02-13 17:47:57 +0000

    +chinese

---
+++ fontlists/chinese.js
@@ -0,0 +1,55 @@
+ var fontSelection = `<option value="0">Select...</option>
+            <optgroup label="Windows SC fonts ">
+            <option value="KaiTi">KaiTi</option>
+            <option value="FangSong">FangSong</option>
+            <option value="Microsoft YaHei">Microsoft YaHei</option>
+            <option value="NSimSun">NSimSun</option>
+            <option value="SimSun">SimSun</option>
+            <option value="SimSun-ExtB">SimSun-ExtB</option>
+            <option value="SimHei">SimHei</option>
+            </optgroup>
+            
+            <optgroup label="Windows TC fonts ">
+            <option value="DFKai-SB">DFKai-SB</option>
+            <option value="MingLiu">MingLiu</option>
+            <option value="MingLiu-ExtB">MingLiu-ExtB</option>
+            <option value="MingLiu-HKSCS-ExtB">MingLiu-HKSCS-ExtB</option>
+            <option value="PMingLiu">PMingLiu</option>
+            <option value="PMingLiu-ExtB">PMingLiu-ExtB</option>
+            <option value="Microsoft JhengHei">Microsoft JhengHei</option>
+            </optgroup>
+            
+            <optgroup label="Mac SC fonts ">
+            <option value="Hei">Hei</option>
+            <option value="Kai">Kai</option>
+            <option value="Song">Song</option>
+            <option value="STSong">STSong</option>
+            <option value="STFangsong">STFangsong</option>
+            <option value="STHeiti">STHeiti*</option>
+            <option value="STKaiti">STKaiti</option>
+            <option value="PingFang SC">PingFang SC</option>
+            
+            <option value="Baoli SC">Baoli SC</option>
+            <option value="Heiti SC">Heiti SC</option>
+            <option value="Kaiti SC">Kaiti SC</option>
+            <option value="Lantinghei SC">Lantinghei SC</option>
+            <option value="Libian SC">Libian SC</option>
+            <option value="LingWai SC">LingWai SC</option>
+            </optgroup>
+            
+            <optgroup label="Mac TC fonts ">
+            <option value="Apple LiGothic">Apple LiGothic</option>
+            <option value="Apple LiSong">Apple LiSong</option>
+            <option value="Biaukai">Biaukai</option>
+            <option value="LiHei Pro">LiHei Pro</option>
+            <option value="LiSong Pro">LiSong Pro</option>
+            </optgroup>
+            
+            <optgroup label="Google fonts ">
+            <option value="Noto Sans CJK SC">Noto Sans CJK SC</option>
+            <option value="Noto Serif CJK TC">Noto Serif CJK TC</option>
+            </optgroup>
+            
+            <optgroup label="Other fonts ">
+            <option value="Arial Unicode MS">Arial Unicode MS</option>
+            </optgroup>`
--- scriptdb/hani.js
+++ scriptdb/hani.js
@@ -1,4 +1,288 @@
 var spreadsheet = `
+「					quotation mark		U+300C: LEFT CORNER BRACKET	punctuation
+」					quotation mark		U+300D: RIGHT CORNER BRACKET	punctuation
+『					quotation mark		U+300E: LEFT WHITE CORNER BRACKET	punctuation
+』					quotation mark		U+300F: RIGHT WHITE CORNER BRACKET	punctuation
+“					quotation mark		U+201C: LEFT DOUBLE QUOTATION MARK	punctuation
+”					quotation mark		U+201D: RIGHT DOUBLE QUOTATION MARK	punctuation
+‘					quotation mark		U+2018: LEFT SINGLE QUOTATION MARK	punctuation
+’					quotation mark		U+2019: RIGHT SINGLE QUOTATION MARK	punctuation
+								
+								
+（		<			brackets		U+FF08: FULLWIDTH LEFT PARENTHESIS	punctuation
+）		>			brackets		U+FF09: FULLWIDTH RIGHT PARENTHESIS	punctuation
+《		<			brackets		U+300A: LEFT DOUBLE ANGLE BRACKET	punctuation
+》		>			brackets		U+300B: RIGHT DOUBLE ANGLE BRACKET	punctuation
+〈		<			brackets		U+3008: LEFT ANGLE BRACKET	punctuation
+〉		>			brackets		U+3009: RIGHT ANGLE BRACKET	punctuation
+【		<			brackets		U+3010: LEFT BLACK LENTICULAR BRACKET	punctuation
+】		>			brackets		U+3011: RIGHT BLACK LENTICULAR BRACKET	punctuation
+〖		<			brackets		U+3016: LEFT WHITE LENTICULAR BRACKET	punctuation
+〗		>			brackets		U+3017: RIGHT WHITE LENTICULAR BRACKET	punctuation
+〔		<			brackets		U+3014: LEFT TORTOISE SHELL BRACKET	punctuation
+〕		>			brackets		U+3015: RIGHT TORTOISE SHELL BRACKET	punctuation
+［		<			brackets		U+FF3B: FULLWIDTH LEFT SQUARE BRACKET	punctuation
+］		>			brackets		U+FF3D: FULLWIDTH RIGHT SQUARE BRACKET	punctuation
+｛		<			brackets		U+FF5B: FULLWIDTH LEFT CURLY BRACKET	punctuation
+｝		>			brackets		U+FF5D: FULLWIDTH RIGHT CURLY BRACKET	punctuation
+								
+								
+⸺					dashes		U+2E3A: TWO-EM DASH	punctuation
+—					dashes		U+2014: EM DASH	punctuation
+-					dashes		U+002D: HYPHEN-MINUS	punctuation
+–					dashes		U+2013: EN DASH	punctuation
+…					dashes		U+2026: HORIZONTAL ELLIPSIS	punctuation
+～					dashes		U+FF5E: FULLWIDTH TILDE	punctuation
+								
+								
+？		?			quotation marks & apostrophe		U+FF1F: FULLWIDTH QUESTION MARK	punctuation
+⁇		?			quotation marks & apostrophe		U+2047: DOUBLE QUESTION MARK	punctuation
+！		!			quotation marks & apostrophe		U+FF01: FULLWIDTH EXCLAMATION MARK	punctuation
+‼		!			quotation marks & apostrophe		U+203C: DOUBLE EXCLAMATION MARK	punctuation
+⁈		?			quotation marks & apostrophe		U+2048: QUESTION EXCLAMATION MARK	punctuation
+⁉		!			quotation marks & apostrophe		U+2049: EXCLAMATION QUESTION MARK	punctuation
+								
+								
+，		,			general punctuation		U+FF0C: FULLWIDTH COMMA	punctuation
+、		,			general punctuation		U+3001: IDEOGRAPHIC COMMA	punctuation
+：		:			general punctuation		U+FF1A: FULLWIDTH COLON	punctuation
+；		;			general punctuation		U+FF1B: FULLWIDTH SEMICOLON	punctuation
+。		.			general punctuation		U+3002: IDEOGRAPHIC FULL STOP	punctuation
+．		.			general punctuation		U+FF0E: FULLWIDTH FULL STOP	punctuation
+·		.			general punctuation		U+00B7: MIDDLE DOT	punctuation
+								
+								
+0		0	0				U+0030: DIGIT ZERO	
+1		1	1				U+0031: DIGIT ONE	
+2		2	2				U+0032: DIGIT TWO	
+3		3	3				U+0033: DIGIT THREE	
+4		4	4				U+0034: DIGIT FOUR	
+5		5	5				U+0035: DIGIT FIVE	
+6		6	6				U+0036: DIGIT SIX	
+7		7	7				U+0037: DIGIT SEVEN	
+8		8	8				U+0038: DIGIT EIGHT	
+9		9	9				U+0039: DIGIT NINE	
+								
+								
+０		0	0		fullwidth digits		U+FF10: FULLWIDTH DIGIT ZERO	punctuation
+１		1	1		fullwidth digits		U+FF11: FULLWIDTH DIGIT ONE	punctuation
+２		2	2		fullwidth digits		U+FF12: FULLWIDTH DIGIT TWO	punctuation
+３		3	3		fullwidth digits		U+FF13: FULLWIDTH DIGIT THREE	punctuation
+４		4	4		fullwidth digits		U+FF14: FULLWIDTH DIGIT FOUR	punctuation
+５		5	5		fullwidth digits		U+FF15: FULLWIDTH DIGIT FIVE	punctuation
+６		6	6		fullwidth digits		U+FF16: FULLWIDTH DIGIT SIX	punctuation
+７		7	7		fullwidth digits		U+FF17: FULLWIDTH DIGIT SEVEN	punctuation
+８		8	8		fullwidth digits		U+FF18: FULLWIDTH DIGIT EIGHT	punctuation
+９		9	9		fullwidth digits		U+FF19: FULLWIDTH DIGIT NINE	punctuation
+								
+								
+\u2060					format character		U+2060: WORD JOINER	punctuation
+								
+								
+								
+								
+								
+								
+								
+\u3000					CJK symbols & punctuation		U+3000 IDEOGRAPHIC SPACE	punctuation
+〃					CJK symbols & punctuation		U+3003: DITTO MARK	punctuation
+々					CJK symbols & punctuation		U+3005: IDEOGRAPHIC ITERATION MARK	punctuation
+〆					CJK symbols & punctuation		U+3006: IDEOGRAPHIC CLOSING MARK	punctuation
+〇					CJK symbols & punctuation		U+3007: IDEOGRAPHIC NUMBER ZERO	punctuation
+								
+〒					CJK symbols		U+3012: POSTAL MARK	punctuation
+〓					CJK symbols		U+3013: GETA MARK	punctuation
+								
+〜					CJK punctuation		U+301C: WAVE DASH	punctuation
+〝					CJK punctuation		U+301D: REVERSED DOUBLE PRIME QUOTATION MARK	punctuation
+〞					CJK punctuation		U+301E: DOUBLE PRIME QUOTATION MARK	punctuation
+〟					CJK punctuation		U+301F: LOW DOUBLE PRIME QUOTATION MARK	punctuation
+								
+〠					CJK symbol		U+3020: POSTAL MARK FACE	punctuation
+								
+〡					suzhou numerals		U+3021: HANGZHOU NUMERAL ONE	punctuation
+〢					suzhou numerals		U+3022: HANGZHOU NUMERAL TWO	punctuation
+〣					suzhou numerals		U+3023: HANGZHOU NUMERAL THREE	punctuation
+〤					suzhou numerals		U+3024: HANGZHOU NUMERAL FOUR	punctuation
+〥					suzhou numerals		U+3025: HANGZHOU NUMERAL FIVE	punctuation
+〦					suzhou numerals		U+3026: HANGZHOU NUMERAL SIX	punctuation
+〧					suzhou numerals		U+3027: HANGZHOU NUMERAL SEVEN	punctuation
+〨					suzhou numerals		U+3028: HANGZHOU NUMERAL EIGHT	punctuation
+〩					suzhou numerals		U+3029: HANGZHOU NUMERAL NINE	punctuation
+								
+〪					combining tone marks		U+302A: IDEOGRAPHIC LEVEL TONE MARK​	punctuation
+〫					combining tone marks		U+302B: IDEOGRAPHIC RISING TONE MARK​	punctuation
+〬					combining tone marks		U+302C: IDEOGRAPHIC DEPARTING TONE MARK​	punctuation
+〭					combining tone marks		U+302D: IDEOGRAPHIC ENTERING TONE MARK​	punctuation
+								
+								
+								
+〰					other CJK punctuation		U+3030: WAVY DASH	punctuation
+								
+〶					other CJK symbols		U+3036: CIRCLED POSTAL MARK	punctuation
+〷					other CJK symbols		U+3037: IDEOGRAPHIC TELEGRAPH LINE FEED SEPARATOR SYMBOL	punctuation
+								
+〸					additional suzhou numerals		U+3038: HANGZHOU NUMERAL TEN	punctuation
+〹					additional suzhou numerals		U+3039: HANGZHOU NUMERAL TWENTY	punctuation
+〺					additional suzhou numerals		U+303A: HANGZHOU NUMERAL THIRTY	punctuation
+								
+〻					other CJK punctuation		U+303B: VERTICAL IDEOGRAPHIC ITERATION MARK	punctuation
+〼					other CJK punctuation		U+303C: MASU MARK	punctuation
+〽					other CJK punctuation		U+303D: PART ALTERNATION MARK	punctuation
+								
+〾					special CJK indicators		U+303E: IDEOGRAPHIC VARIATION INDICATOR	punctuation
+〿					special CJK indicators		U+303F: IDEOGRAPHIC HALF FILL SPACE	punctuation
+								
+／							U+FF0F FULLWIDTH SOLIDUS	punctuation
+
+
+								
+A		A	A				U+0041: LATIN CAPITAL LETTER A	
+B		B	B				U+0042: LATIN CAPITAL LETTER B	
+C		C	C				U+0043: LATIN CAPITAL LETTER C	
+D		D	D				U+0044: LATIN CAPITAL LETTER D	
+E		E	E				U+0045: LATIN CAPITAL LETTER E	
+F		F	F				U+0046: LATIN CAPITAL LETTER F	
+G		G	G				U+0047: LATIN CAPITAL LETTER G	
+H		H	H				U+0048: LATIN CAPITAL LETTER H	
+I		I	I				U+0049: LATIN CAPITAL LETTER I	
+J		J	J				U+004A: LATIN CAPITAL LETTER J	
+K		K	K				U+004B: LATIN CAPITAL LETTER K	
+L		L	L				U+004C: LATIN CAPITAL LETTER L	
+M		M	M				U+004D: LATIN CAPITAL LETTER M	
+N		N	N				U+004E: LATIN CAPITAL LETTER N	
+O		O	O				U+004F: LATIN CAPITAL LETTER O	
+P		P	P				U+0050: LATIN CAPITAL LETTER P	
+Q		Q	Q				U+0051: LATIN CAPITAL LETTER Q	
+R		R	R				U+0052: LATIN CAPITAL LETTER R	
+S		S	S				U+0053: LATIN CAPITAL LETTER S	
+T		T	T				U+0054: LATIN CAPITAL LETTER T	
+U		U	U				U+0055: LATIN CAPITAL LETTER U	
+V		V	V				U+0056: LATIN CAPITAL LETTER V	
+W		W	W				U+0057: LATIN CAPITAL LETTER W	
+X		X	X				U+0058: LATIN CAPITAL LETTER X	
+Y		Y	Y				U+0059: LATIN CAPITAL LETTER Y	
+Z		Z	Z				U+005A: LATIN CAPITAL LETTER Z	
+a		a	a				U+0061: LATIN SMALL LETTER A	
+b		b	b				U+0062: LATIN SMALL LETTER B	
+c		c	c				U+0063: LATIN SMALL LETTER C	
+d		d	d				U+0064: LATIN SMALL LETTER D	
+e		e	e				U+0065: LATIN SMALL LETTER E	
+f		f	f				U+0066: LATIN SMALL LETTER F	
+g		g	g				U+0067: LATIN SMALL LETTER G	
+h		h	h				U+0068: LATIN SMALL LETTER H	
+i		i	i				U+0069: LATIN SMALL LETTER I	
+j		j	j				U+006A: LATIN SMALL LETTER J	
+k		k	k				U+006B: LATIN SMALL LETTER K	
+l		l	l				U+006C: LATIN SMALL LETTER L	
+m		m	m				U+006D: LATIN SMALL LETTER M	
+n		n	n				U+006E: LATIN SMALL LETTER N	
+o		o	o				U+006F: LATIN SMALL LETTER O	
+p		p	p				U+0070: LATIN SMALL LETTER P	
+q		q	q				U+0071: LATIN SMALL LETTER Q	
+r		r	r				U+0072: LATIN SMALL LETTER R	
+s		s	s				U+0073: LATIN SMALL LETTER S	
+t		t	t				U+0074: LATIN SMALL LETTER T	
+u		u	u				U+0075: LATIN SMALL LETTER U	
+v		v	v				U+0076: LATIN SMALL LETTER V	
+w		w	w				U+0077: LATIN SMALL LETTER W	
+x		x	x				U+0078: LATIN SMALL LETTER X	
+y		y	y				U+0079: LATIN SMALL LETTER Y	
+z		z	z				U+007A: LATIN SMALL LETTER Z	
+								
+								
+								
+								
+Ａ		A	A		fullwidth		U+FF21: FULLWIDTH LATIN CAPITAL LETTER A	punctuation
+Ｂ		B	B		fullwidth		U+FF22: FULLWIDTH LATIN CAPITAL LETTER B	punctuation
+Ｃ		C	C		fullwidth		U+FF23: FULLWIDTH LATIN CAPITAL LETTER C	punctuation
+Ｄ		D	D		fullwidth		U+FF24: FULLWIDTH LATIN CAPITAL LETTER D	punctuation
+Ｅ		E	E		fullwidth		U+FF25: FULLWIDTH LATIN CAPITAL LETTER E	punctuation
+Ｆ		F	F		fullwidth		U+FF26: FULLWIDTH LATIN CAPITAL LETTER F	punctuation
+Ｇ		G	G		fullwidth		U+FF27: FULLWIDTH LATIN CAPITAL LETTER G	punctuation
+Ｈ		H	H		fullwidth		U+FF28: FULLWIDTH LATIN CAPITAL LETTER H	punctuation
+Ｉ		I	I		fullwidth		U+FF29: FULLWIDTH LATIN CAPITAL LETTER I	punctuation
+Ｊ		J	J		fullwidth		U+FF2A: FULLWIDTH LATIN CAPITAL LETTER J	punctuation
+Ｋ		K	K		fullwidth		U+FF2B: FULLWIDTH LATIN CAPITAL LETTER K	punctuation
+Ｌ		L	L		fullwidth		U+FF2C: FULLWIDTH LATIN CAPITAL LETTER L	punctuation
+Ｍ		M	M		fullwidth		U+FF2D: FULLWIDTH LATIN CAPITAL LETTER M	punctuation
+Ｎ		N	N		fullwidth		U+FF2E: FULLWIDTH LATIN CAPITAL LETTER N	punctuation
+Ｏ		O	O		fullwidth		U+FF2F: FULLWIDTH LATIN CAPITAL LETTER O	punctuation
+Ｐ		P	P		fullwidth		U+FF30: FULLWIDTH LATIN CAPITAL LETTER P	punctuation
+Ｑ		Q	Q		fullwidth		U+FF31: FULLWIDTH LATIN CAPITAL LETTER Q	punctuation
+Ｒ		R	R		fullwidth		U+FF32: FULLWIDTH LATIN CAPITAL LETTER R	punctuation
+Ｓ		S	S		fullwidth		U+FF33: FULLWIDTH LATIN CAPITAL LETTER S	punctuation
+Ｔ		T	T		fullwidth		U+FF34: FULLWIDTH LATIN CAPITAL LETTER T	punctuation
+Ｕ		U	U		fullwidth		U+FF35: FULLWIDTH LATIN CAPITAL LETTER U	punctuation
+Ｖ		V	V		fullwidth		U+FF36: FULLWIDTH LATIN CAPITAL LETTER V	punctuation
+Ｗ		W	W		fullwidth		U+FF37: FULLWIDTH LATIN CAPITAL LETTER W	punctuation
+Ｘ		X	X		fullwidth		U+FF38: FULLWIDTH LATIN CAPITAL LETTER X	punctuation
+Ｙ		Y	Y		fullwidth		U+FF39: FULLWIDTH LATIN CAPITAL LETTER Y	punctuation
+Ｚ		Z	Z		fullwidth		U+FF3A: FULLWIDTH LATIN CAPITAL LETTER Z	punctuation
+ａ		a	a		fullwidth		U+FF41: FULLWIDTH LATIN SMALL LETTER A	punctuation
+ｂ		b	b		fullwidth		U+FF42: FULLWIDTH LATIN SMALL LETTER B	punctuation
+ｃ		c	c		fullwidth		U+FF43: FULLWIDTH LATIN SMALL LETTER C	punctuation
+ｄ		d	d		fullwidth		U+FF44: FULLWIDTH LATIN SMALL LETTER D	punctuation
+ｅ		e	e		fullwidth		U+FF45: FULLWIDTH LATIN SMALL LETTER E	punctuation
+ｆ		f	f		fullwidth		U+FF46: FULLWIDTH LATIN SMALL LETTER F	punctuation
+ｇ		g	g		fullwidth		U+FF47: FULLWIDTH LATIN SMALL LETTER G	punctuation
+ｈ		h	h		fullwidth		U+FF48: FULLWIDTH LATIN SMALL LETTER H	punctuation
+ｉ		i	i		fullwidth		U+FF49: FULLWIDTH LATIN SMALL LETTER I	punctuation
+ｊ		j	j		fullwidth		U+FF4A: FULLWIDTH LATIN SMALL LETTER J	punctuation
+ｋ		k	k		fullwidth		U+FF4B: FULLWIDTH LATIN SMALL LETTER K	punctuation
+ｌ		l	l		fullwidth		U+FF4C: FULLWIDTH LATIN SMALL LETTER L	punctuation
+ｍ		m	m		fullwidth		U+FF4D: FULLWIDTH LATIN SMALL LETTER M	punctuation
+ｎ		n	n		fullwidth		U+FF4E: FULLWIDTH LATIN SMALL LETTER N	punctuation
+ｏ		o	o		fullwidth		U+FF4F: FULLWIDTH LATIN SMALL LETTER O	punctuation
+ｐ		p	p		fullwidth		U+FF50: FULLWIDTH LATIN SMALL LETTER P	punctuation
+ｑ		q	q		fullwidth		U+FF51: FULLWIDTH LATIN SMALL LETTER Q	punctuation
+ｒ		r	r		fullwidth		U+FF52: FULLWIDTH LATIN SMALL LETTER R	punctuation
+ｓ		s	s		fullwidth		U+FF53: FULLWIDTH LATIN SMALL LETTER S	punctuation
+ｔ		t	t		fullwidth		U+FF54: FULLWIDTH LATIN SMALL LETTER T	punctuation
+ｕ		u	u		fullwidth		U+FF55: FULLWIDTH LATIN SMALL LETTER U	punctuation
+ｖ		v	v		fullwidth		U+FF56: FULLWIDTH LATIN SMALL LETTER V	punctuation
+ｗ		w	w		fullwidth		U+FF57: FULLWIDTH LATIN SMALL LETTER W	punctuation
+ｘ		x	x		fullwidth		U+FF58: FULLWIDTH LATIN SMALL LETTER X	punctuation
+ｙ		y	y		fullwidth		U+FF59: FULLWIDTH LATIN SMALL LETTER Y	punctuation
+ｚ		z	z		fullwidth		U+FF5A: FULLWIDTH LATIN SMALL LETTER Z	punctuation
+								
+								
+								
+IPA1				pʰ				
+IPA2				tʰ				
+IPA3				kʰ				
+IPA4				t͡s				
+IPA5				t͡sʰ				
+IPA6				t͡ɕ				
+IPA7				t͡ɕʰ				
+IPA8				t͡ʂ				
+IPA9				t͡ʂʰ				
+IPA10				ɕ				
+IPA11				ʂ				
+IPA12				ŋ				
+IPA14				ɻ				
+								
+IPA20				ɤ				
+IPA21				ə				
+IPA22				uə				
+								
+								
+一			1				U+4E00 [CJK Unified Ideograph]	
+二			2				U+4E8C [CJK Unified Ideograph]	
+三			3				U+4E09 [CJK Unified Ideograph]	
+四			4				U+56DB [CJK Unified Ideograph]	
+五			5				U+4E94 [CJK Unified Ideograph]	
+六			6				‎U+516D [CJK Unified Ideograph]	
+七			7				U+4E03 [CJK Unified Ideograph]	
+八			8				U+516B [CJK Unified Ideograph]	
+九			9				U+4E5D [CJK Unified Ideograph]	
+〇			0				U+3007 IDEOGRAPHIC NUMBER ZERO	punctuation
+十			{10}				U+5341 [CJK Unified Ideograph]	
+百			{100}				 ‎U+767E [CJK Unified Ideograph]	
+千			{1000}				U+5343 [CJK Unified Ideograph]	
+万			{10000}				U+4E07 [CJK Unified Ideograph]	
+
+
 
 `
 
@@ -10,11 +294,11 @@ var cols = {
 "key":2,
 "transLoc": 3,
 "ipaLoc": 4,
-"equiv": 5,
+"equiv": 0,
 "transckey": 0,
-"transcription": 6,
-"typeLoc": 7,
-"statusLoc": 8,
+"transcription": 0,
+"typeLoc": 5,
+"statusLoc": 6,
 "dvowel": 0,
 "ivowel": 0,
 "subj":0,
@@ -23,11 +307,12 @@ var cols = {
 "lc":0,
 "uc":0,
 "meaning":0,
-"shape": 9,
+"shape": 0,
 "numLoc": 0,
-"nameLoc": 10,
-"nnameLoc": 11,
-"ucsName": 12,
+"nameLoc": 0,
+"nnameLoc": 0,
+"ucsName": 7,
+"block": 8,
 
 //"othertranscriptions": [[6, 'Latin']]
 }
