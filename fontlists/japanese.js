30773f4c2f6920cc410f85c7d8a8bcd44c96d095
r12a <ishida@w3.org>
2020-05-22 06:52:54 +0100

    fontlists/japanese, Add lots of Mac fonts

--- fontlists/japanese.js
+++ fontlists/japanese.js
@@ -3,14 +3,28 @@
             <option value="Meiryo">Meiryo</option>
             <option value="MS Gothic">MS Gothic</option>
             <option value="MS Mincho">MS Mincho</option>
+            <option value="MS PGothic">MS PGothic</option>
+            <option value="MS PMincho">MS PMincho</option>
             </optgroup>
             
             <optgroup label="Mac fonts ">
             <option value="Hiragino Kaku Gothic Pro">Hiragino Kaku Gothic Pro</option>
-            <option value="Hiragino Maru Gothic Pro">Hiragino Maru Gothic Pro</option>
+            <option value="Hiragino Sans">Hiragino Sans</option>
             <option value="Osaka">Osaka</option>
+            <option value="Toppan Bunkyu Gothic">Toppan Bunkyu Gothic</option>
+            <option value="Toppan Bunkyu Midashi Gothic">Toppan Bunkyu Midashi Gothic</option>
+            <option value="YuGothic">YuGothic</option>
+
             <option value="Hiragino Mincho Pro">Hiragino Mincho Pro</option>
-          </optgroup>
+            <option value="Toppan Bunkyu Mincho">Toppan Bunkyu Mincho</option>
+            <option value="YuMincho">YuMincho</option>
+
+            <option value="Hiragino Maru Gothic Pro">Hiragino Maru Gothic Pro</option>
+            <option value="Tsukushi A Round Gothic">Tsukushi A Round Gothic</option>
+            <option value="Tsukushi B Round Gothic">Tsukushi B Round Gothic</option>
+            <option value="YuKyokasho">YuKyokasho</option>
+            <option value="YuKyokasho Yoko">YuKyokasho Yoko</option>
+            </optgroup>
             
             <optgroup label="Google fonts ">
             <option value="Noto Sans CJK JP">Noto Sans CJK JP</option>
