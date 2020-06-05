7e370f34a2694258524a91892d5bf27500373456
r12a <ishida@w3.org>
2020-06-05 09:44:38 +0100

    scriptdb/cans-ike, Reorder -ai

--- scriptdb/cans-ike.js
+++ scriptdb/cans-ike.js
@@ -1,119 +1,134 @@
 var spreadsheet = `
-ᐁ		e	aⁱ	ai	ai		syllable				U+1401: CANADIAN SYLLABICS E	cans
 ᐃ		i	i	i	i		syllable				U+1403: CANADIAN SYLLABICS I	cans
 ᐄ		I	ī	iː	ii		syllable				U+1404: CANADIAN SYLLABICS II	cans
 ᐅ		u	u	u	u		syllable				U+1405: CANADIAN SYLLABICS O	cans
 ᐆ		U	ū	uː	uu		syllable				U+1406: CANADIAN SYLLABICS OO	cans
 ᐊ		a	a	a	a		syllable				U+140A: CANADIAN SYLLABICS A	cans
 ᐋ		A	ā	aː	aa		syllable				U+140B: CANADIAN SYLLABICS AA	cans
-ᐯ		p	pᵃⁱ	pai	pai		syllable				U+142F: CANADIAN SYLLABICS PE	cans
+ᐁ		e	aⁱ	ai	ai		syllable				U+1401: CANADIAN SYLLABICS E	cans
+												
 ᐱ		p	pⁱ	pi	pi		syllable				U+1431: CANADIAN SYLLABICS PI	cans
 ᐲ		P	pⁱ̠	piː	pii		syllable				U+1432: CANADIAN SYLLABICS PII	cans
 ᐳ		p	pᵘ	pu	pu		syllable				U+1433: CANADIAN SYLLABICS PO	cans
 ᐴ		P	pᵘ̱	puː	puu		syllable				U+1434: CANADIAN SYLLABICS POO	cans
 ᐸ		p	pᵃ	pa	pa		syllable				U+1438: CANADIAN SYLLABICS PA	cans
 ᐹ		P	pᵃ̱	paː	paa		syllable				U+1439: CANADIAN SYLLABICS PAA	cans
+ᐯ		p	pᵃⁱ	pai	pai		syllable				U+142F: CANADIAN SYLLABICS PE	cans
 ᑉ		p	p	p	p		final consonant				U+1449: CANADIAN SYLLABICS P	cans
-ᑌ		t	tᵃⁱ	tai	tai		syllable				U+144C: CANADIAN SYLLABICS TE	cans
+												
 ᑎ		t	tⁱ	ti	ti		syllable				U+144E: CANADIAN SYLLABICS TI	cans
 ᑏ		T	tⁱ̠	tiː	tii		syllable				U+144F: CANADIAN SYLLABICS TII	cans
 ᑐ		t	tᵘ	tu	tu		syllable				U+1450: CANADIAN SYLLABICS TO	cans
 ᑑ		T	tᵘ̱	tuː	tuu		syllable				U+1451: CANADIAN SYLLABICS TOO	cans
 ᑕ		t	tᵃ	ta	ta		syllable				U+1455: CANADIAN SYLLABICS TA	cans
 ᑖ		T	tᵃ̱	taː	taa		syllable				U+1456: CANADIAN SYLLABICS TAA	cans
+ᑌ		t	tᵃⁱ	tai	tai		syllable				U+144C: CANADIAN SYLLABICS TE	cans
 ᑦ		t	t	t	t		final consonant				U+1466: CANADIAN SYLLABICS T	cans
-ᑫ		k	kᵃⁱ	kai	kai		syllable				U+146B: CANADIAN SYLLABICS KE	cans
+												
 ᑭ		k	kⁱ	ki	ki		syllable				U+146D: CANADIAN SYLLABICS KI	cans
 ᑮ		K	kⁱ̠	kiː	kii		syllable				U+146E: CANADIAN SYLLABICS KII	cans
 ᑯ		k	kᵘ	ku	ku		syllable				U+146F: CANADIAN SYLLABICS KO	cans
 ᑰ		K	kᵘ̱	kuː	kuu		syllable				U+1470: CANADIAN SYLLABICS KOO	cans
 ᑲ		k	kᵃ	ka	ka		syllable				U+1472: CANADIAN SYLLABICS KA	cans
 ᑳ		K	kᵃ̱	kaː	kaa		syllable				U+1473: CANADIAN SYLLABICS KAA	cans
+ᑫ		k	kᵃⁱ	kai	kai		syllable				U+146B: CANADIAN SYLLABICS KE	cans
 ᒃ		k	k	k	k		final consonant				U+1483: CANADIAN SYLLABICS K	cans
-ᒉ		g	gᵃⁱ	ɣai	gai		syllable				U+1489: CANADIAN SYLLABICS CE	cans
+												
 ᒋ		g	gⁱ	ɣi	gi		syllable				U+148B: CANADIAN SYLLABICS CI	cans
 ᒌ		G	gⁱ̠	ɣiː	gii		syllable				U+148C: CANADIAN SYLLABICS CII	cans
 ᒍ		g	gᵘ	ɣu	gu		syllable				U+148D: CANADIAN SYLLABICS CO	cans
 ᒎ		G	gᵘ̱	ɣuː	guu		syllable				U+148E: CANADIAN SYLLABICS COO	cans
 ᒐ		g	gᵃ	ɣa	ga		syllable				U+1490: CANADIAN SYLLABICS CA	cans
 ᒑ		G	gᵃ̱	ɣaː	gaa		syllable				U+1491: CANADIAN SYLLABICS CAA	cans
+ᒉ		g	gᵃⁱ	ɣai	gai		syllable				U+1489: CANADIAN SYLLABICS CE	cans
 ᒡ		g	g	ɣ	g		final consonant				U+14A1: CANADIAN SYLLABICS C	cans
-ᒣ		m	mᵃⁱ	mai	mai		syllable				U+14A3: CANADIAN SYLLABICS ME	cans
+												
 ᒥ		m	mⁱ	mi	mi		syllable				U+14A5: CANADIAN SYLLABICS MI	cans
 ᒦ		M	mⁱ̠	miː	mii		syllable				U+14A6: CANADIAN SYLLABICS MII	cans
 ᒧ		m	mᵘ	mu	mu		syllable				U+14A7: CANADIAN SYLLABICS MO	cans
 ᒨ		M	mᵘ̱	muː	muu		syllable				U+14A8: CANADIAN SYLLABICS MOO	cans
 ᒪ		m	mᵃ	ma	ma		syllable				U+14AA: CANADIAN SYLLABICS MA	cans
 ᒫ		M	mᵃ̱	maː	maa		syllable				U+14AB: CANADIAN SYLLABICS MAA	cans
+ᒣ		m	mᵃⁱ	mai	mai		syllable				U+14A3: CANADIAN SYLLABICS ME	cans
 ᒻ		m	m	m	m		final consonant				U+14BB: CANADIAN SYLLABICS M	cans
-ᓀ		n	nᵃⁱ	nai	nai		syllable				U+14C0: CANADIAN SYLLABICS NE	cans
+												
 ᓂ		n	nⁱ	ni	ni		syllable				U+14C2: CANADIAN SYLLABICS NI	cans
 ᓃ		N	nⁱ̠	niː	nii		syllable				U+14C3: CANADIAN SYLLABICS NII	cans
 ᓄ		n	nᵘ	nu	nu		syllable				U+14C4: CANADIAN SYLLABICS NO	cans
 ᓅ		N	nᵘ̱	nuː	nuu		syllable				U+14C5: CANADIAN SYLLABICS NOO	cans
 ᓇ		n	nᵃ	na	na		syllable				U+14C7: CANADIAN SYLLABICS NA	cans
 ᓈ		N	nᵃ̱	naː	naa		syllable				U+14C8: CANADIAN SYLLABICS NAA	cans
+ᓀ		n	nᵃⁱ	nai	nai		syllable				U+14C0: CANADIAN SYLLABICS NE	cans
 ᓐ		n	n	n	n		final consonant				U+14D0: CANADIAN SYLLABICS N	cans
-ᓭ		s	sᵃⁱ	sai	sai		syllable				U+14ED: CANADIAN SYLLABICS SE	cans
+												
 ᓯ		s	sⁱ	si	si		syllable				U+14EF: CANADIAN SYLLABICS SI	cans
 ᓰ		S	sⁱ̠	siː	sii		syllable				U+14F0: CANADIAN SYLLABICS SII	cans
 ᓱ		s	sᵘ	su	su		syllable				U+14F1: CANADIAN SYLLABICS SO	cans
 ᓲ		S	sᵘ̱	suː	suu		syllable				U+14F2: CANADIAN SYLLABICS SOO	cans
 ᓴ		s	sᵃ	sa	sa		syllable				U+14F4: CANADIAN SYLLABICS SA	cans
 ᓵ		S	sᵃ̱	saː	saa		syllable				U+14F5: CANADIAN SYLLABICS SAA	cans
+ᓭ		s	sᵃⁱ	sai	sai		syllable				U+14ED: CANADIAN SYLLABICS SE	cans
 ᔅ		s	s	s	s		final consonant				U+1505: CANADIAN SYLLABICS S	cans
-ᓓ		l	lᵃⁱ	lai	lai		syllable				U+14D3: CANADIAN SYLLABICS LE	cans
+												
 ᓕ		l	lⁱ	li	li		syllable				U+14D5: CANADIAN SYLLABICS LI	cans
 ᓖ		L	lⁱ̠	liː	lii		syllable				U+14D6: CANADIAN SYLLABICS LII	cans
 ᓗ		l	lᵘ	lu	lu		syllable				U+14D7: CANADIAN SYLLABICS LO	cans
 ᓘ		L	lᵘ̱	luː	luu		syllable				U+14D8: CANADIAN SYLLABICS LOO	cans
 ᓚ		l	lᵃ	la	la		syllable				U+14DA: CANADIAN SYLLABICS LA	cans
 ᓛ		L	lᵃ̱	laː	laa		syllable				U+14DB: CANADIAN SYLLABICS LAA	cans
+ᓓ		l	lᵃⁱ	lai	lai		syllable				U+14D3: CANADIAN SYLLABICS LE	cans
 ᓪ		l	l	l	l		final consonant				U+14EA: CANADIAN SYLLABICS L	cans
-ᔦ		j	jᵃⁱ	jai	jai		syllable				U+1526: CANADIAN SYLLABICS YE	cans
+												
 ᔨ		j	jⁱ	ji	ji		syllable				U+1528: CANADIAN SYLLABICS YI	cans
 ᔩ		J	jⁱ̠	jiː	jii		syllable				U+1529: CANADIAN SYLLABICS YII	cans
 ᔪ		j	jᵘ	ju	ju		syllable				U+152A: CANADIAN SYLLABICS YO	cans
 ᔫ		J	jᵘ̱	juː	juu		syllable				U+152B: CANADIAN SYLLABICS YOO	cans
 ᔭ		j	jᵃ	ja	ja		syllable				U+152D: CANADIAN SYLLABICS YA	cans
 ᔮ		J	jᵃ̱	jaː	jaa		syllable				U+152E: CANADIAN SYLLABICS YAA	cans
+ᔦ		j	jᵃⁱ	jai	jai		syllable				U+1526: CANADIAN SYLLABICS YE	cans
 ᔾ		j	j	j	j		final consonant				U+153E: CANADIAN SYLLABICS Y	cans
-ᕓ		f	fᵃⁱ	fai	fai		syllable				U+1553: CANADIAN SYLLABICS FE	cans
+												
 ᕕ		v	vⁱ	vi	vi		syllable				U+1555: CANADIAN SYLLABICS FI	cans
 ᕖ		V	vⁱ̠	viː	vii		syllable				U+1556: CANADIAN SYLLABICS FII	cans
 ᕗ		v	vᵘ	vu	vu		syllable				U+1557: CANADIAN SYLLABICS FO	cans
 ᕘ		V	vᵘ̱	vuː	vuu		syllable				U+1558: CANADIAN SYLLABICS FOO	cans
 ᕙ		v	vᵃ	va	va		syllable				U+1559: CANADIAN SYLLABICS FA	cans
 ᕚ		V	vᵃ̱	vaː	vaa		syllable				U+155A: CANADIAN SYLLABICS FAA	cans
+ᕓ		f	fᵃⁱ	fai	fai		syllable				U+1553: CANADIAN SYLLABICS FE	cans
 ᕝ		v	v	v	v		final consonant				U+155D: CANADIAN SYLLABICS F	cans
-ᕃ		r	rᵃⁱ	ʁai	rai		syllable				U+1543: CANADIAN SYLLABICS R-CREE RE	cans
+												
 ᕆ		r	rⁱ	ʁi	ri		syllable				U+1546: CANADIAN SYLLABICS RI	cans
 ᕇ		R	rⁱ̠	ʁiː	rii		syllable				U+1547: CANADIAN SYLLABICS RII	cans
 ᕈ		r	rᵘ	ʁu	ru		syllable				U+1548: CANADIAN SYLLABICS RO	cans
 ᕉ		R	rᵘ̱	ʁuː	ruu		syllable				U+1549: CANADIAN SYLLABICS ROO	cans
 ᕋ		r	rᵃ	ʁa	ra		syllable				U+154B: CANADIAN SYLLABICS RA	cans
 ᕌ		R	rᵃ̱	ʁaː	raa		syllable				U+154C: CANADIAN SYLLABICS RAA	cans
+ᕃ		r	rᵃⁱ	ʁai	rai		syllable				U+1543: CANADIAN SYLLABICS R-CREE RE	cans
 ᕐ		r	r	ʁ	r		final consonant				U+1550: CANADIAN SYLLABICS R	cans
-ᙯ		q	qᵃⁱ	qai	qai		syllable				U+166F: CANADIAN SYLLABICS QAI	cans
+												
 ᕿ		q	qⁱ	qi	qi		syllable				U+157F: CANADIAN SYLLABICS QI	cans
 ᖀ		Q	qⁱ̠	qiː	qii		syllable				U+1580: CANADIAN SYLLABICS QII	cans
 ᖁ		q	qᵘ	qu	qu		syllable				U+1581: CANADIAN SYLLABICS QO	cans
 ᖂ		Q	qᵘ̱	quː	quu		syllable				U+1582: CANADIAN SYLLABICS QOO	cans
 ᖃ		q	qᵃ	qa	qa		syllable				U+1583: CANADIAN SYLLABICS QA	cans
 ᖄ		Q	qᵃ̱	qaː	qaa		syllable				U+1584: CANADIAN SYLLABICS QAA	cans
+ᙯ		q	qᵃⁱ	qai	qai		syllable				U+166F: CANADIAN SYLLABICS QAI	cans
 ᖅ		q	q	q	q		final consonant				U+1585: CANADIAN SYLLABICS Q	cans
-ᙰ		b	ŋᵃⁱ	ŋai	ngai		syllable				U+1670: CANADIAN SYLLABICS NGAI	cans
+												
 ᖏ		b	ŋⁱ	ŋi	ngi		syllable				U+158F: CANADIAN SYLLABICS NGI	cans
 ᖐ		B	ŋⁱ̠	ŋii	ngiː		syllable				U+1590: CANADIAN SYLLABICS NGII	cans
 ᖑ		b	ŋᵘ	ŋu	ngu		syllable				U+1591: CANADIAN SYLLABICS NGO	cans
 ᖒ		B	ŋᵘ̱	ŋuu	nguː		syllable				U+1592: CANADIAN SYLLABICS NGOO	cans
 ᖓ		b	ŋᵃ	ŋa	nga		syllable				U+1593: CANADIAN SYLLABICS NGA	cans
 ᖔ		B	ŋᵃ̱	ŋaa	ngaː		syllable				U+1594: CANADIAN SYLLABICS NGAA	cans
+ᙰ		b	ŋᵃⁱ	ŋai	ngai		syllable				U+1670: CANADIAN SYLLABICS NGAI	cans
 ᖕ		b	ŋ	ŋ	ng		final consonant				U+1595: CANADIAN SYLLABICS NG	cans
-ᙱ		b	ŋᵑⁱ	ŋi	nngi		syllable				U+1671: CANADIAN SYLLABICS NNGI	cans
-ᙳ		b	ŋᵑᵘ	ŋu	nngu		syllable				U+1673: CANADIAN SYLLABICS NNGO	cans
-ᙵ		b	ŋᵑᵃ	ŋa	nnga		syllable				U+1675: CANADIAN SYLLABICS NNGA	cans
+												
+ᙱ		b	ŋᵑⁱ	ŋŋi	nngi		syllable				U+1671: CANADIAN SYLLABICS NNGI	cans
+ᙳ		b	ŋᵑᵘ	ŋŋu	nngu		syllable				U+1673: CANADIAN SYLLABICS NNGO	cans
+ᙵ		b	ŋᵑᵃ	ŋŋa	nnga		syllable				U+1675: CANADIAN SYLLABICS NNGA	cans
 ᖖ		b	ŋᵑ	ŋŋ	nng		final consonant				U+1596: CANADIAN SYLLABICS NNG	cans
+												
 ᖠ		z	ɫⁱ	ɬi	ɫi		syllable				U+15A0: CANADIAN SYLLABICS LHI	cans
 ᖡ		Z	ɫⁱ̠	ɬiː	ɫii		syllable				U+15A1: CANADIAN SYLLABICS LHII	cans
 ᖢ		z	ɫᵘ	ɬu	ɫu		syllable				U+15A2: CANADIAN SYLLABICS LHO	cans
@@ -121,10 +136,11 @@ var spreadsheet = `
 ᖤ		z	ɫᵃ	ɬa	ɫa		syllable				U+15A4: CANADIAN SYLLABICS LHA	cans
 ᖥ		Z	ɫᵃ̱	ɬaː	ɫaa		syllable				U+15A5: CANADIAN SYLLABICS LHAA	cans
 ᖦ		z	ɫ	ɬ	ɫ		syllable				U+15A6: CANADIAN SYLLABICS LH	cans
+												
 ᐦ		h	h	h	h		final consonant				U+1426: CANADIAN SYLLABICS FINAL DOUBLE SHORT VERTICAL STROKES	cans
 ᕼ		H	ʜ	h	h		extended consonant				U+157C: CANADIAN SYLLABICS NUNAVUT H	cans
 ᖯ		B	b	b	b		extended consonant				U+15AF: CANADIAN SYLLABICS AIVILIK B	cans
-
+'		'	ʔ	ʔ	ʔ		extended consonant				U+0027: APOSTROPHE	
 
 
 `
