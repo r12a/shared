b2d1e7227230b6c8ff2c66c44ff0a6af57c14d83
r12a <ishida@w3.org>
2020-06-15 19:44:39 +0100

    webfonts, Add Noto Sans Adlam New

--- fontlists/adlm.js
+++ fontlists/adlm.js
@@ -1,5 +1,6 @@
  var fontSelection = `<option value="0">Select...</option>
             <optgroup label="Web fonts ">
+            <option value="Noto Sans Adlam New WF">Noto Sans Adlam New WF</option>
             <option value="Noto Sans Adlam WF">Noto Sans Adlam WF</option>
             <option value="Noto Sans Adlam Unjoined WF">Noto Sans Adlam Unjoined WF</option>
             </optgroup>
@@ -9,6 +10,7 @@
             </optgroup>
             
             <optgroup label="Google fonts ">
+            <option value="Noto Sans Adlam New">Noto Sans Adlam New</option>
             <option value="Noto Sans Adlam">Noto Sans Adlam</option>
             <option value="Noto Sans Adlam Unjoined">Noto Sans Adlam Unjoined</option>
             </optgroup>
