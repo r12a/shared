6d89e38f359348a10e6fb7cafe5c3a79151af3a3
r12a <ishida@w3.org>
2020-06-30 21:38:32 +0100

    Preliminary osage files

---
+++ fontlists/osge.js
@@ -0,0 +1,25 @@
+ var fontSelection = `<option value="0">Select...</option>
+             <optgroup label="Web font">
+            <option value="Noto Sans Osage WF">Noto Sans Osage WF</option>
+            </optgroup>
+            
+            <optgroup label="Google fonts ">
+            <option value="Noto Sans Osage">Noto Sans Osage</option>
+            </optgroup>
+            
+            <optgroup label="Other fonts ">
+            <option value="Avant">Avant</option>
+            <option value="Barnsdall">Barnsdall</option>
+            <option value="Nelagoney">Nelagoney</option>
+            <option value="Pawhuska">Pawhuska</option>
+            <option value="Prue">Prue</option>
+            <option value="Wazhazhe">Wazhazhe</option>
+            <option value="Wynona">Wynona</option>
+            <option value="GadugiBeta2">GadugiBeta2</option>
+            </optgroup>
+            
+            <optgroup label="General fonts ">
+            <option value="sans-serif">sans-serif</option>
+            <option value="serif">serif</option>
+            </optgroup>
+            `
---
+++ scriptdb/osge.js
@@ -0,0 +1,150 @@
+
+var spreadsheet = `
+𐓣		i	i	i	i					𐒻			U+104E3: OSAGE SMALL LETTER I	osage
+𐓶		u	u	u	ʉ y					𐓎			U+104F6: OSAGE SMALL LETTER U	osage
+𐓟		e	e	ɛ	ε e					𐒷			U+104DF: OSAGE SMALL LETTER E	osage
+𐓪		o	o	o	o					𐓂			U+104EA: OSAGE SMALL LETTER O	osage
+𐓛		ə	e	ə	ə					𐒳			U+104DB: OSAGE SMALL LETTER AH	osage
+𐓘		a	a	a	ɑ ə					𐒰			U+104D8: OSAGE SMALL LETTER A	osage
+𐓙		ai	a	aᶦ	aɪ					𐒱			U+104D9: OSAGE SMALL LETTER AI	osage
+𐓚		aį	a	aⁱ̃	aɪ̃					𐒲			U+104DA: OSAGE SMALL LETTER AIN	osage
+𐓠		eį	e	eⁱ̃	eɪ̃					𐒸			U+104E0: OSAGE SMALL LETTER EIN	osage
+𐓫		oį	o	oⁱ̃	oɪ̃					𐓃			U+104EB: OSAGE SMALL LETTER OIN	osage
+														
+𐒻		I	I	I	i				𐓣				U+104BB: OSAGE CAPITAL LETTER I	osage
+𐓎		U	U	U	ʉ y				𐓶				U+104CE: OSAGE CAPITAL LETTER U	osage
+𐒷		E	E	Ɛ	ε e				𐓟				U+104B7: OSAGE CAPITAL LETTER E	osage
+𐓂		O	O	O	o				𐓪				U+104C2: OSAGE CAPITAL LETTER O	osage
+𐒳		Ə	E	Ə	ə				𐓛				U+104B3: OSAGE CAPITAL LETTER AH	osage
+𐒰		A	A	A	ɑ ə				𐓘				U+104B0: OSAGE CAPITAL LETTER A	osage
+𐒱		Ai	A	Aᶦ	aɪ				𐓙				U+104B1: OSAGE CAPITAL LETTER AI	osage
+𐒲		Aį	A	Aⁱ̃	aɪ̃				𐓚				U+104B2: OSAGE CAPITAL LETTER AIN	osage
+𐒸		Eį	E	Eⁱ̃	eɪ̃				𐓠				U+104B8: OSAGE CAPITAL LETTER EIN	osage
+𐓃		Oį	O	Oⁱ̃	oɪ̃				𐓫				U+104C3: OSAGE CAPITAL LETTER OIN	osage
+														
+														
+														
+͘		̨	[	˜			nasalised vowel						U+0358: COMBINING DOT ABOVE RIGHT​	osage
+́		́	^	´			high tone						U+0301: COMBINING ACUTE ACCENT​	osage
+̄		̄	:	ː			long vowel						U+0304: COMBINING MACRON​	osage
+̋		̋	:	˝			long high tone vowel						U+030B: COMBINING DOUBLE ACUTE ACCENT​	osage
+														
+ʼ		ˀ	]	ˀ	ˀ		ejective indicator						U+02BC: MODIFIER LETTER APOSTROPHE	osage
+														
+𐓬		p	p	p	p					𐓄			U+104EC: OSAGE SMALL LETTER PA	osage
+𐓭		hp	p	pᶣ	ʰp pp					𐓅			U+104ED: OSAGE SMALL LETTER EHPA	osage
+𐓜		br	b	bʳ	bɹ					𐒴			U+104DC: OSAGE SMALL LETTER BRA	osage
+𐓰		t	t	t	t					𐓈			U+104F0: OSAGE SMALL LETTER TA	osage
+𐓱		ht	t	tᶣ	ʰt tt					𐓉			U+104F1: OSAGE SMALL LETTER EHTA	osage
+𐓤		k	k	k	k					𐒼			U+104E4: OSAGE SMALL LETTER KA	osage
+𐓥		hk	k	kᶣ	ʰk kk					𐒽			U+104E5: OSAGE SMALL LETTER EHKA	osage
+𐓦		ky	k	kʲ	kʲ					𐒾			U+104E6: OSAGE SMALL LETTER KYA	osage
+														
+𐓲		c	c	ʦ	t͡s					𐓊			U+104F2: OSAGE SMALL LETTER TSA	osage
+𐓴		ch	c	ʦʰ	t͡sʰ					𐓌			U+104F4: OSAGE SMALL LETTER TSHA	osage
+𐓳		hc	c	ʦᶣ	ʰt͡s					𐓋			U+104F3: OSAGE SMALL LETTER EHTSA	osage
+𐓝		č	c	ʧ	t͡ʃ					𐒵			U+104DD: OSAGE SMALL LETTER CHA	osage
+𐓞		hč	c	ʧᶣ	ʰt͡ʃ					𐒶			U+104DE: OSAGE SMALL LETTER EHCHA	osage
+														
+𐓵		ð	d	ð	ð					𐓍			U+104F5: OSAGE SMALL LETTER DHA	osage
+𐓮		s	s	s	s					𐓆			U+104EE: OSAGE SMALL LETTER SA	osage
+𐓺		z	z	z	z					𐓒			U+104FA: OSAGE SMALL LETTER ZA	osage
+𐓯		š	s	ʃ	ʃ					𐓇			U+104EF: OSAGE SMALL LETTER SHA	osage
+𐓻		ž	z	ʒ	ʒ					𐓓			U+104FB: OSAGE SMALL LETTER ZHA	osage
+𐓸		x	x	x	x					𐓐			U+104F8: OSAGE SMALL LETTER KHA	osage
+𐓹		ɣ	y	ɣ	ɣ					𐓑			U+104F9: OSAGE SMALL LETTER GHA	osage
+𐓡		h	h	h	h					𐒹			U+104E1: OSAGE SMALL LETTER HA	osage
+𐓢		hy	h	hʲ	hʲ					𐒺			U+104E2: OSAGE SMALL LETTER HYA	osage
+														
+𐓨		m	m	m	m					𐓀			U+104E8: OSAGE SMALL LETTER MA	osage
+𐓩		n	n	n	n					𐓁			U+104E9: OSAGE SMALL LETTER NA	osage
+														
+𐓷		w	w	w	w					𐓏			U+104F7: OSAGE SMALL LETTER WA	osage
+𐓧		l	l	l	l					𐒿			U+104E7: OSAGE SMALL LETTER LA	osage
+														
+														
+𐓄		P	P	P	p				𐓬				U+104C4: OSAGE CAPITAL LETTER PA	osage
+𐓅		Hp	P	Pᶣ	ʰp pp				𐓭				U+104C5: OSAGE CAPITAL LETTER EHPA	osage
+𐒴		Br	B	Bʳ	bɹ				𐓜				U+104B4: OSAGE CAPITAL LETTER BRA	osage
+𐓈		T	T	T	t				𐓰				U+104C8: OSAGE CAPITAL LETTER TA	osage
+𐓉		Ht	T	Tᶣ	ʰt tt				𐓱				U+104C9: OSAGE CAPITAL LETTER EHTA	osage
+𐒼		K	K	K	k				𐓤				U+104BC: OSAGE CAPITAL LETTER KA	osage
+𐒽		Hk	K	Kᶣ	ʰk kk				𐓥				U+104BD: OSAGE CAPITAL LETTER EHKA	osage
+𐒾		Ky	K	Kʲ	kʲ				𐓦				U+104BE: OSAGE CAPITAL LETTER KYA	osage
+														
+𐓊		C	C	C	t͡s				𐓲				U+104CA: OSAGE CAPITAL LETTER TSA	osage
+𐓌		Ch	C	Cʰ	t͡sʰ				𐓴				U+104CC: OSAGE CAPITAL LETTER TSHA	osage
+𐓋		Hc	C	Cᶣ	ʰt͡s				𐓳				U+104CB: OSAGE CAPITAL LETTER EHTSA	osage
+𐒵		Č	C	ʨ	t͡ʃ				𐓝				U+104B5: OSAGE CAPITAL LETTER CHA	osage
+𐒶		Hč	C	ʨᶣ	ʰt͡ʃ				𐓞				U+104B6: OSAGE CAPITAL LETTER EHCHA	osage
+														
+𐓍		Ð	D	Ð	ð				𐓵				U+104CD: OSAGE CAPITAL LETTER DHA	osage
+𐓆		S	S	S	s				𐓮				U+104C6: OSAGE CAPITAL LETTER SA	osage
+𐓒		Z	Z	Z	z				𐓺				U+104D2: OSAGE CAPITAL LETTER ZA	osage
+𐓇		Š	S	Ʃ	ʃ				𐓯				U+104C7: OSAGE CAPITAL LETTER SHA	osage
+𐓓		Ž	Z	Ʒ	ʒ				𐓻				U+104D3: OSAGE CAPITAL LETTER ZHA	osage
+𐓐		X	X	X	x				𐓸				U+104D0: OSAGE CAPITAL LETTER KHA	osage
+𐓑		Ɣ	Y	Ɣ	ɣ				𐓹				U+104D1: OSAGE CAPITAL LETTER GHA	osage
+𐒹		H	H	H	h				𐓡				U+104B9: OSAGE CAPITAL LETTER HA	osage
+𐒺		Hy	H	Hʲ	hʲ				𐓢				U+104BA: OSAGE CAPITAL LETTER HYA	osage
+														
+𐓀		M	M	M	m				𐓨				U+104C0: OSAGE CAPITAL LETTER MA	osage
+𐓁		N	N	N	n				𐓩				U+104C1: OSAGE CAPITAL LETTER NA	osage
+														
+𐓏		W	W	W	w				𐓷				U+104CF: OSAGE CAPITAL LETTER WA	osage
+𐒿		L	L	L	l				𐓧				U+104BF: OSAGE CAPITAL LETTER LA	osage
+														
+														
+														
+														
+														
+														
+𐓬ʼ	s	pˀ		pˀ	pˀ								U+104EC U+02BC: OSAGE SMALL LETTER PA, MODIFIER LETTER APOSTROPHE	
+𐓰ʼ	s	dˀ		dˀ	dˀ								U+104F0 U+02BC: OSAGE SMALL LETTER TA, MODIFIER LETTER APOSTROPHE	
+𐓲ʼ	s	cˀ		ʦˀ	tsˀ								U+104F0 U+02BC: OSAGE SMALL LETTER TA, MODIFIER LETTER APOSTROPHE	
+𐓤ʼ	s	kˀ		kˀ	kˀ								U+104E4 U+02BC: OSAGE SMALL LETTER KA, MODIFIER LETTER APOSTROPHE	
+														
+														
+𐓬𐓸	s	px		px	px								U+104EC U+104F8: OSAGE SMALL LETTER PA, OSAGE SMALL LETTER KHA	
+𐓬𐓯	s	pš		pʃ	pʃ								U+104EC U+104EF: OSAGE SMALL LETTER PA, OSAGE SMALL LETTER SHA	
+𐓰𐓸	s	tx		tx	tx								U+104F0 U+104F8: OSAGE SMALL LETTER TA, OSAGE SMALL LETTER KHA	
+𐓤𐓸	s	kx		kx	kx								U+104E4 U+104F8: OSAGE SMALL LETTER KA, OSAGE SMALL LETTER KHA	
+𐓤𐓯	s	kš		kʃ	pʃ								U+104E4 U+104EF: OSAGE SMALL LETTER KA, OSAGE SMALL LETTER SHA	
+														
+𐓣͘	s	į		i˜	ĩ								U+104E3 U+0358: OSAGE SMALL LETTER I, COMBINING DOT ABOVE RIGHT	
+𐓛͘	s	ę		ə˜	ə̃								U+104DB U+0358: OSAGE SMALL LETTER AH, COMBINING DOT ABOVE RIGHT	
+𐓘͘	s	ą		a˜	ã								U+104D8 U+0358: OSAGE SMALL LETTER A, COMBINING DOT ABOVE RIGHT	
+`
+
+
+
+var cols = {
+"class": 1,
+"transcription":2,
+"key":3,
+"transLoc": 4,
+"ipaLoc": 5,
+"equiv": 0,
+"typeLoc": 7,
+"statusLoc": 8,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":9,
+"uc":10,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nameLoc": 11,
+"nnameLoc": 12,
+"ucsName": 13,
+"block": 14,
+
+"othertranscriptions": [[2, 'Latin']]
+}
+
+
+
+
