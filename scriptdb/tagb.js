8249f9174231da5d73c6e623c45b08ace740786b
r12a <ishida@w3.org>
2020-08-06 08:26:04 +0100

    filippino scriptdb

---
+++ scriptdb/tagb.js
@@ -0,0 +1,64 @@
+var spreadsheet = `
+ᝣ		k	k	k	ka		syllable				U+1763 TAGBANWA LETTER KA
+ᝤ		g	g	g	ga		syllable				‎U+1764 TAGBANWA LETTER GA
+ᝥ		N	ng	ŋ	ŋa		syllable				‎U+1765 TAGBANWA LETTER NGA
+ᝦ		t	t	t	ta		syllable				‎U+1766 TAGBANWA LETTER TA
+ᝧ		d	d	d	da		syllable				‎U+1767 TAGBANWA LETTER DA
+ᝨ		n	n	n	na		syllable				‎U+1768 TAGBANWA LETTER NA
+ᝩ		p	p	p	pa		syllable				‎U+1769 TAGBANWA LETTER PA
+ᝪ		b	b	b	ba		syllable				‎U+176A TAGBANWA LETTER BA
+ᝫ		m	m	m	ma		syllable				‎U+176B TAGBANWA LETTER MA
+ᝬ		y	y	y	ya		syllable				‎U+176C TAGBANWA LETTER YA
+ᝮ		l	l	l	la		syllable				‎U+176E TAGBANWA LETTER LA
+ᝯ		w	w	w	wa		syllable				‎U+176F TAGBANWA LETTER WA
+ᝰ		s	s	s	sa		syllable				‎U+1770 TAGBANWA LETTER SA
+											
+											
+ᝠ		A	a	ạ	a		independent vowel				U+1760 TAGBANWA LETTER A
+ᝡ		I	i	ị	i		independent vowel				‎U+1761 TAGBANWA LETTER I
+ᝢ		U	u	ụ	u		independent vowel				‎U+1762 TAGBANWA LETTER U
+											
+											
+\u1772		i	i	i	i		dependent vowel				‎U+1772 TAGBANWA VOWEL SIGN I
+\u1773		u	u	u	u		dependent vowel				‎U+1773 TAGBANWA VOWEL SIGN U
+											
+											
+											
+᜵		,	,	,			comma				U+1735 PHILIPPINE SINGLE PUNCTUATION
+᜶		.	.	.			period				U+1736 PHILIPPINE DOUBLE PUNCTUATION
+
+`
+
+
+
+var cols = {
+"class": 1,
+"key":2,
+"transcription":3,
+"transLoc": 4,
+"ipaLoc": 5,
+"transckey": 0,
+"equiv": 0,
+"typeLoc": 7,
+"statusLoc": 8,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nameLoc": 0,
+"nnameLoc": 0,
+"ucsName": 11,
+"block": 12,
+
+//"othertranscriptions": [[6, 'ISO 9985']]
+}
+
+
+
+
