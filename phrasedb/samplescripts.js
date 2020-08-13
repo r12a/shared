d85a2b205f303de6c65546c74ab071040dcfa02d
r12a <ishida@w3.org>
2020-08-13 07:29:57 +0100

    + Bamum

--- phrasedb/samplescripts.js
+++ phrasedb/samplescripts.js
@@ -4,6 +4,7 @@ var sampleScriptsIndex = {
     'arab': { name: "Arabic", langs:['arb','ha-arab','ks-arab', 'fa', 'ur']}, 
     'armn': { name: "Armenian", langs:['hy']},
     'bali': { name: "Balinese", langs:['ban']}, 
+    'bamu': { name: "Bamum", langs:['bax']}, 
     'bass': { name: "Bassa Vah", langs:['bsq']}, 
     'beng': { name: "Bengali", langs:['bn']},
     'bugi': { name: "Buginese", langs:['bug']},
