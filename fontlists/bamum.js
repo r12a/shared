ca4230f499f8f712d0c5916add6a15d31c4560f9
r12a <ishida@w3.org>
2020-08-14 13:55:17 +0100

    + bamum fontlist

---
+++ fontlists/bamum.js
@@ -0,0 +1,8 @@
+ var fontSelection = `<option value="0">Select...</option>
+             <optgroup label="Web fonts">
+            <option value="Noto Sans Bamu WF">Noto Sans Bamu WF</option>
+            </optgroup>
+
+            <optgroup label="Other fonts ">
+            <option value="Noto Sans Bamu">Noto Sans Bamu</option>
+            </optgroup>`
