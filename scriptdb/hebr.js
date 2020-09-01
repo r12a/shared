52f04391f20af2a4ef07b260cf9ea82777fc0b3b
r12a <ishida@w3.org>
2020-09-01 08:18:10 +0100

    Various

--- scriptdb/hebr.js
+++ scriptdb/hebr.js
@@ -1,49 +1,50 @@
 var spreadsheet = `
 א		}	'  ∅	ʔ	ʔ ∅	consonant		1	ˈalɛf	אָלֶף	U+05D0: HEBREW LETTER ALEF	hebrew
 												
-פּ	s	p	p	f̣	p				pe, pej	פֵּא, פה	U+05E4 U+05BC: HEBREW LETTER PE, POINT DAGESH OR MAPIQ	
-בּ	s	b	b	vˑ	b				bet	בֵּית	U+05D1 U+05BC: HEBREW LETTER BET, POINT DAGESH OR MAPIQ	
+פּ	s	p	p	f̣	p	consonant			pe, pej	פֵּא, פה	U+05E4 U+05BC: HEBREW LETTER PE, POINT DAGESH OR MAPIQ	
+בּ	s	b	b	vˑ	b	consonant			bet	בֵּית	U+05D1 U+05BC: HEBREW LETTER BET, POINT DAGESH OR MAPIQ	
 ט		t	t	t	t	consonant		9	tet	טֵית	U+05D8: HEBREW LETTER TET	hebrew
 ת		t	t	ṫ	t	consonant		400	θav θaf	תו	U+05EA: HEBREW LETTER TAV	hebrew
-תּ	s	t	t	ṫˑ	t				tav, taf	תּו	U+05EA U+05BC: HEBREW LETTER TAV, POINT DAGESH OR MAPIQ	
+תּ	s	t	t	ṫˑ	t	consonant			tav, taf	תּו	U+05EA U+05BC: HEBREW LETTER TAV, POINT DAGESH OR MAPIQ	
 ד		d	d	d	d	consonant		4	ˈdalɛt, ˈdalɛd	דָּלֶת	U+05D3: HEBREW LETTER DALET	hebrew
+דּ	s	d	d	dˑ	d	consonant					U+05D3: HEBREW LETTER DALET, POINT DAGESH OR MAPIQ	
 ק		k	k	k	k	consonant		100	kuf, kof	קוֹף	U+05E7: HEBREW LETTER QOF	hebrew
-כּ	s	k	k	ẋˑ	k				kaf	כַּף	U+05DB U+05BC: HEBREW LETTER KAF, POINT DAGESH OR MAPIQ	
-ךּ	s	k	k	x̽ˑ	k				kaf sofit	כַּף סוֹפִית	U+05DA U+05BC: HEBREW LETTER FINAL KAF, POINT DAGESH OR MAPIQ	
+כּ	s	k	k	ẋˑ	k	consonant			kaf	כַּף	U+05DB U+05BC: HEBREW LETTER KAF, POINT DAGESH OR MAPIQ	
+ךּ	s	k	k	x̽ˑ	k	consonant			kaf sofit	כַּף סוֹפִית	U+05DA U+05BC: HEBREW LETTER FINAL KAF, POINT DAGESH OR MAPIQ	
 ג		g	g	g	ɡ	consonant		3	gimel	גִּימֵל	U+05D2: HEBREW LETTER GIMEL	hebrew
-גּ	s	g	g	gˑ	ɡ						U+05D2 U+05BC: HEBREW LETTER GIMEL, POINT DAGESH OR MAPIQ	
+גּ	s	g	g	gˑ	ɡ	consonant					U+05D2 U+05BC: HEBREW LETTER GIMEL, POINT DAGESH OR MAPIQ	
 ע		}	'	ʔ̇	ʔ ∅	consonant		70	ajin, a.in	עַיִן	U+05E2: HEBREW LETTER AYIN	hebrew
 												
-צ		t	ts	ʦ	ʦ	consonant		90	ʦadi	צַדִי, צדיק	U+05E6: HEBREW LETTER TSADI	hebrew
-ץ		t	ts	ʦ̽	ʦ	word-final consonant variant			ʦadi sofit	צַדִי סוֹפִית, צדיק סופית	U+05E5: HEBREW LETTER FINAL TSADI	hebrew
-צ׳	s	t	č	ʦ´	ʧ						U+05E6 U+05F3: HEBREW LETTER TSADI, PUNCTUATION GERESH	
-ג׳	s	d	ǧ	g´	ʤ						U+05D2 U+05F3: HEBREW LETTER GIMEL, PUNCTUATION GERESH	
+צ		t	ts	ʦ	t͡s	consonant		90	ʦadi	צַדִי, צדיק	U+05E6: HEBREW LETTER TSADI	hebrew
+ץ		T	ts	ʦ̽	t͡s	word-final consonant variant			ʦadi sofit	צַדִי סוֹפִית, צדיק סופית	U+05E5: HEBREW LETTER FINAL TSADI	hebrew
+צ׳	s	t	č	ʦ´	t͡ʃ	consonant	non-native				U+05E6 U+05F3: HEBREW LETTER TSADI, PUNCTUATION GERESH	
+ג׳	s	d	ǧ	g´	d͡ʒ	consonant	non-native				U+05D2 U+05F3: HEBREW LETTER GIMEL, PUNCTUATION GERESH	
 												
 פ		f	f	f	f	consonant		80	fe, fej	פֵא, פה	U+05E4: HEBREW LETTER PE	hebrew
-ף		f	f	f̽	f	word-final consonant variant			fe/fej sofit	פֵּא סוֹפִית, פה סופית	U+05E3: HEBREW LETTER FINAL PE	hebrew
-ב		v	v	v	v	consonant		2	vet	בֵית	U+05D1: HEBREW LETTER BET	hebrew
+ף		F	f	f̽	f	word-final consonant variant			fe/fej sofit	פֵּא סוֹפִית, פה סופית	U+05E3: HEBREW LETTER FINAL PE	hebrew
 ו		v	v	v̇	v w	consonant		6	vav	וָו	U+05D5: HEBREW LETTER VAV	hebrew
-ת׳	s	Q		ṫ´	θ	foreign sound				arabic ﺙ	U+05EA U+05F3: HEBREW LETTER TAV, PUNCTUATION GERESH	
-ד׳	s	d		d´	ð	foreign sound				arabic ذ	U+05D3 U+05F3: HEBREW LETTER DALET, PUNCTUATION GERESH	
+ב		v	v	v	v	consonant		2	vet	בֵית	U+05D1: HEBREW LETTER BET	hebrew
+ת׳	s	Q		ṫ´	θ	foreign sound	non-native			arabic ﺙ	U+05EA U+05F3: HEBREW LETTER TAV, PUNCTUATION GERESH	
+ד׳	s	d		d´	ð	foreign sound	non-native			arabic ذ	U+05D3 U+05F3: HEBREW LETTER DALET, PUNCTUATION GERESH	
 ש		s	s sh	ṡ	s ʃ	consonant		300			U+05E9: HEBREW LETTER SHIN	hebrew
-שׂ	s	s	s	ṡ̍	s				sin	שִׂין	U+05E9 U+05C2: HEBREW LETTER SHIN, POINT SIN DOT	
+שׂ	s	s	s	ṡˢ	s	consonant			sin	שִׂין	U+05E9 U+05C2: HEBREW LETTER SHIN, POINT SIN DOT	
 ס		s	s	s	s	consonant		60	ˈsamɛχ	סָמֶך	U+05E1: HEBREW LETTER SAMEKH	hebrew
 ז		z	z	z	z	consonant		7	zajin, za.in	זַיִן	U+05D6: HEBREW LETTER ZAYIN	hebrew
-שׁ	s	s	sh	ṡ̎	ʃ				ʃin	שִׁין	U+05E9 U+05C1: HEBREW LETTER SHIN, POINT SHIN DOT	
-ז׳	s	z	ž	z´	ʒ						U+05D6 U+05F3: HEBREW LETTER ZAYIN, PUNCTUATION GERESH	
-ח		x	ẖ	x	x χ	consonant		8	χet	חֵית	U+05D7: HEBREW LETTER HET	hebrew
-כ		x	kh	ẋ	x χ	consonant		20	χaf	כַף	U+05DB: HEBREW LETTER KAF	hebrew
-ך		x	kh	x̽	x χ	word-final consonant variant			χaf sofit	כַף סוֹפִית	U+05DA: HEBREW LETTER FINAL KAF	hebrew
-ר		r	r	ʁ	ɣ ʁ	consonant		200	ʁeʃ	רֵישׁ	U+05E8: HEBREW LETTER RESH	hebrew
-ח׳	s	x		x´	χ	foreign sound				arabic خ	U+05D7 U+05F3: HEBREW LETTER HET, PUNCTUATION GERESH	
-ר׳	s	r		ʁ´	ʁ	foreign sound				arabic غ	U+05E8 U+05F3: HEBREW LETTER RESH, PUNCTUATION GERESH	
-ע׳	s	r		ʔ̇´	ʁ	foreign sound				same	U+05E2 U+05F3: HEBREW LETTER AYIN, PUNCTUATION GERESH	
-ה		h	h	h	h ʔ ∅	consonant		5	he, hej	הֵא	U+05D4: HEBREW LETTER HE	hebrew
+שׁ	s	s	sh	ṡᶴ	ʃ	consonant			ʃin	שִׁין	U+05E9 U+05C1: HEBREW LETTER SHIN, POINT SHIN DOT	
+ז׳	s	z	ž	z´	ʒ	consonant	non-native				U+05D6 U+05F3: HEBREW LETTER ZAYIN, PUNCTUATION GERESH	
+ח		x	ẖ	x	x	consonant		8	χet	חֵית	U+05D7: HEBREW LETTER HET	hebrew
+כ		x	kh	ẋ	x	consonant		20	χaf	כַף	U+05DB: HEBREW LETTER KAF	hebrew
+ך		X	kh	x̽	x	word-final consonant variant			χaf sofit	כַף סוֹפִית	U+05DA: HEBREW LETTER FINAL KAF	hebrew
+ר		r	r	ʁ	ʁ	consonant		200	ʁeʃ	רֵישׁ	U+05E8: HEBREW LETTER RESH	hebrew
+ח׳	s	x		x´	χ	consonant	non-native			arabic خ	U+05D7 U+05F3: HEBREW LETTER HET, PUNCTUATION GERESH	
+ר׳	s	r		ʁ´	ʁ	consonant	non-native			arabic غ	U+05E8 U+05F3: HEBREW LETTER RESH, PUNCTUATION GERESH	
+ע׳	s	r		ʔ̇´	ʁ	consonant	non-native			same	U+05E2 U+05F3: HEBREW LETTER AYIN, PUNCTUATION GERESH	
+ה		h	h	h	h ∅	consonant		5	he, hej	הֵא	U+05D4: HEBREW LETTER HE	hebrew
 												
 מ		m	m	m	m	consonant		40	mem	מֵם	U+05DE: HEBREW LETTER MEM	hebrew
-ם		m	m	m̽	m	word-final consonant variant			mem sofit	מֵם סוֹפִית	U+05DD: HEBREW LETTER FINAL MEM	hebrew
+ם		M	m	m̽	m	word-final consonant variant			mem sofit	מֵם סוֹפִית	U+05DD: HEBREW LETTER FINAL MEM	hebrew
 נ		n	n	n	n	consonant		50	nun	נוּן	U+05E0: HEBREW LETTER NUN	hebrew
-ן		n	n	n̽	n	word-final consonant variant			nun sofit	נוּן סוֹפִית	U+05DF: HEBREW LETTER FINAL NUN	hebrew
+ן		N	n	n̽	n	word-final consonant variant			nun sofit	נוּן סוֹפִית	U+05DF: HEBREW LETTER FINAL NUN	hebrew
 												
 װ		w		w	w	consonant					U+05F0: HEBREW LIGATURE YIDDISH DOUBLE VAV	hebrew
 ו׳	s	w	w	v̇´	w						U+05D5 U+05F3: HEBREW LETTER VAV, PUNCTUATION GERESH	
@@ -53,14 +54,14 @@ var spreadsheet = `
 												
 												
 												
+וּ	s	u		ṿ̇	u						U+05D5 U+05BC: HEBREW LETTER VAV, POINT DAGESH OR MAPIQ	
 ִ		i		i	i	vowel point			χiˈʁik	חִירִיק‬	U+05B4: HEBREW POINT HIRIQ​	hebrew
-וּ	s			ṿ̇	u						U+05D5 U+05BC: HEBREW LETTER VAV, POINT DAGESH OR MAPIQ	
 ֻ		u		u	u	vowel point			kuˈbuts	קֻבּוּץ‬	U+05BB: HEBREW POINT QUBUTS​	hebrew
 ְ		:		ᵊ	e ∅	epenthetic vowel/no vowel			ʃvɑ	שְׁוָא	U+05B0: HEBREW POINT SHEVA​	hebrew
 ֵ		e		ē	e ej	vowel point			tseˈɾe	צֵירֵי‬	U+05B5: HEBREW POINT TSERE​	hebrew
 ֶ		e		e	e	vowel point			sɛˈɡol	סֶגּוֹל	U+05B6: HEBREW POINT SEGOL​	hebrew
 ֱ		e		ĕ	e	vowel point			ħaˈtaf sɛˈɡol	חֲטַף סֶגּוֹל‬	U+05B1: HEBREW POINT HATAF SEGOL​	hebrew
-וֹ	s			v̇o	o						U+05D5 U+05B9: HEBREW LETTER VAV, POINT HOLAM	
+וֹ	s	o		v̇o	o						U+05D5 U+05B9: HEBREW LETTER VAV, POINT HOLAM	
 ֹ		o		o	o	vowel point			ħoˈlam	חוֹלָם‬	U+05B9: HEBREW POINT HOLAM​	hebrew
 ֺ		o		ȯ	o	vowel point			ħoˈlam ħaˈser		U+05BA: HEBREW POINT HOLAM HASER FOR VAV​	hebrew
 ֳ		o		ŏ	o	vowel point			ħaˈtaf kaˈmats	חֲטַף קָמָץ	U+05B3: HEBREW POINT HATAF QAMATS​	hebrew
@@ -69,19 +70,18 @@ var spreadsheet = `
 ֲ		a		ă	a	vowel point			ħaˈtaf paˈtaħ	חֲטַף פַּתַח	U+05B2: HEBREW POINT HATAF PATAH​	hebrew
 												
 												
-ֽ		:¶		ː	ː		archaic				U+05BD: HEBREW POINT METEG​	hebrew
-												
 												
 ּ		/		ˑ		hard consonant diacritic			dagesh/mappiq	דָּגֵשׁ	U+05BC: HEBREW POINT DAGESH OR MAPIQ​	hebrew
-ֽ						biblical stress marker			meteg	מֶתֶג	U+05BD: HEBREW POINT METEG​	hebrew
 ־		-		-		hyphen			maqaf	מַקָּף‬	U+05BE: HEBREW PUNCTUATION MAQAF	hebrew
-ֿ						fricative indicator			rafe	רפה	U+05BF: HEBREW POINT RAFE​	hebrew
-׀						word separator			paseq		U+05C0: HEBREW PUNCTUATION PASEQ	hebrew
-ׁ		/		̎		shin dot					U+05C1: HEBREW POINT SHIN DOT​	hebrew
-ׂ		/		̍		sin dot					U+05C2: HEBREW POINT SIN DOT​	hebrew
-׃						biblical end of verse			sof pasuq	סוֹף פָּסוּק	U+05C3: HEBREW PUNCTUATION SOF PASUQ	hebrew
+ׁ		/		ᶴ		shin dot					U+05C1: HEBREW POINT SHIN DOT​	hebrew
+ׂ		/		ˢ		sin dot					U+05C2: HEBREW POINT SIN DOT​	hebrew
 ״		+		¨		acronym indicator			gershayim	גֵּרְשַׁיִם	U+05F4: HEBREW PUNCTUATION GERSHAYIM	hebrew
 ׳		/		´		pronunciation change/initialism/numeral indicator			ˈɡeʁeʃ	גֵּרֶשׁ	U+05F3: HEBREW PUNCTUATION GERESH	hebrew
+												
+ֽ						biblical stress marker	archaic		meteg	מֶתֶג	U+05BD: HEBREW POINT METEG​	hebrew
+ֿ						fricative indicator			rafe	רפה	U+05BF: HEBREW POINT RAFE​	hebrew
+׀						word separator			paseq		U+05C0: HEBREW PUNCTUATION PASEQ	hebrew
+׃						biblical end of verse			sof pasuq	סוֹף פָּסוּק	U+05C3: HEBREW PUNCTUATION SOF PASUQ	hebrew
 												
 												
 ₪		±		¤		shekel					U+20AA: NEW SHEQEL SIGN	currencysymbols
@@ -132,6 +132,9 @@ var spreadsheet = `
 ֭						cantillation mark					U+05AD: HEBREW ACCENT DEHI​	hebrew
 ֮						cantillation mark					U+05AE: HEBREW ACCENT ZINOR​	hebrew
 ֯						cantillation mark					U+05AF: HEBREW MARK MASORA CIRCLE​	hebrew
+
+
+
 `
 
 
