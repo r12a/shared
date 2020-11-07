d9b77e685481d24e87a6dd6d4b1c0b98b8d0665a
r12a <ishida@w3.org>
2020-11-07 05:39:24 +0000

    for template

---
+++ scriptdb/thisScript.js
@@ -0,0 +1,66 @@
+var spreadsheet = `
+ᝃ		k	k	k	k		syllable				U+1743 BUHID LETTER KA	buhid
+ᝄ		g	g	g	g		syllable				U+1744 BUHID LETTER GA	buhid
+ᝅ		N	ng	ŋ	ŋ		syllable				U+1745 BUHID LETTER NGA	buhid
+ᝆ		t	t	t	t		syllable				U+1746 BUHID LETTER TA	buhid
+ᝇ		d	d	d	d		syllable				U+1747 BUHID LETTER DA	buhid
+ᝈ		n	n	n	n		syllable				U+1748 BUHID LETTER NA	buhid
+ᝉ		p	p	p	p		syllable				U+1749 BUHID LETTER PA	buhid
+ᝊ		b	b	b	b		syllable				U+174A BUHID LETTER BA	buhid
+ᝋ		m	m	m	m		syllable				U+174B BUHID LETTER MA	buhid
+ᝌ		y	y	y	y		syllable				U+174C BUHID LETTER YA	buhid
+ᝍ		r	r	r	r		syllable				U+174D BUHID LETTER RA	buhid
+ᝎ		l	l	l	l		syllable				U+174E BUHID LETTER LA	buhid
+ᝏ		w	w	w	w		syllable				U+174F BUHID LETTER WA	buhid
+ᝐ		s	s	s	s		syllable				U+1750 BUHID LETTER SA	buhid
+ᝑ		h	h	h	h		syllable				U+1751 BUHID LETTER HA	buhid
+												
+												
+ᝀ		A	a	ạ	a		independent vowel				U+1740 BUHID LETTER A	buhid
+ᝁ		I	i	ị	i		independent vowel				U+1741 BUHID LETTER I	buhid
+ᝂ		U	u	ụ	u		independent vowel				U+1742 BUHID LETTER U	buhid
+												
+												
+\u1752		i	i	i	i		dependent vowel				U+1752 BUHID VOWEL SIGN I	buhid
+\u1753		u	u	u	u		dependent vowel				U+1753 BUHID VOWEL SIGN U	buhid
+												
+												
+᜵		,	,	,			comma				U+1735 PHILIPPINE SINGLE PUNCTUATION	buhid
+᜶		.	.	.			period				U+1736 PHILIPPINE DOUBLE PUNCTUATION	buhid
+
+
+`
+
+
+
+var cols = {
+"class": 1,
+"key":2,
+"transcription":3,
+"transLoc": 4,
+"ipaLoc": 5,
+"transckey": 0,
+"equiv": 0,
+"typeLoc": 7,
+"statusLoc": 8,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nameLoc": 0,
+"nnameLoc": 0,
+"ucsName": 11,
+"block": 12,
+
+//"othertranscriptions": [[6, 'ISO 9985']]
+}
+
+
+
+
