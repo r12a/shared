71019ff439b017c66e521371fc4e4859ea376cc9
r12a <ishida@w3.org>
2020-12-13 15:32:03 +0000

    scriptdb/bass, Fix some bugs. Add new column for latin letter panel.

--- scriptdb/bass.js
+++ scriptdb/bass.js
@@ -1,104 +1,103 @@
 var spreadsheet = `
-𖫥		p	p	p	p	plosive				U+16AE5: BASSA VAH LETTER PA	bassavah
-𖫡		t	t	t	t	plosive				U+16AE1: BASSA VAH LETTER TO	bassavah
-𖫟		c	c	c	c	affricate				U+16ADF: BASSA VAH LETTER CE	bassavah
-𖫑		k	k	k	k	plosive				U+16AD1: BASSA VAH LETTER KA	bassavah
-𖫘		k	kᵖ	k͡p	kp	plosive				U+16AD8: BASSA VAH LETTER KPAH	bassavah
-											
-𖫢		b	b	b	b	plosive				U+16AE2: BASSA VAH LETTER BA	bassavah
-𖫗		d	d	d	d	plosive				U+16AD7: BASSA VAH LETTER DHII	bassavah
-𖫞		¶d	ɖ	ɗ	ɖ	implosive				U+16ADE: BASSA VAH LETTER DO	bassavah
-𖫙		j	j	ɟ	j	affricate				U+16AD9: BASSA VAH LETTER JO	bassavah
-𖫖		g	g	ɡ	g	plosive				U+16AD6: BASSA VAH LETTER GAH	bassavah
-𖫝		g	gᵇ	g͡b	gb gm	plosive				U+16ADD: BASSA VAH LETTER GBU	bassavah
-											
-𖫓		f	f	f	f	fricative				U+16AD3: BASSA VAH LETTER FA	bassavah
-𖫣		v	v	v	v	fricative				U+16AE3: BASSA VAH LETTER VU	bassavah
-𖫒		s	s	s	s	fricative				U+16AD2: BASSA VAH LETTER SE	bassavah
-𖫜		z	z	z	z	fricative				U+16ADC: BASSA VAH LETTER ZO	bassavah
-𖫚		h	xʷ	ħʷ xʷ	xw	fricative				U+16ADA: BASSA VAH LETTER HWAH	bassavah
-𖫠		h	hʷ	hʷ	hw	fricative				U+16AE0: BASSA VAH LETTER UWU	bassavah
-𖫤		¶h	h	h	h	fricative				U+16AE4: BASSA VAH LETTER YEIN	bassavah
-											
-𖫔		m	mᵇ	m͡b	m ɓ	nasal				U+16AD4: BASSA VAH LETTER MBE	bassavah
-𖫐		n	n	n	n	nasal				U+16AD0: BASSA VAH LETTER ENNI	bassavah
-𖫕		j	dᶯ	ɟ ɲ	dy ny	affricate				U+16AD5: BASSA VAH LETTER YIE	bassavah
-											
-𖫛		w	w	w	w	approximant				U+16ADB: BASSA VAH LETTER WA	bassavah
-𖫦		r	r	r l	r	approximant				U+16AE6: BASSA VAH LETTER WADDA	bassavah
-											
-											
-											
-𖫭		i	i	i	i	vowel				U+16AED: BASSA VAH LETTER I	bassavah
-𖫪		u	u	u	u	vowel				U+16AEA: BASSA VAH LETTER U	bassavah
-𖫫		e	e	e	e	vowel				U+16AEB: BASSA VAH LETTER EE	bassavah
-𖫩		o	o	o	o	vowel				U+16AE9: BASSA VAH LETTER OO	bassavah
-𖫬		e	ɛ	ɛ	ɛ	vowel				U+16AEC: BASSA VAH LETTER E	bassavah
-𖫨		o	ɔ	ɔ	ɔ	vowel				U+16AE8: BASSA VAH LETTER O	bassavah
-𖫧		a	a	a	a	vowel				U+16AE7: BASSA VAH LETTER A	bassavah
-											
-											
-											
-𖫰		^	¹		́	high tone mark				U+16AF0: BASSA VAH COMBINING HIGH TONE​	bassavah
-𖫱		^	²		̀	mid tone mark				U+16AF1: BASSA VAH COMBINING LOW TONE​	bassavah
-𖫲		^	³			low tone mark				U+16AF2: BASSA VAH COMBINING MID TONE​	bassavah
-𖫳		^	⁴		̌	low-mid tone mark				U+16AF3: BASSA VAH COMBINING LOW-MID TONE​	bassavah
-𖫴		^	⁵		̂	high-low tone mark				U+16AF4: BASSA VAH COMBINING HIGH-LOW TONE​	bassavah
-											
-											
-											
-𖫵		.	.			full stop				U+16AF5: BASSA VAH FULL STOP	bassavah
-.		.	.			full stop					
-«		<	 “			quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	punctuation
-»		>	”			quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	punctuation
-											
-											
-											
-											
-											
-											
-\u200B						zero-width space				U+200B ZERO WIDTH SPACE	
-\u200C			ₓ			zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
-\u200D			₊			zero-width joiner				U+200D: ZERO WIDTH JOINER	
-\u034F			ᶜᵍʲ			combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER​	
-\u2067			ʳˡⁱ			rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE	
-\u2066			ˡʳⁱ			ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE	
-\u2068			ᶠˢⁱ			first-strong isolate				U+2068: FIRST STRONG ISOLATE	
-\u2069			ᵖᵈⁱ			pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE	
-\u202B			ʳˡᵉ			rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING	
-\u202A			ˡʳᵉ			ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING	
-\u202C			ᵖᵈᶠ			pop direction				U+202C: POP DIRECTIONAL FORMATTING	
-\u200F			ʳˡᵐ			rtl mark				U+200F: RIGHT-TO-LEFT MARK	
-\u200E			ˡʳᵐ			ltr mark				U+200E: LEFT-TO-RIGHT MARK	
-\u061C			ᵃˡᵐ			arabic letter mark				U+061C: ARABIC LETTER MARK	
-											
-											
-“		<				quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
-”		>				quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
-‘		<				quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
-’		>				quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
-«		"	“			quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
-»		"	”			quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
-ʼ						apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE	
-,						comma				U+002C: COMMA	
-;						semicolon				U+003B: SEMICOLON	
-:						colon				U+003A: COLON	
-.						full stop				U+002E: FULL STOP	
-?						question mark				U+003F: QUESTION MARK	
-!						exclamation mark				U+0021: EXCLAMATION MARK	
-(						parenthesis				U+0028: LEFT PARENTHESIS	
-)						parenthesis				U+0029: RIGHT PARENTHESIS	
-…						ellipsis				U+2026: HORIZONTAL ELLIPSIS	
-–						en dash				U+2013: EN DASH	
-—						em dash				U+2014: EM DASH	
-											
-§						section sign				U+00A7: SECTION SIGN	
-†						dagger				U+2020: DAGGER	
-‡						double dagger				U+2021: DOUBLE DAGGER	
-′						prime				U+2032: PRIME	
-″						double prime				U+2033: DOUBLE PRIME	
-‐						hyphen				U+2010 HYPHEN	
-‑						non-breaking hyphen				U+2011 NON-BREAKING HYPHEN	
+𖫥		p	p	p	p		plosive				U+16AE5: BASSA VAH LETTER PA	bassavah
+𖫡		t	t	t	t		plosive				U+16AE1: BASSA VAH LETTER TO	bassavah
+𖫟		c	c	c	c		affricate				U+16ADF: BASSA VAH LETTER CE	bassavah
+𖫑		k	k	k	k		plosive				U+16AD1: BASSA VAH LETTER KA	bassavah
+𖫘		k	kp	kᵖ	k͡p	k͡p kᵖ	plosive				U+16AD8: BASSA VAH LETTER KPAH	bassavah
+												
+𖫢		b	b	b	b		plosive				U+16AE2: BASSA VAH LETTER BA	bassavah
+𖫗		d	d	d	d		plosive				U+16AD7: BASSA VAH LETTER DHII	bassavah
+𖫞		¶d	ɖ	ɗ	ɗ	ɗ ɖ	implosive				U+16ADE: BASSA VAH LETTER DO	bassavah
+𖫙		j	j	j	ɟ	ɟ	affricate				U+16AD9: BASSA VAH LETTER JO	bassavah
+𖫖		g	g	g	ɡ		plosive				U+16AD6: BASSA VAH LETTER GAH	bassavah
+𖫝		g	gb gm	gᵇ	g͡b	g͡b gᵇ	plosive				U+16ADD: BASSA VAH LETTER GBU	bassavah
+												
+𖫓		f	f	f	f		fricative				U+16AD3: BASSA VAH LETTER FA	bassavah
+𖫣		v	v	v	v		fricative				U+16AE3: BASSA VAH LETTER VU	bassavah
+𖫒		s	s	s	s		fricative				U+16AD2: BASSA VAH LETTER SE	bassavah
+𖫜		z	z	z	z		fricative				U+16ADC: BASSA VAH LETTER ZO	bassavah
+𖫚		h	xw	xʷ	ħʷ xʷ	ħʷ xʷ	fricative				U+16ADA: BASSA VAH LETTER HWAH	bassavah
+𖫠		h	hw	hʷ	hʷ	hʷ	fricative				U+16AE0: BASSA VAH LETTER UWU	bassavah
+𖫤		¶h	h	h	h		fricative				U+16AE4: BASSA VAH LETTER YEIN	bassavah
+												
+𖫔		m	m ɓ	mᵇ	m ɓ	ɓ mᵇ	nasal				U+16AD4: BASSA VAH LETTER MBE	bassavah
+𖫐		n	n	n	n	n	nasal				U+16AD0: BASSA VAH LETTER ENNI	bassavah
+𖫕		j	dy ny	dᶯ	dʲ ɲ	dʲ ɲ dᶯ	ɲ or palatalised d				U+16AD5: BASSA VAH LETTER YIE	bassavah
+												
+𖫛		w	w	w	w	w	approximant				U+16ADB: BASSA VAH LETTER WA	bassavah
+𖫦		r	r	r	r l	r l ɾ	approximant	for loan words			U+16AE6: BASSA VAH LETTER WADDA	bassavah
+												
+												
+												
+𖫭		i	i	i	i	̃ ĩ	vowel				U+16AED: BASSA VAH LETTER I	bassavah
+𖫪		u	u	u	u	ũ ᵘ ᵘ̃	vowel				U+16AEA: BASSA VAH LETTER U	bassavah
+𖫫		e	e	e	e	ẽ ᵉ ᵉ̃	vowel				U+16AEB: BASSA VAH LETTER EE	bassavah
+𖫩		o	o	o	o	õ ᵒ ᵒ̃	vowel				U+16AE9: BASSA VAH LETTER OO	bassavah
+𖫬		e	ɛ	ɛ	ɛ	ɛ ɛ̃ ᵋ ᵋ̃	vowel				U+16AEC: BASSA VAH LETTER E	bassavah
+𖫨		o	ɔ	ɔ	ɔ	ɔ ɔ̃	vowel				U+16AE8: BASSA VAH LETTER O	bassavah
+𖫧		a	a	a	a	ã ᵃ ᵃ̃	vowel				U+16AE7: BASSA VAH LETTER A	bassavah
+
+												
+												
+												
+𖫰		^	́	¹	́	¹ ́	high tone mark				U+16AF0: BASSA VAH COMBINING HIGH TONE​	bassavah
+𖫱		^	̀	²	̀	² ̀	mid tone mark				U+16AF1: BASSA VAH COMBINING LOW TONE​	bassavah
+𖫲		^	̄	³	̄	³ ̄	low tone mark				U+16AF2: BASSA VAH COMBINING MID TONE​	bassavah
+𖫳		^	̌	⁴	᷅	⁴ ᷅	low-mid tone mark				U+16AF3: BASSA VAH COMBINING LOW-MID TONE​	bassavah
+𖫴		^	̂	⁵	́̀	⁵ ́̀	high-low tone mark				U+16AF4: BASSA VAH COMBINING HIGH-LOW TONE​	bassavah
+												
+												
+𖫵		.		.			full stop				U+16AF5: BASSA VAH FULL STOP	bassavah
+.		.		.			full stop					
+«		<		 “		 “ «	quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	punctuation
+»		>		”		” »	quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	punctuation
+												
+												
+												
+												
+												
+												
+\u200B							zero-width space				U+200B ZERO WIDTH SPACE	
+\u200C				ₓ			zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
+\u200D				₊			zero-width joiner				U+200D: ZERO WIDTH JOINER	
+\u034F				ᶜᵍʲ			combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER​	
+\u2067				ʳˡⁱ			rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE	
+\u2066				ˡʳⁱ			ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE	
+\u2068				ᶠˢⁱ			first-strong isolate				U+2068: FIRST STRONG ISOLATE	
+\u2069				ᵖᵈⁱ			pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE	
+\u202B				ʳˡᵉ			rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING	
+\u202A				ˡʳᵉ			ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING	
+\u202C				ᵖᵈᶠ			pop direction				U+202C: POP DIRECTIONAL FORMATTING	
+\u200F				ʳˡᵐ			rtl mark				U+200F: RIGHT-TO-LEFT MARK	
+\u200E				ˡʳᵐ			ltr mark				U+200E: LEFT-TO-RIGHT MARK	
+\u061C				ᵃˡᵐ			arabic letter mark				U+061C: ARABIC LETTER MARK	
+												
+												
+“		<					quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
+”		>					quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
+‘		<					quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+’		>					quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+ʼ							apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE	
+,							comma				U+002C: COMMA	
+;							semicolon				U+003B: SEMICOLON	
+:							colon				U+003A: COLON	
+.							full stop				U+002E: FULL STOP	
+?							question mark				U+003F: QUESTION MARK	
+!							exclamation mark				U+0021: EXCLAMATION MARK	
+(							parenthesis				U+0028: LEFT PARENTHESIS	
+)							parenthesis				U+0029: RIGHT PARENTHESIS	
+…							ellipsis				U+2026: HORIZONTAL ELLIPSIS	
+–							en dash				U+2013: EN DASH	
+—							em dash				U+2014: EM DASH	
+												
+§							section sign				U+00A7: SECTION SIGN	
+†							dagger				U+2020: DAGGER	
+‡							double dagger				U+2021: DOUBLE DAGGER	
+′							prime				U+2032: PRIME	
+″							double prime				U+2033: DOUBLE PRIME	
+‐							hyphen				U+2010 HYPHEN	
+‑							non-breaking hyphen				U+2011 NON-BREAKING HYPHEN	
+
 
 
 `
@@ -109,12 +108,12 @@ var spreadsheet = `
 var cols = {
 "class": 1,
 "key":2,
-"transLoc": 3,
-"ipaLoc": 4,
-"transckey": 0,
-"transcription":5,
-"typeLoc": 6,
-"statusLoc": 7,
+"transcription":3,
+"transLoc": 4,
+"ipaLoc": 5,
+"latin": 6,
+"typeLoc": 7,
+"statusLoc": 8,
 "dvowel": 0,
 "ivowel": 0,
 "subj":0,
@@ -127,8 +126,8 @@ var cols = {
 "numLoc": 0,
 "nameLoc": 0,
 "nnameLoc": 0,
-"ucsName": 10,
-"block": 11,
+"ucsName": 11,
+"block": 12,
 
-"othertranscriptions": [[5, 'Latin']]
+"othertranscriptions": [[3, 'Latin']]
 }
