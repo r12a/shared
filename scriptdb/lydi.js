7a941c0484d3a252b0db63b9b88350f9e54648d6
r12a <ishida@w3.org>
2021-02-21 16:57:01 +0000

    + scriptdb/lydi.js

---
+++ scriptdb/lydi.js
@@ -0,0 +1,68 @@
+var spreadsheet = `
+𐤡		p	p	p	p b	consonant								U+10921: LYDIAN LETTER B	lydian
+𐤯		t	t	t	t d	consonant								U+1092F: LYDIAN LETTER T	lydian
+𐤨		k	k	k	k ɡ	consonant								U+10928: LYDIAN LETTER K	lydian
+𐤲		q	q	q	kʷ									U+10932: LYDIAN LETTER Q	lydian
+𐤢		g	g	g	ɡ	consonant								U+10922: LYDIAN LETTER G	lydian
+															
+𐤹		c	c	ʦ	ts dz	consonant								U+10939: LYDIAN LETTER C	lydian
+𐤴		c	τ	τ	tç tʃ	consonant								U+10934: LYDIAN LETTER TT	lydian
+															
+𐤱		f	f	f	f ɸ	consonant								U+10931: LYDIAN LETTER F	lydian
+𐤣		d	d	θ	θ ð	consonant								U+10923: LYDIAN LETTER D	lydian
+𐤮		s	s	s	s	consonant								U+1092E: LYDIAN LETTER SS	lydian
+𐤳		s	š	š	ç ʃ	consonant								U+10933: LYDIAN LETTER S	lydian
+															
+𐤪		m	m	m	m	consonant								U+1092A: LYDIAN LETTER M	lydian
+𐤫		n	n	n	n	consonant								U+1092B: LYDIAN LETTER N	lydian
+𐤸		v	ν	ŋ	ɲ ŋ	consonant	pronunciation not certain							U+10938: LYDIAN LETTER NN	
+															
+𐤥		w	w	w	w v	consonant								U+10925: LYDIAN LETTER V	lydian
+𐤭		r	r	r	r	consonant								U+1092D: LYDIAN LETTER R	lydian
+𐤩		l	l	l	l	consonant								U+10929: LYDIAN LETTER L	lydian
+𐤷		l	λ	ʎ	ʎ	consonant								U+10937: LYDIAN LETTER LY	lydian
+															
+															
+															
+															
+															
+𐤦		i	i	i	i	vowel								U+10926: LYDIAN LETTER I	lydian
+𐤧		y	y	y	i̯ j	vowel	allophone							U+10927: LYDIAN LETTER Y	
+𐤰		u	u	u	u	vowel								U+10930: LYDIAN LETTER U	lydian
+𐤤		e	e	e	eː	vowel								U+10924: LYDIAN LETTER E	lydian
+𐤬		o	o	o	oː	vowel								U+1092C: LYDIAN LETTER O	lydian
+𐤠		a	a	a	a aː	vowel								U+10920: LYDIAN LETTER A	lydian
+𐤵		a	ã	ã	ãː	nasalised vowel	pronunciation not certain							U+10935: LYDIAN LETTER AN	
+𐤶		e	ẽ	ẽ	ã æ̃	nasalised vowel	pronunciation not certain							U+10936: LYDIAN LETTER EN	
+
+`
+
+
+
+
+var cols = {
+"class": 1,
+"key":2,
+"transcription":0,
+"transLoc": 4,
+"ipaLoc": 5,
+"transckey": 0,
+"typeLoc": 6,
+"statusLoc": 7,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":9,
+"uc":10,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nameLoc": 9,
+"nnameLoc": 10,
+"ucsName": 14,
+"block": 15,
+
+//"othertranscriptions": [[6, 'ISO 9985']]
+}
