aa2cd6e155370748cde06801875d3723d648880a
r12a <ishida@w3.org>
2021-02-21 14:46:06 +0000

    scriptdb/goth.js: Rough start

---
+++ scriptdb/goth.js
@@ -0,0 +1,69 @@
+var spreadsheet = `
+𐍀		p	p	p	p	consonant								U+10340: GOTHIC LETTER PAIRTHRA	gothic
+𐌱		b	b	b	b	consonant								U+10331: GOTHIC LETTER BAIRKAN	gothic
+𐍄		t	t	t	t	consonant								U+10344: GOTHIC LETTER TEIWS	gothic
+𐌳		d	d	d	d	consonant								U+10333: GOTHIC LETTER DAGS	gothic
+𐌺		k	k	k	k	consonant								U+1033A: GOTHIC LETTER KUSMA	gothic
+𐍇		x	x	x	k		proper nouns & loan words from Greek							U+10347: GOTHIC LETTER IGGWS	gothic
+𐌵		q	q	q	kʷ									U+10335: GOTHIC LETTER QAIRTHRA	gothic
+𐌲		g	g	g	ɡ	consonant								U+10332: GOTHIC LETTER GIBA	gothic
+															
+𐍆		f	f	f	ɸ	consonant								U+10346: GOTHIC LETTER FAIHU	gothic
+𐌸		p	þ	þ	θ	consonant								U+10338: GOTHIC LETTER THIUTH	gothic
+𐍃		s	s	s	s	consonant								U+10343: GOTHIC LETTER SAUIL	gothic
+𐌶		z	z	z	z	consonant								U+10336: GOTHIC LETTER IUJA	gothic
+𐌷		h	h	h	h x	consonant								U+10337: GOTHIC LETTER HAGL	gothic
+𐍈		h	ƕ	ƕ	hʷ ʍ	consonant								U+10348: GOTHIC LETTER HWAIR	gothic
+															
+𐌼		m	m	m	m	consonant								U+1033C: GOTHIC LETTER MANNA	gothic
+𐌽		n	n	n	n	consonant								U+1033D: GOTHIC LETTER NAUTHS	gothic
+															
+𐍅		w	w	w	w y									U+10345: GOTHIC LETTER WINJA	gothic
+𐍂		r	r	r	r	consonant								U+10342: GOTHIC LETTER RAIDA	gothic
+𐌻		l	l	l	l	consonant								U+1033B: GOTHIC LETTER LAGUS	gothic
+𐌾		j	j	j	j									U+1033E: GOTHIC LETTER JER	gothic
+															
+𐍁				y	∅	consonant								U+10341: GOTHIC LETTER NINETY	gothic
+𐍊				^	∅	consonant								U+1034A: GOTHIC LETTER NINE HUNDRED	gothic
+															
+															
+															
+															
+𐌹		i	i	i	i	vowel								U+10339: GOTHIC LETTER EIS	gothic
+𐌿		u	u	u	ʊ uː	vowel								U+1033F: GOTHIC LETTER URUS	gothic
+𐌴		e	e	e	eː	vowel								U+10334: GOTHIC LETTER AIHVU	gothic
+𐍉		o	o	o	oː	vowel								U+10349: GOTHIC LETTER OTHAL	gothic
+𐌰		a	a	a	a aː	vowel								U+10330: GOTHIC LETTER AHSA	gothic
+
+
+`
+
+
+
+
+var cols = {
+"class": 1,
+"key":2,
+"transcription":0,
+"transLoc": 4,
+"ipaLoc": 5,
+"transckey": 0,
+"typeLoc": 6,
+"statusLoc": 7,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":9,
+"uc":10,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nameLoc": 9,
+"nnameLoc": 10,
+"ucsName": 14,
+"block": 15,
+
+//"othertranscriptions": [[6, 'ISO 9985']]
+}
