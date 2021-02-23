ba374b3a4848e27ce910cdfc0663be1c7ed1b7ac
r12a <ishida@w3.org>
2021-02-23 11:41:47 +0000

    scriptdb: goth & glag prepped for first draft

--- scriptdb/glag.js
+++ scriptdb/glag.js
@@ -1,110 +1,115 @@
 var spreadsheet = `
-ⱂ		p		p	p	consonant				Ⱂ				U+2C42: GLAGOLITIC SMALL LETTER POKOJI	glagolitic
-ⰱ		b		b	b	consonant				Ⰱ				U+2C31: GLAGOLITIC SMALL LETTER BUKY	glagolitic
-ⱅ		t		t	t	consonant				Ⱅ				U+2C45: GLAGOLITIC SMALL LETTER TVRIDO	glagolitic
-ⰴ		d		d	d	consonant				Ⰴ				U+2C34: GLAGOLITIC SMALL LETTER DOBRO	glagolitic
-ⰽ		k		k	k	consonant				Ⰽ				U+2C3D: GLAGOLITIC SMALL LETTER KAKO	glagolitic
-ⰳ		g		g	ɡ	consonant				Ⰳ				U+2C33: GLAGOLITIC SMALL LETTER GLAGOLI	glagolitic
-															
-ⱋ		c		c	tʲ ʃ͡t	consonant				Ⱋ				U+2C4B: GLAGOLITIC SMALL LETTER SHTA	glagolitic
-ⱌ		c		c	t͡s	consonant				Ⱌ 				U+2C4C U+0020: GLAGOLITIC SMALL LETTER TSI, SPACE	glagolitic
-ⰷ		j		j	d͡z	consonant				Ⰷ				U+2C37: GLAGOLITIC SMALL LETTER DZELO	glagolitic
-ⱍ		c		c	t͡ʃ	consonant				Ⱍ				U+2C4D: GLAGOLITIC SMALL LETTER CHRIVI	glagolitic
-ⰼ		j		j	d͡ʑ	consonant				Ⰼ				U+2C3C: GLAGOLITIC SMALL LETTER DJERVI	glagolitic
-															
-ⱇ		f		f	f	consonant				Ⱇ 				U+2C47 U+0020: GLAGOLITIC SMALL LETTER FRITU, SPACE	glagolitic
-ⰲ		v		v	ʋ	consonant				Ⰲ				U+2C32: GLAGOLITIC SMALL LETTER VEDE	glagolitic
-ⱚ		q		q	θ	consonant				Ⱚ				U+2C5A: GLAGOLITIC SMALL LETTER FITA	glagolitic
-ⱄ		s		s	s	consonant				Ⱄ				U+2C44: GLAGOLITIC SMALL LETTER SLOVO	glagolitic
-ⰸ		z		z	z	consonant				Ⰸ				U+2C38: GLAGOLITIC SMALL LETTER ZEMLJA	glagolitic
-ⱎ		s		s	ʃ	consonant				Ⱎ				U+2C4E: GLAGOLITIC SMALL LETTER SHA	glagolitic
-ⰶ		z		z	ʒ	consonant				Ⰶ				U+2C36: GLAGOLITIC SMALL LETTER ZHIVETE	glagolitic
-ⱈ		x		x	x	consonant				Ⱈ				U+2C48: GLAGOLITIC SMALL LETTER HERU	glagolitic
-															
-ⰿ		m		m	m	consonant				Ⰿ				U+2C3F: GLAGOLITIC SMALL LETTER MYSLITE	glagolitic
-ⱀ		n		n	n ɲ	consonant				Ⱀ				U+2C40: GLAGOLITIC SMALL LETTER NASHI	glagolitic
-															
-ⰾ		l		l	l ʎ	consonant				Ⰾ				U+2C3E: GLAGOLITIC SMALL LETTER LJUDIJE	glagolitic
-ⱃ		r		r	r	consonant				Ⱃ				U+2C43: GLAGOLITIC SMALL LETTER RITSI	glagolitic
-															
-															
-															
-															
-															
-Ⱂ					p	consonant			ⱂ					U+0532: ARMENIAN CAPITAL LETTER BEN	glagolitic
-Ⰱ					b	consonant			ⰱ					U+0533: ARMENIAN CAPITAL LETTER GIM	glagolitic
-Ⱅ					t	consonant			ⱅ					U+0534: ARMENIAN CAPITAL LETTER DA	glagolitic
-Ⰴ					d	consonant			ⰴ					U+0536: ARMENIAN CAPITAL LETTER ZA	glagolitic
-Ⰽ					k	consonant			ⰽ					U+0539: ARMENIAN CAPITAL LETTER TO	glagolitic
-Ⰳ					ɡ	consonant			ⰳ					U+053A: ARMENIAN CAPITAL LETTER ZHE	glagolitic
-														U+053C: ARMENIAN CAPITAL LETTER LIWN	glagolitic
-Ⱋ					tʲ ʃ͡t	consonant			ⱋ					U+053D: ARMENIAN CAPITAL LETTER XEH	glagolitic
-Ⱌ 					t͡s	consonant			ⱌ					U+053E: ARMENIAN CAPITAL LETTER CA	glagolitic
-Ⰷ					d͡z	consonant			ⰷ					U+053F: ARMENIAN CAPITAL LETTER KEN	glagolitic
-Ⱍ					t͡ʃ	consonant			ⱍ					U+0540: ARMENIAN CAPITAL LETTER HO	glagolitic
-Ⰼ					d͡ʑ	consonant			ⰼ					U+0541: ARMENIAN CAPITAL LETTER JA	glagolitic
-														U+0542: ARMENIAN CAPITAL LETTER GHAD	glagolitic
-Ⱇ 					f	consonant			ⱇ					U+0543: ARMENIAN CAPITAL LETTER CHEH	glagolitic
-Ⰲ					ʋ	consonant			ⰲ					U+0544: ARMENIAN CAPITAL LETTER MEN	glagolitic
-Ⱚ					θ	consonant			ⱚ					U+0545: ARMENIAN CAPITAL LETTER YI	glagolitic
-Ⱄ					s	consonant			ⱄ					U+0546: ARMENIAN CAPITAL LETTER NOW	glagolitic
-Ⰸ					z	consonant			ⰸ					U+0547: ARMENIAN CAPITAL LETTER SHA	glagolitic
-Ⱎ					ʃ	consonant			ⱎ					U+0549: ARMENIAN CAPITAL LETTER CHA	glagolitic
-Ⰶ					ʒ	consonant			ⰶ					U+054A: ARMENIAN CAPITAL LETTER PEH	glagolitic
-Ⱈ					x	consonant			ⱈ					U+054B: ARMENIAN CAPITAL LETTER JHEH	glagolitic
-														U+054C: ARMENIAN CAPITAL LETTER RA	glagolitic
-Ⰿ					m	consonant			ⰿ					U+054D: ARMENIAN CAPITAL LETTER SEH	glagolitic
-Ⱀ					n ɲ	consonant			ⱀ					U+054E: ARMENIAN CAPITAL LETTER VEW	glagolitic
-														U+054F: ARMENIAN CAPITAL LETTER TIWN	glagolitic
-Ⰾ					l ʎ	consonant			ⰾ					U+0550: ARMENIAN CAPITAL LETTER REH	glagolitic
-Ⱃ					r	consonant			ⱃ					U+0551: ARMENIAN CAPITAL LETTER CO	glagolitic
-															
-															
-															
-															
-															
-ⰻ		i		i	i j	vowel				Ⰻ				U+2C3B: GLAGOLITIC SMALL LETTER I	glagolitic
-ⰹ		i		í	i j	vowel				Ⰹ				U+2C39: GLAGOLITIC SMALL LETTER IZHE	glagolitic
-ⰺ		i		ī	i j	vowel				Ⰺ				U+2C3A: GLAGOLITIC SMALL LETTER INITIAL IZHE	glagolitic
-ⱛ		i		ʏ	ʏ i	vowel				Ⱛ				U+2C5B: GLAGOLITIC SMALL LETTER IZHITSA	glagolitic
-ⰟⰊ	s				ɨ	vowel				Ⰺ				U+2C1F U+2C0A: GLAGOLITIC CAPITAL LETTER YERU, CAPITAL LETTER INITIAL IZHE	glagolitic
-ⱏ		w		ɯ	ɯ	vowel				Ⱏ				U+2C4F: GLAGOLITIC SMALL LETTER YERU	glagolitic
-ⱆ		u		u	u	vowel				Ⱆ				U+2C46: GLAGOLITIC SMALL LETTER UKU	glagolitic
-ⱓ		u		ŭ	ju	vowel				Ⱓ				U+2C53: GLAGOLITIC SMALL LETTER YU	glagolitic
-ⰵ		e		ɛ	ɛ	vowel				Ⰵ				U+2C35: GLAGOLITIC SMALL LETTER YESTU	glagolitic
-ⱔ		e		ɛ̃	ɛ̃	vowel				Ⱔ 				U+2C54 U+0020: GLAGOLITIC SMALL LETTER SMALL YUS, SPACE	glagolitic
-ⱗ		e		ĕ	jɛ̃	vowel				Ⱗ				U+2C57: GLAGOLITIC SMALL LETTER IOTATED SMALL YUS	glagolitic
-ⱉ		o		ɔ	ɔ	vowel				Ⱉ 				U+2C49 U+0020: GLAGOLITIC SMALL LETTER OTU, SPACE	glagolitic
-ⱁ		o		ó	ɔ	vowel				Ⱁ				U+2C41: GLAGOLITIC SMALL LETTER ONU	glagolitic
-ⱘ		o		õ	ɔ̃	vowel				Ⱘ 				U+2C58 U+0020: GLAGOLITIC SMALL LETTER BIG YUS, SPACE	glagolitic
-ⱙ		o		ŏ	jɔ̃	vowel				Ⱙ				U+2C59: GLAGOLITIC SMALL LETTER IOTATED BIG YUS	glagolitic
-ⱖ		o		ő	jo	vowel				Ⱖ				U+2C56: GLAGOLITIC SMALL LETTER YO	glagolitic
-ⱐ		e		ə	ə					Ⱐ				U+2C50: GLAGOLITIC SMALL LETTER YERI	glagolitic
-ⱑ		a		æ	æ ja	vowel				Ⱑ				U+2C51: GLAGOLITIC SMALL LETTER YATI	glagolitic
-ⰰ		a		ɑ	ɑ	vowel				Ⰰ				U+2C30: GLAGOLITIC SMALL LETTER AZU	glagolitic
-
-															
-Ⰹ					i j	vowel			ⰹ					U+2C09: GLAGOLITIC CAPITAL LETTER IZHE	glagolitic
-Ⰺ					i j	vowel			ⰺ					U+2C0A: GLAGOLITIC CAPITAL LETTER INITIAL IZHE	glagolitic
-Ⰻ					i j	vowel			ⰻ					U+2C0B: GLAGOLITIC CAPITAL LETTER I	glagolitic
-Ⱛ					ʏ i	vowel			ⱛ					U+2C2B: GLAGOLITIC CAPITAL LETTER IZHITSA	glagolitic
-Ⱏ					ɨ	vowel			ⱏ					U+2C1F: GLAGOLITIC CAPITAL LETTER YERU	glagolitic
-Ⰺ					ɨ	vowel			ⰺ					U+2C0A: GLAGOLITIC CAPITAL LETTER INITIAL IZHE	glagolitic
-Ⱏ					ɯ	vowel			ⱏ					U+2C1F: GLAGOLITIC CAPITAL LETTER YERU	glagolitic
-Ⱆ					u	vowel			ⱆ					U+2C16: GLAGOLITIC CAPITAL LETTER UKU	glagolitic
-Ⱓ					ju	vowel			ⱓ					U+2C23: GLAGOLITIC CAPITAL LETTER YU	glagolitic
-Ⱖ					jo	vowel			ⱖ					U+2C26: GLAGOLITIC CAPITAL LETTER YO	glagolitic
-Ⰵ					ɛ	vowel			ⰵ					U+2C05: GLAGOLITIC CAPITAL LETTER YESTU	glagolitic
-Ⱁ					ɔ	vowel			ⱁ					U+2C11: GLAGOLITIC CAPITAL LETTER ONU	glagolitic
-Ⱉ 					ɔ	vowel			ⱉ					U+2C19 U+0020: GLAGOLITIC CAPITAL LETTER OTU, SPACE	glagolitic
-Ⱙ					jɔ̃	vowel			ⱙ					U+2C29: GLAGOLITIC CAPITAL LETTER IOTATED BIG YUS	glagolitic
-Ⱘ 					ɔ̃	vowel			ⱘ					U+2C28 U+0020: GLAGOLITIC CAPITAL LETTER BIG YUS, SPACE	glagolitic
-Ⱐ					ə				ⱐ					U+2C20: GLAGOLITIC CAPITAL LETTER YERI	glagolitic
-Ⱔ 					ɛ̃	vowel			ⱔ					U+2C24 U+0020: GLAGOLITIC CAPITAL LETTER SMALL YUS, SPACE	glagolitic
-Ⱗ					jɛ̃	vowel			ⱗ					U+2C27: GLAGOLITIC CAPITAL LETTER IOTATED SMALL YUS	glagolitic
-Ⱑ					æ ja	vowel			ⱑ					U+2C21: GLAGOLITIC CAPITAL LETTER YATI	glagolitic
-Ⰰ					ɑ	vowel			ⰰ					U+2C00: GLAGOLITIC CAPITAL LETTER AZU	glagolitic
-
+ⱂ		p		p	p	consonant				Ⱂ				U+2C42: GLAGOLITIC SMALL LETTER POKOJI
+ⰱ		b		b	b	consonant				Ⰱ				U+2C31: GLAGOLITIC SMALL LETTER BUKY
+ⱅ		t		t	t	consonant				Ⱅ				U+2C45: GLAGOLITIC SMALL LETTER TVRIDO
+ⰴ		d		d	d	consonant				Ⰴ				U+2C34: GLAGOLITIC SMALL LETTER DOBRO
+ⰽ		k		k	k	consonant				Ⰽ				U+2C3D: GLAGOLITIC SMALL LETTER KAKO
+ⰳ		ɡ		g	g	consonant				Ⰳ				U+2C33: GLAGOLITIC SMALL LETTER GLAGOLI
+														
+ⱋ		tʲ ʃ͡t		tʲ	c	consonant				Ⱋ				U+2C4B: GLAGOLITIC SMALL LETTER SHTA
+ⱌ		t͡s		ʦ	c	consonant				Ⱌ 				U+2C4C U+0020: GLAGOLITIC SMALL LETTER TSI, SPACE
+ⰷ		d͡z		ʣ	j	consonant				Ⰷ				U+2C37: GLAGOLITIC SMALL LETTER DZELO
+ⱍ		t͡ʃ		ʧ	c	consonant				Ⱍ				U+2C4D: GLAGOLITIC SMALL LETTER CHRIVI
+ⰼ		d͡ʑ		ʥ	j	consonant				Ⰼ				U+2C3C: GLAGOLITIC SMALL LETTER DJERVI
+														
+ⱇ		f		f	f	consonant				Ⱇ 				U+2C47 U+0020: GLAGOLITIC SMALL LETTER FRITU, SPACE
+ⰲ		ʋ		ʋ	v	consonant				Ⰲ				U+2C32: GLAGOLITIC SMALL LETTER VEDE
+ⱚ		θ		θ	q	consonant				Ⱚ				U+2C5A: GLAGOLITIC SMALL LETTER FITA
+ⱄ		s		s	s	consonant				Ⱄ				U+2C44: GLAGOLITIC SMALL LETTER SLOVO
+ⰸ		z		z	z	consonant				Ⰸ				U+2C38: GLAGOLITIC SMALL LETTER ZEMLJA
+ⱎ		ʃ		ʃ	s	consonant				Ⱎ				U+2C4E: GLAGOLITIC SMALL LETTER SHA
+ⰶ		ʒ		z	z	consonant				Ⰶ				U+2C36: GLAGOLITIC SMALL LETTER ZHIVETE
+ⱈ		x		x	x	consonant				Ⱈ				U+2C48: GLAGOLITIC SMALL LETTER HERU
+														
+ⰿ		m		m	m	consonant				Ⰿ				U+2C3F: GLAGOLITIC SMALL LETTER MYSLITE
+ⱀ		n ɲ		n	n	consonant				Ⱀ				U+2C40: GLAGOLITIC SMALL LETTER NASHI
+														
+ⰾ		l ʎ		l	l	consonant				Ⰾ				U+2C3E: GLAGOLITIC SMALL LETTER LJUDIJE
+ⱃ		r		r	r	consonant				Ⱃ				U+2C43: GLAGOLITIC SMALL LETTER RITSI
+														
+														
+														
+														
+														
+Ⱂ		p		P	P	consonant			ⱂ					U+0532: ARMENIAN CAPITAL LETTER BEN
+Ⰱ		b		B	B	consonant			ⰱ					U+0533: ARMENIAN CAPITAL LETTER GIM
+Ⱅ		t		T	T	consonant			ⱅ					U+0534: ARMENIAN CAPITAL LETTER DA
+Ⰴ		d		D	D	consonant			ⰴ					U+0536: ARMENIAN CAPITAL LETTER ZA
+Ⰽ		k		K	K	consonant			ⰽ					U+0539: ARMENIAN CAPITAL LETTER TO
+Ⰳ		ɡ		G	G	consonant			ⰳ					U+053A: ARMENIAN CAPITAL LETTER ZHE
+														U+053C: ARMENIAN CAPITAL LETTER LIWN
+Ⱋ		tʲ ʃ͡t		Tʲ	C	consonant			ⱋ					U+053D: ARMENIAN CAPITAL LETTER XEH
+Ⱌ 		t͡s		Tˢ	C	consonant			ⱌ					U+053E: ARMENIAN CAPITAL LETTER CA
+Ⰷ		d͡z		Dᶻ	J	consonant			ⰷ					U+053F: ARMENIAN CAPITAL LETTER KEN
+Ⱍ		t͡ʃ		Tᶴ	C	consonant			ⱍ					U+0540: ARMENIAN CAPITAL LETTER HO
+Ⰼ		d͡ʑ		Dᶽ	J	consonant			ⰼ					U+0541: ARMENIAN CAPITAL LETTER JA
+														U+0542: ARMENIAN CAPITAL LETTER GHAD
+Ⱇ 		f		F	F	consonant			ⱇ					U+0543: ARMENIAN CAPITAL LETTER CHEH
+Ⰲ		ʋ		V	V	consonant			ⰲ					U+0544: ARMENIAN CAPITAL LETTER MEN
+Ⱚ		θ		Θ	Q	consonant			ⱚ					U+0545: ARMENIAN CAPITAL LETTER YI
+Ⱄ		s		S	S	consonant			ⱄ					U+0546: ARMENIAN CAPITAL LETTER NOW
+Ⰸ		z		Z	Z	consonant			ⰸ					U+0547: ARMENIAN CAPITAL LETTER SHA
+Ⱎ		ʃ		Ʃ	S	consonant			ⱎ					U+0549: ARMENIAN CAPITAL LETTER CHA
+Ⰶ		ʒ		Z	Z	consonant			ⰶ					U+054A: ARMENIAN CAPITAL LETTER PEH
+Ⱈ		x		X	X	consonant			ⱈ					U+054B: ARMENIAN CAPITAL LETTER JHEH
+														U+054C: ARMENIAN CAPITAL LETTER RA
+Ⰿ		m		M	M	consonant			ⰿ					U+054D: ARMENIAN CAPITAL LETTER SEH
+Ⱀ		n ɲ		N	N	consonant			ⱀ					U+054E: ARMENIAN CAPITAL LETTER VEW
+														U+054F: ARMENIAN CAPITAL LETTER TIWN
+Ⰾ		l ʎ		L	L	consonant			ⰾ					U+0550: ARMENIAN CAPITAL LETTER REH
+Ⱃ		r		R	R	consonant			ⱃ					U+0551: ARMENIAN CAPITAL LETTER CO
+														
+														
+														
+														
+														
+ⰻ		i j		i	i	vowel				Ⰻ				U+2C3B: GLAGOLITIC SMALL LETTER I
+ⰹ		i j		í	i	vowel				Ⰹ				U+2C39: GLAGOLITIC SMALL LETTER IZHE
+ⰺ		i j		ī	i	vowel				Ⰺ				U+2C3A: GLAGOLITIC SMALL LETTER INITIAL IZHE
+ⱛ		ʏ i		ʏ	i	vowel				Ⱛ				U+2C5B: GLAGOLITIC SMALL LETTER IZHITSA
+ⰟⰊ	s	ɨ				vowel				Ⰺ				U+2C1F U+2C0A: GLAGOLITIC CAPITAL LETTER YERU, CAPITAL LETTER INITIAL IZHE
+ⱏ		ɯ		ɯ	w	vowel				Ⱏ				U+2C4F: GLAGOLITIC SMALL LETTER YERU
+ⱆ		u		u	u	vowel				Ⱆ				U+2C46: GLAGOLITIC SMALL LETTER UKU
+ⱓ		ju		ŭ	u	vowel				Ⱓ				U+2C53: GLAGOLITIC SMALL LETTER YU
+ⰵ		ɛ		ɛ	e	vowel				Ⰵ				U+2C35: GLAGOLITIC SMALL LETTER YESTU
+ⱔ		ɛ̃		ɛ̃	e	vowel				Ⱔ 				U+2C54 U+0020: GLAGOLITIC SMALL LETTER SMALL YUS, SPACE
+ⱗ		jɛ̃		ĕ	e	vowel				Ⱗ				U+2C57: GLAGOLITIC SMALL LETTER IOTATED SMALL YUS
+ⱉ		ɔ		ɔ	o	vowel				Ⱉ 				U+2C49 U+0020: GLAGOLITIC SMALL LETTER OTU, SPACE
+ⱁ		ɔ		ó	o	vowel				Ⱁ				U+2C41: GLAGOLITIC SMALL LETTER ONU
+ⱘ		ɔ̃		õ	o	vowel				Ⱘ 				U+2C58 U+0020: GLAGOLITIC SMALL LETTER BIG YUS, SPACE
+ⱙ		jɔ̃		ŏ	o	vowel				Ⱙ				U+2C59: GLAGOLITIC SMALL LETTER IOTATED BIG YUS
+ⱖ		jo		ő	o	vowel				Ⱖ				U+2C56: GLAGOLITIC SMALL LETTER YO
+ⱐ		ə		ə	e	vowel				Ⱐ				U+2C50: GLAGOLITIC SMALL LETTER YERI
+ⱑ		æ ja		æ	a	vowel				Ⱑ				U+2C51: GLAGOLITIC SMALL LETTER YATI
+ⰰ		ɑ		ɑ	a	vowel				Ⰰ				U+2C30: GLAGOLITIC SMALL LETTER AZU
+														
+Ⰻ		i j		I	I	vowel			ⰻ					U+2C0B: GLAGOLITIC CAPITAL LETTER I
+Ⰹ		i j		Í	I	vowel			ⰹ					U+2C09: GLAGOLITIC CAPITAL LETTER IZHE
+Ⰺ		i j		Ī	I	vowel			ⰺ					U+2C0A: GLAGOLITIC CAPITAL LETTER INITIAL IZHE
+Ⱛ		ʏ i		Y	I	vowel			ⱛ					U+2C2B: GLAGOLITIC CAPITAL LETTER IZHITSA
+Ⱏ		ɯ		Ɯ	W	vowel			ⱏ					U+2C1F: GLAGOLITIC CAPITAL LETTER YERU
+Ⱆ		u		U	U	vowel			ⱆ					U+2C16: GLAGOLITIC CAPITAL LETTER UKU
+Ⱓ		ju		Ŭ	U	vowel			ⱓ					U+2C23: GLAGOLITIC CAPITAL LETTER YU
+Ⰵ		ɛ		Ɛ	E	vowel			ⰵ					U+2C05: GLAGOLITIC CAPITAL LETTER YESTU
+Ⱔ 		ɛ̃		Ɛ̃	E	vowel			ⱔ					U+2C24 U+0020: GLAGOLITIC CAPITAL LETTER SMALL YUS, SPACE
+Ⱗ		jɛ̃		Ĕ	E	vowel			ⱗ					U+2C27: GLAGOLITIC CAPITAL LETTER IOTATED SMALL YUS
+Ⱉ 		ɔ		Ɔ	O	vowel			ⱉ					U+2C19 U+0020: GLAGOLITIC CAPITAL LETTER OTU, SPACE
+Ⱁ		ɔ		Ó	O	vowel			ⱁ					U+2C11: GLAGOLITIC CAPITAL LETTER ONU
+Ⱘ		ɔ̃		Õ	O	vowel			ⱘ					U+2C28 U+0020: GLAGOLITIC CAPITAL LETTER BIG YUS, SPACE
+Ⱙ		jɔ̃		Ŏ	O	vowel			ⱙ					U+2C29: GLAGOLITIC CAPITAL LETTER IOTATED BIG YUS
+Ⱖ		jo		Ő	O	vowel			ⱖ					U+2C26: GLAGOLITIC CAPITAL LETTER YO
+Ⱐ		ə		Ə	E	vowel			ⱐ					U+2C20: GLAGOLITIC CAPITAL LETTER YERI
+Ⱑ		æ ja		Æ	A	vowel			ⱑ					U+2C21: GLAGOLITIC CAPITAL LETTER YATI
+Ⰰ		ɑ		Ɑ	A	vowel			ⰰ					U+2C00: GLAGOLITIC CAPITAL LETTER AZU
+														
+														
+														
+,				,	,	comma								U+002C: COMMA
+․				:	:	colon								U+2024: ONE DOT LEADER
+՝				;	;	(semi) colon								U+055D: ARMENIAN COMMA
+։				.	.	full stop								U+0589: ARMENIAN FULL STOP
+«				“	<	quotation mark								U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK
+»				”	>	quotation mark								U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK
 `
 
 
@@ -112,10 +117,10 @@ var spreadsheet = `
 
 var cols = {
 "class": 1,
-"key":2,
+"ipaLoc": 2,
 "transcription":0,
 "transLoc": 4,
-"ipaLoc": 5,
+"key":5,
 "transckey": 0,
 "typeLoc": 6,
 "statusLoc": 7,
--- scriptdb/goth.js
+++ scriptdb/goth.js
@@ -4,36 +4,50 @@ var spreadsheet = `
 𐍄		t	t	t	t	consonant								U+10344: GOTHIC LETTER TEIWS	gothic
 𐌳		d	d	d	d	consonant								U+10333: GOTHIC LETTER DAGS	gothic
 𐌺		k	k	k	k	consonant								U+1033A: GOTHIC LETTER KUSMA	gothic
-𐍇		x	x	x	k		proper nouns & loan words from Greek							U+10347: GOTHIC LETTER IGGWS	gothic
-𐌵		q	q	q	kʷ									U+10335: GOTHIC LETTER QAIRTHRA	gothic
-𐌲		g	g	g	ɡ	consonant								U+10332: GOTHIC LETTER GIBA	gothic
+𐍇		k	x	x	x	consonant	proper nouns & loan words from Greek							U+10347: GOTHIC LETTER IGGWS	gothic
+𐌵		kʷ	q	q	q	consonant								U+10335: GOTHIC LETTER QAIRTHRA	gothic
+𐌲		ɡ	g	g	g	consonant								U+10332: GOTHIC LETTER GIBA	gothic
 															
-𐍆		f	f	f	ɸ	consonant								U+10346: GOTHIC LETTER FAIHU	gothic
-𐌸		p	þ	þ	θ	consonant								U+10338: GOTHIC LETTER THIUTH	gothic
+𐍆		ɸ	f	f	f	consonant								U+10346: GOTHIC LETTER FAIHU	gothic
+𐌸		θ	þ	þ	p	consonant								U+10338: GOTHIC LETTER THIUTH	gothic
 𐍃		s	s	s	s	consonant								U+10343: GOTHIC LETTER SAUIL	gothic
 𐌶		z	z	z	z	consonant								U+10336: GOTHIC LETTER IUJA	gothic
-𐌷		h	h	h	h x	consonant								U+10337: GOTHIC LETTER HAGL	gothic
-𐍈		h	ƕ	ƕ	hʷ ʍ	consonant								U+10348: GOTHIC LETTER HWAIR	gothic
+𐌷		h x	h	h	h	consonant								U+10337: GOTHIC LETTER HAGL	gothic
+𐍈		hʷ ʍ	ƕ	ƕ	h	consonant								U+10348: GOTHIC LETTER HWAIR	gothic
 															
 𐌼		m	m	m	m	consonant								U+1033C: GOTHIC LETTER MANNA	gothic
 𐌽		n	n	n	n	consonant								U+1033D: GOTHIC LETTER NAUTHS	gothic
 															
-𐍅		w	w	w	w y									U+10345: GOTHIC LETTER WINJA	gothic
+𐍅		w y	w	w	w									U+10345: GOTHIC LETTER WINJA	gothic
 𐍂		r	r	r	r	consonant								U+10342: GOTHIC LETTER RAIDA	gothic
 𐌻		l	l	l	l	consonant								U+1033B: GOTHIC LETTER LAGUS	gothic
-𐌾		j	j	j	j									U+1033E: GOTHIC LETTER JER	gothic
+𐌾		j	j	j	j	consonant								U+1033E: GOTHIC LETTER JER	gothic
 															
-𐍁				y	∅	consonant								U+10341: GOTHIC LETTER NINETY	gothic
-𐍊				^	∅	consonant								U+1034A: GOTHIC LETTER NINE HUNDRED	gothic
+𐍁		∅		y		consonant								U+10341: GOTHIC LETTER NINETY	gothic
+𐍊		∅		^		consonant								U+1034A: GOTHIC LETTER NINE HUNDRED	gothic
 															
 															
 															
 															
 𐌹		i	i	i	i	vowel								U+10339: GOTHIC LETTER EIS	gothic
-𐌿		u	u	u	ʊ uː	vowel								U+1033F: GOTHIC LETTER URUS	gothic
-𐌴		e	e	e	eː	vowel								U+10334: GOTHIC LETTER AIHVU	gothic
-𐍉		o	o	o	oː	vowel								U+10349: GOTHIC LETTER OTHAL	gothic
-𐌰		a	a	a	a aː	vowel								U+10330: GOTHIC LETTER AHSA	gothic
+𐌿		ʊ uː	u	u	u	vowel								U+1033F: GOTHIC LETTER URUS	gothic
+𐌴		eː	e	e	e	vowel								U+10334: GOTHIC LETTER AIHVU	gothic
+𐍉		oː	o	o	o	vowel								U+10349: GOTHIC LETTER OTHAL	gothic
+𐌰		a aː	a	a	a	vowel								U+10330: GOTHIC LETTER AHSA	gothic
+															
+															
+															
+															
+,				,	,	comma								U+002C: COMMA	
+․				:	:	colon								U+2024: ONE DOT LEADER	
+՝				;	;	(semi) colon								U+055D: ARMENIAN COMMA	
+։				.	.	full stop								U+0589: ARMENIAN FULL STOP	
+«				“	<	quotation mark								U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+»				”	>	quotation mark								U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+՞				?	?	question mark								U+055E: ARMENIAN QUESTION MARK	
+՜				!	!	exclamation mark								U+055C: ARMENIAN EXCLAMATION MARK	
+՛				ⸯ	!	emphasis mark								U+055B: ARMENIAN EMPHASIS MARK	
+֊				-	-	soft hyphen								U+058A: ARMENIAN HYPHEN	
 
 
 `
@@ -43,10 +57,10 @@ var spreadsheet = `
 
 var cols = {
 "class": 1,
-"key":2,
+"ipaLoc": 2,
 "transcription":0,
 "transLoc": 4,
-"ipaLoc": 5,
+"key":5,
 "transckey": 0,
 "typeLoc": 6,
 "statusLoc": 7,
