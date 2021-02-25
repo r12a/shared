23b7d7a333e72d7d3b9c2a6e5319deb0974f4a8b
r12a <ishida@w3.org>
2021-02-25 11:51:04 +0000

    Batak char app support files

---
+++ scriptdb/batk.js
@@ -0,0 +1,115 @@
+var spreadsheet = `
+ᯇ		p	p	p	p	¹	consonant	karo mand pakp toba				U+1BC7: BATAK LETTER PA	rejang
+ᯈ		p	p	p¹	p	²	consonant	sima				U+1BC8: BATAK LETTER SIMALUNGUN PA	
+ᯅ		b	b	b	b		consonant	mand pakp toba sima				U+1BC5: BATAK LETTER BA	rejang
+ᯆ		b	b	b¹	b		consonant	karo				U+1BC6: BATAK LETTER KARO BA	
+ᯣ		mb	mb	mᵇ	b	mᵇ	pre-nasalised consonant	sima				U+1BE3: BATAK LETTER MBA	rejang
+ᯖ		t	t	t	t		consonant	mand toba sima				U+1BD6: BATAK LETTER SOUTHERN TA	rejang
+ᯗ		t	t	t¹	t		consonant	karo pakp				U+1BD7: BATAK LETTER NORTHERN TA	
+ᯑ		d	d	d	d		consonant	all				U+1BD1: BATAK LETTER DA	rejang
+ᯢ		nd	nd	nᵈ	d	nᵈ	pre-nasalised consonant	sima				U+1BE2: BATAK LETTER NDA	rejang
+ᯂ		k	k h	k	k		consonant	 karo pakp & toba; also HA for toba				U+1BC2: BATAK LETTER HA	rejang
+ᯃ		k h	k h	k¹	k		consonant	sima				U+1BC3: BATAK LETTER SIMALUNGUN HA	
+ᯎ		ɡ	g	g	g	ɡ	consonant	karo mand pakp toba				U+1BCE: BATAK LETTER GA	rejang
+ᯏ		ɡ	g	g¹	g		consonant	sima				U+1BCF: BATAK LETTER SIMALUNGUN GA	
+													
+ᯡ		t͡ʃ	c	ʧ	c	t͡ʃ	consonant	karo				U+1BE1: BATAK LETTER CA	rejang
+ᯚ᯦	s	t͡ʃ	c		c		consonant	mand					
+ᯐ		d͡ʒ	j	ʤ	j	d͡ʒ	consonant	all				U+1BD0: BATAK LETTER JA	rejang
+													
+ᯘ		s	s	s	s		consonant	karo pakp toba; CA for pakp				U+1BD8: BATAK LETTER SA	rejang
+ᯙ		s	s	s¹	s		consonant	sima				U+1BD9: BATAK LETTER SIMALUNGUN SA	
+ᯚ		s	s	s²	s		consonant	mand				U+1BDA: BATAK LETTER MANDAILING SA	
+ᯄ		h	h	h	h		consonant	mand				U+1BC4: BATAK LETTER MANDAILING HA	
+ᯄ᯦	s	h	h				consonant	mand				U+1BC4 U+1BE6: BATAK LETTER MANDAILING HA, SIGN TOMPI	
+													
+ᯔ		m	m	m	m		consonant	karo mand pakp toba				U+1BD4: BATAK LETTER MA	rejang
+ᯕ		m	m	m¹	m		consonant	sima				U+1BD5: BATAK LETTER SIMALUNGUN MA	
+ᯉ		n	n	n	n		consonant	all				U+1BC9: BATAK LETTER NA	rejang
+ᯊ		n	n	n	n		consonant	mand alternate				U+1BCA: BATAK LETTER MANDAILING NA	
+ᯠ		ɲ	ny	ɲ	n	ɲ	consonant	mand toba sima				U+1BE0: BATAK LETTER NYA	rejang
+ᯝ		ŋ	ng	ŋ	n	ŋ	consonant	all				U+1BDD: BATAK LETTER NGA	rejang
+													
+ᯋ		w	w	w	w		consonant	karo mand				U+1BCB: BATAK LETTER WA	rejang
+ᯌ		w	w	w¹	w		consonant	sima (also HA and KA)				U+1BCC: BATAK LETTER SIMALUNGUN WA	
+ᯍ		w	w	w²	w		consonant	toba				U+1BCD: BATAK LETTER PAKPAK WA	
+ᯒ		r	r	r	r		consonant	karo mand pakp toba				U+1BD2: BATAK LETTER RA	rejang
+ᯓ		r	r	r¹	r		consonant	sima				U+1BD3: BATAK LETTER SIMALUNGUN RA	
+ᯞ		l	l	l	l		consonant	karo mand pakp toba				U+1BDE: BATAK LETTER LA	rejang
+ᯟ		l	l	l¹	l		consonant	sima				U+1BDF: BATAK LETTER SIMALUNGUN LA	
+ᯛ		j	y	y	y		consonant	karo mand pakp toba				U+1BDB: BATAK LETTER YA	rejang
+ᯜ		j	y	y¹	y		consonant	sima				U+1BDC: BATAK LETTER SIMALUNGUN YA	
+													
+													
+ᯰ		-ŋ	ng	ɲ̽	n		final consonant	all				U+1BF0: BATAK CONSONANT SIGN NG​	rejang
+ᯱ		-h	h	r̽	h		final consonant	karo pakp sima				U+1BF1: BATAK CONSONANT SIGN H​	rejang
+᯲				͓	*		vowel killer	mand pakp toba				U+0020 U+1BF2: SPACE, BATAK PANGOLAT	rejang
+᯳				#	*		vowel killer	karo sima				U+0020 U+1BF3: SPACE, BATAK PANONGONAN	rejang
+													
+													
+													
+ᯀ		a	a h	a	A		independent vowel	also HA in karo & pakp				U+1BC0: BATAK LETTER A	
+ᯁ		a	a	a¹	A		independent vowel	sima				U+1BC1: BATAK LETTER SIMALUNGUN A	
+ᯤ		i	i	i	I		independent vowel	all				U+1BE4: BATAK LETTER I	
+ᯥ		u	u	u	U		independent vowel	all				U+1BE5: BATAK LETTER U	
+													
+													
+													
+													
+ᯪ		i	i	i	i		vowel-sign	karo mand pakp toba				U+1BEA: BATAK VOWEL SIGN I​	rejang
+ᯫ		i	i	i¹	i		vowel-sign	karo sima				U+1BEB: BATAK VOWEL SIGN KARO I​	
+ᯮ		u	u	u	u		vowel-sign	mand pakp sima toba				U+1BEE: BATAK VOWEL SIGN U​	rejang
+ᯯ		u	u	u¹	u		vowel-sign	for sima sa				U+1BEF: BATAK VOWEL SIGN U FOR SIMALUNGUN SA​	
+ᯧ		e	e	e	e		vowel-sign	karo				U+1BE7: BATAK VOWEL SIGN E​	rejang
+ᯨ		e o	e	e¹	e		vowel-sign	pakp; O for karo				U+1BE8: BATAK VOWEL SIGN PAKPAK E​	
+ᯩ		e	e	e²	e		vowel-sign	karo mand pakpak sima toba				U+1BE9: BATAK VOWEL SIGN EE​	
+ᯬ		o u	o	o	o		vowel-sign	mand pakp sima toba; U for karo				U+1BEC: BATAK VOWEL SIGN O​	rejang
+ᯭ		o	o	o¹	o		vowel-sign	karo				U+1BED: BATAK VOWEL SIGN KARO O​	
+ᯩᯨ	s	ou	ou	e²e¹			vowel-sign	sima				U+1BE9 U+1BE8: BATAK VOWEL SIGN EE, VOWEL SIGN PAKPAK E​	
+													
+᯦							sound changer	mandailing				U+0020 U+1BE6: SPACE, BATAK SIGN TOMPI	
+													
+													
+													
+,				,	,		comma					U+002C: COMMA	
+«				“	<		quotation mark					U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+»				”	>		quotation mark					U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+᯼				¶	|		section mark					U+1BFC: BATAK SYMBOL BINDU NA METEK	rejang
+᯽												U+1BFD: BATAK SYMBOL BINDU PINARBORAS	
+᯾												U+1BFE: BATAK SYMBOL BINDU JUDUL	
+᯿												U+1BFF: BATAK SYMBOL BINDU PANGOLAT	
+
+
+
+`
+
+
+
+
+var cols = {
+"class": 1,
+"ipaLoc": 2,
+"transcription":3,
+"transLoc": 4,
+"key":5,
+"latin": 6,
+"transckey": 0,
+"typeLoc": 7,
+"statusLoc": 8,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nameLoc": 0,
+"nnameLoc": 0,
+"ucsName": 12,
+"block": 13,
+
+"othertranscriptions": [[3, 'Latin']]
+}
