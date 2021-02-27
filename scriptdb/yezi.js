7bc2d642aebbb3e3afe3807bb23dac74adce02e6
r12a <ishida@w3.org>
2021-02-27 19:04:41 +0000

    Yezidi scriptdb and webfonts

---
+++ scriptdb/yezi.js
@@ -0,0 +1,104 @@
+var spreadsheet = `
+𐺂		p	p	p	p		plosive					U+10E82: YEZIDI LETTER PE	yezidi
+𐺁		b	b	b	b		plosive					U+10E81: YEZIDI LETTER BE	yezidi
+𐺃		pʼ	pʼ	pʼ	p	pʼ	plosive					U+10E83: YEZIDI LETTER PHE	yezidi
+𐺕		t	t	t	t		plosive					U+10E95: YEZIDI LETTER TA	yezidi
+𐺄		tʼ	tʼ	tʼ	t	tʼ	plosive					U+10E84: YEZIDI LETTER THE	yezidi
+𐺋		d	d	d	d		plosive					U+10E8B: YEZIDI LETTER DAL	yezidi
+𐺔		dˁ	dˁ	ḍ	d	ḍ dˁ	plosive					U+10E94: YEZIDI LETTER DAD	yezidi
+𐺝		k	k	k	k		plosive					U+10E9D: YEZIDI LETTER KAF	yezidi
+𐺞		kʼ	kʼ	k	k	kʼ	plosive					U+10E9E: YEZIDI LETTER KHAF	yezidi
+𐺟		ɡ	ɡ	g	g	ɡ	plosive					U+10E9F: YEZIDI LETTER GAF	yezidi
+𐺘		ʕ	ʕ	xʼ	x	ʕ xʼ	plosive					U+10E98: YEZIDI LETTER XHEYN	yezidi
+𐺜		q	q	q	q		plosive					U+10E9C: YEZIDI LETTER QAF	yezidi
+													
+𐺇		t͡ʃ	ʧ	ç	c	t͡ʃ ʧ	affricate					U+10E87: YEZIDI LETTER CHIM	yezidi
+𐺈	s	t͡ʃʼ	ʧʼ	çʼ	c	ʧʼ	affricate					U+10E88: YEZIDI LETTER CHHIM	yezidi
+𐺆		d͡ʒ	ʤ	c	c	d͡ʒ ʤ	affricate					U+10E86: YEZIDI LETTER CIM	yezidi
+													
+𐺙		f	f	f	f		fricative					U+10E99: YEZIDI LETTER FA	yezidi
+𐺚		v	v	v	v		fricative					U+10E9A: YEZIDI LETTER VA	yezidi
+𐺛		v	v	v	v		fricative					U+10E9B: YEZIDI LETTER VA ALTERNATE FORM	yezidi
+𐺅		s	s	s	s		fricative					U+10E85: YEZIDI LETTER SE	yezidi
+𐺑		s	s	s	s		fricative					U+10E91: YEZIDI LETTER SIN	yezidi
+𐺏		z	z	z	z		fricative					U+10E8F: YEZIDI LETTER ZA	yezidi
+𐺓		sˁ	sˁ	ṣ	s	sˁ ṣ	fricative					U+10E93: YEZIDI LETTER SAD	yezidi
+𐺌		zˁ	zˁ	ẓ	z	zˁ ẓ	fricative					U+10E8C: YEZIDI LETTER ZAL	yezidi
+𐺖		zʼ	zʼ	zʼ	z	zʼ	fricative					U+10E96: YEZIDI LETTER ZE	yezidi
+𐺒		ʃ	ʃ	ş	s	ʃ ş	fricative					U+10E91: YEZIDI LETTER SIN	yezidi
+𐺐		ʒ	ʒ	j	j	ʒ	fricative					U+10E90: YEZIDI LETTER JA	yezidi
+𐺊		x	x	x	x		fricative					U+10E8A: YEZIDI LETTER XA	yezidi
+𐺉		ħ	ħ	hʼ	h	ħ hʼ	fricative					U+10E89: YEZIDI LETTER HHA	yezidi
+𐺧		h	h	h	h		fricative					U+10EA7: YEZIDI LETTER HAY	yezidi
+𐺗		ʔ	ʔ	ʼ eʼ	e	ʔ ʼ eʼ	fricative					U+10E97: YEZIDI LETTER EYN	yezidi
+													
+𐺡		m	m	m	m		nasal					U+10EA1: YEZIDI LETTER MIM	yezidi
+𐺢		n	n	n	n		nasal					U+10EA2: YEZIDI LETTER NUN	yezidi
+													
+𐺤		w	w	w	w		liquid					U+10EA4: YEZIDI LETTER WAW	yezidi
+𐺍		r	r	r	r		liquid					U+10E8D: YEZIDI LETTER RA	yezidi
+𐺎		rʼ	rʼ	rʼ	r	rʼ	liquid					U+10E8E: YEZIDI LETTER RHA	yezidi
+𐺠		l	l	l	l		liquid					U+10EA0: YEZIDI LETTER LAM	yezidi
+𐺨		j	j	î y	y	î	liquid					U+10EA8: YEZIDI LETTER YOT	yezidi
+													
+													
+𐺫		ʕ	ʕ		{	ʕ	hamza					U+10EAB: YEZIDI COMBINING HAMZA MARK​	yezidi
+𐺬							madda					U+10EAC: YEZIDI COMBINING MADDA MARK​	yezidi
+𐺭					*	͓	hyphenation					U+10EAD: YEZIDI HYPHENATION MARK	yezidi
+													
+													
+													
+													
+													
+𐺣		u	u	u	u		vowel					U+10EA3: YEZIDI LETTER UM	yezidi
+𐺩		e	e	ê	e	ê	vowel					U+10EA9: YEZIDI LETTER ET	yezidi
+𐺥		o	o	o	o		vowel					U+10EA5: YEZIDI LETTER OW	yezidi
+𐺦		ə	ə	e	e	ə	vowel					U+10EA6: YEZIDI LETTER EW	yezidi
+𐺀		a	a	a	a		vowel					U+10E80: YEZIDI LETTER ELIF	yezidi
+													
+													
+													
+													
+𐺰		ɫ	ɫ		I	ɫ	liquid	historical				U+10EB0: YEZIDI LETTER LAM WITH DOT ABOVE	yezidi
+𐺱		ə	ɚ		U		consonant	historical				U+10EB1: YEZIDI LETTER YOT WITH CIRCUMFLEX ABOVE	yezidi
+													
+													
+													
+,			,		,		comma					U+002C: COMMA	
+“			“		<		quotation mark					U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+”			”		>		quotation mark					U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+
+
+
+`
+
+
+
+
+var cols = {
+"class": 1,
+"ipaLoc": 2,
+"transLoc": 3,
+"transcription": 4,
+"key":5,
+"latin": 6,
+"transckey": 0,
+"typeLoc": 7,
+"statusLoc": 8,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nameLoc": 0,
+"nnameLoc": 0,
+"ucsName": 12,
+"block": 13,
+
+"othertranscriptions": [[4, 'Latin']]
+}
