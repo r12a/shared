399788aa697063227f5ec5b9e40224bbd62f534c
r12a <ishida@w3.org>
2021-03-02 18:04:36 +0000

    scriptdb: + orya, olck

---
+++ scriptdb/orya.js
@@ -0,0 +1,242 @@
+var spreadsheet = `
+													
+ପ		p	p	p	p		plosive					U+0B2A: ORIYA LETTER PA	oriya
+ଫ		pʰ	pʰ	ph	p	pʰ	plosive					U+0B2B: ORIYA LETTER PHA	oriya
+ବ		b	bʰ	b	b	bʰ	plosive					U+0B2C: ORIYA LETTER BA	oriya
+ଭ		bʰ	bʰ	bh	b		plosive					U+0B2D: ORIYA LETTER BHA	oriya
+ତ		t	t	t	t		plosive					U+0B24: ORIYA LETTER TA	oriya
+ଥ		tʰ	tʰ	th	t	tʰ	plosive					U+0B25: ORIYA LETTER THA	oriya
+ଦ		d	d	d	d		plosive					U+0B26: ORIYA LETTER DA	oriya
+ଧ		dʰ	dʰ	dh	d	dʰ	plosive					U+0B27: ORIYA LETTER DHA	oriya
+ଟ		ʈ	ʈ	ṭ	t	ʈ ṭ	plosive					U+0B1F: ORIYA LETTER TTA	oriya
+ଠ		ʈʰ	ʈʰ	ṭh	t	ʈʰ ṭh	plosive					U+0B20: ORIYA LETTER TTHA	oriya
+ଡ		ɖ	ɖ	ḍ	d	ɖ ḍ	plosive					U+0B21: ORIYA LETTER DDA	oriya
+ଢ		ɖʰ	ɖʰ	ḍh	d	ɖʰ ḍh	plosive					U+0B22: ORIYA LETTER DDHA	oriya
+କ		k	k	k	k		plosive					U+0B15: ORIYA LETTER KA	oriya
+ଖ		kʰ	kʰ	kh	k	kʰ	plosive					U+0B16: ORIYA LETTER KHA	oriya
+ଗ		g	g	g	g		plosive					U+0B17: ORIYA LETTER GA	oriya
+ଘ		gʰ	gʰ	gh	g	gʰ	plosive					U+0B18: ORIYA LETTER GHA	oriya
+													
+ଚ		t͡ʃ	c	c	c	t͡ʃ	affricate					U+0B1A: ORIYA LETTER CA	oriya
+ଛ		t͡ʃʰ	cʰ	ch	c	t͡ʃʰ cʰ	affricate					U+0B1B: ORIYA LETTER CHA	oriya
+ଜ		d͡ʒ	j	j	j	d͡ʒ	affricate					U+0B1C: ORIYA LETTER JA	oriya
+ଝ		d͡ʒʰ	jʰ	jh	j	d͡ʒʰ jʰ	affricate					U+0B1D: ORIYA LETTER JHA	oriya
+ଯ		d͡ʒ	ʤ	ẏ	y	ʤ ẏ	afficate			antaḥstha ja		U+0B2F: ORIYA LETTER YA	oriya
+													
+ଵ			v		v		fricative					U+0B35: ORIYA LETTER VA	oriya
+ସ		s	s	s	s		fricative			dantya sa	ଦନ୍ତ୍ୟ	U+0B38: ORIYA LETTER SA	oriya
+ଷ		s	ṣ	ṣ	s	ṣ	fricative			murddhanya sa	ମୂର୍ଦ୍ଧନ୍ୟ	U+0B37: ORIYA LETTER SSA	oriya
+ଶ		s	ś	ś	s	ś	fricative			talabya sa	ତାଲବ୍ୟ	U+0B36: ORIYA LETTER SHA	oriya
+ହ		ɦ	ɦ	h	h	ɦ	fricative					U+0B39: ORIYA LETTER HA	oriya
+													
+ମ		m	m	m	m		nasal					U+0B2E: ORIYA LETTER MA	oriya
+ନ		n	n	n	n		nasal					U+0B28: ORIYA LETTER NA	oriya
+ଞ		ɲ	ɲ	ñ	n	ɲ ñ	nasal					U+0B1E: ORIYA LETTER NYA	oriya
+ଣ		ɳ	ɳ	ṇ	n	ɳ ṇ	nasal					U+0B23: ORIYA LETTER NNA	oriya
+ଙ		ŋ	ŋ	ṅ	N	ṅ	nasal					U+0B19: ORIYA LETTER NGA	oriya
+													
+ୱ		w	w	w	w		liquid			abargya ba	ଅବର୍ଗ୍ୟ ବ	U+0B71: ORIYA LETTER WA	oriya
+ର		r	r	r	r		liquid					U+0B30: ORIYA LETTER RA	oriya
+ଡ଼	s	ɽ	rˑ	ṛ	r	ɽ ṛ	liquid					U+0B21 U+0B3C: ORIYA LETTER DDA, SIGN NUKTA	
+\u0B5C	p	ɽ	rˑ	ṛ	r		liquid					U+0B5C: ORIYA LETTER RRA	oriya
+ଢ଼	s	ɽʰ	rʰˑ	ṛh	r	ɽʰ	liquid					U+0B22 U+0B3C: ORIYA LETTER DDHA, SIGN NUKTA	
+\u0B5D	p	ɽʰ	rʰˑ	ṛh	r		liquid					U+0B5D: ORIYA LETTER RHA	oriya
+ଲ		l	l	l	l		liquid					U+0B32: ORIYA LETTER LA	oriya
+ଳ		ɭ	ɭ	ḷ	l	ɭ ḷ	liquid					U+0B33: ORIYA LETTER LLA	oriya
+ୟ		j	y	y	y		liquid					U+0B5F: ORIYA LETTER YYA	oriya
+													
+କ୍ଷ	s	ks		kṣ								U+0B15 U+0B4D U+0B37: ORIYA LETTER KA, SIGN VIRAMA, LETTER SSA	
+													
+													
+ି		i	i	i	i		vowel-sign					U+0B3F: ORIYA VOWEL SIGN I​	oriya
+ୀ		i	ī	ī	i	ī	vowel-sign					U+0B40: ORIYA VOWEL SIGN II​	oriya
+ୁ		u	u	u	u		vowel-sign					U+0B41 U+0020: ORIYA VOWEL SIGN U	oriya
+ୂ		u	ū	ū	u	ū	vowel-sign					U+0B42: ORIYA VOWEL SIGN UU​	oriya
+େ		e	e	e	e		vowel-sign					U+0B47: ORIYA VOWEL SIGN E​	oriya
+ୋ		o	o	o	o		vowel-sign					U+0B4B: ORIYA VOWEL SIGN O​	oriya
+ା		a	ā	ā	a	ā	vowel-sign					U+0B3E: ORIYA VOWEL SIGN AA​	oriya
+													
+ୈ		ɔi	ɔʲ	ai	a	ɔʲ	vowel-sign					U+0B48: ORIYA VOWEL SIGN AI​	oriya
+ୌ		ɔu	ɔᵘ	au	a	ɔᵘ	vowel-sign					U+0B4C: ORIYA VOWEL SIGN AU​	oriya
+													
+ୖ			xʲ		x		lengthening mark					U+0B56: ORIYA AI LENGTH MARK​	oriya
+ୗ			xᵘ		x		lengthening mark					U+0B57: ORIYA AU LENGTH MARK​	oriya
+													
+													
+													
+													
+ଇ		i	ị	i	i	ị	independent vowel					U+0B07 U+0020: ORIYA LETTER I	oriya
+ଈ		i	ị̄	ī	i	ị̄	independent vowel					U+0B08: ORIYA LETTER II	oriya
+ଉ		u	ụ	u	u	ụ	independent vowel					U+0B09: ORIYA LETTER U	oriya
+ଊ		u	ụ̄	ū	u	ụ̄	independent vowel					U+0B0A: ORIYA LETTER UU	oriya
+ଏ		e	ẹ	e	e	ẹ	independent vowel					U+0B0F: ORIYA LETTER E	oriya
+ଓ		o	ọ	o	o	ọ	independent vowel					U+0B13: ORIYA LETTER O	oriya
+ଅ		ɔ	ɔ̣	a	a	ɔ ɔ̣	independent vowel					U+0B05: ORIYA LETTER A	oriya
+ଆ		a	ạ̄	ā	ā		independent vowel					U+0B06 U+0020: ORIYA LETTER AA	oriya
+													
+ଐ		ɔi	ɔ̣ʲ	ai	a	ɔ̣ʲ	independent vowel					U+0B10: ORIYA LETTER AI	oriya
+ଔ		ɔu	ɔ̣ᵘ	au	a	ɔ̣ᵘ	independent vowel					U+0B14: ORIYA LETTER AU	oriya
+													
+													
+ୃ		ru	r̥	r̥	R	r̥	vocalic					U+0B43: ORIYA VOWEL SIGN VOCALIC R​	oriya
+ୄ		ru	r̥̄	r̥̄	R	r̥̄	vocalic	for Sanskrit				U+0B44: ORIYA VOWEL SIGN VOCALIC RR​	oriya
+ୢ		lu	l̥	l̥	L	l̥	vocalic	for Sanskrit				U+0B62 U+0020: ORIYA VOWEL SIGN VOCALIC L	oriya
+ୣ		lu	l̥̄	l̥̄	L	l̥̄	vocalic	for Sanskrit				U+0B63: ORIYA VOWEL SIGN VOCALIC LL​	oriya
+													
+ଋ		ru	r̥̣	r̥	R	r̥̣	vocalic					U+0B0B U+0020: ORIYA LETTER VOCALIC R	oriya
+ୠ		ru	r̥̣̄	r̥̄	R	r̥̣̄	vocalic	for Sanskrit				U+0B60: ORIYA LETTER VOCALIC RR	oriya
+ଌ		lu	l̥̣	l̥	L	l̥̣	vocalic	for Sanskrit				U+0B0C: ORIYA LETTER VOCALIC L	oriya
+ୡ		lu	l̥̣̄	l̥̄	L	l̥̣̄	vocalic	for Sanskrit				U+0B61: ORIYA LETTER VOCALIC LL	oriya
+													
+													
+ଂ		m ŋ	m̽	ṃ	m	m̽ ṃ	final nasal			anusvara	ଅନୁସ୍ୱାର	U+0B02: ORIYA SIGN ANUSVARA​	oriya
+ଃ		h	h̽	ḥ	h	h̽ ḥ	final aspiration/consonant doubler			visarga	ବିସର୍ଗ	U+0B03: ORIYA SIGN VISARGA​	oriya
+													
+													
+													
+													
+													
+													
+													
+													
+													
+୕			ˉ		[		overline	Kuvi				U+0B55: ORIYA SIGN OVERLINE​	oriya
+ଓଁ	s	om			[		om sign					U+0B13 U+0B01: LETTER O, SIGN CANDRABINDU	
+													
+													
+ଁ		̃	˜	̃	[		nasalisation			chandrabindu	ଚନ୍ଦ୍ରବିନ୍ଦୁ	U+0B01: ORIYA SIGN CANDRABINDU​	oriya
+													
+													
+୍			͓		*		vowel-killer			halanta	ହଳନ୍ତ	U+0B4D: ORIYA SIGN VIRAMA​	oriya
+													
+													
+ଽ			′		]		elision; vowel prolongation			avagraha	ଅବଗ୍ରହ	U+0B3D: ORIYA SIGN AVAGRAHA	oriya
+													
+													
+଼			ˑ		/		nukta			nukta		U+0B3C: ORIYA SIGN NUKTA​	oriya
+													
+													
+			ː		:		length mark						
+													
+													
+୰			°		@		deceased sign; name of deity			iswara		U+0B70: ORIYA ISSHAR	oriya
+													
+													
+।			|		|		section divider			purṇṇacheda	ପୂର୍ଣ୍ଣଛେଦ	U+0964: DEVANAGARI DANDA	
+॥			‖		|		section divider			double purṇṇacheda		U+0965: DEVANAGARI DOUBLE DANDA	
+.			.		.		full stop					U+002E: FULL STOP	
+⋯			⋯		.		ellipsis					U+22EF: MIDLINE HORIZONTAL ELLIPSIS	
+,			,		,		comma					U+002C: COMMA	
+⹁			⹁		,		comma					U+2E41: REVERSED COMMA	
+:			:		:		colon					U+003A: COLON	
+;			;		;		semicolon					U+003B: SEMICOLON	
+!			!		!		exclamation mark					U+0021: EXCLAMATION MARK	
+?			?		?		question mark					U+003F: QUESTION MARK	
+													
+													
+													
+“			“		<		quotation mark					U+201C: LEFT DOUBLE QUOTATION MARK	
+‘			‘		<		quotation mark					U+2018: LEFT SINGLE QUOTATION MARK	
+”			”		>		quotation mark					U+201D: RIGHT DOUBLE QUOTATION MARK	
+’			’		>		quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	
+													
+													
+													
+(			(		(		parenthesis					U+0028: LEFT PARENTHESIS	
+[			[		(		bracket					U+005B: LEFT SQUARE BRACKET	
+)			)		)		parenthesis					U+0029: RIGHT PARENTHESIS	
+]			]		)		bracket					U+005D: RIGHT SQUARE BRACKET	
+													
+													
+-			-		-		hyphen					:	
+‑			‑		-		non-breaking hyphen					U+2011: NON-BREAKING HYPHEN	
+–			–		-		en dash					U+2013: EN DASH	
+—			—		-		em dash					U+2014: EM DASH	
+													
+													
+ʼ			ʼ				apostrophe					U+02BC: MODIFIER LETTER APOSTROPHE	
+§			§				section sign					U+00A7: SECTION SIGN	
+†			†				dagger					U+2020: DAGGER	
+‡			‡				double dagger					U+2021: DOUBLE DAGGER	
+′			′				prime					U+2032: PRIME	
+″			″				double prime					U+2033: DOUBLE PRIME	
+													
+													
+													
+													
+													
+													
+\u200C			ₓ		=		zwnj					U+200C: ZERO WIDTH NON-JOINER	
+\u200D			₊		=		zwj					U+200D: ZERO WIDTH JOINER	
+\u2067			ʳˡⁱ		=		rtl isolate					U+2067: RIGHT-TO-LEFT ISOLATE	
+\u2066			ˡʳⁱ		=		ltr isolate					U+2066: LEFT-TO-RIGHT ISOLATE	
+\u2068			ᶠˢⁱ		=		first-strong isolate					U+2068: FIRST STRONG ISOLATE	
+\u2069			ᵖᵈⁱ		=		pop direction isolate					U+2069: POP DIRECTIONAL ISOLATE	
+\u202B			ʳˡᵉ		=		rtl embed					U+202B: RIGHT-TO-LEFT EMBEDDING	
+\u202A			ˡʳᵉ		=		ltr embed					U+202A: LEFT-TO-RIGHT EMBEDDING	
+\u202C			ᵖᵈᶠ		=		pop direction					U+202C: POP DIRECTIONAL FORMATTING	
+\u200F			ʳˡᵐ		=		rtl mark					U+200F: RIGHT-TO-LEFT MARK	
+\u200E			ˡʳᵐ		=		ltr mark					U+200E: LEFT-TO-RIGHT MARK	
+\u034F			ᶜᵍʲ		=		combining grapheme joiner					U+034F: COMBINING GRAPHEME JOINER​	
+\u061C			ᵃˡᵐ		=		arabic letter mark					U+061C: ARABIC LETTER MARK	
+													
+													
+													
+													
+୦			0̣	0	±		digit					U+0B66: ORIYA DIGIT ZERO	
+୧ 			1̣	1	±		digit					U+0B67 U+0020: ORIYA DIGIT ONE, SPACE	
+୨			2̣	2	±		digit					U+0B68: ORIYA DIGIT TWO	
+୩			3̣	3	±		digit					U+0B69: ORIYA DIGIT THREE	
+୪			4̣	4	±		digit					U+0B6A: ORIYA DIGIT FOUR	
+୫			5̣	5	±		digit					U+0B6B: ORIYA DIGIT FIVE	
+୬			6̣	6	±		digit					U+0B6C: ORIYA DIGIT SIX	
+୭			7̣	7	±		digit					U+0B6D: ORIYA DIGIT SEVEN	
+୮			8̣	8	±		digit					U+0B6E: ORIYA DIGIT EIGHT	
+୯			9̣	9	±		digit					U+0B6F: ORIYA DIGIT NINE	
+													
+													
+୲			¼		#		quarter					U+0B72: ORIYA FRACTION ONE QUARTER	
+୳ 			½		#		half					U+0B73 U+0020: ORIYA FRACTION ONE HALF, SPACE	
+୴			¾		#		three-quarters					U+0B74: ORIYA FRACTION THREE QUARTERS	
+୶			{1/8}				one eighth					U+0B76: ORIYA FRACTION ONE EIGHTH	
+୵			{1/16}				one sixteenth					U+0B75: ORIYA FRACTION ONE SIXTEENTH	
+୷			{3/16}				three sixteenths					U+0B77: ORIYA FRACTION THREE SIXTEENTHS	
+													
+													
+													
+%			%		%		percentage mark					U+0025: PERCENT SIGN	
+
+
+
+`
+
+
+
+
+var cols = {
+"class": 1,
+"ipaLoc": 2,
+"transLoc": 3,
+"transcription": 4,
+"key":5,
+"latin": 6,
+"transckey": 0,
+"typeLoc": 7,
+"statusLoc": 8,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nameLoc": 10,
+"nnameLoc": 0,
+"ucsName": 12,
+"block": 13,
+
+"othertranscriptions": [[4, 'ALA LC']]
+}
