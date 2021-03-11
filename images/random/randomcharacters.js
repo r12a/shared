5ecdc9f2149df937d1510baa98be8455522d8a84
r12a <ishida@w3.org>
2021-03-11 09:34:30 +0000

    Minor changes to prompt build

--- images/random/randomcharacters.js
+++ images/random/randomcharacters.js
@@ -11,11 +11,14 @@ new Array ('/images/11619.png', 127, 150),
 new Array ('/images/4628.png', 174, 150),
 new Array ('/images/26412.png', 155, 150),
 new Array ('/images/4314.png', 159, 150) 
-);
+)
+
+
+
 function chooseRandomPicture () {
-	var picture = document.getElementById( 'level2-picture' );
-	index = Math.floor(Math.random()*graphicsArray.length);
-	picture.setAttribute( 'src', graphicsArray[index][0] );
-	picture.setAttribute( 'width', graphicsArray[index][1] );
-	picture.setAttribute( 'height', graphicsArray[index][2] );
+	var picture = document.getElementById( 'level2-picture' )
+	index = Math.floor(Math.random()*graphicsArray.length)
+	picture.setAttribute( 'src', graphicsArray[index][0] )
+	picture.setAttribute( 'width', graphicsArray[index][1] )
+	picture.setAttribute( 'height', graphicsArray[index][2] )
 	}
