138a9251ff2147da429bd3be23c4619310bc5219
r12a <ishida@w3.org>
2021-03-17 06:17:38 +0000

    olck update

--- scriptdb/olck.js
+++ scriptdb/olck.js
@@ -1,69 +1,69 @@
 var spreadsheet = `
-ᱯ		p	p	p	p		consonant			ep		U+1C6F: OL CHIKI LETTER EP	olck
-ᱵ		p’ b	b	b	b		consonant			ob		U+1C75: OL CHIKI LETTER OB	olck
-ᱛ		t	t	t	t		consonant			at		U+1C5B: OL CHIKI LETTER AT	olck
-ᱴ		ʈ	ʈ	ṭ	t	ʈ ṭ						U+1C74: OL CHIKI LETTER OTT	olck
-ᱫ		d	d	d	d		consonant			ud		U+1C6B: OL CHIKI LETTER UD	olck
-ᱰ		ɖ	ɖ	ḍ	d	ɖ ḍ	pre-nasalised consonant			edd		U+1C70: OL CHIKI LETTER EDD	olck
-ᱠ		k	k	k	k					aak		U+1C60: OL CHIKI LETTER AAK	olck
-ᱜ		k’ ɡ	g	g	g	k’	consonant			ag		U+1C5C: OL CHIKI LETTER AG	olck
+ᱯ		p	p	p	p		consonant				ep	U+1C6F: OL CHIKI LETTER EP	olck
+ᱵ		b p̚	b	b	b	p̚ p’	consonant				ob	U+1C75: OL CHIKI LETTER OB	olck
+ᱛ		t	t	t	t		consonant				at	U+1C5B: OL CHIKI LETTER AT	olck
+ᱴ		ʈ	ʈ	ṭ	t							U+1C74: OL CHIKI LETTER OTT	olck
+ᱫ		d t̚	d	d	d	t̚ t’ ʈ ṭ	consonant				ud	U+1C6B: OL CHIKI LETTER UD	olck
+ᱰ		ɖ	ɖ	ḍ	d	ɖ ḍ	pre-nasalised consonant				edd	U+1C70: OL CHIKI LETTER EDD	olck
+ᱠ		k	k	k	k						aak	U+1C60: OL CHIKI LETTER AAK	olck
+ᱜ		ɡ k̚	g	g	g	k̚ k’ ɡ	consonant				ag	U+1C5C: OL CHIKI LETTER AG	olck
 													
-ᱪ		c	c	c	c		consonant			uch		U+1C6A: OL CHIKI LETTER UC	olck
-ᱡ		c’ j	j	j	j	c’	consonant			aaj		U+1C61: OL CHIKI LETTER AAJ	olck
+ᱪ		c	c	c	c		consonant				uch	U+1C6A: OL CHIKI LETTER UC	olck
+ᱡ		ɟ c’	j	j	j	ɟ c̚ c’	consonant				aaj	U+1C61: OL CHIKI LETTER AAJ	olck
 													
-ᱥ		s	s	s	s		consonant			is		U+1C65: OL CHIKI LETTER IS	olck
-ᱦ		ʔ h	h	ẖ	h	ʔ	consonant			ih		U+1C66: OL CHIKI LETTER IH	olck
-ᱷ		ʰ	ʰ	h	h	ʰ				oh		U+1C77: OL CHIKI LETTER OH	olck
+ᱥ		s	s	s	s		consonant				is	U+1C65: OL CHIKI LETTER IS	olck
+ᱦ		ʔ h	h	ẖ	h	ʔ ẖ	consonant				ih	U+1C66: OL CHIKI LETTER IH	olck
+ᱷ		ʰ	ʰ	h	h	ʰ					oh	U+1C77: OL CHIKI LETTER OH	olck
 													
-ᱢ		m	m	m	m		consonant			aam		U+1C62: OL CHIKI LETTER AAM	olck
-ᱱ		n	n	n	n		consonant			en		U+1C71: OL CHIKI LETTER EN	olck
-ᱧ		ɲ	ɲ	ñ	n	ɲ ñ	consonant			iny		U+1C67: OL CHIKI LETTER INY	olck
-ᱬ		ɳ	ɳ	ṇ	n	ɳ ṇ				unn		U+1C6C: OL CHIKI LETTER UNN	olck
-ᱝ		ŋ	ŋ	ṃ	m	ṃ	consonant			ang		U+1C5D: OL CHIKI LETTER ANG	olck
+ᱢ		m	m	m	m		consonant				aam	U+1C62: OL CHIKI LETTER AAM	olck
+ᱱ		n	n	n	n		consonant				en	U+1C71: OL CHIKI LETTER EN	olck
+ᱧ		ɲ	ɲ	ñ	n	ɲ ñ	consonant				iny	U+1C67: OL CHIKI LETTER INY	olck
+ᱬ		ɳ	ɳ	ṇ	n	ɳ ṇ					unn	U+1C6C: OL CHIKI LETTER UNN	olck
+ᱝ		ŋ	ŋ	ṃ	m	ŋ ṃ	consonant				ang	U+1C5D: OL CHIKI LETTER ANG	olck
 													
-ᱣ		w v	w	w	w		consonant			aaw		U+1C63: OL CHIKI LETTER AAW	olck
-ᱶ		w̃	w̃	ṅ	n	w̃ 				ov		U+1C76: OL CHIKI LETTER OV	olck
-ᱨ		r	r	r	r		consonant			ir		U+1C68: OL CHIKI LETTER IR	olck
-ᱲ		ɽ	ɽ	ṛ	r	ɽ ṛ				err		U+1C72: OL CHIKI LETTER ERR	olck
-ᱞ		l	l	l	l		consonant			al		U+1C5E: OL CHIKI LETTER AL	olck
-ᱭ		j	y	y	y		consonant			uy		U+1C6D: OL CHIKI LETTER UY	olck
-													
-													
-													
-ᱤ		i	i	i	i		vowel			li		U+1C64: OL CHIKI LETTER LI	olck
-ᱩ		u	u	u	u		vowel			lu		U+1C69: OL CHIKI LETTER LU	olck
-ᱮ		e	e	e	e		vowel			le		U+1C6E: OL CHIKI LETTER LE	olck
-ᱳ		o	o	o	o		vowel			lo		U+1C73: OL CHIKI LETTER LO	olck
-ᱚ		ɔ	ɔ	a	a	ɔ	vowel			la		U+1C5A: OL CHIKI LETTER LA	olck
-ᱟ		a	a	ā	a	ā	vowel			laa		U+1C5F: OL CHIKI LETTER LAA	olck
-													
-ᱮᱹ	s	ɛ		ĕ		ɛ ĕ	extended vowel					U+1C6E U+1C79: OL CHIKI LETTER LE, GAAHLAA TTUDDAAG	
-ᱚᱹ	s	ɔ		ŏ		ŏ	extended vowel					U+1C5A U+1C79: OL CHIKI LETTER LA, GAAHLAA TTUDDAAG	
-ᱟᱹ	s	ə		ă		ă ə	extended vowel					U+1C5F U+1C79: OL CHIKI LETTER LAA, GAAHLAA TTUDDAAG	
+ᱣ		w v	w	w	w		consonant				aaw	U+1C63: OL CHIKI LETTER AAW	olck
+ᱶ		w̃	w̃	ṅ	n	w̃ ṅ					ov	U+1C76: OL CHIKI LETTER OV	olck
+ᱨ		r	r	r	r		consonant				ir	U+1C68: OL CHIKI LETTER IR	olck
+ᱲ		ɽ	ɽ	ṛ	r	ɽ ṛ					err	U+1C72: OL CHIKI LETTER ERR	olck
+ᱞ		l	l	l	l		consonant				al	U+1C5E: OL CHIKI LETTER AL	olck
+ᱭ		j	y	y	y		consonant				uy	U+1C6D: OL CHIKI LETTER UY	olck
 													
 													
+													
+ᱤ		i	i	i	i	ĩ	vowel				li	U+1C64: OL CHIKI LETTER LI	olck
+ᱩ		u	u	u	u	ũ	vowel				lu	U+1C69: OL CHIKI LETTER LU	olck
+ᱮ		e	e	e	e	ẽ	vowel				le	U+1C6E: OL CHIKI LETTER LE	olck
+ᱳ		o	o	o	o	õ	vowel				lo	U+1C73: OL CHIKI LETTER LO	olck
+ᱚ		ɔ	ɔ	a	O	ɔ ɔ̃	vowel				la	U+1C5A: OL CHIKI LETTER LA	olck
+ᱟ		a	a	ā	a	ã ā	vowel				laa	U+1C5F: OL CHIKI LETTER LAA	olck
+													
+ᱟᱹ	s	ə	aˑ	ă	E	ă ə ə̃	extended vowel					U+1C5F U+1C79: OL CHIKI LETTER LAA, GAAHLAA TTUDDAAG	
+ᱮᱹ	s	ɛ	eˑ	ĕ	E	ɛ ɛ̃ ĕ	extended vowel					U+1C6E U+1C79: OL CHIKI LETTER LE, GAAHLAA TTUDDAAG	
+ᱚᱹ	s	ɔ	ɔˑ	ŏ	O	ɔ̃ ŏ	extended vowel					U+1C5A U+1C79: OL CHIKI LETTER LA, GAAHLAA TTUDDAAG	
 													
 													
 													
 													
 													
 													
-ᱽ			°	'	[		deglottaliser			ahad		U+1C7D: OL CHIKI AHAD	olck
-ᱼ			⁎		[		glottal protector			phārkā		U+1C7C: OL CHIKI PHAARKAA	olck
 													
 													
-ᱸ			˜	m̐	[	m̐	nasalisation marker			mũ ṭuḍăg		U+1C78: OL CHIKI MU TTUDDAG	olck
+ᱽ			°	'	[	̚	deglottaliser				ahad	U+1C7D: OL CHIKI AHAD	olck
+ᱼ			⁎		[		glottal protector				phārkā	U+1C7C: OL CHIKI PHAARKAA	olck
 													
 													
-ᱹ			ˑ		/		nukta			găhlă ṭuḍăg		U+1C79: OL CHIKI GAAHLAA TTUDDAAG	olck
-ᱺ			˙		/		nukta + nasalisation marker			mũ găhlă ṭuḍăg		U+1C7A: OL CHIKI MU-GAAHLAA TTUDDAAG	olck
+ᱸ			˜	m̐	[	m̐	nasalisation marker				mũ ṭuḍăg	U+1C78: OL CHIKI MU TTUDDAG	olck
 													
 													
-ᱻ			ː		:		vowel lengther			relā		U+1C7B: OL CHIKI RELAA	olck
+ᱹ			ˑ		/		nukta				găhlă ṭuḍăg	U+1C79: OL CHIKI GAAHLAA TTUDDAAG	olck
+ᱺ			˙		/		nukta + nasalisation marker				mũ găhlă ṭuḍăg	U+1C7A: OL CHIKI MU-GAAHLAA TTUDDAAG	olck
 													
 													
-᱾			|	.	|		minor break			mucăd		U+1C7E: OL CHIKI PUNCTUATION MUCAAD	olck
-᱿			‖	|	|		major break			double mucăd		U+1C7F: OL CHIKI PUNCTUATION DOUBLE MUCAAD	olck
+ᱻ			ː		:	ː	vowel lengther				relā	U+1C7B: OL CHIKI RELAA	olck
+													
+													
+᱾			|	.	|		minor break				mucăd	U+1C7E: OL CHIKI PUNCTUATION MUCAAD	olck
+᱿			‖	|	|		major break				double mucăd	U+1C7F: OL CHIKI PUNCTUATION DOUBLE MUCAAD	olck
 .			.		.		full stop					U+002E: FULL STOP	
 ⋯			⋯		.		ellipsis					U+22EF: MIDLINE HORIZONTAL ELLIPSIS	
 ,			,		,		comma					U+002C: COMMA	
@@ -87,7 +87,7 @@ var spreadsheet = `
 ]			]		)		bracket					U+005D: RIGHT SQUARE BRACKET	
 													
 													
--			-		-		hyphen						
+-			-		-		hyphen					U+002D HYPHEN-MINUS	
 ‑			‑		-		non-breaking hyphen					U+2011: NON-BREAKING HYPHEN	
 –			–		-		en dash					U+2013: EN DASH	
 —			—		-		em dash					U+2014: EM DASH	
@@ -137,6 +137,9 @@ var spreadsheet = `
 													
 													
 %			%		%		percentage mark					U+0025: PERCENT SIGN	
+
+
+
 `
 
 
