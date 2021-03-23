1dd975194e3bd3ff4ba86ccea6ab4f8acb1cd14a
r12a <ishida@w3.org>
2021-03-23 16:17:35 +0000

    scriptdb/rohg: Banking early version (note that this is actually arranged for v27)

--- scriptdb/rohg.js
+++ scriptdb/rohg.js
@@ -1,76 +1,217 @@
 var spreadsheet = `
-𐴀		q	a	ʔ	ɔ ʔ		consonant		a		U+10D00: HANIFI ROHINGYA LETTER A	rohg
-												
-𐴂		p	p	p	p		consonant		pa		U+10D02: HANIFI ROHINGYA LETTER PA	rohg
-𐴁		b	b	b	b		consonant		ba		U+10D01: HANIFI ROHINGYA LETTER BA	rohg
-𐴃		t	t	t	t		consonant		ta		U+10D03: HANIFI ROHINGYA LETTER TA	rohg
-𐴊		d	d	d	d		consonant		da		U+10D0A: HANIFI ROHINGYA LETTER DA	rohg
-𐴄		T	tt	ʈ	ʈ		consonant		tta		U+10D04: HANIFI ROHINGYA LETTER TTA	rohg
-𐴋		D	dd	ɖ	ɖ		consonant		dda		U+10D0B: HANIFI ROHINGYA LETTER DDA	rohg
-𐴑		k	k	k	k		consonant		ka		U+10D11: HANIFI ROHINGYA LETTER KA	rohg
-𐴒		g	g	g	g		consonant		ga		U+10D12: HANIFI ROHINGYA LETTER GA	rohg
-												
-𐴆		c	c	c	c		consonant		ca		U+10D06: HANIFI ROHINGYA LETTER CA	rohg
-𐴅		j	j	ɟ	ɟ		consonant		ja		U+10D05: HANIFI ROHINGYA LETTER JA	rohg
-												
-𐴉		f	f	f	f		consonant		fa		U+10D09: HANIFI ROHINGYA LETTER FA	rohg
-𐴜		v	v	v	v		consonant		va		U+10D1C: HANIFI ROHINGYA LETTER VA	rohg
-𐴏		s	s	s	s		consonant		sa		U+10D0F: HANIFI ROHINGYA LETTER SA	rohg
-𐴎		z	z	z	z		consonant		za		U+10D0E: HANIFI ROHINGYA LETTER ZA	rohg
-𐴐		S	sh	ʃ	ʃ		consonant		sha		U+10D10: HANIFI ROHINGYA LETTER SHA	rohg
-𐴈		x	kh	x	x		consonant		kha		U+10D08: HANIFI ROHINGYA LETTER KHA	rohg
-𐴇		h	h	h	h		consonant		ha		U+10D07: HANIFI ROHINGYA LETTER HA	rohg
-												
-𐴔		m	m	m	m		consonant		ma		U+10D14: HANIFI ROHINGYA LETTER MA	rohg
-𐴕		n	n	n	n		consonant		na		U+10D15: HANIFI ROHINGYA LETTER NA	rohg
-𐴛		n	nya	ɲ	ɲ		consonant		nya = nayya		U+10D1B: HANIFI ROHINGYA LETTER NYA	rohg
-𐴚		N	nga	ŋ	ŋ		consonant		nga = gan		U+10D1A: HANIFI ROHINGYA LETTER NGA	rohg
-												
-𐴖		w	w	ʋ	ʋ v		consonant		wa		U+10D16: HANIFI ROHINGYA LETTER WA	rohg
-𐴗		w	w	u	u		consonant		kinna wa		U+10D17: HANIFI ROHINGYA LETTER KINNA WA	rohg
-𐴌		r	r	ɾ	ɾ		consonant		ra		U+10D0C: HANIFI ROHINGYA LETTER RA	rohg
-𐴍		r	rr	ɽ	ɽ		consonant		rra		U+10D0D: HANIFI ROHINGYA LETTER RRA	rohg
-𐴓		l	l	l	l		consonant		la		U+10D13: HANIFI ROHINGYA LETTER LA	rohg
-𐴘		y	y	j	j		consonant		ya		U+10D18: HANIFI ROHINGYA LETTER YA	rohg
-𐴙		y	y	i	i		consonant		kinna ya		U+10D19: HANIFI ROHINGYA LETTER KINNA YA	rohg
-												
-												
-												
-𐴞		i	i	i	i		vowel		i-for		U+10D1E: HANIFI ROHINGYA VOWEL I	rohg
-𐴟		u	u	u	u		vowel		u-for		U+10D1F: HANIFI ROHINGYA VOWEL U	rohg
-𐴠		e	e	e	e		vowel		e-for		U+10D20: HANIFI ROHINGYA VOWEL E	rohg
-𐴡		o	o	o	o		vowel		o-for		U+10D21: HANIFI ROHINGYA VOWEL O	rohg
-𐴝		a	a	a	a		vowel		aa-for		U+10D1D: HANIFI ROHINGYA VOWEL A	rohg
-												
-												
-												
-𐴢		*		͓			vowel silencer		sakin		U+10D22: HANIFI ROHINGYA MARK SAKIN	rohg
-𐴣		[		˜			nasalisation mark		na khonna		U+10D23: HANIFI ROHINGYA MARK NA KHONNA	rohg
-𐴧		&		˖			gemination sign		tassi		U+10D27: HANIFI ROHINGYA SIGN TASSI​	rohg
-												
-												
-𐴤		^		¹			tone mark		hārbāy		U+10D24: HANIFI ROHINGYA SIGN HARBAHAY​	rohg
-𐴥		^		²			tone mark		ṭelā		U+10D25: HANIFI ROHINGYA SIGN TAHALA​	rohg
-𐴦		^		³			tone mark		ṭānā		U+10D26: HANIFI ROHINGYA SIGN TANA​	rohg												
-												
-												
-𐴰		0	0	0			digit				U+10D30: HANIFI ROHINGYA DIGIT ZERO	rohg
-𐴱		1	1	1			digit				U+10D31: HANIFI ROHINGYA DIGIT ONE	rohg
-𐴲		2	2	2			digit				U+10D32: HANIFI ROHINGYA DIGIT TWO	rohg
-𐴳		3	3	3			digit				U+10D33: HANIFI ROHINGYA DIGIT THREE	rohg
-𐴴		4	4	4			digit				U+10D34: HANIFI ROHINGYA DIGIT FOUR	rohg
-𐴵		5	5	5			digit				U+10D35: HANIFI ROHINGYA DIGIT FIVE	rohg
-𐴶		6	6	6			digit				U+10D36: HANIFI ROHINGYA DIGIT SIX	rohg
-𐴷		7	7	7			digit				U+10D37: HANIFI ROHINGYA DIGIT SEVEN	rohg
-𐴸		8	8	8			digit				U+10D38: HANIFI ROHINGYA DIGIT EIGHT	rohg
-𐴹		9	9	9			digit				U+10D39: HANIFI ROHINGYA DIGIT NINE	rohg
-												
-												
-،		,	,	,			comma				U+060C: ARABIC COMMA	
-۔		.	.	.			full stop				U+06D4: ARABIC FULL STOP	
-؛		;	;	;			semicolon				U+061B: ARABIC SEMICOLON	
-؟		?	?	?			question mark				U+061F: ARABIC QUESTION MARK	
-ـ		_		_			tatweel				U+0640: ARABIC TATWEEL	
+𐴂		p	p	p	p	p		stop				pa	U+10D02: HANIFI ROHINGYA LETTER PA	rohg
+𐴁		b	b	b	b	b		stop				ba	U+10D01: HANIFI ROHINGYA LETTER BA	rohg
+𐴃		t	t	t	t	t		stop				ta	U+10D03: HANIFI ROHINGYA LETTER TA	rohg
+𐴊		d	d	d	d	.		stop				da	U+10D0A: HANIFI ROHINGYA LETTER DA	rohg
+𐴄		ʈ	ʈ	tt	T	t	ʈ	stop				tta	U+10D04: HANIFI ROHINGYA LETTER TTA	rohg
+𐴋		ɖ	ɖ	dd	D	d	ɖ	stop				dda	U+10D0B: HANIFI ROHINGYA LETTER DDA	rohg
+𐴑		k	k	k	k	k		stop				ka	U+10D11: HANIFI ROHINGYA LETTER KA	rohg
+𐴒		g	g	g	g	g		stop				ga	U+10D12: HANIFI ROHINGYA LETTER GA	rohg
+														
+𐴆		c	c	c	c	;		affricate				ca	U+10D06: HANIFI ROHINGYA LETTER CA	rohg
+𐴅		ɟ	ɟ	j	j	j	ɟ	affricate				ja	U+10D05: HANIFI ROHINGYA LETTER JA	rohg
+														
+𐴉		f	f	f	f	f		fricative				fa	U+10D09: HANIFI ROHINGYA LETTER FA	rohg
+𐴜		v	v	v	v			fricative	obsolete			va	U+10D1C: HANIFI ROHINGYA LETTER VA	rohg
+𐴏		s	s	s	s	s		fricative				sa	U+10D0F: HANIFI ROHINGYA LETTER SA	rohg
+𐴎		z	z	z	z	z		fricative				za	U+10D0E: HANIFI ROHINGYA LETTER ZA	rohg
+𐴐		ʃ	ʃ	sh	S	c	ʃ	fricative				sha	U+10D10: HANIFI ROHINGYA LETTER SHA	rohg
+𐴈		x	x	kh	x	q		fricative				kha	U+10D08: HANIFI ROHINGYA LETTER KHA	rohg
+𐴇		h	h	h	h	h		fricative				ha	U+10D07: HANIFI ROHINGYA LETTER HA	rohg
+														
+𐴔		m	m	m	m	m		nasal				ma	U+10D14: HANIFI ROHINGYA LETTER MA	rohg
+𐴕		n	n	n	n	n		nasal				na	U+10D15: HANIFI ROHINGYA LETTER NA	rohg
+𐴛		ɲ	ɲ	nya	n	'	ɲ	nasal				nya = nayya	U+10D1B: HANIFI ROHINGYA LETTER NYA	rohg
+𐴚		ŋ	ŋ	nga	N	x	ŋ	nasal				nga = gan	U+10D1A: HANIFI ROHINGYA LETTER NGA	rohg
+														
+𐴖		ʋ v	ʋ	w	w	w	ʋ	liquid				wa	U+10D16: HANIFI ROHINGYA LETTER WA	rohg
+𐴗		u	u	w	w	[		liquid				kinna wa	U+10D17: HANIFI ROHINGYA LETTER KINNA WA	rohg
+𐴌		ɾ	ɾ	r	r	r	ɾ	liquid				ra	U+10D0C: HANIFI ROHINGYA LETTER RA	rohg
+𐴍		ɽ	ɽ	rr	r	/	ɽ	liquid				rra	U+10D0D: HANIFI ROHINGYA LETTER RRA	rohg
+𐴓		l	l	l	l	l		liquid				la	U+10D13: HANIFI ROHINGYA LETTER LA	rohg
+𐴘		j	j	y	y	y		liquid				ya	U+10D18: HANIFI ROHINGYA LETTER YA	rohg
+𐴙		i	i	y	y	]		liquid				kinna ya	U+10D19: HANIFI ROHINGYA LETTER KINNA YA	rohg
+														
+														
+														
+𐴀		ɔ ∅	‸	a	q	a	ɔ ʔ ∅	consonant				a	U+10D00: HANIFI ROHINGYA LETTER A	rohg
+𐴞		i	i	i	i	i	iː ĩ	vowel				i-for	U+10D1E: HANIFI ROHINGYA VOWEL I	rohg
+𐴀𐴞	s	i	ʔi					standalone vowel					U+10D00 U+10D1E: HANIFI ROHINGYA LETTER A, VOWEL I	
+𐴟		u	u	u	u	u	uː ũ	vowel				u-for	U+10D1F: HANIFI ROHINGYA VOWEL U	rohg
+𐴀𐴟	s	u	ʔu					standalone vowel					U+10D00 U+10D1F: HANIFI ROHINGYA LETTER A, VOWEL U	
+𐴠		e	e	e	e	e	eː ẽ	vowel				e-for	U+10D20: HANIFI ROHINGYA VOWEL E	rohg
+𐴀𐴠	s	e	ʔe					standalone vowel					U+10D00 U+10D20: HANIFI ROHINGYA LETTER A, VOWEL E	
+𐴡		o	o	o	o	o	oː õ	vowel				o-for	U+10D21: HANIFI ROHINGYA VOWEL O	rohg
+𐴀𐴡	s	o	ʔo					standalone vowel					U+10D00 U+10D21: HANIFI ROHINGYA LETTER A, VOWEL O	
+𐴝		a	a	a	a	v	aː ã	vowel				aa-for	U+10D1D: HANIFI ROHINGYA VOWEL A	rohg
+𐴀𐴝	s	a	ʔa		ɔ		ɔː ɔ̃	standalone vowel					U+10D00 U+10D1D: HANIFI ROHINGYA LETTER A, VOWEL A	
+														
+														
+														
+														
+𐴤			¹	a	^	H		tone mark				hārbāy	U+10D24: HANIFI ROHINGYA SIGN HARBAHAY​	rohg
+𐴥			²		^	T		tone mark				ṭelā	U+10D25: HANIFI ROHINGYA SIGN TAHALA​	rohg
+𐴦			³		^	N		tone mark				ṭānā	U+10D26: HANIFI ROHINGYA SIGN TANA​	rohg
+														
+														
+														
+														
+														
+			ˈ		[			superscript alef						
+			ʿ		[			hamza above						
+			˓		[			hamza below						
+			*		[			star						
+			~		[			maddah						
+			˘		[			vowel shortener						
+			⁎		[			special						
+			″		[			register change						
+			°		[			special						
+			˙		[			combining dot above						
+			ʼ		[			combining hook above						
+			ˆ		[			combining circumflex						
+			ˉ		[			combining macron						
+			̨		[			combining ogonek						
+			⁑		[			feminine marker						
+														
+														
+			‸					vowel carrier						
+														
+														
+𐴣		̃	˜	̃	[			nasalisation mark				na khonna	U+10D23: HANIFI ROHINGYA MARK NA KHONNA	rohg
+														
+														
+𐴢			͓		*	§		vowel silencer				sakin	U+10D22: HANIFI ROHINGYA MARK SAKIN	rohg
+			˟		\									
+			ˣ		\									
+			¯		\									
+			ʿ		\									
+														
+														
+			′		]			elision						
+			‵		]									
+			ʳ		]									
+														
+														
+			ˑ		/			nukta						
+			˙		/			nukta						
+			ˇ		/			nukta						
+			˷		/			nukta						
+			~		/			nukta						
+														
+														
+𐴧			&		&	S		gemination sign				tassi	U+10D27: HANIFI ROHINGYA SIGN TASSI​	rohg
+			˖		&			gemination						
+														
+			ː		:			length mark						
+														
+														
+			¨		+			abbreviation marker						
+														
+														
+			␣		]			zwsp						
+			-		]			word separator						
+														
+														
+			»		@			opening head mark						
+			«		@			closing head mark						
+			›		@			2nd opening head						
+			‹		@			2 closing head						
+			§ᵃ		@			honorific						
+			 §ᵇ		@			honorific						
+			§ᶜ		@			honorific						
+			°		@			extra						
+														
+														
+			¶		|			section divider						
+			|	.	|			section divider						
+			—		|			section divider						
+۔			.	.	.			full stop					U+06D4: ARABIC FULL STOP	rohg
+⋯			⋯	⋯	.			ellipsis						
+،			,	,	,			comma					U+060C: ARABIC COMMA	rohg
+:			:	:	:			colon						
+			ˉ	ˉ	:									
+؛			;	;	;			semicolon					U+061B: ARABIC SEMICOLON	rohg
+!			!	!	!			exclamation mark						
+؟			?	?	?			question mark					U+061F: ARABIC QUESTION MARK	rohg
+														
+														
+														
+“			“	“	<			quotation mark						
+‘			‘	‘	<			quotation mark						
+”			”	”	>			quotation mark						
+’			’	’	>			quotation mark						
+														
+														
+														
+(			(	(	(			parenthesis						
+[			[	[	(			bracket						
+)			)	)	)			parenthesis						
+]			]	]	)			bracket						
+														
+														
+-			-	-	-			hyphen						
+‑			‑	‑	-			non-breaking hyphen						
+–			–	–	-			en dash						
+—			—	—	-			em dash						
+														
+														
+ـ			_	_	_			baseline extender					U+0640: ARABIC TATWEEL	
+														
+ʼ			ʼ	ʼ				apostrophe						
+§			§	§				section sign						
+†			†	†				dagger						
+‡			‡	‡				double dagger						
+′			′	′				prime						
+″			″	″				double prime						
+														
+														
+														
+														
+														
+														
+\u200C			ₓ		=			zwnj						
+\u200D			₊		=			zwj						
+\u2067			ʳˡⁱ		=			rtl isolate						
+\u2066			ˡʳⁱ		=			ltr isolate						
+\u2068			ᶠˢⁱ		=			first-strong isolate						
+\u2069			ᵖᵈⁱ		=			pop direction isolate						
+\u202B			ʳˡᵉ		=			rtl embed						
+\u202A			ˡʳᵉ		=			ltr embed						
+\u202C			ᵖᵈᶠ		=			pop direction						
+\u200F			ʳˡᵐ		=			rtl mark						
+\u200E			ˡʳᵐ		=			ltr mark						
+\u034F			ᶜᵍʲ		=			combining grapheme joiner						
+\u061C			ᵃˡᵐ		=			arabic letter mark						
+														
+														
+														
+														
+𐴰			0̣	0	±	0		digit					U+10D30: HANIFI ROHINGYA DIGIT ZERO	rohg
+𐴱			1̣	1	±	1		digit					U+10D31: HANIFI ROHINGYA DIGIT ONE	rohg
+𐴲			2̣	2	±	2		digit					U+10D32: HANIFI ROHINGYA DIGIT TWO	rohg
+𐴳			3̣	3	±	3		digit					U+10D33: HANIFI ROHINGYA DIGIT THREE	rohg
+𐴴			4̣	4	±	4		digit					U+10D34: HANIFI ROHINGYA DIGIT FOUR	rohg
+𐴵			5̣	5	±	5		digit					U+10D35: HANIFI ROHINGYA DIGIT FIVE	rohg
+𐴶			6̣	6	±	6		digit					U+10D36: HANIFI ROHINGYA DIGIT SIX	rohg
+𐴷			7̣	7	±	7		digit					U+10D37: HANIFI ROHINGYA DIGIT SEVEN	rohg
+𐴸			8̣	8	±	8		digit					U+10D38: HANIFI ROHINGYA DIGIT EIGHT	rohg
+𐴹			9̣	9	±	9		digit					U+10D39: HANIFI ROHINGYA DIGIT NINE	rohg
+														
+														
+			‛		#			thousands separator						
+			·		#			decimal separator						
+			/		#			date separator						
+			¤		#			currency symbol						
+			¢		#									
+			#		#			number symbol						
+			¼		#			quarter						
+			½		#			half						
+			¾		#			three-quarters						
+			{10} etc		#			extended number						
+														
+														
+														
+%			%		%			percentage mark						
 
 
 
@@ -79,13 +220,15 @@ var spreadsheet = `
 
 var cols = {
 "class": 1,
-"key":2,
-"transcription":3,
-"transLoc": 4,
-"ipaLoc": 5,
+"ipaLoc": 2,
+"transLoc": 3,
+"transcription": 4,
+"key": 5,
+"kbd": 6,
+"latin": 7,
 "transckey": 0,
-"typeLoc": 7,
-"statusLoc": 8,
+"typeLoc": 8,
+"statusLoc": 9,
 "dvowel": 0,
 "ivowel": 0,
 "subj":0,
@@ -96,10 +239,10 @@ var cols = {
 "meaning":0,
 "shape": 0,
 "numLoc": 0,
-"nameLoc": 9,
-"nnameLoc": 10,
-"ucsName": 11,
-"block": 12,
+"nnameLoc": 11,
+"nameLoc": 12,
+"ucsName": 13,
+"block": 14,
 
-//"othertranscriptions": [[4, 'Elder'], [5, 'LB/ST'], [6, 'Medieval'], [7, 'AS/F']]
+"othertranscriptions": [[4, 'Latin']]
 }
