7e4c67235bb1c79345d15da42e15c97f54f56113
r12a <ishida@w3.org>
2021-03-30 10:51:07 +0100

    scriptdb/kali

---
+++ scriptdb/kali.js
@@ -0,0 +1,183 @@
+var spreadsheet = `
+ꤕ		p	p	p	p	l		stop					U+A915: KAYAH LI LETTER PA	kali
+ꤙ		b	b	b	b	e		stop					U+A919: KAYAH LI LETTER BA	kali
+ꤖ		Pʰ	pʰ	P	ph	n	pʰ	aspirated stop					U+A916: KAYAH LI LETTER PHA	kali
+ꤒ		t	t	t	t	j		stop					U+A912: KAYAH LI LETTER TA	kali
+ꤘ		d	d	d	d	[		stop					U+A918: KAYAH LI LETTER DA	kali
+ꤓ		Tʰ	tʰ	T	ht	o	tʰ	aspirated stop					U+A913: KAYAH LI LETTER HTA	kali
+ꤊ		k	k	k	k	a		stop					U+A90A: KAYAH LI LETTER KA	kali
+ꤌ		ɡ	ɡ	g	g	x	ɡ	stop	used for loan words				U+A90C: KAYAH LI LETTER GA	kali
+ꤋ		Kʰ	kʰ	K	kh	v	kʰ	stop					U+A90B: KAYAH LI LETTER KHA	kali
+														
+ꤡ		d͡ʒ	ʤ	d	c	z	d͡ʒ	affricate					U+A921: KAYAH LI LETTER CA	kali
+														
+ꤠ		v	v	v	v	q		fricative					U+A920: KAYAH LI LETTER VA	kali
+ꤞ		ʂ θ	ʂ	q	th	i	ʂ θ	fricative					U+A91E: KAYAH LI LETTER THA	kali
+ꤏ		Sʰ	sʰ	S	sh	c	sʰ	aspirated fricative					U+A90F: KAYAH LI LETTER SHA	kali
+ꤎ		s	s	s	s	w		fricative	used for loan words				U+A90E: KAYAH LI LETTER SA	kali
+ꤐ		z	z	z	zh	,		fricative					U+A910: KAYAH LI LETTER ZA	kali
+ꤟ		h	h	h	h	k	̤	fricative					U+A91F: KAYAH LI LETTER HA	kali
+														
+ꤗ		m	m	m	m	b		nasal					U+A917: KAYAH LI LETTER MA	kali
+ꤔ		n	n	n	n	s		nasal					U+A914: KAYAH LI LETTER NA	kali
+ꤑ		ɲ	ɲ	n	ny	p	ɲ	nasal					U+A911: KAYAH LI LETTER NYA	kali
+ꤍ		ŋ	ŋ	N	ng	'	ŋ	nasal					U+A90D: KAYAH LI LETTER NGA	kali
+														
+ꤝ		w	w	w	w	m		approximant					U+A91D: KAYAH LI LETTER WA	kali
+ꤚ		r	r	r	r	r		trill					U+A91A: KAYAH LI LETTER RA	kali
+ꤜ		l	l	l	l	;		lateral					U+A91C: KAYAH LI LETTER LA	kali
+ꤛ		j	j	j	j	d		approximant					U+A91B: KAYAH LI LETTER YA	kali
+														
+														
+ꤤ		i	i	i	i	y		vowel					U+A924: KAYAH LI LETTER I	kali
+ꤢꤦ	s	ɯ	aɯ		ư	F	ɯ ư	vowel					U+A922 U+A926: KAYAH LI LETTER A, VOWEL UE	kali
+ꤢꤨ	s	u	au		u	H		vowel					U+A922 U+A928: KAYAH LI LETTER A, VOWEL U	kali
+ꤢꤩ	s	e	ae		ê	J	ê	vowel					U+A922 U+A929: KAYAH LI LETTER A, VOWEL EE	kali
+ꤥ		o	o	o	ô	u	ô	vowel					U+A925: KAYAH LI LETTER OO	kali
+ꤢꤧ	s	Ɛ	aɛ		e	G	ɛ	vowel					U+A922 U+A927: KAYAH LI LETTER A, VOWEL E	kali
+ꤢꤪ	s	ɔ	aɔ		o	K	ɔ	vowel					U+A922 U+A92A: KAYAH LI LETTER A, VOWEL O	kali
+ꤣ		ə	ə	e	ơ	t	ə ơ	vowel					U+A923: KAYAH LI LETTER OE	kali
+ꤢ		a	‸	a	a	f		vowel					U+A922: KAYAH LI LETTER A	kali
+														
+														
+ꤛꤣ	s	ɯə	jə		ươ			non-breathy diphthong					U+A91B U+A923: KAYAH LI LETTER YA, LETTER OE	
+ꤟꤌꤣ	s	ɯ̤ə	hgə		ươ			breathy diphthong					U+A91F U+A90C U+A923: KAYAH LI LETTER HA, LETTER GA, LETTER OE	
+														
+														
+ꤣꤦ	s	əɯ			ơư			non-native vowel	loanwords				U+A923 U+A926: KAYAH LI LETTER OE, VOWEL UE	
+ꤣꤧ	s	ɯɛ			ưe			non-native vowel	loanwords				U+A923 U+A927: KAYAH LI LETTER OE, VOWEL E	
+ꤣꤨ	s	əu			ơu			non-native vowel	loanwords				U+A923 U+A928: KAYAH LI LETTER OE, VOWEL U	
+ꤣꤩ	s	əe			ơê			non-native vowel	loanwords				U+A923 U+A929: KAYAH LI LETTER OE, VOWEL EE	
+ꤣꤪ	s	əɔ			ơo			non-native vowel	loanwords				U+A923 U+A92A: KAYAH LI LETTER OE, VOWEL O	
+														
+														
+														
+ꤦ		ɯ	ɯ	U	ư			vowel-sign					U+A926: KAYAH LI VOWEL UE​	kali
+ꤨ		u	u	u	u			vowel-sign					U+A928: KAYAH LI VOWEL U​	kali
+ꤩ		e	e	e	ê	g		vowel-sign					U+A929: KAYAH LI VOWEL EE​	kali
+ꤧ		ɛ	ɛ	e	e			vowel-sign					U+A927: KAYAH LI VOWEL E​	kali
+ꤪ		ɔ	ɔ	o	o	h		vowel-sign					U+A92A: KAYAH LI VOWEL O​	kali
+														
+														
+														
+꤬		˨	¹	^	¹		˨ ¹	low tone marker					U+A92C: KAYAH LI TONE CALYA​	kali
+꤭		˧	³	^	³		˧ ³	mid tone marker					U+A92D: KAYAH LI TONE CALYA PLOPHU​	kali
+꤫		˥	²	^	²		˦ ² ˥	high tone marker					U+A92B: KAYAH LI TONE PLOPHU​	kali														
+														
+														
+														
+														
+														
+														
+														
+														
+														
+꤮		ː	ː	:		@		extended intonation					U+A92E: KAYAH LI SIGN CWI	kali
+														
+														
+														
+꤯			|	|	.	/		phrase/sentence delimiter					U+A92F: KAYAH LI SIGN SHYA	kali
+.			.	.	.			full stop					U+002E: FULL STOP	
+,			,	,	,	.		comma					U+002C: COMMA	kali
+:			:	:	:	$		colon					U+003A: COLON	
+;			;	;	;			semicolon					U+003B: SEMICOLON	
+!			!	!	!	!		exclamation mark					U+0021: EXCLAMATION MARK	kali
+?			?	?	?			question mark					U+003F: QUESTION MARK	kali
+														
+														
+…			…	.	…			ellipsis					U+2026: HORIZONTAL ELLIPSIS	
+⋯			⋯	.	⋯			midline ellipsis					U+22EF: MIDLINE HORIZONTAL ELLIPSIS	
+														
+														
+														
+“			 “	<	“	^		quotation mark					U+201C: LEFT DOUBLE QUOTATION MARK	kali
+”			”	>	”	&		quotation mark					U+201D: RIGHT DOUBLE QUOTATION MARK	kali
+‘			‘	<	‘			quotation mark					U+2018: LEFT SINGLE QUOTATION MARK	
+’			’	>	’			quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	
+														
+														
+														
+(			(	(	(	(		parenthesis					U+0028: LEFT PARENTHESIS	kali
+)			)	)	)	)		parenthesis					U+0029: RIGHT PARENTHESIS	kali
+[			[	(	[			bracket					U+005B: LEFT SQUARE BRACKET	
+]			]	)	]			bracket					U+005D: RIGHT SQUARE BRACKET	
+														
+														
+-			-	-	-	-		hyphen					U+2010: HYPHEN	
+‑			‑	-	‑			non-breaking hyphen					U+2011: NON-BREAKING HYPHEN	
+–			–	-	–	_		en dash					U+2013: EN DASH	
+—			—	-	—			em dash					U+2014: EM DASH	
+														
+														
+														
+														
+ʼ			ʼ		ʼ			apostrophe					U+02BC: MODIFIER LETTER APOSTROPHE	
+§			§		§			section sign					U+00A7: SECTION SIGN	
+†			†		†			dagger					U+2020: DAGGER	
+‡			‡		‡			double dagger					U+2021: DOUBLE DAGGER	
+′			′		′			prime					U+2032: PRIME	
+″			″		″			double prime					U+2033: DOUBLE PRIME	
+														
+														
+														
+														
+														
+														
+\u200C			ₓ	=				zwnj					U+200C: ZERO WIDTH NON-JOINER	
+\u200D			₊	=				zwj					U+200D: ZERO WIDTH JOINER	
+\u034F			ᶜᵍʲ	=				combining grapheme joiner					U+034F: COMBINING GRAPHEME JOINER​	
+														
+														
+														
+														
+꤁			1̣	±	1	1		digit						
+꤂			2̣	±	2	2		digit						
+꤃			3̣	±	3	3		digit						
+꤄			4̣	±	4	4		digit						
+꤅			5̣	±	5	5		digit						
+꤆			6̣	±	6	6		digit						
+꤇			7̣	±	7	7		digit						
+꤈			8̣	±	8	8		digit						
+꤉			9̣	±	9	9		digit						
+꤀			0̣	±	0	0		digit						
+														
+														
+														
+														
+%			%	%		%		percentage mark					U+0025: PERCENT SIGN	kali
+
+
+
+
+`
+
+
+var cols = {
+"class": 1,
+"ipaLoc": 2,
+"transLoc": 3,
+"key": 4,
+"transcription": 5,
+"kbd": 6,
+"latin": 7,
+"typeLoc": 8,
+"statusLoc": 9,
+"equiv": 0,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+"nnameLoc": 11,
+"nameLoc": 12,
+"ucsName": 13,
+"block": 14,
+//"transckey": 0,
+
+"othertranscriptions": [[5, 'Latin']]
+}
