70aa02295417723c5f87826ef7b3013b20c939e1
r12a <ishida@w3.org>
2021-08-26 19:23:48 +0100

    + scriptdb/arab-misc.js

---
+++ scriptdb/arab-misc.js
@@ -0,0 +1,59 @@
+var spreadsheet = `
+ځ								consonant					U+0681: ARABIC LETTER HAH WITH HAMZA ABOVE	🗸
+ݬ								consonant					U+076C: ARABIC LETTER REH WITH HAMZA ABOVE	🗸
+ࢡ								consonant					U+08A1: ARABIC LETTER BEH WITH HAMZA ABOVE	🗸
+ئ													U+0626: ARABIC LETTER YEH WITH HAMZA ABOVE	🗸
+ࢨ								consonant					U+08A8: ARABIC LETTER YEH WITH TWO DOTS BELOW AND HAMZA ABOVE	🗸
+ۆ								vowel					U+06C6: ARABIC LETTER OE	🗸
+ۊ													U+06CA: ARABIC LETTER WAW WITH TWO DOTS ABOVE	🗸
+ێ								vowel					U+06CE: ARABIC LETTER YEH WITH SMALL V	🗸
+														
+ڽ		ɲ						nasal		ڽڽڽ ڽ			U+06BD: ARABIC LETTER NOON WITH THREE DOTS ABOVE	🗸
+														
+ڵ		ɫ						velarised approximant		ڵڵڵ ڵ			U+06B5: ARABIC LETTER LAM WITH SMALL V	🗸
+ڶ		ɫ						velarised approximant	variant				U+06B6: ARABIC LETTER LAM WITH DOT ABOVE	🗸
+ڷ		ɫ						velarised approximant	variant				U+06B7: ARABIC LETTER LAM WITH THREE DOTS ABOVE	🗸ڕ		ɾ						flap		ـڕ ڕ			U+0695: ARABIC LETTER REH WITH SMALL V BELOW	🗸
+و								approximant/mater lectionis					U+0648: ARABIC LETTER WAW	🗸
+ڒ		ɾ						flap	variant				U+0692: ARABIC LETTER REH WITH SMALL V	🗸
+ڔ								consonant	variant				U+0694: ARABIC LETTER REH WITH DOT BELOW	🗸
+
+
+
+`
+
+
+
+var cols = {
+"class": 1,
+"ipaLoc": 2,
+"transcription": 3,
+"latin": 4,
+"key": 5,
+"transLoc": 6,
+"kbd": 7,
+"typeLoc": 8,
+"statusLoc": 9,
+
+"equiv": 0,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 10,
+"numLoc": 0,
+
+"nnameLoc": 11,
+"nameLoc": 12,
+"ucsName": 13,
+"block": 14,
+
+//"othertranscriptions": [[5, 'LOC']]
+}
+
+
+
+
