05a1b1b9e08b3f1f49786da9a4c6e5fd3c037e6f
r12a <ishida@w3.org>
2021-08-28 05:55:38 +0100

    + scriptdb/runr-misc

---
+++ scriptdb/runr-misc.js
@@ -0,0 +1,38 @@
+var spreadsheet = `
+ᛮ								golden number				U+16EE: RUNIC ARLAUG SYMBOL	🗸
+ᛯ								golden number				U+16EF: RUNIC TVIMADUR SYMBOL	🗸
+ᛰ								golden number				U+16F0: RUNIC BELGTHOR SYMBOL	🗸
+
+`
+
+
+var cols = {
+"class": 1,
+"ipaLoc": 2,
+"transcription": 3,
+"latin": 4,
+"key": 5,
+"transLoc": 6,
+"kbd": 7,
+"typeLoc": 8,
+"statusLoc": 9,
+
+"equiv": 0,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+
+"nnameLoc": 10,
+"nameLoc": 11,
+"ucsName": 12,
+"block": 13,
+
+//"othertranscriptions": [[3, 'Latin']]
+}
