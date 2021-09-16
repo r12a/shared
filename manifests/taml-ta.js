39994b79b78f9bb48c57e3453d9e5da0c6a92c14
r12a <ishida@w3.org>
2021-09-16 20:26:02 +0100

    + manifests/taml-ta.js

---
+++ manifests/taml-ta.js
@@ -0,0 +1,24 @@
+
+langTag='ta'
+scriptTag='taml'
+notesLangtag='ta'
+
+explainerDir='tamil'
+blockDir='tamil/'
+pickerDir='taml/'
+scriptSummaryTableName='ta'
+charUsageBCP='ta'
+orthogName = 'Tamil'
+indic = true
+
+pageWebfonts = ["Noto Serif Tamil WF"]
+defaultSize = '24px'
+
+
+
+// block settings
+
+languageName = 'Tamil'
+letterClassName = 'ta'
+lang = 'ta'
+direction = ''
