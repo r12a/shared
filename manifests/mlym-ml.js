9ab9c74ce5c32dac7068e7115853f9580154db3a
r12a <ishida@w3.org>
2021-09-17 06:36:19 +0100

    + manifests/mlym-ml

---
+++ manifests/mlym-ml.js
@@ -0,0 +1,24 @@
+
+langTag='ml'
+scriptTag='mlym'
+notesLangtag='ml'
+
+explainerDir='malayalam'
+blockDir='malayalam/'
+pickerDir='mlym/'
+scriptSummaryTableName='ml'
+charUsageBCP='ml'
+orthogName = 'Malayalam'
+indic = true
+
+pageWebfonts = ["Noto Serif Malayalam WF", "Noto Sans Malayalam WF"]
+defaultSize = '24px'
+
+
+
+// block settings
+
+languageName = 'Malayalam'
+letterClassName = 'ml'
+lang = 'ml'
+direction = ''
