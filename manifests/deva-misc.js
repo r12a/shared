61ee51a5a5f03568014d71a33c8c85fc4b2f2786
r12a <ishida@w3.org>
2021-10-01 17:05:13 +0100

    + deva-misc manifest and scriptdb

---
+++ manifests/deva-misc.js
@@ -0,0 +1,25 @@
+
+langTag='hi'
+scriptTag='deva'
+notesLangtag='misc'
+
+explainerDir=''
+blockDir='devanagari/'
+pickerDir='deva-all/'
+scriptSummaryTableName=''
+charUsageBCP=''
+orthogName = 'Devanagari'
+indic = true
+
+pageWebfonts = []
+defaultSize = '24px'
+
+
+
+
+// block settings
+
+languageName = 'Miscellanea'
+letterClassName = 'misc'
+lang = 'hi'
+direction = ''
---
+++ scriptdb/deva-misc.js
@@ -0,0 +1,41 @@
+var spreadsheet = `
+ऽ								letter					U+093D: DEVANAGARI SIGN AVAGRAHA	✓
+
+
+`
+
+
+latinPanel = 'aː ā æ bʱ dʱ ɖ ḍ ɖʱ ḍh d͡ʒ d͡ʒʱ ə eː ē ɛː ɡʱ ġ ɦ ḥ ɪ iː ī kʰ k͟h ɭ̆ ḷ ṁ ɳ ṇ ɲ ñ ŋ ṅ oː ō ɔː pʰ ɽ ṛ ɽʱ ṛh riː r̥̄ ʃ ś ṣ tʰ ṭ ʈʰ ṭh t͡ʃ t͡ʃʰ ʊ uː ū ʋ ɣ'
+
+
+var cols = {
+"class": 1,
+"ipaLoc": 2,
+"transcription": 3,
+"latin": 4,
+"key": 5,
+"transLoc": 6,
+"kbd": 7,
+"typeLoc": 8,
+"statusLoc": 9,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"transckey": 0,
+"numLoc": 0,
+"nnameLoc": 10,
+"nameLoc": 11,
+"ucsName": 12,
+"block": 13,
+
+//"othertranscriptions": [[3, 'ISO 15919']]
+}
+
+
+
