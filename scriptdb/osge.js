3b71d1b7e07f5238f6bd6e7316b94b57b229eb5a
r12a <ishida@w3.org>
2021-10-06 08:01:46 +0100

    osa: v29 updates

--- scriptdb/osge.js
+++ scriptdb/osge.js
@@ -1,121 +1,121 @@
 var spreadsheet = `
-𐓣		i	i		i	i	i	vowel		a		𐒻			U+104E3: OSAGE SMALL LETTER I	🗸
-𐓣͘	s	ĩ	į	ĩ į		i˜		nasalised vowel							U+104E3 U+0358: OSAGE SMALL LETTER I, COMBINING DOT ABOVE RIGHT	
-𐓶		ʉ y	u	ʉ	u	u	u	vowel		u		𐓎			U+104F6: OSAGE SMALL LETTER U	🗸
-𐓟		ε e	e	ε	e	ɛ	e	vowel		e		𐒷			U+104DF: OSAGE SMALL LETTER E	🗸
-𐓪		o	o		o	o	o	vowel		o		𐓂			U+104EA: OSAGE SMALL LETTER O	🗸
-𐓪͘	s	õ	ǫ	õ ǫ		o˜		nasalised vowel							U+104EA U+0358: OSAGE SMALL LETTER O, COMBINING DOT ABOVE RIGHT	
-𐓛		ə	ə	ə	e	ə	j	vowel		j		𐒳			U+104DB: OSAGE SMALL LETTER AH	🗸
-𐓛͘	s	ə̃	ę	ə̃ ę		ə˜		nasalised vowel							U+104DB U+0358: OSAGE SMALL LETTER AH, COMBINING DOT ABOVE RIGHT	
-𐓘		ɑ	a	ɑ	a	a	a	vowel		a		𐒰			U+104D8: OSAGE SMALL LETTER A	🗸
-𐓘͘	s	ã	ą	ã ą		a˜		nasalised vowel							U+104D8 U+0358: OSAGE SMALL LETTER A, COMBINING DOT ABOVE RIGHT	
-𐓙		aɪ	ai	ɪ	a	aᶦ		diphthong		i2		𐒱			U+104D9: OSAGE SMALL LETTER AI	🗸
-𐓚		aɪ̃	aį	ɪ̃	a	aⁱ̃		nasalised diphthong		a2		𐒲			U+104DA: OSAGE SMALL LETTER AIN	🗸
-𐓠		eɪ̃	eį	į	e	eⁱ̃		nasalised diphthong		e2		𐒸			U+104E0: OSAGE SMALL LETTER EIN	🗸
-𐓫		oɪ̃	oį		o	oⁱ̃		nasalised diphthong		o2		𐓃			U+104EB: OSAGE SMALL LETTER OIN	🗸
+𐓣	Ll	i	i		i	i	i	vowel		a		𐒻			U+104E3: OSAGE SMALL LETTER I	✓
+𐓣͘		ĩ	į	ĩ į		i˜		nasalised vowel							U+104E3 U+0358: OSAGE SMALL LETTER I, COMBINING DOT ABOVE RIGHT	
+𐓶	Ll	ʉ y	u	ʉ	u	u	u	vowel		u		𐓎			U+104F6: OSAGE SMALL LETTER U	✓
+𐓟	Ll	ε e	e	ε	e	ɛ	e	vowel		e		𐒷			U+104DF: OSAGE SMALL LETTER E	✓
+𐓪	Ll	o	o		o	o	o	vowel		o		𐓂			U+104EA: OSAGE SMALL LETTER O	✓
+𐓪͘		õ	ǫ	õ ǫ		o˜		nasalised vowel							U+104EA U+0358: OSAGE SMALL LETTER O, COMBINING DOT ABOVE RIGHT	
+𐓛	Ll	ə	ə	ə	e	ə	j	vowel		j		𐒳			U+104DB: OSAGE SMALL LETTER AH	✓
+𐓛͘		ə̃	ę	ə̃ ę		ə˜		nasalised vowel							U+104DB U+0358: OSAGE SMALL LETTER AH, COMBINING DOT ABOVE RIGHT	
+𐓘	Ll	ɑ	a	ɑ	a	a	a	vowel		a		𐒰			U+104D8: OSAGE SMALL LETTER A	✓
+𐓘͘		ã	ą	ã ą		a˜		nasalised vowel							U+104D8 U+0358: OSAGE SMALL LETTER A, COMBINING DOT ABOVE RIGHT	
+𐓙	Ll	aɪ	ai	ɪ	a	aᶦ		diphthong		i2		𐒱			U+104D9: OSAGE SMALL LETTER AI	✓
+𐓚	Ll	aɪ̃	aį	ɪ̃	a	aⁱ̃		nasalised diphthong		a2		𐒲			U+104DA: OSAGE SMALL LETTER AIN	✓
+𐓠	Ll	eɪ̃	eį	į	e	eⁱ̃		nasalised diphthong		e2		𐒸			U+104E0: OSAGE SMALL LETTER EIN	✓
+𐓫	Ll	oɪ̃	oį		o	oⁱ̃		nasalised diphthong		o2		𐓃			U+104EB: OSAGE SMALL LETTER OIN	✓
 																
 																
-𐒻		I	I		I	I	I	vowel			𐓣				U+104BB: OSAGE CAPITAL LETTER I	🗸
-𐒻͘	s	Ĩ	į	ĩ į		I˜		nasalised vowel							U+104BB U+0358: OSAGE CAPITAL LETTER I, COMBINING DOT ABOVE RIGHT	
-𐓎		Ʉ Y	U		U	U	U	vowel			𐓶				U+104CE: OSAGE CAPITAL LETTER U	🗸
-𐒷		Ε E	E		E	Ɛ	E	vowel			𐓟				U+104B7: OSAGE CAPITAL LETTER E	🗸
-𐓂		O	O		O	O	O	vowel			𐓪				U+104C2: OSAGE CAPITAL LETTER O	🗸
-𐓂͘	s	Õ	ǫ	õ ǫ		O˜		nasalised vowel							U+104C2 U+0358: OSAGE CAPITAL LETTER O, COMBINING DOT ABOVE RIGHT	
-𐒳		Ə	Ə		E	Ə	J	vowel			𐓛				U+104B3: OSAGE CAPITAL LETTER AH	🗸
-𐒳͘	s	Ə̃	Ę	ə̃ ę		Ə˜		nasalised vowel							U+104B3 U+0358: OSAGE CAPITAL LETTER AH, COMBINING DOT ABOVE RIGHT	
-𐒰		Ɑ	A		A	A	A	vowel			𐓘				U+104B0: OSAGE CAPITAL LETTER A	🗸
-𐒰͘	s	Ã	Ą	ã ą		A˜		nasalised vowel							U+104B0 U+0358: OSAGE CAPITAL LETTER A, COMBINING DOT ABOVE RIGHT	
-𐒱		AꞮ	Ai		A	Aᶦ		diphthong			𐓙				U+104B1: OSAGE CAPITAL LETTER AI	🗸
-𐒲		AꞮ̃	Aį		A	Aⁱ̃		nasalised diphthong			𐓚				U+104B2: OSAGE CAPITAL LETTER AIN	🗸
-𐒸		EꞮ̃	Eį		E	Eⁱ̃		nasalised diphthong			𐓠				U+104B8: OSAGE CAPITAL LETTER EIN	🗸
-𐓃		OꞮ̃	Oį		O	Oⁱ̃		nasalised diphthong			𐓫				U+104C3: OSAGE CAPITAL LETTER OIN	🗸
-																
-																
-																
-𐓬		p	p		p	p	p	plosive		p		𐓄			U+104EC: OSAGE SMALL LETTER PA	🗸
-𐓭		ʰp pp	hp	ʰp	p	pᶣ		pre-aspirated plosive	infrequent	p2		𐓅			U+104ED: OSAGE SMALL LETTER EHPA	🗸
-𐓜		bɹ	br	ɹ	b	bʳ	b	diphone		b		𐒴			U+104DC: OSAGE SMALL LETTER BRA	🗸
-𐓰		t	t		t	t	d	plosive		d		𐓈			U+104F0: OSAGE SMALL LETTER TA	🗸
-𐓱		ʰt tt	ht	ʰt	t	tᶣ		pre-aspirated plosive	infrequent	d2		𐓉			U+104F1: OSAGE SMALL LETTER EHTA	🗸
-𐓤		k	k		k	k	k	plosive		k2		𐒼			U+104E4: OSAGE SMALL LETTER KA	🗸
-𐓥		ʰk kk	hk	ʰk	k	kᶣ		pre-aspirated plosive	infrequent	k2		𐒽			U+104E5: OSAGE SMALL LETTER EHKA	🗸
-𐓦		kʲ	ky	kʲ	k	kʲ	q	palatalised plosive		q		𐒾			U+104E6: OSAGE SMALL LETTER KYA	🗸
-																
-𐓲		t͡s	c	t͡s	c	ʦ	r	affricate		r		𐓊			U+104F2: OSAGE SMALL LETTER TSA	🗸
-𐓳		ʰt͡s	hc	ʰt͡s	c	ʦᶣ		pre-aspirated affricate	infrequent	r2		𐓋			U+104F3: OSAGE SMALL LETTER EHTSA	🗸
-𐓴		t͡sʰ	ch	t͡sʰ	c	ʦʰ		aspirated affricate		t2		𐓌			U+104F4: OSAGE SMALL LETTER TSHA	🗸
-𐓝		t͡ʃ	č	t͡ʃ č	c	ʧ	c	affricate		c		𐒵			U+104DD: OSAGE SMALL LETTER CHA	🗸
-𐓞		ʰt͡ʃ	hč	ʰt͡ʃ	c	ʧᶣ		pre-aspirated affricate	infrequent	c2		𐒶			U+104DE: OSAGE SMALL LETTER EHCHA	🗸
-																
-𐓵		ð	ð	ð	d	ð	t	fricative		t		𐓍			U+104F5: OSAGE SMALL LETTER DHA	🗸
-𐓮		s	s		s	s	s	fricative		s		𐓆			U+104EE: OSAGE SMALL LETTER SA	🗸
-𐓺		z	z		z	z	z	fricative		z		𐓒			U+104FA: OSAGE SMALL LETTER ZA	🗸
-𐓯		ʃ	š	ʃ š	s	ʃ	v	fricative		v		𐓇			U+104EF: OSAGE SMALL LETTER SHA	🗸
-𐓻		ʒ	ž	ʒ ž	z	ʒ	y	fricative		y		𐓓			U+104FB: OSAGE SMALL LETTER ZHA	🗸
-𐓸		x	x		x	x	x	fricative		x		𐓐			U+104F8: OSAGE SMALL LETTER KHA	🗸
-𐓹		ɣ	ɣ	ɣ	y	ɣ		fricative		x2		𐓑			U+104F9: OSAGE SMALL LETTER GHA	🗸
-𐓡		h	h		h	h	h	fricative		h		𐒹			U+104E1: OSAGE SMALL LETTER HA	🗸
-𐓢		hʲ	hy	hʲ	h	hʲ	g	palatalised fricative		g		𐒺			U+104E2: OSAGE SMALL LETTER HYA	🗸
-																
-𐓨		m	m		m	m	m	nasal		m		𐓀			U+104E8: OSAGE SMALL LETTER MA	🗸
-𐓩		n	n		n	n	n	nasal		n		𐓁			U+104E9: OSAGE SMALL LETTER NA	🗸
-																
-𐓷		w	w		w	w	w	approximant		w		𐓏			U+104F7: OSAGE SMALL LETTER WA	🗸
-𐓧		l	l		l	l	l	approximant		l		𐒿			U+104E7: OSAGE SMALL LETTER LA	🗸
-																
-																
-𐓄		P	P		P	P	P	plosive			𐓬				U+104C4: OSAGE CAPITAL LETTER PA	🗸
-𐓅		ʰP PP	Hp		P	Pᶣ		pre-aspirated plosive	infrequent		𐓭				U+104C5: OSAGE CAPITAL LETTER EHPA	🗸
-𐒴		Bɹ	Br		B	Bʳ	B				𐓜				U+104B4: OSAGE CAPITAL LETTER BRA	🗸
-𐓈		T	T		T	T	D	plosive			𐓰				U+104C8: OSAGE CAPITAL LETTER TA	🗸
-𐓉		ʰT TT	Ht		T	Tᶣ		pre-aspirated plosive	infrequent		𐓱				U+104C9: OSAGE CAPITAL LETTER EHTA	🗸
-𐒼		K	K		K	K	K	plosive			𐓤				U+104BC: OSAGE CAPITAL LETTER KA	🗸
-𐒽		ʰK KK	Hk		K	Kᶣ		pre-aspirated plosive	infrequent		𐓥				U+104BD: OSAGE CAPITAL LETTER EHKA	🗸
-𐒾		Kʲ	Ky		K	Kʲ	Q	palatalised plosive			𐓦				U+104BE: OSAGE CAPITAL LETTER KYA	🗸
-																
-𐓊		T͡S	C		C	C	R	affricate			𐓲				U+104CA: OSAGE CAPITAL LETTER TSA	🗸
-𐓌		T͡Sʰ	Ch		C	Cʰ		pre-aspirated affricate			𐓴				U+104CC: OSAGE CAPITAL LETTER TSHA	🗸
-𐓋		ʰT͡S	Hc		C	Cᶣ		aspirated affricate	infrequent		𐓳				U+104CB: OSAGE CAPITAL LETTER EHTSA	🗸
-𐒵		T͡Ʃ	Č		C	ʨ	C	affricate			𐓝				U+104B5: OSAGE CAPITAL LETTER CHA	🗸
-𐒶		ʰT͡Ʃ	Hč		C	ʨᶣ		pre-aspirated affricate	infrequent		𐓞				U+104B6: OSAGE CAPITAL LETTER EHCHA	🗸
-																
-𐓍		Ð	Ð		D	Ð	T	fricative			𐓵				U+104CD: OSAGE CAPITAL LETTER DHA	🗸
-𐓆		S	S		S	S	S	fricative			𐓮				U+104C6: OSAGE CAPITAL LETTER SA	🗸
-𐓒		Z	Z		Z	Z	Z	fricative			𐓺				U+104D2: OSAGE CAPITAL LETTER ZA	🗸
-𐓇		Ʃ	Š		S	Ʃ	V	fricative			𐓯				U+104C7: OSAGE CAPITAL LETTER SHA	🗸
-𐓓		Ʒ	Ž		Z	Ʒ	Y	fricative			𐓻				U+104D3: OSAGE CAPITAL LETTER ZHA	🗸
-𐓐		X	X		X	X	X	fricative			𐓸				U+104D0: OSAGE CAPITAL LETTER KHA	🗸
-𐓑		Ɣ	Ɣ		Y	Ɣ		fricative			𐓹				U+104D1: OSAGE CAPITAL LETTER GHA	🗸
-𐒹		H	H		H	H	H	fricative			𐓡				U+104B9: OSAGE CAPITAL LETTER HA	🗸
-𐒺		Hʲ	Hy		H	Hʲ	G	palatalised fricative			𐓢				U+104BA: OSAGE CAPITAL LETTER HYA	🗸
-																
-𐓀		M	M		M	M	M	nasal			𐓨				U+104C0: OSAGE CAPITAL LETTER MA	🗸
-𐓁		N	N		N	N	N	nasal			𐓩				U+104C1: OSAGE CAPITAL LETTER NA	🗸
-																
-𐓏		W	W		W	W	W	approximant			𐓷				U+104CF: OSAGE CAPITAL LETTER WA	🗸
-𐒿		L	L		L	L	L	approximant			𐓧				U+104BF: OSAGE CAPITAL LETTER LA	🗸
+𐒻	Lu	I	I		I	I	I	vowel			𐓣				U+104BB: OSAGE CAPITAL LETTER I	✓
+𐒻͘		Ĩ	į	ĩ į		I˜		nasalised vowel							U+104BB U+0358: OSAGE CAPITAL LETTER I, COMBINING DOT ABOVE RIGHT	
+𐓎	Lu	Ʉ Y	U		U	U	U	vowel			𐓶				U+104CE: OSAGE CAPITAL LETTER U	✓
+𐒷	Lu	Ε E	E		E	Ɛ	E	vowel			𐓟				U+104B7: OSAGE CAPITAL LETTER E	✓
+𐓂	Lu	O	O		O	O	O	vowel			𐓪				U+104C2: OSAGE CAPITAL LETTER O	✓
+𐓂͘		Õ	ǫ	õ ǫ		O˜		nasalised vowel							U+104C2 U+0358: OSAGE CAPITAL LETTER O, COMBINING DOT ABOVE RIGHT	
+𐒳	Lu	Ə	Ə		E	Ə	J	vowel			𐓛				U+104B3: OSAGE CAPITAL LETTER AH	✓
+𐒳͘		Ə̃	Ę	ə̃ ę		Ə˜		nasalised vowel							U+104B3 U+0358: OSAGE CAPITAL LETTER AH, COMBINING DOT ABOVE RIGHT	
+𐒰	Lu	Ɑ	A		A	A	A	vowel			𐓘				U+104B0: OSAGE CAPITAL LETTER A	✓
+𐒰͘		Ã	Ą	ã ą		A˜		nasalised vowel							U+104B0 U+0358: OSAGE CAPITAL LETTER A, COMBINING DOT ABOVE RIGHT	
+𐒱	Lu	AꞮ	Ai		A	Aᶦ		diphthong			𐓙				U+104B1: OSAGE CAPITAL LETTER AI	✓
+𐒲	Lu	AꞮ̃	Aį		A	Aⁱ̃		nasalised diphthong			𐓚				U+104B2: OSAGE CAPITAL LETTER AIN	✓
+𐒸	Lu	EꞮ̃	Eį		E	Eⁱ̃		nasalised diphthong			𐓠				U+104B8: OSAGE CAPITAL LETTER EIN	✓
+𐓃	Lu	OꞮ̃	Oį		O	Oⁱ̃		nasalised diphthong			𐓫				U+104C3: OSAGE CAPITAL LETTER OIN	✓
 																
 																
 																
+𐓬	Ll	p	p		p	p	p	plosive		p		𐓄			U+104EC: OSAGE SMALL LETTER PA	✓
+𐓭	Ll	ʰp pp	hp	ʰp	p	pᶣ		pre-aspirated plosive	infrequent	p2		𐓅			U+104ED: OSAGE SMALL LETTER EHPA	✓
+𐓜	Ll	bɹ	br	ɹ	b	bʳ	b	diphone		b		𐒴			U+104DC: OSAGE SMALL LETTER BRA	✓
+𐓰	Ll	t	t		t	t	d	plosive		d		𐓈			U+104F0: OSAGE SMALL LETTER TA	✓
+𐓱	Ll	ʰt tt	ht	ʰt	t	tᶣ		pre-aspirated plosive	infrequent	d2		𐓉			U+104F1: OSAGE SMALL LETTER EHTA	✓
+𐓤	Ll	k	k		k	k	k	plosive		k2		𐒼			U+104E4: OSAGE SMALL LETTER KA	✓
+𐓥	Ll	ʰk kk	hk	ʰk	k	kᶣ		pre-aspirated plosive	infrequent	k2		𐒽			U+104E5: OSAGE SMALL LETTER EHKA	✓
+𐓦	Ll	kʲ	ky	kʲ	k	kʲ	q	palatalised plosive		q		𐒾			U+104E6: OSAGE SMALL LETTER KYA	✓
+																
+𐓲	Ll	t͡s	c	t͡s	c	ʦ	r	affricate		r		𐓊			U+104F2: OSAGE SMALL LETTER TSA	✓
+𐓳	Ll	ʰt͡s	hc	ʰt͡s	c	ʦᶣ		pre-aspirated affricate	infrequent	r2		𐓋			U+104F3: OSAGE SMALL LETTER EHTSA	✓
+𐓴	Ll	t͡sʰ	ch	t͡sʰ	c	ʦʰ		aspirated affricate		t2		𐓌			U+104F4: OSAGE SMALL LETTER TSHA	✓
+𐓝	Ll	t͡ʃ	č	t͡ʃ č	c	ʧ	c	affricate		c		𐒵			U+104DD: OSAGE SMALL LETTER CHA	✓
+𐓞	Ll	ʰt͡ʃ	hč	ʰt͡ʃ	c	ʧᶣ		pre-aspirated affricate	infrequent	c2		𐒶			U+104DE: OSAGE SMALL LETTER EHCHA	✓
+																
+𐓵	Ll	ð	ð	ð	d	ð	t	fricative		t		𐓍			U+104F5: OSAGE SMALL LETTER DHA	✓
+𐓮	Ll	s	s		s	s	s	fricative		s		𐓆			U+104EE: OSAGE SMALL LETTER SA	✓
+𐓺	Ll	z	z		z	z	z	fricative		z		𐓒			U+104FA: OSAGE SMALL LETTER ZA	✓
+𐓯	Ll	ʃ	š	ʃ š	s	ʃ	v	fricative		v		𐓇			U+104EF: OSAGE SMALL LETTER SHA	✓
+𐓻	Ll	ʒ	ž	ʒ ž	z	ʒ	y	fricative		y		𐓓			U+104FB: OSAGE SMALL LETTER ZHA	✓
+𐓸	Ll	x	x		x	x	x	fricative		x		𐓐			U+104F8: OSAGE SMALL LETTER KHA	✓
+𐓹	Ll	ɣ	ɣ	ɣ	y	ɣ		fricative		x2		𐓑			U+104F9: OSAGE SMALL LETTER GHA	✓
+𐓡	Ll	h	h		h	h	h	fricative		h		𐒹			U+104E1: OSAGE SMALL LETTER HA	✓
+𐓢	Ll	hʲ	hy	hʲ	h	hʲ	g	palatalised fricative		g		𐒺			U+104E2: OSAGE SMALL LETTER HYA	✓
+																
+𐓨	Ll	m	m		m	m	m	nasal		m		𐓀			U+104E8: OSAGE SMALL LETTER MA	✓
+𐓩	Ll	n	n		n	n	n	nasal		n		𐓁			U+104E9: OSAGE SMALL LETTER NA	✓
+																
+𐓷	Ll	w	w		w	w	w	approximant		w		𐓏			U+104F7: OSAGE SMALL LETTER WA	✓
+𐓧	Ll	l	l		l	l	l	approximant		l		𐒿			U+104E7: OSAGE SMALL LETTER LA	✓
+																
+																
+𐓄	Lu	P	P		P	P	P	plosive			𐓬				U+104C4: OSAGE CAPITAL LETTER PA	✓
+𐓅	Lu	ʰP PP	Hp		P	Pᶣ		pre-aspirated plosive	infrequent		𐓭				U+104C5: OSAGE CAPITAL LETTER EHPA	✓
+𐒴	Lu	Bɹ	Br		B	Bʳ	B				𐓜				U+104B4: OSAGE CAPITAL LETTER BRA	✓
+𐓈	Lu	T	T		T	T	D	plosive			𐓰				U+104C8: OSAGE CAPITAL LETTER TA	✓
+𐓉	Lu	ʰT TT	Ht		T	Tᶣ		pre-aspirated plosive	infrequent		𐓱				U+104C9: OSAGE CAPITAL LETTER EHTA	✓
+𐒼	Lu	K	K		K	K	K	plosive			𐓤				U+104BC: OSAGE CAPITAL LETTER KA	✓
+𐒽	Lu	ʰK KK	Hk		K	Kᶣ		pre-aspirated plosive	infrequent		𐓥				U+104BD: OSAGE CAPITAL LETTER EHKA	✓
+𐒾	Lu	Kʲ	Ky		K	Kʲ	Q	palatalised plosive			𐓦				U+104BE: OSAGE CAPITAL LETTER KYA	✓
+																
+𐓊	Lu	T͡S	C		C	C	R	affricate			𐓲				U+104CA: OSAGE CAPITAL LETTER TSA	✓
+𐓌	Lu	T͡Sʰ	Ch		C	Cʰ		pre-aspirated affricate			𐓴				U+104CC: OSAGE CAPITAL LETTER TSHA	✓
+𐓋	Lu	ʰT͡S	Hc		C	Cᶣ		aspirated affricate	infrequent		𐓳				U+104CB: OSAGE CAPITAL LETTER EHTSA	✓
+𐒵	Lu	T͡Ʃ	Č		C	ʨ	C	affricate			𐓝				U+104B5: OSAGE CAPITAL LETTER CHA	✓
+𐒶	Lu	ʰT͡Ʃ	Hč		C	ʨᶣ		pre-aspirated affricate	infrequent		𐓞				U+104B6: OSAGE CAPITAL LETTER EHCHA	✓
+																
+𐓍	Lu	Ð	Ð		D	Ð	T	fricative			𐓵				U+104CD: OSAGE CAPITAL LETTER DHA	✓
+𐓆	Lu	S	S		S	S	S	fricative			𐓮				U+104C6: OSAGE CAPITAL LETTER SA	✓
+𐓒	Lu	Z	Z		Z	Z	Z	fricative			𐓺				U+104D2: OSAGE CAPITAL LETTER ZA	✓
+𐓇	Lu	Ʃ	Š		S	Ʃ	V	fricative			𐓯				U+104C7: OSAGE CAPITAL LETTER SHA	✓
+𐓓	Lu	Ʒ	Ž		Z	Ʒ	Y	fricative			𐓻				U+104D3: OSAGE CAPITAL LETTER ZHA	✓
+𐓐	Lu	X	X		X	X	X	fricative			𐓸				U+104D0: OSAGE CAPITAL LETTER KHA	✓
+𐓑	Lu	Ɣ	Ɣ		Y	Ɣ		fricative			𐓹				U+104D1: OSAGE CAPITAL LETTER GHA	✓
+𐒹	Lu	H	H		H	H	H	fricative			𐓡				U+104B9: OSAGE CAPITAL LETTER HA	✓
+𐒺	Lu	Hʲ	Hy		H	Hʲ	G	palatalised fricative			𐓢				U+104BA: OSAGE CAPITAL LETTER HYA	✓
+																
+𐓀	Lu	M	M		M	M	M	nasal			𐓨				U+104C0: OSAGE CAPITAL LETTER MA	✓
+𐓁	Lu	N	N		N	N	N	nasal			𐓩				U+104C1: OSAGE CAPITAL LETTER NA	✓
+																
+𐓏	Lu	W	W		W	W	W	approximant			𐓷				U+104CF: OSAGE CAPITAL LETTER WA	✓
+𐒿	Lu	L	L		L	L	L	approximant			𐓧				U+104BF: OSAGE CAPITAL LETTER LA	✓
 																
 																
 																
-𐓬ʼ	s	pˀ	pˀ	pˀ		pˀ		ejective plosive							U+104EC U+02BC: OSAGE SMALL LETTER PA, MODIFIER LETTER APOSTROPHE	
-𐓰ʼ	s	dˀ	dˀ	dˀ		dˀ		ejective plosive							U+104F0 U+02BC: OSAGE SMALL LETTER TA, MODIFIER LETTER APOSTROPHE	
-𐓲ʼ	s	tsˀ	cˀ	tsˀ		ʦˀ		ejective affricate							U+104F0 U+02BC: OSAGE SMALL LETTER TA, MODIFIER LETTER APOSTROPHE	
-𐓤ʼ	s	kˀ	kˀ	kˀ		kˀ		ejective plosive							U+104E4 U+02BC: OSAGE SMALL LETTER KA, MODIFIER LETTER APOSTROPHE	
-																
-																
-𐓬𐓸	s	px	px			px		aspirated plosive							U+104EC U+104F8: OSAGE SMALL LETTER PA, OSAGE SMALL LETTER KHA	
-𐓬𐓯	s	pʃ	pš			pʃ		aspirated plosive							U+104EC U+104EF: OSAGE SMALL LETTER PA, OSAGE SMALL LETTER SHA	
-𐓰𐓸	s	tx	tx			tx		aspirated plosive							U+104F0 U+104F8: OSAGE SMALL LETTER TA, OSAGE SMALL LETTER KHA	
-𐓤𐓸	s	kx	kx			kx		aspirated plosive							U+104E4 U+104F8: OSAGE SMALL LETTER KA, OSAGE SMALL LETTER KHA	
-𐓤𐓯	s	pʃ	kš			kʃ		aspirated plosive							U+104E4 U+104EF: OSAGE SMALL LETTER KA, OSAGE SMALL LETTER SHA	
 																
 																
 																
-́			́		^	´		high tone	infrequent						U+0301: COMBINING ACUTE ACCENT​	🗸
-ʼ		ˀ	ˀ		]	ˀ		ejective indicator							U+02BC: MODIFIER LETTER APOSTROPHE	🗸
+𐓬ʼ		pˀ	pˀ	pˀ		pˀ		ejective plosive							U+104EC U+02BC: OSAGE SMALL LETTER PA, MODIFIER LETTER APOSTROPHE	
+𐓰ʼ		dˀ	dˀ	dˀ		dˀ		ejective plosive							U+104F0 U+02BC: OSAGE SMALL LETTER TA, MODIFIER LETTER APOSTROPHE	
+𐓲ʼ		tsˀ	cˀ	tsˀ		ʦˀ		ejective affricate							U+104F0 U+02BC: OSAGE SMALL LETTER TA, MODIFIER LETTER APOSTROPHE	
+𐓤ʼ		kˀ	kˀ	kˀ		kˀ		ejective plosive							U+104E4 U+02BC: OSAGE SMALL LETTER KA, MODIFIER LETTER APOSTROPHE	
+																
+																
+𐓬𐓸		px	px			px		aspirated plosive							U+104EC U+104F8: OSAGE SMALL LETTER PA, OSAGE SMALL LETTER KHA	
+𐓬𐓯		pʃ	pš			pʃ		aspirated plosive							U+104EC U+104EF: OSAGE SMALL LETTER PA, OSAGE SMALL LETTER SHA	
+𐓰𐓸		tx	tx			tx		aspirated plosive							U+104F0 U+104F8: OSAGE SMALL LETTER TA, OSAGE SMALL LETTER KHA	
+𐓤𐓸		kx	kx			kx		aspirated plosive							U+104E4 U+104F8: OSAGE SMALL LETTER KA, OSAGE SMALL LETTER KHA	
+𐓤𐓯		pʃ	kš			kʃ		aspirated plosive							U+104E4 U+104EF: OSAGE SMALL LETTER KA, OSAGE SMALL LETTER SHA	
+																
+																
+																
+́	Mn​		́		^	´		high tone	infrequent						U+0301: COMBINING ACUTE ACCENT​	✓
+ʼ	Lm	ˀ	ˀ		]	ˀ		ejective indicator							U+02BC: MODIFIER LETTER APOSTROPHE	✓
 																
 																
 																
@@ -139,65 +139,65 @@ var spreadsheet = `
 					[	⁑		feminine marker								
 																
 																
-͘		̃	̨		[	˜	f	vowel nasalisation							U+0358: COMBINING DOT ABOVE RIGHT​	🗸
-																
+͘	Mn​	̃	̨		[	˜	f	vowel nasalisation							U+0358: COMBINING DOT ABOVE RIGHT​	✓
+^	Sk					–		vowel nasalisation	archaic						U+005E: CIRCUMFLEX ACCENT	
 																
 																
 					&	&		repetition								
 					&	˖		gemination								
 																
 																
-̄			̄		:	ː		long vowel	infrequent						U+0304: COMBINING MACRON​	🗸
-̋			̋		:	˝		long high tone vowel	infrequent						U+030B: COMBINING DOUBLE ACUTE ACCENT​	🗸
+̄	Mn​		̄		:	ː		long vowel	infrequent						U+0304: COMBINING MACRON​	✓
+̋	Mn​		̋		:	˝		long high tone vowel	infrequent						U+030B: COMBINING DOUBLE ACUTE ACCENT​	✓
 																
 																
 					+	¨		abbreviation marker								
 																
 																
 																
-.			.		.	.		full stop							U+002E: FULL STOP	🗸
-,			,		,	,		comma							U+002C: COMMA	
-:			:		:	:		colon							U+003A: COLON	🗸
-;			;		;	;		semicolon							U+003B: SEMICOLON	🗸
-!			!		!	!		exclamation mark							U+0021: EXCLAMATION MARK	🗸
-?			?		?	?		question mark							U+003F: QUESTION MARK	🗸
+.	Po		.		.	.		full stop							U+002E: FULL STOP	✓
+,	Po		,		,	,		comma							U+002C: COMMA	
+:	Po		:		:	:		colon							U+003A: COLON	✓
+;	Po		;		;	;		semicolon							U+003B: SEMICOLON	✓
+!	Po		!		!	!		exclamation mark							U+0021: EXCLAMATION MARK	✓
+?	Po		?		?	?		question mark							U+003F: QUESTION MARK	✓
 																
 																
-…			…		.	…		ellipsis							U+2026: HORIZONTAL ELLIPSIS	🗸
-																
-																
-																
-“			“		<	 “		quotation mark							U+201C: LEFT DOUBLE QUOTATION MARK	🗸
-”			”		>	”		quotation mark							U+201D: RIGHT DOUBLE QUOTATION MARK	🗸
-‘			‘		<	‘		quotation mark							U+2018: LEFT SINGLE QUOTATION MARK	🗸
-’			’		>	’		quotation mark							U+2019: RIGHT SINGLE QUOTATION MARK	🗸
+…	Po		…		.	…		ellipsis							U+2026: HORIZONTAL ELLIPSIS	✓
 																
 																
 																
-(			(		(	(		parenthesis							U+0028: LEFT PARENTHESIS	🗸
-[			[		(	[		bracket							U+005B: LEFT SQUARE BRACKET	
-)			)		)	)		parenthesis							U+0029: RIGHT PARENTHESIS	🗸
-]			]		)	]		bracket							U+005D: RIGHT SQUARE BRACKET	
+“	Pi		“		<	 “		quotation mark							U+201C: LEFT DOUBLE QUOTATION MARK	✓
+”	Pf		”		>	”		quotation mark							U+201D: RIGHT DOUBLE QUOTATION MARK	✓
+‘	Pi		‘		<	‘		quotation mark							U+2018: LEFT SINGLE QUOTATION MARK	✓
+’	Pf		’		>	’		quotation mark							U+2019: RIGHT SINGLE QUOTATION MARK	✓
+																
+																
+																
+(	Ps		(		(	(		parenthesis							U+0028: LEFT PARENTHESIS	✓
+[	Ps		[		(	[		bracket							U+005B: LEFT SQUARE BRACKET	
+)	Pe		)		)	)		parenthesis							U+0029: RIGHT PARENTHESIS	✓
+]	Pe		]		)	]		bracket							U+005D: RIGHT SQUARE BRACKET	
 																
 																
 -			-		-	-		hyphen							U+2010: HYPHEN	
-‑			‑		-	‑		non-breaking hyphen							U+2011: NON-BREAKING HYPHEN	
-–			–		-	–		en dash							U+2013: EN DASH	🗸
-—			—		-	—		em dash							U+2014: EM DASH	🗸
+‑	Pd		‑		-	‑		non-breaking hyphen							U+2011: NON-BREAKING HYPHEN	
+–	Pd		–		-	–		en dash							U+2013: EN DASH	✓
+—	Pd		—		-	—		em dash							U+2014: EM DASH	✓
 																
 																
 																
 																
-ʼ			ʼ			ʼ		apostrophe							U+02BC: MODIFIER LETTER APOSTROPHE	🗸
-§			§			§		section sign							U+00A7: SECTION SIGN	
-†			†			†		dagger							U+2020: DAGGER	
-‡			‡			‡		double dagger							U+2021: DOUBLE DAGGER	
-′			′			′		prime							U+2032: PRIME	
-″			″			″		double prime							U+2033: DOUBLE PRIME	
+ʼ	Lm		ʼ			ʼ		apostrophe							U+02BC: MODIFIER LETTER APOSTROPHE	✓
+§	Po		§			§		section sign							U+00A7: SECTION SIGN	
+†	Po		†			†		dagger							U+2020: DAGGER	
+‡	Po		‡			‡		double dagger							U+2021: DOUBLE DAGGER	
+′	Po		′			′		prime							U+2032: PRIME	
+″	Po		″			″		double prime							U+2033: DOUBLE PRIME	
 																
 																
 																
-%					%	%		percentage mark							U+0025: PERCENT SIGN	
+%	Po				%	%		percentage mark							U+0025: PERCENT SIGN	
 
 
 
