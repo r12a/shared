2110b4580deddf843c8f589266a1cbdcc8e64d37
r12a <ishida@w3.org>
2021-10-12 09:00:30 +0100

    + manifests/java-misc

---
+++ manifests/java-misc.js
@@ -0,0 +1,29 @@
+
+langTag='jv'
+scriptTag='java'
+notesLangtag='jv'
+
+explainerDir=''
+blockDir='javanese/'
+pickerDir='java/'
+scriptSummaryTableName=''
+charUsageBCP=''
+orthogName = 'Javanese'
+indic = true
+
+pageWebfonts = ["Tuladha Jejeg WF", "Noto Sans Javanese WF"]
+defaultSize = '24px'
+
+
+
+
+autoIndex = true
+
+
+
+// block settings
+
+languageName = 'Other information'
+letterClassName = 'misc'
+lang = 'jv'
+direction = ''
