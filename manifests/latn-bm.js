0a6116f82d4e1890008b6e4edfe4878fe69fa3c9
r12a <ishida@w3.org>
2021-11-14 11:50:10 +0000

    manifests/latn-bm.js

---
+++ manifests/latn-bm.js
@@ -0,0 +1,27 @@
+
+langTag='bm'
+scriptTag='latn'
+notesLangtag='bm'
+
+explainerDir='latin/bm'
+blockDir='latin/'
+pickerDir='latn-bm/'
+scriptSummaryTableName='bm'
+charUsageBCP='bm'
+orthogName = 'Latin'
+indic = false
+
+pageWebfonts = ["Gentium Plus WF"]
+defaultSize = '24px'
+
+
+autoIndex = true
+
+
+
+// block settings
+
+languageName = 'Bambara'
+letterClassName = 'bm'
+lang = 'bm'
+direction = ''
