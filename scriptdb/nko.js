8fc999070aa3fee4863f63f8c11dfe02d2dd248c
r12a <ishida@w3.org>
2021-11-21 13:52:00 +0000

    scriptdb/nko: Major update, including key-arranged cols

--- scriptdb/nko.js
+++ scriptdb/nko.js
@@ -1,88 +1,82 @@
 var spreadsheet = `
-ߔ	Lo	p	p	p	p	p	plosive				U+07D4: NKO LETTER PA	✓
-ߕ	Lo	t	t	t	t	t	plosive				U+07D5: NKO LETTER TA	✓
-ߗ	Lo	t͡ʃ	ʧ	c	t	c	affricate				U+07D7: NKO LETTER CHA	✓
-ߞ	Lo	k	k	k	k	k	plosive				U+07DE: NKO LETTER KA	✓
-												
 ߓ	Lo	b	b	b	b	b	plosive				U+07D3: NKO LETTER BA	✓
+ߗ	Lo	c	c	t͡ʃ	ʧ	c	affricate				U+07D7: NKO LETTER CHA	✓
 ߘ	Lo	d	d	d	d	d	plosive				U+07D8: NKO LETTER DA	✓
-ߖ	Lo	d͡ʒ	ʤ	j	d	j	affricate				U+07D6: NKO LETTER JA	✓
-ߜ	Lo	ɡ͡b	gᵇ	gb	g	g	plosive				U+07DC: NKO LETTER GBA	✓
-												
 ߝ	Lo	f	f	f	f	f	fricative				U+07DD: NKO LETTER FA	✓
-ߛ	Lo	s	s	s	s	s	fricative				U+07DB: NKO LETTER SA	✓
 ߤ	Lo	h	h	h	h	h	fricative				U+07E4: NKO LETTER HA	✓
-												
+ߖ	Lo	j	j	d͡ʒ	ʤ	j	affricate				U+07D6: NKO LETTER JA	✓
+ߜ	Lo	g	gb	ɡ͡b	gᵇ	g	plosive				U+07DC: NKO LETTER GBA	✓
+ߞ	Lo	k	k	k	k	k	plosive				U+07DE: NKO LETTER KA	✓
+ߟ	Lo	l	l	l	l	l	approximant				U+07DF: NKO LETTER LA	✓
 ߡ	Lo	m	m	m	m	m	nasal				U+07E1: NKO LETTER MA	✓
 ߣ	Lo	n	n	n	n	n	nasal				U+07E3: NKO LETTER NA	✓
-ߢ	Lo	ɲ	ɲ	ɲ	n		nasal				U+07E2: NKO LETTER NYA	✓
-ߒ	Lo	n̩ ŋ	ŋ̩	n'	n	N	nasal syllable				U+07D2: NKO LETTER N	✓
-												
-ߥ	Lo	w	w	w	w	w	approximant				U+07E5: NKO LETTER WA	✓
+ߢ	Lo	n	ɲ	ɲ	ɲ		nasal				U+07E2: NKO LETTER NYA	✓
+ߒ	Lo	N	ń	n̩	ŋ̩	N	nasal syllable				U+07D2: NKO LETTER N	✓
+ߠ	Lo	N	n	n	ł		abstract consonant			ná-woloso	U+07E0: NKO LETTER NA WOLOSO	✓
+ߧ	Lo	N	y	ɲ	y		abstract consonant			ɲá-woloso	U+07E7: NKO LETTER NYA WOLOSO	✓
+ߔ	Lo	p	p	p	p	p	plosive				U+07D4: NKO LETTER PA	✓
 ߙ	Lo	r	r	r	r	r	trill				U+07D9: NKO LETTER RA	✓
-ߚ	Lo	rr	ṙ	rr	r	R	trill				U+07DA: NKO LETTER RRA	✓
-ߟ	Lo	l	l	l	l	l	approximant				U+07DF: NKO LETTER LA	✓
-ߠ	Lo	ł	ł	nw	l		abstract consonant				U+07E0: NKO LETTER NA WOLOSO	✓
-ߦ	Lo	j	j	y	y	y	approximant				U+07E6: NKO LETTER YA	✓
-ߧ	Lo	ñ	y	yw	j		abstract consonant				U+07E7: NKO LETTER NYA WOLOSO	✓
+ߚ	Lo	r	rr	rr	ṙ	R	trill				U+07DA: NKO LETTER RRA	✓
+ߛ	Lo	s	s	s	s	s	fricative				U+07DB: NKO LETTER SA	✓
+ߕ	Lo	t	t	t	t	t	plosive				U+07D5: NKO LETTER TA	✓
+ߥ	Lo	w	w	w	w	w	approximant				U+07E5: NKO LETTER WA	✓
+ߦ	Lo	y	y	j	j	y	approximant				U+07E6: NKO LETTER YA	✓
 												
 												
-ߨ	Lo	d͡ʒ	ʤ̄				consonant	archaic			U+07E8: NKO LETTER JONA JA	✓
-ߩ	Lo	t͡ʃ	ʧ̄				consonant	archaic			U+07E9: NKO LETTER JONA CHA	✓
-ߪ	Lo	r rː	r̄				consonant	archaic			U+07EA: NKO LETTER JONA RA	✓
+												
+ߨ	Lo			d͡ʒ	ʤ̄		consonant	archaic			U+07E8: NKO LETTER JONA JA	✓
+ߩ	Lo			t͡ʃ	ʧ̄		consonant	archaic			U+07E9: NKO LETTER JONA CHA	✓
+ߪ	Lo			r rː	r̄		consonant	archaic			U+07EA: NKO LETTER JONA RA	✓
 												
 												
 ߌ	Lo	i	i	i	i	i	vowel				U+07CC: NKO LETTER I	✓
-ߎ	Lo	u	u	u	i	u	vowel				U+07CE: NKO LETTER U	✓
+ߎ	Lo	u	u	u	u	u	vowel				U+07CE: NKO LETTER U	✓
+ߍ	Lo	E	ɛ	ɛ	ɛ	e	vowel				U+07CD: NKO LETTER E	✓
 ߋ	Lo	e	e	e	e	E	vowel				U+07CB: NKO LETTER EE	✓
+ߐ	Lo	O	ɔ	ɔ	ɔ	o	vowel				U+07D0: NKO LETTER O	✓
 ߏ	Lo	o	o	o	o	O	vowel				U+07CF: NKO LETTER OO	✓
-ߍ	Lo	ɛ	ɛ	ɛ	e	e	vowel				U+07CD: NKO LETTER E	✓
-ߐ	Lo	ɔ	ɔ	ɔ	o	o	vowel				U+07D0: NKO LETTER O	✓
 ߊ	Lo	a	a	a	a	a	vowel				U+07CA: NKO LETTER A	✓
 												
 												
-߫	Mn​	˦	ˊ	́	^		tone mark				U+07EB: NKO COMBINING SHORT HIGH TONE​	✓
-߯	Mn​	˦	ʺ	́	^	.	tone mark				U+07EF: NKO COMBINING LONG HIGH TONE​	✓
-߬	Mn​	˨	ˋ	̀	^	±	tone mark				U+07EC: NKO COMBINING SHORT LOW TONE​	✓
-߰	Mn​	˨	˵	̀	^	#	tone mark				U+07F0: NKO COMBINING LONG LOW TONE​	✓
-߭	Mn​	˨˦	ˇ	̌	^		tone mark				U+07ED: NKO COMBINING SHORT RISING TONE​	✓
-߱	Mn​	˨˦	˅	̀́	^	<	tone mark				U+07F1: NKO COMBINING LONG RISING TONE​	✓
-߮	Mn​	˦˨	ˆ	́̀	^	^	tone mark				U+07EE: NKO COMBINING LONG DESCENDING TONE​	✓
-ߴ	Lm	˦	◌́	ˊ	^		tone mark				U+07F4: NKO HIGH TONE APOSTROPHE	✓
-ߵ	Lm	˨	◌̀	ˋ	^		tone mark				U+07F5: NKO LOW TONE APOSTROPHE	✓
+߫	Mn​	^	́	˦	ˊ		tone mark				U+07EB: NKO COMBINING SHORT HIGH TONE​	✓
+߯	Mn​	^	́	˦	ʺ	.	tone mark				U+07EF: NKO COMBINING LONG HIGH TONE​	✓
+߬	Mn​	^	̀	˨	ˋ	±	tone mark				U+07EC: NKO COMBINING SHORT LOW TONE​	✓
+߰	Mn​	^	̀	˨	˵	#	tone mark				U+07F0: NKO COMBINING LONG LOW TONE​	✓
+߭	Mn​	^	̌	˨˦	ˇ		tone mark				U+07ED: NKO COMBINING SHORT RISING TONE​	✓
+߱	Mn​	^	̀́	˨˦	˅	<	tone mark				U+07F1: NKO COMBINING LONG RISING TONE​	✓
+߮	Mn​	^	́̀	˦˨	ˆ	^	tone mark				U+07EE: NKO COMBINING LONG DESCENDING TONE​	✓
+ߴ	Lm	^	ˊ	˦	◌́		tone mark				U+07F4: NKO HIGH TONE APOSTROPHE	✓
+ߵ	Lm	^	ˋ	˨	◌̀		tone mark				U+07F5: NKO LOW TONE APOSTROPHE	✓
 												
 												
 												
-ߓ߭	s	bʰ	b³	bʰ	B		plosive	non-native			U+07D3 U+07ED: NKO LETTER BA, COMBINING SHORT RISING TONE	
-ߕ߭	s	tˤ	t³	ᵵ	T		plosive	non-native ط			U+07D5 U+07ED: NKO LETTER TA, COMBINING SHORT RISING TONE	
-ߙ߳	s	dʰ	rˑ	dʰ	D		plosive	non-native			U+07D9 U+07F3: NKO LETTER RA, COMBINING DOUBLE DOT ABOVE	
-ߘ߭	s	dˤ	d³	ᵭ	D		consonant	non-native			U+07D8 U+07ED: NKO LETTER DA, COMBINING SHORT RISING TONE	
-ߞ߫	s	q	k¹	q	Q		plosive	non-native ق			U+07DE U+07EB: NKO LETTER KA, COMBINING SHORT HIGH TONE	
-ߜ߳	s	kp	gᵇˑ	kp	K		plosive	non-native			U+07DC U+07F3: NKO LETTER GBA, COMBINING DOUBLE DOT ABOVE	
-ߜ߭	s	ɡ	gᵇ³	ɡ	G		plosive	non-native			U+07DC U+07ED: NKO LETTER GBA, COMBINING SHORT RISING TONE	
-												
-ߝ߭	s	v	f³	v	V		fricative	non-native			U+07DD U+07ED: NKO LETTER FA, COMBINING SHORT RISING TONE	
-ߛ߳	s	θ	sˑ	θ	Q		fricative	ث non-native			U+07DB U+07F3: NKO LETTER SA, COMBINING DOUBLE DOT ABOVE	
-ߗ߭	s	ð ʒ	ʧ³	ð ʒ	D		fricative	 non-native ذ			U+07D7 U+07ED: NKO LETTER CHA, COMBINING SHORT RISING TONE	
-ߛ߫	s	sˤ	s¹	ᵴ	S		fricative	 non-native ص			U+07DB U+07EB: NKO LETTER SA, COMBINING SHORT HIGH TONE	
-ߖ߭	s	z	ʤ³	z	Z		fricative	non-native			U+07D6 U+07ED: NKO LETTER JA, COMBINING SHORT RISING TONE	
-ߖ߳	s	zˤ	ʤˑ	ƶ	Z		fricative	ظ non-native			U+07D6 U+07F3: NKO LETTER JA, COMBINING DOUBLE DOT ABOVE	
-ߛ߭	s	ʃ	s³	ʃ	S		fricative	ش non-native			U+07DB U+07ED: NKO LETTER SA, COMBINING SHORT RISING TONE	
-ߜ߫	s	ɣ	gᵇ¹	ɣ	Y		fricative	non-native غ			U+07DC U+07EB: NKO LETTER GBA, COMBINING SHORT HIGH TONE	
-ߞ߭	s	x	k³	x	X		fricative	non-native			U+07DE U+07ED: NKO LETTER KA, COMBINING SHORT RISING TONE	
-ߤ߭	s	ħ	h³	ḥ	H		fricative	non-native			U+07E4 U+07ED: NKO LETTER HA, COMBINING SHORT RISING TONE	
-ߙ߭	s	ʀ	r³	ʀ	R		fricative	non-native			U+07D9 U+07ED: NKO LETTER RA, COMBINING SHORT RISING TONE	
-												
-ߡ߭	s	mʰ	m³	mʰ	M		nasal	non-native			U+07E1 U+07ED: NKO LETTER MA, COMBINING SHORT RISING TONE	
-ߢ߭	s	nʰ	ɲ³	nʰ	N		nasal	non-native			U+07E2 U+07ED: NKO LETTER NYA, COMBINING SHORT RISING TONE	
-												
-ߟ߭	s	lʰ	l³	lʰ	J		approximant	non-native			U+07DF U+07ED: NKO LETTER LA, COMBINING SHORT RISING TONE	
-ߦ߭	s	yʰ	y³	yʰ	Y		approximant	non-native			U+07E6 U+07ED: NKO LETTER YA, COMBINING SHORT RISING TONE	
+ߓ߭	s	B	bʰ	bʰ	b³		plosive	non-native			U+07D3 U+07ED: NKO LETTER BA, COMBINING SHORT RISING TONE	
+ߙ߳	s	D	dʰ	dʰ	rˑ		plosive	non-native			U+07D9 U+07F3: NKO LETTER RA, COMBINING DOUBLE DOT ABOVE	
+ߘ߭	s	D	ᵭ	dˤ	d³		consonant	non-native			U+07D8 U+07ED: NKO LETTER DA, COMBINING SHORT RISING TONE	
+ߗ߭	s	D	ð ʒ	ð ʒ	ʧ³		fricative	 non-native ذ			U+07D7 U+07ED: NKO LETTER CHA, COMBINING SHORT RISING TONE	
+ߜ߭	s	G	ɡ	ɡ	gᵇ³		plosive	non-native			U+07DC U+07ED: NKO LETTER GBA, COMBINING SHORT RISING TONE	
+ߤ߭	s	H	ḥ	ħ	h³		fricative	non-native			U+07E4 U+07ED: NKO LETTER HA, COMBINING SHORT RISING TONE	
+ߟ߭	s	J	lʰ	lʰ	l³		approximant	non-native			U+07DF U+07ED: NKO LETTER LA, COMBINING SHORT RISING TONE	
+ߜ߳	s	K	kp	kp	gᵇˑ		plosive	non-native			U+07DC U+07F3: NKO LETTER GBA, COMBINING DOUBLE DOT ABOVE	
+ߡ߭	s	M	mʰ	mʰ	m³		nasal	non-native			U+07E1 U+07ED: NKO LETTER MA, COMBINING SHORT RISING TONE	
+ߢ߭	s	N	nʰ	nʰ	ɲ³		nasal	non-native			U+07E2 U+07ED: NKO LETTER NYA, COMBINING SHORT RISING TONE	
+ߙ߭	s	R	ʀ	ʀ	r³		fricative	non-native			U+07D9 U+07ED: NKO LETTER RA, COMBINING SHORT RISING TONE	
+ߛ߫	s	S	ᵴ	sˤ	s¹		fricative	 non-native ص			U+07DB U+07EB: NKO LETTER SA, COMBINING SHORT HIGH TONE	
+ߛ߭	s	S	ʃ	ʃ	s³		fricative	ش non-native			U+07DB U+07ED: NKO LETTER SA, COMBINING SHORT RISING TONE	
+ߕ߭	s	T	ᵵ	tˤ	t³		plosive	non-native ط			U+07D5 U+07ED: NKO LETTER TA, COMBINING SHORT RISING TONE	
+ߞ߫	s	Q	q	q	k¹		plosive	non-native ق			U+07DE U+07EB: NKO LETTER KA, COMBINING SHORT HIGH TONE	
+ߛ߳	s	Q	θ	θ	sˑ		fricative	ث non-native			U+07DB U+07F3: NKO LETTER SA, COMBINING DOUBLE DOT ABOVE	
+ߝ߭	s	V	v	v	f³		fricative	non-native			U+07DD U+07ED: NKO LETTER FA, COMBINING SHORT RISING TONE	
+ߞ߭	s	X	x	x	k³		fricative	non-native			U+07DE U+07ED: NKO LETTER KA, COMBINING SHORT RISING TONE	
+ߜ߫	s	Y	ɣ	ɣ	gᵇ¹		fricative	non-native غ			U+07DC U+07EB: NKO LETTER GBA, COMBINING SHORT HIGH TONE	
+ߦ߭	s	Y	yʰ	yʰ	y³		approximant	non-native			U+07E6 U+07ED: NKO LETTER YA, COMBINING SHORT RISING TONE	
+ߖ߭	s	Z	z	z	ʤ³		fricative	non-native			U+07D6 U+07ED: NKO LETTER JA, COMBINING SHORT RISING TONE	
+ߖ߳	s	Z	ƶ	zˤ	ʤˑ		fricative	ظ non-native			U+07D6 U+07F3: NKO LETTER JA, COMBINING DOUBLE DOT ABOVE	
 												
 												
-ߊ߳	s	ʕa	aˑ		A		vowel	non-native عَ			U+07CA U+07F3: NKO LETTER A, COMBINING DOUBLE DOT ABOVE	
-ߋ߳	s	ǝ	eˑ		E		vowel	non-native			U+07CB U+07F3: NKO LETTER EE, COMBINING DOUBLE DOT ABOVE	
-ߎ߳	s	y	uˑ		U		vowel	non-native			U+07CE U+07F3: NKO LETTER U, COMBINING DOUBLE DOT ABOVE	
+ߊ߳	s	A	y	ʕa	aˑ		vowel	non-native عَ			U+07CA U+07F3: NKO LETTER A, COMBINING DOUBLE DOT ABOVE	
+ߋ߳	s	E	ǝ	ǝ	eˑ		vowel	non-native			U+07CB U+07F3: NKO LETTER EE, COMBINING DOUBLE DOT ABOVE	
+ߎ߳	s	U	ʕa	y	uˑ		vowel	non-native			U+07CE U+07F3: NKO LETTER U, COMBINING DOUBLE DOT ABOVE	
 												
 												
 												
@@ -90,113 +84,114 @@ var spreadsheet = `
 												
 												
 												
-߷	Po		¶		|		major section terminator				U+07F7: NKO SYMBOL GBAKURUNEN	✓
-߸	Po		,		,	,	comma				U+07F8: NKO COMMA	✓
-،	Po		⹁		,		arabic comma				U+060C: ARABIC COMMA	
-؛	Po		;	;	;		arabic semicolon				U+061B: ARABIC SEMICOLON	
-.	Po		.	.	.		full stop				U+002E: FULL STOP	
-:	Po		:	:	:		colon				U+003A: COLON	
-߹	Po		!		!	!	exclamation mark				U+07F9: NKO EXCLAMATION MARK	✓
-؟	Po		?		?		arabic question mark				U+061F: ARABIC QUESTION MARK	
+߷	Po	|			¶		major section terminator				U+07F7: NKO SYMBOL GBAKURUNEN	✓
+߸	Po	,			,	,	comma				U+07F8: NKO COMMA	✓
+،	Po	,			⹁		arabic comma				U+060C: ARABIC COMMA	
+؛	Po	;	;		;		arabic semicolon				U+061B: ARABIC SEMICOLON	
+:	Po	:	:		:		colon				U+003A: COLON	
+.	Po	.	.		.		full stop				U+002E: FULL STOP	
+߹	Po	!			!	!	exclamation mark				U+07F9: NKO EXCLAMATION MARK	✓
+؟	Po	?			?		arabic question mark				U+061F: ARABIC QUESTION MARK	
 												
 												
-…	Po		…	…	.		ellipsis				U+2026: HORIZONTAL ELLIPSIS	
+…	Po	.	…		…		ellipsis				U+2026: HORIZONTAL ELLIPSIS	
 												
 												
+⸜	Pi	<	“				quotation mark				U+2E1C: LEFT LOW PARAPHRASE BRACKET	
+«	Pi	<	“		«		quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	Pi	<	‘		‹		quotation mark				U+2039: SINGLE LEFT-POINTING ANGLE QUOTATION MARK	
+“	Pi	<	“		 “		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
+‘	Pi	<	‘		‘		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+⸝	Pf	>	”				quotation mark				U+2E1D: RIGHT LOW PARAPHRASE BRACKET	
+»	Pf	>	”		»		quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+›	Pf	>	’		›		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+”	Pf	>	”		”		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
+’	Pf	>	’		’		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK													
 												
-«	Pi		«	“	<		quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
-»	Pf		»	”	>		quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
-‹	Pi		‹	‘	<		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
-›	Pf		›	’	>		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
-“	Pi		 “	“	<		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
-”	Pf		”	”	>		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
-‘	Pi		‘	‘	<		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
-’	Pf		’	’	>		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
-												
-												
-												
-(	Ps		(	(	(		parenthesis				U+0028: LEFT PARENTHESIS	
-)	Ps		)	)	)		parenthesis				U+0029: RIGHT PARENTHESIS	
-[	Pe		[	[	(		bracket				U+005B: LEFT SQUARE BRACKET	
-]	Pe		]	]	)		bracket				U+005D: RIGHT SQUARE BRACKET	
-												
-												
--	Pd		-	-	-		hyphen				U+2010: HYPHEN	
-‑	Pd		‑	‑	-		non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
-–	Pd		–	–	-		en dash				U+2013: EN DASH	
-—	Pd		—	—	-		em dash				U+2014: EM DASH	
+(	Ps	(	(		(		parenthesis				U+0028: LEFT PARENTHESIS	
+[	Ps	(	[		[		bracket				U+005B: LEFT SQUARE BRACKET	
+﴿	Ps	(	(		﴿		quotation bracket				U+FD3F: ORNATE RIGHT PARENTHESIS	
+)	Pe	)	)		)		parenthesis				U+0029: RIGHT PARENTHESIS	
+]	Pe	)	]		]		bracket				U+005D: RIGHT SQUARE BRACKET	
+﴾	Pe	)	)		﴾		quotation bracket				U+FD3E: ORNATE LEFT PARENTHESIS	
 												
 												
-												
-߶	So		–			/	grammatical marker				U+07F6: NKO SYMBOL OO DENNEN	✓
+-	Pd	-	-		-		hyphen				U+2010: HYPHEN	
+‑	Pd	-	‑		‑		non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
+–	Pd	-	–		–		en dash				U+2013: EN DASH	
+—	Pd	-	—		—		em dash				U+2014: EM DASH	
 												
 												
 												
-												
-߲	Mn​	̃	˜	̃	[		nasalisation mark				U+07F2: NKO COMBINING NASALIZATION MARK​	✓
-												
-												
-ߑ	Lo		͞		*	§	vowel killer				U+07D1: NKO LETTER DAGBASINNA	✓
+߶	So	]			–	/	grammatical marker				U+07F6: NKO SYMBOL OO DENNEN	✓
 												
 												
 												
 												
-߳	Mn​		ˑ		/	%	repertoire extension				U+07F3: NKO COMBINING DOUBLE DOT ABOVE​	✓
+߲	Mn​	[	̃	̃	˜		nasalisation mark			kánnadiyalan	U+07F2: NKO COMBINING NASALIZATION MARK​	✓
+												
+												
+ߑ	Lo	*			͞	§	vowel killer				U+07D1: NKO LETTER DAGBASINNA	✓
 												
 												
 												
 												
-												
-												
-߽	Mn​		¨		+		abbreviation mark		ߘߊ߲ߕߦߊߟߊ߲	dantayalan	U+07FD NKO DANTAYALAN	✓
+߳	Mn​	/			ˑ	%	repertoire extension				U+07F3: NKO COMBINING DOUBLE DOT ABOVE​	✓
 												
 												
 												
 												
 												
 												
-ߺ	Lm		_		_		baseline extender				U+07FA: NKO LAJANYALAN	✓
+߽	Mn​	+			¨		abbreviation mark		ߘߊ߲ߕߦߊߟߊ߲	dantayalan	U+07FD NKO DANTAYALAN	✓
 												
 												
 												
 												
-\u200B	Cf		␣		]		zero-width space				U+200B: ZERO WIDTH SPACE	
-\u200C	Cf		ᶻʷⁿʲ		=		zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
-\u200D	Cf		ᶻʷʲ		=		zero-width joiner				U+200D: ZERO WIDTH JOINER	
-\u034F	Mn		ᶜᵍʲ		=		combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER​	
-\u2060	Cf		ʷʲ		=		word joiner				U+2060 WORD JOINER	
 												
-\u2067	Cf		ʳˡⁱ		=		rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE	
-\u2066	Cf		ˡʳⁱ		=		ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE	
-\u2068	Cf		ᶠˢⁱ		=		first-strong isolate				U+2068: FIRST STRONG ISOLATE	
-\u2069	Cf		ᵖᵈⁱ		=		pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE	
-\u202B	Cf		ʳˡᵉ		=		rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING	
-\u202A	Cf		ˡʳᵉ		=		ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING	
-\u202C	Cf		ᵖᵈᶠ		=		pop direction				U+202C: POP DIRECTIONAL FORMATTING	
-\u200F	Cf		ʳˡᵐ		=		rtl mark				U+200F: RIGHT-TO-LEFT MARK	
-\u200E	Cf		ˡʳᵐ		=		ltr mark				U+200E: LEFT-TO-RIGHT MARK	
-\u061C	Cf		ᵃˡᵐ		=		arabic letter mark				U+061C: ARABIC LETTER MARK	
+												
+ߺ	Lm	_			_		baseline extender				U+07FA: NKO LAJANYALAN	✓
 												
 												
 												
 												
-߀	Nd		0̣	0	±	0	digit				U+07C0: NKO DIGIT ZERO	✓
-߁	Nd		1̣	1	±	1	digit				U+07C1: NKO DIGIT ONE	✓
-߂	Nd		2̣	2	±	2	digit				U+07C2: NKO DIGIT TWO	✓
-߃	Nd		3̣	3	±	3	digit				U+07C3: NKO DIGIT THREE	✓
-߄	Nd		4̣	4	±	4	digit				U+07C4: NKO DIGIT FOUR	✓
-߅	Nd		5̣	5	±	5	digit				U+07C5: NKO DIGIT FIVE	✓
-߆	Nd		6̣	6	±	6	digit				U+07C6: NKO DIGIT SIX	✓
-߇	Nd		7̣	7	±	7	digit				U+07C7: NKO DIGIT SEVEN	✓
-߈	Nd		8̣	8	±	8	digit				U+07C8: NKO DIGIT EIGHT	✓
-߉	Nd		9̣	9	±	9	digit				U+07C9: NKO DIGIT NINE	✓
+\u200B	Cf	]			␣		zero-width space				U+200B: ZERO WIDTH SPACE	
+\u200C	Cf	=			ᶻʷⁿʲ		zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
+\u200D	Cf	=			ᶻʷʲ		zero-width joiner				U+200D: ZERO WIDTH JOINER	
+\u034F	Mn	=			ᶜᵍʲ		combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER​	
+\u2060	Cf	=			ʷʲ		word joiner				U+2060 WORD JOINER	
+												
+\u2067	Cf	=			ʳˡⁱ		rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE	
+\u2066	Cf	=			ˡʳⁱ		ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE	
+\u2068	Cf	=			ᶠˢⁱ		first-strong isolate				U+2068: FIRST STRONG ISOLATE	
+\u2069	Cf	=			ᵖᵈⁱ		pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE	
+\u202B	Cf	=			ʳˡᵉ		rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING	
+\u202A	Cf	=			ˡʳᵉ		ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING	
+\u202C	Cf	=			ᵖᵈᶠ		pop direction				U+202C: POP DIRECTIONAL FORMATTING	
+\u200F	Cf	=			ʳˡᵐ		rtl mark				U+200F: RIGHT-TO-LEFT MARK	
+\u200E	Cf	=			ˡʳᵐ		ltr mark				U+200E: LEFT-TO-RIGHT MARK	
+\u061C	Cf	=			ᵃˡᵐ		arabic letter mark				U+061C: ARABIC LETTER MARK	
 												
 												
-߾	Sc		¤		#		dorome currency sign				U+07FE: NKO DOROME SIGN	✓
-߿	Sc		¢		#		taman currency sign				U+07FF: NKO TAMAN SIGN	✓
 												
 												
-٪	Po		%		%		arabic percent sign				U+066A: ARABIC PERCENT SIGN	
+߁	Nd	±	1		1̣	1	digit				U+07C1: NKO DIGIT ONE	✓
+߂	Nd	±	2		2̣	2	digit				U+07C2: NKO DIGIT TWO	✓
+߃	Nd	±	3		3̣	3	digit				U+07C3: NKO DIGIT THREE	✓
+߄	Nd	±	4		4̣	4	digit				U+07C4: NKO DIGIT FOUR	✓
+߅	Nd	±	5		5̣	5	digit				U+07C5: NKO DIGIT FIVE	✓
+߆	Nd	±	6		6̣	6	digit				U+07C6: NKO DIGIT SIX	✓
+߇	Nd	±	7		7̣	7	digit				U+07C7: NKO DIGIT SEVEN	✓
+߈	Nd	±	8		8̣	8	digit				U+07C8: NKO DIGIT EIGHT	✓
+߉	Nd	±	9		9̣	9	digit				U+07C9: NKO DIGIT NINE	✓
+߀	Nd	±	0		0̣	0	digit				U+07C0: NKO DIGIT ZERO	✓
+												
+												
+߾	Sc	#			¤		dorome currency sign				U+07FE: NKO DOROME SIGN	✓
+߿	Sc	#			¢		taman currency sign				U+07FF: NKO TAMAN SIGN	✓
+												
+												
+٪	Po	%			%		arabic percent sign				U+066A: ARABIC PERCENT SIGN	
 
 
 
@@ -211,10 +206,10 @@ latinPanel = '́ ̀ ̌ ̂ ̃ á à ã ǎ ɛ ɛ́ ɛ̀ ɛ̃ é è ẽ í ì i
 
 var cols = {
 "class": 1,
-"ipaLoc": 2,
-"transLoc":3,
-"transcription":4,
-"key":5,
+"key":2,
+"transcription":3,
+"ipaLoc": 4,
+"transLoc":5,
 "kbd": 6,
 "typeLoc": 7,
 "statusLoc": 8,
@@ -235,5 +230,5 @@ var cols = {
 "ucsName": 11,
 "block": 12,
 
-"othertranscriptions": [[4, 'Latin']]
+"othertranscriptions": [[3, 'Latin']]
 }
