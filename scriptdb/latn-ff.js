8450cfffe3e54355bbc1aa5e9d5af6df53af3e76
r12a <ishida@w3.org>
2021-11-26 19:35:43 +0000

    scriptdb/latn-ff: Add capital letters

--- scriptdb/latn-ff.js
+++ scriptdb/latn-ff.js
@@ -1,33 +1,32 @@
 var spreadsheet = `
 b	Ll	b	b		b		consonant			U+0062: LATIN SMALL LETTER B	
-ɓ	Ll	ɓ	ɓ		b		consonant			U+0253: LATIN SMALL LETTER B WITH HOOK	
+ɓ	Ll	ɓ	ɓ		b		consonant			U+0253: LATIN SMALL LETTER B WITH HOOK	✓
 mb		ᵐb	mb				pre-nasalised consonant			U+006D U+0062: LATIN SMALL LETTER M, SMALL LETTER B	
 nb		ᵐb	nb				pre-nasalised consonant	Guinea		U+006E U+0062: LATIN SMALL LETTER N, SMALL LETTER B	
 c	Ll	c t͡ʃ	c				consonant			U+0063: LATIN SMALL LETTER C	
 d	Ll	d	d		d		consonant			U+0064: LATIN SMALL LETTER D	
-ɗ	Ll	ɗ	ɗ		d		consonant			U+0257: LATIN SMALL LETTER D WITH HOOK	
+ɗ	Ll	ɗ	ɗ		d		consonant			U+0257: LATIN SMALL LETTER D WITH HOOK	✓
 nd		ⁿd	nd				pre-nasalised consonant			U+006E U+0064: LATIN SMALL LETTER N, SMALL LETTER D	
 f	Ll	f	f				consonant			U+0066: LATIN SMALL LETTER F	
 g	Ll	ɡ	g				consonant			U+0067: LATIN SMALL LETTER G	
-ɠ	Ll		ɠ		g			Guinea		U+0260: LATIN SMALL LETTER G WITH HOOK	
+ɠ	Ll	ɠ	ɠ		g		consonant	Guinea		U+0260: LATIN SMALL LETTER G WITH HOOK	✓
 ng		ᵑɡ	ng				pre-nasalised consonant			U+006E U+0067: LATIN SMALL LETTER N, SMALL LETTER G	
 h	Ll	h	h				consonant			U+0068: LATIN SMALL LETTER H	
 j	Ll	ɟ d͡ʒ	j				consonant			U+006A: LATIN SMALL LETTER J	
-nj		ᶮj	nj				pre-nasalised consonant			U+006E U+006A: LATIN SMALL LETTER N, SMALL LETTER J	
+nj		ᶮɟ ᶮd͡ʒ	nj				pre-nasalised consonant			U+006E U+006A: LATIN SMALL LETTER N, SMALL LETTER J	
 k	Ll	k	k		k		consonant			U+006B: LATIN SMALL LETTER K	
 l	Ll	l	l				consonant			U+006C: LATIN SMALL LETTER L	
 m	Ll	m	m				consonant			U+006D: LATIN SMALL LETTER M	
 n	Ll	n	n		n		consonant			U+006E: LATIN SMALL LETTER N	
-ŋ	Li	ŋ	ŋ		n		consonant			U+014B: LATIN SMALL LETTER ENG	
-ɲ	Ll	ɲ	ɲ		n		consonant	Guinea; Mali, Burkina Faso		U+0272: LATIN SMALL LETTER N WITH LEFT HOOK	
-ñ	Ll	ɲ	ñ		n		consonant	Senegal, Gambia, Mauritania; Sierra Leone, Guinea-Bissau, Liberia		U+00F1: LATIN SMALL LETTER N WITH TILDE	
+ŋ	Li	ŋ	ŋ		n		consonant			U+014B: LATIN SMALL LETTER ENG	✓
+ɲ	Ll	ɲ	ɲ		n		consonant	Guinea; Mali, Burkina Faso		U+0272: LATIN SMALL LETTER N WITH LEFT HOOK	✓
+ñ	Ll	ɲ	ñ		n		consonant	Senegal, Gambia, Mauritania; Sierra Leone, Guinea-Bissau, Liberia		U+00F1: LATIN SMALL LETTER N WITH TILDE	✓
 ny		ɲ	ny				consonant	Nigeria; Niger, Cameroon, Chad, CAR		U+006E U+0079: LATIN SMALL LETTER N, SMALL LETTER Y	
-p	Ll	p	p				consonant			U+0070: LATIN SMALL LETTER P	✓
+p	Ll	p	p				consonant			U+0070: LATIN SMALL LETTER P	
 r	Ll	r ɾ	r				consonant			U+0072: LATIN SMALL LETTER R	
 s	Ll	s	s		s		consonant			U+0073: LATIN SMALL LETTER S	
 t	Ll	t	t		t		consonant			U+0074: LATIN SMALL LETTER T	
 w	Ll	w	w				consonant			U+0077: LATIN SMALL LETTER W	
-x	Ll		x							U+0078: LATIN SMALL LETTER X	
 y	Ll	j	y		y		consonant			U+0079: LATIN SMALL LETTER Y	
 ƴ	Ll	ʔʲ	ƴ		y		consonant			U+01B4: LATIN SMALL LETTER Y WITH HOOK	✓
 ʼy		ʔʲ	ʼy		y		consonant	Nigeria		U+02BC U+0079: MODIFIER LETTER APOSTROPHE, SMALL LETTER Y	
@@ -39,36 +38,50 @@ v	Ll	v	v				consonant	loan words only (esp. Arabic)		U+0076: LATIN SMALL LETTER 
 q	Ll	q	q				consonant	loan words only (esp. Arabic)		U+0071: LATIN SMALL LETTER Q	
 sh		ʃ	sh				consonant	loan words only (esp. Arabic)		U+0073 U+0068: LATIN SMALL LETTER S, SMALL LETTER H	
 x	Ll	x	x				consonant	loan words only (esp. Arabic)		U+0078: LATIN SMALL LETTER X	
-z	Ll	z	z				consonant	loan words only (esp. Arabic)		U+007A: LATIN SMALL LETTER Z												
+z	Ll	z	z				consonant	loan words only (esp. Arabic)		U+007A: LATIN SMALL LETTER Z	
 											
 											
 											
-‘y		j̰	ʼy				consonant			U+2018 U+0079: LEFT SINGLE QUOTATION MARK, SMALL LETTER Y	
+B	Ll	b	B		B		consonant			U+0042: LATIN CAPITAL LETTER B	
+Ɓ	Ll	ɓ	Ɓ		B		consonant			U+0181: LATIN CAPITAL LETTER B WITH HOOK	✓
+Mb		ᵐb	Mb				pre-nasalised consonant			U+004D U+0062: LATIN CAPITAL LETTER M, SMALL LETTER B	
+Nb		ᵐb	Nb				pre-nasalised consonant	Guinea		U+004E U+0062: LATIN CAPITAL LETTER N, SMALL LETTER B	
+C	Ll	c t͡ʃ	C				consonant			U+0043: LATIN CAPITAL LETTER C	
+D	Ll	d	D		D		consonant			U+0044: LATIN CAPITAL LETTER D	
+Ɗ	Ll	ɗ	Ɗ		D		consonant			U+018A: LATIN CAPITAL LETTER D WITH HOOK	✓
+Nd		ⁿd	Nd				pre-nasalised consonant			U+004E U+0064: LATIN CAPITAL LETTER N, SMALL LETTER D	
+F	Ll	f	F				consonant			U+0046: LATIN CAPITAL LETTER F	
+G	Ll	ɡ	G				consonant			U+0047: LATIN CAPITAL LETTER G	
+Ɠ	Ll	ɠ	Ɠ		G		consonant	Guinea		U+0193: LATIN CAPITAL LETTER G WITH HOOK	
+Ng		ᵑɡ	Ng				pre-nasalised consonant			U+004E U+0067: LATIN CAPITAL LETTER N, SMALL LETTER G	
+H	Ll	h	H				consonant			U+0048: LATIN CAPITAL LETTER H	
+J	Ll	ɟ d͡ʒ	J				consonant			U+004A: LATIN CAPITAL LETTER J	
+Nj		ᶮɟ ᶮd͡ʒ	Nj				pre-nasalised consonant			U+004E U+006A: LATIN CAPITAL LETTER N, SMALL LETTER J	
+K	Ll	k	K		K		consonant			U+004B: LATIN CAPITAL LETTER K	
+L	Ll	l	L				consonant			U+004C: LATIN CAPITAL LETTER L	
+M	Ll	m	M				consonant			U+004D: LATIN CAPITAL LETTER M	
+N	Ll	n	N		N		consonant			U+004E: LATIN CAPITAL LETTER N	
+Ŋ	Li	ŋ	Ŋ		N		consonant			U+014A: LATIN CAPITAL LETTER ENG	✓
+Ɲ	Ll	ɲ	Ɲ		N		consonant	Guinea; Mali, Burkina Faso		U+019D: LATIN CAPITAL LETTER N WITH LEFT HOOK	✓
+Ñ	Ll	ɲ	Ñ		N		consonant	Senegal, Gambia, Mauritania; Sierra Leone, Guinea-Bissau, Liberia		U+00D1: LATIN CAPITAL LETTER N WITH TILDE	
+Ny		ɲ	Ny				consonant	Nigeria; Niger, Cameroon, Chad, CAR		U+004E U+0079: LATIN CAPITAL LETTER N, SMALL LETTER Y	
+P	Ll	p	P				consonant			U+0050: LATIN CAPITAL LETTER P	
+R	Ll	r ɾ	R				consonant			U+0052: LATIN CAPITAL LETTER R	
+S	Ll	s	S		S		consonant			U+0053: LATIN CAPITAL LETTER S	
+T	Ll	t	T		T		consonant			U+0054: LATIN CAPITAL LETTER T	
+W	Ll	w	W				consonant			U+0057: LATIN CAPITAL LETTER W	
+Y	Ll	j	Y		Y		consonant			U+0059: LATIN CAPITAL LETTER Y	
+Ƴ	Ll	ʔʲ	Ƴ		Y		consonant			U+01B3: LATIN CAPITAL LETTER Y WITH HOOK	✓
+ʼY		ʔʲ	ʼY		Y		consonant	Nigeria		U+02BC U+0059: MODIFIER LETTER APOSTROPHE, CAPITAL LETTER Y	
+'Y		ʔʲ	ʼY				consonant	Nigeria (ASCII alternative)		U+0059: LATIN CAPITAL LETTER Y	
 											
-Ɓ	Lu	ɓ bˤ	Ɓ		B		consonant			U+0181: LATIN CAPITAL LETTER B WITH HOOK	✓
-TS		sʼ t͡sʼ	TS		T		consonant			U+0054 U+0053: LATIN CAPITAL LETTER T, CAPITAL LETTER S	
-Ɗ	Lu	ɗ	Ɗ		D		consonant			U+018A: LATIN CAPITAL LETTER D WITH HOOK	✓
-B	Lu	b	B		¶B		consonant			U+0042: LATIN CAPITAL LETTER B	
-T	Lu	t	T		¶T		consonant			U+0054: LATIN CAPITAL LETTER T	
-D	Lu	d	D		¶D		consonant			U+0044: LATIN CAPITAL LETTER D	
-C	Lu	t͡ʃ	C				consonant			U+0043: LATIN CAPITAL LETTER C	
-J	Lu	d͡ʒ ʒ	J				consonant			U+004A: LATIN CAPITAL LETTER J	
-G	Lu	ɡ	G				consonant			U+0047: LATIN CAPITAL LETTER G	
-K	Lu	k	K		¶K		consonant			U+004B: LATIN CAPITAL LETTER K	
-Ƙ	Lu	kʼ	Ƙ		K		consonant			U+0198: LATIN CAPITAL LETTER K WITH HOOK	✓
-F	Lu	ɸ	F				consonant			U+0046: LATIN CAPITAL LETTER F	
-S	Lu	s	S		¶S		consonant			U+0053: LATIN CAPITAL LETTER S	
-Z	Lu	z	Z				consonant			U+005A: LATIN CAPITAL LETTER Z	
-SH		ʃ	SH		S		consonant			U+0053 U+0048: LATIN CAPITAL LETTER S, CAPITAL LETTER H	
-H	Lu	h	H				consonant			U+0048: LATIN CAPITAL LETTER H	
-M	Lu	m	M				consonant			U+004D: LATIN CAPITAL LETTER M	
-N	Lu	n	N				consonant			U+004E: LATIN CAPITAL LETTER N	
-W	Lu	w	W				consonant			U+0057: LATIN CAPITAL LETTER W	
-R	Lu	ɽ ɾ	R				consonant			U+0052: LATIN CAPITAL LETTER R	
-R̃		r	R̃				consonant	academic use only		U+0052 U+0303: LATIN CAPITAL LETTER R, COMBINING TILDE	
-L	Lu	l	L				consonant			U+004C: LATIN CAPITAL LETTER L	
-Y	Lu	j	Y		¶Y		consonant			U+0059: LATIN CAPITAL LETTER Y	
-Ƴ	Lu	ʔʲ	Ƴ		Y		consonant			U+01B3: LATIN CAPITAL LETTER Y WITH HOOK	✓
+											
+V	Ll	v	V				consonant	loan words only (esp. Arabic)		U+0056: LATIN CAPITAL LETTER V	
+Q	Ll	q	Q				consonant	loan words only (esp. Arabic)		U+0051: LATIN CAPITAL LETTER Q	
+SH		ʃ	SH				consonant	loan words only (esp. Arabic)		U+0053 U+0048: LATIN CAPITAL LETTER S, CAPITAL LETTER H	
+X	Ll	x	X				consonant	loan words only (esp. Arabic)		U+0058: LATIN CAPITAL LETTER X	
+Z	Ll	z	Z				consonant	loan words only (esp. Arabic)		U+005A: LATIN CAPITAL LETTER Z	
+											
 											
 											
 											
@@ -107,11 +120,11 @@ A	Lu	a aː	A				vowel			U+0041: LATIN CAPITAL LETTER A
 …	Po		…	…	.		ellipsis			U+2026: HORIZONTAL ELLIPSIS	
 											
 											
-«	Pi		«		<		quotation mark			U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+«	Pi		«		<		quotation mark			U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	✓
 ‹	Pi		‹	‘	<		quotation mark			U+2018: LEFT SINGLE QUOTATION MARK	
 “	Pi		 “	“	<		quotation mark			U+201C: LEFT DOUBLE QUOTATION MARK	
 ‘	Pi		‘	‘	<		quotation mark			U+2018: LEFT SINGLE QUOTATION MARK	
-»	Pf		»		>		quotation mark			U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+»	Pf		»		>		quotation mark			U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	✓
 ›	Pf		›	’	>		quotation mark			U+2019: RIGHT SINGLE QUOTATION MARK	
 ”	Pf		”	”	>		quotation mark			U+201D: RIGHT DOUBLE QUOTATION MARK	
 ’	Pf		’	’	>		quotation mark			U+2019: RIGHT SINGLE QUOTATION MARK	
@@ -154,7 +167,7 @@ A	Lu	a aː	A				vowel			U+0041: LATIN CAPITAL LETTER A
 											
 											
 											
-§	Po		§	§			section sign			U+00A7: SECTION SIGN	
+§	Po		§	§			section sign			U+00A7: SECTION SIGN	✓
 †	Po		†	†			dagger			U+2020: DAGGER	
 ‡	Po		‡	‡			double dagger			U+2021: DOUBLE DAGGER	
 ′	Po		′	′			prime			U+2032: PRIME	
