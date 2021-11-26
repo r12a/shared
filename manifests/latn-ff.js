c82f167d0ce68076740f0205749916e30e26917b
r12a <ishida@w3.org>
2021-11-26 10:16:30 +0000

    + latn-ff

---
+++ manifests/latn-ff.js
@@ -0,0 +1,27 @@
+
+langTag='ff'
+scriptTag='latn'
+notesLangtag='ff'
+
+explainerDir='latin/ff'
+blockDir='latin/'
+pickerDir='latn-ff/'
+scriptSummaryTableName='ff'
+charUsageBCP='ff'
+orthogName = 'Latin'
+indic = false
+
+pageWebfonts = ["Gentium Plus WF"]
+defaultSize = '24px'
+
+
+autoIndex = true
+
+
+
+// block settings
+
+languageName = 'Fula'
+letterClassName = 'ff'
+lang = 'ff'
+direction = ''
---
+++ scriptdb/latn-ff.js
@@ -0,0 +1,215 @@
+var spreadsheet = `
+b	Ll	b	b		b		consonant			U+0062: LATIN SMALL LETTER B	
+ɓ	Ll	ɓ	ɓ		b		consonant			U+0253: LATIN SMALL LETTER B WITH HOOK	
+mb		ᵐb	mb				pre-nasalised consonant			U+006D U+0062: LATIN SMALL LETTER M, SMALL LETTER B	
+nb		ᵐb	nb				pre-nasalised consonant	Guinea		U+006E U+0062: LATIN SMALL LETTER N, SMALL LETTER B	
+c	Ll	c t͡ʃ	c				consonant			U+0063: LATIN SMALL LETTER C	
+d	Ll	d	d		d		consonant			U+0064: LATIN SMALL LETTER D	
+ɗ	Ll	ɗ	ɗ		d		consonant			U+0257: LATIN SMALL LETTER D WITH HOOK	
+nd		ⁿd	nd				pre-nasalised consonant			U+006E U+0064: LATIN SMALL LETTER N, SMALL LETTER D	
+f	Ll	f	f				consonant			U+0066: LATIN SMALL LETTER F	
+g	Ll	ɡ	g				consonant			U+0067: LATIN SMALL LETTER G	
+ɠ	Ll		ɠ		g			Guinea		U+0260: LATIN SMALL LETTER G WITH HOOK	
+ng		ᵑɡ	ng				pre-nasalised consonant			U+006E U+0067: LATIN SMALL LETTER N, SMALL LETTER G	
+h	Ll	h	h				consonant			U+0068: LATIN SMALL LETTER H	
+j	Ll	ɟ d͡ʒ	j				consonant			U+006A: LATIN SMALL LETTER J	
+nj		ᶮj	nj				pre-nasalised consonant			U+006E U+006A: LATIN SMALL LETTER N, SMALL LETTER J	
+k	Ll	k	k		k		consonant			U+006B: LATIN SMALL LETTER K	
+l	Ll	l	l				consonant			U+006C: LATIN SMALL LETTER L	
+m	Ll	m	m				consonant			U+006D: LATIN SMALL LETTER M	
+n	Ll	n	n		n		consonant			U+006E: LATIN SMALL LETTER N	
+ŋ	Li	ŋ	ŋ		n		consonant			U+014B: LATIN SMALL LETTER ENG	
+ɲ	Ll	ɲ	ɲ		n		consonant	Guinea; Mali, Burkina Faso		U+0272: LATIN SMALL LETTER N WITH LEFT HOOK	
+ñ	Ll	ɲ	ñ		n		consonant	Senegal, Gambia, Mauritania; Sierra Leone, Guinea-Bissau, Liberia		U+00F1: LATIN SMALL LETTER N WITH TILDE	
+ny		ɲ	ny				consonant	Nigeria; Niger, Cameroon, Chad, CAR		U+006E U+0079: LATIN SMALL LETTER N, SMALL LETTER Y	
+p	Ll	p	p				consonant			U+0070: LATIN SMALL LETTER P	✓
+r	Ll	r ɾ	r				consonant			U+0072: LATIN SMALL LETTER R	
+s	Ll	s	s		s		consonant			U+0073: LATIN SMALL LETTER S	
+t	Ll	t	t		t		consonant			U+0074: LATIN SMALL LETTER T	
+w	Ll	w	w				consonant			U+0077: LATIN SMALL LETTER W	
+x	Ll		x							U+0078: LATIN SMALL LETTER X	
+y	Ll	j	y		y		consonant			U+0079: LATIN SMALL LETTER Y	
+ƴ	Ll	ʔʲ	ƴ		y		consonant			U+01B4: LATIN SMALL LETTER Y WITH HOOK	✓
+ʼy		ʔʲ	ʼy		y		consonant	Nigeria		U+02BC U+0079: MODIFIER LETTER APOSTROPHE, SMALL LETTER Y	
+'y		ʔʲ	ʼy				consonant	Nigeria (ASCII alternative)		U+0027 U+0079: APOSTROPHE, SMALL LETTER Y	
+ʼ	Lm	ʔ	ʼ		}		consonant			U+02BC: MODIFIER LETTER APOSTROPHE	
+											
+											
+v	Ll	v	v				consonant	loan words only (esp. Arabic)		U+0076: LATIN SMALL LETTER V	
+q	Ll	q	q				consonant	loan words only (esp. Arabic)		U+0071: LATIN SMALL LETTER Q	
+sh		ʃ	sh				consonant	loan words only (esp. Arabic)		U+0073 U+0068: LATIN SMALL LETTER S, SMALL LETTER H	
+x	Ll	x	x				consonant	loan words only (esp. Arabic)		U+0078: LATIN SMALL LETTER X	
+z	Ll	z	z				consonant	loan words only (esp. Arabic)		U+007A: LATIN SMALL LETTER Z												
+											
+											
+											
+‘y		j̰	ʼy				consonant			U+2018 U+0079: LEFT SINGLE QUOTATION MARK, SMALL LETTER Y	
+											
+Ɓ	Lu	ɓ bˤ	Ɓ		B		consonant			U+0181: LATIN CAPITAL LETTER B WITH HOOK	✓
+TS		sʼ t͡sʼ	TS		T		consonant			U+0054 U+0053: LATIN CAPITAL LETTER T, CAPITAL LETTER S	
+Ɗ	Lu	ɗ	Ɗ		D		consonant			U+018A: LATIN CAPITAL LETTER D WITH HOOK	✓
+B	Lu	b	B		¶B		consonant			U+0042: LATIN CAPITAL LETTER B	
+T	Lu	t	T		¶T		consonant			U+0054: LATIN CAPITAL LETTER T	
+D	Lu	d	D		¶D		consonant			U+0044: LATIN CAPITAL LETTER D	
+C	Lu	t͡ʃ	C				consonant			U+0043: LATIN CAPITAL LETTER C	
+J	Lu	d͡ʒ ʒ	J				consonant			U+004A: LATIN CAPITAL LETTER J	
+G	Lu	ɡ	G				consonant			U+0047: LATIN CAPITAL LETTER G	
+K	Lu	k	K		¶K		consonant			U+004B: LATIN CAPITAL LETTER K	
+Ƙ	Lu	kʼ	Ƙ		K		consonant			U+0198: LATIN CAPITAL LETTER K WITH HOOK	✓
+F	Lu	ɸ	F				consonant			U+0046: LATIN CAPITAL LETTER F	
+S	Lu	s	S		¶S		consonant			U+0053: LATIN CAPITAL LETTER S	
+Z	Lu	z	Z				consonant			U+005A: LATIN CAPITAL LETTER Z	
+SH		ʃ	SH		S		consonant			U+0053 U+0048: LATIN CAPITAL LETTER S, CAPITAL LETTER H	
+H	Lu	h	H				consonant			U+0048: LATIN CAPITAL LETTER H	
+M	Lu	m	M				consonant			U+004D: LATIN CAPITAL LETTER M	
+N	Lu	n	N				consonant			U+004E: LATIN CAPITAL LETTER N	
+W	Lu	w	W				consonant			U+0057: LATIN CAPITAL LETTER W	
+R	Lu	ɽ ɾ	R				consonant			U+0052: LATIN CAPITAL LETTER R	
+R̃		r	R̃				consonant	academic use only		U+0052 U+0303: LATIN CAPITAL LETTER R, COMBINING TILDE	
+L	Lu	l	L				consonant			U+004C: LATIN CAPITAL LETTER L	
+Y	Lu	j	Y		¶Y		consonant			U+0059: LATIN CAPITAL LETTER Y	
+Ƴ	Lu	ʔʲ	Ƴ		Y		consonant			U+01B3: LATIN CAPITAL LETTER Y WITH HOOK	✓
+											
+											
+											
+											
+i	Ll	i	i				vowel			U+0069: LATIN SMALL LETTER I	
+ii		iː	ii				long vowel			U+0069 U+0069: LATIN SMALL LETTER I, SMALL LETTER I	
+u	Ll	u	u				vowel			U+0075: LATIN SMALL LETTER U	
+uu		uː	uu				long vowel			U+0075 U+0075: LATIN SMALL LETTER U, SMALL LETTER U	
+e	Ll	e	e				vowel			U+0065: LATIN SMALL LETTER E	
+ee		eː	ee				long vowel			U+0065 U+0065: LATIN SMALL LETTER E, SMALL LETTER E	
+o	Ll	o	o				vowel			U+006F: LATIN SMALL LETTER O	
+oo		oː	oo				long vowel			U+006F U+006F: LATIN SMALL LETTER O, SMALL LETTER O	
+a	Ll	a	a				vowel			U+0061: LATIN SMALL LETTER A	
+aa		aː	aa				long vowel			U+0061 U+0061: LATIN SMALL LETTER A, SMALL LETTER A	
+											
+I	Lu	i iː	I				vowel			U+0049: LATIN CAPITAL LETTER I	
+U	Lu	u uː	U				vowel			U+0055: LATIN CAPITAL LETTER U	
+E	Lu	e eː	E				vowel			U+0045: LATIN CAPITAL LETTER E	
+O	Lu	o oː	O				vowel			U+004F: LATIN CAPITAL LETTER O	
+A	Lu	a aː	A				vowel			U+0041: LATIN CAPITAL LETTER A	
+											
+											
+											
+											
+											
+											
+											
+.	Po		.	.	.		full stop			U+002E: FULL STOP	
+,	Po		,	,	,		comma			U+002C: COMMA	
+:	Po		:	:	:		colon			U+003A: COLON	
+;	Po		;	;	;		semicolon			U+003B: SEMICOLON	
+!	Po		!	!	!		exclamation mark			U+0021: EXCLAMATION MARK	
+?	Po		?	?	?		question mark			U+003F: QUESTION MARK	
+											
+											
+…	Po		…	…	.		ellipsis			U+2026: HORIZONTAL ELLIPSIS	
+											
+											
+«	Pi		«		<		quotation mark			U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	Pi		‹	‘	<		quotation mark			U+2018: LEFT SINGLE QUOTATION MARK	
+“	Pi		 “	“	<		quotation mark			U+201C: LEFT DOUBLE QUOTATION MARK	
+‘	Pi		‘	‘	<		quotation mark			U+2018: LEFT SINGLE QUOTATION MARK	
+»	Pf		»		>		quotation mark			U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+›	Pf		›	’	>		quotation mark			U+2019: RIGHT SINGLE QUOTATION MARK	
+”	Pf		”	”	>		quotation mark			U+201D: RIGHT DOUBLE QUOTATION MARK	
+’	Pf		’	’	>		quotation mark			U+2019: RIGHT SINGLE QUOTATION MARK	
+											
+											
+(	Ps		(	(	(		parenthesis			U+0028: LEFT PARENTHESIS	
+[	Pe		[	[	(		bracket			U+005B: LEFT SQUARE BRACKET	
+)	Ps		)	)	)		parenthesis			U+0029: RIGHT PARENTHESIS	
+]	Pe		]	]	)		bracket			U+005D: RIGHT SQUARE BRACKET	
+											
+											
+-	Pd		-	-	-		hyphen			U+2010: HYPHEN	
+‑	Pd		‑	‑	-		non-breaking hyphen			U+2011: NON-BREAKING HYPHEN	
+–	Pd		–	–	-		en dash			U+2013: EN DASH	
+—	Pd		—	—	-		em dash			U+2014: EM DASH	
+											
+											
+’	Pf		’		'		apostrophe			U+2019: RIGHT SINGLE QUOTATION MARK	
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+											
+§	Po		§	§			section sign			U+00A7: SECTION SIGN	
+†	Po		†	†			dagger			U+2020: DAGGER	
+‡	Po		‡	‡			double dagger			U+2021: DOUBLE DAGGER	
+′	Po		′	′			prime			U+2032: PRIME	
+″	Po		″	″			double prime			U+2033: DOUBLE PRIME	
+											
+											
+											
+											
+											
+											
+											
+											
+											
+%	Po		%		%		percentage mark			U+0025: PERCENT SIGN	
+‰	Po		‰		%		per mille mark			U+2030: PER MILLE SIGN	
+
+
+
+
+`
+
+
+
+latinPanel = 'd͡ʒ t͡ʃ ñ ƴ ɓ ɗ ɟ ɲ ŋ ʼ ʼy ɡ ᵐb ᵑɡ ᶮɟ ᶮd͡ʒ ⁿd iː uː eː oː aː ɪ ɛ ɔ ʊ ʔ ʔʲ'
+
+
+
+
+var cols = {
+"class": 1,
+"ipaLoc": 2,
+"transLoc":3,
+"key":4,
+"transcription": 0,
+"kbd": 6,
+"typeLoc": 7,
+"statusLoc": 8,
+
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+
+"nameLoc": 9,
+"ucsName": 10,
+"block": 11,
+
+//"othertranscriptions": [[4, 'ISO 15919']]
+}
+
+
+
