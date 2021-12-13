c673d93800f9b9b179e945c65fe3ab71c6ec9efe
r12a <ishida@w3.org>
2021-12-13 07:57:47 +0000

    + scriptdb/mlym-ml

---
+++ scriptdb/mlym-ml.js
@@ -0,0 +1,260 @@
+var spreadsheet = `
+ക	k	k	a	k	k		Lo		consonant						U+0D15: MALAYALAM LETTER KA	✓
+ഖ	K	kʰ	a	kʰ	kh		Lo		consonant						U+0D16: MALAYALAM LETTER KHA	✓
+ഗ	g	g	a	g	g		Lo		consonant						U+0D17: MALAYALAM LETTER GA	✓
+ഘ	G	gʰ	a	gʰ	gh		Lo		consonant						U+0D18: MALAYALAM LETTER GHA	✓
+ങ	N	ŋ	a	ŋ	ṅ		Lo		consonant						U+0D19: MALAYALAM LETTER NGA	✓
+ച	c	ʧ	a	c	c		Lo		consonant						U+0D1A: MALAYALAM LETTER CA	✓
+ഛ	C	ʧʰ	a	cʰ	ch		Lo		consonant						U+0D1B: MALAYALAM LETTER CHA	✓
+ജ	j	ɟ	a	j	j		Lo		consonant						U+0D1C: MALAYALAM LETTER JA	✓
+ഝ	J	ɟʰ	a	jʰ	jh		Lo		consonant						U+0D1D: MALAYALAM LETTER JHA	✓
+ഞ	n	ɲ	a	ɲ	ñ		Lo		consonant						U+0D1E: MALAYALAM LETTER NYA	✓
+ട	t	ʈ	a	ʈ	ṭ		Lo		consonant						U+0D1F: MALAYALAM LETTER TTA	✓
+ഠ	T	ʈʰ	a	ʈʰ	ṭh		Lo		consonant						U+0D20: MALAYALAM LETTER TTHA	✓
+ഡ	d	ɖ	a	ɖ	ḍ		Lo		consonant						U+0D21: MALAYALAM LETTER DDA	✓
+ഢ	D	ɖʰ	a	ɖʰ	ḍh		Lo		consonant						U+0D22: MALAYALAM LETTER DDHA	✓
+ണ	n	ɳ	a	ɳ	ṇ		Lo		consonant						U+0D23: MALAYALAM LETTER NNA	✓
+ത	t¶	t̪	a	t	t		Lo		consonant						U+0D24: MALAYALAM LETTER TA	✓
+ഥ	T¶	t̪ʰ	a	tʰ	th		Lo		consonant						U+0D25: MALAYALAM LETTER THA	✓
+ദ	d¶	d̪	a	d	d		Lo		consonant						U+0D26: MALAYALAM LETTER DA	✓
+ധ	D¶	d̪ʰ	a	dʰ	dh		Lo		consonant						U+0D27: MALAYALAM LETTER DHA	✓
+ന	n¶	n̪ n	a	n	n		Lo		consonant						U+0D28: MALAYALAM LETTER NA	✓
+ഩ	n	n	a	ṅ	ṉ		Lo	a	consonant	archaic					U+0D29: MALAYALAM LETTER NNNA	✓
+പ	p	p	a	p	p		Lo		consonant						U+0D2A: MALAYALAM LETTER PA	✓
+ഫ	P	pʰ	a	pʰ	ph		Lo		consonant						U+0D2B: MALAYALAM LETTER PHA	✓
+ബ	b	b	a	b	b		Lo		consonant						U+0D2C: MALAYALAM LETTER BA	✓
+ഭ	B	bʰ	a	bʰ	bh		Lo		consonant						U+0D2D: MALAYALAM LETTER BHA	✓
+മ	m	m	a	m	m		Lo		consonant						U+0D2E: MALAYALAM LETTER MA	✓
+യ	y	j	a	y	y		Lo		consonant						U+0D2F: MALAYALAM LETTER YA	✓
+ര	r	r̪	a	r	r		Lo		consonant						U+0D30: MALAYALAM LETTER RA	✓
+റ	r	r t	a	ṙ	ṟ		Lo		consonant						U+0D31: MALAYALAM LETTER RRA	✓
+ല	l	l	a	l	l		Lo		consonant						U+0D32: MALAYALAM LETTER LA	✓
+ള	l	ɭ,	a	ɭ	ḷ		Lo		consonant						U+0D33: MALAYALAM LETTER LLA	✓
+ഴ	l	ɻ	a	ɻ	ḻ		Lo		consonant						U+0D34: MALAYALAM LETTER LLLA	✓
+വ	v	ʋ	a	ʋ	v		Lo		consonant						U+0D35: MALAYALAM LETTER VA	✓
+ശ	s	ɕ ʃ	a	ʃ	ś		Lo		consonant						U+0D36: MALAYALAM LETTER SHA	✓
+ഷ	s	ʂ	a	ʂ	ṣ		Lo		consonant						U+0D37: MALAYALAM LETTER SSA	✓
+സ	s¶	s	a	s	s		Lo		consonant						U+0D38: MALAYALAM LETTER SA	✓
+ഹ	h	ɦ	a	h	h		Lo		consonant						U+0D39: MALAYALAM LETTER HA	✓
+ഺ	t	t	a	ṫ	 ṯ		Lo	a	consonant	archaic					U+0D3A: MALAYALAM LETTER TTTA	✓
+																
+																
+ൺ	N	n	a	ɳ̽	ṇ^		Lo		chillu consonant						U+0D7A: MALAYALAM LETTER CHILLU NN	✓
+ൻ	N	n	a	n̽	n^		Lo		chillu consonant						U+0D7B: MALAYALAM LETTER CHILLU N	✓
+ർ	R	r	a	r̽	r^		Lo		chillu consonant						U+0D7C: MALAYALAM LETTER CHILLU RR	✓
+ൽ	L	l	a	l̽	l^		Lo		chillu consonant						U+0D7D: MALAYALAM LETTER CHILLU L	✓
+ൾ	L	ɭ	a	ɭ̽	ḷ^		Lo		chillu consonant						U+0D7E: MALAYALAM LETTER CHILLU LL	✓
+ൿ	K	k	a	k̽	k^		Lo		chillu consonant						U+0D7F: MALAYALAM LETTER CHILLU K	✓
+																
+																
+ം	m	m		m̽	ṃ		Mc​		anusvara						U+0D02: MALAYALAM SIGN ANUSVARA​	✓
+ഀ							Mn​								U+0D00: MALAYALAM SIGN COMBINING ANUSVARA ABOVE​	✓
+ഃ	h	ɦ		h̽	ḥ		Mc​		visarga						U+0D03: MALAYALAM SIGN VISARGA​	✓
+																
+																
+																
+അ	A	a		ạ̄	a		Lo		independent vowel						U+0D05: MALAYALAM LETTER A	✓
+ആ		aː		ậ	ā		Lo		independent vowel		ാ				U+0D06: MALAYALAM LETTER AA	✓
+ഇ	I	i		ị	i		Lo		independent vowel		ി				U+0D07: MALAYALAM LETTER I	✓
+ഈ	I	iː		ị̄	ī		Lo		independent vowel		ീ				U+0D08: MALAYALAM LETTER II	✓
+ഉ	U	u		ụ	u		Lo		independent vowel		ു				U+0D09: MALAYALAM LETTER U	✓
+ഊ	U	uː		ụ̄	ū		Lo		independent vowel		ൂ				U+0D0A: MALAYALAM LETTER UU	✓
+എ	E¶	e		ẹ	e		Lo		independent vowel		െ				U+0D0E: MALAYALAM LETTER E	✓
+ഏ	E	eː		ẹ̄	ē		Lo		independent vowel		േ				U+0D0F: MALAYALAM LETTER EE	✓
+ഒ	O	o		ọ	o		Lo		independent vowel		ൊ				U+0D12: MALAYALAM LETTER O	✓
+ഓ	O	oː		ọ̄	ō		Lo		independent vowel		ോ				U+0D13: MALAYALAM LETTER OO	✓
+ഐ	A	ai̯		ạʲ	ai		Lo		independent vowel		ൈ				U+0D10: MALAYALAM LETTER AI	✓
+ഔ	A	au̯		ạʷ	au		Lo		independent vowel		ൗ				U+0D14: MALAYALAM LETTER AU	✓
+																
+																
+ാ	a	aː		ā	ā		Mc​		vowel sign			ആ			U+0D3E: MALAYALAM VOWEL SIGN AA​	✓
+ി	i	i		i	i		Mc​		vowel sign			ഇ			U+0D3F: MALAYALAM VOWEL SIGN I​	✓
+ീ	i	iː		ī	ī		Mc​		vowel sign			ഈ			U+0D40: MALAYALAM VOWEL SIGN II​	✓
+ു	u	u		u	u		Mn​		vowel sign			ഉ			U+0D41: MALAYALAM VOWEL SIGN U​	✓
+ൂ	u	uː		ū	ū		Mn​		vowel sign			ഊ			U+0D42: MALAYALAM VOWEL SIGN UU​	✓
+െ	e¶	e		e	e		Mc​		vowel sign			എ			U+0D46: MALAYALAM VOWEL SIGN E​	✓
+േ	e	eː		ē	ē		Mc​		vowel sign			ഏ			U+0D47: MALAYALAM VOWEL SIGN EE​	✓
+ൊ	o	o		o	o		Mc​		vowel sign			ഒ			U+0D4A: MALAYALAM VOWEL SIGN O​	✓
+ോ	o	oː		ō	ō		Mc​		vowel sign			ഓ			U+0D4B: MALAYALAM VOWEL SIGN OO​	✓
+ൈ	a	ai̯		aʲ	ai		Mc​		vowel sign			ഐ			U+0D48: MALAYALAM VOWEL SIGN AI​	✓
+ൗ	a	au̯		aʷ	au		Mc​		vowel sign			ഔ			U+0D57: MALAYALAM AU LENGTH MARK​	✓
+ൌ	a	au̯		ȧʷ	au		Mc​	a	historic vowel sign	archaic					U+0D4C: MALAYALAM VOWEL SIGN AU​	✓
+																
+																
+ഋ	R	rɨ		r̥̣	r̥̣		Lo		independent vowel		ൃ				U+0D0B: MALAYALAM LETTER VOCALIC R	✓
+ൠ	R	rɨː		r̥̣̄	r̥̣̄		Lo	r	independent vowel	very rare	ൄ				U+0D60: MALAYALAM LETTER VOCALIC RR	✓
+ഌ	L	lɨ		l̥̣	l̥̣		Lo	r	independent vowel	(in one Sanskrit word only)	ൢ				U+0D0C: MALAYALAM LETTER VOCALIC L	✓
+ൡ	L	lɨː		l̥̣̄	l̥̣̄		Lo	r	independent vowel	very rare	ൣ				U+0D61: MALAYALAM LETTER VOCALIC LL	✓
+ൃ	R	rɨ		r̥	r̥		Mn​		vowel sign			ഋ			U+0D43: MALAYALAM VOWEL SIGN VOCALIC R​	✓
+ൄ	R	rɨː		r̥̄	r̥̄		Mn​	r	vowel sign	very rare		ൠ			U+0D44: MALAYALAM VOWEL SIGN VOCALIC RR​	✓
+ൢ	L	lɨ		l̥	l̥		Mn​	r	vowel sign	very rare		ഌ			U+0D62: MALAYALAM VOWEL SIGN VOCALIC L​	✓
+ൣ	L	lɨː		l̥̄	l̥̄		Mn​	r	vowel sign	very rare		ൡ			U+0D63: MALAYALAM VOWEL SIGN VOCALIC LL​	✓
+																
+																
+൏				~			So	a	measure of rice						U+0D4F: MALAYALAM SIGN PARA	✓
+																
+																
+ഽ				ˣ	’		Lo		avagraha				പ്രശ്ലേഷം	praślēṣam	U+0D3D: MALAYALAM SIGN AVAGRAHA	✓
+ൎ				⁎			Lo	a	repha	pre 1970s reform					U+0D4E: MALAYALAM LETTER DOT REPH	✓
+ൟ							Lo	a							U+0D5F: MALAYALAM LETTER ARCHAIC II	✓
+																
+																
+																
+																
+																
+\u{D46}\u{D3E}	o	oː		eā	ō				ൊ						U+0D46 U+0D3E: MALAYALAM VOWEL SIGN E, VOWEL SIGN AA​	
+\u{D47}\u{D3E}	a	ai̯		ēā	ai				ോ						U+0D47 U+0D3E: MALAYALAM VOWEL SIGN EE, VOWEL SIGN AA​	
+\u{D46}\u{D57}	a	au̯		eaʷ	au				ൌ						U+0D46 U+0D57: MALAYALAM VOWEL SIGN E, AU LENGTH MARK​	
+റ്റ	t	tt		ṙ͓ṙ	ṯṯ										U+0D31 U+0D4D U+0D31: MALAYALAM LETTER RRA, SIGN VIRAMA, LETTER RRA	
+																
+																
+																
+																
+																
+																
+।	|			|	.		Po	a	phrase separator	for older texts					U+0964: DEVANAGARI DANDA	✓
+॥	|			¶			Po	a	phrase separator	for older texts					U+0965: DEVANAGARI DOUBLE DANDA	✓
+.	.			.	.		Po		full stop						U+002E: FULL STOP	
+,	,			,	,		Po		comma						U+002C: COMMA	
+:	:			:	:		Po		colon						U+003A: COLON	
+;	;			;	;		Po		semicolon						U+003B: SEMICOLON	
+!	!			!	!		Po		exclamation mark						U+0021: EXCLAMATION MARK	
+?	?			?	?		Po		question mark						U+003F: QUESTION MARK	
+ʼ	'			ʼ	ʼ		Lm		apostrophe						U+02BC: MODIFIER LETTER APOSTROPHE	
+																
+																
+…	.			…	…		Po		ellipsis						U+2026: HORIZONTAL ELLIPSIS	
+																
+																
+																
+«	<			«	“		Pi		quotation mark						U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	<			‹	‘		Pi		quotation mark						U+2018: LEFT SINGLE QUOTATION MARK	
+“	<			 “	“		Pi		quotation mark						U+201C: LEFT DOUBLE QUOTATION MARK	
+‘	<			‘	‘		Pi		quotation mark						U+2018: LEFT SINGLE QUOTATION MARK	
+»	>			»	”		Pf		quotation mark						U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+›	>			›	’		Pf		quotation mark						U+2019: RIGHT SINGLE QUOTATION MARK	
+”	>			”	”		Pf		quotation mark						U+201D: RIGHT DOUBLE QUOTATION MARK	
+’	>			’	’		Pf		quotation mark						U+2019: RIGHT SINGLE QUOTATION MARK	
+																
+																
+																
+(	(			(	(		Ps		parenthesis						U+0028: LEFT PARENTHESIS	
+[	(			[	[		Ps		bracket						U+005B: LEFT SQUARE BRACKET	
+)	)			)	)		Pe		parenthesis						U+0029: RIGHT PARENTHESIS	
+]	)			]	]		Pe		bracket						U+005D: RIGHT SQUARE BRACKET	
+																
+																
+-	-			-	-				hyphen						U+2010: HYPHEN	
+‑	-			‑	‑		Pd		non-breaking hyphen						U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd		en dash						U+2013: EN DASH	
+—	-			—	—		Pd		em dash						U+2014: EM DASH	
+																
+																
+																
+ഁ	[	̃		˜	̃		Mn​		nasalisation						U+0D01: MALAYALAM SIGN CANDRABINDU​	✓
+																
+																
+																
+്	*			͞			Mn​		virama					chandrakkala	U+0D4D: MALAYALAM SIGN VIRAMA​	✓
+഻	\			˟			Mn​								U+0D3B: MALAYALAM SIGN VERTICAL BAR VIRAMA​	✓
+഼	\			ˣ			Mn​								U+0D3C: MALAYALAM SIGN CIRCULAR VIRAMA​	✓
+																
+																
+																
+§				§	§		Po		section sign						U+00A7: SECTION SIGN	
+†				†	†		Po		dagger						U+2020: DAGGER	
+‡				‡	‡		Po		double dagger						U+2021: DOUBLE DAGGER	
+′				′	′		Po		prime						U+2032: PRIME	
+″				″	″		Po		double prime						U+2033: DOUBLE PRIME	
+																
+																
+																
+																
+																
+																
+\u200B	=			␣			Cf		zero-width space						U+200B: ZERO WIDTH SPACE	
+\u200C	=			ᶻʷⁿʲ			Cf		zero-width non-joiner						U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=			ᶻʷʲ			Cf		zero-width joiner						U+200D: ZERO WIDTH JOINER	
+\u034F	=			ᶜᵍʲ			Mn​		combining grapheme joiner						U+034F: COMBINING GRAPHEME JOINER​	
+\u2060	=			ʷʲ			Cf		word joiner						U+2060 WORD JOINER	
+																
+																
+																
+																
+																
+൧	±	1		1̣	1		Nd		digit						U+0D67: MALAYALAM DIGIT ONE	✓
+൨	±	2		2̣	2		Nd		digit						U+0D68: MALAYALAM DIGIT TWO	✓
+൩	±	3		3̣	3		Nd		digit						U+0D69: MALAYALAM DIGIT THREE	✓
+൪	±	4		4̣	4		Nd		digit						U+0D6A: MALAYALAM DIGIT FOUR	✓
+൫	±	5		5̣	5		Nd		digit						U+0D6B: MALAYALAM DIGIT FIVE	✓
+൬	±	6		6̣	6		Nd		digit						U+0D6C: MALAYALAM DIGIT SIX	✓
+൭	±	7		7̣	7		Nd		digit						U+0D6D: MALAYALAM DIGIT SEVEN	✓
+൮	±	8		8̣	8		Nd		digit						U+0D6E: MALAYALAM DIGIT EIGHT	✓
+൯	±	9		9̣	9		Nd		digit						U+0D6F: MALAYALAM DIGIT NINE	✓
+൦	±	0		0̣	0		Nd		digit						U+0D66: MALAYALAM DIGIT ZERO	✓
+൰				{10}			No		number sign						U+0D70: MALAYALAM NUMBER TEN	✓
+൱				{100}			No		number sign						U+0D71: MALAYALAM NUMBER ONE HUNDRED	✓
+൲				{1000}			No		number sign						U+0D72: MALAYALAM NUMBER ONE THOUSAND	✓
+൳				¼			No		number sign						U+0D73: MALAYALAM FRACTION ONE QUARTER	✓
+൴				½			No		number sign						U+0D74: MALAYALAM FRACTION ONE HALF	✓
+൵				¾			No		number sign						U+0D75: MALAYALAM FRACTION THREE QUARTERS	✓
+൶				{1/16}			No		fraction						U+0D76: MALAYALAM FRACTION ONE SIXTEENTH	✓
+൷				{1/8}			No		fraction						U+0D77: MALAYALAM FRACTION ONE EIGHTH	✓
+൸				{3/16}			No		fraction						U+0D78: MALAYALAM FRACTION THREE SIXTEENTHS	✓
+൘				{1/160}			No		fraction						U+0D58: MALAYALAM FRACTION ONE ONE-HUNDRED-AND-SIXTIETH	✓
+൙				{1/40}			No		fraction						U+0D59: MALAYALAM FRACTION ONE FORTIETH	✓
+൚				{3/8}			No		fraction						U+0D5A: MALAYALAM FRACTION THREE EIGHTIETHS	✓
+൛				{1/20}			No		fraction						U+0D5B: MALAYALAM FRACTION ONE TWENTIETH	✓
+൜				{1/10}			No		fraction						U+0D5C: MALAYALAM FRACTION ONE TENTH	✓
+൝				{3/20}			No		fraction						U+0D5D: MALAYALAM FRACTION THREE TWENTIETHS	✓
+൞				{1/5}			No		fraction						U+0D5E: MALAYALAM FRACTION ONE FIFTH	✓
+																
+																
+൹							So	r	date sign	usage fading					U+0D79: MALAYALAM DATE MARK	✓
+																
+																
+																
+%	%			%			Po		percentage mark						U+0025: PERCENT SIGN	
+‰	%			‰			Po		per mille mark						U+2030: PER MILLE SIGN	
+
+
+
+
+`
+
+
+
+latinPanel = 'aː ā ai̯ au̯ bʰ ɕ ɖ ḍ ɖʰ ḍh d̪d̪d̪ d̪ʰ eː ē gʰ ɦ iː ī ɨ ɟ ɟʰ kʰ ɭ, ḷ ḻ l̥̣ l̥̣̄ l̥ l̥̄ ŋ ṅ ɲ ñ ɳ ṇ n̪ ṉ oː ō pʰ r̪ ṟ ɻ r̥̣ r̥̣̄ r̥ r̥̄ ʃ ś ʂ ṣ ʧ ʧʰ ʈ ṭ ʈʰ ṭh t̪ t̪ʰ ṯ ṯṯ uː ū ʋ'
+
+
+var cols = {
+"key": 1,
+"ipaLoc": 2,
+"ipaPlus": 3,
+"transLoc": 4,
+"transcription": 5,
+"kbd": 6,
+"class": 7,
+"status": 8,
+"typeLoc": 9,
+"statusLoc": 10,
+
+"transckey": 0,
+"dvowel": 11,
+"ivowel": 12,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+
+"nnameLoc": 13,
+"nameLoc": 14,
+"ucsName": 15,
+"block": 16,
+
+"othertranscriptions": [[5, 'ISO 15919']]
+}
