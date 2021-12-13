67267e486df726a720ed21b2f0bb8f3de23d2704
r12a <ishida@w3.org>
2021-12-13 11:04:52 +0000

    scriptdb/lisu: Apply latest template #96

--- scriptdb/lisu.js
+++ scriptdb/lisu.js
@@ -1,124 +1,153 @@
 var spreadsheet = `
-ꓑ	Lo	p	p		p	p	p	consonant				U+A4D1: LISU LETTER PA	lisu
-ꓐ	Lo	b	b		b	b	b	consonant				U+A4D0: LISU LETTER BA	lisu
-ꓒ	Lo	pʰ	pʰ	pʰ	p	pʰ	P	consonant				U+A4D2: LISU LETTER PHA	lisu
-ꓔ	Lo	t	t		t	t	t	consonant				U+A4D4: LISU LETTER TA	lisu
-ꓓ	Lo	d	d		d	d	d	consonant				U+A4D3: LISU LETTER DA	lisu
-ꓕ	Lo	tʰ	tʰ	tʰ	t	tʰ	T	consonant				U+A4D5: LISU LETTER THA	lisu
-ꓗ	Lo	k	k		k	k	k	consonant				U+A4D7: LISU LETTER KA	lisu
-ꓖ	Lo	ɡ	ɡ		g	ɡ	g	consonant				U+A4D6: LISU LETTER GA	lisu
-ꓘ	Lo	kʰ	kʰ	kʰ	k	kʰ	K	consonant				U+A4D8: LISU LETTER KHA	lisu
-ꓝ	Lo	t͡s	ʦ	t͡s	t	ʦ	f	consonant				U+A4DD: LISU LETTER TSA	lisu
-ꓜ	Lo	d͡z	ʣ	d͡z	d	ʣ	z	consonant				U+A4DC: LISU LETTER DZA	lisu
-ꓞ	Lo	ʦʰ	ʦʰ	ʦʰ	t	ʦʰ	F	consonant				U+A4DE: LISU LETTER TSHA	lisu
-ꓚ	Lo	ʨ	ʨ	ʨ	t	ʨ	c	consonant				U+A4DA: LISU LETTER CA	lisu
-ꓙ	Lo	ʥ	ʥ	ʥ	d	ʥ	j	consonant				U+A4D9: LISU LETTER JA	lisu
-ꓛ	Lo	ʨʰ	ʨʰ	ʨʰ	t	ʨʰ	C	consonant				U+A4DB: LISU LETTER CHA	lisu
-ꓩ	Lo	f	f		f	f	J	consonant				U+A4E9: LISU LETTER FA	lisu
-ꓢ	Lo	s	s		s	s	s	consonant				U+A4E2: LISU LETTER SA	lisu
-ꓤ	Lo	z	z		z	z	r	consonant				U+A4E4: LISU LETTER ZA	lisu
-ꓣ	Lo	ʒ	ʒ	ʒ	z	ʒ	R	consonant				U+A4E3: LISU LETTER ZHA	lisu
-ꓫ	Lo	ɕ	ɕ	ɕ	c	ɕ	x	consonant				U+A4EB: LISU LETTER SHA	lisu
-ꓬ	Lo	ʑ	ʑ	ʑ	z	ʑ	y	consonant/vowel				U+A4EC: LISU LETTER YA	lisu
-ꓧ	Lo	x	x		x	x	h	consonant				U+A4E7: LISU LETTER XA	lisu
-ꓭ	Lo	ɣ	ɣ	ɣ	y	ɣ	B	consonant/vowel				U+A4ED: LISU LETTER GHA	lisu
-ꓨ	Lo	ɦ	ɦ	ɦ	h	ɦ	G	consonant				U+A4E8: LISU LETTER HHA	lisu
-ꓦ	Lo	h	h		h	h	v	consonant				U+A4E6: LISU LETTER HA	lisu
-ꓟ	Lo	m	m		m	m	m	consonant				U+A4DF: LISU LETTER MA	lisu
-ꓠ	Lo	n	n		n	n	n	consonant				U+A4E0: LISU LETTER NA	lisu
-ꓥ	Lo	ŋ	ŋ	ŋ	n	ŋ	V	consonant				U+A4E5: LISU LETTER NGA	lisu
-ꓪ	Lo	w	w		w	w	w	consonant/vowel				U+A4EA: LISU LETTER WA	lisu
-ꓡ	Lo	l	l		l	l	l	consonant				U+A4E1: LISU LETTER LA	lisu
-													
-													
-ꓮ	Lo	ɑ	ɑ	ɑ	a	ɑ	a	vowel				U+A4EE: LISU LETTER A	lisu
-ꓯ	Lo	ɛ	ɛ	ɛ	e	ɛ	A	vowel				U+A4EF: LISU LETTER AE	lisu
-ꓷ	Lo	ə	ə	ə	e	ə	D	vowel				U+A4F7: LISU LETTER OE	lisu
-ꓰ	Lo	e	e		e	e	e	vowel				U+A4F0: LISU LETTER E	lisu
-ꓱ	Lo	ø	ø	ø	o	ø	E	vowel				U+A4F1: LISU LETTER EU	lisu
-ꓲ	Lo	i	i		i	i	i	vowel				U+A4F2: LISU LETTER I	lisu
-ꓶ	Lo	ɯ	ɯ	ɯ	u	ɯ	L	vowel				U+A4F6: LISU LETTER UH	lisu
-ꓳ	Lo	o	o		o	o	o	vowel				U+A4F3: LISU LETTER O	lisu
-ꓴ	Lo	u	u		u	u	u	vowel				U+A4F4: LISU LETTER U	lisu
-ꓵ	Lo	y	y		y	y	U	vowel				U+A4F5: LISU LETTER UE	lisu
-ˍ	Lm		ä	ä	a	ä	q	vowel				U+02CD: MODIFIER LETTER LOW MACRON	
-ʼ	Lm		ñ	ñ	n	ñ	'	vowel				U+02BC: MODIFIER LETTER APOSTROPHE	
-													
-													
-ꓸ	Lm	˥˥	˥˥	˥˥	^	¹		pitch 55 tone mark				U+A4F8: LISU LETTER TONE MYA TI	lisu
-ꓹ	Lm	˧˥	˧˥	˧˥	^	²		pitch 35 tone mark				U+A4F9: LISU LETTER TONE NA PO	lisu
-ꓺ	Lm	˦˦	˦˦	˦˦	^	³	H	pitch 44 tone mark				U+A4FA: LISU LETTER TONE MYA CYA	lisu
-ꓻ	Lm	˧˧	˧˧	˧˧	^	⁴	Y	pitch 33 tone mark				U+A4FB: LISU LETTER TONE MYA BO	lisu
-ꓼ	Lm	˦˨	˦˨	˦˨	^	⁵		pitch 42 tone mark				U+A4FC: LISU LETTER TONE MYA NA	lisu
-ꓽ	Lm	˧˩	˧˩	˧˩	^	⁶		pitch 31 tone mark				U+A4FD: LISU LETTER TONE MYA JEU	lisu
-													
-													
-꓾	Po		,		,	,	I	comma				U+A4FE: LISU PUNCTUATION COMMA	lisu
-꓿	Po		.		.	.	O	full stop				U+A4FF: LISU PUNCTUATION FULL STOP	lisu
-《	Ps		<		(	<	Z	bracket				U+300A: LEFT DOUBLE ANGLE BRACKET	
-》	Pe		>		)	>	X	bracket				U+300B: RIGHT DOUBLE ANGLE BRACKET	
-													
-													
-													
-ꓹꓼ						²⁵	W					U+A4F9 U+A4FC: LISU LETTER TONE NA PO, LISU LETTER TONE MYA NA	
-													
-													
-													
-													
-													
-\u200B	Cf							zero-width space				U+200B ZERO WIDTH SPACE	
-\u200C	Cf					ₓ		zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
-\u200D	Cf					₊		zero-width joiner				U+200D: ZERO WIDTH JOINER	
-\u034F	Mn​					ᶜᵍʲ		combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER​	
-\u2067	Cf					ʳˡⁱ		rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE	
-\u2066	Cf					ˡʳⁱ		ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE	
-\u2068	Cf					ᶠˢⁱ		first-strong isolate				U+2068: FIRST STRONG ISOLATE	
-\u2069	Cf					ᵖᵈⁱ		pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE	
-\u202B	Cf					ʳˡᵉ		rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING	
-\u202A	Cf					ˡʳᵉ		ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING	
-\u202C	Cf					ᵖᵈᶠ		pop direction				U+202C: POP DIRECTIONAL FORMATTING	
-\u200F	Cf					ʳˡᵐ		rtl mark				U+200F: RIGHT-TO-LEFT MARK	
-\u200E	Cf					ˡʳᵐ		ltr mark				U+200E: LEFT-TO-RIGHT MARK	
-\u061C	Cf					ᵃˡᵐ		arabic letter mark				U+061C: ARABIC LETTER MARK	
-													
-													
-“	Pi				<			quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
-”	Pf				>			quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
-‘	Pi				<			quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
-’	Pf				>			quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
-«	Pi				"	“		quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
-»	Pf				"	”		quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
-ʼ	Lm							apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE	
-,	Po							comma				U+002C: COMMA	
-;	Po							semicolon				U+003B: SEMICOLON	
-:	Po							colon				U+003A: COLON	
-.	Po							full stop				U+002E: FULL STOP	
-?	Po							question mark				U+003F: QUESTION MARK	
-!	Po							exclamation mark				U+0021: EXCLAMATION MARK	
-(	Ps							parenthesis				U+0028: LEFT PARENTHESIS	
-)	Pe							parenthesis				U+0029: RIGHT PARENTHESIS	
-…	Po					…	M	ellipsis				U+2026: HORIZONTAL ELLIPSIS	
-–	Pd							en dash				U+2013: EN DASH	
-—	Pd							em dash				U+2014: EM DASH	
-													
-§	Po							section sign				U+00A7: SECTION SIGN	
-†	Po							dagger				U+2020: DAGGER	
-‡	Po							double dagger				U+2021: DOUBLE DAGGER	
-′	Po							prime				U+2032: PRIME	
-″	Po							double prime				U+2033: DOUBLE PRIME	
-‐	Pd							hyphen				U+2010 HYPHEN	
-‑	Pd							non-breaking hyphen				U+2011 NON-BREAKING HYPHEN	
-													
-													
-													
-													
-ꓸꓼ						–	S					U+A4F8 U+A4FC: LISU LETTER TONE MYA TI, LETTER TONE MYA NA	
-ꓸꓽ						–	,					U+A4F8 U+A4FD: LISU LETTER TONE MYA TI, LETTER TONE MYA JEU	
-ꓹꓽ						–	<					U+A4F9 U+A4FD: LISU LETTER TONE NA PO, LETTER TONE MYA JEU	
-ꓺꓽ						–	.					U+A4FA U+A4FD: LISU LETTER TONE MYA CYA, LETTER TONE MYA JEU	
-ꓺꓼ						–	>					U+A4FA U+A4FC: LISU LETTER TONE MYA CYA, LETTER TONE MYA NA	
-ꓻꓽ						–	/					U+A4FB U+A4FD: LISU LETTER TONE MYA BO, LETTER TONE MYA JEU	
-ꓻꓼ						–	?					U+A4FB U+A4FC: LISU LETTER TONE MYA BO, LETTER TONE MYA NA	
+ꓑ	p	p		p	p	p	Lo		consonant				U+A4D1: LISU LETTER PA	✓
+ꓐ	b	b		b	b	b	Lo		consonant				U+A4D0: LISU LETTER BA	✓
+ꓒ	p	pʰ		pʰ	pʰ	P	Lo		consonant				U+A4D2: LISU LETTER PHA	✓
+ꓔ	t	t		t	t	t	Lo		consonant				U+A4D4: LISU LETTER TA	✓
+ꓓ	d	d		d	d	d	Lo		consonant				U+A4D3: LISU LETTER DA	✓
+ꓕ	t	tʰ		tʰ	tʰ	T	Lo		consonant				U+A4D5: LISU LETTER THA	✓
+ꓗ	k	k		k	k	k	Lo		consonant				U+A4D7: LISU LETTER KA	✓
+ꓖ	g	ɡ		ɡ	ɡ	g	Lo		consonant				U+A4D6: LISU LETTER GA	✓
+ꓘ	k	kʰ		kʰ	kʰ	K	Lo		consonant				U+A4D8: LISU LETTER KHA	✓
+ꓝ	t	t͡s		ʦ	ʦ	f	Lo		consonant				U+A4DD: LISU LETTER TSA	✓
+ꓜ	d	d͡z		ʣ	ʣ	z	Lo		consonant				U+A4DC: LISU LETTER DZA	✓
+ꓞ	t	ʦʰ		ʦʰ	ʦʰ	F	Lo		consonant				U+A4DE: LISU LETTER TSHA	✓
+ꓚ	t	ʨ		ʨ	ʨ	c	Lo		consonant				U+A4DA: LISU LETTER CA	✓
+ꓙ	d	ʥ		ʥ	ʥ	j	Lo		consonant				U+A4D9: LISU LETTER JA	✓
+ꓛ	t	ʨʰ		ʨʰ	ʨʰ	C	Lo		consonant				U+A4DB: LISU LETTER CHA	✓
+ꓩ	f	f		f	f	J	Lo		consonant				U+A4E9: LISU LETTER FA	✓
+ꓢ	s	s		s	s	s	Lo		consonant				U+A4E2: LISU LETTER SA	✓
+ꓤ	z	z		z	z	r	Lo		consonant				U+A4E4: LISU LETTER ZA	✓
+ꓣ	z	ʒ		ʒ	ʒ	R	Lo		consonant				U+A4E3: LISU LETTER ZHA	✓
+ꓫ	c	ɕ		ɕ	ɕ	x	Lo		consonant				U+A4EB: LISU LETTER SHA	✓
+ꓬ	z	ʑ		ʑ	ʑ	y	Lo		consonant/vowel				U+A4EC: LISU LETTER YA	✓
+ꓧ	x	x		x	x	h	Lo		consonant				U+A4E7: LISU LETTER XA	✓
+ꓭ	y	ɣ		ɣ	ɣ	B	Lo		consonant/vowel				U+A4ED: LISU LETTER GHA	✓
+ꓨ	h	ɦ		ɦ	ɦ	G	Lo		consonant				U+A4E8: LISU LETTER HHA	✓
+ꓦ	h	h		h	h	v	Lo		consonant				U+A4E6: LISU LETTER HA	✓
+ꓟ	m	m		m	m	m	Lo		consonant				U+A4DF: LISU LETTER MA	✓
+ꓠ	n	n		n	n	n	Lo		consonant				U+A4E0: LISU LETTER NA	✓
+ꓥ	n	ŋ		ŋ	ŋ	V	Lo		consonant				U+A4E5: LISU LETTER NGA	✓
+ꓪ	w	w		w	w	w	Lo		consonant/vowel				U+A4EA: LISU LETTER WA	✓
+ꓡ	l	l		l	l	l	Lo		consonant				U+A4E1: LISU LETTER LA	✓
+														
+														
+ꓮ	a	ɑ		ɑ	ɑ	a	Lo		vowel				U+A4EE: LISU LETTER A	✓
+ꓯ	e	ɛ		ɛ	ɛ	A	Lo		vowel				U+A4EF: LISU LETTER AE	✓
+ꓷ	e	ə		ə	ə	D	Lo		vowel				U+A4F7: LISU LETTER OE	✓
+ꓰ	e	e		e	e	e	Lo		vowel				U+A4F0: LISU LETTER E	✓
+ꓱ	o	ø		ø	ø	E	Lo		vowel				U+A4F1: LISU LETTER EU	✓
+ꓲ	i	i		i	i	i	Lo		vowel				U+A4F2: LISU LETTER I	✓
+ꓶ	u	ɯ		ɯ	ɯ	L	Lo		vowel				U+A4F6: LISU LETTER UH	✓
+ꓳ	o	o		o	o	o	Lo		vowel				U+A4F3: LISU LETTER O	✓
+ꓴ	u	u		u	u	u	Lo		vowel				U+A4F4: LISU LETTER U	✓
+ꓵ	y	y		y	y	U	Lo		vowel				U+A4F5: LISU LETTER UE	✓
+ˍ	a			ä	ä	q	Lm		vowel				U+02CD: MODIFIER LETTER LOW MACRON	
+ʼ	n			ñ	ñ	'	Lm		vowel				U+02BC: MODIFIER LETTER APOSTROPHE	
+														
+														
+ꓸ	^	˥˥		¹	˥˥		Lm		pitch 55 tone mark				U+A4F8: LISU LETTER TONE MYA TI	✓
+ꓹ	^	˧˥		²	˧˥		Lm		pitch 35 tone mark				U+A4F9: LISU LETTER TONE NA PO	✓
+ꓺ	^	˦˦		³	˦˦	H	Lm		pitch 44 tone mark				U+A4FA: LISU LETTER TONE MYA CYA	✓
+ꓻ	^	˧˧		⁴	˧˧	Y	Lm		pitch 33 tone mark				U+A4FB: LISU LETTER TONE MYA BO	✓
+ꓼ	^	˦˨		⁵	˦˨		Lm		pitch 42 tone mark				U+A4FC: LISU LETTER TONE MYA NA	✓
+ꓽ	^	˧˩		⁶	˧˩		Lm		pitch 31 tone mark				U+A4FD: LISU LETTER TONE MYA JEU	✓
+ꓹꓼ				²⁵		W			tone mark				U+A4F9 U+A4FC: LISU LETTER TONE NA PO, LISU LETTER TONE MYA NA	
+														
+														
+														
+														
+														
+														
+.	.			.	.		Po		full stop				U+002E: FULL STOP	
+꓿	.			|	.	O	Lu		full stop				U+A4FF: LISU PUNCTUATION FULL STOP	✓
+,	,			,	,		Po		comma				U+002C: COMMA	
+꓾	,			⹁	⹁	I	Po		comma				U+A4FE: LISU PUNCTUATION COMMA	✓
+:	:			:	:		Po	i	colon	biblical texts			U+003A: COLON	
+;	;			;	;		Po	i	semicolon	biblical texts			U+003B: SEMICOLON	
+!	!			!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK	
+?	?			?	?		Po		question mark				U+003F: QUESTION MARK	
+														
+														
+…	.			…	…		Po		ellipsis				U+2026: HORIZONTAL ELLIPSIS	
+														
+														
+														
+														
+														
+«	<			«	“		Pi		quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	<			‹	‘		Pi		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+“	<			 “	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
+‘	<			‘	‘		Pi		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+»	>			»	”		Pf		quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+›	>			›	’		Pf		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+”	>			”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
+’	>			’	’		Pf		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+														
+														
+														
+(	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS	
+《	(			[	[		Ps		bracket				U+300A: LEFT DOUBLE ANGLE BRACKET	
+)	)			)	)		Pe		parenthesis				U+0029: RIGHT PARENTHESIS	
+》	)			]	]		Pe		bracket				U+300B: RIGHT DOUBLE ANGLE BRACKET	
+														
+														
+-	-			-	-		Pd		hyphen				U+2010: HYPHEN	
+‑	-			‑	‑		Pd		non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd		en dash				U+2013: EN DASH	
+—	-			—	—		Pd		em dash				U+2014: EM DASH	
+														
+														
+														
+														
+														
+ʼ				ʼ	ʼ		Lm		apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE	
+§				§	§		Po		section sign				U+00A7: SECTION SIGN	
+†				†	†		Po		dagger				U+2020: DAGGER	
+‡				‡	‡		Po		double dagger				U+2021: DOUBLE DAGGER	
+′				′	′		Po		prime				U+2032: PRIME	
+″				″	″		Po		double prime				U+2033: DOUBLE PRIME	
+														
+														
+														
+														
+														
+														
+\u200B	=			␣			Cf		zero-width space				U+200B: ZERO WIDTH SPACE	
+\u200C	=			ᶻʷⁿʲ			Cf		zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=			ᶻʷʲ			Cf		zero-width joiner				U+200D: ZERO WIDTH JOINER	
+\u034F	=			ᶜᵍʲ			Mn		combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER​	
+\u2060	=			ʷʲ			Cf		word joiner				U+2060 WORD JOINER	
+														
+\u2067	=			ʳˡⁱ			Cf		rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE	
+\u2066	=			ˡʳⁱ			Cf		ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE	
+\u2068	=			ᶠˢⁱ			Cf		first-strong isolate				U+2068: FIRST STRONG ISOLATE	
+\u2069	=			ᵖᵈⁱ			Cf		pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE	
+\u202B	=			ʳˡᵉ			Cf		rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING	
+\u202A	=			ˡʳᵉ			Cf		ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING	
+\u202C	=			ᵖᵈᶠ			Cf		pop direction				U+202C: POP DIRECTIONAL FORMATTING	
+\u200F	=			ʳˡᵐ			Cf		rtl mark				U+200F: RIGHT-TO-LEFT MARK	
+\u200E	=			ˡʳᵐ			Cf		ltr mark				U+200E: LEFT-TO-RIGHT MARK	
+\u061C	=			ᵃˡᵐ			Cf		arabic letter mark				U+061C: ARABIC LETTER MARK	
+														
+														
+														
+														
+														
+														
+%	%			%			Po		percentage mark				U+0025: PERCENT SIGN	
+‰	%			‰			Po		per mille mark				U+2030: PER MILLE SIGN	
+														
+														
+														
+														
+ꓸꓼ				–		S							U+A4F8 U+A4FC: LISU LETTER TONE MYA TI, LETTER TONE MYA NA	
+ꓸꓽ				–		,							U+A4F8 U+A4FD: LISU LETTER TONE MYA TI, LETTER TONE MYA JEU	
+ꓹꓽ				–		<							U+A4F9 U+A4FD: LISU LETTER TONE NA PO, LETTER TONE MYA JEU	
+ꓺꓽ				–		.							U+A4FA U+A4FD: LISU LETTER TONE MYA CYA, LETTER TONE MYA JEU	
+ꓺꓼ				–		>							U+A4FA U+A4FC: LISU LETTER TONE MYA CYA, LETTER TONE MYA NA	
+ꓻꓽ				–		/							U+A4FB U+A4FD: LISU LETTER TONE MYA BO, LETTER TONE MYA JEU	
+ꓻꓼ				–		?							U+A4FB U+A4FC: LISU LETTER TONE MYA BO, LETTER TONE MYA NA	
+
 
 
 
@@ -130,15 +159,16 @@ latinPanel = '˥˥ ˧˥ ˦˦ ˧˧ ˦˨ ˧˩ ɑ ä ɕ d͡z ʥ ɛ ə ɦ kʰ ŋ ø 
 
 
 var cols = {
-"class": 1,
+"key": 1,
 "ipaLoc": 2,
-"transcription":3,
-"latin": 4,
-"key":5,
-"transLoc":6,
-"kbd": 7,
-"typeLoc": 8,
-"statusLoc": 9,
+"ipaPlus": 3,
+"transLoc": 4,
+"transcription": 5,
+"kbd": 6,
+"class": 7,
+"status": 8,
+"typeLoc": 9,
+"statusLoc": 10,
 
 "dvowel": 0,
 "ivowel": 0,
@@ -150,10 +180,11 @@ var cols = {
 "meaning":0,
 "shape": 0,
 "numLoc": 0,
+
 "nameLoc": 0,
 "nnameLoc": 0,
-"ucsName": 12,
-"block": 13,
+"ucsName": 13,
+"block": 14,
 
 //"othertranscriptions": [[3, 'ISO 9985']]
 }
