065ae5ac0d9e839bf628c7d96972bcf1e6a734f0
r12a <ishida@w3.org>
2021-12-14 19:59:21 +0000

    pa: Apply latest template #96

--- manifests/guru-pa.js
+++ manifests/guru-pa.js
@@ -15,6 +15,9 @@ pageWebfonts = ["Noto Sans Gurmukhi WF"]
 defaultSize = '24px'
 
 
+autoIndex = true
+
+
 
 // block settings
 
---
+++ scriptdb/guru-pa.js
@@ -0,0 +1,249 @@
+var spreadsheet = `
+ਕ	k	k	ə	k	k	k	Lo		consonant					kəkːɑ	U+0A15: GURMUKHI LETTER KA	✓
+ਖ	K	kʰ	ə	kh	kʰ	K	Lo		consonant					kʰəkʰːɑ	U+0A16: GURMUKHI LETTER KHA	✓
+ਗ	g	g	ə	g	g	i	Lo		consonant					gəgːɑ	U+0A17: GURMUKHI LETTER GA	✓
+ਘ	G	k	ə̀	gh	gʰ	I	Lo		consonant					kə̀gːɑ	U+0A18: GURMUKHI LETTER GHA	✓
+ਙ	N	ŋ	ə	ṅ	ŋ	U	Lo		consonant					ŋɑŋːɑ̃	U+0A19: GURMUKHI LETTER NGA	✓
+ਚ	t	t͡ʃ	ə	c	ʧ	;	Lo		consonant					ʧəʧːɑ	U+0A1A: GURMUKHI LETTER CA	✓
+ਛ	T	t͡ʃʰ	ə	ch	ʧʰ	:	Lo		consonant					ʧʰəʧʰːɑ	U+0A1B: GURMUKHI LETTER CHA	✓
+ਜ	d	d͡ʒ	ə	j	ʤ	p	Lo		consonant					ʤəʤːɑ	U+0A1C: GURMUKHI LETTER JA	✓
+ਝ	D	t͡ʃ	ə̀	jh	ʤʰ	P	Lo		consonant					ʧə̀ʤːɑ	U+0A1D: GURMUKHI LETTER JHA	✓
+ਞ	n	ɲ	ə	ñ	ɲ	}	Lo		consonant					ɲəɲːɑ̃	U+0A1E: GURMUKHI LETTER NYA	✓
+ਟ	t	ʈ	ə	ṭ	ʈ	'	Lo		consonant					ʈɑtt:a	U+0A1F: GURMUKHI LETTER TTA	✓
+ਠ	T	ʈʰ	ə	ṭh	ʈʰ	"	Lo		consonant					ʈʰəʈʰːɑ	U+0A20: GURMUKHI LETTER TTHA	✓
+ਡ	d	ɖ	ə	ḍ	ɖ	[	Lo		consonant					ɖəɖːɑ	U+0A21: GURMUKHI LETTER DDA	✓
+ਢ	D	ʈ	ə̀	ḍh	ɖʰ	{	Lo		consonant					ʈə̀ɖːɑ	U+0A22: GURMUKHI LETTER DDHA	✓
+ਣ	n	ɳ	ə	ṇ	ɳ	C	Lo		consonant					ɳɑɳɑ	U+0A23: GURMUKHI LETTER NNA	✓
+ਤ	t¶	t	ə	t	t	l	Lo		consonant					t̪ət̪ːɑ	U+0A24: GURMUKHI LETTER TA	✓
+ਥ	T	tʰ	ə	th	tʰ	L	Lo		consonant					t̪ʰət̪ʰːɑ	U+0A25: GURMUKHI LETTER THA	✓
+ਦ	d¶	d	ə	d	d	o	Lo		consonant					d̪əd̪ːɑ	U+0A26: GURMUKHI LETTER DA	✓
+ਧ	D	t	ə̀	dh	dʰ	O	Lo		consonant					t̪ə̀d̪ːɑ	U+0A27: GURMUKHI LETTER DHA	✓
+ਨ	n¶	n	ə	n	n	v	Lo		consonant					nənːɑ	U+0A28: GURMUKHI LETTER NA	✓
+ਪ	p	p	ə	p	p	h	Lo		consonant					pəpːɑ	U+0A2A: GURMUKHI LETTER PA	✓
+ਫ	P	pʰ	ə	ph	pʰ	H	Lo		consonant					pʰəpʰːɑ	U+0A2B: GURMUKHI LETTER PHA	✓
+ਬ	b	b	ə	b	b	y	Lo		consonant					bəbːɑ	U+0A2C: GURMUKHI LETTER BA	✓
+ਭ	B	p	ə̀	bh	bʰ	Y	Lo		consonant					pə̀bːɑ	U+0A2D: GURMUKHI LETTER BHA	✓
+ਮ	m	m	ə	m	m	c	Lo		consonant					məmːɑ	U+0A2E: GURMUKHI LETTER MA	✓
+ਯ	y	j	ə	y	y	/	Lo		consonant					jəjːɑ	U+0A2F: GURMUKHI LETTER YA	✓
+ਰ	r	r	ə	r	r	j	Lo		consonant					ɾɑɾɑ	U+0A30: GURMUKHI LETTER RA	✓
+ੜ	r	ɽ	ə	ṛ	ɽ	J	Lo		consonant					ɽɑɽɑ	U+0A5C: GURMUKHI LETTER RRA	✓
+ਲ	l	l	ə	l	l	n	Lo		consonant					ləlːɑ	U+0A32: GURMUKHI LETTER LA	✓
+ਵ	v	ʋ	ə	v	v	b	Lo		consonant					ʋɑʋːɑ	U+0A35: GURMUKHI LETTER VA	✓
+ਸ	s¶	s	ə	s	s	m	Lo		consonant					səsːɑ	U+0A38: GURMUKHI LETTER SA	✓
+ਹ	h	ɦ	ə	h	h	u	Lo		consonant					ɦɑɦɑ	U+0A39: GURMUKHI LETTER HA	✓
+																
+																
+\u0A59	x	x	ə	x	x		Lo		consonant					kʰəkʰːɑ pɛɾ bɪnd̪i	U+0A59: GURMUKHI LETTER KHHA	
+\u0A5A	y	ɣ	ə	ġ	ɣ		Lo		consonant					gəgːɑ pɛɾ bɪnd̪i	U+0A5A: GURMUKHI LETTER GHHA	
+\u0A33	l	ɭ	ə	ḷ	ɭ		Lo		consonant					ləlːɑ pɛɾ bɪnd̪i	U+0A33: GURMUKHI LETTER LLA	
+\u0A5B	z	z	ə	z	z		Lo		consonant					ʤəʤːɑ pɛɾ bɪnd̪i	U+0A5B: GURMUKHI LETTER ZA	
+\u0A5E	f	f	ə	f	f		Lo		consonant					pʰəpʰːɑ pɛɾ bɪnd̪i	U+0A5E: GURMUKHI LETTER FA	
+\u0A36	s	ʃ	ə	ś	ʃ		Lo		consonant					səsːɑ pɛɾ bɪnd̪i	U+0A36: GURMUKHI LETTER SHA	
+																
+																
+ੵ	y				y̆	|	Mn​		medial y					yakash	U+0A75: GURMUKHI SIGN YAKASH​	✓
+																
+																
+ਁ				m̐	–	+	Mn​								U+0A01: GURMUKHI SIGN ADAK BINDI​	✓
+ੰ	N			ṃ	ŋ̽	X	Mn​		vowel nasalisation					tippi	U+0A70: GURMUKHI TIPPI​	✓
+ਂ	[			ṁ	˜	x	Mn​		vowel nasalisation					bindi	U+0A02: GURMUKHI SIGN BINDI​	✓
+ਃ	h			ḥ	h̽	_	Mc​		visarga					visarg	U+0A03: GURMUKHI SIGN VISARGA​	✓
+																
+																
+																
+																
+ਆ	A	ɑ ä		ā	ạ̄	E	Lo		independent vowel		ਾ			kannā	U+0A06: GURMUKHI LETTER AA	✓
+ਇ	I	ɪ		i	ɪ̣	F	Lo		independent vowel		ਿ			sihārī	U+0A07: GURMUKHI LETTER I	✓
+ਈ	I	i		ī	ị	R	Lo		independent vowel		ੀ			bihārī	U+0A08: GURMUKHI LETTER II	✓
+ਉ	U	ʊ		u	ʊ̣	G	Lo		independent vowel		ੁ			onkaṛ	U+0A09: GURMUKHI LETTER U	✓
+ਊ	U	u		ū	ụ	T	Lo		independent vowel		ੂ			dulankaṛ	U+0A0A: GURMUKHI LETTER UU	✓
+ਏ	E	e		ē	ẹ	S	Lo		independent vowel		ੇ			lāvā̃	U+0A0F: GURMUKHI LETTER EE	✓
+ਐ	E	ɛ		ai	ɛ̣	W	Lo		independent vowel		ੈ			dulāvā̃	U+0A10: GURMUKHI LETTER AI	✓
+ਓ	O	o		o	ọ	A	Lo		independent vowel		ੋ			hōṛā	U+0A13: GURMUKHI LETTER OO	✓
+ਔ	O	ɔ		au	ɔ̣	Q	Lo		independent vowel		ੌ			kanōṛā	U+0A14: GURMUKHI LETTER AU	✓
+																
+																
+ਾ	a	ɑ ä		ā	ā	e	Mc​		vowel sign			ਆ		kannā	U+0A3E: GURMUKHI VOWEL SIGN AA​	✓
+ਿ	i	ɪ		i	ɪ	f	Mc​		vowel sign			ਇ		sihārī	U+0A3F: GURMUKHI VOWEL SIGN I​	✓
+ੀ	i	i		ī	ī	r	Mc​		vowel sign			ਈ		bihārī	U+0A40: GURMUKHI VOWEL SIGN II​	✓
+ੁ	u	ʊ		u	ʊ	g	Mn​		vowel sign			ਉ		onkaṛ	U+0A41: GURMUKHI VOWEL SIGN U​	✓
+ੂ	u	u		ū	ū	t	Mn​		vowel sign			ਊ		dulankaṛ	U+0A42: GURMUKHI VOWEL SIGN UU​	✓
+ੇ	e	e		ē	e	s	Mn​		vowel sign			ਏ		lāvā̃	U+0A47: GURMUKHI VOWEL SIGN EE​	✓
+ੈ	e	ɛ		ai	ɛ	w	Mn​		vowel sign			ਐ		dulāvā̃	U+0A48: GURMUKHI VOWEL SIGN AI​	✓
+ੋ	o	o		o	o	a	Mn​		vowel sign			ਓ		hōṛā	U+0A4B: GURMUKHI VOWEL SIGN OO​	✓
+ੌ	o	ɔ		au	ɔ	q	Mn​		vowel sign			ਔ		kanōṛā	U+0A4C: GURMUKHI VOWEL SIGN AU​	✓
+																
+																
+																
+																
+ੴ					–	V	Lo		symbol					ek onkar	U+0A74: GURMUKHI EK ONKAR	✓
+☬							So		symbol					adi shakti	U+262C: ADI SHAKTI	
+																
+																
+ੑ	[				ˊ	\	Mn​		high tone marker					udaat	U+0A51: GURMUKHI SIGN UDAAT​	✓
+																
+																
+																
+																
+																
+																
+ਖ਼	x	x	ə	x	kʰˑ	$	s		consonant with nukta					kʰəkʰːɑ pɛɾ bɪnd̪i	U+0A16 U+0A3C: GURMUKHI LETTER KHA, GURMUKHI SIGN NUKTA	
+ਗ਼	y	ɣ	ə	ġ	gˑ	%	s		consonant with nukta					gəgːɑ pɛɾ bɪnd̪i	U+0A17 U+0A3C: GURMUKHI LETTER GA, GURMUKHI SIGN NUKTA	
+ਲ਼	l	ɭ	ə	ḷ	lˑ	n	s		consonant with nukta					ləlːɑ pɛɾ bɪnd̪i	U+0A32 U+0A3C: GURMUKHI LETTER LA, GURMUKHI SIGN NUKTA	
+ਜ਼	z	z	ə	z	ʤˑ	^	s		consonant with nukta					ʤəʤːɑ pɛɾ bɪnd̪i	U+0A1C U+0A3C: GURMUKHI LETTER JA, GURMUKHI SIGN NUKTA	
+ਫ਼	f	f	ə	f	pʰˑ	&	s		consonant with nukta					pʰəpʰːɑ pɛɾ bɪnd̪i	U+0A2B U+0A3C: GURMUKHI LETTER PHA, GURMUKHI SIGN NUKTA	
+ਸ਼	s	ʃ	ə	ś	sˑ	M	s		consonant with nukta						U+0A38 U+0A3C: GURMUKHI LETTER SA, GURMUKHI SIGN NUKTA	
+																
+																
+																
+੍ਰ					–	#									U+0A4D U+0A30: GURMUKHI SIGN VIRAMA, LETTER RA​	
+ੜ੍ਹ					–	Z									U+0A5C U+0A4D U+0A39: GURMUKHI LETTER RRA, SIGN VIRAMA, LETTER HA	
+																
+																
+																
+																
+																
+।	|			.	|	>	Po	i	section divider						U+0964: DEVANAGARI DANDA	
+॥	|			|	¶	<	Po		section divider						U+0965: DEVANAGARI DOUBLE DANDA	
+.	.			.	.		Po		full stop						U+002E: FULL STOP	
+,	,			,	,		Po		comma						U+002C: COMMA	
+⹁	,			⹁	⹁		Po		comma						U+2E41: REVERSED COMMA	
+:	:			:	:		Po		colon						U+003A: COLON	
+;	;			;	;		Po		semicolon						U+003B: SEMICOLON	
+!	!			!	!		Po		exclamation mark						U+0021: EXCLAMATION MARK	
+?	?			?	?		Po		question mark						U+003F: QUESTION MARK	
+																
+																
+…	.			…	…		Po		ellipsis						U+2026: HORIZONTAL ELLIPSIS	
+																
+																
+																
+																
+																
+«	<			“	«		Pi		quotation mark						U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	<			‘	‹		Pi		quotation mark						U+2018: LEFT SINGLE QUOTATION MARK	
+“	<			“	 “		Pi		quotation mark						U+201C: LEFT DOUBLE QUOTATION MARK	
+‘	<			‘	‘		Pi		quotation mark						U+2018: LEFT SINGLE QUOTATION MARK	
+»	>			”	»		Pf		quotation mark						U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+›	>			’	›		Pf		quotation mark						U+2019: RIGHT SINGLE QUOTATION MARK	
+”	>			”	”		Pf		quotation mark						U+201D: RIGHT DOUBLE QUOTATION MARK	
+’	>			’	’		Pf		quotation mark						U+2019: RIGHT SINGLE QUOTATION MARK	
+																
+																
+																
+(	(			(	(		Ps		parenthesis						U+0028: LEFT PARENTHESIS	
+[	(			[	[		Pe		bracket						U+005B: LEFT SQUARE BRACKET	
+)	)			)	)		Ps		parenthesis						U+0029: RIGHT PARENTHESIS	
+]	)			]	]		Pe		bracket						U+005D: RIGHT SQUARE BRACKET	
+																
+																
+-	-			-	-		Pd		hyphen						U+2010: HYPHEN	
+‑	-			‑	‑		Pd		non-breaking hyphen						U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd		en dash						U+2013: EN DASH	
+—	-			—	—		Pd		em dash						U+2014: EM DASH	
+																
+																
+																
+																
+ਅ	E	ə		a	+	D	Lo		vowel support & independent vowel					æɽɑ	U+0A05: GURMUKHI LETTER A	✓
+ੲ	[	∅			‘	@	Lo	d	vowel support	deprecated				iɽi	U+0A72: GURMUKHI IRI	✓
+ੳ	[	∅			’	!	Lo	d	vowel support	deprecated				uɽɑ	U+0A73: GURMUKHI URA	✓
+																
+																
+‍ଁ	[	̃		̃	˜				nasalisation							
+																
+																
+																
+																
+																
+																
+੍	*				͞	d	Mn​		virama					halant	U+0A4D: GURMUKHI SIGN VIRAMA​	✓
+					͓											
+																
+਼	/				ˑ	]	Mn​		nukta						U+0A3C: GURMUKHI SIGN NUKTA​	✓
+																
+																
+ੱ	&				&	=	Mn​		gemination marker					addak	U+0A71: GURMUKHI ADDAK​	✓
+																
+																
+																
+																
+																
+																
+																
+																
+ʼ				ʼ	ʼ		Lm		apostrophe						U+02BC: MODIFIER LETTER APOSTROPHE	
+§				§	§		Po		section sign						U+00A7: SECTION SIGN	
+†				†	†		Po		dagger						U+2020: DAGGER	
+‡				‡	‡		Po		double dagger						U+2021: DOUBLE DAGGER	
+′				′	′		Po		prime						U+2032: PRIME	
+″				″	″		Po		double prime						U+2033: DOUBLE PRIME	
+																
+																
+																
+																
+																
+																
+\u200B	=				␣		Cf		zero-width space						U+200B: ZERO WIDTH SPACE	
+\u200C	=				ᶻʷⁿʲ		Cf		zero-width non-joiner						U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=				ᶻʷʲ		Cf		zero-width joiner						U+200D: ZERO WIDTH JOINER	
+\u034F	=				ᶜᵍʲ		Mn		combining grapheme joiner						U+034F: COMBINING GRAPHEME JOINER​	
+\u2060	=				ʷʲ		Cf		word joiner						U+2060 WORD JOINER	
+																
+																
+																
+																
+੧	±			1	1̣	1	Nd		digit				ਇੱਕ	ɪkː	U+0A67: GURMUKHI DIGIT ONE	✓
+੨	±			2	2̣	2	Nd		digit				ਦੋ	d̪oː	U+0A68: GURMUKHI DIGIT TWO	✓
+੩	±			3	3̣	3	Nd		digit				ਤਿੰਨ	t̪ɪnː	U+0A69: GURMUKHI DIGIT THREE	✓
+੪	±			4	4̣	4	Nd		digit				ਚਾਰ	tʃɑːɾ	U+0A6A: GURMUKHI DIGIT FOUR	✓
+੫	±			5	5̣	5	Nd		digit				ਪੰਜ	pəndʒ	U+0A6B: GURMUKHI DIGIT FIVE	✓
+੬	±			6	6̣	6	Nd		digit				ਛੇ	tʃʰeː	U+0A6C: GURMUKHI DIGIT SIX	✓
+੭	±			7	7̣	7	Nd		digit				ਸੱਤ	sət̪ː	U+0A6D: GURMUKHI DIGIT SEVEN	✓
+੮	±			8	8̣	8	Nd		digit				ਅੱਠ	əʈʰː	U+0A6E: GURMUKHI DIGIT EIGHT	✓
+੯	±			9	9̣	9	Nd		digit				ਨੌਂ	nɔ̃	U+0A6F: GURMUKHI DIGIT NINE	✓
+੦	±			0	0̣	0	Nd		digit				ਸਿਫ਼ਰ	sɪfəɾ	U+0A66: GURMUKHI DIGIT ZERO	✓
+																
+																
+%	%				%		Po		percentage mark						U+0025: PERCENT SIGN	
+‰	%				‰		Po		per mille mark						U+2030: PER MILLE SIGN	
+
+
+
+
+`
+
+
+latinPanel = 'ɑ ä ā d͡ʒ ɖ ḍ ə ə̀ ē ɛ ġ ɦ ḥ ɪ ī kʰ ɭ ḷ m̐ ṃ ṁ ŋə ṅ ɲ ñ ɳ ṇ ∅ ɔ pʰ ɽ ṛ ʃ ś t͡ʃ t͡ʃʰ ʈ ṭ ʈʰ tʰ ʊ ū ʋ ɣ'
+
+
+var cols = {
+"key": 1,
+"ipaLoc": 2,
+"ipaPlus": 3,
+"transcription": 4,
+"transLoc": 5,
+"kbd": 6,
+"class": 7,
+"status": 8,
+"typeLoc": 9,
+"statusLoc": 10,
+
+"dvowel": 11,
+"ivowel": 12,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+"numLoc": 0,
+
+"nnameLoc": 13,
+"nameLoc": 14,
+"ucsName": 15,
+"block": 16,
+
+"othertranscriptions": [[4, 'ISO 19515']]
+}
