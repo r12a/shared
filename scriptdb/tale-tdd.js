ed66e2a9a7d62705f7dae5fb89bb34e5a6e8c339
r12a <ishida@w3.org>
2021-12-16 17:47:54 +0000

    tdd: Apply latest template #96

---
+++ scriptdb/tale-tdd.js
@@ -0,0 +1,134 @@
+var spreadsheet = `
+ᥐ	k	k	a		k	g	Lo		consonant				U+1950: TAI LE LETTER KA	✓
+ᥠ	k	kʰ	a		kʰ	/	Lo		consonant				U+1960: TAI LE LETTER KHA	✓
+ᥒ	n	ŋ	a		ŋ	n	Lo		consonant				U+1952: TAI LE LETTER NGA	✓
+														
+ᥓ	c	t͡s	a		ʦ	j	Lo		consonant				U+1953: TAI LE LETTER TSA	✓
+ᥡ	c	t͡sʰ	a		ʦʰ	.	Lo		consonant				U+1961: TAI LE LETTER TSHA	✓
+														
+ᥖ	t	t	a		t	d	Lo		consonant				U+1956: TAI LE LETTER TA	✓
+ᥗ	t	tʰ	a		tʰ	t	Lo		consonant				U+1957: TAI LE LETTER THA	✓
+ᥢ	n¶	n	a		n	,	Lo		consonant				U+1962: TAI LE LETTER NA	✓
+														
+ᥙ	p	p	a		p	b	Lo		consonant				U+1959: TAI LE LETTER PA	✓
+ᥚ	p	pʰ	a		pʰ	p	Lo		consonant				U+195A: TAI LE LETTER PHA	✓
+ᥛ	m	m	a		m	m	Lo		consonant				U+195B: TAI LE LETTER MA	✓
+														
+ᥕ	y	j	a		y	y	Lo		consonant				U+1955: TAI LE LETTER YA	✓
+ᥘ	l	l	a		l	l	Lo		consonant				U+1958: TAI LE LETTER LA	✓
+ᥝ	w	v ʷ	a		v	v	Lo		consonant				U+195D: TAI LE LETTER VA	✓
+														
+ᥞ	h	h	a		h	h	Lo		consonant				U+195E: TAI LE LETTER HA	✓
+ᥑ	x	x	a		x	k	Lo		consonant				U+1951: TAI LE LETTER XA	✓
+ᥔ	s	s	a		s	s	Lo		consonant				U+1954: TAI LE LETTER SA	✓
+ᥜ	f	f	a		f	f	Lo		consonant				U+195C: TAI LE LETTER FA	✓
+														
+ᥟ	}	ʔa	a		ʔ	z	Lo		consonant				U+195F: TAI LE LETTER QA	✓
+														
+														
+														
+ᥣ	a	aː			a	a	Lo		vowel				U+1963: TAI LE LETTER A	✓
+ᥥ	e	e			e	e	Lo		vowel				U+1965: TAI LE LETTER EE	✓
+ᥫ	e	ə			ə	r	Lo		vowel				U+196B: TAI LE LETTER E	✓
+ᥦ	e	ɛ ia			ɛ	x	Lo		vowel				U+1966: TAI LE LETTER EH	✓
+ᥤ	i	i			i	i	Lo		vowel				U+1964: TAI LE LETTER I	✓
+ᥪ	u	ɯ			ɯ	q	Lo		vowel				U+196A: TAI LE LETTER UE	✓
+ᥩ	o	ɔ ua			ɔ	w	Lo		vowel				U+1969: TAI LE LETTER O	✓
+ᥨ	o	o			o	o	Lo		vowel				U+1968: TAI LE LETTER OO	✓
+ᥧ	u	u			u	u	Lo		vowel				U+1967: TAI LE LETTER U	✓
+														
+ᥬ	a	aɯ			aᵚ	c	Lo		dipthong				U+196C: TAI LE LETTER AUE	✓
+ᥭ	j	j			ʲ	;	Lo		semivowel				U+196D: TAI LE LETTER AI	✓
+														
+														
+ᥰ	^	˥˥			²̱	-	Lo		tone letter				U+1970: TAI LE LETTER TONE-2	✓
+ᥱ	^	˩˩			³̱	=	Lo		tone letter				U+1971: TAI LE LETTER TONE-3	✓
+ᥲ	^	˧˩			⁴̱	[	Lo		tone letter				U+1972: TAI LE LETTER TONE-4	✓
+ᥳ	^	˥˧			⁵̱	]	Lo		tone letter				U+1973: TAI LE LETTER TONE-5	✓
+ᥴ	^	˨˦			⁶̱	'	Lo		tone letter				U+1974: TAI LE LETTER TONE-6	✓
+														
+														
+														
+														
+														
+														
+̈	^	˥˥			²		Mn​	i	tone mark	infrequent			U+0308: COMBINING DIAERESIS​	
+̌	^	˩˩			³		Mn​	i	tone mark	infrequent			U+030C: COMBINING CARON​	
+̀	^	˧˩			⁴		Mn​	i	tone mark	infrequent			U+0300: COMBINING GRAVE ACCENT​	
+̇	^	˥˧			⁵		Mn​	i	tone mark	infrequent			U+0307: COMBINING DOT ABOVE​	
+́	^	˨˦			⁶		Mn​	i	tone mark	infrequent			U+0301: COMBINING ACUTE ACCENT​	
+														
+〈	(				(		Ps						U+3008: LEFT ANGLE BRACKET	
+〉	)				)		Pe						U+3009: RIGHT ANGLE BRACKET	
+《	(				(	<	Ps						U+300A: LEFT DOUBLE ANGLE BRACKET	
+》	)				)	>	Pe						U+300B: RIGHT DOUBLE ANGLE BRACKET	
+（	(				(		Ps						U+FF08: FULLWIDTH LEFT PARENTHESIS	
+）	)				)		Pe						U+FF09: FULLWIDTH RIGHT PARENTHESIS	
+！	!				!		Po						U+FF01: FULLWIDTH EXCLAMATION MARK	
+？	?				?		Po						U+FF1F: FULLWIDTH QUESTION MARK	
+：	:				:		Po						U+FF1A: FULLWIDTH COLON	
+；	;				;	L	Po						U+FF1B: FULLWIDTH SEMICOLON	
+。	.				.	.	Po						U+3002: IDEOGRAPHIC FULL STOP	
+、	,				,	,	Po						U+3001: IDEOGRAPHIC COMMA	
+，	,				,		Po						U+FF0C: FULLWIDTH COMMA	
+．	.				.	\	Po						U+FF0E: FULLWIDTH FULL STOP	
+														
+														
+														
+၁	#	1			1̣	1	Nd	i	myanmar digit	infrequent			U+1041: MYANMAR DIGIT ONE	
+၂	#	2			2̣	2	Nd	i	myanmar digit	infrequent			U+1042: MYANMAR DIGIT TWO	
+၃	#	3			3̣	3	Nd	i	myanmar digit	infrequent			U+1043: MYANMAR DIGIT THREE	
+၄	#	4			4̣	4	Nd	i	myanmar digit	infrequent			U+1044: MYANMAR DIGIT FOUR	
+၅	#	5			5̣	5	Nd	i	myanmar digit	infrequent			U+1045: MYANMAR DIGIT FIVE	
+၆	#	6			6̣	6	Nd	i	myanmar digit	infrequent			U+1046: MYANMAR DIGIT SIX	
+၇	#	7			7̣	7	Nd	i	myanmar digit	infrequent			U+1047: MYANMAR DIGIT SEVEN	
+၈	#	8			8̣	8	Nd	i	myanmar digit	infrequent			U+1048: MYANMAR DIGIT EIGHT	
+၉	#	9			9̣	9	Nd	i	myanmar digit	infrequent			U+1049: MYANMAR DIGIT NINE	
+၀	#	0			0̣	0	Nd	i	myanmar digit	infrequent			U+1040: MYANMAR DIGIT ZERO	
+														
+														
+														
+														
+														
+														
+ᥨᥝ	o	o			ov		s						U+1968 U+195D: TAI LE LETTER OO, LETTER VA	
+
+
+
+`
+
+
+latinPanel = '˥˥ ˩˩ ˧˩ ˥˧ ˨˦ aː ə ɛ kʰ ŋ ɔ pʰ t͡s t͡sʰ tʰ ʷ ɯ ʔ'
+
+
+var cols = {
+"key": 1,
+"ipaLoc": 2,
+"ipaPlus": 3,
+"transcription": 4,
+"transLoc": 5,
+"kbd": 6,
+"class": 7,
+"status": 8,
+"typeLoc": 9,
+"statusLoc": 10,
+
+"numLoc": 0,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"lc":0,
+"uc":0,
+"meaning":0,
+"shape": 0,
+
+"nnameLoc": 11,
+"nameLoc": 12,
+"ucsName": 13,
+"block":14,
+
+//"othertranscriptions": [[4, 'ISO 15919']]
+}
+
