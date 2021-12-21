bbe90e996d0b256e1a2dd3192ddd4e4a8134d650
r12a <ishida@w3.org>
2021-12-21 12:58:19 +0000

    + manifests/mong-mn.js

---
+++ manifests/mong-mn.js
@@ -0,0 +1,24 @@
+
+langTag='mn'
+scriptTag='mong'
+notesLangtag='mn'
+
+explainerDir='mongolian/'
+blockDir='mongolian/'
+pickerDir='mong-mn/'
+scriptSummaryTableName='khk-mong'
+charUsageBCP='khk-mong'
+orthogName = 'Mongolian'
+indic = false
+
+pageWebfonts = ["Noto Serif Mongolian WF"]
+defaultSize = '24px'
+
+autoIndex = true
+
+// block settings
+
+languageName = 'Halh Mongolian'
+letterClassName = 'mn'
+lang = 'mn'
+direction = ''
