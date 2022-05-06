b89e559ed3c8cbdf3882790195a365ca3a160de9
r12a <ishida@w3.org>
2022-05-06 08:23:25 +0100

    + manifests/btk.js

---
+++ manifests/btk.js
@@ -0,0 +1,27 @@
+
+langTag='btk'
+scriptTag='batk'
+notesLangtag='btk'
+
+explainerDir='batk'
+blockDir='batk/'
+pickerDir='batk/'
+scriptSummaryTableName='btk'
+charUsageBCP='btk'
+orthogName = 'Batak'
+indic = true
+
+pageWebfonts = ["Noto Sans Batak WF"]
+defaultSize = '28px'
+
+autoIndex = true
+
+
+
+// block settings
+
+languageName = 'Batak'
+letterClassName = 'btk'
+lang = 'btk'
+direction = ''
+
