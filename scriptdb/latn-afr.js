d79628b93f2212cf186f97199f36fa565d624e22
Denis Moyogo Jacquerye <moyogo@gmail.com>
2022-09-02 13:44:41 +0200

    latn-afr: add characters used in African orthographies
    - æ in General Alphabet for Cameroon Languages, used in Tikar
    - ɵ and ɥ are used in Dan in Liberia
    - ɯ used in Eastern Dan (Côte d'Ivoire)
    - ʘ used in Nǀuu (South Africa)
    - Ƃ sometimes used as uppercase of ɓ (although it should be at font
      level) in Dan, Kpele, Loma (Liberia)
    - β used in Gabon Scientific Alphabet
    - ŧ used in Balante, Hassaniya, Mancagne (Senegal)
    - ʝ used in Ik (Uganda)

--- scriptdb/latn-afr.js
+++ scriptdb/latn-afr.js
@@ -1,6 +1,8 @@
 var spreadsheet = `
 b		ː	¶b	b		consonant		U+0062: LATIN SMALL LETTER B	
 ɓ		t͡s	b	ɓ		consonant		U+0253: LATIN SMALL LETTER B WITH HOOK	
+ꞵ			b	ꞵ		consonant		U+A7B5: LATIN SMALL LETTER BETA	
+ƃ			b	ƃ		consonant		U+0183: LATIN SMALL LETTER B WITH TOPBAR	
 c		d͡z	¶c	c		consonant		U+0063: LATIN SMALL LETTER C	
 ƈ		t͡ʃ	c	ƈ		consonant		U+0188: LATIN SMALL LETTER C WITH HOOK	
 d		d͡ʒ	¶d	d		consonant		U+0064: LATIN SMALL LETTER D	
@@ -16,6 +18,7 @@ h			¶h	h		consonant		U+0068: LATIN SMALL LETTER H
 ɦ			h	ɦ		consonant		U+0266: LATIN SMALL LETTER H WITH HOOK	
 j			¶j	j		consonant		U+006A: LATIN SMALL LETTER J	
 ɉ			j	ɉ		consonant		U+0249: LATIN SMALL LETTER J WITH STROKE	
+ʝ			j	ʝ		consonant		U+029D: LATIN SMALL LETTER J WITH CROSSED-TAIL	
 k			¶k	k		consonant		U+006B: LATIN SMALL LETTER K	
 ƙ			k	ƙ		consonant		U+0199: LATIN SMALL LETTER K WITH HOOK	
 l			¶l	l		consonant		U+006C: LATIN SMALL LETTER L	
@@ -34,6 +37,7 @@ r			¶r	r		consonant		U+0072: LATIN SMALL LETTER R
 s			¶s	s		consonant		U+0073: LATIN SMALL LETTER S	
 ʃ			s	ʃ		consonant		U+0283: LATIN SMALL LETTER ESH	
 t			¶t	t		consonant		U+0074: LATIN SMALL LETTER T	
+ŧ			t	ŧ		consonant		U+0167: LATIN SMALL LETTER T WITH STROKE	
 ƭ			t	ƭ		consonant		U+01AD: LATIN SMALL LETTER T WITH HOOK	
 ʈ			t	ʈ		consonant		U+0288: LATIN SMALL LETTER T WITH RETROFLEX HOOK	
 v			¶v	v		consonant		U+0076: LATIN SMALL LETTER V	
@@ -54,12 +58,15 @@ z			¶z	z		consonant		U+007A: LATIN SMALL LETTER Z
 ǁ			|	ǁ		consonant		U+01C1: LATIN LETTER LATERAL CLICK	
 ǂ			|	ǂ		consonant		U+01C2: LATIN LETTER ALVEOLAR CLICK	
 ǃ			|	ǃ		consonant		U+01C3: LATIN LETTER RETROFLEX CLICK	
+ʘ			|	ʘ		consonant		U+0298: LATIN LETTER BILABIAL CLICK	
 ꞌ			'	ꞌ		consonant		U+A78C: LATIN SMALL LETTER SALTILLO	
 ʼ			'	ʼ		consonant		U+02BC: MODIFIER LETTER APOSTROPHE	
 									
 									
 B			¶B	B		uppercase consonant		U+0042: LATIN CAPITAL LETTER B	
 Ɓ			B	Ɓ		uppercase consonant		U+0181: LATIN CAPITAL LETTER B WITH HOOK	
+Ƃ			B	Ƃ		uppercase consonant		U+0182: LATIN CAPITAL LETTER B WITH TOPBAR	
+Ꞵ			B	Ꞵ		uppercase consonant		U+A7B4: LATIN CAPITAL LETTER BETA	
 C			¶C	C		uppercase consonant		U+0043: LATIN CAPITAL LETTER C	
 Ƈ			C	Ƈ		uppercase consonant		U+0187: LATIN CAPITAL LETTER C WITH HOOK	
 D			¶D	D		uppercase consonant		U+0044: LATIN CAPITAL LETTER D	
@@ -75,6 +82,7 @@ H			¶H	H		uppercase consonant		U+0048: LATIN CAPITAL LETTER H
 Ɦ			H	Ɦ		uppercase consonant		U+A7AA: LATIN CAPITAL LETTER H WITH HOOK	
 J			¶J	J		uppercase consonant		U+004A: LATIN CAPITAL LETTER J	
 Ɉ			J	Ɉ		uppercase consonant		U+0248: LATIN SMALL LETTER J WITH STROKE	
+Ʝ			J	Ʝ		uppercase consonant		U+A7B2: LATIN SMALL LETTER J WITH CROSSED-TAIL	
 K			¶K	K		uppercase consonant		U+004B: LATIN CAPITAL LETTER K	
 Ƙ			K	Ƙ		uppercase consonant		U+0198: LATIN CAPITAL LETTER K WITH HOOK	
 L			¶L	L		uppercase consonant		U+004C: LATIN CAPITAL LETTER L	
@@ -93,6 +101,7 @@ R			¶R	R		uppercase consonant		U+0052: LATIN CAPITAL LETTER R
 S			¶S	S		uppercase consonant		U+0053: LATIN CAPITAL LETTER S	
 Ʃ			S	Ʃ		uppercase consonant		U+01A9: LATIN CAPITAL LETTER ESH	
 T			¶T	T		uppercase consonant		U+0054: LATIN CAPITAL LETTER T	
+Ŧ			T	Ŧ		uppercase consonant		U+0166: LATIN CAPITAL LETTER T WITH STROKE	
 Ƭ			T	Ƭ		uppercase consonant		U+01AC: LATIN CAPITAL LETTER T WITH HOOK	
 Ʈ			T	Ʈ		uppercase consonant		U+01AE: LATIN CAPITAL LETTER T WITH RETROFLEX HOOK	
 V			¶V	V		uppercase consonant		U+0056: LATIN CAPITAL LETTER V	
@@ -115,6 +124,7 @@ Z			Y	Z		uppercase consonant		U+005A: LATIN CAPITAL LETTER Z
 a			a	a		vowel		U+0061: LATIN SMALL LETTER A	
 ɑ			a	ɑ		vowel		U+0251: LATIN SMALL LETTER ALPHA	
 ʌ			a	ʌ		vowel		U+028C: LATIN SMALL LETTER TURNED V	
+æ			a	æ		vowel		U+00E6: LATIN SMALL LETTER AE	
 e			e	e		vowel		U+0065: LATIN SMALL LETTER E	
 ə			e	ə		vowel		U+0259: LATIN SMALL LETTER SCHWA	
 ǝ			e	ǝ		vowel		U+01DD: LATIN SMALL LETTER TURNED E	
@@ -126,16 +136,20 @@ i			i	i		vowel		U+0069: LATIN SMALL LETTER I
 ɪ			i	ɪ		vowel		U+026A: LATIN LETTER SMALL CAPITAL I	
 o			o	o		vowel		U+006F: LATIN SMALL LETTER O	
 ø			o	ø		vowel		U+00F8: LATIN SMALL LETTER O WITH STROKE	
+ɵ			o	ɵ		vowel		U+0275: LATIN CAPITAL LETTER BARRED O	
 ɔ			o	ɔ		vowel		U+0254: LATIN SMALL LETTER OPEN O	
 œ			o	œ		vowel		U+0153: LATIN SMALL LIGATURE OE	
+ꞷ			o	ꞷ		vowel		U+A7B7 LATIN SMALL LETTER OMEGA	
 u			u	u		vowel		U+0075: LATIN SMALL LETTER U	
 ʉ			u	ʉ		vowel		U+0289: LATIN SMALL LETTER U BAR	
+ɥ			u	ɥ		vowel		U+02AF: LATIN CAPITAL LETTER TURNED H	
+ɯ			u	ɯ		vowel		U+026F: LATIN SMALL LETTER TURNED M	
 ʊ			u	ʊ		vowel		U+028A: LATIN SMALL LETTER UPSILON	
-ꞷ			w	ꞷ		vowel		U+A7B7 LATIN SMALL LETTER OMEGA	
 									
 A			A	A		uppercase vowel		U+0041: LATIN CAPITAL LETTER A	
 Ɑ			A	Ɑ		uppercase vowel		U+2C6D: LATIN CAPITAL LETTER ALPHA	
 Ʌ			A	Ʌ		uppercase vowel		U+0245: LATIN CAPITAL LETTER TURNED V	
+Æ			A	Æ		uppercase vowel		U+00C6: LATIN CAPITAL LETTER AE	
 E			E	E		uppercase vowel		U+0045: LATIN CAPITAL LETTER E	
 Ə			E	Ə		uppercase vowel		U+018F: LATIN CAPITAL LETTER SCHWA	
 Ǝ			E	Ǝ		uppercase vowel		U+018E: LATIN CAPITAL LETTER REVERSED E	
@@ -147,12 +161,15 @@ I			I	I		uppercase vowel		U+0049: LATIN CAPITAL LETTER I
 Ɪ			I	Ɪ		uppercase vowel		U+A7AE: LATIN CAPITAL LETTER SMALL CAPITAL I	
 O			O	O		uppercase vowel		U+004F: LATIN CAPITAL LETTER O	
 Ø			O	Ø		uppercase vowel		U+00D8: LATIN CAPITAL LETTER O WITH STROKE	
+Ɵ			O	Ɵ		uppercase vowel		U+019F: LATIN CAPITAL LETTER O WITH MIDDLE TILDE	
 Ɔ			O	Ɔ		uppercase vowel		U+0186: LATIN CAPITAL LETTER OPEN O	
 Œ			O	Œ		uppercase vowel		U+0152: LATIN CAPITAL LIGATURE OE	
+Ꞷ			O	Ꞷ		uppercase vowel		U+A7B7 LATIN CAPITAL LETTER OMEGA	
 U			U	U		uppercase vowel		U+0055: LATIN CAPITAL LETTER U	
 Ʉ			U	Ʉ		uppercase vowel		U+0244: LATIN CAPITAL LETTER U BAR	
+Ɥ			U	Ɥ		uppercase vowel		U+A78D: LATIN CAPITAL LETTER TURNED H	
+Ɯ			U	Ɯ		uppercase vowel		U+019C: LATIN CAPITAL LETTER TURNED M	
 Ʊ			U	Ʊ		uppercase vowel		U+01B1: LATIN CAPITAL LETTER UPSILON	
-Ꞷ			W	Ꞷ		uppercase vowel		U+A7B7 LATIN CAPITAL LETTER OMEGA	
 									
 									
 									
