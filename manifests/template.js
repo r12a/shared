b364dd42d64bcafb973bd3aaca5f56ca45bbcefe
r12a <ishida@w3.org>
2023-01-01 16:04:10 +0000

    template update (this file should be deleted soon)

--- manifests/template.js
+++ manifests/template.js
@@ -1,24 +1,24 @@
 
-langTag='th'
-scriptTag='thai'
-notesLangtag='th'
+langTag='th' 					// used by functions(initialiseSummary, makeFontChanger)
+scriptTag='thai'				// used by functions(makeFontChanger); prompts
+notesLangtag='th'				// used by show_codepoints
 
-explainerDir='thai/'
-blockDir='thai/'
-pickerDir='thai/'
-scriptSummaryTableName='th'
-charUsageBCP='th'
-orthogName = 'Thai'
-indic = true
+explainerDir='thai/'			// used by block-functions; functions
+blockDir='thai/'				// block; show_codepoints; block-functions; egcode; functions
+pickerDir='thai/'				// show_codepoints; block-functions; prompts; functions
+scriptSummaryTableName='th'		// functions(initialiseSummary)
+charUsageBCP='th'				// prompts
+orthogName = 'Thai'				// prompts
+indic = true					
 
-pageWebfonts = ["Noto Serif Thai WF"]
-defaultSize = '24px'
+pageWebfonts = ["Noto Serif Thai WF"]	// functions(makeFontChanger)
+defaultSize = '24px'			// block, manageFonts, block-functions, functions
 
 
 
 // block settings
 
-languageName = 'Thai'
-letterClassName = 'th'
-lang = 'th'
-direction = ''
+languageName = 'Thai'			// block, show_codepoints, block-functions, prompts
+letterClassName = 'th'			// block
+lang = 'th'						
+direction = ''					
