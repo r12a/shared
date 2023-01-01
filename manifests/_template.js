21c337f3d35df93a596a88b59f269694c79e6284
r12a <ishida@w3.org>
2023-01-01 16:02:26 +0000

    template update (manifest)

---
+++ manifests/_template.js
@@ -0,0 +1,24 @@
+
+langTag='th' 					// used by functions(initialiseSummary)
+scriptTag='thai'				// used by functions; prompts
+notesLangtag='th'				// used by show_codepoints
+
+explainerDir='thai/'			// used by block-functions; functions (doHeadersFooters)
+blockDir='thai/th'				// block; show_codepoints; block-functions; egcode; functions
+pickerDir='thai/'				// show_codepoints; block-functions; prompts; functions
+scriptSummaryTableName='th'		// functions(initialiseSummary)
+charUsageBCP='th'				// prompts
+orthogName = 'Thai'				// prompts
+indic = true					
+
+pageWebfonts = ["Noto Serif Thai WF"]	// functions(makeFontChanger)
+defaultSize = '24px'			// block, manageFonts, block-functions, functions
+
+
+
+// block settings
+
+languageName = 'Thai'			// block, show_codepoints, block-functions, prompts
+letterClassName = 'th'			// block
+lang = 'th'						
+direction = ''					// used in showNameDetails to set direction
