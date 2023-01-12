fe8417668ecf626ef720646bca0868330d42612c
r12a <ishida@w3.org>
2023-01-12 11:10:00 +0000

    tavt db update

--- scriptdb/tavt.js
+++ scriptdb/tavt.js
@@ -1,7 +1,7 @@
 var spreadsheet = `tavt-blt	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	ltone	htone	native name	latin name	ucs name	block
 ꫝ	[			–			Lm		repetition mark				sam		U+AADD: TAI VIET SYMBOL SAM	✓
-꫞	[			–		,	Po		poetic head mark				ho hoi		U+AADE: TAI VIET SYMBOL HO HOI	✓
-꫟	[			–		.	Po		poetic end mark				koi koi		U+AADF: TAI VIET SYMBOL KOI KOI	✓
+꫞	[			–		,	Po	i	poetic head mark				ho hoi		U+AADE: TAI VIET SYMBOL HO HOI	✓
+꫟	[			–		.	Po	i	poetic end mark				koi koi		U+AADF: TAI VIET SYMBOL KOI KOI	✓
 ꪯ	}	ʔ ɔ		ʔ	ʔ		Lo		high tone consonant		ꪮ				U+AAAF: TAI VIET LETTER HIGH O	✓
 ꪮ	}	ʔ ɔ		ʔ̱	ʔ̱		Lo		low tone consonant			ꪯ			U+AAAE: TAI VIET LETTER LOW O	✓
 ꪿	^			¹	¹	r	Mn		tone mark				mai ek		U+AABF: TAI VIET TONE MAI EK	✓
@@ -17,19 +17,20 @@ var spreadsheet = `tavt-blt	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 ꪚꪾ	a	ap		-aᵖ	aᵖ										U+002D U+AA9A U+AABE: TAI VIET LETTER LOW BO, VOWEL AM	
 ꪛ	b	b		b	b	B	Lo		high tone consonant		ꪚ				U+AA9B: TAI VIET LETTER HIGH BO	✓
 ꪚ	b	b -p		ḇ	ḇ	b	Lo		low tone consonant			ꪛ			U+AA9A: TAI VIET LETTER LOW BO	✓
-ꪋ	c	ʨ		c	c	C	Lo		high tone consonant		ꪊ				U+AA8B: TAI VIET LETTER HIGH CO	✓
-ꪊ	c	ʨ		c̱	c̱	c	Lo		low tone consonant			ꪋ			U+AA8A: TAI VIET LETTER LOW CO	✓
-ꪍ	c	ʨʰ		cʰ	cʰ	%	Lo		high tone consonant		ꪌ				U+AA8D: TAI VIET LETTER HIGH CHO	✓
-ꪌ	c	ʨʰ		c̱ʰ	c̱ʰ	5	Lo		low tone consonant			ꪍ			U+AA8C: TAI VIET LETTER LOW CHO	✓
+ꪋ	c	t͡ɕ		c	c	C	Lo		high tone consonant		ꪊ				U+AA8B: TAI VIET LETTER HIGH CO	✓
+ꪊ	c	t͡ɕ		c̱	c̱	c	Lo		low tone consonant			ꪋ			U+AA8A: TAI VIET LETTER LOW CO	✓
+ꪍ	c	t͡ɕʰ		cʰ	cʰ	%	Lo	u	high tone consonant	for Tai Don	ꪌ				U+AA8D: TAI VIET LETTER HIGH CHO	✓
+ꪌ	c	t͡ɕʰ		c̱ʰ	c̱ʰ	5	Lo	u	low tone consonant	for Tai Don		ꪍ			U+AA8C: TAI VIET LETTER LOW CHO	✓
 ꪓ	d	d		d	d	D	Lo		high tone consonant		ꪒ				U+AA93: TAI VIET LETTER HIGH DO	✓
 ꪒ	d	d -t		ḏ	ḏ	d	Lo		low tone consonant			ꪓ			U+AA92: TAI VIET LETTER LOW DO	✓
 ꪻ	e	əw		əʷ	əʷ	Y	Lo		prescript vowel						U+AABB: TAI VIET VOWEL AUE	✓
 ꪵ	e	ɛ		ɛ	ɛ	E	Lo		prescript vowel						U+AAB5: TAI VIET VOWEL E	✓
 ꪵꪫ	e	ʷɛ		ɛ-w	ɛ										U+AAB5 U+AAAB: TAI VIET VOWEL E, LETTER HIGH VO	
+ꪵꪫꪥ	e	ʷɛ		ɛ-wj	ɛ										U+AAB5 U+AAAB U+AAA5: TAI VIET VOWEL E, LETTER HIGH VO, LETTER HIGH YO	
 ꪡ	f	f		f	f	F	Lo		high tone consonant		ꪠ				U+AAA1: TAI VIET LETTER HIGH FO	✓
 ꪠ	f	f		f̱	f̱	f	Lo		low tone consonant			ꪡ			U+AAA0: TAI VIET LETTER LOW FO	✓
-ꪆ	g	g		g	g	$	Lo		high tone consonant		ꪇ				U+AA86: TAI VIET LETTER LOW GO	✓
-ꪇ	g	gʰ		ḡ	ḡ	4	Lo		low tone consonant			ꪆ			U+AA87: TAI VIET LETTER HIGH GO	✓
+ꪇ	g	g		ḡ	ḡ	4	Lo		high tone consonant		ꪆ				U+AA87: TAI VIET LETTER HIGH GO	✓
+ꪆ	g	g		g	g	$	Lo		low tone consonant			ꪇ			U+AA86: TAI VIET LETTER LOW GO	✓
 ꪭ	h	h		h	h	H	Lo		high tone consonant		ꪬ				U+AAAD: TAI VIET LETTER HIGH HO	✓
 ꪬ	h	h		ẖ	ẖ	h	Lo		low tone consonant			ꪭ			U+AAAC: TAI VIET LETTER LOW HO	✓
 ꪹ	i	ɨə		ɨᵊ	ɨ ə ɨᵊ	e	Lo		prescript vowel						U+AAB9: TAI VIET VOWEL UEA	✓
@@ -41,26 +42,26 @@ var spreadsheet = `tavt-blt	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 ꪹꪷ	i	ə		ɨᵊ-ɔ	ə										U+AAB9 U+AAB7: TAI VIET VOWEL UEA, MAI KHIT	
 ꪁ	k	k		k	k	K	Lo		high tone consonant		ꪀ				U+AA81: TAI VIET LETTER HIGH KO	✓
 ꪀ	k	k		ḵ	ḵ	k	Lo		low tone consonant			ꪁ			U+AA80: TAI VIET LETTER LOW KO	✓
-ꪃ	k	kʰ		kʰ	kʰ	#	Lo		high tone consonant		ꪂ				U+AA83: TAI VIET LETTER HIGH KHO	✓
-ꪂ	k	kʰ		ḵʰ	ḵʰ	3	Lo		low tone consonant			ꪃ			U+AA82: TAI VIET LETTER LOW KHO	✓
+ꪃ	k	kʰ		kʰ	kʰ	#	Lo	u	high tone consonant	for Tai Don	ꪂ				U+AA83: TAI VIET LETTER HIGH KHO	✓
+ꪂ	k	kʰ		ḵʰ	ḵʰ	3	Lo	u	low tone consonant	for Tai Don		ꪃ			U+AA82: TAI VIET LETTER LOW KHO	✓
 ꫛ	k	kon⁴		kⁿ	kⁿ	}	Lo		logograph						U+AADB: TAI VIET SYMBOL KON	✓
 ꪩ	l	l		l	l	L	Lo		high tone consonant		ꪨ				U+AAA9: TAI VIET LETTER HIGH LO	✓
 ꪨ	l	l		ḻ	ḻ	l	Lo		low tone consonant			ꪩ			U+AAA8: TAI VIET LETTER LOW LO	✓
 ꪣ	m	m		m	m	M	Lo		high tone consonant		ꪢ				U+AAA3: TAI VIET LETTER HIGH MO	✓
 ꪢ	m	m		m̱	m̱	m	Lo		low tone consonant			ꪣ			U+AAA2: TAI VIET LETTER LOW MO	✓
-ꪉ	n	ŋ		ŋ	ŋ	G	Lo		high tone consonant		ꪈ				U+AA89: TAI VIET LETTER HIGH NGO	✓
-ꪈ	n	ŋ		ŋ̄	ŋ̄	g	Lo		low tone consonant			ꪉ			U+AA88: TAI VIET LETTER LOW NGO	✓
-ꪑ	n	ʃ ɲ		ɲ	ɲ	W	Lo		high tone consonant		ꪐ				U+AA91: TAI VIET LETTER HIGH NYO	✓
-ꪐ	n	ʃ ɲ		ɲ̄	ɲ̄	w	Lo		low tone consonant			ꪑ			U+AA90: TAI VIET LETTER LOW NYO	✓
 ꪙ	n	n		n	n	N	Lo		high tone consonant		ꪘ				U+AA99: TAI VIET LETTER HIGH NO	✓
 ꪘ	n	n		ṉ	ṉ	n	Lo		low tone consonant			ꪙ			U+AA98: TAI VIET LETTER LOW NO	✓
+ꪉ	n	ŋ		ŋ	ŋ	G	Lo		high tone consonant		ꪈ				U+AA89: TAI VIET LETTER HIGH NGO	✓
+ꪈ	n	ŋ		ŋ̄	ŋ̄	g	Lo		low tone consonant			ꪉ			U+AA88: TAI VIET LETTER LOW NGO	✓
+ꪑ	n	ɲ		ɲ	ɲ	W	Lo		high tone consonant		ꪐ				U+AA91: TAI VIET LETTER HIGH NYO	✓
+ꪐ	n	ɲ		ɲ̄	ɲ̄	w	Lo		low tone consonant			ꪑ			U+AA90: TAI VIET LETTER LOW NYO	✓
 ꫜ	n	nɨŋ⁵		nᵑ	nᵑ		Lo		logograph						U+AADC: TAI VIET SYMBOL NUENG	✓
 ꪶ	o	o		o	o	o	Lo		prescript vowel						U+AAB6: TAI VIET VOWEL O	✓
 ꪷ	o	ɔ		ɔ	ɔ	[	Mn		dependent vowel						U+AAB7: TAI VIET MAI KHIT	✓
 ꪝ	p	p		p	p	P	Lo		high tone consonant		ꪜ				U+AA9D: TAI VIET LETTER HIGH PO	✓
 ꪜ	p	p		p̄	p̄	p	Lo		low tone consonant			ꪝ			U+AA9C: TAI VIET LETTER LOW PO	✓
-ꪟ	p	pʰ		pʰ	pʰ	^	Lo		high tone consonant		ꪞ				U+AA9F: TAI VIET LETTER HIGH PHO	✓
-ꪞ	p	pʰ		p̄ʰ	p̄ʰ	6	Lo		low tone consonant			ꪟ			U+AA9E: TAI VIET LETTER LOW PHO	✓
+ꪟ	p	pʰ		pʰ	pʰ	^	Lo	u	high tone consonant	for Tai Don	ꪞ				U+AA9F: TAI VIET LETTER HIGH PHO	✓
+ꪞ	p	pʰ		p̄ʰ	p̄ʰ	6	Lo	u	low tone consonant	for Tai Don		ꪟ			U+AA9E: TAI VIET LETTER LOW PHO	✓
 ꪧ	r	r		r	r	&	Lo		high tone consonant		ꪦ				U+AAA7: TAI VIET LETTER HIGH RO	✓
 ꪦ	r	r		ṟ	ṟ	7	Lo		low tone consonant			ꪧ			U+AAA6: TAI VIET LETTER LOW RO	✓
 ꪏ	s	s		s	s	S	Lo		high tone consonant		ꪎ				U+AA8F: TAI VIET LETTER HIGH SO	✓
@@ -78,6 +79,14 @@ var spreadsheet = `tavt-blt	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 ꪥ	y	j		y	y	J	Lo		high tone consonant		ꪤ				U+AAA5: TAI VIET LETTER HIGH YO	✓
 ꪤ	y	j		ȳ	ȳ	j	Lo		low tone consonant			ꪥ			U+AAA4: TAI VIET LETTER LOW YO	✓
 																
+.	.			.	.		Po		full stop						U+002E: FULL STOP	
+,	,			,	,		Po		comma						U+002C: COMMA	
+;	;			;	;	;;⁏⸵	Po		semicolon						U+003B: SEMICOLON	
+!	!			!	!		Po		exclamation mark						U+0021: EXCLAMATION MARK	
+?	?			?	?		Po		question mark						U+003F: QUESTION MARK	
+(	(			(	(		Ps		parenthesis						U+0028: LEFT PARENTHESIS	
+)	)			)	)		Ps		parenthesis						U+0029: RIGHT PARENTHESIS	
+–	-			–	–		Pd		en dash						U+2013: EN DASH	
 `																
 																
 latinPanel = '¹ ² ¹̱ ²̱ ³ ⁴ aʲ aː ā aᵐ aⁿ aːw aʷ aᵖ ḇ c̱ cʰ c̱ʰ ḏ əʷ ɛ f̱ gʰ ḡ ẖ ɨᵊ ɨ iᵊ ḵ kʰ ḵʰ kon⁴ ḻ m̱ ŋ ŋ̄ ɲ ɲ̄ ṉ nɨŋ⁵ ɔ p̄ pʰ p̄ʰ ṟ ʃ s̱ ʨ ʨʰ ṯ tʰ ṯʰ uᵊ w̱ ʷɛ x̱ ȳ ʔ ʔ̱ ˨ ˦˥ ˦ ˥'																
