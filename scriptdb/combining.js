7fe5ff07e44b7d51a76c941dbdc80324d14d6f90
r12a <ishida@w3.org>
2023-01-27 08:13:56 +0000

    +combining

---
+++ scriptdb/combining.js
@@ -0,0 +1,147 @@
+var spreadsheet = `combining	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	ucs name	block
+̀							Mn​		Ordinary diacritics				U+0300: COMBINING GRAVE ACCENT	
+́							Mn​		Ordinary diacritics				U+0301: COMBINING ACUTE ACCENT	
+̂							Mn​		Ordinary diacritics				U+0302: COMBINING CIRCUMFLEX ACCENT	
+̃							Mn​		Ordinary diacritics				U+0303: COMBINING TILDE	
+̄							Mn​		Ordinary diacritics				U+0304: COMBINING MACRON	
+̅							Mn​		Ordinary diacritics				U+0305: COMBINING OVERLINE	
+̆							Mn​		Ordinary diacritics				U+0306: COMBINING BREVE	
+̇							Mn​		Ordinary diacritics				U+0307: COMBINING DOT ABOVE	
+̈							Mn​		Ordinary diacritics				U+0308: COMBINING DIAERESIS	
+̉							Mn​		Ordinary diacritics				U+0309: COMBINING HOOK ABOVE	
+̊							Mn​		Ordinary diacritics				U+030A: COMBINING RING ABOVE	
+̋							Mn​		Ordinary diacritics				U+030B: COMBINING DOUBLE ACUTE ACCENT	
+̌							Mn​		Ordinary diacritics				U+030C: COMBINING CARON	
+̍							Mn​		Ordinary diacritics				U+030D: COMBINING VERTICAL LINE ABOVE	
+̎							Mn​		Ordinary diacritics				U+030E: COMBINING DOUBLE VERTICAL LINE ABOVE	
+̏							Mn​		Ordinary diacritics				U+030F: COMBINING DOUBLE GRAVE ACCENT	
+̐							Mn​		Ordinary diacritics				U+0310: COMBINING CANDRABINDU	
+̑							Mn​		Ordinary diacritics				U+0311: COMBINING INVERTED BREVE	
+̒							Mn​		Ordinary diacritics				U+0312: COMBINING TURNED COMMA ABOVE	
+̓							Mn​		Ordinary diacritics				U+0313: COMBINING COMMA ABOVE	
+̔							Mn​		Ordinary diacritics				U+0314: COMBINING REVERSED COMMA ABOVE	
+̕							Mn​		Ordinary diacritics				U+0315: COMBINING COMMA ABOVE RIGHT	
+̖							Mn​		Ordinary diacritics				U+0316: COMBINING GRAVE ACCENT BELOW	
+̗							Mn​		Ordinary diacritics				U+0317: COMBINING ACUTE ACCENT BELOW	
+̘							Mn​		Ordinary diacritics				U+0318: COMBINING LEFT TACK BELOW	
+̙							Mn​		Ordinary diacritics				U+0319: COMBINING RIGHT TACK BELOW	
+̚							Mn​		Ordinary diacritics				U+031A: COMBINING LEFT ANGLE ABOVE	
+̛							Mn​		Ordinary diacritics				U+031B: COMBINING HORN	
+̜							Mn​		Ordinary diacritics				U+031C: COMBINING LEFT HALF RING BELOW	
+̝							Mn​		Ordinary diacritics				U+031D: COMBINING UP TACK BELOW	
+̞							Mn​		Ordinary diacritics				U+031E: COMBINING DOWN TACK BELOW	
+̟							Mn​		Ordinary diacritics				U+031F: COMBINING PLUS SIGN BELOW	
+̠							Mn​		Ordinary diacritics				U+0320: COMBINING MINUS SIGN BELOW	
+̡							Mn​		Ordinary diacritics				U+0321: COMBINING PALATALIZED HOOK BELOW	
+̢							Mn​		Ordinary diacritics				U+0322: COMBINING RETROFLEX HOOK BELOW	
+̣							Mn​		Ordinary diacritics				U+0323: COMBINING DOT BELOW	
+̤							Mn​		Ordinary diacritics				U+0324: COMBINING DIAERESIS BELOW	
+̥							Mn​		Ordinary diacritics				U+0325: COMBINING RING BELOW	
+̦							Mn​		Ordinary diacritics				U+0326: COMBINING COMMA BELOW	
+̧							Mn​		Ordinary diacritics				U+0327: COMBINING CEDILLA	
+̨							Mn​		Ordinary diacritics				U+0328: COMBINING OGONEK	
+̩							Mn​		Ordinary diacritics				U+0329: COMBINING VERTICAL LINE BELOW	
+̪							Mn​		Ordinary diacritics				U+032A: COMBINING BRIDGE BELOW	
+̫							Mn​		Ordinary diacritics				U+032B: COMBINING INVERTED DOUBLE ARCH BELOW	
+̬							Mn​		Ordinary diacritics				U+032C: COMBINING CARON BELOW	
+̭							Mn​		Ordinary diacritics				U+032D: COMBINING CIRCUMFLEX ACCENT BELOW	
+̮							Mn​		Ordinary diacritics				U+032E: COMBINING BREVE BELOW	
+̯							Mn​		Ordinary diacritics				U+032F: COMBINING INVERTED BREVE BELOW	
+̰							Mn​		Ordinary diacritics				U+0330: COMBINING TILDE BELOW	
+̱							Mn​		Ordinary diacritics				U+0331: COMBINING MACRON BELOW	
+̲							Mn​		Ordinary diacritics				U+0332: COMBINING LOW LINE	
+̳							Mn​		Ordinary diacritics				U+0333: COMBINING DOUBLE LOW LINE	
+̴							Mn​		Overstruck diacritics				U+0334: COMBINING TILDE OVERLAY	
+̵							Mn​		Overstruck diacritics				U+0335: COMBINING SHORT STROKE OVERLAY	
+̶							Mn​		Overstruck diacritics				U+0336: COMBINING LONG STROKE OVERLAY	
+̷							Mn​		Overstruck diacritics				U+0337: COMBINING SHORT SOLIDUS OVERLAY	
+̸							Mn​		Overstruck diacritics				U+0338: COMBINING LONG SOLIDUS OVERLAY	
+̹							Mn​		Miscellaneous additions				U+0339: COMBINING RIGHT HALF RING BELOW	
+̺							Mn​		Miscellaneous additions				U+033A: COMBINING INVERTED BRIDGE BELOW	
+̻							Mn​		Miscellaneous additions				U+033B: COMBINING SQUARE BELOW	
+̼							Mn​		Miscellaneous additions				U+033C: COMBINING SEAGULL BELOW	
+̽							Mn​		Miscellaneous additions				U+033D: COMBINING X ABOVE	
+̾							Mn​		Miscellaneous additions				U+033E: COMBINING VERTICAL TILDE	
+̿							Mn​		Miscellaneous additions				U+033F: COMBINING DOUBLE OVERLINE	
+̀							Mn​		Vietnamese tone marks				U+0300: COMBINING GRAVE ACCENT	
+́							Mn​		Vietnamese tone marks				U+0301: COMBINING ACUTE ACCENT	
+͂							Mn​		Additions for Greek				U+0342: COMBINING GREEK PERISPOMENI	
+̓							Mn​		Additions for Greek				U+0313: COMBINING COMMA ABOVE	
+\u0344							Mn​	d	Additions for Greek				U+0344: COMBINING GREEK DIALYTIKA TONOS	
+ͅ							Mn​		Additions for Greek				U+0345: COMBINING GREEK YPOGEGRAMMENI	
+͆							Mn​		Additions for IPA				U+0346: COMBINING BRIDGE ABOVE	
+͇							Mn​		Additions for IPA				U+0347: COMBINING EQUALS SIGN BELOW	
+͈							Mn​		Additions for IPA				U+0348: COMBINING DOUBLE VERTICAL LINE BELOW	
+͉							Mn​		Additions for IPA				U+0349: COMBINING LEFT ANGLE BELOW	
+͊							Mn​		Additions for IPA				U+034A: COMBINING NOT TILDE ABOVE	
+͋							Mn​		IPA diacritics for disordered speech				U+034B: COMBINING HOMOTHETIC ABOVE	
+͌							Mn​		IPA diacritics for disordered speech				U+034C: COMBINING ALMOST EQUAL TO ABOVE	
+͍							Mn​		IPA diacritics for disordered speech				U+034D: COMBINING LEFT RIGHT ARROW BELOW	
+͎							Mn​		IPA diacritics for disordered speech				U+034E: COMBINING UPWARDS ARROW BELOW	
+\u034F							Mn​		Grapheme joiner				U+034F: COMBINING GRAPHEME JOINER	
+͐							Mn​		Additions for the Uralic Phonetic Alphabet				U+0350: COMBINING RIGHT ARROWHEAD ABOVE	
+͑							Mn​		Additions for the Uralic Phonetic Alphabet				U+0351: COMBINING LEFT HALF RING ABOVE	
+͒							Mn​		Additions for the Uralic Phonetic Alphabet				U+0352: COMBINING FERMATA	
+͓							Mn​		Additions for the Uralic Phonetic Alphabet				U+0353: COMBINING X BELOW	
+͔							Mn​		Additions for the Uralic Phonetic Alphabet				U+0354: COMBINING LEFT ARROWHEAD BELOW	
+͕							Mn​		Additions for the Uralic Phonetic Alphabet				U+0355: COMBINING RIGHT ARROWHEAD BELOW	
+͖							Mn​		Additions for the Uralic Phonetic Alphabet				U+0356: COMBINING RIGHT ARROWHEAD AND UP ARROWHEAD BELOW	
+͗							Mn​		Additions for the Uralic Phonetic Alphabet				U+0357: COMBINING RIGHT HALF RING ABOVE	
+͘							Mn​		Miscellaneous additions				U+0358: COMBINING DOT ABOVE RIGHT	
+͙							Mn​		Miscellaneous additions				U+0359: COMBINING ASTERISK BELOW	
+͚							Mn​		Miscellaneous additions				U+035A: COMBINING DOUBLE RING BELOW	
+͛							Mn​		Miscellaneous additions				U+035B: COMBINING ZIGZAG ABOVE	
+͜							Mn​		Double diacritics				U+035C: COMBINING DOUBLE BREVE BELOW	
+͝							Mn​		Double diacritics				U+035D: COMBINING DOUBLE BREVE	
+͞							Mn​		Double diacritics				U+035E: COMBINING DOUBLE MACRON	
+͟							Mn​		Double diacritics				U+035F: COMBINING DOUBLE MACRON BELOW	
+͠							Mn​		Double diacritics				U+0360: COMBINING DOUBLE TILDE	
+͡							Mn​		Double diacritics				U+0361: COMBINING DOUBLE INVERTED BREVE	
+͢							Mn​		Double diacritics				U+0362: COMBINING DOUBLE RIGHTWARDS ARROW BELOW	
+ͣ							Mn​		Medieval superscript letter diacritics				U+0363: COMBINING LATIN SMALL LETTER A	
+ͤ							Mn​		Medieval superscript letter diacritics				U+0364: COMBINING LATIN SMALL LETTER E	
+ͥ							Mn​		Medieval superscript letter diacritics				U+0365: COMBINING LATIN SMALL LETTER I	
+ͦ							Mn​		Medieval superscript letter diacritics				U+0366: COMBINING LATIN SMALL LETTER O	
+ͧ							Mn​		Medieval superscript letter diacritics				U+0367: COMBINING LATIN SMALL LETTER U	
+ͨ							Mn​		Medieval superscript letter diacritics				U+0368: COMBINING LATIN SMALL LETTER C	
+ͩ							Mn​		Medieval superscript letter diacritics				U+0369: COMBINING LATIN SMALL LETTER D	
+ͪ							Mn​		Medieval superscript letter diacritics				U+036A: COMBINING LATIN SMALL LETTER H	
+ͫ							Mn​		Medieval superscript letter diacritics				U+036B: COMBINING LATIN SMALL LETTER M	
+ͬ							Mn​		Medieval superscript letter diacritics				U+036C: COMBINING LATIN SMALL LETTER R	
+ͭ							Mn​		Medieval superscript letter diacritics				U+036D: COMBINING LATIN SMALL LETTER T	
+ͮ							Mn​		Medieval superscript letter diacritics				U+036E: COMBINING LATIN SMALL LETTER V	
+ͯ							Mn​		Medieval superscript letter diacritics				U+036F: COMBINING LATIN SMALL LETTER X	
+`														
+														
+latinPanel = 'ä bʷ č'														
+														
+var cols = {														
+key: 1,														
+ipaLoc: 2,														
+ipaPlus: 3,														
+transLoc: 4,														
+transcription: 5,														
+kbd: 6,														
+class: 7,														
+status: 8,														
+typeLoc: 9,														
+statusLoc: 10,														
+														
+dvowel: 0,														
+ivowel: 0,														
+subj:0,														
+ltone:0,														
+htone:0,														
+lc:0,														
+uc:0,														
+meaning:0,														
+shape: 0,														
+numLoc: 0,														
+														
+nnameLoc: 11,														
+nameLoc: 12,														
+ucsName: 13,														
+block: 14,														
+														
+//othertranscriptions: [[5, 'Latin']]														
+}														
