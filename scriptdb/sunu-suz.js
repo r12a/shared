8c0ca7bbeae9a54a96e752e1ed0e4ebf32c588b1
r12a <ishida@w3.org>
2023-03-05 05:56:27 +0000

    Initial scriptdb and webfont for Sunuwar

---
+++ scriptdb/sunu-suz.js
@@ -0,0 +1,108 @@
+var spreadsheet = `sunu-suz	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	deva	ucs name	block
+-	-			-	-				hyphen					U+2010: HYPHEN	
+–	-			–	–		Pd		en dash					U+2013: EN DASH	
+—	-			—	—		Pd		em dash					U+2014: EM DASH	
+\u{0331}	*			˟			Lo	o	vowel silencer			sangmilu		U+0331 COMBINING MACRON BELOW	
+\u{003A}	:			ː			Lo	o	vowel lengthener			laissi		U+003A COLON	
+,	,			,̣	,		Po		comma					U+002C: COMMA	
+;	;			;̣	;		Po		semicolon					U+003B: SEMICOLON	
+:	:			:	:		Po		colon					U+003A: COLON	
+!	!			!	!		Po		exclamation mark					U+0021: EXCLAMATION MARK	
+?	?			?	?		Po		question mark					U+003F: QUESTION MARK	
+.	.			.	.		Po		full stop					U+002E: FULL STOP	
+…	.			…	…		Po		ellipsis					U+2026: HORIZONTAL ELLIPSIS	
+(	(			(	(		Ps		parenthesis					U+0028: LEFT PARENTHESIS	
+[	(			[	[		Ps		bracket					U+005B: LEFT SQUARE BRACKET	
+)	)			)	)		Pe		parenthesis					U+0029: RIGHT PARENTHESIS	
+]	)			]	]		Pe		bracket					U+005D: RIGHT SQUARE BRACKET	
+%	%			%			Po		percentage mark					U+0025: PERCENT SIGN	
+‰	%			‰			Po		per mille mark					U+2030: PER MILLE SIGN	
+“	<			“	“		Pi		quotation mark					U+201C: LEFT DOUBLE QUOTATION MARK	
+\‘	<			\‘	\‘		Pi		quotation mark					U+2018: LEFT SINGLE QUOTATION MARK	
+”	>			”	”		Pf		quotation mark					U+201D: RIGHT DOUBLE QUOTATION MARK	
+\’	>			\’	\’		Pf		quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	
+\u{030D}	^			ˈ	ˈ				stress mark			sotte'si		U+030D COMBINING VERTICAL LINE ABOVE	
+\u{0301}	^								high tone			ta renmaresi		U+0301 COMBINING ACUTE ACCENT	
+\u{0300}	^								low tone			di renmaresi		U+0300 COMBINING GRAVE ACCENT	
+\u{1DF5}	[			˙			Mn	o	repha			sangkirs		U+1DF5 COMBINING UP TACK ABOVE	
+\u{032D}	[			¨			Mn	o	sangrums			ra-vattu / trill mark		U+032D COMBINING CIRCUMFLEX ACCENT BELOW	
+\u{0303}	[	̃		˜	̃		Mn		nasalisation			nesante		U+0303 COMBINING TILDE	
+\u{0310}	[	̃		˜̣				o	nasal marker			taslathenk		U+0310 COMBINING CANDRABINDU	
+\u{11BE0}	}	ʔ		ʔ			Lo		glottal stop			kloko	अ ◌	U+11BE0 SUNUWAR LETTER KLOKO	
+\u{11BE1}	B	ƥ		ƥ			Lo		auspicious sign			pvo		U+11BE1 SUNUWAR SIGN PVO	
+\u{11BC8}	a	ə		ə			Lo		vowel			appho	अ	U+11BC8 SUNUWAR LETTER APPHO	
+\u{11BD6}	A	aː		a			Lo		long vowel			aal	आ	U+11BD6 SUNUWAR LETTER AAL	
+\u{11BCE}	b	b		b			Lo		consonant			bur	ब	U+11BCE SUNUWAR LETTER BUR	
+\u{11BD5}	B	ɓ		ɓ			Lo		consonant			ava	ब	U+11BD5 SUNUWAR LETTER AVA	
+\u{11BCC}	c	t͡ʃ		ʧ			Lo		consonant			carmi	च	U+11BCC SUNUWAR LETTER CARMI	
+\u{11BDD}	C	t͡ʃʰ		ʧʰ			Lo		consonant			chelap	छ	U+11BDD SUNUWAR LETTER CHELAP	
+\u{11BC0}	d	d ɖ		d			Lo		consonant			devi	द ड	U+11BC0 SUNUWAR LETTER DEVI	
+\u{11BD7}	D	ɖ		ɖ			Lo		consonant			donga	ड	U+11BD7 SUNUWAR LETTER DONGA	
+\u{11BC2}	e	e		e			Lo		vowel			eko	ए	U+11BC2 SUNUWAR LETTER EKO	
+\u{11BCA}	g	ɡ		g			Lo		consonant			gil	ग	U+11BE0 SUNUWAR LETTER KLOKO	
+\u{11BCB}	h	h		h			Lo		consonant			hamso	ह	U+11BCB SUNUWAR LETTER HAMSO	
+\u{11BC3}	i	i		i			Lo		vowel			imar	इ	U+11BC3 SUNUWAR LETTER IMAR	
+\u{11BCF}	j	d͡ʒ		ʤ			Lo		consonant			jyah	ज	U+11BCF SUNUWAR LETTER JYAH	
+\u{11BC6}	k	k		k			Lo		consonant			kik	क	U+11BC6 SUNUWAR LETTER KIK	
+\u{11BDB}	K	kʰ		kʰ			Lo		consonant			kha	ख	U+11BDB SUNUWAR LETTER KHA	
+\u{11BD0}	l	l ɭ		l			Lo		consonant			loacha	ल	U+11BD0 SUNUWAR LETTER LOACHA	
+\u{11BC7}	m	m		m			Lo		consonant			mama	म	U+11BC7 SUNUWAR LETTER MAMA	
+\u{11BCD}	n	n		n			Lo		consonant			nah	न ण	U+11BCD SUNUWAR LETTER NAH	
+\u{11BDA}	N	ŋ		ŋ			Lo		consonant			ngar	ङ	U+11BDA SUNUWAR LETTER NGAR	
+\u{11BD1}	o	o		o			Lo		vowel			otthi	ओ	U+11BD1 SUNUWAR LETTER OTTHI	
+\u{11BC9}	p	p		p			Lo		consonant			pip	प	U+11BC9 SUNUWAR LETTER PIP	
+\u{11BD9}	P	pʰ		pʰ			Lo		consonant			phar	फ	U+11BD9 SUNUWAR LETTER PHAR	
+\u{11BC4}	r	r ɽ ɭ		r			Lo		consonant			reu	र ड़	U+11BC4 SUNUWAR LETTER REU	
+\u{11BD2}	s	s		s			Lo		consonant			shyele	स	U+11BD2 SUNUWAR LETTER SHYELE	
+\u{11BDC}	S	ʃ		ʃ			Lo		consonant			shyer	श ष	U+11BDC SUNUWAR LETTER SHYER	
+\u{11BC1}	t	t ʈ		t			Lo		consonant			tasla	त ट	U+11BC1 SUNUWAR LETTER TASLA	
+\u{11BD8}	T	tʰ		tʰ			Lo		consonant			thari	थ	U+11BD8 SUNUWAR LETTER THARI	
+\u{11BDE}	t	ʈ		ʈ			Lo		consonant			tentu	ट	U+11BDE SUNUWAR LETTER TENTU	
+\u{11BDF}	T	ʈʰ		ʈʰ			Lo		consonant			thele	ठ	U+11BDF SUNUWAR LETTER THELE	
+\u{11BC5}	u	u		u			Lo		vowel			utthi	उ	U+11BC5 SUNUWAR LETTER UTTHI	
+\u{11BD3}	v	v		v			Lo		consonant			varca	व	U+11BD3 SUNUWAR LETTER VARCA	
+\u{11BD4}	y	j		y			Lo		consonant			yat	य	U+11BD4 SUNUWAR LETTER YAT	
+															
+§				§	§		Po		section sign					U+00A7: SECTION SIGN	
+†				†	†		Po		dagger					U+2020: DAGGER	
+‡				‡	‡		Po		double dagger					U+2021: DOUBLE DAGGER	
+′				′	′		Po		prime					U+2032: PRIME	
+″				″	″		Po		double prime					U+2033: DOUBLE PRIME	
+															
+\u{11BF1}	±	1		1̣	1		Nd		digit			ka	१	U+11BF1 SUNUWAR DIGIT ONE	
+\u{11BF2}	±	2		2̣	2		Nd		digit			ni'ashi	२	U+11BF2 SUNUWAR DIGIT TWO	
+\u{11BF3}	±	3		3̣	3		Nd		digit			san	३	U+11BF3 SUNUWAR DIGIT THREE	
+\u{11BF4}	±	4		4̣	4		Nd		digit			le	४	U+11BF4 SUNUWAR DIGIT FOUR	
+\u{11BF5}	±	5		5̣	5		Nd		digit			nga	५	U+11BF5 SUNUWAR DIGIT FIVE	
+\u{11BF6}	±	6		6̣	6		Nd		digit			raku	६	U+11BF6 SUNUWAR DIGIT SIX	
+\u{11BF7}	±	7		7̣	7		Nd		digit			cani	७	U+11BF7 SUNUWAR DIGIT SEVEN	
+\u{11BF8}	±	8		8̣	8		Nd		digit			sasi	८	U+11BF8 SUNUWAR DIGIT EIGHT	
+\u{11BF9}	±	9		9̣	9		Nd		digit			yan	९	U+11BF9 SUNUWAR DIGIT NINE	
+\u{11BF0}	±	0		0̣	0		Nd		digit			sum	०	U+11BF0 SUNUWAR DIGIT ZERO	
+															
+\u200C	=			ₓ			Cf		zero-width non-joiner					U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=			₊			Cf		zero-width joiner					U+200D: ZERO WIDTH JOINER	
+															
+`															
+															
+latinPanel = 'aː d͡ʒ kʰ pʰ tʰ t͡ʃ t͡ʃʰ ŋ ƥ ɓ ɖ ə ɡ ɭ ɽ ʃ ʈ ʈʰ ʔ ʤ ʧ ʧʰ'															
+															
+var cols = {															
+key: 1,															
+ipaLoc: 2,															
+ipaPlus: 3,															
+transLoc: 4,															
+transcription: 5,															
+kbd: 6,															
+class: 7,															
+status: 8,															
+typeLoc: 9,															
+statusLoc: 10,															
+															
+nnameLoc: 11,															
+nameLoc: 12,															
+ucsName: 14,															
+block: 15,															
+															
+othertranscriptions: [[13, 'Deva']]															
+}															
