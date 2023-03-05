3775692dc604d62c8214d757a913db82276c3175
r12a <ishida@w3.org>
2023-03-05 05:57:05 +0000

    scriptdb/arab-ff

---
+++ scriptdb/arab-ff.js
@@ -0,0 +1,106 @@
+var spreadsheet = `arab-ff	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	ucs name	block
+-	-			-	-		Pd		hyphen				U+2010: HYPHEN	
+‑	-			‑	‑		Pd		non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd		en dash				U+2013: EN DASH	
+—	-			—	—		Pd		em dash				U+2014: EM DASH	
+،	,			,	,		Po		comma				U+060C: ARABIC COMMA	
+؛	;			;			Po		semicolon				U+061B: ARABIC SEMICOLON	
+:	:			:	:		Po		colon				U+003A: COLON	
+!	!			!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK	
+؟	?			?	?		Po		question mark				U+061F: ARABIC QUESTION MARK	
+.	.			.	.		Po		full stop				U+002E: FULL STOP	
+…	.			…	…		Po		ellipsis				U+2026: HORIZONTAL ELLIPSIS	
+(	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS	
+[	(			[	[		Pe		bracket				U+005B: LEFT SQUARE BRACKET	
+)	)			)	)		Ps		parenthesis				U+0029: RIGHT PARENTHESIS	
+]	)			]	]		Pe		bracket				U+005D: RIGHT SQUARE BRACKET	
+%	%			%			Po		percentage mark				U+0025: PERCENT SIGN	
+‰	%			‰			Po		per mille mark				U+2030: PER MILLE SIGN	
+«	<			«			Pi		quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	✓
+‹	<			‹			Pi		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+“	<			“	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
+\‘	<			\‘	\‘		Pi		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+»	>			»			Pf		quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	✓
+›	>			›			Pf		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+”	>			”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
+\’	>			\’	\’		Pf		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+َ	a	a		a			Mn​		vowel				U+064E: ARABIC FATHA	
+َا	A	aː		aa					long vowel				U+064E U+0627: ARABIC FATHA, ARABIC LETTER ALEF	
+ا	a	–		±			Lo		vowel support				U+0627: ARABIC LETTER ALEF	
+ب	b	b		b			Lo		consonant				U+0628: ARABIC LETTER BEH	
+ࢡ	b	ɓ		ɓ			Lo		consonant	Cameroon			U+08A1: ARABIC LETTER BEH WITH HAMZA ABOVE	✓
+ٻ	b	ɓ		ɓ̣			Lo			Nigeria			U+067B: ARABIC LETTER BEEH	
+مب	b	ᵐb		mb					pre-nasalised consonant				U+0645 U+0628: ARABIC LETTER MEEM, ARABIC LETTER BEH	
+ش	c	c t͡ʃ		c			Lo		consonant				U+0634: ARABIC LETTER SHEEN	
+ث	c	c t͡ʃ		c̣			Lo	i	consonant	alternate from Hausa			U+062B: ARABIC LETTER THEH	
+د	d	d		d			Lo		consonant				U+062F: ARABIC LETTER DAL	
+ط	d	ɗ		ɗ			Lo		consonant				U+0637: ARABIC LETTER TAH	✓
+ࢽد	d	ⁿd		nd					pre-nasalised consonant				U+08BD U+062F: ARABIC LETTER AFRICAN NOON, ARABIC LETTER DAL	
+ٜ	e	e		e			Mn​		vowel				U+065C: ARABIC VOWEL SIGN DOT BELOW	
+ٜىٰ	E	eː							long vowel				U+065C U+0649 U+0670: ARABIC VOWEL SIGN DOT BELOW, ARABIC LETTER ALEF MAKSURA, ARABIC LETTER SUPERSCRIPT ALEF	
+ڢ	f	f		f			Lo		consonant				U+06A2: ARABIC LETTER FEH WITH DOT MOVED BELOW	
+ڤ	f	f		f̣			Lo		consonant	Nigeria (hafs)			U+06A4: ARABIC LETTER VEH	
+ɠ		ɠ		ɠ			Ll		consonant	Guinea			U+0260: LATIN SMALL LETTER G WITH HOOK	✓
+Ɠ		ɠ		Ɠ			Lu		consonant	Guinea			U+0193: LATIN CAPITAL LETTER G WITH HOOK	
+غ	g	ɡ		g			Lo		consonant				U+063A: ARABIC LETTER GHAIN	
+ح	h	h		h			Lo		consonant				U+062D: ARABIC LETTER HAH	
+ِ	i	i		i			Mn​		vowel				U+0650: ARABIC KASRA	
+َي	I	iː		iy					long vowel				U+064E U+064A: ARABIC FATHA, ARABIC LETTER YEH	
+ج	j	ɟ d͡ʒ		ʤ			Lo		consonant				U+062C: ARABIC LETTER JEEM	
+نج	j	ᶮɟ ᶮd͡ʒ		nʤ					pre-nasalised consonant				U+0646 U+062C: ARABIC LETTER NOON, ARABIC LETTER JEEM	
+ک	k	k		k			Lo		consonant				U+06A9: ARABIC LETTER KEHEH	
+ڪ	k	k		ḳ			Lo		consonant	Nigeria			U+06AA: ARABIC LETTER SWASH KAF	
+ل	l	l		l			Lo		consonant				U+0644: ARABIC LETTER LAM	
+م	m	m		m			Lo		consonant				U+0645: ARABIC LETTER MEEM	
+ن	n	n		n			Lo		consonant				U+0646: ARABIC LETTER NOON	
+نغ	n	ᵑɡ/ŋ		ng					pre-nasalised consonant	Cameroon; Nigeria only ng			U+0646 U+063A: ARABIC LETTER NOON, ARABIC LETTER GHAIN	
+ݝ	n	ŋ		ŋ			Lo		consonant	Nigeria			U+075D: ARABIC LETTER AIN WITH TWO DOTS ABOVE	✓
+ࢩ	n	ɲ		ɲ			Lo		consonant	Cameroon			U+08A9: ARABIC LETTER YEH WITH TWO DOTS BELOW AND DOT ABOVE	✓
+ݧ	n	ɲ		ɲ̣			Lo		consonant	Nigeria			U+0767: ARABIC LETTER NOON WITH TWO DOTS BELOW	✓
+ٛ	o	o		o			Mn​		vowel				U+065B: ARABIC VOWEL SIGN INVERTED SMALL V ABOVE	
+ٛو	O	oː		ow					long vowel				U+065B U+0648: ARABIC VOWEL SIGN INVERTED SMALL V ABOVE, ARABIC LETTER WAW	
+ݠ	p	p		p			Lo		consonant				U+0760: ARABIC LETTER FEH WITH TWO DOTS BELOW	
+q		q		q			Ll	i	consonant	loan words only (esp. Arabic)			U+0071: LATIN SMALL LETTER Q	
+ر	r	r ɾ		r			Lo		consonant				U+0631: ARABIC LETTER REH	
+س	s	s		s			Lo		consonant				U+0633: ARABIC LETTER SEEN	
+ش	s	ʃ		ʃ			Lo	i	consonant	loan words only (esp. Arabic)			U+0634: ARABIC LETTER SHEEN	
+ت	t	t		t			Lo		consonant				U+062A: ARABIC LETTER TEH	
+ُ	u	u		u			Mn​		vowel				U+064F: ARABIC DAMMA	
+ُو	U	uː		uw					long vowel				U+064F U+0648: ARABIC DAMMA, ARABIC LETTER WAW	
+v		v		v			Ll	i	consonant	loan words only (esp. Arabic)			U+0076: LATIN SMALL LETTER V	
+و	w	w		w			Lo		consonant				U+0648: ARABIC LETTER WAW	
+x		ɣ		ɣ			Ll	i	consonant	loan words only (esp. Arabic)			U+0078: LATIN SMALL LETTER X	
+ي	y	j		y			Lo		consonant				U+064A: ARABIC LETTER YEH	
+ࢨ	y	ʔʲ		ƴ			Lo		consonant	Cameroon			U+08A8: ARABIC LETTER YEH WITH TWO DOTS BELOW AND HAMZA ABOVE	✓
+ۑ	y	ʔʲ		ƴ̣			Lo		consonant	Nigeria			U+06D1: ARABIC LETTER YEH WITH THREE DOTS BELOW	
+z		z		z			Ll	i	consonant	loan words only (esp. Arabic)			U+007A: LATIN SMALL LETTER Z	
+ع	{	ʔ		ʔ			Lo		glottal stop				U+0639: ARABIC LETTER AIN	
+														
+§				§	§		Po		section sign				U+00A7: SECTION SIGN	✓
+†				†	†		Po		dagger				U+2020: DAGGER	
+‡				‡	‡		Po		double dagger				U+2021: DOUBLE DAGGER	
+′				′	′		Po		prime				U+2032: PRIME	
+″				″	″		Po		double prime				U+2033: DOUBLE PRIME	
+`														
+														
+latinPanel = 'd͡ʒ t͡ʃ ñ ƴ ɓ ɗ ɟ ɲ ŋ ʼ ʼy ɡ ᵐb ᵑɡ ᶮɟ ᶮd͡ʒ ⁿd iː uː eː oː aː ɪ ɛ ɔ ʊ ʔ ʔʲ'														
+														
+var cols = {														
+key: 1,														
+ipaLoc: 2,														
+ipaPlus: 3,														
+transLoc: 4,														
+transcription: 5,														
+kbd: 6,														
+class: 7,														
+status: 8,														
+typeLoc: 9,														
+statusLoc: 10,														
+														
+nnameLoc: 11,														
+nameLoc: 12,														
+ucsName: 13,														
+block: 14,														
+														
+//othertranscriptions: [[5, 'LOC']]														
+}														
