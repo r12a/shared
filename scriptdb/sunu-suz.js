4e9827a5b6dd744fe828d10209329694146e4eb9
r12a <ishida@w3.org>
2023-03-08 05:39:36 +0000

    scriptdb/sunu-suz: Add keyboard mappings

--- scriptdb/sunu-suz.js
+++ scriptdb/sunu-suz.js
@@ -21,47 +21,47 @@ var spreadsheet = `sunu-suz	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 \‘	<			\‘	\‘		Pi		quotation mark					U+2018: LEFT SINGLE QUOTATION MARK	
 ”	>			”	”		Pf		quotation mark					U+201D: RIGHT DOUBLE QUOTATION MARK	
 \’	>			\’	\’		Pf		quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	
-\u{030D}	^			ˈ	ˈ		Mn​		stress mark			sotte'si		U+030D COMBINING VERTICAL LINE ABOVE	
-\u{0301}	^			ˊ			Mn​		high tone			ta renmaresi		U+0301 COMBINING ACUTE ACCENT	
-\u{0300}	^			ˋ			Mn​		low tone			di renmaresi		U+0300 COMBINING GRAVE ACCENT	
-\u{0303}	[	̃		˜	̃		Mn​		nasalisation			nesante		U+0303 COMBINING TILDE	
+\u{030D}	^			ˈ	ˈ	Z	Mn​		stress mark			sotte'si		U+030D COMBINING VERTICAL LINE ABOVE	
+\u{0301}	^			ˊ		x	Mn​		high tone			ta renmaresi		U+0301 COMBINING ACUTE ACCENT	
+\u{0300}	^			ˋ		X	Mn​		low tone			di renmaresi		U+0300 COMBINING GRAVE ACCENT	
+\u{0303}	[	̃		˜	̃	z	Mn​		nasalisation			nesante		U+0303 COMBINING TILDE	
 \u{0310}	[	̃		˜̣			Mn​	o	nasal marker			taslathenk		U+0310 COMBINING CANDRABINDU	
 \u{1DF5}	[			˙			Mn	o	repha			sangkirs		U+1DF5 COMBINING UP TACK ABOVE	
 \u{032D}	[			¨			Mn	o	sangrums			ra-vattu / trill mark		U+032D COMBINING CIRCUMFLEX ACCENT BELOW	
-\u{11BE0}	}	ʔ		ʔ			Lo		glottal stop			kloko	अ ◌	U+11BE0 SUNUWAR LETTER KLOKO	
+\u{11BE0}	}	ʔ		ʔ		v	Lo		glottal stop			kloko	अ ◌	U+11BE0 SUNUWAR LETTER KLOKO	
 \u{11BE1}	B	ɓ̥		ƥ			Lo		auspicious sign			pvo		U+11BE1 SUNUWAR SIGN PVO	
-\u{11BC8}	A	ə		ə			Lo		vowel			appho	अ	U+11BC8 SUNUWAR LETTER APPHO	
-\u{11BD6}	a	aː		a			Lo		long vowel			aal	आ	U+11BD6 SUNUWAR LETTER AAL	
-\u{11BCE}	b	b		b			Lo		consonant			bur	ब	U+11BCE SUNUWAR LETTER BUR	
-\u{11BD5}	B	ɓ		ɓ			Lo		consonant			ava	ब	U+11BD5 SUNUWAR LETTER AVA	
-\u{11BCC}	c	t͡ʃ		ʧ			Lo		consonant			carmi	च	U+11BCC SUNUWAR LETTER CARMI	
-\u{11BDD}	C	t͡ʃʰ		ʧʰ			Lo		consonant			chelap	छ	U+11BDD SUNUWAR LETTER CHELAP	
-\u{11BC0}	d	d		d			Lo		consonant			devi	द ड	U+11BC0 SUNUWAR LETTER DEVI	
-\u{11BD7}	D	ɖ		ɖ			Lo		consonant			donga	ड	U+11BD7 SUNUWAR LETTER DONGA	
-\u{11BC2}	e	e		e			Lo		vowel			eko	ए	U+11BC2 SUNUWAR LETTER EKO	
-\u{11BCA}	g	ɡ		g			Lo		consonant			gil	ग	U+11BE0 SUNUWAR LETTER KLOKO	
-\u{11BCB}	h	h		h			Lo		consonant			hamso	ह	U+11BCB SUNUWAR LETTER HAMSO	
-\u{11BC3}	i	i		i			Lo		vowel			imar	इ	U+11BC3 SUNUWAR LETTER IMAR	
-\u{11BCF}	j	d͡ʒ		ʤ			Lo		consonant			jyah	ज	U+11BCF SUNUWAR LETTER JYAH	
-\u{11BC6}	k	k		k			Lo		consonant			kik	क	U+11BC6 SUNUWAR LETTER KIK	
-\u{11BDB}	K	kʰ		kʰ			Lo		consonant			kha	ख	U+11BDB SUNUWAR LETTER KHA	
-\u{11BD0}	l	l ɭ		l			Lo		consonant			loacha	ल	U+11BD0 SUNUWAR LETTER LOACHA	
-\u{11BC7}	m	m		m			Lo		consonant			mama	म	U+11BC7 SUNUWAR LETTER MAMA	
-\u{11BCD}	n	n		n			Lo		consonant			nah	न ण	U+11BCD SUNUWAR LETTER NAH	
-\u{11BDA}	N	ŋ		ŋ			Lo		consonant			ngar	ङ	U+11BDA SUNUWAR LETTER NGAR	
-\u{11BD1}	o	o		o			Lo		vowel			otthi	ओ	U+11BD1 SUNUWAR LETTER OTTHI	
-\u{11BC9}	p	p		p			Lo		consonant			pip	प	U+11BC9 SUNUWAR LETTER PIP	
-\u{11BD9}	P	pʰ		pʰ			Lo		consonant			phar	फ	U+11BD9 SUNUWAR LETTER PHAR	
-\u{11BC4}	r	r ɽ		r			Lo		consonant			reu	र ड़	U+11BC4 SUNUWAR LETTER REU	
-\u{11BD2}	s	s		s			Lo		consonant			shyele	स	U+11BD2 SUNUWAR LETTER SHYELE	
-\u{11BDC}	S	ʃ		ʃ			Lo		consonant			shyer	श ष	U+11BDC SUNUWAR LETTER SHYER	
-\u{11BC1}	t	t		t			Lo		consonant			tasla	त ट	U+11BC1 SUNUWAR LETTER TASLA	
-\u{11BD8}	T	tʰ		tʰ			Lo		consonant			thari	थ	U+11BD8 SUNUWAR LETTER THARI	
-\u{11BDE}	t	ʈ		ʈ			Lo		consonant			tentu	ट	U+11BDE SUNUWAR LETTER TENTU	
-\u{11BDF}	T	ʈʰ		ʈʰ			Lo		consonant			thele	ठ	U+11BDF SUNUWAR LETTER THELE	
-\u{11BC5}	u	u		u			Lo		vowel			utthi	उ	U+11BC5 SUNUWAR LETTER UTTHI	
-\u{11BD3}	w	w v		w			Lo		consonant			varca	व	U+11BD3 SUNUWAR LETTER VARCA	
-\u{11BD4}	y	j		y			Lo		consonant			yat	य	U+11BD4 SUNUWAR LETTER YAT	
+\u{11BC8}	A	ə		ə		a	Lo		vowel			appho	अ	U+11BC8 SUNUWAR LETTER APPHO	
+\u{11BD6}	a	aː		a		A	Lo		long vowel			aal	आ	U+11BD6 SUNUWAR LETTER AAL	
+\u{11BCE}	b	b		b		b	Lo		consonant			bur	ब	U+11BCE SUNUWAR LETTER BUR	
+\u{11BD5}	B	ɓ		ɓ		B	Lo		consonant			ava	ब	U+11BD5 SUNUWAR LETTER AVA	
+\u{11BCC}	c	t͡ʃ		ʧ		c	Lo		consonant			carmi	च	U+11BCC SUNUWAR LETTER CARMI	
+\u{11BDD}	C	t͡ʃʰ		ʧʰ		C	Lo		consonant			chelap	छ	U+11BDD SUNUWAR LETTER CHELAP	
+\u{11BC0}	d	d		d		d	Lo		consonant			devi	द ड	U+11BC0 SUNUWAR LETTER DEVI	
+\u{11BD7}	D	ɖ		ɖ		D	Lo		consonant			donga	ड	U+11BD7 SUNUWAR LETTER DONGA	
+\u{11BC2}	e	e		e		e	Lo		vowel			eko	ए	U+11BC2 SUNUWAR LETTER EKO	
+\u{11BCA}	g	ɡ		g		g	Lo		consonant			gil	ग	U+11BE0 SUNUWAR LETTER KLOKO	
+\u{11BCB}	h	h		h		h	Lo		consonant			hamso	ह	U+11BCB SUNUWAR LETTER HAMSO	
+\u{11BC3}	i	i		i		i	Lo		vowel			imar	इ	U+11BC3 SUNUWAR LETTER IMAR	
+\u{11BCF}	j	d͡ʒ		ʤ		j	Lo		consonant			jyah	ज	U+11BCF SUNUWAR LETTER JYAH	
+\u{11BC6}	k	k		k		k	Lo		consonant			kik	क	U+11BC6 SUNUWAR LETTER KIK	
+\u{11BDB}	K	kʰ		kʰ		K	Lo		consonant			kha	ख	U+11BDB SUNUWAR LETTER KHA	
+\u{11BD0}	l	l ɭ		l		l	Lo		consonant			loacha	ल	U+11BD0 SUNUWAR LETTER LOACHA	
+\u{11BC7}	m	m		m		m	Lo		consonant			mama	म	U+11BC7 SUNUWAR LETTER MAMA	
+\u{11BCD}	n	n		n		n	Lo		consonant			nah	न ण	U+11BCD SUNUWAR LETTER NAH	
+\u{11BDA}	N	ŋ		ŋ		N	Lo		consonant			ngar	ङ	U+11BDA SUNUWAR LETTER NGAR	
+\u{11BD1}	o	o		o		o	Lo		vowel			otthi	ओ	U+11BD1 SUNUWAR LETTER OTTHI	
+\u{11BC9}	p	p		p		p	Lo		consonant			pip	प	U+11BC9 SUNUWAR LETTER PIP	
+\u{11BD9}	f	pʰ		pʰ		f	Lo		consonant			phar	फ	U+11BD9 SUNUWAR LETTER PHAR	
+\u{11BC4}	r	r ɽ		r		r	Lo		consonant			reu	र ड़	U+11BC4 SUNUWAR LETTER REU	
+\u{11BD2}	s	s		s		s	Lo		consonant			shyele	स	U+11BD2 SUNUWAR LETTER SHYELE	
+\u{11BDC}	S	ʃ		ʃ		S	Lo		consonant			shyer	श ष	U+11BDC SUNUWAR LETTER SHYER	
+\u{11BC1}	t	t		t		t	Lo		consonant			tasla	त ट	U+11BC1 SUNUWAR LETTER TASLA	
+\u{11BD8}	T	tʰ		tʰ		T	Lo		consonant			thari	थ	U+11BD8 SUNUWAR LETTER THARI	
+\u{11BDE}	q	ʈ		ʈ		q	Lo		consonant			tentu	ट	U+11BDE SUNUWAR LETTER TENTU	
+\u{11BDF}	Q	ʈʰ		ʈʰ		Q	Lo		consonant			thele	ठ	U+11BDF SUNUWAR LETTER THELE	
+\u{11BC5}	u	u		u		u	Lo		vowel			utthi	उ	U+11BC5 SUNUWAR LETTER UTTHI	
+\u{11BD3}	w	w v		w		w	Lo		consonant			varca	व	U+11BD3 SUNUWAR LETTER VARCA	
+\u{11BD4}	y	j		y		y	Lo		consonant			yat	य	U+11BD4 SUNUWAR LETTER YAT	
 															
 §				§	§		Po		section sign					U+00A7: SECTION SIGN	
 †				†	†		Po		dagger					U+2020: DAGGER	
@@ -69,16 +69,16 @@ var spreadsheet = `sunu-suz	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 ′				′	′		Po		prime					U+2032: PRIME	
 ″				″	″		Po		double prime					U+2033: DOUBLE PRIME	
 															
-\u{11BF1}	±	1		1̣	1		Nd		digit			ka	१	U+11BF1 SUNUWAR DIGIT ONE	
-\u{11BF2}	±	2		2̣	2		Nd		digit			ni'ashi	२	U+11BF2 SUNUWAR DIGIT TWO	
-\u{11BF3}	±	3		3̣	3		Nd		digit			san	३	U+11BF3 SUNUWAR DIGIT THREE	
-\u{11BF4}	±	4		4̣	4		Nd		digit			le	४	U+11BF4 SUNUWAR DIGIT FOUR	
-\u{11BF5}	±	5		5̣	5		Nd		digit			nga	५	U+11BF5 SUNUWAR DIGIT FIVE	
-\u{11BF6}	±	6		6̣	6		Nd		digit			raku	६	U+11BF6 SUNUWAR DIGIT SIX	
-\u{11BF7}	±	7		7̣	7		Nd		digit			cani	७	U+11BF7 SUNUWAR DIGIT SEVEN	
-\u{11BF8}	±	8		8̣	8		Nd		digit			sasi	८	U+11BF8 SUNUWAR DIGIT EIGHT	
-\u{11BF9}	±	9		9̣	9		Nd		digit			yan	९	U+11BF9 SUNUWAR DIGIT NINE	
-\u{11BF0}	±	0		0̣	0		Nd		digit			sum	०	U+11BF0 SUNUWAR DIGIT ZERO	
+\u{11BF1}	±	1		1̣	1	1	Nd		digit			ka	१	U+11BF1 SUNUWAR DIGIT ONE	
+\u{11BF2}	±	2		2̣	2	2	Nd		digit			ni'ashi	२	U+11BF2 SUNUWAR DIGIT TWO	
+\u{11BF3}	±	3		3̣	3	3	Nd		digit			san	३	U+11BF3 SUNUWAR DIGIT THREE	
+\u{11BF4}	±	4		4̣	4	4	Nd		digit			le	४	U+11BF4 SUNUWAR DIGIT FOUR	
+\u{11BF5}	±	5		5̣	5	5	Nd		digit			nga	५	U+11BF5 SUNUWAR DIGIT FIVE	
+\u{11BF6}	±	6		6̣	6	6	Nd		digit			raku	६	U+11BF6 SUNUWAR DIGIT SIX	
+\u{11BF7}	±	7		7̣	7	7	Nd		digit			cani	७	U+11BF7 SUNUWAR DIGIT SEVEN	
+\u{11BF8}	±	8		8̣	8	8	Nd		digit			sasi	८	U+11BF8 SUNUWAR DIGIT EIGHT	
+\u{11BF9}	±	9		9̣	9	9	Nd		digit			yan	९	U+11BF9 SUNUWAR DIGIT NINE	
+\u{11BF0}	±	0		0̣	0	0	Nd		digit			sum	०	U+11BF0 SUNUWAR DIGIT ZERO	
 															
 \u200C	=			ₓ			Cf		zero-width non-joiner					U+200C: ZERO WIDTH NON-JOINER	
 \u200D	=			₊			Cf		zero-width joiner					U+200D: ZERO WIDTH JOINER	
