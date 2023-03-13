48084a7334c61fd6c538aeb68b22d22d07728451
r12a <ishida@w3.org>
2023-03-13 07:44:56 +0000

    lif: Scriptdb & webfont

---
+++ scriptdb/limb-lif.js
@@ -0,0 +1,123 @@
+var spreadsheet = `limb-lif	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	deva	ucs name	block
+-	-			-	-				hyphen					U+2010: HYPHEN	
+–	-			–	–		Pd		en dash					U+2013: EN DASH	
+—	-			—	—		Pd		em dash					U+2014: EM DASH	
+᤻	*			˟			Mn​		vowel silencer					U+193B: LIMBU SIGN SA-I	
+᤺	:	ː		ː			Mn​		vowel lengthener					U+193A: LIMBU SIGN KEMPHRENG	
+,	,			,̣	,		Po		comma					U+002C: COMMA	
+;	;			;̣	;		Po		semicolon					U+003B: SEMICOLON	
+:	:			:	:		Po		colon					U+003A: COLON	
+᥄	!			!	!		Po		exclamation mark					U+1944: LIMBU EXCLAMATION MARK	
+᥀	!			!̣	lo		So	o	exclamatory particle		ᤗᤥ			U+1940: LIMBU SIGN LOO	
+᥅	?			?	?		Po		question mark					U+1945: LIMBU QUESTION MARK	
+॥	.			.	.		Po		sentence end					U+0965: DEVANAGARI DOUBLE DANDA	
+.	.			.̣	.		Po		full stop					U+002E: FULL STOP	
+…	.			…	…		Po		ellipsis					U+2026: HORIZONTAL ELLIPSIS	
+(	(			(	(		Ps		parenthesis					U+0028: LEFT PARENTHESIS	
+[	(			[	[		Ps		bracket					U+005B: LEFT SQUARE BRACKET	
+)	)			)	)		Pe		parenthesis					U+0029: RIGHT PARENTHESIS	
+]	)			]	]		Pe		bracket					U+005D: RIGHT SQUARE BRACKET	
+%	%			%			Po		percentage mark					U+0025: PERCENT SIGN	
+‰	%			‰			Po		per mille mark					U+2030: PER MILLE SIGN	
+“	<			“	“		Pi		quotation mark					U+201C: LEFT DOUBLE QUOTATION MARK	
+\‘	<			\‘	\‘				quotation mark					U+2018: LEFT SINGLE QUOTATION MARK	
+”	>			”	”		Pf		quotation mark					U+201D: RIGHT DOUBLE QUOTATION MARK	
+\’	>			\’	\’				quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	
+ᤀ	[	ɔ		+			Lo		vowel carrier					U+1900: LIMBU VOWEL-CARRIER LETTER	
+ᤲ	[	̃		˜			Mn​	o	nasalisation	alternate				U+1932: LIMBU SMALL LETTER ANUSVARA	
+᤹	}	ʔ		ʔ			Mn​		glottalisation marker				अ ◌	U+1939: LIMBU SIGN MUKPHRENG	
+ᤤ	A	ai		aⁱ	ai		Mc​		diphthong				अ	U+1924: LIMBU VOWEL SIGN AI	
+ᤦ	A	au		aʷ	au		Mc​		dipthong					U+1926: LIMBU VOWEL SIGN AU	
+ᤠ	a	a		a	a		Mn​		vowel				आ	U+1920: LIMBU VOWEL SIGN A	
+ᤒ	b	b	ɔ	b	b		Lo		consonant				ब	U+1912: LIMBU LETTER BA	
+ᤓ	B	bʰ	ɔ	bʰ	bh		Lo		consonant				ब	U+1913: LIMBU LETTER BHA	
+ᤆ	c	t͡ʃ	ɔ	ʧ	c		Lo		consonant				च	U+1906: LIMBU LETTER CA	
+ᤇ	C	t͡ʃʰ	ɔ	ʧʰ	ch		Lo		consonant				छ	U+1907: LIMBU LETTER CHA	
+ᤍ	d	d	ɔ	d	d		Lo		consonant				द ड	U+190D: LIMBU LETTER DA	
+ᤎ	d	dʰ	ɔ	dʰ	dh		Lo		consonant					U+190E: LIMBU LETTER DHA	
+ᤧ	e	ɛ		ɛ	e		Mn​		vowel					U+0020 U+1927: SPACE, LIMBU VOWEL SIGN E	
+ᤣ	E	e		e	ee		Mc​		vowel				ए	U+1923: LIMBU VOWEL SIGN EE	
+ᤃ	g	ɡ	ɔ	g	g		Lo		consonant				ग	U+1903: LIMBU LETTER GA	
+ᤄ	g	ɡʰ	ɔ	gʰ	gh		Lo		consonant					U+1904: LIMBU LETTER GHA	
+ᤜ	h	h	ɔ	h	h		Lo		consonant				ह	U+191C: LIMBU LETTER HA	
+ᤡ	i	i		i	i		Mn​		vowel				इ	U+1921: LIMBU VOWEL SIGN I	
+ᤈ	j	d͡ʒ	ɔ	ʤ	j		Lo		consonant				ज	U+1908: LIMBU LETTER JA	
+ᤁ	k	k	ɔ	k	k		Lo		consonant				क	U+1901: LIMBU LETTER KA	
+ᤰ	K	k		k̽	k		Mc​		final consonant					U+1930: LIMBU SMALL LETTER KA	
+ᤂ	K	kʰ	ɔ	kʰ	kh		Lo		consonant				ख	U+1902: LIMBU LETTER KHA	
+ᤗ	l	l	ɔ	l	l		Lo		consonant				ल	U+1917: LIMBU LETTER LA	
+ᤸ	L	l		l̽	l		Mc​		final consonant					U+1938: LIMBU SMALL LETTER LA	
+ᤔ	m	m	ɔ	m	m		Lo		consonant				म	U+1914: LIMBU LETTER MA	
+ᤶ	M	m		m̽	m		Mc​		final consonant					U+1936: LIMBU SMALL LETTER MA	
+ᤏ	n	n	ɔ	n	n		Lo		consonant				न ण	U+190F: LIMBU LETTER NA	
+ᤴ	N	n		n̽	n		Mc​		final consonant					U+0020 U+1934: SPACE, LIMBU SMALL LETTER NA	
+ᤅ	N	ŋ	ɔ	ŋ	ng		Lo		consonant				ङ	U+1905: LIMBU LETTER NGA	
+ᤱ	N	ŋ		ŋ̽	ng		Mc​		final consonant					U+0020 U+1931: SPACE, LIMBU SMALL LETTER NGA	
+ᤨ	o	ɔ		ɔ	o		Mn​		vowel	optional				U+0020 U+1928: SPACE, LIMBU VOWEL SIGN O	
+ᤥ	O	o		o	oo		Mc​		vowel				ओ	U+1925: LIMBU VOWEL SIGN OO	
+ᤐ	p	p	ɔ	p	p		Lo		consonant				प	U+1910: LIMBU LETTER PA	
+ᤵ	P	p		p̽	p		Mc​		final consonant					U+1935: LIMBU SMALL LETTER PA	
+ᤑ	f	pʰ	ɔ	pʰ	ph		Lo		consonant				फ	U+1911: LIMBU LETTER PHA	
+ᤖ	r	r	ɔ	r	r		Lo		consonant				र ड़	U+1916: LIMBU LETTER RA	
+ᤪ	R	r		r̆	r		Mc​		medial consonant					U+192A: LIMBU SUBJOINED LETTER RA	
+ᤷ	R	r		r̽	r		Mc​		final consonant					U+1937: LIMBU SMALL LETTER RA	
+ᤛ	s	s	ɔ	s	s		Lo		consonant				स	U+191B: LIMBU LETTER SA	
+ᤙ	S	ʃ	ɔ	ʃ	sh		Lo		consonant				श ष	U+1919: LIMBU LETTER SHA	
+ᤋ	t	t	ɔ	t	t		Lo		consonant				त ट	U+190B: LIMBU LETTER TA	
+ᤳ	T	t		t̽	t		Mc​		final consonant					U+1933: LIMBU SMALL LETTER TA	
+ᤌ	T	tʰ	ɔ	tʰ	th		Lo		consonant				थ	U+190C: LIMBU LETTER THA	
+ᤢ	u	u		u	u		Mn​		vowel				उ	U+1922: LIMBU VOWEL SIGN U	
+ᤘ	w	w	ɔ	w	w		Lo		consonant				व	U+1918: LIMBU LETTER WA	
+ᤫ	W	w		w̆	w		Mc​		medial consonant					U+192B: LIMBU SUBJOINED LETTER WA	
+ᤕ	y	j	ɔ	y	y		Lo		consonant				य	U+1915: LIMBU LETTER YA	
+ᤩ	y	j		y̆	y		Mc​		medial consonant					U+1929: LIMBU SUBJOINED LETTER YA	
+															
+ᤉ		d͡ʑʱ	ɔ	ʤʰ			Lo	o	consonant	early versions of script				U+1909: LIMBU LETTER JHA	
+ᤊ		ɲ	ɔ	ɲ			Lo	o	consonant	early versions of script				U+190A: LIMBU LETTER YAN	
+ᤚ		ʂ	ɔ	ʂ			Lo	o	consonant	early versions of script				U+191A: LIMBU LETTER SSA	
+ᤝ		d͡ʑɲ	a		jñ		Lo	o	conjunct	early versions of script				U+191D: LIMBU LETTER GYAN	
+ᤞ		tr	a		tr		Lo	o	conjunct	early versions of script				U+191E: LIMBU LETTER TRA	
+															
+§				§			Po		section sign					U+00A7: SECTION SIGN	
+†				†			Po		dagger					U+2020: DAGGER	
+‡				‡			Po		double dagger					U+2021: DOUBLE DAGGER	
+′				′			Po		prime					U+2032: PRIME	
+″				″			Po		double prime					U+2033: DOUBLE PRIME	
+															
+᥇	±	1		1̣	1		Nd		digit				१	U+1947: LIMBU DIGIT ONE	
+᥈	±	2		2̣	2		Nd		digit				२	U+1948: LIMBU DIGIT TWO	
+᥉	±	3		3̣	3		Nd		digit				३	U+1949: LIMBU DIGIT THREE	
+᥊	±	4		4̣	4		Nd		digit				४	U+194A: LIMBU DIGIT FOUR	
+᥋	±	5		5̣	5		Nd		digit				५	U+194B: LIMBU DIGIT FIVE	
+᥌	±	6		6̣	6		Nd		digit				६	U+194C: LIMBU DIGIT SIX	
+᥍	±	7		7̣	7		Nd		digit				७	U+194D: LIMBU DIGIT SEVEN	
+᥎	±	8		8̣	8		Nd		digit				८	U+194E: LIMBU DIGIT EIGHT	
+᥏	±	9		9̣	9		Nd		digit				९	U+194F: LIMBU DIGIT NINE	
+᥆	±	0		0̣	0		Nd		digit				०	U+1946: LIMBU DIGIT ZERO	
+															
+\u200C	=			ₓ			Cf		zero-width non-joiner					U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=			₊			Cf		zero-width joiner					U+200D: ZERO WIDTH JOINER	
+◌									placeholder					U+25CC: DOTTED CIRCLE	
+`															
+															
+latinPanel = 'ə bʰ dʰ d͡ʑʱ d͡ʒ gʰ kʰ pʰ tʰ t͡ʃ t͡ʃʰ ŋ ɔ ɛ ɡ ɡʰ ɲ ʂ ʃ ʔ ˜ ̃ ā ī ū ɛ̄ ō ɔ̄ aː iː uː ɛː ɔː oː'															
+															
+var cols = {															
+key: 1,															
+ipaLoc: 2,															
+ipaPlus: 3,															
+transLoc: 4,															
+transcription: 5,															
+kbd: 6,															
+class: 7,															
+status: 8,															
+typeLoc: 9,															
+statusLoc: 10,															
+															
+nnameLoc: 11,															
+nameLoc: 12,															
+ucsName: 14,															
+block: 15,															
+															
+//othertranscriptions: [[13, 'Deva']]															
+}															
