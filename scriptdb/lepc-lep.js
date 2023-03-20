1b3f71e4ab154dff55d56947199e1131c9428cad
r12a <ishida@w3.org>
2023-03-20 10:15:36 +0000

    + scriptdb/lepc-lep

---
+++ scriptdb/lepc-lep.js
@@ -0,0 +1,138 @@
+var spreadsheet = `lepc-lep	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	onset	finals	native name	latin name	deva	ucs name	block
+-	-			-	-				hyphen							U+2010: HYPHEN	
+–	-			–	–		Pd		en dash							U+2013: EN DASH	
+—	-			—	—		Pd		em dash							U+2014: EM DASH	
+	*			˟			Mn​		vowel silencer								
+	:	ː		ː			Mn​		vowel lengthener								
+᰻	.			.̣												U+1C3B: LEPCHA PUNCTUATION TA-ROL	
+᰼	.			.̱												U+1C3C: LEPCHA PUNCTUATION NYET THYOOM TA-ROL	
+᰽	.			.͈					full stop							U+1C3D: LEPCHA PUNCTUATION CER-WA	
+᰾	.			.͇					full stop							U+1C3E: LEPCHA PUNCTUATION TSHOOK CER-WA	
+᰿	.			|				r								U+1C3F: LEPCHA PUNCTUATION TSHOOK	
+.	.			.	.		Po		full stop							U+002E: FULL STOP	
+,	,			,̣	,		Po		comma							U+002C: COMMA	
+;	;			;̣	;		Po		semicolon							U+003B: SEMICOLON	
+:	:			:	:		Po		colon							U+003A: COLON	
+!	!			!	!		Po		exclamation mark								
+?	?			?	?		Po		question mark								
+…	.			…	…		Po		ellipsis							U+2026: HORIZONTAL ELLIPSIS	
+(	(			(	(		Ps		parenthesis							U+0028: LEFT PARENTHESIS	
+[	(			[	[		Ps		bracket							U+005B: LEFT SQUARE BRACKET	
+)	)			)	)		Pe		parenthesis							U+0029: RIGHT PARENTHESIS	
+]	)			]	]		Pe		bracket							U+005D: RIGHT SQUARE BRACKET	
+%	%			%			Po		percentage mark							U+0025: PERCENT SIGN	
+‰	%			‰			Po		per mille mark							U+2030: PER MILLE SIGN	
+“	<			“	“		Pi		quotation mark							U+201C: LEFT DOUBLE QUOTATION MARK	
+\‘	<			\‘	\‘				quotation mark							U+2018: LEFT SINGLE QUOTATION MARK	
+”	>			”	”		Pf		quotation mark							U+201D: RIGHT DOUBLE QUOTATION MARK	
+\’	>			\’	\’				quotation mark							U+2019: RIGHT SINGLE QUOTATION MARK	
+᰷	/			ˑ			Mn		nukta							U+1C37: LEPCHA SIGN NUKTA	
+ᰣ	[	ʔ		ʔ			Lo		vowel carrier							U+1C23: LEPCHA LETTER A	
+ᰶ	A			â	â		Mn​		vowel							U+1C36: LEPCHA SIGN RAN	
+ᰦ	a	a		 á	 á		Mc​		vowel							U+1C26: LEPCHA VOWEL SIGN AA	
+ᰓ	b	b	a	b	b		Lo		consonant						ब	U+1C13: LEPCHA LETTER BA	
+ᰆ	c	c	a	c	c		Lo		consonant						च	U+1C06: LEPCHA LETTER CA	
+ᰇ	C	cʰ	a	cʰ	ch		Lo		consonant						छ	U+1C07: LEPCHA LETTER CHA	
+ᰌ	d	d	a	d	d		Lo		consonant						द ड	U+1C0C: LEPCHA LETTER DA	
+ᰬ	e	e ɛ		ɛ	e		Mn​		vowel							U+1C2C: LEPCHA VOWEL SIGN E	
+ᰑ	f	f	a	f	f		Lo		consonant							U+1C11: LEPCHA LETTER FA	
+ᰃ	g	ɡ	a	g	g		Lo		consonant						ग	U+1C03: LEPCHA LETTER GA	
+ᰝ	h	h	a	h	h		Lo		consonant						ह	U+1C1D: LEPCHA LETTER HA	
+ᰧ	i	i		i	i		Mc​		vowel						इ	U+1C27: LEPCHA VOWEL SIGN I	
+ᰈ	j	ʒ	a	ʒ	j		Lo		consonant						ज	U+1C08: LEPCHA LETTER JA	
+ᰀ	k	k	a	k	k		Lo		consonant			ᰭ			क	U+1C00: LEPCHA LETTER KA	
+ᰭ	K	k		k̽	k		Mn​		final consonant		ᰀ		ᰜᰀᰦᰳ	lakát		U+1C2D: LEPCHA CONSONANT SIGN K	
+ᰂ	K	kʰ	a	kʰ	kh		Lo		consonant						ख	U+1C02: LEPCHA LETTER KHA	
+ᰜ	l	l	a	l	l		Lo		consonant			ᰯ			ल	U+1C1C: LEPCHA LETTER LA	
+ᰯ	L	l		l̽	l		Mn​		final consonant		ᰜ		ᰜᰠᰦᰮ	lasám		U+1C2F: LEPCHA CONSONANT SIGN L	
+ᰕ	m	m	a	m	m		Lo		consonant			ᰮ			म	U+1C15: LEPCHA LETTER MA	
+ᰮ	M	m		m̽	m		Mn​		final consonant		ᰕ		ᰜᰉᰬᰳ	lanyet		U+1C2E: LEPCHA CONSONANT SIGN M	
+ᰍ	n	n	a	n	n		Lo		consonant			ᰰ			न ण	U+1C0D: LEPCHA LETTER NA	
+ᰰ	N	n		n̽	n		Mn​		final consonant		ᰍ		ᰜᰦᰍᰪᰰ	lanun		U+1C30: LEPCHA CONSONANT SIGN N	
+ᰅ	N	ŋ	a	ŋ	ng		Lo		consonant			ᰵ			ङ	U+1C05: LEPCHA LETTER NGA	
+ᰵ	N	ŋ		ŋ̽	ng		Mc		final consonant		ᰅ		ᰜᰀᰴ	lakang		U+1C35: LEPCHA CONSONANT SIGN KANG	
+ᰴ	N	ŋ		ŋ̣̽	ng		Mc​		final consonant		ᰅ		ᰉᰧᰰᰶᰌᰨ	nyíndo		U+1C34: LEPCHA CONSONANT SIGN NYIN-DO	
+ᰉ	n	ɲ		ɲ	ny		Lo		consonant							U+1C09: LEPCHA LETTER NYA	
+ᰩ	o	ɔ		ɔ	ó		Mc​		vowel							U+1C29: LEPCHA VOWEL SIGN OO	
+ᰨ	O	o		o	o		Mc​		vowel						ओ	U+1C28: LEPCHA VOWEL SIGN O	
+ᰎ	p	p	a	p	p		Lo		consonant			ᰱ			प	U+1C0E: LEPCHA LETTER PA	
+ᰱ	P	p		p̽	p		Mn​		final consonant		ᰎ		ᰜᰀᰪᰱ	lakup		U+1C31: LEPCHA CONSONANT SIGN P	
+ᰐ	P	pʰ	a	pʰ	ph		Lo		consonant						फ	U+1C10: LEPCHA LETTER PHA	
+ᰛ	r	r	a	r	r		Lo		consonant			ᰲ			र ड़	U+1C1B: LEPCHA LETTER RA	
+ᰥ	R	r		r̆	r		Mc​		medial consonant							U+1C25: LEPCHA SUBJOINED LETTER RA	
+ᰲ	R	r		r̽	r		Mn​		final consonant		ᰛ		ᰜᰌᰲ	ladar		U+1C32: LEPCHA CONSONANT SIGN R	
+ᰠ	s	s	a	s	s		Lo		consonant						स	U+1C20: LEPCHA LETTER SA	
+ᰡ	S	ʃ	a	ʃ	sh		Lo		consonant						श ष	U+1C21: LEPCHA LETTER SHA	
+ᰊ	t	t	a	t	t		Lo		consonant			ᰳ			त ट	U+1C0A: LEPCHA LETTER TA	
+ᰳ	T	t		t̽	t		Mn​		final consonant		ᰊ		ᰜᰀᰳ	lakat		U+1C33: LEPCHA CONSONANT SIGN T	
+ᰋ	T	tʰ	a	tʰ	th		Lo		consonant						थ	U+1C0B: LEPCHA LETTER THA	
+ᰗ	t	t͡s	a	tˢ	ts		Lo		consonant							U+1C17: LEPCHA LETTER TSA	
+ᰘ	T	t͡sʰ	a	ʧʰ	tsh		Lo		consonant							U+1C18: LEPCHA LETTER TSHA	
+ᰪ	u	ɯ		ɯ	u		Mc​									U+1C2A: LEPCHA VOWEL SIGN U	
+ᰫ	U	u		u	ú		Mc​		vowel						उ	U+1C2B: LEPCHA VOWEL SIGN UU	
+ᰟ	v	v	a	v	v		Lo		consonant							U+1C1F: LEPCHA LETTER VA	
+ᰢ	w	w	a	w	w		Lo		consonant						व	U+1C22: LEPCHA LETTER WA	
+ᰚ	y	j	a	y	y		Lo		consonant						य	U+1C1A: LEPCHA LETTER YA	
+ᰤ	Y	j	a	y̆	y		Mc​		medial consonant							U+1C24: LEPCHA SUBJOINED LETTER YA	
+ᰙ	z	z d͡z	a	z	z		Lo		consonant							U+1C19: LEPCHA LETTER DZA	
+																	
+ᰁ	L	kl	a	kˡ	kl		Lo		onset cluster							U+1C01: LEPCHA LETTER KLA	
+ᰄ	L	gl	a	gˡ	gl		Lo		onset cluster							U+1C04: LEPCHA LETTER GLA	
+ᰏ	L	pl	a	pˡ	pl		Lo		onset cluster							U+1C0F: LEPCHA LETTER PLA	
+ᰒ	L	fl	a	fˡ	fl		Lo		onset cluster							U+1C12: LEPCHA LETTER FLA	
+ᰔ	L	bl	a	bˡ	bl		Lo		onset cluster							U+1C14: LEPCHA LETTER BLA	
+ᰖ	L	ml	a	mˡ	ml		Lo		onset cluster							U+1C16: LEPCHA LETTER MLA	
+ᰞ	L	hl	a	hˡ	hl		Lo		onset cluster							U+1C1E: LEPCHA LETTER HLA	
+ᱍ	t	ʈ	a	ʈ	tt		Lo		consonant							U+1C4D: LEPCHA LETTER TTA	
+ᱎ	T	ʈʰ	a	ʈʰ	tth		Lo		consonant							U+1C4E: LEPCHA LETTER TTHA	
+ᱏ	d	ɖ	a	ɖ	dd		Lo		consonant							U+1C4F: LEPCHA LETTER DDA	
+ᰀ᰷ᰥ		ʈ	a	hˑr̆	tr				consonant							U+1C00 U+1C37 U+1C25: LEPCHA LETTER KA, SIGN NUKTA, SUBJOINED LETTER RA	
+ᰝ᰷ᰥ		ʈʰ	a	hˑr̆	thr				consonant							U+1C1D U+1C37 U+1C25: LEPCHA LETTER HA, SIGN NUKTA, SUBJOINED LETTER RA	
+ᰃ᰷ᰥ		ɖ	a	gˑr̆	dr				consonant							U+1C03 U+1C37 U+1C25: LEPCHA LETTER GA, SIGN NUKTA, SUBJOINED LETTER RA	
+																	
+§				§			Po		section sign							U+00A7: SECTION SIGN	
+†				†			Po		dagger							U+2020: DAGGER	
+‡				‡			Po		double dagger							U+2021: DOUBLE DAGGER	
+′				′			Po		prime							U+2032: PRIME	
+″				″			Po		double prime							U+2033: DOUBLE PRIME	
+																	
+᱁	±	1		1̣	1		Nd		digit						१	U+1C41: LEPCHA DIGIT ONE	
+᱂	±	2		2̣	2		Nd		digit						२	U+1C42: LEPCHA DIGIT TWO	
+᱃	±	3		3̣	3		Nd		digit						३	U+1C43: LEPCHA DIGIT THREE	
+᱄	±	4		4̣	4		Nd		digit						४	U+1C44: LEPCHA DIGIT FOUR	
+᱅	±	5		5̣	5		Nd		digit						५	U+1C45: LEPCHA DIGIT FIVE	
+᱆	±	6		6̣	6		Nd		digit						६	U+1C46: LEPCHA DIGIT SIX	
+᱇	±	7		7̣	7		Nd		digit						७	U+1C47: LEPCHA DIGIT SEVEN	
+᱈	±	8		8̣	8		Nd		digit						८	U+1C48: LEPCHA DIGIT EIGHT	
+᱉	±	9		9̣	9		Nd		digit						९	U+1C49: LEPCHA DIGIT NINE	
+᱀	±	0		0̣	0		Nd		digit						०	U+1C40: LEPCHA DIGIT ZERO	
+																	
+\u200C	=			ₓ			Cf		zero-width non-joiner							U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=			₊			Cf		zero-width joiner							U+200D: ZERO WIDTH JOINER	
+◌									placeholder							U+25CC: DOTTED CIRCLE	
+`																	
+																	
+latinPanel = 'd͡ʒ ʰ ː pʰ tʰ t͡ʃ t͡s r̥ l̥ d͡z á â ɑ í ó ú ʲ ʔk̚ ŋ ɔ ɖ ə ɛ ɯ ɲ ʔp̚ ʔt̚ ʃ ʈ ʈʰ ʌ ʒ ʔ'																	
+																	
+var cols = {																	
+key: 1,																	
+ipaLoc: 2,																	
+ipaPlus: 3,																	
+transLoc: 4,																	
+transcription: 5,																	
+kbd: 6,																	
+class: 7,																	
+status: 8,																	
+typeLoc: 9,																	
+statusLoc: 10,																	
+																	
+onset: 11,																	
+finals: 12,																	
+																	
+nnameLoc: 13,																	
+nameLoc: 14,																	
+ucsName: 16,																	
+block: 17,																	
+																	
+//othertranscriptions: [[13, 'Deva']]																	
+}																	
