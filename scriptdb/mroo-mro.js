96e776724664c3e68f7598c84ab4ce09b8e0370f
r12a <ishida@w3.org>
2023-04-02 22:14:10 +0100

    + Mro

---
+++ scriptdb/mroo-mro.js
@@ -0,0 +1,91 @@
+var spreadsheet = `mroo-mro	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	ucs name	block
+-	-			-	-				hyphen				U+2010: HYPHEN	✓
+–	-			–	–		Pd		en dash				U+2013: EN DASH	✓
+—	-			—	—		Pd		em dash				U+2014: EM DASH	✓
+𖩮	.			.̣	.		Po		full stop				U+16A6E: MRO DANDA	
+𖩯	.			.̱	.		Po		section delimiter				U+16A6F: MRO DOUBLE DANDA	
+.	.			.	.		Po		full stop				U+002E: FULL STOP	✓
+,	,			,̣	,		Po		comma				U+002C: COMMA	✓
+;	;			;̣	;		Po		semicolon				U+003B: SEMICOLON	✓
+:	:			:	:		Po		colon				U+003A: COLON	✓
+!	!			!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK	✓
+?	?			?	?		Po		question mark				U+003F: QUESTION MARK	✓
+…	.			…	…		Po		ellipsis				U+2026: HORIZONTAL ELLIPSIS	✓
+(	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS	✓
+[	(			[	[		Ps		bracket				U+005B: LEFT SQUARE BRACKET	✓
+)	)			)	)		Pe		parenthesis				U+0029: RIGHT PARENTHESIS	✓
+]	)			]	]		Pe		bracket				U+005D: RIGHT SQUARE BRACKET	✓
+%	%			%			Po		percentage mark				U+0025: PERCENT SIGN	✓
+‰	%			‰			Po		per mille mark				U+2030: PER MILLE SIGN	✓
+“	<			“	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	✓
+‘	<			\‘	\‘				quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	✓
+”	>			”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	✓
+’	>			\’	\’				quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	✓
+𖩒	}	ʔ		ʔ	’		Lo		consonant, stop				U+16A52: MRO LETTER O	✓
+𖩆	a	a ɑ		a	a		Lo		vowel				U+16A46: MRO LETTER A	✓
+𖩄	b	b		b	b		Lo		consonant				U+16A44: MRO LETTER BA	✓
+𖩋	c	t͡s		c	c		Lo		consonant				U+16A4B: MRO LETTER CHU	✓
+𖩅	d	d		d	d		Lo		consonant				U+16A45: MRO LETTER DA	✓
+𖩖	e	ɛ ə		ɛ	ä		Lo		vowel				U+16A56: MRO LETTER EA	✓
+𖩘	e	e		e	e		Lo		vowel				U+16A58: MRO LETTER E	
+𖩉	h	h		h	h		Lo		consonant				U+16A49: MRO LETTER HAO	✓
+𖩜	h	h		ḥ	h		Lo		consonant, fricative				U+16A5C: MRO LETTER HAI	
+𖩊	i	i		i	i		Lo		vowel				U+16A4A: MRO LETTER DAI	✓
+𖩌	k	k		k	k		Lo		consonant				U+16A4C: MRO LETTER KEAAE	✓
+𖩙	k	k		ḳ	ḳ		Lo		consonant, stop				U+16A59: MRO LETTER KO	
+𖩈	K	kʰ		kʰ	kh		Lo		aspirated consonant				U+16A48: MRO LETTER KHAI	
+𖩍	l	l		l	l		Lo		consonant, other				U+16A4D: MRO LETTER OL	✓
+𖩚	l	l		ḷ	ḷ		Lo		consonant, other				U+16A5A: MRO LETTER LAN	
+𖩛	l	l		ḻ	ḻ		Lo		consonant, other				U+16A5B: MRO LETTER LA	
+𖩎	m	m		ṃ	ṃ		Lo		consonant, nasal				U+16A4E: MRO LETTER MAEM	
+𖩃	m	m		m	m		Lo		consonant				U+16A43: MRO LETTER MIM	✓
+𖩏	n	n		n	n		Lo		consonant				U+16A4F: MRO LETTER NIN	✓
+𖩁	N	ŋ		ŋ	ng		Lo		consonant				U+16A41: MRO LETTER NGI	✓
+𖩝	o	ɔ		ɔ	o		Lo		vowel				U+16A5D: MRO LETTER RI	✓
+𖩑𖩖	o	ø		uɛ	uä				vowel digraph				U+16A51 U+16A56: MRO LETTER OO, LETTER EA	
+𖩐	p	p		p	p		Lo		consonant, stop				U+16A50: MRO LETTER PA	✓
+𖩇	P	pʰ		pʰ	ph		Lo		aspirated consonant				U+16A47: MRO LETTER PHI	
+𖩓	r	r		r	r		Lo		consonant				U+16A53: MRO LETTER RO	✓
+𖩔	s	ʃ		ʃ	s		Lo		consonant. fricative				U+16A54: MRO LETTER SHI	
+𖩀	t	t		t	t		Lo		consonant				U+16A40: MRO LETTER TA	✓
+𖩞	t	t		ṭ	t		Lo		consonant, stop				U+16A5E: MRO LETTER TEK	
+𖩕	T	tʰ		tʰ	th		Lo		consonant, stop aspirated				U+16A55: MRO LETTER THEA	✓
+𖩑	u	u o		u	u		Lo		vowel				U+16A51: MRO LETTER OO	✓
+𖩗	w	ɯ		ɯ	w		Lo		vowel				U+16A57: MRO LETTER WA	✓
+𖩂	y	ʝ j		y	y		Lo		consonant				U+16A42: MRO LETTER YO	✓
+														✓
+𖩠	±	0		0̣	0		Nd		digit				U+16A60: MRO DIGIT ZERO	✓
+𖩡	±	1		1̣	1		Nd		digit				U+16A61: MRO DIGIT ONE	✓
+𖩢	±	2		2̣	2		Nd		digit				U+16A62: MRO DIGIT TWO	✓
+𖩣	±	3		3̣	3		Nd		digit				U+16A63: MRO DIGIT THREE	✓
+𖩤	±	4		4̣	4		Nd		digit				U+16A64: MRO DIGIT FOUR	✓
+𖩥	±	5		5̣	5		Nd		digit				U+16A65: MRO DIGIT FIVE	✓
+𖩦	±	6		6̣	6		Nd		digit				U+16A66: MRO DIGIT SIX	✓
+𖩧	±	7		7̣	7		Nd		digit				U+16A67: MRO DIGIT SEVEN	✓
+𖩨	±	8		8̣	8		Nd		digit				U+16A68: MRO DIGIT EIGHT	✓
+𖩩	±	9		9̣	9		Nd		digit				U+16A69: MRO DIGIT NINE	✓
+														✓
+◌									placeholder				U+25CC: DOTTED CIRCLE	✓
+`														
+														
+latinPanel = 'kʰ pʰ tʰ t͡s ä ŋ ɑ ɔ ə ɛ ɯ ʃ ʔ ʝ ḥ ḳ ḷ ḻ ṃ ṭ ’ ś'														
+														
+var cols = {														
+key: 1,														
+ipaLoc: 2,														
+ipaPlus: 3,														
+transLoc: 4,														
+transcription: 5,														
+kbd: 6,														
+class: 7,														
+status: 8,														
+typeLoc: 9,														
+statusLoc: 10,														
+														
+nnameLoc: 11,														
+nameLoc: 12,														
+ucsName: 13,														
+block: 14,														
+														
+//othertranscriptions: [[13, 'Deva']]														
+}														
