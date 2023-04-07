c51b9ed1835f85d056901d4803847acdff0a262c
r12a <ishida@w3.org>
2023-04-07 10:28:30 +0100

    unr: Add sorting column

--- scriptdb/nagm-unr.js
+++ scriptdb/nagm-unr.js
@@ -1,124 +1,122 @@
-var spreadsheet = `nagm-unr	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	ucs name	block
--	-			-	-				hyphen		\u{1E4E4}\u{1E4E0}\u{1E4E4}\u{1E4DC}	et͡ʃeɖ	U+2010: HYPHEN	✓
-–	-			–	–		Pd		en dash				U+2013: EN DASH	✓
-—	-			—	—		Pd		em dash				U+2014: EM DASH	✓
-\u{1E4ED}	:	ː		ː		x	Mn​		vowel lengthener		\u{1E4DD}\u{1E4D0}\u{1E4D3}\u{1E4D0}\u{1E4E3}	toyor	U+1E4ED: NAG MUNDARI SIGN TOYOR	✓
-\u{1E4EC}	[	̃		˜	̃	v	Mn​		nasalisation mark		\u{1E4E7}\u{1E4DF}\u{1E4EC}\u{1E4DE}\u{1E4D0}\u{1E4E3}	mũhor	U+1E4EC: NAG MUNDARI SIGN MUHOR	✓
-.	.			.	.		Po		full stop				U+002E: FULL STOP	✓
-,	,			,̣	,		Po		comma				U+002C: COMMA	✓
-;	;			;̣	;		Po		semicolon				U+003B: SEMICOLON	✓
-:	:			:	:		Po		colon				U+003A: COLON	✓
-!	!			!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK	✓
-?	?			?	?		Po		question mark				U+003F: QUESTION MARK	✓
-…	.			…	…		Po		ellipsis				U+2026: HORIZONTAL ELLIPSIS	✓
-(	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS	✓
-[	(			[	[		Ps		bracket				U+005B: LEFT SQUARE BRACKET	✓
-)	)			)	)		Pe		parenthesis				U+0029: RIGHT PARENTHESIS	✓
-]	)			]	]		Pe		bracket				U+005D: RIGHT SQUARE BRACKET	✓
-%	%			%			Po		percentage mark				U+0025: PERCENT SIGN	✓
-‰	%			‰			Po		per mille mark				U+2030: PER MILLE SIGN	✓
-“	<			“	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	✓
-\‘	<			\‘	\‘				quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	✓
-”	>			”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	✓
-\’	>			\’	\’				quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	✓
-\u{1E4EF}	/			ˑ			Mn​		nukta		\u{1E4DB}\u{1E4DF}\u{1E4DD}\u{1E4DF}\u{1E4D9}	sutu\u{294}	U+1E4EF: NAG MUNDARI SIGN SUTUH	✓
-\u{1E4D9}	}	ʔ		ʔ		H	Lo		consonant				U+1E4D9: NAG MUNDARI LETTER AH	✓
-\u{1E4EB}	}	ˀ		ˀ		z	Lm		checked final mark		\u{1E4D0}\u{1E4D6}\u{1E4D0}\u{1E4E1}	ojod	U+1E4EB: NAG MUNDARI SIGN OJOD	✓
-\u{1E4D5}	a	a		a	a	a	Lo		vowel				U+1E4D5: NAG MUNDARI LETTER A	✓
-\u{1E4D7}	b	b		b	b	b	Lo		consonant				U+1E4D7: NAG MUNDARI LETTER AB	✓
-\u{1E4E0}	c	t͡ʃ		c	c	c	Lo		consonant				U+1E4E0: NAG MUNDARI LETTER UC	✓
-\u{1E4E1}	d	d		d	d	D	Lo		consonant				U+1E4E1: NAG MUNDARI LETTER UD	✓
-\u{1E4DC}	D	ɖ		ɖ	ḍ	d	Lo		consonant				U+1E4DC: NAG MUNDARI LETTER IDD	✓
-\u{1E4E4}	e	e		e	e	e	Lo		vowel				U+1E4E4: NAG MUNDARI LETTER E	✓
-\u{1E4E6}	g	ɡ		g	g	g	Lo		consonant				U+1E4E6: NAG MUNDARI LETTER EG	✓
-\u{1E4DE}	h	h		h	h	h	Lo		consonant				U+1E4DE: NAG MUNDARI LETTER IH	✓
-\u{1E4DA}	i	i		i	i	i	Lo		vowel				U+1E4DA: NAG MUNDARI LETTER I	✓
-\u{1E4D6}	j	d͡ʒ		ʤ	j	j	Lo		consonant				U+1E4D6: NAG MUNDARI LETTER AJ	✓
-\u{1E4E2}	k	k		k	k	k	Lo		consonant				U+1E4E2: NAG MUNDARI LETTER UK	✓
-\u{1E4D2}	l	l		l	l	l	Lo		consonant				U+1E4D2: NAG MUNDARI LETTER OL	✓
-\u{1E4E7}	m	m		m	m	m	Lo		consonant				U+1E4E7: NAG MUNDARI LETTER EM	✓
-\u{1E4E8}	n	n		n	n	n	Lo		consonant				U+1E4E8: NAG MUNDARI LETTER EN	✓
-\u{1E4D8}	n	ɲ		ɲ	ny	J	Lo		consonant				U+1E4D8: NAG MUNDARI LETTER ANY	✓
-\u{1E4D4}	N	ŋ		ŋ	ng	G	Lo		consonant				U+1E4D4: NAG MUNDARI LETTER ONG	✓
-\u{1E4E5}	N	ɳ		ŋ	ṇ	N	Lo		consonant				U+1E4E5: NAG MUNDARI LETTER ENN	✓
-\u{1E4D0}	o	o		o	o	o	Lo		vowel				U+1E4D0: NAG MUNDARI LETTER O	✓
-\u{1E4D1}	p	p		p	p	p	Lo		consonant				U+1E4D1: NAG MUNDARI LETTER OP	✓
-\u{1E4E3}	r	r		r	r	r	Lo		consonant				U+1E4E3: NAG MUNDARI LETTER UR	✓
-\u{1E4EA}	R	ɽ		ɽ	r	L	Lo		consonant				U+1E4EA: NAG MUNDARI LETTER ELL	✓
-\u{1E4DB}	s	s		s	s	s	Lo		consonant				U+1E4DB: NAG MUNDARI LETTER IS	✓
-\u{1E4DD}	t	t		t	t	T	Lo		consonant				U+1E4DD: NAG MUNDARI LETTER IT	✓
-\u{1E4E9}	T	ʈ		ʈ	tt	t	Lo		consonant				U+1E4E9: NAG MUNDARI LETTER ETT	✓
-\u{1E4DF}	u	u		u	u	u	Lo		vowel				U+1E4DF: NAG MUNDARI LETTER U	✓
-\u{1E4EE}	w	ʷ		ʷ		w	Mn​		labialisation mark		\u{1E4DA}\u{1E4E2}\u{1E4DA}\u{1E4E3}	ikir	U+1E4EE: NAG MUNDARI SIGN IKIR	✓
-\u{1E4D3}	y	j		y	y	y	Lo		consonant				U+1E4D3: NAG MUNDARI LETTER OY	✓
-														✓
-\u{1E4F0}	±	0		0̣	0	0	Nd		digit				U+1E4F0: NAG MUNDARI DIGIT ZERO	✓
-\u{1E4F1}	±	1		1̣	1	1	Nd		digit				U+1E4F1: NAG MUNDARI DIGIT ONE	✓
-\u{1E4F2}	±	2		2̣	2	2	Nd		digit				U+1E4F2: NAG MUNDARI DIGIT TWO	✓
-\u{1E4F3}	±	3		3̣	3	3	Nd		digit				U+1E4F3: NAG MUNDARI DIGIT THREE	✓
-\u{1E4F4}	±	4		4̣	4	4	Nd		digit				U+1E4F4: NAG MUNDARI DIGIT FOUR	✓
-\u{1E4F5}	±	5		5̣	5	5	Nd		digit				U+1E4F5: NAG MUNDARI DIGIT FIVE	✓
-\u{1E4F6}	±	6		6̣	6	6	Nd		digit				U+1E4F6: NAG MUNDARI DIGIT SIX	✓
-\u{1E4F7}	±	7		7̣	7	7	Nd		digit				U+1E4F7: NAG MUNDARI DIGIT SEVEN	✓
-\u{1E4F8}	±	8		8̣	8	8	Nd		digit				U+1E4F8: NAG MUNDARI DIGIT EIGHT	✓
-\u{1E4F9}	±	9		9̣	9	9	Nd		digit				U+1E4F9: NAG MUNDARI DIGIT NINE	✓
-														✓
-०	±						Nd		digit				U+0966: DEVANAGARI DIGIT ZERO	✓
-१	±						Nd		digit				U+0967: DEVANAGARI DIGIT ONE	✓
-२	±						Nd		digit				U+0968: DEVANAGARI DIGIT TWO	✓
-३	±						Nd		digit				U+0969: DEVANAGARI DIGIT THREE	✓
-४	±						Nd		digit				U+096A: DEVANAGARI DIGIT FOUR	✓
-५	±						Nd		digit				U+096B: DEVANAGARI DIGIT FIVE	✓
-६	±						Nd		digit				U+096C: DEVANAGARI DIGIT SIX	✓
-७	±						Nd		digit				U+096D: DEVANAGARI DIGIT SEVEN	✓
-८	±						Nd		digit				U+096E: DEVANAGARI DIGIT EIGHT	✓
-९	±						Nd		digit				U+096F: DEVANAGARI DIGIT NINE	✓
-														✓
-০	±	•					Nd		digit			sɪfar	U+09E6: BENGALI DIGIT ZERO	✓
-১	±	•					Nd		digit			æk	U+09E7: BENGALI DIGIT ONE	✓
-২	±	•					Nd		digit			dui	U+09E8: BENGALI DIGIT TWO	✓
-৩	±	•					Nd		digit			tin	U+09E9: BENGALI DIGIT THREE	✓
-৪	±	•					Nd		digit			car	U+09EA: BENGALI DIGIT FOUR	✓
-৫	±	•					Nd		digit			pãc	U+09EB: BENGALI DIGIT FIVE	✓
-৬	±	•					Nd		digit			chɔy	U+09EC: BENGALI DIGIT SIX	✓
-৭	±	•					Nd		digit			sat	U+09ED: BENGALI DIGIT SEVEN	✓
-৮	±	•					Nd		digit			aʈ	U+09EE: BENGALI DIGIT EIGHT	✓
-৯	±	•					Nd		digit			nɔy	U+09EF: BENGALI DIGIT NINE	✓
-														✓
-୦	±						Nd		digit		ଶୂନ୍ୟ	śūnya	U+0B66: ORIYA DIGIT ZERO	✓
-୧	±						Nd		digit		ଏକ	eka	U+0B67: ORIYA DIGIT ONE	✓
-୨	±						Nd		digit		ଦୁଇ	dui	U+0B68: ORIYA DIGIT TWO	✓
-୩	±						Nd		digit		ତିନି	tini	U+0B69: ORIYA DIGIT THREE	✓
-୪	±						Nd		digit		ଚାରି	cāri	U+0B6A: ORIYA DIGIT FOUR	✓
-୫	±						Nd		digit		ପାଞ୍ଚ	pāñca	U+0B6B: ORIYA DIGIT FIVE	✓
-୬	±						Nd		digit		ଛଅ	cha'a	U+0B6C: ORIYA DIGIT SIX	✓
-୭	±						Nd		digit		ସାତ	sāta	U+0B6D: ORIYA DIGIT SEVEN	✓
-୮	±						Nd		digit		ଆଠ	āṭha	U+0B6E: ORIYA DIGIT EIGHT	✓
-୯	±						Nd		digit		ନଅ	na'a	U+0B6F: ORIYA DIGIT NINE	✓
-														✓
-\u200C	=			ₓ			Cf		zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	✓
-\u200D	=			₊			Cf		zero-width joiner				U+200D: ZERO WIDTH JOINER	✓
-◌									placeholder				U+25CC: DOTTED CIRCLE	✓
-`														
-														
-latinPanel = 'ʔ ɖ ḍ ɡ t͡ʃ d͡ʒ ɲ ŋ ɳ ṇ ɽ ʈ ú ː ̃'														
-														
-var cols = {														
-key: 1,														
-ipaLoc: 2,														
-ipaPlus: 3,														
-transLoc: 4,														
-transcription: 5,														
-kbd: 6,														
-class: 7,														
-status: 8,														
-typeLoc: 9,														
-statusLoc: 10,														
-														
-nnameLoc: 11,														
-nameLoc: 12,														
-ucsName: 13,														
-block: 14,														
-														
-//othertranscriptions: [[13, 'Deva']]														
-}														
+var spreadsheet = `nagm-unr	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	ucs name	block	sortas
+-	-			-	-				hyphen		\u{1E4E4}\u{1E4E0}\u{1E4E4}\u{1E4DC}	et͡ʃeɖ	U+2010: HYPHEN	✓	
+–	-			–	–		Pd		en dash				U+2013: EN DASH	✓	
+—	-			—	—		Pd		em dash				U+2014: EM DASH	✓	
+\u{1E4ED}	:	ː		ː		x	Mn​		vowel lengthener		\u{1E4DD}\u{1E4D0}\u{1E4D3}\u{1E4D0}\u{1E4E3}	toyor	U+1E4ED: NAG MUNDARI SIGN TOYOR	✓	
+\u{1E4EC}	[	̃		˜	̃	v	Mn​		nasalisation mark		\u{1E4E7}\u{1E4DF}\u{1E4EC}\u{1E4DE}\u{1E4D0}\u{1E4E3}	mũhor	U+1E4EC: NAG MUNDARI SIGN MUHOR	✓	
+.	.			.	.		Po		full stop				U+002E: FULL STOP	✓	P ASCII
+,	,			,̣	,		Po		comma				U+002C: COMMA	✓	P ASCII
+;	;			;̣	;		Po		semicolon				U+003B: SEMICOLON	✓	P ASCII
+:	:			:	:		Po		colon				U+003A: COLON	✓	P ASCII
+!	!			!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK	✓	P ASCII
+?	?			?	?		Po		question mark				U+003F: QUESTION MARK	✓	P ASCII
+…	.			…	…		Po		ellipsis				U+2026: HORIZONTAL ELLIPSIS	✓	
+(	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS	✓	P ASCII
+[	(			[	[		Ps		bracket				U+005B: LEFT SQUARE BRACKET	✓	P ASCII
+)	)			)	)		Pe		parenthesis				U+0029: RIGHT PARENTHESIS	✓	P ASCII
+]	)			]	]		Pe		bracket				U+005D: RIGHT SQUARE BRACKET	✓	P ASCII
+%	%			%			Po		percentage mark				U+0025: PERCENT SIGN	✓	P ASCII
+‰	%			‰			Po		per mille mark				U+2030: PER MILLE SIGN	✓	
+“	<			“	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	✓	
+‘	<			‘	‘				quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	✓	
+”	>			”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	✓	
+’	>			’	’				quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	✓	
+\u{1E4EF}	/			ˑ			Mn​		nukta		\u{1E4DB}\u{1E4DF}\u{1E4DD}\u{1E4DF}\u{1E4D9}	sutu\u{294}	U+1E4EF: NAG MUNDARI SIGN SUTUH	✓	Mn
+\u{1E4D9}	}	ʔ		ʔ		H	Lo		consonant				U+1E4D9: NAG MUNDARI LETTER AH	✓	Lo stop
+\u{1E4EB}	}	ˀ		ˀ		z	Lm		checked final mark		\u{1E4D0}\u{1E4D6}\u{1E4D0}\u{1E4E1}	ojod	U+1E4EB: NAG MUNDARI SIGN OJOD	✓	Lm 
+\u{1E4D5}	a	a		a	a	a	Lo		vowel				U+1E4D5: NAG MUNDARI LETTER A	✓	Lo vowel
+\u{1E4D7}	b	b		b	b	b	Lo		consonant				U+1E4D7: NAG MUNDARI LETTER AB	✓	Lo stop
+\u{1E4E0}	c	t͡ʃ		c	c	c	Lo		consonant				U+1E4E0: NAG MUNDARI LETTER UC	✓	Lo affricate
+\u{1E4E1}	d	d		d	d	D	Lo		consonant				U+1E4E1: NAG MUNDARI LETTER UD	✓	Lo stop
+\u{1E4DC}	D	ɖ		ɖ	ḍ	d	Lo		consonant				U+1E4DC: NAG MUNDARI LETTER IDD	✓	Lo stop
+\u{1E4E4}	e	e		e	e	e	Lo		vowel				U+1E4E4: NAG MUNDARI LETTER E	✓	Lo vowel
+\u{1E4E6}	g	ɡ		g	g	g	Lo		consonant				U+1E4E6: NAG MUNDARI LETTER EG	✓	Lo stop
+\u{1E4DE}	h	h		h	h	h	Lo		consonant				U+1E4DE: NAG MUNDARI LETTER IH	✓	Lo fricative
+\u{1E4DA}	i	i		i	i	i	Lo		vowel				U+1E4DA: NAG MUNDARI LETTER I	✓	Lo vowel
+\u{1E4D6}	j	d͡ʒ		ʤ	j	j	Lo		consonant				U+1E4D6: NAG MUNDARI LETTER AJ	✓	Lo affricate
+\u{1E4E2}	k	k		k	k	k	Lo		consonant				U+1E4E2: NAG MUNDARI LETTER UK	✓	Lo stop
+\u{1E4D2}	l	l		l	l	l	Lo		consonant				U+1E4D2: NAG MUNDARI LETTER OL	✓	Lo other
+\u{1E4E7}	m	m		m	m	m	Lo		consonant				U+1E4E7: NAG MUNDARI LETTER EM	✓	Lo nasal
+\u{1E4E8}	n	n		n	n	n	Lo		consonant				U+1E4E8: NAG MUNDARI LETTER EN	✓	Lo nasal
+\u{1E4D8}	n	ɲ		ɲ	ny	J	Lo		consonant				U+1E4D8: NAG MUNDARI LETTER ANY	✓	Lo nasal
+\u{1E4D4}	N	ŋ		ŋ	ng	G	Lo		consonant				U+1E4D4: NAG MUNDARI LETTER ONG	✓	Lo nasal
+\u{1E4E5}	N	ɳ		ŋ	ṇ	N	Lo		consonant				U+1E4E5: NAG MUNDARI LETTER ENN	✓	Lo nasal
+\u{1E4D0}	o	o		o	o	o	Lo		vowel				U+1E4D0: NAG MUNDARI LETTER O	✓	Lo vowel
+\u{1E4D1}	p	p		p	p	p	Lo		consonant				U+1E4D1: NAG MUNDARI LETTER OP	✓	Lo stop
+\u{1E4E3}	r	r		r	r	r	Lo		consonant				U+1E4E3: NAG MUNDARI LETTER UR	✓	Lo other
+\u{1E4EA}	R	ɽ		ɽ	r	L	Lo		consonant				U+1E4EA: NAG MUNDARI LETTER ELL	✓	Lo other
+\u{1E4DB}	s	s		s	s	s	Lo		consonant				U+1E4DB: NAG MUNDARI LETTER IS	✓	Lo fricative
+\u{1E4DD}	t	t		t	t	T	Lo		consonant				U+1E4DD: NAG MUNDARI LETTER IT	✓	Lo stop
+\u{1E4E9}	T	ʈ		ʈ	tt	t	Lo		consonant				U+1E4E9: NAG MUNDARI LETTER ETT	✓	Lo stop
+\u{1E4DF}	u	u		u	u	u	Lo		vowel				U+1E4DF: NAG MUNDARI LETTER U	✓	Lo vowel
+\u{1E4EE}	w	w ʷ		ʷ		w	Mn​		labialisation mark		\u{1E4DA}\u{1E4E2}\u{1E4DA}\u{1E4E3}	ikir	U+1E4EE: NAG MUNDARI SIGN IKIR	✓	Mn​
+\u{1E4D3}	y	j		y	y	y	Lo		consonant				U+1E4D3: NAG MUNDARI LETTER OY	✓	Lo stop
+														✓	
+\u{1E4F0}	±	0		0̣	0	0	Nd		digit				U+1E4F0: NAG MUNDARI DIGIT ZERO	✓	Nd
+\u{1E4F1}	±	1		1̣	1	1	Nd		digit				U+1E4F1: NAG MUNDARI DIGIT ONE	✓	Nd
+\u{1E4F2}	±	2		2̣	2	2	Nd		digit				U+1E4F2: NAG MUNDARI DIGIT TWO	✓	Nd
+\u{1E4F3}	±	3		3̣	3	3	Nd		digit				U+1E4F3: NAG MUNDARI DIGIT THREE	✓	Nd
+\u{1E4F4}	±	4		4̣	4	4	Nd		digit				U+1E4F4: NAG MUNDARI DIGIT FOUR	✓	Nd
+\u{1E4F5}	±	5		5̣	5	5	Nd		digit				U+1E4F5: NAG MUNDARI DIGIT FIVE	✓	Nd
+\u{1E4F6}	±	6		6̣	6	6	Nd		digit				U+1E4F6: NAG MUNDARI DIGIT SIX	✓	Nd
+\u{1E4F7}	±	7		7̣	7	7	Nd		digit				U+1E4F7: NAG MUNDARI DIGIT SEVEN	✓	Nd
+\u{1E4F8}	±	8		8̣	8	8	Nd		digit				U+1E4F8: NAG MUNDARI DIGIT EIGHT	✓	Nd
+\u{1E4F9}	±	9		9̣	9	9	Nd		digit				U+1E4F9: NAG MUNDARI DIGIT NINE	✓	Nd
+														✓	
+०	±						Nd		digit				U+0966: DEVANAGARI DIGIT ZERO	✓	
+१	±						Nd		digit				U+0967: DEVANAGARI DIGIT ONE	✓	
+२	±						Nd		digit				U+0968: DEVANAGARI DIGIT TWO	✓	
+३	±						Nd		digit				U+0969: DEVANAGARI DIGIT THREE	✓	
+४	±						Nd		digit				U+096A: DEVANAGARI DIGIT FOUR	✓	
+५	±						Nd		digit				U+096B: DEVANAGARI DIGIT FIVE	✓	
+६	±						Nd		digit				U+096C: DEVANAGARI DIGIT SIX	✓	
+७	±						Nd		digit				U+096D: DEVANAGARI DIGIT SEVEN	✓	
+८	±						Nd		digit				U+096E: DEVANAGARI DIGIT EIGHT	✓	
+९	±						Nd		digit				U+096F: DEVANAGARI DIGIT NINE	✓	
+														✓	
+০	±	•					Nd		digit			sɪfar	U+09E6: BENGALI DIGIT ZERO	✓	
+১	±	•					Nd		digit			æk	U+09E7: BENGALI DIGIT ONE	✓	
+২	±	•					Nd		digit			dui	U+09E8: BENGALI DIGIT TWO	✓	
+৩	±	•					Nd		digit			tin	U+09E9: BENGALI DIGIT THREE	✓	
+৪	±	•					Nd		digit			car	U+09EA: BENGALI DIGIT FOUR	✓	
+৫	±	•					Nd		digit			pãc	U+09EB: BENGALI DIGIT FIVE	✓	
+৬	±	•					Nd		digit			chɔy	U+09EC: BENGALI DIGIT SIX	✓	
+৭	±	•					Nd		digit			sat	U+09ED: BENGALI DIGIT SEVEN	✓	
+৮	±	•					Nd		digit			aʈ	U+09EE: BENGALI DIGIT EIGHT	✓	
+৯	±	•					Nd		digit			nɔy	U+09EF: BENGALI DIGIT NINE	✓	
+														✓	
+୦	±						Nd		digit		ଶୂନ୍ୟ	śūnya	U+0B66: ORIYA DIGIT ZERO	✓	
+୧	±						Nd		digit		ଏକ	eka	U+0B67: ORIYA DIGIT ONE	✓	
+୨	±						Nd		digit		ଦୁଇ	dui	U+0B68: ORIYA DIGIT TWO	✓	
+୩	±						Nd		digit		ତିନି	tini	U+0B69: ORIYA DIGIT THREE	✓	
+୪	±						Nd		digit		ଚାରି	cāri	U+0B6A: ORIYA DIGIT FOUR	✓	
+୫	±						Nd		digit		ପାଞ୍ଚ	pāñca	U+0B6B: ORIYA DIGIT FIVE	✓	
+୬	±						Nd		digit		ଛଅ	cha'a	U+0B6C: ORIYA DIGIT SIX	✓	
+୭	±						Nd		digit		ସାତ	sāta	U+0B6D: ORIYA DIGIT SEVEN	✓	
+୮	±						Nd		digit		ଆଠ	āṭha	U+0B6E: ORIYA DIGIT EIGHT	✓	
+୯	±						Nd		digit		ନଅ	na'a	U+0B6F: ORIYA DIGIT NINE	✓	
+														✓	
+◌									placeholder				U+25CC: DOTTED CIRCLE	✓	
+`															
+															
+latinPanel = 'ʔ ɖ ḍ ɡ t͡ʃ d͡ʒ ɲ ŋ ɳ ṇ ɽ ʈ ú ː ̃'															
+															
+var cols = {															
+key: 1,															
+ipaLoc: 2,															
+ipaPlus: 3,															
+transLoc: 4,															
+transcription: 5,															
+kbd: 6,															
+class: 7,															
+status: 8,															
+typeLoc: 9,															
+statusLoc: 10,															
+															
+nnameLoc: 11,															
+nameLoc: 12,															
+ucsName: 13,															
+block: 14,															
+															
+//othertranscriptions: [[13, 'Deva']]															
+}															
