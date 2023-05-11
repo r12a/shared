0808f4d46eeb9e49f9d5886960eeb4bb64b1c841
r12a <ishida@w3.org>
2023-05-11 06:29:56 +0100

    bali-ban: Add u and ? etc to match spreadsheet and index

--- scriptdb/bali-ban.js
+++ scriptdb/bali-ban.js
@@ -1,21 +1,21 @@
 var spreadsheet = `bali-ban	key	ipa	ipa+	translit	tulisan bali	kbd	class	status	type	usage	dvowel	ivowel	shape	native name	latin name	ucs name	LOC	block
-‒		a			a				inherent vowel									
+‒		a			a			?	inherent vowel									
 ᭠	-			-			Po		line-breaking hyphen						pameneng	U+1B60: BALINESE PAMENENG		✓
--	-			-					hyphen							U+2010: HYPHEN	-	
-‑	-			‑			Pd		non-breaking hyphen							U+2011: NON-BREAKING HYPHEN	‑	
-–	-			–			Pd		en dash							U+2013: EN DASH	–	
-—	-			—			Pd		em dash							U+2014: EM DASH	—	
+-	-			-				?	hyphen							U+2010: HYPHEN	-	
+‑	-			‑			Pd	?	non-breaking hyphen							U+2011: NON-BREAKING HYPHEN	‑	
+–	-			–			Pd	?	en dash							U+2013: EN DASH	–	
+—	-			—			Pd	?	em dash							U+2014: EM DASH	—	
 ᭞	,			,		,	Po		~comma						carik	U+1B5E: BALINESE CARIK SIKI	,	✓
-;	;			;			Po		semicolon							U+003B: SEMICOLON	;	
+;	;			;			Po	?	semicolon							U+003B: SEMICOLON	;	
 ᭝	:			:			Po		colon						carik pamungkah	U+1B5D: BALINESE CARIK PAMUNGKAH	:	✓
-!	!			!			Po		exclamation mark							U+0021: EXCLAMATION MARK	!	
-?	?			?			Po		question mark							U+003F: QUESTION MARK	?	
+!	!			!			Po	?	exclamation mark							U+0021: EXCLAMATION MARK	!	
+?	?			?			Po	?	question mark							U+003F: QUESTION MARK	?	
 ᭟	.			.		.	Po		~full stop						carik pareren	U+1B5F: BALINESE CARIK PAREREN	.	✓
-…	.			…			Po		ellipsis							U+2026: HORIZONTAL ELLIPSIS	…	
-(	(			(			Ps		parenthesis							U+0028: LEFT PARENTHESIS	(	
-[	(			[			Ps		bracket							U+005B: LEFT SQUARE BRACKET	[	
-)	)			)			Pe		parenthesis							U+0029: RIGHT PARENTHESIS	)	
-]	)			]			Pe		bracket							U+005D: RIGHT SQUARE BRACKET	]	
+…	.			…			Po	?	ellipsis							U+2026: HORIZONTAL ELLIPSIS	…	
+(	(			(			Ps	?	parenthesis							U+0028: LEFT PARENTHESIS	(	
+[	(			[			Ps	?	bracket							U+005B: LEFT SQUARE BRACKET	[	
+)	)			)			Pe	?	parenthesis							U+0029: RIGHT PARENTHESIS	)	
+]	)			]			Pe	?	bracket							U+005D: RIGHT SQUARE BRACKET	]	
 \u200B	]			␣			Cf		zero-width space							U+200B: ZERO WIDTH SPACE		
 ᬗ᬴	{	ʔ		ŋˑ					consonant+nukta							U+1B17 U+1B34: BALINESE LETTER NGA, SIGN REREKAN		
 ᭚	@			ᵖ>		<	Po		text start symbol						panten	U+1B5A: BALINESE PANTI		✓
@@ -23,8 +23,8 @@ var spreadsheet = `bali-ban	key	ipa	ipa+	translit	tulisan bali	kbd	class	status	
 ᭜	@			°			Po		punctuation							U+1B5C: BALINESE WINDU		✓
 ᭄	*			͞		/	Mc		vowel-killer						adegadeg	U+1B44: BALINESE ADEG ADEG		✓
 ᬴	/			ˑ			Mn		nukta						rerekan	U+1B34: BALINESE SIGN REREKAN		✓
-%	%			%			Po		percentage mark							U+0025: PERCENT SIGN		
-‰	%			‰			Po		per mille mark							U+2030: PER MILLE SIGN		
+%	%			%			Po	?	percentage mark							U+0025: PERCENT SIGN		
+‰	%			‰			Po	?	per mille mark							U+2030: PER MILLE SIGN		
 ᭐	±	0		0̣	0	0	Nd		digit						bindu/windu	U+1B50: BALINESE DIGIT ZERO	0	✓
 ᭑	±	1		1̣	1	1	Nd		digit						siki/besik	U+1B51: BALINESE DIGIT ONE	1	✓
 ᭒	±	2		2̣	2	2	Nd		digit						kalih/dua	U+1B52: BALINESE DIGIT TWO	2	✓
@@ -35,17 +35,17 @@ var spreadsheet = `bali-ban	key	ipa	ipa+	translit	tulisan bali	kbd	class	status	
 ᭗	±	7		7̣	7	7	Nd		digit						pitu	U+1B57: BALINESE DIGIT SEVEN	7	✓
 ᭘	±	8		8̣	8	8	Nd		digit						kutus	U+1B58: BALINESE DIGIT EIGHT	8	✓
 ᭙	±	9		9̣	9	9	Nd		digit						sanga/sia	U+1B59: BALINESE DIGIT NINE	9	✓
-«	<			«			Pi		quotation mark							U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	“	
-‹	<			‹			Pi		quotation mark							U+2018: LEFT SINGLE QUOTATION MARK		
-“	<			“			Pi		quotation mark							U+201C: LEFT DOUBLE QUOTATION MARK	“	
-\‘	<			\‘			Pi		quotation mark							U+2018: LEFT SINGLE QUOTATION MARK		
+«	<			«			Pi	?	quotation mark							U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	“	
+‹	<			‹			Pi	?	quotation mark							U+2018: LEFT SINGLE QUOTATION MARK		
+“	<			“			Pi	?	quotation mark							U+201C: LEFT DOUBLE QUOTATION MARK	“	
+‘	<			\‘			Pi	?	quotation mark							U+2018: LEFT SINGLE QUOTATION MARK		
 \u200C	=			ₓ			Cf		zwnj							U+200C: ZERO WIDTH NON-JOINER		
-\u200D	=			₊			Cf		zwj							U+200D: ZERO WIDTH JOINER		
-\u034F	=			ᶜᵍʲ			Mn		combining grapheme joiner							U+034F: COMBINING GRAPHEME JOINER		
-»	>			»			Pf		quotation mark							U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	”	
-›	>			›			Pf		quotation mark							U+2019: RIGHT SINGLE QUOTATION MARK		
-”	>			”			Pf		quotation mark							U+201D: RIGHT DOUBLE QUOTATION MARK	”	
-\’	>			\’			Pf		quotation mark							U+2019: RIGHT SINGLE QUOTATION MARK		
+\u200D	=			₊			Cf	?	zwj							U+200D: ZERO WIDTH JOINER		
+\u034F	=			ᶜᵍʲ			Mn	?	combining grapheme joiner							U+034F: COMBINING GRAPHEME JOINER		
+»	>			»			Pf	?	quotation mark							U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	”	
+›	>			›			Pf	?	quotation mark							U+2019: RIGHT SINGLE QUOTATION MARK		
+”	>			”			Pf	?	quotation mark							U+201D: RIGHT DOUBLE QUOTATION MARK	”	
+’	>			\’			Pf	?	quotation mark							U+2019: RIGHT SINGLE QUOTATION MARK		
 ᬵ	a	ɑː		ɑ̄	a	a	Mc		vowel sign			ᬆ			tedong	U+1B35: BALINESE VOWEL SIGN TEDUNG	ā	✓
 ᬿ	a	aːi		aʲ	ai	E	Mc		vowel sign			ᬐ			taleng-repa	U+1B3F: BALINESE VOWEL SIGN TALING REPA	ai	✓
 ᭁ	a	aːu		aʷ		O	Mc		vowel sign			ᬒ			taleng-repa tedong	U+1B41: BALINESE VOWEL SIGN TALING REPA TEDUNG	au	✓
@@ -57,12 +57,12 @@ var spreadsheet = `bali-ban	key	ipa	ipa+	translit	tulisan bali	kbd	class	status	
 \u1B11\u1B35	A	aːu		ạʷ					compound vowel						ᬒ	U+1B11 U+1B35: BALINESE LETTER OKARA, VOWEL SIGN TEDUNG		
 \u1B3F\u1B35	a	aːu		aʷ					compound vowel						ᭁ	U+1B3F U+1B35: BALINESE VOWEL SIGN TALING REPA, VOWEL SIGN TEDUNG		
 ᬩ	b	b	a ə	b	b	b	Lo		consonant				ᬩ ᭄ᬩ			U+1B29: BALINESE LETTER BA	b	✓
-ᬪ	B	b	a ə	ḅ	b bh	v	Lo	u	kawi consonant	kawi			ᬪ ᭄ᬪ		ba kembang	U+1B2A: BALINESE LETTER BA KEMBANG	b bh	✓
+ᬪ	B	b	a ə	ḅ	b bh	v	Lo		kawi consonant	kawi			ᬪ ᭄ᬪ		ba kembang	U+1B2A: BALINESE LETTER BA KEMBANG	b bh	✓
 ᬘ	c	t͡ʃ	a ə	ʧ	c	c	Lo		consonant				ᬘ ᭄ᬘ			U+1B18: BALINESE LETTER CA	c	✓
 ᬙ	C	t͡ʃ	a ə	T͡Ʃ			Lo	r	honorific consonant	only found in subjoined form			ᬙ ᭄ᬙ			U+1B19: BALINESE LETTER CA LACA	c ch	✓
 ᭄ᬙ	C	t͡ʃ	a ə	͞T͡Ʃ				r	honorific consonant	very rare			ᬙ ᭄ᬙ			U+1B44 U+1B19: BALINESE ADEG ADEG, LETTER CA LACA	c ch	
 ᬤ	d	d	a ə	d	d	d	Lo		consonant				ᬤ ᭄ᬤ			U+1B24: BALINESE LETTER DA	d	✓
-ᬥ	D	d	a ə	ḍ	d ḍ dh	p	Lo	u	kawi consonant	kawi			ᬥ ᭄ᬥ		da madu	U+1B25: BALINESE LETTER DA MADU	dh	✓
+ᬥ	D	d	a ə	ḍ	d ḍ dh	p	Lo		kawi consonant	kawi			ᬥ ᭄ᬥ		da madu	U+1B25: BALINESE LETTER DA MADU	dh	✓
 ᬟ	D	d	a ə	D			Lo		honorific consonant				ᬟ ᭄ᬟ			U+1B1F: BALINESE LETTER DA MURDA ALPAPRANA	ḍ	✓
 ᬠ	D	d	a ə	Ḍ			Lo		honorific consonant				ᬠ ᭄ᬠ			U+1B20: BALINESE LETTER DA MURDA MAHAPRANA		✓
 ᬤ᬴	D	ɖ		dˑ					consonant+nukta							U+1B24 U+1B34: BALINESE LETTER DA, SIGN REREKAN		
@@ -73,7 +73,7 @@ var spreadsheet = `bali-ban	key	ipa	ipa+	translit	tulisan bali	kbd	class	status	
 \u1B42\u1B35	e	əː		ə̄					compound vowel						ᭃ	U+1B42 U+1B35: BALINESE VOWEL SIGN PEPET, VOWEL SIGN TEDUNG		
 ᬧ᬴	F	f		pˑ	f				consonant+nukta							U+1B27 U+1B34: BALINESE LETTER PA, SIGN REREKAN	f	
 ᬕ	g	ɡ	a ə	g	g	g	Lo		consonant				ᬕ ᭄ᬕ			U+1B15: BALINESE LETTER GA	g	✓
-ᬖ	G	ɡ	a ə	g̣	gh		Lo	u	kawi consonant	kawi			ᬖ ᭄ᬖ		ga gora	U+1B16: BALINESE LETTER GA GORA	g gh	✓
+ᬖ	G	ɡ	a ə	g̣	gh		Lo		kawi consonant	kawi			ᬖ ᭄ᬖ		ga gora	U+1B16: BALINESE LETTER GA GORA	g gh	✓
 ᬳ	h	h ∅	a ə	h	h ∅	h	Lo		consonant				ᬳ ᭄ᬳ			U+1B33: BALINESE LETTER HA	h ∅	✓
 ᬄ	H	-h		h̽	h		Mc		final consonant						bisah	U+1B04: BALINESE SIGN BISAH	h	✓
 ᬳ᬴	H	ħ		hˑ					consonant+nukta							U+1B33 U+1B34: BALINESE LETTER HA, SIGN REREKAN	h	
@@ -100,12 +100,12 @@ var spreadsheet = `bali-ban	key	ipa	ipa+	translit	tulisan bali	kbd	class	status	
 ᬜ	n	ɲ	a ə	ɲ	nya	x	Lo		consonant				ᬜ ᭄ᬜ			U+1B1C: BALINESE LETTER NYA	ñ	✓
 ᬂ	N	-ŋ		ŋ̽	ng	]	Mn		final consonant						cecek	U+1B02: BALINESE SIGN CECEK	ng	✓
 ᬁ	N	-ŋ		ŋ̇̽		}	Mn	i	final consonant						ulu candra	U+1B01: BALINESE SIGN ULU CANDRA		✓
-ᬡ	N	n	a ə	ṇ	n ṇ	z	Lo	u	kawi consonant	kawi			ᬡ ᭄ᬡ		na rambat	U+1B21: BALINESE LETTER NA RAMBAT	n ṇ	✓
+ᬡ	N	n	a ə	ṇ	n ṇ	z	Lo		kawi consonant	kawi			ᬡ ᭄ᬡ		na rambat	U+1B21: BALINESE LETTER NA RAMBAT	n ṇ	✓
 ᭀ	o	o ɔ		o	o	o	Mc		vowel sign			ᬑ			taleng tedong	U+1B40: BALINESE VOWEL SIGN TALING TEDUNG	o	✓
 ᬑ	O	O Ɔ		ọ	o	)	Lo		independent vowel		ᭀ				o kara	U+1B11: BALINESE LETTER OKARA	O	✓
 \u1B3E\u1B35	o	o ɔ		o					compound vowel						ᭀ	U+1B3E U+1B35: BALINESE VOWEL SIGN TALING, VOWEL SIGN TEDUNG		
 ᬧ	p	p	a ə	p	p	p	Lo		consonant				ᬧ ᭄ᬧ			U+1B27: BALINESE LETTER PA	p	✓
-ᬨ	P	p	a ə	p̣	p ph		Lo	u	kawi consonant	kawi			ᬨ ᭄ᬨ		pa kapal	U+1B28: BALINESE LETTER PA KAPAL	p ph	✓
+ᬨ	P	p	a ə	p̣	p ph		Lo		kawi consonant	kawi			ᬨ ᭄ᬨ		pa kapal	U+1B28: BALINESE LETTER PA KAPAL	p ph	✓
 ᬭ	r	r	a ə	r	r	r	Lo		consonant				ᬭ ᭄ᬭ		ra / guwung	U+1B2D: BALINESE LETTER RA	r	✓
 ᬃ	R	-r		r̽	r	[	Mn		final consonant						surang	U+1B03: BALINESE SIGN SURANG	r	✓
 ᬋ	r	rə		r̥̣	rě		Lo		vocalic		ᬺ		ᬋ ᭄ᬋ		ra repa	U+1B0B: BALINESE LETTER RA REPA	rĕ	✓
@@ -115,12 +115,12 @@ var spreadsheet = `bali-ban	key	ipa	ipa+	translit	tulisan bali	kbd	class	status	
 \u1B0B\u1B35	R	rəː		r̥̣̄					compound vowel						ᬌ	U+1B0B U+1B35: BALINESE LETTER RA REPA, VOWEL SIGN TEDUNG		
 \u1B3A\u1B35	R	rəː		r̥̄					compound vowel						ᬻ	U+1B3A U+1B35: BALINESE VOWEL SIGN RA REPA, VOWEL SIGN TEDUNG		
 ᬲ	s	s	a ə	s	s	s	Lo		consonant				ᬲ ᭄ᬲ			U+1B32: BALINESE LETTER SA	s	✓
-ᬰ	S	s	a ə	ṣ	s sy	%	Lo	u	kawi consonant	kawi			ᬰ ᭄ᬰ		sa saga	U+1B30: BALINESE LETTER SA SAGA	s ṣ	✓
-ᬱ	S	s	a ə	s̱	s ṣ	$	Lo	u	kawi consonant	kawi			ᬱ ᭄ᬱ		sa sapa	U+1B31: BALINESE LETTER SA SAPA	s ś	✓
+ᬰ	S	s	a ə	ṣ	s sy	%	Lo		kawi consonant	kawi			ᬰ ᭄ᬰ		sa saga	U+1B30: BALINESE LETTER SA SAGA	s ṣ	✓
+ᬱ	S	s	a ə	s̱	s ṣ	$	Lo		kawi consonant	kawi			ᬱ ᭄ᬱ		sa sapa	U+1B31: BALINESE LETTER SA SAPA	s ś	✓
 ᬢ	t	t	a ə	t	t	t	Lo		consonant				ᬢ ᭄ᬢ			U+1B22: BALINESE LETTER TA	t	✓
 ᬞ	T¶	t	a ə	T	t		Lo		honorific consonant				ᬞ ᭄ᬞ			U+1B1E: BALINESE LETTER TA MURDA MAHAPRANA		✓
-ᬝ	T	t	a ə	ṭ	t ṭ		Lo	u	kawi consonant	kawi			ᬝ ᭄ᬝ		ta latik	U+1B1D: BALINESE LETTER TA LATIK	ṭ	✓
-ᬣ	T	t	a ə	ṯ	t th	@	Lo	u	kawi consonant	kawi			ᬣ ᭄ᬣ		ta tawa	U+1B23: BALINESE LETTER TA TAWA	t th	✓
+ᬝ	T	t	a ə	ṭ	t ṭ		Lo		kawi consonant	kawi			ᬝ ᭄ᬝ		ta latik	U+1B1D: BALINESE LETTER TA LATIK	ṭ	✓
+ᬣ	T	t	a ə	ṯ	t th	@	Lo		kawi consonant	kawi			ᬣ ᭄ᬣ		ta tawa	U+1B23: BALINESE LETTER TA TAWA	t th	✓
 ᬸ	u	u		u	u	u	Mn		vowel sign			ᬉ			suku	U+1B38: BALINESE VOWEL SIGN SUKU	u	✓
 ᬹ	u	uː		ū	u	U	Mn		vowel sign			ᬊ			suku-ilut	U+1B39: BALINESE VOWEL SIGN SUKU ILUT	ū	✓
 ᬉ	U	U		ụ	u	*	Lo		independent vowel		ᬸ				u kara	U+1B09: BALINESE LETTER UKARA	U	✓
@@ -155,19 +155,15 @@ var spreadsheet = `bali-ban	key	ipa	ipa+	translit	tulisan bali	kbd	class	status	
 ᬳᬿ		aːi		haʲ					medial standalone vowel							U+1B33 U+1B3F: BALINESE LETTER HA VOWEL SIGN TALING REPA	ai	
 ᬳᭁ		aːu		haʷ					medial standalone vowel							U+1B33 U+1B41: BALINESE LETTER HA VOWEL SIGN TALING REPA TEDUNG	au	
 																		
-ʼ				ʼ			Lm		apostrophe							U+02BC: MODIFIER LETTER APOSTROPHE	ʼ	
-§				§			Po		section sign							U+00A7: SECTION SIGN	§	
-†				†			Po		dagger							U+2020: DAGGER	†	
-‡				‡			Po		double dagger							U+2021: DOUBLE DAGGER	‡	
-′				′			Po		prime							U+2032: PRIME	′	
-″				″			Po		double prime							U+2033: DOUBLE PRIME	″	
-																		
+ʼ				ʼ			Lm	?	apostrophe							U+02BC: MODIFIER LETTER APOSTROPHE	ʼ	
+§				§			Po	?	section sign							U+00A7: SECTION SIGN	§	
+†				†			Po	?	dagger							U+2020: DAGGER	†	
+‡				‡			Po	?	double dagger							U+2021: DOUBLE DAGGER	‡	
+′				′			Po	?	prime							U+2032: PRIME	′	
+″				″			Po	?	double prime							U+2033: DOUBLE PRIME	″	
 `																		
 																		
-																		
 latinPanel = 'ā ɑː aːi aːu ḍ d͡ʒ ə əː ĕ ɛ iː ī ɲ ñ ŋ ∅ ö ɔ t͡ʃ uː ū'																		
-																		
-																		
 																		
 var cols = {																		
 key: 1,																		
@@ -192,4 +188,3 @@ block: 18,
 																		
 othertranscriptions: [[4, 'Tulisan'], [17, 'LOC']]																		
 }																		
-																		
