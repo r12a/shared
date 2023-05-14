f40a97967087bb208dbc624aa692140e6cde1916
r12a <ishida@w3.org>
2023-05-14 19:12:47 +0100

    scriptdb/gu: Mark possibles

--- scriptdb/gujr-gu.js
+++ scriptdb/gujr-gu.js
@@ -1,25 +1,25 @@
 var spreadsheet = `gujr-gu	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	dvowel	ivowel	native name	latin name	ucs name	block
 -	-			-	-		Pd		hyphen						U+2010: HYPHEN	
-‑	-			‑	‑		Pd		non-breaking hyphen						U+2011: NON-BREAKING HYPHEN	
-–	-			–	–		Pd		en dash						U+2013: EN DASH	
-—	-			—	—		Pd		em dash						U+2014: EM DASH	
+‑	-			‑	‑		Pd	?	non-breaking hyphen						U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd	?	en dash						U+2013: EN DASH	
+—	-			—	—		Pd	?	em dash						U+2014: EM DASH	
 ,	,			,	,		Po		comma						U+002C: COMMA	
 ;	;			;	;		Po		semicolon						U+003B: SEMICOLON	
 :	:			:	:		Po		colon						U+003A: COLON	
 !	!			!	!		Po		exclamation mark						U+0021: EXCLAMATION MARK	
 ?	?			?	?		Po		question mark						U+003F: QUESTION MARK	
 .	.			.	.		Po		full stop						U+002E: FULL STOP	
-…	.			…	…		Po		ellipsis						U+2026: HORIZONTAL ELLIPSIS	
+…	.			…	…		Po	?	ellipsis						U+2026: HORIZONTAL ELLIPSIS	
 (	(			(	(		Ps		parenthesis						U+0028: LEFT PARENTHESIS	
-[	(			[	[		Pe		bracket						U+005B: LEFT SQUARE BRACKET	
+[	(			[	[		Pe	?	bracket						U+005B: LEFT SQUARE BRACKET	
 )	)			)	)		Ps		parenthesis						U+0029: RIGHT PARENTHESIS	
-]	)			]	]		Pe		bracket						U+005D: RIGHT SQUARE BRACKET	
+]	)			]	]		Pe	?	bracket						U+005D: RIGHT SQUARE BRACKET	
 ં	[	ä		˜	ṁ	x	Mn		vowel nasalisation					anusvār	U+0A82: GUJARATI SIGN ANUSVARA	✓
 ્	*			͞		d	Mn		virama						U+0ACD: GUJARATI SIGN VIRAMA	✓
 ઼	/			ˑ		]	Mn		nukta						U+0ABC: GUJARATI SIGN NUKTA	✓
-%	%			%			Po		percentage mark						U+0025: PERCENT SIGN	
-‰	%			‰			Po		per mille mark						U+2030: PER MILLE SIGN	
-૰	+			¨			Po	i	abbreviation marker						U+0AF0: GUJARATI ABBREVIATION SIGN	✓
+%	%			%			Po	?	percentage mark						U+0025: PERCENT SIGN	
+‰	%			‰			Po	?	per mille mark						U+2030: PER MILLE SIGN	
+૰	+			¨			Po		abbreviation marker						U+0AF0: GUJARATI ABBREVIATION SIGN	✓
 ૧	±			1̣	1	1	Nd		digit					ekado or ek	U+0AE7: GUJARATI DIGIT ONE	✓
 ૨	±			2̣	2	2	Nd		digit					bagado or bay	U+0AE8: GUJARATI DIGIT TWO	✓
 ૩	±			3̣	3	3	Nd		digit					tragado or tran	U+0AE9: GUJARATI DIGIT THREE	✓
@@ -30,19 +30,19 @@ var spreadsheet = `gujr-gu	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 ૮	±			8̣	8	8	Nd		digit					āṭhado or āanth	U+0AEE: GUJARATI DIGIT EIGHT	✓
 ૯	±			9̣	9	9	Nd		digit					navado or nav	U+0AEF: GUJARATI DIGIT NINE	✓
 ૦	±			0̣	0	0	Nd		digit					mīṇḍu or shunya	U+0AE6: GUJARATI DIGIT ZERO	✓
-«	<			«	“		Pi		quotation mark						U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
-‹	<			‹			Pi		quotation mark						U+2018: LEFT SINGLE QUOTATION MARK	
+«	<			«	“		Pi	?	quotation mark						U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	<			‹			Pi	?	quotation mark						U+2018: LEFT SINGLE QUOTATION MARK	
 “	<			“	“		Pi		quotation mark						U+201C: LEFT DOUBLE QUOTATION MARK	
-\‘	<			\‘	\‘		Pi		quotation mark						U+2018: LEFT SINGLE QUOTATION MARK	
-\u200B	=			␣			Cf		zero-width space						U+200B: ZERO WIDTH SPACE	
+‘	<			\‘	\‘		Pi		quotation mark						U+2018: LEFT SINGLE QUOTATION MARK	
+\u200B	=			␣			Cf	?	zero-width space						U+200B: ZERO WIDTH SPACE	
 \u200C	=			ᶻʷⁿʲ			Cf		zero-width non-joiner						U+200C: ZERO WIDTH NON-JOINER	
 \u200D	=			ᶻʷʲ			Cf		zero-width joiner						U+200D: ZERO WIDTH JOINER	
-\u034F	=			ᶜᵍʲ			Mn		combining grapheme joiner						U+034F: COMBINING GRAPHEME JOINER	
-\u2060	=			ʷʲ			Cf		word joiner						U+2060 WORD JOINER	
-»	>			»	”		Pf		quotation mark						U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
-›	>			›			Pf		quotation mark						U+2019: RIGHT SINGLE QUOTATION MARK	
+\u034F	=			ᶜᵍʲ			Mn	?	combining grapheme joiner						U+034F: COMBINING GRAPHEME JOINER	
+\u2060	=			ʷʲ			Cf	?	word joiner						U+2060 WORD JOINER	
+»	>			»	”		Pf	?	quotation mark						U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+›	>			›			Pf	?	quotation mark						U+2019: RIGHT SINGLE QUOTATION MARK	
 ”	>			”	”		Pf		quotation mark						U+201D: RIGHT DOUBLE QUOTATION MARK	
-\’	>			\’	\’		Pf		quotation mark						U+2019: RIGHT SINGLE QUOTATION MARK	
+’	>			\’	\’		Pf		quotation mark						U+2019: RIGHT SINGLE QUOTATION MARK	
 ।	|			|	.		Po	i	section divider						U+0964: DEVANAGARI DANDA	
 ॥	|			¶			Po	i	section divider						U+0965: DEVANAGARI DOUBLE DANDA	
 આ	A	ɑ		ạ̄	ā	E	Lo		independent vowel		ા				U+0A86: GUJARATI LETTER AA	✓
@@ -82,10 +82,10 @@ var spreadsheet = `gujr-gu	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 ક્ષ	k	kʃ		k͓ʃ		&			consonant						U+0A95 U+0ACD U+0AB7: GUJARATI LETTER KA, SIGN VIRAMA, LETTER SSA	
 લ	l	l	ə	l	l	n	Lo		consonant						U+0AB2: GUJARATI LETTER LA	✓
 ળ	l	ɭ	ə	ɭ	ḷ	N	Lo		consonant						U+0AB3: GUJARATI LETTER LLA	✓
-ૢ	L			l̥	l̥		Mn		dependent vocalic			ઌ			U+0AE2: GUJARATI VOWEL SIGN VOCALIC L	✓
-ૣ	L			l̥̄	l̥̄		Mn		dependent vocalic			ૡ			U+0AE3: GUJARATI VOWEL SIGN VOCALIC LL	✓
-ઌ	L			l̥̣	l̥		Lo		independent vocalic		ૢ				U+0A8C: GUJARATI LETTER VOCALIC L	✓
-ૡ	L			l̥̣̄	l̥̄		Lo		independent vocalic		ૣ				U+0AE1: GUJARATI LETTER VOCALIC LL	✓
+ૢ	L			l̥	l̥		Mn	?	dependent vocalic			ઌ			U+0AE2: GUJARATI VOWEL SIGN VOCALIC L	✓
+ૣ	L			l̥̄	l̥̄		Mn	?	dependent vocalic			ૡ			U+0AE3: GUJARATI VOWEL SIGN VOCALIC LL	✓
+ઌ	L			l̥̣	l̥		Lo	?	independent vocalic		ૢ				U+0A8C: GUJARATI LETTER VOCALIC L	✓
+ૡ	L			l̥̣̄	l̥̄		Lo	?	independent vocalic		ૣ				U+0AE1: GUJARATI LETTER VOCALIC LL	✓
 મ	m	m	ə	m	m	c	Lo		consonant						U+0AAE: GUJARATI LETTER MA	✓
 ઙ	N	ŋ	ə	ŋ	ṅ	U	Lo		consonant						U+0A99: GUJARATI LETTER NGA	✓
 ઞ	n	ɲ	ə	ɲ	ñ	}	Lo		consonant						U+0A9E: GUJARATI LETTER NYA	✓
@@ -115,29 +115,25 @@ var spreadsheet = `gujr-gu	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 ય	y	j	ə	y	y	/	Lo		consonant						U+0AAF: GUJARATI LETTER YA	✓
 																
 																
-ઁ				[	m̐	X	Mn								U+0A81: GUJARATI SIGN CANDRABINDU	✓
+ઁ				[	m̐	X	Mn	?							U+0A81: GUJARATI SIGN CANDRABINDU	✓
 ૐ							Lo								U+0AD0: GUJARATI OM	✓
 ૱							Sc								U+0AF1: GUJARATI RUPEE SIGN	✓
 																
-ʼ				ʼ	ʼ		Lm		apostrophe						U+02BC: MODIFIER LETTER APOSTROPHE	
-§				§	§		Po		section sign						U+00A7: SECTION SIGN	
-†				†	†		Po		dagger						U+2020: DAGGER	
-‡				‡	‡		Po		double dagger						U+2021: DOUBLE DAGGER	
-′				′	′		Po		prime						U+2032: PRIME	
-″				″	″		Po		double prime						U+2033: DOUBLE PRIME	
+ʼ				ʼ	ʼ		Lm	?	apostrophe						U+02BC: MODIFIER LETTER APOSTROPHE	
+§				§	§		Po	?	section sign						U+00A7: SECTION SIGN	
+†				†	†		Po	?	dagger						U+2020: DAGGER	
+‡				‡	‡		Po	?	double dagger						U+2021: DOUBLE DAGGER	
+′				′	′		Po	?	prime						U+2032: PRIME	
+″				″	″		Po	?	double prime						U+2033: DOUBLE PRIME	
 																
 ્ર				–		#			for keyboard						U+0ACD U+0AB0: GUJARATI SIGN VIRAMA, GUJARATI LETTER RA	
 ર્				–		$			for keyboard						U+0AB0 U+0ACD: GUJARATI LETTER RA, SIGN VIRAMA	
 ત્ર				–		^			for keyboard						U+0AA4 U+0ACD U+0AB0: GUJARATI LETTER TA, SIGN VIRAMA, LETTER RA	
 શ્ર				–		*			for keyboard						U+0AB6 U+0ACD U+0AB0: GUJARATI LETTER SHA, SIGN VIRAMA, LETTER RA	
 ઽ				–											U+0ABD: GUJARATI SIGN AVAGRAHA	
-																
 `																
 																
-																
 latinPanel = 'ä ɑ ā æ bʰ ɕ ʤ ʤʰ ɖ ḍ ɖʰ dʰ ə ɛ ē ê ɡʰ ɦ ḥ ɨ ī kʰ l̥ l̥̄ m̐ ṁ ŋ ṅ ɲ ñ ɳ ṇ ɔ ō ô r̥ r̥̄ ʃ ś ṣ ʧ ʧʰ ʈ ṭ ʈʰ tʰ ū ʋ'																
-																
-																
 																
 var cols = {																
 key: 1,																
@@ -161,4 +157,3 @@ block: 16,
 																
 othertranscriptions: [[5, 'ISO 19515']]																
 }																
-																
