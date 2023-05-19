a5fa3c84f96d03a42bb6e899db4e387aa12cec94
r12a <ishida@w3.org>
2023-05-19 07:00:48 +0100

    scriptdb/lis: Mark possibles

--- scriptdb/lisu.js
+++ scriptdb/lisu.js
@@ -1,42 +1,42 @@
 var spreadsheet = `lisu-lis	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	ucs name	block
 -	-			-	-		Pd		hyphen				U+2010: HYPHEN	
-‑	-			‑	‑		Pd		non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
-–	-			–	–		Pd		en dash				U+2013: EN DASH	
-—	-			—	—		Pd		em dash				U+2014: EM DASH	
-,	,			,	,		Po		comma				U+002C: COMMA	
+‑	-			‑	‑		Pd	?	non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd	?	en dash				U+2013: EN DASH	
+—	-			—	—		Pd	?	em dash				U+2014: EM DASH	
+,	,			,	,		Po	?	comma				U+002C: COMMA	
 ꓾	,			⹁	⹁	I	Po		comma				U+A4FE: LISU PUNCTUATION COMMA	✓
 ;	;			;	;		Po	i	semicolon	biblical texts			U+003B: SEMICOLON	
 :	:			:	:		Po	i	colon	biblical texts			U+003A: COLON	
 !	!			!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK	
 ?	?			?	?		Po		question mark				U+003F: QUESTION MARK	
-.	.			.	.		Po		full stop				U+002E: FULL STOP	
+.	.			.	.		Po	?	full stop				U+002E: FULL STOP	
 ꓿	.			|	.	O	Lu		full stop				U+A4FF: LISU PUNCTUATION FULL STOP	✓
 …	.			…	…		Po		ellipsis				U+2026: HORIZONTAL ELLIPSIS	
 (	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS	
 《	(			[	[		Ps		bracket				U+300A: LEFT DOUBLE ANGLE BRACKET	
 )	)			)	)		Pe		parenthesis				U+0029: RIGHT PARENTHESIS	
 》	)			]	]		Pe		bracket				U+300B: RIGHT DOUBLE ANGLE BRACKET	
-%	%			%			Po		percentage mark				U+0025: PERCENT SIGN	
-‰	%			‰			Po		per mille mark				U+2030: PER MILLE SIGN	
+%	%			%			Po	?	percentage mark				U+0025: PERCENT SIGN	
+‰	%			‰			Po	?	per mille mark				U+2030: PER MILLE SIGN	
 ꓸ	^	˥˥		¹	˥˥		Lm		pitch 55 tone mark				U+A4F8: LISU LETTER TONE MYA TI	✓
 ꓹ	^	˧˥		²	˧˥		Lm		pitch 35 tone mark				U+A4F9: LISU LETTER TONE NA PO	✓
 ꓺ	^	˦˦		³	˦˦	H	Lm		pitch 44 tone mark				U+A4FA: LISU LETTER TONE MYA CYA	✓
 ꓻ	^	˧˧		⁴	˧˧	Y	Lm		pitch 33 tone mark				U+A4FB: LISU LETTER TONE MYA BO	✓
 ꓼ	^	˦˨		⁵	˦˨		Lm		pitch 42 tone mark				U+A4FC: LISU LETTER TONE MYA NA	✓
 ꓽ	^	˧˩		⁶	˧˩		Lm		pitch 31 tone mark				U+A4FD: LISU LETTER TONE MYA JEU	✓
-«	<			«	“		Pi		quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
-‹	<			‹			Pi		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
-“	<			“	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
-\‘	<			\‘	\‘		Pi		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
-\u200B	=			␣			Cf		zero-width space				U+200B: ZERO WIDTH SPACE	
-\u200C	=			ᶻʷⁿʲ			Cf		zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
-\u200D	=			ᶻʷʲ			Cf		zero-width joiner				U+200D: ZERO WIDTH JOINER	
-\u034F	=			ᶜᵍʲ			Mn		combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER	
-\u2060	=			ʷʲ			Cf		word joiner				U+2060 WORD JOINER	
-»	>			»	”		Pf		quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
-›	>			›			Pf		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
-”	>			”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
-\’	>			\’	\’		Pf		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+«	<			«	“		Pi	?	quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	<			‹			Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+“	<			“	“		Pi	?	quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
+‘	<			\‘	\‘		Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+\u200B	=			␣			Cf	?	zero-width space				U+200B: ZERO WIDTH SPACE	
+\u200C	=			ᶻʷⁿʲ			Cf	?	zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=			ᶻʷʲ			Cf	?	zero-width joiner				U+200D: ZERO WIDTH JOINER	
+\u034F	=			ᶜᵍʲ			Mn	?	combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER	
+\u2060	=			ʷʲ			Cf	?	word joiner				U+2060 WORD JOINER	
+»	>			»	”		Pf	?	quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+›	>			›			Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+”	>			”	”		Pf	?	quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
+’	>			\’	\’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
 														
 														
 ꓮ	a	ɑ		ɑ	ɑ	a	Lo		vowel				U+A4EE: LISU LETTER A	✓
@@ -84,12 +84,12 @@ var spreadsheet = `lisu-lis	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 														
 														
 ꓹꓼ				²⁵		W			tone mark				U+A4F9 U+A4FC: LISU LETTER TONE NA PO, LISU LETTER TONE MYA NA	
-ʼ				ʼ	ʼ		Lm		apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE	
-§				§	§		Po		section sign				U+00A7: SECTION SIGN	
-†				†	†		Po		dagger				U+2020: DAGGER	
-‡				‡	‡		Po		double dagger				U+2021: DOUBLE DAGGER	
-′				′	′		Po		prime				U+2032: PRIME	
-″				″	″		Po		double prime				U+2033: DOUBLE PRIME	
+ʼ				ʼ	ʼ		Lm	?	apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE	
+§				§	§		Po	?	section sign				U+00A7: SECTION SIGN	
+†				†	†		Po	?	dagger				U+2020: DAGGER	
+‡				‡	‡		Po	?	double dagger				U+2021: DOUBLE DAGGER	
+′				′	′		Po	?	prime				U+2032: PRIME	
+″				″	″		Po	?	double prime				U+2033: DOUBLE PRIME	
 														
 ꓸꓼ				–		S			for keyboard				U+A4F8 U+A4FC: LISU LETTER TONE MYA TI, LETTER TONE MYA NA	
 ꓸꓽ				–		,			for keyboard				U+A4F8 U+A4FD: LISU LETTER TONE MYA TI, LETTER TONE MYA JEU	
@@ -98,13 +98,9 @@ var spreadsheet = `lisu-lis	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 ꓺꓼ				–		>			for keyboard				U+A4FA U+A4FC: LISU LETTER TONE MYA CYA, LETTER TONE MYA NA	
 ꓻꓽ				–		/			for keyboard				U+A4FB U+A4FD: LISU LETTER TONE MYA BO, LETTER TONE MYA JEU	
 ꓻꓼ				–		?			for keyboard				U+A4FB U+A4FC: LISU LETTER TONE MYA BO, LETTER TONE MYA NA	
-														
 `														
 														
-														
 latinPanel = '˥˥ ˧˥ ˦˦ ˧˧ ˦˨ ˧˩ ɑ ä ɕ d͡z ʥ ɛ ə ɦ kʰ ŋ ø pʰ tʰ t͡s ʦʰ ʨ ʨʰ ɯ ɣ ʒ ʑ'														
-														
-														
 														
 var cols = {														
 key: 1,														
@@ -123,6 +119,5 @@ nameLoc: 12,
 ucsName: 13,														
 block: 14,														
 														
-othertranscriptions: [[5, 'Latin']]														
+//othertranscriptions: [[5, 'LOC']]														
 }														
-														
