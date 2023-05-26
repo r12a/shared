c69f200432aa009c5a46156a09ff609fa9c01a4c
r12a <ishida@w3.org>
2023-05-26 18:22:30 +0100

    scriptdb/mymr-my: Reverse the translit and transc columns

--- scriptdb/mymr-my.js
+++ scriptdb/mymr-my.js
@@ -1,23 +1,23 @@
-var spreadsheet = `mymr-my	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	dvowel	ivowel	native name	latin name	ucs name	block
+var spreadsheet = `mymr-my	key	ipa	ipa+	transc	translit	kbd	class	status	type	usage	dvowel	ivowel	native name	latin name	ucs name	block
 \u0020	 						Zs		space						U+0020 SPACE	
-\u2060	=			ʷʲ			Cf		word joiner						U+2060 WORD JOINER	
-\u200B	]			ᶻʷˢᵖ	␣		Cf		zero width space						U+200B: ZERO WIDTH SPACE	
+\u2060	=				ʷʲ		Cf		word joiner						U+2060 WORD JOINER	
+\u200B	]			␣	ᶻʷˢᵖ		Cf		zero width space						U+200B: ZERO WIDTH SPACE	
 ္	*			͓	͓	F	Mn		virama						U+1039: MYANMAR SIGN VIRAMA	✓
 ်	\\			ˣ	ˣ	f	Mn		asat				အသတ်	əθaʔ	U+103A: MYANMAR SIGN ASAT	✓
-့	^			.	¹	h	Mn		tone mark						U+1037: MYANMAR SIGN DOT BELOW	✓
-း	^			:	²	;	Mc		tone mark						U+1038: MYANMAR SIGN VISARGA	✓
+့	^			¹	.	h	Mn		tone mark						U+1037: MYANMAR SIGN DOT BELOW	✓
+း	^			²	:	;	Mc		tone mark						U+1038: MYANMAR SIGN VISARGA	✓
 “	<			“	“		Pi		quotation mark						U+201C: LEFT DOUBLE QUOTATION MARK	
 ‘	<			\‘	\‘		Pi	?	quotation mark						U+2018: LEFT SINGLE QUOTATION MARK	
-\u200C	=			ᶻʷⁿʲ	ₓ		Cf		zero width non-joiner						U+200C: ZERO WIDTH NON-JOINER	
-\u200D	=			ᶻʷʲ	₊		Cf	?	zero width joiner						U+200D: ZERO WIDTH JOINER	
+\u200C	=			ₓ	ᶻʷⁿʲ		Cf		zero width non-joiner						U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=			₊	ᶻʷʲ		Cf	?	zero width joiner						U+200D: ZERO WIDTH JOINER	
 ”	>			”	”		Pf		quotation mark						U+201D: RIGHT DOUBLE QUOTATION MARK	
 ’	>			\’	\’		Pf	?	quotation mark						U+2019: RIGHT SINGLE QUOTATION MARK	
-၊	|			၊	,	,	Po		comma				ပုဒ်ဖြတ်, ပုဒ်ကလေး, ပုဒ်ထီး, or တစ်ချောင်းပုဒ်		U+104A: MYANMAR SIGN LITTLE SECTION	✓
-။	|			။	.	.	Po		full stop				ပုဒ်ကြီး, ပုဒ်မ, or နှစ်ချောင်းပုဒ်		U+104B: MYANMAR SIGN SECTION	✓
-၌	$	n̥aiʔ		hnai.	(loc)	Y	Po		symbol					ɛʔkʰəjanʰaɪʔ	U+104C: MYANMAR SYMBOL LOCATIVE	✓
-၍	$	jwɛ		rwe	(sub)	I	Po		symbol					ɛʔkʰəjajwɛ	U+104D: MYANMAR SYMBOL COMPLETED	✓
-၎	$			la. kaung	(afore)	R	Po		symbol					ɛʔkʰəjajwɛ	U+104E: MYANMAR SYMBOL AFOREMENTIONED	✓
-၏	$	í		e	(gen)	\	Po		symbol					ɛʔkʰəjaí	U+104F: MYANMAR SYMBOL GENITIVE	✓
+၊	|			,	၊	,	Po		comma				ပုဒ်ဖြတ်, ပုဒ်ကလေး, ပုဒ်ထီး, or တစ်ချောင်းပုဒ်		U+104A: MYANMAR SIGN LITTLE SECTION	✓
+။	|			.	။	.	Po		full stop				ပုဒ်ကြီး, ပုဒ်မ, or နှစ်ချောင်းပုဒ်		U+104B: MYANMAR SIGN SECTION	✓
+၌	$	n̥aiʔ		(loc)	hnai.	Y	Po		symbol					ɛʔkʰəjanʰaɪʔ	U+104C: MYANMAR SYMBOL LOCATIVE	✓
+၍	$	jwɛ		(sub)	rwe	I	Po		symbol					ɛʔkʰəjajwɛ	U+104D: MYANMAR SYMBOL COMPLETED	✓
+၎	$			(afore)	la. kaung	R	Po		symbol					ɛʔkʰəjajwɛ	U+104E: MYANMAR SYMBOL AFOREMENTIONED	✓
+၏	$	í		(gen)	e	\	Po		symbol					ɛʔkʰəjaí	U+104F: MYANMAR SYMBOL GENITIVE	✓
 ၀	0			0	0	0	Nd		digit						U+1040: MYANMAR DIGIT ZERO	✓
 ၁	1			1	1	1	Nd		digit						U+1041: MYANMAR DIGIT ONE	✓
 ၂	2			2	2	2	Nd		digit						U+1042: MYANMAR DIGIT TWO	✓
@@ -28,99 +28,99 @@ var spreadsheet = `mymr-my	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 ၇	7			7	7	7	Nd		digit						U+1047: MYANMAR DIGIT SEVEN	✓
 ၈	8			8	8	8	Nd		digit						U+1048: MYANMAR DIGIT EIGHT	✓
 ၉	9			9	9	9	Nd		digit						U+1049: MYANMAR DIGIT NINE	✓
-အ	a	ʔ	a ə	a	ʔ	t	Lo		consonant/indpendent vowel				အ	ʔa̰	U+1021: MYANMAR LETTER A	✓
+အ	a	ʔ	a ə	ʔ	a	t	Lo		consonant/indpendent vowel				အ	ʔa̰	U+1021: MYANMAR LETTER A	✓
 ာ	a	-a		a	a	m	Mc		vowel sign						U+102C: MYANMAR VOWEL SIGN AA	✓
-ါ	a	-a		a	ä	g	Mc		vowel sign						U+102B: MYANMAR VOWEL SIGN TALL AA	✓
-အာ	a	-a		a	ʔa		s		standalone vowel						U+1021 U+102C: MYANMAR LETTER A, VOWEL SIGN AA	
-အါ	a	-a		a	ʔä		s		standalone vowel						U+1021 U+102B: MYANMAR LETTER A, VOWEL SIGN TALL AA	
-ဗ	b	b v	a ə	ba.	b	A	Lo		consonant				ဗထက်ခြိုက်‌	ba̰ lɛʔ tɕʰaɪʔ	U+1017: MYANMAR LETTER BA	✓
-ဘ	b	b pʰ-	a ə	bha.	ḃ	b	Lo		consonant				ဘကုန်း	bˀa̰ ɡóʊɰ̃	U+1018: MYANMAR LETTER BHA	✓
-ဒ	d¶	d	a ə	da.	d	K	Lo		consonant				ဒထွေး	da̰ dwé	U+1012: MYANMAR LETTER DA	✓
-ဓ	d	d	a ə	dha.	d̊	L	Lo		consonant				ဓအောက်ခြိုက်	dˀa̰ ʔaʊʔ tɕʰaɪʔ	U+1013: MYANMAR LETTER DHA	✓
-ဍ	d	d	a ə	ḍa.	ḋ	!	Lo	a	consonant	mostly archaic			ဍရင်ကောက်	da̰ jɪ̀ɰ̃ ɡaʊʔ	U+100D: MYANMAR LETTER DDA	✓
-ဎ	d	d	a ə	ḍha.	d̈	§	Lo	a	consonant	mostly archaic			ဎရေမှုတ်	dˀa̰ jè m̥oʊʔ	U+100E: MYANMAR LETTER DDHA	✓
-ဧ	E	ʔè		ei:	ẹ	{	Lo		independent vowel		ေ				U+1027: MYANMAR LETTER E	✓
+ါ	a	-a		ä	a	g	Mc		vowel sign						U+102B: MYANMAR VOWEL SIGN TALL AA	✓
+အာ	a	-a		ʔa	a		s		standalone vowel						U+1021 U+102C: MYANMAR LETTER A, VOWEL SIGN AA	
+အါ	a	-a		ʔä	a		s		standalone vowel						U+1021 U+102B: MYANMAR LETTER A, VOWEL SIGN TALL AA	
+ဗ	b	b v	a ə	b	ba.	A	Lo		consonant				ဗထက်ခြိုက်‌	ba̰ lɛʔ tɕʰaɪʔ	U+1017: MYANMAR LETTER BA	✓
+ဘ	b	b pʰ-	a ə	ḃ	bha.	b	Lo		consonant				ဘကုန်း	bˀa̰ ɡóʊɰ̃	U+1018: MYANMAR LETTER BHA	✓
+ဒ	d¶	d	a ə	d	da.	K	Lo		consonant				ဒထွေး	da̰ dwé	U+1012: MYANMAR LETTER DA	✓
+ဓ	d	d	a ə	d̊	dha.	L	Lo		consonant				ဓအောက်ခြိုက်	dˀa̰ ʔaʊʔ tɕʰaɪʔ	U+1013: MYANMAR LETTER DHA	✓
+ဍ	d	d	a ə	ḋ	ḍa.	!	Lo	a	consonant	mostly archaic			ဍရင်ကောက်	da̰ jɪ̀ɰ̃ ɡaʊʔ	U+100D: MYANMAR LETTER DDA	✓
+ဎ	d	d	a ə	d̈	ḍha.	§	Lo	a	consonant	mostly archaic			ဎရေမှုတ်	dˀa̰ jè m̥oʊʔ	U+100E: MYANMAR LETTER DDHA	✓
+ဧ	E	ʔè		ẹ	ei:	{	Lo		independent vowel		ေ				U+1027: MYANMAR LETTER E	✓
 ေ	e	-e		e	e	a	Mc		vowel sign			ဧ			U+1031: MYANMAR VOWEL SIGN E	✓
-ဲ	e	-ɛ		ai:	ɛ	J	Mn		vowel sign						U+1032: MYANMAR VOWEL SIGN AI	✓
-အေ	e	-e		e	ʔe		s		standalone vowel						U+1021 U+1031: MYANMAR LETTER A, VOWEL SIGN E	
-အဲ	e	-ɛ		ai:	ʔɛ		s		standalone vowel						U+1021 U+1032: MYANMAR LETTER A, VOWEL SIGN AI	
-ယ်	e	-ɛ		ai	yˣ		s		vowel						U+101A U+103A: MYANMAR LETTER YA, SIGN ASAT	
-ဂ	g	ɡ	a ə	ga.	g	:	Lo		consonant				ဂငယ်	ɡa̰ ŋɛ̀	U+1002: MYANMAR LETTER GA	✓
-ဃ	g	ɡ	a ə	gha.	ġ	C	Lo	r	consonant				ဃကြီး	ɡˀa̰ dʑí	U+1003: MYANMAR LETTER GHA	✓
-ဟ	h	h	a ə	ha.	h	[	Lo		consonant				ဟ‌	ha̰	U+101F: MYANMAR LETTER HA	✓
-ှ	H	–̥ -ʃ		h	h̆	S	Mn		medial consonant						U+103E: MYANMAR CONSONANT SIGN MEDIAL HA	✓
-ဣ	I	ʔḭ		i.	ị	E	Lo		independent vowel		ိ				U+1023: MYANMAR LETTER I	✓
-ဤ	I	ʔì		i:	ị̈	T	Lo		independent vowel		ီ				U+1024: MYANMAR LETTER II	✓
-ိ	i	-i -eɪ-		i.	i	d	Mn		vowel sign			ဣ			U+102D: MYANMAR VOWEL SIGN I	✓
-ီ	i	-i		i	ï	D	Mn		vowel sign			ဤ			U+102E: MYANMAR VOWEL SIGN II	✓
-အိ	i	-i -eɪ-		i.	ʔi		s		standalone vowel						U+1021 U+102D: MYANMAR LETTER A, VOWEL SIGN I	
-အီ	i	-i		i	ʔï		s		standalone vowel						U+1021 U+102E: MYANMAR LETTER A, VOWEL SIGN II	
-က	k	k -ʔ ɡ	a ə	ka.	k	u	Lo		consonant				ကကြီး	ka̰ dʑí	U+1000: MYANMAR LETTER KA	✓
-ခ	k	kʰ ɡ	a ə	hka.	kʰ	c	Lo		consonant				ခကွေး	kʰa̰ ɡwé	U+1001: MYANMAR LETTER KHA	✓
-လ	l	l	a ə	la.	l	v	Lo		consonant				လငယ်	la̰ ŋɛ̀	U+101C: MYANMAR LETTER LA	✓
-ဠ	l	l	a ə	ḷ	l̇	V	Lo	r	consonant				ဠကြီး	la̰ dʑí	U+1020: MYANMAR LETTER LLA	✓
-္လ	L			la.	͓l		s		medial l						U+1039 U+101C: MYANMAR SIGN VIRAMA, LETTER LA	
-မ	m	m	a ə	ma.	m	r	Lo		consonant				မ	ma̰	U+1019: MYANMAR LETTER MA	✓
-န	n¶	n	a ə	na.	n	e	Lo		consonant				နငယ်	na̰ ŋɛ̀	U+1014: MYANMAR LETTER NA	✓
-ည	n	ɲ -∅	a ə	nya.	ɲ	n	Lo		consonant				ညကြီး	ɲa̰ dʑí	U+100A: MYANMAR LETTER NNYA	✓
-ဉ	n	ɲ -ɴ	a ə	nya.	ɲ̇	N	Lo		consonant				ညကလေး	ɲa̰ka̰lé	U+1009: MYANMAR LETTER NYA	✓
-ဏ	n	n	a ə	ṇa.	ṅ	p	Lo	a	consonant	mostly archaic			ဏကြီး	na̰ dʑí	U+100F: MYANMAR LETTER NNA	✓
-ံ	n	ɴ		m	n̽	H	Mn		final consonant						U+1036: MYANMAR SIGN ANUSVARA	✓
-င	N	ŋ	a ə	nga.	ŋ	i	Lo		consonant				င	ŋa̰	U+1004: MYANMAR LETTER NGA	✓
-င်္	N			ng	ŋˣ͓		s		kinzi						U+1004 U+103A U+1039: MYANMAR LETTER NGA, SIGN ASAT, SIGN VIRAMA	
-ဩ	O	ʔɔ́		au:	ɔ̣	]	Lo		independent vowel						U+1029: MYANMAR LETTER O	✓
-ဪ	O	ʔɔ̀		au	ɔ̣̇	}	Lo		independent vowel						U+102A: MYANMAR LETTER AU	✓
-ို	o	-o -aɪ-		ui	iu		s		vowel						U+102D U+102F: MYANMAR VOWEL SIGN I, VOWEL SIGN U	
-ော	o	-ɔ -aʊ-		au:	ea		s		vowel						U+1031 U+102C: MYANMAR VOWEL SIGN E, VOWEL SIGN AA	
-ေါ	o	-ɔ -aʊ-		au:	eä		s		vowel						U+1031 U+102B: MYANMAR VOWEL SIGN E, VOWEL SIGN TALL AA	
-ော်	o	-ɔ		au	eaˣ		s		vowel						U+1031 U+102C U+103A: MYANMAR VOWEL SIGN E, VOWEL SIGN AA, SIGN ASAT	
-ေါ်	o	-ɔ		au	eäˣ		s		vowel						U+1031 U+102B U+103A: MYANMAR VOWEL SIGN E, VOWEL SIGN TALL AA, SIGN ASAT	
-ပ	p	p -ʔ b	a ə	pa.	p	y	Lo		consonant				ပစောက်	pa̰ zaʊʔ	U+1015: MYANMAR LETTER PA	✓
-ဖ	p	pʰ f b	a ə	hpa.	pʰ	z	Lo		consonant				ဖဦးထုပ်	pʰa̰ ʔóʊʔ tʰoʊʔ	U+1016: MYANMAR LETTER PHA	✓
-သ	q	θ ð	a ə	sa.	θ	o	Lo		consonant				သ‌	θa̰	U+101E: MYANMAR LETTER SA	✓
-ဿ	q	θ	a ə	ssa.	θː	O	Lo		consonant ligature						U+103F: MYANMAR LETTER GREAT SA	✓
-ရ	r	j ɹ	a ə	ra.	r	&	Lo		consonant				ရကောက်‌	ja̰ ɡaʊʔ	U+101B: MYANMAR LETTER RA	✓
-ြ	R	j ʲ		r	j̆	j	Mc		medial consonant						U+103C: MYANMAR CONSONANT SIGN MEDIAL RA	✓
-စ	s	s -ʔ z	a ə	ca.	ṡ	p	Lo		consonant				စလုံး	sa̰ lóʊɰ̃	U+1005: MYANMAR LETTER CA	✓
-ဆ	s	sʰ z	a ə	hca.	ṡʰ	q	Lo		consonant				ဆလိမ်	sʰa̰ lèɪɰ̃	U+1006: MYANMAR LETTER CHA	✓
-တ	t¶	t -ʔ d	a ə	ta.	t	w	Lo		consonant				တဝမ်းပူ	ta̰ wʊ́ɰ̃ bù	U+1010: MYANMAR LETTER TA	✓
-ထ	t	tʰ d	a ə	hta.	tʰ	x	Lo		consonant				ထဆင်ထူး	tʰa̰ sʰɪ̀ɰ̃ dú	U+1011: MYANMAR LETTER THA	✓
-ဋ	t	t	a ə	ṭa.	ṫ	#	Lo	a	consonant	mostly archaic			ဋသန်လျင်းချိတ်	ta̰ təlɪ́ɰ̃ dʑeɪʔ	U+100B: MYANMAR LETTER TTA	✓
-ဌ	t	tʰ	a ə	hṭa.	ẗ	X	Lo	a	consonant	mostly archaic			ဌဝမ်းဘဲ	tʰa̰ wʊ́ɰ̃ bɛ́	U+100C: MYANMAR LETTER TTHA	✓
-ဥ	U	ʔṵ		u.	ụ	U	Lo		independent vowel		ု				U+1025: MYANMAR LETTER U	✓
-ဦ	U	ʔù		u	ụ̇	M	Lo		independent vowel		ူ				U+1026: MYANMAR LETTER UU	✓
-\u1025\u102E	U	ʔù		u	ụ̇		s		ဦ						U+1025 U+102E: MYANMAR LETTER U, VOWEL SIGN II	
-ု	u	-u -oʊ-		u.	u	k	Mn		vowel sign			ဥ			U+102F: MYANMAR VOWEL SIGN U	✓
-ူ	u	-u		u	u̇	l	Mn		vowel sign			ဦ			U+1030: MYANMAR VOWEL SIGN UU	✓
-အု	u	-u -oʊ-		u.	ʔu		s		standalone vowel						U+1021 U+102F: MYANMAR LETTER A, VOWEL SIGN U	
-အူ	u	-u		u	ʔu̇		s		standalone vowel						U+1021 U+1030: MYANMAR LETTER A, VOWEL SIGN UU	
-ဝ	w	w	a ə	wa.	w	W	Lo		consonant				ဝ‌	wa̰	U+101D: MYANMAR LETTER WA	✓
-ွ	W	w ʊ		w	w̆	G	Mn		medial consonant/vowel						U+103D: MYANMAR CONSONANT SIGN MEDIAL WA	✓
-ယ	y	j ɛ	a ə	ya.	y	B	Lo		consonant				ယပက်လက်	ja̰ pɛʔ lɛʔ	U+101A: MYANMAR LETTER YA	✓
-ျ	Y	j ʲ		y	y̆	s	Mc		medial consonant						U+103B: MYANMAR CONSONANT SIGN MEDIAL YA	✓
-ဇ	z	z	a ə	ja.	ż	Z	Lo		consonant				ဇကွဲ	za̰ ɡwɛ́	U+1007: MYANMAR LETTER JA	✓
-ဈ	z	z	a ə	jha.	z̈	Q	Lo	r	consonant				ဈမျဉ်းဆွဲ	zˀa̰ mjɪ̀ɰ̃ zwɛ́	U+1008: MYANMAR LETTER JHA	✓
+ဲ	e	-ɛ		ɛ	ai:	J	Mn		vowel sign						U+1032: MYANMAR VOWEL SIGN AI	✓
+အေ	e	-e		ʔe	e		s		standalone vowel						U+1021 U+1031: MYANMAR LETTER A, VOWEL SIGN E	
+အဲ	e	-ɛ		ʔɛ	ai:		s		standalone vowel						U+1021 U+1032: MYANMAR LETTER A, VOWEL SIGN AI	
+ယ်	e	-ɛ		yˣ	ai		s		vowel						U+101A U+103A: MYANMAR LETTER YA, SIGN ASAT	
+ဂ	g	ɡ	a ə	g	ga.	:	Lo		consonant				ဂငယ်	ɡa̰ ŋɛ̀	U+1002: MYANMAR LETTER GA	✓
+ဃ	g	ɡ	a ə	ġ	gha.	C	Lo	r	consonant				ဃကြီး	ɡˀa̰ dʑí	U+1003: MYANMAR LETTER GHA	✓
+ဟ	h	h	a ə	h	ha.	[	Lo		consonant				ဟ‌	ha̰	U+101F: MYANMAR LETTER HA	✓
+ှ	H	–̥ -ʃ		h̆	h	S	Mn		medial consonant						U+103E: MYANMAR CONSONANT SIGN MEDIAL HA	✓
+ဣ	I	ʔḭ		ị	i.	E	Lo		independent vowel		ိ				U+1023: MYANMAR LETTER I	✓
+ဤ	I	ʔì		ị̈	i:	T	Lo		independent vowel		ီ				U+1024: MYANMAR LETTER II	✓
+ိ	i	-i -eɪ-		i	i.	d	Mn		vowel sign			ဣ			U+102D: MYANMAR VOWEL SIGN I	✓
+ီ	i	-i		ï	i	D	Mn		vowel sign			ဤ			U+102E: MYANMAR VOWEL SIGN II	✓
+အိ	i	-i -eɪ-		ʔi	i.		s		standalone vowel						U+1021 U+102D: MYANMAR LETTER A, VOWEL SIGN I	
+အီ	i	-i		ʔï	i		s		standalone vowel						U+1021 U+102E: MYANMAR LETTER A, VOWEL SIGN II	
+က	k	k -ʔ ɡ	a ə	k	ka.	u	Lo		consonant				ကကြီး	ka̰ dʑí	U+1000: MYANMAR LETTER KA	✓
+ခ	k	kʰ ɡ	a ə	kʰ	hka.	c	Lo		consonant				ခကွေး	kʰa̰ ɡwé	U+1001: MYANMAR LETTER KHA	✓
+လ	l	l	a ə	l	la.	v	Lo		consonant				လငယ်	la̰ ŋɛ̀	U+101C: MYANMAR LETTER LA	✓
+ဠ	l	l	a ə	l̇	ḷ	V	Lo	r	consonant				ဠကြီး	la̰ dʑí	U+1020: MYANMAR LETTER LLA	✓
+္လ	L			͓l	la.		s		medial l						U+1039 U+101C: MYANMAR SIGN VIRAMA, LETTER LA	
+မ	m	m	a ə	m	ma.	r	Lo		consonant				မ	ma̰	U+1019: MYANMAR LETTER MA	✓
+န	n¶	n	a ə	n	na.	e	Lo		consonant				နငယ်	na̰ ŋɛ̀	U+1014: MYANMAR LETTER NA	✓
+ည	n	ɲ -∅	a ə	ɲ	nya.	n	Lo		consonant				ညကြီး	ɲa̰ dʑí	U+100A: MYANMAR LETTER NNYA	✓
+ဉ	n	ɲ -ɴ	a ə	ɲ̇	nya.	N	Lo		consonant				ညကလေး	ɲa̰ka̰lé	U+1009: MYANMAR LETTER NYA	✓
+ဏ	n	n	a ə	ṅ	ṇa.	p	Lo	a	consonant	mostly archaic			ဏကြီး	na̰ dʑí	U+100F: MYANMAR LETTER NNA	✓
+ံ	n	ɴ		n̽	m	H	Mn		final consonant						U+1036: MYANMAR SIGN ANUSVARA	✓
+င	N	ŋ	a ə	ŋ	nga.	i	Lo		consonant				င	ŋa̰	U+1004: MYANMAR LETTER NGA	✓
+င်္	N			ŋˣ͓	ng		s		kinzi						U+1004 U+103A U+1039: MYANMAR LETTER NGA, SIGN ASAT, SIGN VIRAMA	
+ဩ	O	ʔɔ́		ɔ̣	au:	]	Lo		independent vowel						U+1029: MYANMAR LETTER O	✓
+ဪ	O	ʔɔ̀		ɔ̣̇	au	}	Lo		independent vowel						U+102A: MYANMAR LETTER AU	✓
+ို	o	-o -aɪ-		iu	ui		s		vowel						U+102D U+102F: MYANMAR VOWEL SIGN I, VOWEL SIGN U	
+ော	o	-ɔ -aʊ-		ea	au:		s		vowel						U+1031 U+102C: MYANMAR VOWEL SIGN E, VOWEL SIGN AA	
+ေါ	o	-ɔ -aʊ-		eä	au:		s		vowel						U+1031 U+102B: MYANMAR VOWEL SIGN E, VOWEL SIGN TALL AA	
+ော်	o	-ɔ		eaˣ	au		s		vowel						U+1031 U+102C U+103A: MYANMAR VOWEL SIGN E, VOWEL SIGN AA, SIGN ASAT	
+ေါ်	o	-ɔ		eäˣ	au		s		vowel						U+1031 U+102B U+103A: MYANMAR VOWEL SIGN E, VOWEL SIGN TALL AA, SIGN ASAT	
+ပ	p	p -ʔ b	a ə	p	pa.	y	Lo		consonant				ပစောက်	pa̰ zaʊʔ	U+1015: MYANMAR LETTER PA	✓
+ဖ	p	pʰ f b	a ə	pʰ	hpa.	z	Lo		consonant				ဖဦးထုပ်	pʰa̰ ʔóʊʔ tʰoʊʔ	U+1016: MYANMAR LETTER PHA	✓
+သ	q	θ ð	a ə	θ	sa.	o	Lo		consonant				သ‌	θa̰	U+101E: MYANMAR LETTER SA	✓
+ဿ	q	θ	a ə	θː	ssa.	O	Lo		consonant ligature						U+103F: MYANMAR LETTER GREAT SA	✓
+ရ	r	j ɹ	a ə	r	ra.	&	Lo		consonant				ရကောက်‌	ja̰ ɡaʊʔ	U+101B: MYANMAR LETTER RA	✓
+ြ	R	j ʲ		j̆	r	j	Mc		medial consonant						U+103C: MYANMAR CONSONANT SIGN MEDIAL RA	✓
+စ	s	s -ʔ z	a ə	ṡ	ca.	p	Lo		consonant				စလုံး	sa̰ lóʊɰ̃	U+1005: MYANMAR LETTER CA	✓
+ဆ	s	sʰ z	a ə	ṡʰ	hca.	q	Lo		consonant				ဆလိမ်	sʰa̰ lèɪɰ̃	U+1006: MYANMAR LETTER CHA	✓
+တ	t¶	t -ʔ d	a ə	t	ta.	w	Lo		consonant				တဝမ်းပူ	ta̰ wʊ́ɰ̃ bù	U+1010: MYANMAR LETTER TA	✓
+ထ	t	tʰ d	a ə	tʰ	hta.	x	Lo		consonant				ထဆင်ထူး	tʰa̰ sʰɪ̀ɰ̃ dú	U+1011: MYANMAR LETTER THA	✓
+ဋ	t	t	a ə	ṫ	ṭa.	#	Lo	a	consonant	mostly archaic			ဋသန်လျင်းချိတ်	ta̰ təlɪ́ɰ̃ dʑeɪʔ	U+100B: MYANMAR LETTER TTA	✓
+ဌ	t	tʰ	a ə	ẗ	hṭa.	X	Lo	a	consonant	mostly archaic			ဌဝမ်းဘဲ	tʰa̰ wʊ́ɰ̃ bɛ́	U+100C: MYANMAR LETTER TTHA	✓
+ဥ	U	ʔṵ		ụ	u.	U	Lo		independent vowel		ု				U+1025: MYANMAR LETTER U	✓
+ဦ	U	ʔù		ụ̇	u	M	Lo		independent vowel		ူ				U+1026: MYANMAR LETTER UU	✓
+\u1025\u102E	U	ʔù		ụ̇	u		s		ဦ						U+1025 U+102E: MYANMAR LETTER U, VOWEL SIGN II	
+ု	u	-u -oʊ-		u	u.	k	Mn		vowel sign			ဥ			U+102F: MYANMAR VOWEL SIGN U	✓
+ူ	u	-u		u̇	u	l	Mn		vowel sign			ဦ			U+1030: MYANMAR VOWEL SIGN UU	✓
+အု	u	-u -oʊ-		ʔu	u.		s		standalone vowel						U+1021 U+102F: MYANMAR LETTER A, VOWEL SIGN U	
+အူ	u	-u		ʔu̇	u		s		standalone vowel						U+1021 U+1030: MYANMAR LETTER A, VOWEL SIGN UU	
+ဝ	w	w	a ə	w	wa.	W	Lo		consonant				ဝ‌	wa̰	U+101D: MYANMAR LETTER WA	✓
+ွ	W	w ʊ		w̆	w	G	Mn		medial consonant/vowel						U+103D: MYANMAR CONSONANT SIGN MEDIAL WA	✓
+ယ	y	j ɛ	a ə	y	ya.	B	Lo		consonant				ယပက်လက်	ja̰ pɛʔ lɛʔ	U+101A: MYANMAR LETTER YA	✓
+ျ	Y	j ʲ		y̆	y	s	Mc		medial consonant						U+103B: MYANMAR CONSONANT SIGN MEDIAL YA	✓
+ဇ	z	z	a ə	ż	ja.	Z	Lo		consonant				ဇကွဲ	za̰ ɡwɛ́	U+1007: MYANMAR LETTER JA	✓
+ဈ	z	z	a ə	z̈	jha.	Q	Lo	r	consonant				ဈမျဉ်းဆွဲ	zˀa̰ mjɪ̀ɰ̃ zwɛ́	U+1008: MYANMAR LETTER JHA	✓
 																
-ကြ		t͡ɕ		ky	kj̆		s								U+1000 U+103C: MYANMAR LETTER KA, CONSONANT SIGN MEDIAL RA	
-ခြ		t͡ɕʰ		hky	kʰj̆		s								U+1001 U+103C: MYANMAR LETTER KHA, CONSONANT SIGN MEDIAL RA	
-ဂြ		d͡ʑ		gy	gj̆		s								U+1002 U+103C: MYANMAR LETTER GA, CONSONANT SIGN MEDIAL RA	
-ငြ		ɲ		ngr	ŋj̆		s								U+1004 U+103C: MYANMAR LETTER NGA, CONSONANT SIGN MEDIAL RA	
-ြှ		–̥j		h–r	j̆h̆		s								U+103C U+103E: MYANMAR CONSONANT SIGN MEDIAL RA, CONSONANT SIGN MEDIAL HA	
-ြွ		–w		–tw	j̆w̆		s								U+103C U+103D: MYANMAR CONSONANT SIGN MEDIAL RA, CONSONANT SIGN MEDIAL WA	
-ြွှ		–̥w		h–rw	j̆w̆h̆		s								U+103C U+103D U+103E: MYANMAR CONSONANT SIGN MEDIAL RA, CONSONANT SIGN MEDIAL WA, CONSONANT SIGN MEDIAL HA	
+ကြ		t͡ɕ		kj̆	ky		s								U+1000 U+103C: MYANMAR LETTER KA, CONSONANT SIGN MEDIAL RA	
+ခြ		t͡ɕʰ		kʰj̆	hky		s								U+1001 U+103C: MYANMAR LETTER KHA, CONSONANT SIGN MEDIAL RA	
+ဂြ		d͡ʑ		gj̆	gy		s								U+1002 U+103C: MYANMAR LETTER GA, CONSONANT SIGN MEDIAL RA	
+ငြ		ɲ		ŋj̆	ngr		s								U+1004 U+103C: MYANMAR LETTER NGA, CONSONANT SIGN MEDIAL RA	
+ြှ		–̥j		j̆h̆	h–r		s								U+103C U+103E: MYANMAR CONSONANT SIGN MEDIAL RA, CONSONANT SIGN MEDIAL HA	
+ြွ		–w		j̆w̆	–tw		s								U+103C U+103D: MYANMAR CONSONANT SIGN MEDIAL RA, CONSONANT SIGN MEDIAL WA	
+ြွှ		–̥w		j̆w̆h̆	h–rw		s								U+103C U+103D U+103E: MYANMAR CONSONANT SIGN MEDIAL RA, CONSONANT SIGN MEDIAL WA, CONSONANT SIGN MEDIAL HA	
 																
-ကျ		t͡ɕ		ky	ky̆		s								U+1000 U+103B: MYANMAR LETTER KA, CONSONANT SIGN MEDIAL YA	
-ချ		t͡ɕʰ		hky	kʰy̆		s								U+1001 U+103B: MYANMAR LETTER KHA, CONSONANT SIGN MEDIAL YA	
-ဂျ		d͡ʑ		gy	gy̆		s								U+1002 U+103B: MYANMAR LETTER GA, CONSONANT SIGN MEDIAL YA	
-သျှ		ʃ		hsy	θy̆h̆		s								U+101E U+103B U+103E: MYANMAR LETTER SA, CONSONANT SIGN MEDIAL YA, CONSONANT SIGN MEDIAL HA	
-လျှ		ʃ		hly	ly̆h̆		s								U+101C U+103B U+103E: MYANMAR LETTER LA, CONSONANT SIGN MEDIAL YA, CONSONANT SIGN MEDIAL HA	
-ျှ		–̥j		h–y	y̆h̆		s								U+103B U+103E: MYANMAR CONSONANT SIGN MEDIAL YA, CONSONANT SIGN MEDIAL HA	
-ျွ		–w		–yw	y̆w̆		s								U+103B U+103D: MYANMAR CONSONANT SIGN MEDIAL YA, CONSONANT SIGN MEDIAL WA	
-ျွှ		–̥w		h–yw	y̆w̆h̆		s								U+103B U+103D U+103E: MYANMAR CONSONANT SIGN MEDIAL YA, CONSONANT SIGN MEDIAL WA, CONSONANT SIGN MEDIAL HA	
+ကျ		t͡ɕ		ky̆	ky		s								U+1000 U+103B: MYANMAR LETTER KA, CONSONANT SIGN MEDIAL YA	
+ချ		t͡ɕʰ		kʰy̆	hky		s								U+1001 U+103B: MYANMAR LETTER KHA, CONSONANT SIGN MEDIAL YA	
+ဂျ		d͡ʑ		gy̆	gy		s								U+1002 U+103B: MYANMAR LETTER GA, CONSONANT SIGN MEDIAL YA	
+သျှ		ʃ		θy̆h̆	hsy		s								U+101E U+103B U+103E: MYANMAR LETTER SA, CONSONANT SIGN MEDIAL YA, CONSONANT SIGN MEDIAL HA	
+လျှ		ʃ		ly̆h̆	hly		s								U+101C U+103B U+103E: MYANMAR LETTER LA, CONSONANT SIGN MEDIAL YA, CONSONANT SIGN MEDIAL HA	
+ျှ		–̥j		y̆h̆	h–y		s								U+103B U+103E: MYANMAR CONSONANT SIGN MEDIAL YA, CONSONANT SIGN MEDIAL HA	
+ျွ		–w		y̆w̆	–yw		s								U+103B U+103D: MYANMAR CONSONANT SIGN MEDIAL YA, CONSONANT SIGN MEDIAL WA	
+ျွှ		–̥w		y̆w̆h̆	h–yw		s								U+103B U+103D U+103E: MYANMAR CONSONANT SIGN MEDIAL YA, CONSONANT SIGN MEDIAL WA, CONSONANT SIGN MEDIAL HA	
 																
-ွှ		–̥w		h–w	w̆h̆		s								U+103D U+103E: MYANMAR CONSONANT SIGN MEDIAL WA, CONSONANT SIGN MEDIAL HA	
+ွှ		–̥w		w̆h̆	h–w		s								U+103D U+103E: MYANMAR CONSONANT SIGN MEDIAL WA, CONSONANT SIGN MEDIAL HA	
 																
-\u034F				ᶜᵍʲ				?	combining grapheme joiner						U+034F: COMBINING GRAPHEME JOINER	
+\u034F					ᶜᵍʲ			?	combining grapheme joiner						U+034F: COMBINING GRAPHEME JOINER	
 																
 ,	,			,	,	?	Po		comma						U+002C: COMMA	
 ;	;			;	;	?	Po		semicolon						U+003B: SEMICOLON	
@@ -140,12 +140,12 @@ var spreadsheet = `mymr-my	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 ′							Po	?	prime						U+2032: PRIME	
 ″							Po	?	double prime						U+2033: DOUBLE PRIME	
 																
-ၐ					–	±	Lo	u	for the keyboard						U+1050: MYANMAR LETTER SHA	✓
-ၒ					–	@	Lo	u	for the keyboard						U+1052: MYANMAR LETTER VOCALIC R	✓
-ၓ					–	$	Lo	u	for the keyboard						U+1053: MYANMAR LETTER VOCALIC RR	✓
-ၔ					–	%	Lo	u	for the keyboard						U+1054: MYANMAR LETTER VOCALIC L	✓
-ၕ					–	^	Lo	u	for the keyboard						U+1055: MYANMAR LETTER VOCALIC LL	✓
-ၑ					–	|	Lo	u	for the keyboard						U+1051: MYANMAR LETTER SSA	✓
+ၐ				–		±	Lo	u	for the keyboard						U+1050: MYANMAR LETTER SHA	✓
+ၒ				–		@	Lo	u	for the keyboard						U+1052: MYANMAR LETTER VOCALIC R	✓
+ၓ				–		$	Lo	u	for the keyboard						U+1053: MYANMAR LETTER VOCALIC RR	✓
+ၔ				–		%	Lo	u	for the keyboard						U+1054: MYANMAR LETTER VOCALIC L	✓
+ၕ				–		^	Lo	u	for the keyboard						U+1055: MYANMAR LETTER VOCALIC LL	✓
+ၑ				–		|	Lo	u	for the keyboard						U+1051: MYANMAR LETTER SSA	✓
 \u25CC								u	for display						Consonant	
 `																
 																
