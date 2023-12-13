3cd11b7082d87de6d8a05162834c251e0db2d83d
r12a <ishida@w3.org>
2023-12-13 17:44:09 +0000

    syrc-axp: Refine spreadsheet based on latest Tech Note.  Add new font.

--- scriptdb/syrc-axp.js
+++ scriptdb/syrc-axp.js
@@ -1,80 +1,88 @@
-var spreadsheet = `syrc-axp	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	shape	# value	native name	latin name	ucs name	block	transc name
-ـ	_			_			Lm								U+0640 ARABIC TATWEEL	✓	
-،	,			,			Po	?	comma						U+060C: ARABIC COMMA	✓	
-؛	;			;	;		Po	?	semi-colon						U+061B: ARABIC SEMICOLON	✓	
-؟	?			?	?		Po	?	question mark						U+061F: ARABIC QUESTION MARK	✓	
-̇	*			‐͑			Mn		disambiguation mark						U+0307: COMBINING DOT ABOVE	✓	
-ܥ	{	ʕ		ʕ	ʿ		Lo		consonant		ܥ ܥܥܥ	70	ܥܹܐ	ʕe	U+0725: SYRIAC LETTER E	✓	ʿē
-̈	&			˖	\u030B		Mn		plural marker				ܣܝܵܡܹ̈ܐ	syame	U+0308: COMBINING DIAERESIS	✓	syame
-\u200B	=			␣			Cf		zero-width space						U+200B: ZERO WIDTH SPACE		
-\u200C	=			ᶻʷⁿʲ			Cf		zero-width non-joiner						U+200C: ZERO WIDTH NON-JOINER		
-\u200D	=			ᶻʷʲ			Cf		zero-width joiner						U+200D: ZERO WIDTH JOINER		
-\u034F	=			ᶜᵍʲ			Mn		combining grapheme joiner						U+034F: COMBINING GRAPHEME JOINER		
-\u2060	=			ʷʲ			Cf		word joiner						U+2060 WORD JOINER		
-\u2067	=			ʳˡⁱ			Cf		rtl isolate						U+2067: RIGHT-TO-LEFT ISOLATE		
-\u2066	=			ˡʳⁱ			Cf		ltr isolate						U+2066: LEFT-TO-RIGHT ISOLATE		
-\u2068	=			ᶠˢⁱ			Cf		first-strong isolate						U+2068: FIRST STRONG ISOLATE		
-\u2069	=			ᵖᵈⁱ			Cf		pop direction isolate						U+2069: POP DIRECTIONAL ISOLATE		
-\u202B	=			ʳˡᵉ			Cf		rtl embed						U+202B: RIGHT-TO-LEFT EMBEDDING		
-\u202A	=			ˡʳᵉ			Cf		ltr embed						U+202A: LEFT-TO-RIGHT EMBEDDING		
-\u202C	=			ᵖᵈᶠ			Cf		pop direction						U+202C: POP DIRECTIONAL FORMATTING		
-\u200F	=			ʳˡᵐ			Cf		rtl mark						U+200F: RIGHT-TO-LEFT MARK		
-\u200E	=			ˡʳᵐ			Cf		ltr mark						U+200E: LEFT-TO-RIGHT MARK		
-\u061C	=			ᵃˡᵐ			Cf		arabic letter mark						U+061C: ARABIC LETTER MARK		
-ܐ	a	ʔ ɑ		ʔ	ʾ ȃ		Lo		consonant/mater lectionis		ܐ ܐܐܐ	1	ܐܵܠܲܦ	ɑlap	U+0710: SYRIAC LETTER ALAPH	✓	ʾālap̄
-ܒ	b	b		b	b		Lo		consonant		ܒ ܒܒܒ	2	ܒܹܝܬ	bet	U+0712: SYRIAC LETTER BETH	✓	bēṯ
-ܖ	d	d r		ḍ	d		Lo		consonant		ܖ ܖܖܖ	4			U+0716 SYRIAC LETTER DOTLESS DALATH RISH		
-ܕ	d	d		d	d		Lo		consonant		ܕ ܕܕܕ	4	ܕܵܠܲܬ	dɑlat	U+0715: SYRIAC LETTER DALATH	✓	dālaṯ
-ܓ	g	ɡ		g	g		Lo		consonant		ܓ ܓܓܓ	3	ܓܵܡܵܠ	gɑmal	U+0713: SYRIAC LETTER GAMAL	✓	gāmal
-ܗ	h	h		h	h		Lo		consonant		ܗ ܗܗܗ	5	ܗܹܐ	he	U+0717: SYRIAC LETTER HE	✓	hē
-ܚ	H	x		ḥ	ḥ		Lo		consonant		ܚ ܚܚܚ	8	ܚܹܝܬ	xet	U+071A: SYRIAC LETTER HETH	✓	ḥēṯ
-ܟ	k	k		k	k		Lo		consonant		ܟ ܟܟܟ	20	ܟܵܦ	kɑp	U+071F: SYRIAC LETTER KAPH	✓	kāp̄
-ܠ	l	l		l	l		Lo		consonant		ܠ ܠܠܠ	30	ܠܵܡܲܕ	lɑmad	U+0720: SYRIAC LETTER LAMADH	✓	lāmaḏ
-ܡ	m	m		m	m		Lo		consonant		ܡ ܡܡܡ	40	ܡܝܼܡ	mim	U+0721: SYRIAC LETTER MIM	✓	mīm
-ܢ	n	n		n	n		Lo		consonant		ܢ ܢܢܢ	50	ܢܘܼܢ	nun	U+0722: SYRIAC LETTER NUN	✓	nūn
-ܧ	p	p		p	p		Lo		consonant		ܧ ܧܧܧ	90			U+0727 SYRIAC LETTER REVERSED PE		
-ܦ	P	pʰ		pʰ	pʰ		Lo		consonant		ܦ ܦܦܦ	80	ܦܹܐ	pe	U+0726: SYRIAC LETTER PE	✓	pē
-ܩ	q	q		q	q		Lo		consonant		ܩ ܩܩܩ	200	ܩܘܿܦ	qop	U+0729: SYRIAC LETTER QAPH	✓	qōp̄
-ܪ	r	r		r	r		Lo		consonant		ܪ ܪܪܪ	300	ܪܹܝܫ	reʃ	U+072A: SYRIAC LETTER RISH	✓	rēš
-ܣ	s	s		s	s		Lo		consonant		ܣ ܣܣܣ	60	ܣܸܡܟܲܬ	sɪmkat	U+0723: SYRIAC LETTER SEMKATH	✓	semkaṯ
-ܫ	s	ʃ		ʃ	š		Lo		consonant		ܫ ܫܫܫ	400	ܫܝܼܢ	ʃin	U+072B: SYRIAC LETTER SHIN	✓	šīn
-ܨ	S	sˤ		ᵴ	ṣ		Lo		consonant		ܨ ܨܨܨ	100	ܨܵܕܹܐ	sˤɑde	U+0728: SYRIAC LETTER SADHE	✓	ṣāḏē
-ܬ	t	t		t	t		Lo		consonant		ܬ ܬܬܬ	400	ܬܵܘ	tɑw	U+072C: SYRIAC LETTER TAW	✓	taw
-ܛ	T	tˤ		ᵵ	ṭ		Lo		consonant		ܛ ܛܛܛ	9	ܛܹܝܬ	tˤet	U+071B: SYRIAC LETTER TETH	✓	ṭēṯ
-ܘ	w	w u o		w	w û ô		Lo		consonant		ܘ ܘܘܘ	6	ܘܵܘ	wɑw	U+0718: SYRIAC LETTER WAW	✓	waw
-ܝ	y	j i e		y	y		Lo		consonant/mater lectionis		ܝ ܝܝܝ	10	ܝܘܿܕ	yod	U+071D: SYRIAC LETTER YUDH	✓	yōḏ
-ܙ	z	z		z	z		Lo		consonant		ܙ ܙܙܙ	7	ܙܵܝܢ	zɑjn	U+0719: SYRIAC LETTER ZAIN	✓	zayn
-																	
-܁	.			.	.		Po		full stop						U+0701: SYRIAC SUPRALINEAR FULL STOP	✓	
-܂				.̣	.		Po		full stop						U+0702: SYRIAC SUBLINEAR FULL STOP	✓	
-܃	:			:	:		Po		colon						U+0703: SYRIAC SUPRALINEAR COLON	✓	
-܄				:	:		Po	?	colon						U+0704: SYRIAC SUBLINEAR COLON	✓	
-\u070F							Cf	?	abbreviation mark						U+070F SYRIAC ABBREVIATION MARK		
-܍							Po		note marker						U+070D: SYRIAC HARKLEAN ASTERISCUS		
-							Zs		space						U+0020: SPACE		
-`																	
-																	
-latinPanel = 'ɑ ȃ â ḇ č ḏ ḡ ǧ ḥ ɪ ȋ î ḵ ∅ ô sˤ ṣ ʃ š tˤ ṭ ṯ û ʒ ẑ ʕ ʔ ʾ'																	
-																	
-var cols = {																	
-key: 1,																	
-ipaLoc: 2,																	
-ipaPlus: 3,																	
-transLoc: 4,																	
-transcription: 5,																	
-kbd: 6,																	
-class: 7,																	
-status: 8,																	
-typeLoc: 9,																	
-statusLoc: 10,																	
-																	
-shape: 11,																	
-numLoc: 12,																	
-																	
-nnameLoc: 13,																	
-nameLoc: 14,																	
-ucsName: 15,																	
-block: 16,																	
-																	
-othertranscriptions: [[5, 'Latin']]																	
-}																	
+var spreadsheet = `syrc-axp	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	shape	# value	native name	latin name	ucs name	block
+ـ	_			_			Lm								U+0640 ARABIC TATWEEL	x
+̇	[			‐͑			Mn		various meanings, including feminine	depends on the manuscript					U+0307: COMBINING DOT ABOVE	x
+[	(			[			Ps		editing conjecture (open)						U+005B: LEFT SQUARE BRACKET	x
+]	)			]			Pe		editing conjecture (close)						U+005D: RIGHT SQUARE BRACKET	x
+/	/			/			Po		solidus						U+002F: SOLIDUS	
+⸣				⸣			Pe		uncertain letter (open)						U+2E23: TOP RIGHT HALF BRACKET	x
+⸢				⸢			Ps		uncertain letter (close)						U+2E22: TOP LEFT HALF BRACKET	x
+ܥ	{	ʕ		ʕ	ʿ		Lo		consonant		ܥ ܥـܥـܥ				U+0725: SYRIAC LETTER E	x
+̈	&			˖	\u030B		Mn		plural marker						U+0308: COMBINING DIAERESIS	x
+\u200B	=			␣			Cf		zero-width space						U+200B: ZERO WIDTH SPACE	
+\u200C	=			ᶻʷⁿʲ			Cf		zero-width non-joiner						U+200C: ZERO WIDTH NON-JOINER	x
+\u200D	=			ᶻʷʲ			Cf		zero-width joiner						U+200D: ZERO WIDTH JOINER	x
+\u034F	=			ᶜᵍʲ			Mn	?	combining grapheme joiner						U+034F: COMBINING GRAPHEME JOINER	
+\u2060	=			ʷʲ			Cf	?	word joiner						U+2060 WORD JOINER	
+\u2067	=			ʳˡⁱ			Cf		rtl isolate						U+2067: RIGHT-TO-LEFT ISOLATE	
+\u2066	=			ˡʳⁱ			Cf		ltr isolate						U+2066: LEFT-TO-RIGHT ISOLATE	
+\u2068	=			ᶠˢⁱ			Cf		first-strong isolate						U+2068: FIRST STRONG ISOLATE	
+\u2069	=			ᵖᵈⁱ			Cf		pop direction isolate						U+2069: POP DIRECTIONAL ISOLATE	
+\u202B	=			ʳˡᵉ			Cf	?	rtl embed						U+202B: RIGHT-TO-LEFT EMBEDDING	
+\u202A	=			ˡʳᵉ			Cf	?	ltr embed						U+202A: LEFT-TO-RIGHT EMBEDDING	
+\u202C	=			ᵖᵈᶠ			Cf	?	pop direction						U+202C: POP DIRECTIONAL FORMATTING	
+\u200F	=			ʳˡᵐ			Cf		rtl mark						U+200F: RIGHT-TO-LEFT MARK	
+\u200E	=			ˡʳᵐ			Cf		ltr mark						U+200E: LEFT-TO-RIGHT MARK	
+\u061C	=			ᵃˡᵐ			Cf	?	arabic letter mark						U+061C: ARABIC LETTER MARK	
+ܐ	A	ʔ ɑ		ʔ	ʾ ȃ		Lo		consonant/mater lectionis		ܐ ܐܐܐ				U+0710: SYRIAC LETTER ALAPH	
+ܒ	b	b		b	b		Lo		consonant		ܒ ܒـܒـܒ				U+0712: SYRIAC LETTER BETH	x
+ܕ	d	d		d	d		Lo		consonant	medieval	ܕܝܕ				U+0715: SYRIAC LETTER DALATH	x
+ܖ	D	d		dˑ	ḏ		s		consonant		ܖܝܖ				U+0716: SYRIAC LETTER DOTLESS DALATH RISH	x
+ܓ	g	ɡ		g	g		Lo		consonant		ܓ ܓـܓـܓ				U+0713: SYRIAC LETTER GAMAL	x
+ܗ	h	h		h	h		Lo		consonant		ܗ ܗܗܗ				U+0717: SYRIAC LETTER HE	x
+ܟ	k	k		k	k		Lo		consonant		ܟ ܟـܟـܟ				U+071F: SYRIAC LETTER KAPH	x
+ܠ	l	l		l	l		Lo		consonant		ܠ ܠـܠـܠ				U+0720: SYRIAC LETTER LAMADH	x
+ܡ	m	m		m	m		Lo		consonant		ܡ ܡـܡـܡ				U+0721: SYRIAC LETTER MIM	x
+ܢ	n	n		n	n		Lo		consonant		ܢ ܢـܢـܢ				U+0722: SYRIAC LETTER NUN	x
+ܦ	p	p		p	p		Lo		consonant		ܦ ܦـܦـܦ				U+0726: SYRIAC LETTER PE	x
+ܧ	p	p		p	p		Lo		consonant		ܦ ܦـܦـܦ				U+0727: SYRIAC LETTER REVERSED PE	x
+ܩ	q	q		q	q		Lo		consonant		ܩ ܩـܩـܩ				U+0729: SYRIAC LETTER QAPH	x
+ܪ	r	r		r	r		Lo		consonant		ܪ ܝܪ				U+072A: SYRIAC LETTER RISH	x
+ܣ	s	s		s	s		Lo		consonant		ܣ ܣـܣـܣ				U+0723: SYRIAC LETTER SEMKATH	x
+ܨ	S	sˤ		ᵴ	ṣ		Lo		consonant		ܨ ـܨ				U+0728: SYRIAC LETTER SADHE	x
+ܫ	s	ʃ		ʃ	š		Lo		consonant		ܫ ܫـܫـܫ				U+072B: SYRIAC LETTER SHIN	x
+ܛ	T	tˤ		ᵵ	ṭ		Lo		consonant		ܛ ܛـܛـܛ				U+071B: SYRIAC LETTER TETH	x
+ܬ	t¶	t		t	t		Lo		consonant		ܬ ـܬ				U+072C: SYRIAC LETTER TAW	x
+ܘ	w	w u o		w	w û ô		Lo		consonant		ܘ ܘܘܘ			waw	U+0718: SYRIAC LETTER WAW	x
+ܚ	x	x		x	ḥ		Lo		consonant		ܚ ܚـܚـܚ			heth	U+071A: SYRIAC LETTER HETH	x
+ܝ	y	j i e		y	y		Lo		consonant/mater lectionis		ܝ ܝـܝـܝ				U+071D: SYRIAC LETTER YUDH	x
+ܙ	z	z		z	z		Lo		consonant		ܙ ܝܙ			zain	U+0719: SYRIAC LETTER ZAIN	x
+																
+܀							Po		end of paragraph						U+0700: SYRIAC END OF PARAGRAPH	x
+܁				.	.		Po		half stop						U+0701: SYRIAC SUPRALINEAR FULL STOP	x
+܃				:	:		Po		half or final stop						U+0703: SYRIAC SUPRALINEAR COLON	x
+⁖							Po		half or final stop						U+2056 THREE DOT PUNCTUATION	x
+⁝							Po		half or final stop	medieval					U+205D: TRICOLON	x
+⸰							Po		half or final stop	medieval					U+2E30: RING POINT	x
+⁕							Po		end of title, final stop	medieval					U+2055: FLOWER PUNCTUATION MARK	x
+܍							Po		end of paragraph						U+070D: SYRIAC HARKLEAN ASTERISCUS	x
+																
+							Zs		space						U+0020: SPACE	
+•				•			Po		bullet						U+2022: BULLET	
+`																
+																
+latinPanel = 'ɑ ȃ â ḇ č ḏ ḡ ǧ ḥ ɪ ȋ î ḵ ∅ ô sˤ ṣ ʃ š tˤ ṭ ṯ û ʒ ẑ ʕ ʔ ʾ'																
+																
+																
+																
+var cols = {																
+key: 1,																
+ipaLoc: 2,																
+ipaPlus: 3,																
+transLoc: 4,																
+transcription: 5,																
+kbd: 6,																
+class: 7,																
+status: 8,																
+typeLoc: 9,																
+statusLoc: 10,																
+																
+shape: 11,																
+numLoc: 12,																
+																
+nnameLoc: 13,																
+nameLoc: 14,																
+ucsName: 15,																
+block: 16,																
+																
+othertranscriptions: [[5, 'Latin']]																
+}																
