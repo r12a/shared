6d957fa7874c4761d0f0885ffcffdcead11db975
r12a <ishida@w3.org>
2023-12-14 18:53:02 +0000

    + scriptdb/arab-wo

---
+++ scriptdb/arab-wo.js
@@ -0,0 +1,153 @@
+var spreadsheet = `arab-wo	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	ucs name	block	
+\u0020									space				U+0020: SPACE		
+\u25CC									dotted circle				U+25CC: DOTTED CIRCLE		
+-	-			-	-				hyphen				U+2010: HYPHEN		
+–	-			–	–		Pd	?	en dash				U+2013: EN DASH		
+—	-			—	—		Pd	?	em dash				U+2014: EM DASH		
+،	,			,	,		Po		comma				U+060C: ARABIC COMMA		
+,	,			,̣	,		Po	?	comma				U+002C: COMMA		
+؛	;			;	;		Po		semicolon				U+061B: ARABIC SEMICOLON		
+;	;			;̣	;		Po	?	semicolon				U+003B: SEMICOLON		
+:	:			:	:		Po		colon				U+003A: COLON		
+!	!			!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK		
+؟	?			?	?	?	Po		question mark				U+061F: ARABIC QUESTION MARK		
+?	?			?	?		Po	?	question mark				U+003F: QUESTION MARK		
+.	.			.	.		Po		full stop				U+002E: FULL STOP		
+…	.			…	…		Po		ellipsis				U+2026: HORIZONTAL ELLIPSIS		
+(	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS		
+[	(			[	[		Ps	?	bracket				U+005B: LEFT SQUARE BRACKET		
+)	)			)	)		Pe		parenthesis				U+0029: RIGHT PARENTHESIS		
+]	)			]	]		Pe	?	bracket				U+005D: RIGHT SQUARE BRACKET		
+%	%			%			Po	?	percentage mark				U+0025: PERCENT SIGN		
+‰	%			‰			Po	?	per mille mark				U+2030: PER MILLE SIGN		
+«	<			«			Pi	?	quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK		
+‹	<			‹			Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK		
+“	<			“	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK		
+‘	<			‘	‘		Pi		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK		
+»	>			»			Pf	?	quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK		
+›	>			›			Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK		
+”	>			”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK		
+’	>			’	’		Pf		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK		
+ّ	&			˖			Mn		gemination mark				U+0651: ARABIC SHADDA		
+ْ	*			˟			Mn		vowel absence indicator				U+0652: ARABIC SUKUN		
+															
+ا	A	aː		ā	aa		Lo		long vowel / vowel carrier		اَلِف	alif	U+0627: ARABIC LETTER ALEF		
+ب	b	b		b	b		Lo		consonant		بࣹهْ	beh	U+0628: ARABIC LETTER BEH		
+ݒ	p	p		p	p		Lo		consonant		ݒࣹهْ	peh	U+0752: ARABIC LETTER BEH WITH THREE DOTS POINTING UPWARDS BELOW		
+ت	t	t		t	t		Lo		consonant		تࣹهْ	teh	U+062A: ARABIC LETTER TEH		
+د	d	d		d	d		Lo		consonant		دَلْ	dal	U+062F: ARABIC LETTER DAL		
+ݖ	c	c		c	c		Lo		consonant		ݖࣹهْ	ceh	U+0756: ARABIC LETTER BEH WITH SMALL V		
+ج	j	ɟ		ɟ	j		Lo		consonant		جࣹيمْ	jeem	U+062C: ARABIC LETTER JEEM		
+ک	k	k		k	k		Lo		consonant		کَفْ	kaf	U+ 06A9: ARABIC LETTER KEHEH		
+گ	g	ɡ		ɡ	g		Lo		consonant		گࣹهْ	geh	U+06AF: ARABIC LETTER GAF		
+ق	q	q		q	q		Lo		consonant		قَفْ	qaf	U+0642: ARABIC LETTER QAF		
+ف	f	f		f	f		Lo		consonant		فࣹهْ	feh	U+0641: ARABIC LETTER FEH		
+س	s	s		s	s		Lo		consonant		سࣹينْ	seen	U+0633: ARABIC LETTER SEEN		
+خ	x	x		x	x		Lo		consonant		خَهْ	xah	U+062E: ARABIC LETTER KHAH		
+ه	h	h		h	h		Lo		consonant		هࣹهْ	heh	U+0647: ARABIC LETTER HEH		
+ع	{	∅		∅			Lo		word-medial vowel carrier		عَيْنْ	ayn	U+0639: ARABIC LETTER AIN		
+م	m	m		m	m		Lo		consonant		مࣹيمْ	meem	U+0645: ARABIC LETTER MEEM		
+ن	n	n		n	n		Lo		consonant		نࣷونْ	noon	U+0646: ARABIC LETTER NOON		
+ݝ	n	ŋ		ŋ	ŋ		Lo		consonant		ݝࣷونْ	ŋoon	U+075D: ARABIC LETTER AIN WITH TWO DOTS ABOVE		
+ݧ	n	ɲ		ɲ	ñ		Lo		consonant		ݧࣷونْ	ñoon	U+0767: ARABIC LETTER NOON WITH TWO DOTS BELOW		
+و	w	w		w	w		Lo		consonant		وَوْ	waw	U+0648: ARABIC LETTER WAW		
+ر	r	r		r	r		Lo		consonant		رࣹهْ	reh	U+0631: ARABIC LETTER REH		
+ل	l	l		l	l		Lo		consonant		لَمْ	lam	U+0644: ARABIC LETTER LAM		
+ي	y	j		j	y		Lo		consonant		يࣹهْ	yeh	U+064A: ARABIC LETTER YEH		
+															
+مب		ᵐb			mb				prenasalised consonant				U+0645 U+0628: ARABIC LETTER MEEM, LETTER BEH		
+مݒ		ᵐp			mp				prenasalised consonant	not word-initial			U+0645 U+0752: ARABIC LETTER MEEM, LETTER BEH WITH THREE DOTS POINTING UPWARDS BELOW		
+ند		ⁿd			nd				prenasalised consonant				U+0646 U+062F: ARABIC LETTER NOON, LETTER DAL		
+نݖ		ᶮc			nc				prenasalised consonant	not word-initial			U+0646 U+0756: ARABIC LETTER NOON, LETTER BEH WITH SMALL V		
+نج		ᶮɟ			nj				prenasalised consonant				U+0646 U+062C: ARABIC LETTER NOON, LETTER JEEM		
+نق		ᵑq			nq				prenasalised consonant	not word-initial			U+0646 U+0642: ARABIC LETTER NOON, LETTER QAF		
+نک		ɴk			nk				prenasalised consonant				U+0646 U+06A9: ARABIC LETTER NOON, LETTER KEHEH		
+نگ		ᵑɡ			ng				prenasalised consonant				U+0646 U+06AF: ARABIC LETTER NOON, LETTER GAF		
+															
+															
+پ		p					Lo	o	obsolete consonant				U+067E: ARABIC LETTER PEH		
+ݑ		p ᵐb					Lo	o	obsolete consonant				U+0751: ARABIC LETTER BEH WITH DOT BELOW AND THREE DOTS ABOVE		
+چ		c ᶮɟ					Lo	o	obsolete consonant				U+0686: ARABIC LETTER TCHEH		
+ڭ		ŋ ɡ					Lo	o	obsolete consonant				U+06AD: ARABIC LETTER NG		
+ݤ		ŋ					Lo	o	obsolete consonant				U+0764: ARABIC LETTER KEHEH WITH THREE DOTS POINTING UPWARDS BELOW		
+چ		ɲ					Lo	o	obsolete consonant				U+0686: ARABIC LETTER TCHEH		
+ڎ		ⁿd						o	obsolete consonant				U+068E: ARABIC LETTER DUL		
+ݣ		ᵑɡ						o	obsolete consonant				U+0763: ARABIC LETTER KEHEH WITH THREE DOTS ABOVE		
+جۛ		c ɲ						o	obsolete consonant				U+062C U+06DB: ARABIC LETTER JEEM, SMALL HIGH THREE DOTS		
+\u06DB		–						o	obsolete diacritic				U+06DB: ARABIC SMALL HIGH THREE DOTS		
+															
+ِ	i	i		i	i		Mn​		vowel				U+0650: ARABIC KASRA		
+ُ	u	u		u	u		Mn​		vowel				U+064F: ARABIC DAMMA		
+ࣷ	o	ɔ		o	o		Mn​		vowel				U+08F7: ARABIC LEFT ARROWHEAD ABOVE		
+ࣸ	o	o		ó	ó		Mn​		vowel				U+08F8: ARABIC RIGHT ARROWHEAD ABOVE		
+ࣹ	e	ɛ		e	e		Mn​		vowel				U+08F9: ARABIC LEFT ARROWHEAD BELOW		
+ࣺ	e	e		e	e		Mn​		vowel				U+08FA: ARABIC RIGHT ARROWHEAD BELOW		
+ࣴ	e	ə		ë	ë		Mn​		vowel				U+08F4: ARABIC FATHA WITH RING		
+َ	a	a		a	a		Mn​		vowel				U+064E: ARABIC FATHA		
+ࣵ	a	a		à	à		Mn​		vowel	before geminate/prenasalised consonant			U+08F5: ARABIC FATHA WITH DOT ABOVE		
+آ	a	aː		ȁ	aa		Lo		vowel	word-initial			U+0622: ARABIC LETTER ALEF WITH MADDA ABOVE		
+ٜ		ɛ e					Mn​	o	obsolete vowel				U+065C: ARABIC VOWEL SIGN DOT BELOW		
+ٝ		ɔ					Mn​	o	obsolete vowel				U+065D: ARABIC REVERSED DAMMA		
+															
+َا	A	aː		ā	aa				long vowel				U+064E U+0627: ARABIC FATHA, LETTER ALEF		
+ࣴعࣴ	E	əː			ëë				long vowel				U+08F4 U+0639 U+08F4: ARABIC FATHA WITH RING, LETTER AIN, FATHA WITH RING		
+ࣹي	E	ɛː			ee				long vowel				U+08F9 U+064A: ARABIC LEFT ARROWHEAD BELOW, LETTER YEH		
+ࣺي	E	eː			ee				long vowel				U+08FA U+064A: ARABIC RIGHT ARROWHEAD BELOW, LETTER YEH		
+ِي	I	iː			ii				long vowel				U+0650 U+064A: ARABIC KASRA, LETTER YEH		
+ࣷو	O	ɔː			oo				long vowel				U+08F7 U+0648: ARABIC LEFT ARROWHEAD ABOVE, LETTER WAW		
+ࣸو	O	oː			óó				long vowel				U+08F8 U+0648: ARABIC RIGHT ARROWHEAD ABOVE, LETTER WAW		
+ُو	U	uː			uu				long vowel				U+064F U+0648: ARABIC DAMMA, LETTER WAW		
+															
+\u200C	=			ᶻʷⁿʲ			Cf		zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER		
+\u200D	=			ᶻʷʲ			Cf		zero-width joiner				U+200D: ZERO WIDTH JOINER		
+\u034F	=			ᶜᵍʲ			Mn		combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER		
+\u2060	=			ʷʲ			Cf		word joiner				U+2060 WORD JOINER		
+\u2067	=			ʳˡⁱ			Cf		rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE		
+\u2066	=			ˡʳⁱ			Cf		ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE		
+\u2068	=			ᶠˢⁱ			Cf		first-strong isolate				U+2068: FIRST STRONG ISOLATE		
+\u2069	=			ᵖᵈⁱ			Cf		pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE		
+\u202B	=			ʳˡᵉ			Cf	x	rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING		
+\u202A	=			ˡʳᵉ			Cf	x	ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING		
+\u202C	=			ᵖᵈᶠ			Cf	x	pop direction				U+202C: POP DIRECTIONAL FORMATTING		
+\u200F	=			ʳˡᵐ			Cf		rtl mark				U+200F: RIGHT-TO-LEFT MARK		
+\u200E	=			ˡʳᵐ			Cf		ltr mark				U+200E: LEFT-TO-RIGHT MARK		
+\u061C	=			ᵃˡᵐ			Cf		arabic letter mark				U+061C: ARABIC LETTER MARK		
+															
+															
+															
+`															
+															
+latinPanel = 'aː á ɛ ɛː ə əː ᵊ eː ë é iː ɔ ɔː oː ó uː ŋ ɟ ɡ ɲ ñ ɸ ɽ ɾ χ ᵐb ᵑɡ ᶮɟ ⁿd ʔ'															
+															
+var cols = {															
+key: 1,															
+ipaLoc: 2,															
+ipaPlus: 3,															
+transLoc: 4,															
+transcription: 5,															
+kbd: 6,															
+class: 7,															
+status: 8,															
+typeLoc: 9,															
+statusLoc: 10,															
+															
+dvowel: 0,															
+ivowel: 0,															
+onset:0,															
+final:0,															
+subj:0,															
+ltone:0,															
+htone:0,															
+lc:0,															
+uc:0,															
+meaning:0,															
+shape: 0,															
+numLoc: 0,															
+															
+nnameLoc: 11,															
+nameLoc: 12,															
+ucsName: 13,															
+block: 14,															
+															
+//othertranscriptions: [[5, 'Latin']]															
+}															
