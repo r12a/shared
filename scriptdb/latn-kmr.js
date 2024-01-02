f718aaea402349e8483525a8157f132dbcb78adf
r12a <ishida@w3.org>
2024-01-02 19:01:43 +0000

    scriptdb/latn-kmr: Clarify allophonic info

--- scriptdb/latn-kmr.js
+++ scriptdb/latn-kmr.js
@@ -38,8 +38,7 @@ i	i	ɪ		i	i		Ll						U+0069: LATIN SMALL LETTER I	–	–
 î	i	iː		î	î		Ll		vowel				U+00EE: LATIN SMALL LETTER I WITH CIRCUMFLEX	ی	𐺨	
 j	j	ʒ		ʒ	j		Ll		consonant				U+006A: LATIN SMALL LETTER J	ژ	𐺐	
 k	k	k c		k	k		Ll		consonant				U+006B: LATIN SMALL LETTER K	ک	𐺝	
-l	l	l ɫ		l	l		Ll		consonant				U+006C: LATIN SMALL LETTER L	ل ڵ	𐺠	
-ll		ɫ												ڵ	𐺰	
+l	l	l		l	l		Ll		consonant				U+006C: LATIN SMALL LETTER L	ل ڵ	𐺠	
 m	m	m		m	m		Ll		consonant				U+006D: LATIN SMALL LETTER M	م	𐺡	
 n	n	n		n	n		Ll		consonant				U+006E: LATIN SMALL LETTER N	ن	𐺢	
 o	o	oː		o	o		Ll		vowel				U+006F: LATIN SMALL LETTER O	ۆ	𐺥	
@@ -48,12 +47,12 @@ q	q	q		q	q		Ll		consonant				U+0071: LATIN SMALL LETTER Q	ق	𐺜
 r	r	ɾ r		ɾ	r		Ll		consonant				U+0072: LATIN SMALL LETTER R	ر ڕ	𐺍 𐺎	
 s	s	s		s	s		Ll		consonant				U+0073: LATIN SMALL LETTER S	س	𐺑	
 ş	s	ʃ		ʃ	sh		Ll		consonant				U+015F: LATIN SMALL LETTER S WITH CEDILLA	ش	𐺒	
-t	t	t tʰ		t	t		Ll		consonant				U+0074: LATIN SMALL LETTER T	ت	𐺕	
+t	t	t		t	t		Ll		consonant				U+0074: LATIN SMALL LETTER T	ت	𐺕	
 u	u	ʊ		ʊ	u		Ll		vowel				U+0075: LATIN SMALL LETTER U	و	𐺣	
 û	u	uː		u	û		Ll		vowel				U+00FB: LATIN SMALL LETTER U WITH CIRCUMFLEX	وو	𐺣𐺣	
 v	v	v		v	v		Ll		consonant				U+0076: LATIN SMALL LETTER V	ڤ	𐺚 𐺛	
 w	w	w		w	w		Ll		consonant				U+0077: LATIN SMALL LETTER W	و	𐺤	
-x	x	x ɣ		x	x		Ll		consonant				U+0078: LATIN SMALL LETTER X	خ غ	𐺊 𐺘	
+x	x	x χ		x	x		Ll		consonant				U+0078: LATIN SMALL LETTER X	خ غ	𐺊 𐺘	
 y	y	j		y	y		Ll		consonant				U+0079: LATIN SMALL LETTER Y	ی	𐺨	
 z	z	z		z	z		Ll		consonant				U+007A: LATIN SMALL LETTER Z	ز	𐺏	
 ň	n	ŋ		ŋ	ň		Ll						U+0148: LATIN SMALL LETTER N WITH CARON	نٚ		
@@ -71,8 +70,7 @@ I	I	ɪ		I	I		Lu						U+0049: LATIN CAPITAL LETTER I	–	–
 Î	I	iː		Î	Î		Lu		vowel				U+00CE: LATIN CAPITAL LETTER I WITH CIRCUMFLEX	ی	𐺨	
 J	J	ʒ		Ʒ	J		Lu		consonant				U+004A: LATIN CAPITAL LETTER J	ژ	𐺐	
 K	K	k		K	K		Lu		consonant				U+004B: LATIN CAPITAL LETTER K	ک	𐺝	
-L	L	l ɫ		L	L		Lu		consonant				U+004C: LATIN CAPITAL LETTER L	ل ڵ	𐺠	
-LL		ɫ							digraph				U+004C U+004C: LATIN CAPITAL LETTER L, CAPITAL LETTER L	ڵ	𐺰	
+L	L	l		L	L		Lu		consonant				U+004C: LATIN CAPITAL LETTER L	ل ڵ	𐺠	
 M	M	m		M	M		Lu		consonant				U+004D: LATIN CAPITAL LETTER M	م	𐺡	
 N	N	n		N	N		Lu		consonant				U+004E: LATIN CAPITAL LETTER N	ن	𐺢	
 O	O	oː		O	O		Lu		vowel				U+004F: LATIN CAPITAL LETTER O	ۆ	𐺥	
@@ -86,7 +84,7 @@ U	U	ʊ		Ʊ	U		Lu		vowel				U+0055: LATIN CAPITAL LETTER U	و	𐺣
 Û	U	uː		U	Û		Lu		vowel				U+00DB: LATIN CAPITAL LETTER U WITH CIRCUMFLEX	وو	𐺣𐺣	
 V	V	v		V	V		Lu		consonant				U+0056: LATIN CAPITAL LETTER V	ڤ	𐺚 𐺛	
 W	W	w		W	W		Lu		consonant				U+0057: LATIN CAPITAL LETTER W	و	𐺤	
-X	X	x ɣ		X	X		Lu		consonant				U+0058: LATIN CAPITAL LETTER X	خ غ	𐺊 𐺘	
+X	X	x χ		X	X		Lu		consonant				U+0058: LATIN CAPITAL LETTER X	خ غ	𐺊 𐺘	
 Y	Y	j		Y	Y		Lu		consonant				U+0059: LATIN CAPITAL LETTER Y	ی	𐺨	
 Z	Z	z		Z	Z		Lu		consonant				U+005A: LATIN CAPITAL LETTER Z	ز	𐺏	
 Ň	N	ŋ		Ŋ	Ň		Lu	r	alternate consonant for allophone	not used			U+0147: LATIN CAPITAL LETTER N WITH CARON	نٚ		
