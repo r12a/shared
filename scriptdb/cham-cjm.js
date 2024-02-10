e3a3334909f908e18be7cb93b076a1200f06abcd
r12a <ishida@w3.org>
2024-02-10 07:33:05 +0000

    +cham-cjm: scriptdb & webfont

---
+++ scriptdb/cham-cjm.js
@@ -0,0 +1,140 @@
+var spreadsheet = `cham-cjm	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	ucs name	block
+\u0020	 						Zs		space				U+0020: SPACE	
+														
+														
+				ẹ										
+ꨁ		ʔi		ị	i		Lo		independent vowel				U+AA01: CHAM LETTER I	
+ꨂ		ʔu		ụ	u		Lo		independent vowel				U+AA02: CHAM LETTER U	
+ꨃ		ʔɛ		ɛ̣	é		Lo		independent vowel				U+AA03: CHAM LETTER E	
+ꨅ		ʔo		ọ	o		Lo		independent vowel				U+AA05: CHAM LETTER O	
+ꨀ		ʔa		ạ	a		Lo		independent vowel				U+AA00: CHAM LETTER A	
+ꨄ		ʔai		ại	ai		Lo		independent vowel				U+AA04: CHAM LETTER AI	
+														
+ꨚ		p	a	p	pa		Lo		consonant				U+AA1A: CHAM LETTER PA	
+ꨛ		p	a	ṗ	pa		Lo		consonant				U+AA1B: CHAM LETTER PPA	
+ꨜ		pʰ	a	pʰ	pha		Lo		consonant				U+AA1C: CHAM LETTER PHA	
+ꨝ		b	a	p̄	ba		Lo		consonant				U+AA1D: CHAM LETTER BA	
+ꨞ		bʰ	a	p̄ʰ	bha		Lo		consonant				U+AA1E: CHAM LETTER BHA	
+ꨡ		ɓ	a	ɓ	mba		Lo		consonant				U+AA21: CHAM LETTER BBA	
+ꨓ		t	a	t	ta		Lo		consonant				U+AA13: CHAM LETTER TA	
+ꨔ		tʰ	a	tʰ	tha		Lo		consonant				U+AA14: CHAM LETTER THA	
+ꨕ		d	a	ṭ	da		Lo		consonant				U+AA15: CHAM LETTER DA	
+ꨖ		dʰ	a	ṭʰ	dha		Lo		consonant				U+AA16: CHAM LETTER DHA	
+ꨙ		ɗ	a	ɗ	nda		Lo		consonant				U+AA19: CHAM LETTER DDA	
+ꨌ		c	a	c	ca		Lo		consonant				U+AA0C: CHAM LETTER CHA	
+ꨍ		cʰ	a	cʰ	cha		Lo		consonant				U+AA0D: CHAM LETTER CHHA	
+ꨎ		d͡ʑ	a	c̣	ja		Lo		consonant				U+AA0E: CHAM LETTER JA	
+ꨏ		d͡ʑʰ	a	c̣ʰ	jha		Lo		consonant				U+AA0F: CHAM LETTER JHA	
+ꨆ		k	a	k	ka		Lo		consonant				U+AA06: CHAM LETTER KA	
+ꨇ		kʰ	a	kʰ	kha		Lo		consonant				U+AA07: CHAM LETTER KHA	
+ꨈ		ɡ͡ɣ	a	ɡ	ga		Lo		consonant				U+AA08: CHAM LETTER GA	
+ꨉ		ɡʰ	a	ɡʰ	gha		Lo		consonant				U+AA09: CHAM LETTER GHA	
+ꨦ		s	a	ṣ	ṣa		Lo		consonant				U+AA26: CHAM LETTER SSA	
+ꨧ		s	a	s	sa		Lo		consonant				U+AA27: CHAM LETTER SA	
+ꨨ		h	a	h	ha		Lo		consonant				U+AA28: CHAM LETTER HA	
+ꨟ		m	ɨ	m	mâ		Lo		consonant				U+AA1F: CHAM LETTER MUE	
+ꨠ		m	a	ṃ	ma		Lo		consonant				U+AA20: CHAM LETTER MA	
+ꨗ		n	ɨ	n	nâ		Lo		consonant				U+AA17: CHAM LETTER NUE	
+ꨘ		n	a	n	na		Lo		consonant				U+AA18: CHAM LETTER NA	
+ꨐ		ɲ	ɨ	ɲ	nyâ		Lo		consonant				U+AA10: CHAM LETTER NHUE	
+ꨑ		ɲ	a	ɲ	nya		Lo		consonant				U+AA11: CHAM LETTER NHA	
+ꨊ		ŋ	ɨ	ŋ	ngâ		Lo		consonant				U+AA0A: CHAM LETTER NGUE	
+ꨋ		ŋʰ	a	ŋʰ	nga		Lo		consonant				U+AA0B: CHAM LETTER NGA	
+ꨥ		w	a	w	wa		Lo		consonant				U+AA25: CHAM LETTER VA	
+ꨣ		ɾ	a	ɾ	ra		Lo		consonant				U+AA23: CHAM LETTER RA	
+ꨤ		l	a	l	la		Lo		consonant				U+AA24: CHAM LETTER LA	
+ꨒ		j	a	j̣	nja		Lo		consonant				U+AA12: CHAM LETTER NHJA	
+ꨢ		j	a	j	ya		Lo		consonant				U+AA22: CHAM LETTER YA	
+														
+ꨶ		w	a	w̆	ua		Mn​		medial consonant				U+AA36: CHAM CONSONANT SIGN WA	
+ꨴ		r	a	r̆	ra		Mc​		medial consonant				U+AA34: CHAM CONSONANT SIGN RA	
+ꨵ		l	a	l̆	la		Mn​		medial consonant				U+AA35: CHAM CONSONANT SIGN LA	
+ꨳ		j	a	j̆	ya		Mc​		medial consonant				U+AA33: CHAM CONSONANT SIGN YA	
+														
+ꨪ		i		i	i		Mn​		dependent vowel				U+AA2A: CHAM VOWEL SIGN I	
+ꨫ		iː		ī	ī		Mn​		dependent vowel				U+AA2B: CHAM VOWEL SIGN II	
+ꨲ		ɨ		ɨ	â		Mn​		dependent vowel				U+AA32: CHAM VOWEL SIGN UE	
+ꨲꨩ		ɨː		ɨ̄	â				dependent vowel				U+AA32 U+AA29: CHAM VOWEL SIGN UE, VOWEL SIGN AA	
+ꨭ		u		u	u		Mn​		dependent vowel				U+AA2D: CHAM VOWEL SIGN U	
+ꨭꨩ		uː		ū	ū				dependent vowel				U+AA2D U+AA29: CHAM VOWEL SIGN U, VOWEL SIGN AA	
+ꨬ		eɪ		eⁱ	ei		Mn​		dependent vowel				U+AA2C: CHAM VOWEL SIGN EI	
+ꨯ		o		o	o		Mc​		dependent vowel				U+AA2F: CHAM VOWEL SIGN O	
+ꨯꨩ		oː		ō	ō				dependent vowel				U+AA2F U+AA29: CHAM VOWEL SIGN O, VOWEL SIGN AA	
+ꨮ		ə		ə	e		Mn​		dependent vowel				U+AA2E: CHAM VOWEL SIGN OE	
+ꨱ		əː		ə̄	ē		Mn​		dependent vowel				U+AA31: CHAM VOWEL SIGN AU	
+ꨯꨮ		ɛ		ɛ	é				dependent vowel				U+AA2F U+AA2E: CHAM VOWEL SIGN O, VOWEL SIGN OE	
+ꨯꨮꨩ		ɛː		ɛ̄	é				dependent vowel				U+AA2F U+AA2E U+AA29: CHAM VOWEL SIGN O, VOWEL SIGN OE, VOWEL SIGN AA	
+ꨯꨱ		ɔ		ɔ	ao				dependent vowel				U+AA2F U+AA31: CHAM VOWEL SIGN O, VOWEL SIGN AU	
+ꨩ		aː		ā	ā		Mn​		dependent vowel				U+AA29: CHAM VOWEL SIGN AA	
+ꨰ		aɪ		aⁱ	ai		Mc​		dependent vowel				U+AA30: CHAM VOWEL SIGN AI	
+ꨮꨭ		aʊ		aʷ	au				dependent vowel				U+AA2E U+AA2D:CHAM VOWEL SIGN OE, VOWEL SIGN U	
+														
+ꩇ		p̚		p̽	p		Lo		final consonant				U+AA47: CHAM LETTER FINAL P	
+ꩅ		t̚		t̽	t		Lo		final consonant				U+AA45: CHAM LETTER FINAL T	
+ꩄ		c̚		c̽	c		Lo		final consonant				U+AA44: CHAM LETTER FINAL CH	
+ꩀ		k		k̽	k		Lo		final consonant				U+AA40: CHAM LETTER FINAL K	
+ꩁ		ɡ		ɡ̽	g		Lo		final consonant				U+AA41: CHAM LETTER FINAL G	
+ꩌ		m		m̽	m		Mn​		final consonant				U+AA4C: CHAM CONSONANT SIGN FINAL M	
+ꩆ		n		n̽	n		Lo		final consonant				U+AA46: CHAM LETTER FINAL N	
+ꩂ		ŋ		ŋ̽	ng		Lo		final consonant				U+AA42: CHAM LETTER FINAL NG	
+ꩃ		ŋ		ŋ̣̽	ng		Mn​		final consonant				U+AA43: CHAM CONSONANT SIGN FINAL NG	
+ꩋ		s		s̽	ṣ		Lo		final consonant				U+AA4B: CHAM LETTER FINAL SS	
+ꩍ		h		h̽	h		Mc​		final consonant				U+AA4D: CHAM CONSONANT SIGN FINAL H	
+ꩉ		r		r̽	r		Lo		final consonant				U+AA49: CHAM LETTER FINAL R	
+ꩊ		l		l̽	l		Lo		final consonant				U+AA4A: CHAM LETTER FINAL L	
+ꩈ		j		j̽	y		Lo		final consonant				U+AA48: CHAM LETTER FINAL Y	
+														
+꩐		0		0̣	0		Nd		digit		ꨔꨯꨱꩍ	thaoh	U+AA50: CHAM DIGIT ZERO	
+꩑		1		1̣	1		Nd		digit		ꨧ	sa	U+AA51: CHAM DIGIT ONE	
+꩒		2		2̣	2		Nd		digit		ꨕꨶ	dua	U+AA52: CHAM DIGIT TWO	
+꩓		3		3̣	3		Nd		digit		ꨆꨵꨮꨭ	klau	U+AA53: CHAM DIGIT THREE	
+꩔		4		4̣	4		Nd		digit			pak	U+AA54: CHAM DIGIT FOUR	
+꩕		5		5̣	5		Nd		digit		ꨤꨟꨩ	limâ	U+AA55: CHAM DIGIT FIVE	
+꩖		6		6̣	6		Nd		digit		ꨘꨮꩌ	nam	U+AA56: CHAM DIGIT SIX	
+꩗		7		7̣	7		Nd		digit		ꨓꨎꨭꩍ	tajuh	U+AA57: CHAM DIGIT SEVEN	
+꩘		8		8̣	8		Nd		digit		ꨕꨤꨚꩆ	dalapan	U+AA58: CHAM DIGIT EIGHT	
+꩙		9		9̣	9		Nd		digit		ꨧꨤꨚꩆ	salapan	U+AA59: CHAM DIGIT NINE	
+														
+꩜				|			Po		punctuation				U+AA5C: CHAM PUNCTUATION SPIRAL	
+꩝				,	,		Po		punctuation				U+AA5D: CHAM PUNCTUATION DANDA	
+꩞				.	.		Po		punctuation				U+AA5E: CHAM PUNCTUATION DOUBLE DANDA	
+꩟				|	|		Po		punctuation				U+AA5F: CHAM PUNCTUATION TRIPLE DANDA	
+														
+														
+														
+`														
+														
+latinPanel = 'â é ŋ ɓ ɔ ɗ ə ɛ ɡ ɨ ɪ ɲ ɾ ʔ ʊ ʰ ʷ ː ̚'														
+														
+var cols = {														
+key: 1,														
+ipaLoc: 2,														
+ipaPlus: 3,														
+transLoc: 4,														
+transcription: 5,														
+kbd: 6,														
+class: 7,														
+status: 8,														
+typeLoc: 9,														
+statusLoc: 10,														
+														
+dvowel: 0,														
+ivowel: 0,														
+onset:0,														
+final:0,														
+subj:0,														
+ltone:0,														
+htone:0,														
+lc:0,														
+uc:0,														
+meaning:0,														
+shape: 0,														
+numLoc: 0,														
+														
+nnameLoc: 11,														
+nameLoc: 12,														
+ucsName: 13,														
+block: 14,														
+														
+//othertranscriptions: [[5, 'Latin']]														
+}														
