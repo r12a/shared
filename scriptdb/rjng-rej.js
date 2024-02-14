e387232f6c4d44a33f5c9de587d6a4f5c079a7b9
r12a <ishida@w3.org>
2024-02-14 10:10:49 +0000

    scriptdb/rjng-rej.js

---
+++ scriptdb/rjng-rej.js
@@ -0,0 +1,112 @@
+var spreadsheet = `rjng-rej	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	ucs name	block
+ꤰ	k	k	a	k	k		Lo		consonant				U+A930: REJANG LETTER KA	
+ꤱ	g	ɡ	a	ɡ	g		Lo		consonant				U+A931: REJANG LETTER GA	
+ꤲ	n	ŋ	a	ŋ	ng		Lo		consonant				U+A932: REJANG LETTER NGA	
+ꤳ	t	t	a	t	t		Lo		consonant				U+A933: REJANG LETTER TA	
+ꤴ	d	d	a	d	d		Lo		consonant				U+A934: REJANG LETTER DA	
+ꤵ	n	n	a	n	n		Lo		consonant				U+A935: REJANG LETTER NA	
+ꤶ	p	p	a	p	p		Lo		consonant				U+A936: REJANG LETTER PA	
+ꤷ	b	b	a	b	b		Lo		consonant				U+A937: REJANG LETTER BA	
+ꤸ	m	m	a	m	m		Lo		consonant				U+A938: REJANG LETTER MA	
+ꤹ	c	c	a	c	c		Lo		consonant				U+A939: REJANG LETTER CA	
+ꤺ	j	ɟ	a	ɟ	j		Lo		consonant				U+A93A: REJANG LETTER JA	
+ꤻ	n	ɲ	a	ɲ	ny		Lo		consonant				U+A93B: REJANG LETTER NYA	
+ꤼ	s	s	a	s	s		Lo		consonant				U+A93C: REJANG LETTER SA	
+ꤽ	r	r	a	r	r		Lo		consonant				U+A93D: REJANG LETTER RA	
+ꤾ	l	l	a	l	l		Lo		consonant				U+A93E: REJANG LETTER LA	
+ꤿ	y	j	a	j	y		Lo		consonant				U+A93F: REJANG LETTER YA	
+ꥀ	w	w	a	w	w		Lo		consonant				U+A940: REJANG LETTER WA	
+ꥁ	h	h	a	h	h		Lo		consonant				U+A941: REJANG LETTER HA	
+														
+ꥂ	b	ᵐb	a	ᵐb	mb		Lo		prenasalised consonant				U+A942: REJANG LETTER MBA	
+ꥃ	g	ⁿɡ	a	ⁿɡ	ng		Lo		prenasalised consonant				U+A943: REJANG LETTER NGGA	
+ꥄ	d	ⁿd	a	ⁿd	nd		Lo		prenasalised consonant				U+A944: REJANG LETTER NDA	
+ꥅ	j	ⁿɟ	a	ⁿɟ	nj		Lo		prenasalised consonant				U+A945: REJANG LETTER NYJA	
+														
+ꥆ	a	a ə		a	a		Lo		vowel				U+A946: REJANG LETTER A	
+														
+ꥇ	i	i ɪ		i	i		Mn​		vowel				U+A947: REJANG VOWEL SIGN I	
+ꥈ	u	u ʊ		u	u		Mn​		vowel				U+A948: REJANG VOWEL SIGN U	
+ꥉ	e	ɛ e		ɛ	ɛ e		Mn​		vowel				U+A949: REJANG VOWEL SIGN E	
+ꥊ	a	ai		aⁱ	ai		Mn​		vowel				U+A94A: REJANG VOWEL SIGN AI	
+ꥋ	o	o ɔ		o	o		Mn​		vowel				U+A94B: REJANG VOWEL SIGN O	
+														
+ꥌ	a	au		aᵘ	au		Mn​		diphthong				U+A94C: REJANG VOWEL SIGN AU	
+ꥍ	e	əu		əᵘ	əu		Mn​		diphthong				U+A94D: REJANG VOWEL SIGN EU	
+ꥎ	e	ɛa̯		ɛᵃ	ɛa̯		Mn​		diphthong				U+A94E: REJANG VOWEL SIGN EA	
+														
+ꥏ	N	-ŋ		ŋ̽	ng		Mn​		final consonant				U+A94F: REJANG CONSONANT SIGN NG	
+ꥐ	N	-n		n̽	n		Mn​		final consonant				U+A950: REJANG CONSONANT SIGN N	
+ꥑ	R	-r		r̽	r		Mn​		final consonant				U+A951: REJANG CONSONANT SIGN R	
+ꥒ	}	-ʔ		ʔ			Mc​		final consonant				U+A952: REJANG CONSONANT SIGN H	
+														
+꥓	*			˟			Mc​		vowel absence indicator				U+A953: REJANG VIRAMA	
+꥟	|			|	.		Po		section divider				U+A95F: REJANG SECTION MARK	
+														
+\u0020	 						Zs		space				U+0020: SPACE	
+\u200B	=			ᶻʷˢᵖ			Cf		zero-width space				U+200B: ZERO WIDTH SPACE	
+,	,			,	,		Po		comma				U+002C: COMMA	
+;	;			;	;	;;⁏⸵	Po		semicolon				U+003B: SEMICOLON	
+!	!			!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK	
+?	?			?	?		Po		question mark				U+003F: QUESTION MARK	
+:	:			:	:		Po		colon				U+003A: COLON	
+.	.			.	.		Po		full stop				U+002E: FULL STOP	
+…	.			…	…		Po		ellipsis				U+2026: HORIZONTAL ELLIPSIS	
+“	<			“	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
+‘	<			‘	‘		Pi		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+”	>			”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
+’	>			’	’		Pf		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+(	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS	
+)	)			)	)		Ps		parenthesis				U+0029: RIGHT PARENTHESIS	
+[	(			[	[		Pe		bracket				U+005B: LEFT SQUARE BRACKET	
+]	)			] 	]		Pe		bracket				U+005D: RIGHT SQUARE BRACKET	
+-	-			-	-		Pd		hyphen				U+2010: HYPHEN	
+‑	-			‑	‑		Pd		non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd		en dash				U+2013: EN DASH	
+—	-			—	—		Pd		em dash				U+2014: EM DASH	
+0	0			0	0		Nd		digit				U+0030: DIGIT ZERO	
+1	1			1	1		Nd		digit				U+0031: DIGIT ONE	
+2	2			2	2		Nd		digit				U+0032: DIGIT TWO	
+3	3			3	3		Nd		digit				U+0033: DIGIT THREE	
+4	4			4	4		Nd		digit				U+0034: DIGIT FOUR	
+5	5			5	5		Nd		digit				U+0035: DIGIT FIVE	
+6	6			6	6		Nd		digit				U+0036: DIGIT SIX	
+7	7			7	7		Nd		digit				U+0037: DIGIT SEVEN	
+8	8			8	8		Nd		digit				U+0038: DIGIT EIGHT	
+9	9			9	9		Nd		digit				U+0039: DIGIT NINE	
+`														
+														
+latinPanel = 'ʔ ŋ ɔ ə əu ɛ ɛa̯ ɟ ɡ ɪ ɲ ʊ ᵐb ⁿd ⁿɟ ⁿɡ'														
+														
+var cols = {														
+key: 1,														
+ipaLoc: 2,														
+ipaPlus: 3,														
+transLoc: 4,														
+transcription: 5,														
+kbd: 6,														
+class: 7,														
+status: 8,														
+typeLoc: 9,														
+statusLoc: 10,														
+														
+dvowel: 0,														
+ivowel: 0,														
+onset:0,														
+final:0,														
+subj:0,														
+ltone:0,														
+htone:0,														
+lc:0,														
+uc:0,														
+meaning:0,														
+shape: 0,														
+numLoc: 0,														
+														
+nnameLoc: 11,														
+nameLoc: 12,														
+ucsName: 13,														
+block: 14,														
+														
+//othertranscriptions: [[5, 'Latin']]														
+}														
