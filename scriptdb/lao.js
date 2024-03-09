8bcef0afe3bac1e1472d3bfe59fa50111dfe8f2d
r12a <ishida@w3.org>
2024-03-09 08:18:58 +0000

    scriptdb/lao: Feb template update.

--- scriptdb/lao.js
+++ scriptdb/lao.js
@@ -1,4 +1,4 @@
-var spreadsheet = `laoo-lo	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	equiv	native name	latin name	ucs name	simmala	block
+var spreadsheet = /* laoo-lo	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	equiv	native name	latin name	ucs name	simmala	block*/`
 \u0020	 						Zs		space					U+0020: SPACE		
 \u200B	]			␣			Cf		zero-width space					U+200B: ZERO WIDTH SPACE		✓
 -	-			-	-	{	Po		hyphen					U+2010: HYPHEN		
@@ -13,11 +13,10 @@ var spreadsheet = `laoo-lo	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 .	.			.	.	D	Po		full stop					U+002E: FULL STOP		
 ຯ	.			…	…	D	Lo		ellipsis					U+0EAF: LAO ELLIPSIS		✓
 (	(			(	(	X	Ps		parenthesis					U+0028: LEFT PARENTHESIS		
-[	(			[	[		Ps	?	bracket					U+005B: LEFT SQUARE BRACKET		
 )	)			)	)	?	Pe		parenthesis					U+0029: RIGHT PARENTHESIS		
+[	(			[	[		Ps	?	bracket					U+005B: LEFT SQUARE BRACKET		
 ]	)			]	]		Pe	?	bracket					U+005D: RIGHT SQUARE BRACKET		
 ໌	]			′		%	Mn	a	cancellation mark					U+0ECC: LAO CANCELLATION MARK		✓
-ອ	}	ʔ ɔː		ʔ	- ǭ	v	Lo		mid class vowel carrier/vowel			ອ ໂອ	ɔ̀ː ʔòː	U+0EAD: LAO LETTER O	∅	✓
 ໆ	&			&		N	Lm		repetition mark			ເຄ່ຶອງໝາຍຊ້ຳ	kʰɯaŋ-mǎːj-sâm	U+0EC6: LAO KO LA		✓
 ₭	#			¤		Z	Sc		currency symbol					U+20AD: KIP SIGN		
 %	%			%		:	Po	?	percentage mark					U+0025: PERCENT SIGN		
@@ -51,84 +50,93 @@ var spreadsheet = `laoo-lo	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 ’	>			\’	\’		Pf		quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK		
 																
 																
-າ	a	aː		ā	ā	k	Lo		long vowel					U+0EB2: LAO VOWEL SIGN AA	aa	✓
-ັ	a	a		ä	a	a	Mn		short vowel			ໄມ້ກັນ	mâj kàn	U+0EB1: LAO VOWEL SIGN MAI KAN	a	✓
-ໃ	a	aj		äʲ	ai	.	Lo		prescript long vowel			ໄມ້ມ້ວນ	mâj muân	U+0EC3: LAO VOWEL SIGN AY	ai	✓
-ໄ	a	aj		aʲ	ai	w	Lo		prescript long vowel			ໄມ້ມາຽ	mâj máːj	U+0EC4: LAO VOWEL SIGN AI	ai	✓
-ຳ	a	am		aᵐ	am	e	Lo		long vowel					U+0EB3: LAO VOWEL SIGN AM	am	✓
-ັຍ	A	-AJ		-äɲ̱	AI									U+0EB1 U+0E8D: LAO VOWEL SIGN MAI KAN, LETTER NYO	aai	
-າຍ	A	-AːJ		-āɲ̱	ĀI									U+0EB2 U+0E8D: LAO VOWEL SIGN AA, LETTER NYO	aao	
-າຽ	A	-AːJ		-āiᵊ	ĀI									U+0EB2 U+0EBD: LAO VOWEL SIGN AA, SEMIVOWEL SIGN NYO		
-ເ-ົາ	A	-AW		ē-oā	AO									U+0EC0 U+0EBB U+0EB2: LAO VOWEL SIGN E, VOWEL SIGN MAI KON, VOWEL SIGN AA	aao	
-າວ	A	-AːW		āw̱	AO									U+0EB2 U+0EA7: LAO VOWEL SIGN AA, LETTER WO		
-ະ	a¶	a		a	a		Lo		short vowel					U+0EB0: LAO VOWEL SIGN A	a	✓
-ບ	b	b- -p		b̯	b -p	p	Lo		mid class plosive			ບ ແບ້	bɔ̀ː bɛ̑ː	U+0E9A: LAO LETTER BO	b	✓
-ຈ	c	t͡ɕ-		ʨ̯	ch	9	Lo		mid class affricate			ຈ ຈອກ	tɕɔ̀ː tɕɔ̏ːk	U+0E88: LAO LETTER CO	j	✓
-ດ	d	d- -t		d̯	d -t	f	Lo		mid class plosive			ດ ເດັກ	dɔ̀ː dék	U+0E94: LAO LETTER DO	d	✓
-ເ	e	eː		ē	ē	g	Lo		prescript long vowel					U+0EC0: LAO VOWEL SIGN E	ee	✓
-ແ	E	ɛː		ɛ̄	ǣ	c	Lo		prescript long vowel					U+0EC1: LAO VOWEL SIGN EI	ɛɛ	✓
-ເ-ະ	E	-E		ē-a	E									U+0EC0 U+0EB0: LAO VOWEL SIGN E, VOWEL SIGN A	ei	
-ເ-ັ	E	-E-		ē-ä	E									U+0EC0 U+0EB1: LAO VOWEL SIGN E, VOWEL SIGN MAI KAN	e	
-ແ-ະ	E	-Ɛ		ɛ̄-a	Æ									U+0EC1 U+0EB0: LAO VOWEL SIGN EI, VOWEL SIGN A	ɛ	
-ແ-ັ	E	-Ɛ-		ɛ̄-ä	Æ									U+0EC1 U+0EB1: LAO VOWEL SIGN EI, VOWEL SIGN MAI KAN		
-ແ-ວ	E	-ƐːW		ɛ̄-w̱	ÆW									U+0EC1 U+0EA7: LAO VOWEL SIGN EI, LETTER WO	ɛɛo	
-ຝ	f	f		f	f	/	Lo		high class fricative			ຝ ຝົນ	fɔ̌ː fǒn	U+0E9D: LAO LETTER FO TAM	f	✓
-ຟ	f	f		f̱	f	2	Lo		low class fricative			ຟ ໄຟ	fɔ́ː fáj	U+0E9F: LAO LETTER FO SUNG	f	✓
-ຫ	h	h		h	h	s	Lo		high class fricative			ຫ ຫ່ານ	hɔ̌ː hāːn	U+0EAB: LAO LETTER HO SUNG	h	✓
-ຮ	h	h		ẖ	h	u	Lo		low class fricative			ຮ ເຮືອນ	hɔ́ː hɯ́ːən	U+0EAE: LAO LETTER HO TAM	h	✓
-ໜ	h	n		n	n	O	Lo		high class digraph nasal		ຫນ			U+0EDC: LAO HO NO	n	✓
-ໝ	h	m		m	m	M	Lo		high class digraph nasal		ຫມ			U+0EDD: LAO HO MO	m	✓
-ິ	i	i		i	i	t	Mn		short vowel					U+0EB4: LAO VOWEL SIGN I	i	✓
 ີ	i	iː		ī	ī	y	Mn		long vowel					U+0EB5: LAO VOWEL SIGN II	ii	✓
-ຽ	i	iːə̯		iᵊ	īa	P	Lo		vowel			ວິຣາມ	vī ráːm	U+0EBD: LAO SEMIVOWEL SIGN NYO	ia	✓
-ັຽ	I	-IƏ̯		-äiᵊ	IA									U+0EB1 U+0EBD: LAO VOWEL SIGN MAI KAN, SEMIVOWEL SIGN NYO	ia	
-ເ-ັຍ	I	-IƏ̯		ē-äɲ̱	IA									U+0EC0 U+0EB1 U+0E8D: LAO VOWEL SIGN E, VOWEL SIGN MAI KAN, LETTER NYO	ia	
-ເ-ຍ	I	-IːƏ̯		ē-ɲ̱	ĪA									U+0EC0 U+0E8D: LAO VOWEL SIGN E, LETTER NYO	ia	
-ິວ	I	-IW		-iw̱	IU									U+0EB4 U+0EA7: LAO VOWEL SIGN I, LETTER WO	iu	
-ີວ	I	-IːW		-īw̱	ĪU									U+0EB5 U+0EA7: LAO VOWEL SIGN II, LETTER WO		
-ເ-ັຍະ	I	-IA		ē-äɲ̱a										U+0EC0 U+0EB1 U+0E8D U+0EB0: LAO VOWEL SIGN E, VOWEL SIGN MAI KAN, LETTER NYO, VOWEL SIGN A	ia	
-ຽວ	I	-IːƏ̯W		-iᵊw̱	ĪEO									U+0EBD U+0EA7: LAO SEMIVOWEL SIGN NYO, LETTER WO	iao	
-ເ-ັຽວ	I			ē-äiᵊw̱	ĪEO									U+0EC0 U+0EB1 U+0EBD U+0EA7: LAO VOWEL SIGN E, VOWEL SIGN MAI KAN, SEMIVOWEL SIGN NYO, LETTER WO		
-ກ	k	k		k̯	k	d	Lo		mid class plosive			ກ ໄກ່	kɔː kāi	U+0E81: LAO LETTER KO	g	✓
-ຂ	k	kʰ- x		kʰ	kh	0	Lo		high class plosive			ຂ ໄຂ່	kʰɔ̌ː kʰāi	U+0E82: LAO LETTER KHO SUNG	k	✓
-ຄ	k	kʰ- x		ḵʰ	kh	7	Lo		low class plosive			ຄ ຄວາຢ	kʰɔ́ː kʷʰáːj	U+0E84: LAO LETTER KHO TAM	k	✓
-ລ	l	l		ḻ	l	[	Lo		low class approximant			ລ ລີງ	lɔ́ː líːŋ	U+0EA5: LAO LETTER LO LOOT	l	✓
-ຼ	l	l		l̆	l	^	Mn		high class subjoined approximant					U+0EBC: LAO SEMIVOWEL SIGN LO	l	✓
-ມ	m	m		m̱	m	,	Lo		low class nasal			ມ ແມວ	mɔ́ː mɛ́ːw	U+0EA1: LAO LETTER MO	m	✓
-ຍ	n	ɲ -j		ɲ̱	ny -j	o	Lo		low class nasal			ຍ ຍຸງ	ɲɔ́ː ɲúŋ	U+0E8D: LAO LETTER NYO	ng	✓
-ງ	n	ŋ		ŋ̱	ng		Lo		low class nasal			ງ ງົວ	ŋɔ́ː ŋúə	U+0E87: LAO LETTER NGO	ny	✓
-ນ	n¶	n		ṉ	n	i	Lo		low class nasal			ນ ນົກ	nɔ́ː nōk	U+0E99: LAO LETTER NO	n	✓
-ົ	o	o		o	o	q	Mn		short vowel			ໄມ້ກົງ	mâj kòŋ	U+0EBB: LAO VOWEL SIGN MAI KON		✓
-ໂ	o	oː		ō	ō	3	Lo		prescript long vowel					U+0EC2: LAO VOWEL SIGN O	oo	✓
-ໍ	O	ɔː		ɔ̄	ǭ	=	Mn		long vowel			ນິກຄະຫິດ	nīk kʰā hǐt	U+0ECD: LAO NIGGAHITA	ɔ	✓
-ໂ-ະ	O	-O		ō-a	O									U+0EC2 U+0EB0: LAO VOWEL SIGN O, VOWEL SIGN A	o	
-ອຍ	O	-OːJ		ʔɲ̱	OI									U+0EAD U+0E8D: LAO LETTER O, LETTER NYO		
-ເ-າະ	O	-Ɔ		ē-āa	Ǫ									U+0EC0 U+0EB2 U+0EB0: LAO VOWEL SIGN E, VOWEL SIGN AA, VOWEL SIGN A	ɔ	
-ັອ	O	-Ɔ		-äʔ	Ǫ									U+0EB1 U+0EAD: LAO VOWEL SIGN MAI KAN, LETTER O	ɔ	
-ປ	p	p-		p̯	p	x	Lo		mid class plosive			ປ ປາ	pɔ̀ː paː	U+0E9B: LAO LETTER PO	bp	✓
-ຜ	p	pʰ-		pʰ	ph	z	Lo		high class plosive			ຜ ເຜິ້ງ	pʰɔ̌ː pʰɤ̏ŋ	U+0E9C: LAO LETTER PHO SUNG	p	✓
-ພ	p	pʰ-		p̱ʰ	ph	r	Lo		low class consonant			ພ ພູ	pʰɔ́ː pʰúː	U+0E9E: LAO LETTER PHO TAM	p	✓
-ຣ	r	l -n		ṟ	r -n	I	Lo	l	low class approximant			ຣະຄັງ	rākʰáŋ	U+0EA3: LAO LETTER LO LING		✓
-ສ	s	s		s	s	l	Lo		high class fricative			ສ ເສືອ	sɔ̌ː sɯ̌ːə	U+0EAA: LAO LETTER SO SUNG	s	✓
-ຊ	s	s		s̱	x	-	Lo		low class fricative			ຊ ຊ້າງ	sɔ́ː sâːŋ	U+0E8A: LAO LETTER SO TAM	s	✓
-ຕ	t	t		t̯	t	8	Lo		mid class plosive			ຕ ຕາ	tɔ̀ː tàː	U+0E95: LAO LETTER TO	dt	✓
-ຖ	t	tʰ-		tʰ	th	4	Lo		high class plosive			ຖ ຖົງ	tʰɔ̌ː tʰǒŋ	U+0E96: LAO LETTER THO SUNG	t	✓
-ທ	t	tʰ-		ṯʰ	th	m	Lo		low class plosive			ທ ທຸງ	tʰɔ́ː tʰúŋ	U+0E97: LAO LETTER THO TAM	t	✓
-ຶ	U	ɯ		ɯ	ư	b	Mn		short vowel					U+0EB6: LAO VOWEL SIGN Y	ʉ	✓
+ິ	i	i		i	i	t	Mn		short vowel					U+0EB4: LAO VOWEL SIGN I	i	✓
+ຽ	i	iːə̯		iᵊ	īa	P	Lo		diphthong	Closed syllables.		ວິຣາມ	vī ráːm	U+0EBD: LAO SEMIVOWEL SIGN NYO	ia	✓
+ັຽ	i	-IƏ̯		-äiᵊ	IA				diphthong	Closed syllables.				U+0EB1 U+0EBD: LAO VOWEL SIGN MAI KAN, SEMIVOWEL SIGN NYO	ia	
+ເ◌ຍ	i	-IːƏ̯		ē-ɲ̱	ĪA				diphthong					U+0EC0 U+0E8D: LAO VOWEL SIGN E, LETTER NYO	ia	
+ເ◌ັຍ	i	-IƏ̯		ē-äɲ̱	IA				diphthong					U+0EC0 U+0EB1 U+0E8D: LAO VOWEL SIGN E, VOWEL SIGN MAI KAN, LETTER NYO	ia	
+ີວ		-IːW		-īw̱	ĪU				diphthong					U+0EB5 U+0EA7: LAO VOWEL SIGN II, LETTER WO		
+ິວ		-IW		-iw̱	IU				diphthong					U+0EB4 U+0EA7: LAO VOWEL SIGN I, LETTER WO	iu	
+ຽວ		-IːƏ̯W		-iᵊw̱	ĪEO				triphthong	Closed syllables.				U+0EBD U+0EA7: LAO SEMIVOWEL SIGN NYO, LETTER WO	iao	
+ເ◌ັຽວ		-IƏ̯W		ē-äiᵊw̱	ĪEO				diphthong					U+0EC0 U+0EB1 U+0EBD U+0EA7: LAO VOWEL SIGN E, VOWEL SIGN MAI KAN, SEMIVOWEL SIGN NYO, LETTER WO		
+ເ◌ັຍະ	I	-IA		ē-äɲ̱a					diphthong					U+0EC0 U+0EB1 U+0E8D U+0EB0: LAO VOWEL SIGN E, VOWEL SIGN MAI KAN, LETTER NYO, VOWEL SIGN A	ia	
 ື	U	ɯː		ɯ̄	ư̄	n	Mn		long vowel					U+0EB7: LAO VOWEL SIGN YY	ʉʉ	✓
-ຸ	u	u		u	u	5	Mn		short vowel					U+0EB8: LAO VOWEL SIGN U	u	✓
+ຶ	U	ɯ		ɯ	ư	b	Mn		short vowel					U+0EB6: LAO VOWEL SIGN Y	ʉ	✓
+ເ◌ືອ	U	-ƜːƏ̯		ē-ɯ̄ʔ	Ư̄A				diphthong					U+0EC0 U+0EB7 U+0EAD: LAO VOWEL SIGN E, VOWEL SIGN YY, LETTER O	ʉa	
+ເ◌ຶອ	U	-ƜƏ̯		ē-ɯʔ	ƯA				diphthong					U+0EC0 U+0EB6 U+0EAD: LAO VOWEL SIGN E, VOWEL SIGN Y, LETTER O	ʉa	
 ູ	u	uː		ū	ū	6	Mn		long vowel					U+0EB9: LAO VOWEL SIGN UU	uu	✓
-ເ-ຶອ	U	-ƜƏ̯		ē-ɯʔ	ƯA									U+0EC0 U+0EB6 U+0EAD: LAO VOWEL SIGN E, VOWEL SIGN Y, LETTER O	ʉa	
-ເ-ືອ	U	-ƜːƏ̯		ē-ɯ̄ʔ	Ư̄A									U+0EC0 U+0EB7 U+0EAD: LAO VOWEL SIGN E, VOWEL SIGN YY, LETTER O	ʉa	
-ັວ	U	-UƏ̯-		äw̱-	ŪA									U+0EB1 U+0EA7: LAO VOWEL SIGN MAI KAN, LETTER WO	ua	
-ົວະ	U	-UƏ̯		-ow̱a	UA									U+0EBB U+0EA7 U+0EB0: LAO VOWEL SIGN MAI KON, LETTER WO, VOWEL SIGN A		
-ົວ	U	-UːƏ̯		-ow̱-	ŪA									U+0EBB U+0EA7: LAO VOWEL SIGN MAI KON, LETTER WO	ua	
-ເ-ິ	V	-ɤ		ē-i	Œ									U+0EC0 U+0EB4: LAO VOWEL SIGN E, VOWEL SIGN I	ə	
-ເ-ີ	V	-ɤː		ē-ī	Œ̄									U+0EC0 U+0EB5: LAO VOWEL SIGN E, VOWEL SIGN II	əə	
-ເ-ີຽ	V	-ɤːj		ē-īiᵊ	Œ̄Y									U+0EC0 U+0EB5 U+0EBD: LAO VOWEL SIGN E, VOWEL SIGN II, SEMIVOWEL SIGN NYO		
-ເ-ີຍ	V	-ɤːj		ē-īɲ̱	Œ̄Y									U+0EC0 U+0EB5 U+0E8D: LAO VOWEL SIGN E, VOWEL SIGN II, LETTER NYO	əəi	
-ວ	w	ʋ w -w -uːə-		w̱	v ūa	;	Lo		low class approximant/closed vowel			ວ ວີ	vɔ́ː víː	U+0EA7: LAO LETTER WO	w ua o	✓
-ຢ	y	j		y̯	y	1	Lo		mid class approximant			ຢ ຢາ	jɔ̀ː jaː	U+0EA2: LAO LETTER YO	y	✓
+ຸ	u	u		u	u	5	Mn		short vowel					U+0EB8: LAO VOWEL SIGN U	u	✓
+ົວ	u	-UːƏ̯		-ow̱-	ŪA				diphthong					U+0EBB U+0EA7: LAO VOWEL SIGN MAI KON, LETTER WO	ua	
+ັວ	u	-UƏ̯-		äw̱-	ŪA				diphthong					U+0EB1 U+0EA7: LAO VOWEL SIGN MAI KAN, LETTER WO	ua	
+ວາ	u	-UːƏ̯		-w̱ā-					diphthong					U+0EA7 U+0EB2: LAO LETTER WO, LAO VOWEL SIGN AA		
+◌ົວະ	u	-UƏ̯		-ow̱a	UA				diphthong					U+0EBB U+0EA7 U+0EB0: LAO VOWEL SIGN MAI KON, LETTER WO, VOWEL SIGN A		
+ເ	e	eː		ē	ē	g	Lo		prescript long vowel					U+0EC0: LAO VOWEL SIGN E	ee	✓
+ເ◌ະ	e	-E		ē-a	E				short vowel					U+0EC0 U+0EB0: LAO VOWEL SIGN E, VOWEL SIGN A	ei	
+ເ◌ັ	e	-E-		ē-ä	E				short vowel					U+0EC0 U+0EB1: LAO VOWEL SIGN E, VOWEL SIGN MAI KAN	e	
+ໂ	o	oː		ō	ō	3	Lo		prescript long vowel					U+0EC2: LAO VOWEL SIGN O	oo	✓
+ົ	o	o		o	o	q	Mn		short vowel			ໄມ້ກົງ	mâj kòŋ	U+0EBB: LAO VOWEL SIGN MAI KON		✓
+ໂ◌ະ	o	-O		ō-a	O				short vowel					U+0EC2 U+0EB0: LAO VOWEL SIGN O, VOWEL SIGN A	o	
+ແ	E	ɛː		ɛ̄	ǣ	c	Lo		prescript long vowel					U+0EC1: LAO VOWEL SIGN EI	ɛɛ	✓
+ແ◌ະ	E	-Ɛ		ɛ̄-a	Æ				short vowel					U+0EC1 U+0EB0: LAO VOWEL SIGN EI, VOWEL SIGN A	ɛ	
+ແ◌ັ	E	-Ɛ-		ɛ̄-ä	Æ				short vowel					U+0EC1 U+0EB1: LAO VOWEL SIGN EI, VOWEL SIGN MAI KAN		
+ແ◌ວ		-ƐːW		ɛ̄-w̱	ÆW				diphthong					U+0EC1 U+0EA7: LAO VOWEL SIGN EI, LETTER WO	ɛɛo	
+ອ	}	ʔ ɔː		ʔ	- ǭ	v	Lo		mid class vowel carrier/long vowel			ອ ໂອ	ɔ̀ː ʔòː	U+0EAD: LAO LETTER O	∅	✓
+ໍ	O	ɔː		ɔ̄	ǭ	=	Mn		long vowel			ນິກຄະຫິດ	nīk kʰā hǐt	U+0ECD: LAO NIGGAHITA	ɔ	✓
+ເ◌າະ	O	-Ɔ		ē-āa	Ǫ				short vowel					U+0EC0 U+0EB2 U+0EB0: LAO VOWEL SIGN E, VOWEL SIGN AA, VOWEL SIGN A	ɔ	
+◌ັອ	O	-Ɔ		-äʔ	Ǫ				short vowel					U+0EB1 U+0EAD: LAO VOWEL SIGN MAI KAN, LETTER O	ɔ	
+ອຍ		-ƆːJ		ʔɲ̱	OI				diphthong					U+0EAD U+0E8D: LAO LETTER O, LETTER NYO		
+ເ◌ີ	V	-ɤː		ē-ī	Œ̄				long vowel					U+0EC0 U+0EB5: LAO VOWEL SIGN E, VOWEL SIGN II	əə	
+ເ◌ິ	V	-ɤ		ē-i	Œ				short vowel					U+0EC0 U+0EB4: LAO VOWEL SIGN E, VOWEL SIGN I	ə	
+ເ◌ີຽ		-ɤːj		ē-īiᵊ	Œ̄Y				diphthong					U+0EC0 U+0EB5 U+0EBD: LAO VOWEL SIGN E, VOWEL SIGN II, SEMIVOWEL SIGN NYO		
+ເ◌ີຍ		-ɤːj		ē-īɲ̱	Œ̄Y				diphthong					U+0EC0 U+0EB5 U+0E8D: LAO VOWEL SIGN E, VOWEL SIGN II, LETTER NYO	əəi	
+າ	a	aː		ā	ā	k	Lo		long vowel					U+0EB2: LAO VOWEL SIGN AA	aa	✓
+ະ	a	a		a	a		Lo		short vowel					U+0EB0: LAO VOWEL SIGN A	a	✓
+ັ	a	a		ä	a	a	Mn		short vowel			ໄມ້ກັນ	mâj kàn	U+0EB1: LAO VOWEL SIGN MAI KAN	a	✓
+ຳ	a	am		aᵐ	am	e	Lo		rhyme					U+0EB3: LAO VOWEL SIGN AM	am	✓
+າຍ		-AːJ		-āɲ̱	ĀI				diphthong					U+0EB2 U+0E8D: LAO VOWEL SIGN AA, LETTER NYO	aao	
+ັຍ		-AJ		-äɲ̱	AI				diphthong					U+0EB1 U+0E8D: LAO VOWEL SIGN MAI KAN, LETTER NYO	aai	
+າຽ		-AːJ		-āiᵊ	ĀI				diphthong					U+0EB2 U+0EBD: LAO VOWEL SIGN AA, SEMIVOWEL SIGN NYO		
+ໃ	a	aj		äʲ	ai	.	Lo		prescript long diphthong			ໄມ້ມ້ວນ	mâj muân	U+0EC3: LAO VOWEL SIGN AY	ai	✓
+ໄ	a	aj		aʲ	ai	w	Lo		prescript long diphthong			ໄມ້ມາຽ	mâj máːj	U+0EC4: LAO VOWEL SIGN AI	ai	✓
+າວ		-AːW		āw̱	AO				diphthong					U+0EB2 U+0EA7: LAO VOWEL SIGN AA, LETTER WO		
+ເ◌ົາ	a	-AW		ē-oā	AO				diphthong					U+0EC0 U+0EBB U+0EB2: LAO VOWEL SIGN E, VOWEL SIGN MAI KON, VOWEL SIGN AA	aao	
+																
+ບ	b	b- -p		b̯	b -p	p	Lo		mid class consonant			ບ ແບ້	bɔ̀ː bɛ̑ː	U+0E9A: LAO LETTER BO	b	✓
+ຈ	c	t͡ɕ-		ʨ̯	ch	9	Lo		mid class consonant			ຈ ຈອກ	tɕɔ̀ː tɕɔ̏ːk	U+0E88: LAO LETTER CO	j	✓
+ດ	d	d- -t		d̯	d -t	f	Lo		mid class consonant			ດ ເດັກ	dɔ̀ː dék	U+0E94: LAO LETTER DO	d	✓
+ຝ	f	f		f	f	/	Lo		high class consonant			ຝ ຝົນ	fɔ̌ː fǒn	U+0E9D: LAO LETTER FO TAM	f	✓
+ຟ	f	f		f̱	f	2	Lo		low class consonant			ຟ ໄຟ	fɔ́ː fáj	U+0E9F: LAO LETTER FO SUNG	f	✓
+ຫ	h	h		h	h	s	Lo		high class consonant			ຫ ຫ່ານ	hɔ̌ː hāːn	U+0EAB: LAO LETTER HO SUNG	h	✓
+ຮ	h	h		ẖ	h	u	Lo		low class consonant			ຮ ເຮືອນ	hɔ́ː hɯ́ːən	U+0EAE: LAO LETTER HO TAM	h	✓
+ກ	k	k		k̯	k	d	Lo		mid class consonant	syllable initial & final		ກ ໄກ່	kɔː kāi	U+0E81: LAO LETTER KO	g	✓
+ຂ	k	kʰ- x		kʰ	kh	0	Lo		high class consonant	syllable-initial only		ຂ ໄຂ່	kʰɔ̌ː kʰāi	U+0E82: LAO LETTER KHO SUNG	k	✓
+ຄ	k	kʰ- x		ḵʰ	kh	7	Lo		low class consonant	syllable-initial only		ຄ ຄວາຢ	kʰɔ́ː kʷʰáːj	U+0E84: LAO LETTER KHO TAM	k	✓
+ລ	l	l		ḻ	l	[	Lo		low class consonant			ລ ລີງ	lɔ́ː líːŋ	U+0EA5: LAO LETTER LO LOOT	l	✓
+ຫຼ	l	l		hl̆	l	}			high class digraph					U+0EAB U+0EBC: LAO LETTER HO SUNG, SEMIVOWEL SIGN LO	l	
+ຫລ	l	l		hḻ	l				high class digraph					U+0EAB U+0EA5: LAO LETTER HO SUNG, LETTER LO LOOT	l	
+ຼ	l	l		l̆	l	^	Mn		high class subjoined consonant					U+0EBC: LAO SEMIVOWEL SIGN LO	l	✓
+ມ	m	m		m̱	m	,	Lo		low class consonant			ມ ແມວ	mɔ́ː mɛ́ːw	U+0EA1: LAO LETTER MO	m	✓
+ໝ	m	m		m	m	M	Lo		atomic high class consonant digraph		ຫມ			U+0EDD: LAO HO MO	m	✓
+ຫມ	m	m		hm̱	m				high class digraph					U+0EAB U+0EA1: LAO LETTER HO SUNG, LETTER MO	m	
+ນ	n	n		ṉ	n	i	Lo		low class consonant			ນ ນົກ	nɔ́ː nōk	U+0E99: LAO LETTER NO	n	✓
+ໜ	n	n		n	n	O	Lo		atomic high class consonant digraph		ຫນ			U+0EDC: LAO HO NO	n	✓
+ຫນ	n	n		hṉ	n				high class digraph					U+0EAB U+0E99: LAO LETTER HO SUNG, LETTER NO	n	
+ຍ	n	ɲ -j		ɲ̱	ny -j	o	Lo		low class consonant			ຍ ຍຸງ	ɲɔ́ː ɲúŋ	U+0E8D: LAO LETTER NYO	ng	✓
+ຫຍ	n	ɲ		hɲ̱	j				high class digraph					U+0EAB U+0E8D: LAO LETTER HO SUNG, LETTER NYO	ny	
+ງ	n	ŋ		ŋ̱	ng		Lo		low class consonant	syllable initial & final		ງ ງົວ	ŋɔ́ː ŋúə	U+0E87: LAO LETTER NGO	ny	✓
+ຫງ	n	ŋ		hŋ̱	ng				high class digraph					U+0EAB U+0E87: LAO LETTER HO SUNG, LETTER NGO	ng	
+ປ	p	p-		p̯	p	x	Lo		mid class consonant			ປ ປາ	pɔ̀ː paː	U+0E9B: LAO LETTER PO	bp	✓
+ຜ	p	pʰ-		pʰ	ph	z	Lo		high class consonant			ຜ ເຜິ້ງ	pʰɔ̌ː pʰɤ̏ŋ	U+0E9C: LAO LETTER PHO SUNG	p	✓
+ພ	p	pʰ-		p̱ʰ	ph	r	Lo		low class consonant			ພ ພູ	pʰɔ́ː pʰúː	U+0E9E: LAO LETTER PHO TAM	p	✓
+ຣ	r	l -n		ṟ	r -n	I	Lo	l	low class consonant			ຣະຄັງ	rākʰáŋ	U+0EA3: LAO LETTER LO LING		✓
+ສ	s	s		s	s	l	Lo		high class consonant			ສ ເສືອ	sɔ̌ː sɯ̌ːə	U+0EAA: LAO LETTER SO SUNG	s	✓
+ຊ	s	s		s̱	x	-	Lo		low class consonant			ຊ ຊ້າງ	sɔ́ː sâːŋ	U+0E8A: LAO LETTER SO TAM	s	✓
+ຕ	t	t		t̯	t	8	Lo		mid class consonant			ຕ ຕາ	tɔ̀ː tàː	U+0E95: LAO LETTER TO	dt	✓
+ຖ	t	tʰ-		tʰ	th	4	Lo		high class consonant			ຖ ຖົງ	tʰɔ̌ː tʰǒŋ	U+0E96: LAO LETTER THO SUNG	t	✓
+ທ	t	tʰ-		ṯʰ	th	m	Lo		low class consonant			ທ ທຸງ	tʰɔ́ː tʰúŋ	U+0E97: LAO LETTER THO TAM	t	✓
+ວ	w	ʋ w -w -uːə-		w̱	v ūa	;	Lo		low class consonant/closed vowel			ວ ວີ	vɔ́ː víː	U+0EA7: LAO LETTER WO	w ua o	✓
+ຢ	y	j		y̯	y	1	Lo		mid class consonant			ຢ ຢາ	jɔ̀ː jaː	U+0EA2: LAO LETTER YO	y	✓
 																
 ັ້				ä²		Q								U+0EB1 U+0EC9: LAO VOWEL SIGN MAI KAN, TONE MAI THO		
 ິ້				i²		Y								U+0EB4 U+0EC9: LAO VOWEL SIGN I, TONE MAI THO		
@@ -137,31 +145,25 @@ var spreadsheet = `laoo-lo	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 ຶ້				ɯ²		V								U+0EB6 U+0EC9: LAO VOWEL SIGN Y, TONE MAI THO		
 ື້				ɯ̄²		B								U+0EB7 U+0EC9: LAO VOWEL SIGN YY, TONE MAI THO		
 																
-ຫງ		ŋ		hŋ̱	ng				high class digraph					U+0EAB U+0E87: LAO LETTER HO SUNG, LETTER NGO	ng	
-ຫຍ		ɲ		hɲ̱	j				high class digraph					U+0EAB U+0E8D: LAO LETTER HO SUNG, LETTER NYO	ny	
-ຫນ		n		hṉ	n				high class digraph					U+0EAB U+0E99: LAO LETTER HO SUNG, LETTER NO	n	
-ຫມ		m		hm̱	m				high class digraph					U+0EAB U+0EA1: LAO LETTER HO SUNG, LETTER MO	m	
-ຫຼ		l		hl̆	l	}			high class digraph					U+0EAB U+0EBC: LAO LETTER HO SUNG, SEMIVOWEL SIGN LO	l	
-ຫລ		l		hḻ	l				high class digraph					U+0EAB U+0EA5: LAO LETTER HO SUNG, LETTER LO LOOT	l	
 ຫວ		ʋ w		hw̱	v w				high class digraph					U+0EAB U+0EA7: LAO LETTER HO SUNG, LETTER WO	w	
 																
-ໆລໆ		la		&ḻ&	la							ເຄຶ່ອງໝາຍ ແລະອຶ່ນໆ	kʰɯaŋ-mǎːj-lɛ-ɯːn-ɯːn	U+0EC6 U+0EA5 U+0EC6: LAO KO LA, LAO LETTER LO LOOT, LAO KO LA		
+ໆລໆ		la		&ḻ&	la				et cetera logograph			ເຄຶ່ອງໝາຍ ແລະອຶ່ນໆ	kʰɯaŋ-mǎːj-lɛ-ɯːn-ɯːn	U+0EC6 U+0EA5 U+0EC6: LAO KO LA, LAO LETTER LO LOOT, LAO KO LA		
 																
-ຆ							Lo	u		pali				U+0E86: LAO LETTER PALI GHA		✓
-ຉ							Lo	u		pali				U+0E89: LAO LETTER PALI CHA		✓
-ຌ							Lo	u		pali				U+0E8C: LAO LETTER PALI JHA		✓
-ຎ							Lo	u		pali				U+0E8E: LAO LETTER PALI NYA		✓
-ຏ				...			Lo	u		pali				U+0E8F: LAO LETTER PALI TTA		✓
-ຐ							Lo	u		pali				U+0E90: LAO LETTER PALI TTHA		✓
-ຑ							Lo	u		pali				U+0E91: LAO LETTER PALI DDA		✓
-ຒ							Lo	u		pali				U+0E92: LAO LETTER PALI DDHA		✓
-ຓ							Lo	u		pali				U+0E93: LAO LETTER PALI NNA		✓
-ຘ							Lo	u		pali				U+0E98: LAO LETTER PALI DHA		✓
-ຠ							Lo	u		pali				U+0EA0: LAO LETTER PALI BHA		✓
-ຨ							Lo	u		pali				U+0EA8: LAO LETTER SANSKRIT SHA		✓
-ຩ							Lo	u		pali				U+0EA9: LAO LETTER SANSKRIT SSA		✓
-ຬ							Lo	u		pali				U+0EAC: LAO LETTER PALI LLA		✓
-຺							Mn	u		pali				U+0EBA: LAO SIGN PALI VIRAMA		✓
+ຆ							Lo	u	consonant	pali				U+0E86: LAO LETTER PALI GHA		✓
+ຉ							Lo	u	consonant	pali				U+0E89: LAO LETTER PALI CHA		✓
+ຌ							Lo	u	consonant	pali				U+0E8C: LAO LETTER PALI JHA		✓
+ຎ							Lo	u	consonant	pali				U+0E8E: LAO LETTER PALI NYA		✓
+ຏ				...			Lo	u	consonant	pali				U+0E8F: LAO LETTER PALI TTA		✓
+ຐ							Lo	u	consonant	pali				U+0E90: LAO LETTER PALI TTHA		✓
+ຑ							Lo	u	consonant	pali				U+0E91: LAO LETTER PALI DDA		✓
+ຒ							Lo	u	consonant	pali				U+0E92: LAO LETTER PALI DDHA		✓
+ຓ							Lo	u	consonant	pali				U+0E93: LAO LETTER PALI NNA		✓
+ຘ							Lo	u	consonant	pali				U+0E98: LAO LETTER PALI DHA		✓
+ຠ							Lo	u	consonant	pali				U+0EA0: LAO LETTER PALI BHA		✓
+ຨ							Lo	u	consonant	pali				U+0EA8: LAO LETTER SANSKRIT SHA		✓
+ຩ							Lo	u	consonant	pali				U+0EA9: LAO LETTER SANSKRIT SSA		✓
+ຬ							Lo	u	consonant	pali				U+0EAC: LAO LETTER PALI LLA		✓
+຺							Mn	u	consonant	pali				U+0EBA: LAO SIGN PALI VIRAMA		✓
 																
 …					…	C	Po		ellipsis					U+2026: HORIZONTAL ELLIPSIS		
 																
@@ -173,9 +175,10 @@ var spreadsheet = `laoo-lo	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 ″				″	″		Po	?	double prime					U+2033: DOUBLE PRIME		
 *						E	Po	?								
 _						R	Pc	?								
+◌														C		
 `																
 																
-latinPanel = '˨˦ ˦ ˥˧ ˧ ǣ aː ā ə eː ē ɛː iː ī iːə̯ īa kʰ ɲ ŋ ɔː oː ō ǭ pʰ tʰ t͡ɕ uː ūa ư ư̄ ū ʋ ɤ ɯ ɯː ʔ'																
+latinPanel = '˨˦ ˦ ˥˧ ˧  ᷆ ̌ ̄ ́ ̂ ̀ ǣ aː ā ə eː ē ɛː iː ī iːə̯ īa kʰ ɲ ŋ ɔː oː ō ǭ pʰ tʰ t͡ɕ uː ūa ư ư̄ ū ʋ ɤ ɯ ɯː ʔ'																
 																
 var cols = {																
 key: 1,																
