bc102c4591a532ded759762e7b0e721f287a7420
r12a <ishida@w3.org>
2024-03-25 11:17:26 +0000

    scriptdb/mong-mn: Feb template update.

--- scriptdb/mong-mn.js
+++ scriptdb/mong-mn.js
@@ -1,147 +1,150 @@
-var spreadsheet = `mong-mn	key	khalkha ipa	ipa+	translit	Joint Sino-Mongolian	kbd	class	status	type	usage	native name	latin name	ucs name	cyrillic	block	chakhar ipa
-᠊	_			_	–		Po		baseline extender				U+180A: MONGOLIAN NIRUGU		✓	
--	-			-	-				hyphen				U+2010: HYPHEN			
-‑	-			‑	‑		Pd	?	non-breaking hyphen				U+2011: NON-BREAKING HYPHEN			
-\u1806	-			–͇			Pd		hyphen				U+1806: MONGOLIAN TODO SOFT HYPHEN		✓	
-–	-			–	–		Pd	?	en dash				U+2013: EN DASH			
-—	-			—	—		Pd	?	em dash				U+2014: EM DASH			
-、	,			⹁	,		Po	?	ideographic comma				U+3001: IDEOGRAPHIC COMMA			
-᠂	,¶			,	,	,	Po		comma				U+1802: MONGOLIAN COMMA		✓	
-;	;			;	;		Po	?	semicolon				U+003B: SEMICOLON			
-᠄	:			:	:		Po		colon				U+1804: MONGOLIAN COLON		✓	
-！	!			!	!		Po		exclamation mark				U+FF01: FULLWIDTH EXCLAMATION MARK			
-⁉	!			⁉			Po	?	exclamation-question mark				U+2049: EXCLAMATION QUESTION MARK			
-？	?			?	?		Po		question mark				U+FF1F: FULLWIDTH QUESTION MARK			
-⁈	?			⁈			Po	?	question-exclamation mark				U+2048: QUESTION EXCLAMATION MARK			
-᠃	.			.	.	.	Po		full stop				U+1803: MONGOLIAN FULL STOP		✓	
-。	.			•	.		Po	?	ideographic full stop				U+3002: IDEOGRAPHIC FULL STOP			
-᠁	.			…	…	Q	Po		ellipsis				U+1801: MONGOLIAN ELLIPSIS		✓	
-（	(			(			Ps		fullwidth parenthesis				U+FF08: FULLWIDTH LEFT PARENTHESIS			
-）	)			)			Pe		fullwidth parenthesis				U+FF09: FULLWIDTH RIGHT PARENTHESIS			
-(	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS			
-[	(			[	[		Ps	?	bracket				U+005B: LEFT SQUARE BRACKET			
-)	)			)	)		Pe		parenthesis				U+0029: RIGHT PARENTHESIS			
-]	)			]	]		Pe	?	bracket				U+005D: RIGHT SQUARE BRACKET			
-\u180E	[			ᵐᵛˢ	_		Cf		mvs				U+180E: MONGOLIAN VOWEL SEPARATOR		✓	
-\u202F	]			ⁿⁿᵇˢᵖ	-		Zs		nnbsp				U+202F NARROW NO-BREAK SPACE		✓	
-%	%			%			Po	?	percentage mark				U+0025: PERCENT SIGN			
-‰	%			‰			Po	?	per mille mark				U+2030: PER MILLE SIGN			
-\u180B	^			ᶠᵛˢ¹			Mn		fvs1				U+180B: MONGOLIAN FREE VARIATION SELECTOR ONE		✓	
-\u180C	^			ᶠᵛˢ²			Mn		fvs2				U+180C: MONGOLIAN FREE VARIATION SELECTOR TWO		✓	
-\u180D	^			ᶠᵛˢ³			Mn		fvs3				U+180D: MONGOLIAN FREE VARIATION SELECTOR THREE		✓	
-\u180F	^			ᶠᵛˢ4			Mn		fvs4				U+180F: MONGOLIAN FREE VARIATION SELECTOR FOUR			
-᠐	±			0̣	0	0	Nd		digit				U+1810: MONGOLIAN DIGIT ZERO		✓	
-᠑	±			1̣	1	1	Nd		digit				U+1811: MONGOLIAN DIGIT ONE		✓	
-᠒	±			2̣	2	2	Nd		digit				U+1812: MONGOLIAN DIGIT TWO		✓	
-᠓	±			3̣	3	3	Nd		digit				U+1813: MONGOLIAN DIGIT THREE		✓	
-᠔	±			4̣	4	4	Nd		digit				U+1814: MONGOLIAN DIGIT FOUR		✓	
-᠕	±			5̣	5	5	Nd		digit				U+1815: MONGOLIAN DIGIT FIVE		✓	
-᠖	±			6̣	6	6	Nd		digit				U+1816: MONGOLIAN DIGIT SIX		✓	
-᠗	±			7̣	7	7	Nd		digit				U+1817: MONGOLIAN DIGIT SEVEN		✓	
-᠘	±			8̣	8	8	Nd		digit				U+1818: MONGOLIAN DIGIT EIGHT		✓	
-᠙	±			9̣	9	9	Nd		digit				U+1819: MONGOLIAN DIGIT NINE		✓	
-《	<			{	“		Ps		quotation mark				U+300A: LEFT DOUBLE ANGLE BRACKET			
-«	<			«	“	:	Pi		quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK			
-〈	<			<	“		Ps		quotation mark				U+3008: LEFT ANGLE BRACKET			
-‹	<			‹			Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK			
-“	<			“	“		Pi	?	quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK			
-‘	<			\‘	\‘		Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK			
-〔	<			[			Ps		bracket				U+3014: LEFT TORTOISE SHELL BRACKET			
-\u200C	=			ᶻʷⁿʲ			Cf		zwnj				U+200C: ZERO WIDTH NON-JOINER			
-\u200D	=			ᶻʷʲ			Cf		zwj				U+200D: ZERO WIDTH JOINER			
-\u2067	=			ʳˡⁱ			Cf	u	rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE			
-\u2066	=			ˡʳⁱ			Cf	u	ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE			
-\u2068	=			ᶠˢⁱ			Cf	u	first-strong isolate				U+2068: FIRST STRONG ISOLATE			
-\u2069	=			ᵖᵈⁱ			Cf	u	pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE			
-\u202B	=			ʳˡᵉ			Cf	u	rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING			
-\u202A	=			ˡʳᵉ			Cf	u	ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING			
-\u202C	=			ᵖᵈᶠ			Cf	u	pop direction				U+202C: POP DIRECTIONAL FORMATTING			
-\u200F	=			ʳˡᵐ			Cf	u	rtl mark				U+200F: RIGHT-TO-LEFT MARK			
-\u200E	=			ˡʳᵐ			Cf	u	ltr mark				U+200E: LEFT-TO-RIGHT MARK			
-\u034F	=			ᶜᵍʲ			Mn	?	combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER			
-\u061C	=			ᵃˡᵐ			Cf	u	arabic letter mark				U+061C: ARABIC LETTER MARK			
-》	>			}	”		Pe		quotation mark				U+300B: RIGHT DOUBLE ANGLE BRACKET			
-»	>			»	”	;	Pf		quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK			
-〉	>			>	”		Pe		quotation mark				U+3009: RIGHT ANGLE BRACKET			
-›	>			›			Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK			
-”	>			”	”		Pf	?	quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK			
-’	>			\’	\’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK			
-〕	>			]			Pe		bracket				U+3015: RIGHT TORTOISE SHELL BRACKET			
-᠀	|			¶	»		Po	?	section mark				U+1800: MONGOLIAN BIRGA		✓	
-᠅	|			|	«		Po	?	section mark				U+1805: MONGOLIAN FOUR DOTS		✓	
-																
-																
-ᠠ	a	a	ə	a	a	a	Lo		vowel				U+1820: MONGOLIAN LETTER A	а	✓	ɑ
-ᠪ	b	p w̜		p	b	b	Lo		consonant				U+182A: MONGOLIAN LETTER BA	б	✓	b
-ᠴ	c	t͡sʰ t͡ʃʰ		ʦʰ	č	q	Lo		consonant				U+1834: MONGOLIAN LETTER CHA	ч ц	✓	ʧʰ
-ᡂ	c	ɕ		ɕ	ĉ	C	Lo	l	consonant	for transliteration			U+1842: MONGOLIAN LETTER CHI	ч	✓	ʈʂʰɻ
-ᠳ	d	t		t	d	d	Lo		consonant				U+1833: MONGOLIAN LETTER DA	д	✓	d
-ᠵ	D	t͡s t͡ʃ		ʦ	ǰ	j	Lo		consonant				U+1835: MONGOLIAN LETTER JA	ж з	✓	ʤ
-ᠽ	d	t͡s		ʦ̣	z		Lo	l	consonant	for transliteration			U+183D: MONGOLIAN LETTER ZA	з	✓	ʣ
-ᠡ	e	e i	ə ∅	e	e	e	Lo		vowel				U+1821: MONGOLIAN LETTER E	є	✓	ə
-ᠧ	E	i e	ə	i	ë	e	Lo		vowel				U+1827: MONGOLIAN LETTER EE	э	✓	ə
-ᠹ	f	f		f	f	f	Lo	l	consonant	for transliteration			U+1839: MONGOLIAN LETTER FA	ф	✓	f
-ᠭ	g	ɢ ɡ k	∅ ɣ	ɢ	g	g	Lo		consonant				U+182D: MONGOLIAN LETTER GA	г	✓	ɣ ɡ
-ᠾ	h	h		h	h	H	Lo	l	consonant	for transliteration			U+183E: MONGOLIAN LETTER HAA	х	✓	x
-ᠢ	i	i	ə ∅	i	i	i	Lo		vowel				U+1822: MONGOLIAN LETTER I	и й ы ь	✓	i ɪ
-ᠺ	k	k		k	k	k	Lo	l	consonant	for transliteration			U+183A: MONGOLIAN LETTER KA	к	✓	k
-ᠻ	k	k		ḳ	ḳ	K	Lo	l	consonant	for transliteration			U+183B: MONGOLIAN LETTER KHA		✓	
-ᠯ	l	ɮ		l	l	l	Lo		consonant				U+182F: MONGOLIAN LETTER LA	л	✓	l
-ᡀ	l	lʰ		lʰ	lh		Lo	l	consonant	for transliteration			U+1840: MONGOLIAN LETTER LHA	лх	✓	lh
-ᠮ	m	m		m	m	m	Lo		consonant				U+182E: MONGOLIAN LETTER MA	м	✓	m
-ᠨ	n	n -ŋ		n	n	n	Lo		consonant				U+1828: MONGOLIAN LETTER NA	н	✓	n ŋ
-ᠩ	N	ŋ		ŋ	ng	N	Lo		consonant				U+1829: MONGOLIAN LETTER ANG	нг	✓	ŋ
-ᠣ	o	ɔ	ə ∅	ɔ	o	W	Lo		vowel				U+1823: MONGOLIAN LETTER O	о	✓	ɔ
-ᠥ	O	o	ə ∅	o	ö	o	Lo		vowel				U+1825: MONGOLIAN LETTER OE	ө	✓	o
-ᠫ	p	pʰ		pʰ	p	p	Lo		consonant				U+182B: MONGOLIAN LETTER PA	п	✓	p
-ᠷ	r	r		r	r	r	Lo		consonant				U+1837: MONGOLIAN LETTER RA	р	✓	r
-ᠰ	s	s ʃ		s	s	s	Lo		consonant				U+1830: MONGOLIAN LETTER SA	с	✓	s ʃ
-ᠱ	S	ʃ		ʃ	š	x	Lo		consonant				U+1831: MONGOLIAN LETTER SHA	ш	✓	ʃ
-ᠲ	t	tʰ		tʰ	t	t	Lo		consonant				U+1832: MONGOLIAN LETTER TA	т	✓	t
-ᠼ	t	t͡sʰ		ʦʰ	c	c	Lo	l	consonant	for transliteration			U+183C: MONGOLIAN LETTER TSA	ц	✓	ʦ
-ᠤ	u	ʊ	ə ∅	ʊ	u	v	Lo		vowel				U+1824: MONGOLIAN LETTER U	у	✓	ʊ
-ᠦ	U	u	ə	u	ü	u	Lo		vowel				U+1826: MONGOLIAN LETTER UE	ү	✓	u
-ᠸ	v	w	v	w	w		Lo	l	consonant	for transliteration			U+1838: MONGOLIAN LETTER WA	в	✓	w v
-ᠬ	x	x		x	x	h	Lo		consonant				U+182C: MONGOLIAN LETTER QA	х	✓	x
-ᠶ	y	j		j	y	y	Lo		consonant				U+1836: MONGOLIAN LETTER YA	-й е* ё* ю* я*	✓	j
-ᠿ	z	ʐ		ʐ	ž		Lo	l	consonant	for transliteration			U+183F: MONGOLIAN LETTER ZRA	ж	✓	ʐ
-ᡁ	z	ʑ		ʑ	ẑ	Z	Lo	l	consonant	for transliteration			U+1841: MONGOLIAN LETTER ZHI	з	✓	ʈʂɻ
-																
-ᡛ				ń	ń		Lo	u					U+185B: MONGOLIAN LETTER TODO NIA		✓	
-																
-ᢅ		–					Mn​	i					U+1885: MONGOLIAN LETTER ALI GALI BALUDA			
-ᢆ		–					Mn​	i					U+1886: MONGOLIAN LETTER ALI GALI THREE BALUDA			
-ᢩ		–					Mn​	i					U+18A9: MONGOLIAN LETTER ALI GALI DAGALGA			
-																
-ʼ				ʼ	ʼ		Lm	?	apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE			
-§				§	§		Po	?	section sign				U+00A7: SECTION SIGN			
-†				†	†		Po	?	dagger				U+2020: DAGGER			
-‡				‡	‡		Po	?	double dagger				U+2021: DOUBLE DAGGER			
-′				′	′		Po	?	prime				U+2032: PRIME			
-″				″	″		Po	?	double prime				U+2033: DOUBLE PRIME			
-`																
-																
-latinPanel = 'ĉ d͡ʒ d͡z ə ɡ ḳ ɮ ŋ ń ∅ ɔ pʰ ʃ š tʰ t͡ʃʰ t͡sʰ ʊ ɣ ž ẑ'																
-																
-var cols = {																
-key: 1,																
-ipaLoc: 2,																
-ipaPlus: 3,																
-transLoc: 4,																
-transcription: 5,																
-kbd: 6,																
-class: 7,																
-status: 8,																
-typeLoc: 9,																
-statusLoc: 10,																
-																
-nnameLoc: 11,																
-nameLoc: 12,																
-ucsName: 13,																
-block: 15,																
-																
-othertranscriptions: [[5, 'Joint Sino-Mongolian'], [14, 'Cyrillic']]																
-}																
-																
-// Joint Sino-Mongolian																
-// https://zh.wikipedia.org/wiki/%E4%B8%AD%E8%92%99%E8%81%94%E5%90%88%E8%BD%AC%E5%86%99																
+var spreadsheet = `mong-mn	key	khalkha ipa	ipa+	ipa++	translit	Joint Sino-Mongolian	kbd	class	status	type	usage	native name	latin name	ucs name	cyrillic	block	chakhar ipa
+᠊	_				_	–		Po		baseline extender				U+180A: MONGOLIAN NIRUGU		✓	
+-	-				-	-				hyphen				U+2010: HYPHEN			
+‑	-				‑	‑		Pd	?	non-breaking hyphen				U+2011: NON-BREAKING HYPHEN			
+\u1806	-				–͇			Pd		hyphen				U+1806: MONGOLIAN TODO SOFT HYPHEN		✓	
+–	-				–	–		Pd	?	en dash				U+2013: EN DASH			
+—	-				—	—		Pd	?	em dash				U+2014: EM DASH			
+、	,				⹁	,		Po	?	ideographic comma				U+3001: IDEOGRAPHIC COMMA			
+᠂	,¶				,	,	,	Po		comma				U+1802: MONGOLIAN COMMA		✓	
+;	;				;	;		Po	?	semicolon				U+003B: SEMICOLON			
+᠄	:				:	:		Po		colon				U+1804: MONGOLIAN COLON		✓	
+！	!				!	!		Po		exclamation mark				U+FF01: FULLWIDTH EXCLAMATION MARK			
+⁉	!				⁉			Po	?	exclamation-question mark				U+2049: EXCLAMATION QUESTION MARK			
+？	?				?	?		Po		question mark				U+FF1F: FULLWIDTH QUESTION MARK			
+⁈	?				⁈			Po	?	question-exclamation mark				U+2048: QUESTION EXCLAMATION MARK			
+᠃	.				.	.	.	Po		full stop				U+1803: MONGOLIAN FULL STOP		✓	
+。	.				•	.		Po	?	ideographic full stop				U+3002: IDEOGRAPHIC FULL STOP			
+᠁	.				…	…	Q	Po		ellipsis				U+1801: MONGOLIAN ELLIPSIS		✓	
+（	(				(			Ps		fullwidth parenthesis				U+FF08: FULLWIDTH LEFT PARENTHESIS			
+）	)				)			Pe		fullwidth parenthesis				U+FF09: FULLWIDTH RIGHT PARENTHESIS			
+(	(				(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS			
+[	(				[	[		Ps	?	bracket				U+005B: LEFT SQUARE BRACKET			
+)	)				)	)		Pe		parenthesis				U+0029: RIGHT PARENTHESIS			
+]	)				]	]		Pe	?	bracket				U+005D: RIGHT SQUARE BRACKET			
+\u180E	[				ᵐᵛˢ	_		Cf		mvs				U+180E: MONGOLIAN VOWEL SEPARATOR		✓	
+\u202F	]				ⁿⁿᵇˢᵖ	-		Zs		nnbsp				U+202F NARROW NO-BREAK SPACE		✓	
+%	%				%			Po	?	percentage mark				U+0025: PERCENT SIGN			
+‰	%				‰			Po	?	per mille mark				U+2030: PER MILLE SIGN			
+\u180B	^				ᶠᵛˢ¹			Mn		fvs1				U+180B: MONGOLIAN FREE VARIATION SELECTOR ONE		✓	
+\u180C	^				ᶠᵛˢ²			Mn		fvs2				U+180C: MONGOLIAN FREE VARIATION SELECTOR TWO		✓	
+\u180D	^				ᶠᵛˢ³			Mn		fvs3				U+180D: MONGOLIAN FREE VARIATION SELECTOR THREE		✓	
+\u180F	^				ᶠᵛˢ4			Mn		fvs4				U+180F: MONGOLIAN FREE VARIATION SELECTOR FOUR			
+᠐	±				0̣	0	0	Nd		digit				U+1810: MONGOLIAN DIGIT ZERO		✓	
+᠑	±				1̣	1	1	Nd		digit				U+1811: MONGOLIAN DIGIT ONE		✓	
+᠒	±				2̣	2	2	Nd		digit				U+1812: MONGOLIAN DIGIT TWO		✓	
+᠓	±				3̣	3	3	Nd		digit				U+1813: MONGOLIAN DIGIT THREE		✓	
+᠔	±				4̣	4	4	Nd		digit				U+1814: MONGOLIAN DIGIT FOUR		✓	
+᠕	±				5̣	5	5	Nd		digit				U+1815: MONGOLIAN DIGIT FIVE		✓	
+᠖	±				6̣	6	6	Nd		digit				U+1816: MONGOLIAN DIGIT SIX		✓	
+᠗	±				7̣	7	7	Nd		digit				U+1817: MONGOLIAN DIGIT SEVEN		✓	
+᠘	±				8̣	8	8	Nd		digit				U+1818: MONGOLIAN DIGIT EIGHT		✓	
+᠙	±				9̣	9	9	Nd		digit				U+1819: MONGOLIAN DIGIT NINE		✓	
+《	<				{	“		Ps		quotation mark				U+300A: LEFT DOUBLE ANGLE BRACKET			
+«	<				«	“	:	Pi		quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK			
+〈	<				<	“		Ps		quotation mark				U+3008: LEFT ANGLE BRACKET			
+‹	<				‹			Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK			
+“	<				“	“		Pi	?	quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK			
+‘	<				\‘	\‘		Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK			
+〔	<				[			Ps		bracket				U+3014: LEFT TORTOISE SHELL BRACKET			
+\u200C	=				ᶻʷⁿʲ			Cf		zwnj				U+200C: ZERO WIDTH NON-JOINER			
+\u200D	=				ᶻʷʲ			Cf		zwj				U+200D: ZERO WIDTH JOINER			
+\u2067	=				ʳˡⁱ			Cf	u	rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE			
+\u2066	=				ˡʳⁱ			Cf	u	ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE			
+\u2068	=				ᶠˢⁱ			Cf	u	first-strong isolate				U+2068: FIRST STRONG ISOLATE			
+\u2069	=				ᵖᵈⁱ			Cf	u	pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE			
+\u202B	=				ʳˡᵉ			Cf	u	rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING			
+\u202A	=				ˡʳᵉ			Cf	u	ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING			
+\u202C	=				ᵖᵈᶠ			Cf	u	pop direction				U+202C: POP DIRECTIONAL FORMATTING			
+\u200F	=				ʳˡᵐ			Cf	u	rtl mark				U+200F: RIGHT-TO-LEFT MARK			
+\u200E	=				ˡʳᵐ			Cf	u	ltr mark				U+200E: LEFT-TO-RIGHT MARK			
+\u034F	=				ᶜᵍʲ			Mn	?	combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER			
+\u061C	=				ᵃˡᵐ			Cf	u	arabic letter mark				U+061C: ARABIC LETTER MARK			
+》	>				}	”		Pe		quotation mark				U+300B: RIGHT DOUBLE ANGLE BRACKET			
+»	>				»	”	;	Pf		quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK			
+〉	>				>	”		Pe		quotation mark				U+3009: RIGHT ANGLE BRACKET			
+›	>				›			Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK			
+”	>				”	”		Pf	?	quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK			
+’	>				\’	\’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK			
+〕	>				]			Pe		bracket				U+3015: RIGHT TORTOISE SHELL BRACKET			
+᠀	|				¶	»		Po	?	section mark				U+1800: MONGOLIAN BIRGA		✓	
+᠅	|				|	«		Po	?	section mark				U+1805: MONGOLIAN FOUR DOTS		✓	
+																	
+																	
+ᠪ	b	p w̜			p	b	b	Lo		consonant				U+182A: MONGOLIAN LETTER BA	б	✓	b
+ᠫ	p	pʰ			pʰ	p	p	Lo		consonant				U+182B: MONGOLIAN LETTER PA	п	✓	p
+ᠳ	d	t			t	d	d	Lo		consonant				U+1833: MONGOLIAN LETTER DA	д	✓	d
+ᠲ	t	tʰ			tʰ	t	t	Lo		consonant				U+1832: MONGOLIAN LETTER TA	т	✓	t
+ᠽ	d	t͡s			ʦ̣	z		Lo	l	consonant	Used for transliteration.			U+183D: MONGOLIAN LETTER ZA	з	✓	ʣ
+ᠼ	t	t͡sʰ			ʦʰ	c	c	Lo	l	consonant	Used for transliteration.			U+183C: MONGOLIAN LETTER TSA	ц	✓	ʦ
+ᠵ	D	t͡s t͡ʃ			ʦ	ǰ	j	Lo		consonant				U+1835: MONGOLIAN LETTER JA	ж з	✓	ʤ
+ᠴ	c	t͡sʰ t͡ʃʰ			ʦʰ	č	q	Lo		consonant				U+1834: MONGOLIAN LETTER CHA	ч ц	✓	ʧʰ
+ᠺ	k	k			k	k	k	Lo	l	consonant	Used for transliteration.			U+183A: MONGOLIAN LETTER KA	к	✓	k
+ᠻ	k	k			ḳ	ḳ	K	Lo	l	consonant	Used for transliteration.			U+183B: MONGOLIAN LETTER KHA		✓	
+ᠭ	g	ɢ ɡ k		∅ ɣ	ɢ	g	g	Lo		consonant				U+182D: MONGOLIAN LETTER GA	г	✓	ɣ ɡ
+ᠹ	f	f			f	f	f	Lo	l	consonant	Used for transliteration.			U+1839: MONGOLIAN LETTER FA	ф	✓	f
+ᠰ	s	s ʃ			s	s	s	Lo		consonant				U+1830: MONGOLIAN LETTER SA	с	✓	s ʃ
+ᠱ	S	ʃ			ʃ	š	x	Lo		consonant				U+1831: MONGOLIAN LETTER SHA	ш	✓	ʃ
+ᠿ	z	ʐ			ʐ	ž		Lo	l	consonant	Used for transliteration.			U+183F: MONGOLIAN LETTER ZRA	ж	✓	ʐ
+ᡂ	c	ɕ			ɕ	ĉ	C	Lo	l	consonant	Used for transliteration.			U+1842: MONGOLIAN LETTER CHI	ч	✓	ʈʂʰɻ
+ᡁ	z	ʑ			ʑ	ẑ	Z	Lo	l	consonant	Used for transliteration.			U+1841: MONGOLIAN LETTER ZHI	з	✓	ʈʂɻ
+ᠬ	x	x			x	x	h	Lo		consonant				U+182C: MONGOLIAN LETTER QA	х	✓	x
+ᠾ	h	h			h	h	H	Lo	l	consonant	Used for transliteration.			U+183E: MONGOLIAN LETTER HAA	х	✓	x
+ᠮ	m	m			m	m	m	Lo		consonant				U+182E: MONGOLIAN LETTER MA	м	✓	m
+ᠨ	n	n -ŋ			n	n	n	Lo		consonant				U+1828: MONGOLIAN LETTER NA	н	✓	n ŋ
+ᠩ	N	ŋ			ŋ	ng	N	Lo		consonant				U+1829: MONGOLIAN LETTER ANG	нг	✓	ŋ
+ᠸ	v	w		v	w	w		Lo	l	consonant	Used for transliteration.			U+1838: MONGOLIAN LETTER WA	в	✓	w v
+ᠷ	r	r			r	r	r	Lo		consonant				U+1837: MONGOLIAN LETTER RA	р	✓	r
+ᡀ	l	lʰ			lʰ	lh		Lo	l	consonant	Used for transliteration.			U+1840: MONGOLIAN LETTER LHA	лх	✓	lh
+ᠯ	l	ɮ			l	l	l	Lo		consonant				U+182F: MONGOLIAN LETTER LA	л	✓	l
+ᠶ	y	j			j	y	y	Lo		consonant				U+1836: MONGOLIAN LETTER YA	-й е* ё* ю* я*	✓	j
+																	
+ᡛ					ń	ń		Lo	u					U+185B: MONGOLIAN LETTER TODO NIA		✓	
+																	
+ᠢ	i	i		ə ∅	i	i	i	Lo		vowel				U+1822: MONGOLIAN LETTER I	и й ы ь	✓	i ɪ
+ᠧ	E	i e		ə	i	ë	e	Lo		vowel				U+1827: MONGOLIAN LETTER EE	э	✓	ə
+ᠤ	u	ʊ		ə ∅	ʊ	u	v	Lo		vowel				U+1824: MONGOLIAN LETTER U	у	✓	ʊ
+ᠦ	U	u		ə	u	ü	u	Lo		vowel				U+1826: MONGOLIAN LETTER UE	ү	✓	u
+ᠡ	e	e i		ə ∅	e	e	e	Lo		vowel				U+1821: MONGOLIAN LETTER E	є	✓	ə
+ᠥ	O	o		ə ∅	o	ö	o	Lo		vowel				U+1825: MONGOLIAN LETTER OE	ө	✓	o
+ᠣ	o	ɔ		ə ∅	ɔ	o	W	Lo		vowel				U+1823: MONGOLIAN LETTER O	о	✓	ɔ
+ᠠ	a	a		ə	a	a	a	Lo		vowel				U+1820: MONGOLIAN LETTER A	а	✓	ɑ
+																	
+																	
+ᢅ		–						Mn​	i					U+1885: MONGOLIAN LETTER ALI GALI BALUDA			
+ᢆ		–						Mn​	i					U+1886: MONGOLIAN LETTER ALI GALI THREE BALUDA			
+ᢩ		–						Mn​	i					U+18A9: MONGOLIAN LETTER ALI GALI DAGALGA			
+																	
+ʼ					ʼ	ʼ		Lm	?	apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE			
+§					§	§		Po	?	section sign				U+00A7: SECTION SIGN			
+†					†	†		Po	?	dagger				U+2020: DAGGER			
+‡					‡	‡		Po	?	double dagger				U+2021: DOUBLE DAGGER			
+′					′	′		Po	?	prime				U+2032: PRIME			
+″					″	″		Po	?	double prime				U+2033: DOUBLE PRIME			
+`																	
+																	
+latinPanel = 'ĉ d͡ʒ d͡z ə ɡ ḳ ɮ ŋ ń ∅ ɔ pʰ ʃ š tʰ t͡ʃʰ t͡sʰ ʊ ɣ ž ẑ'																	
+																	
+var cols = {																	
+key: 1,																	
+ipaLoc: 2,																	
+ipaPlus: 3,																	
+ipaOther: 4,																	
+transLoc: 5,																	
+transcription: 6,																	
+kbd: 7,																	
+class: 8,																	
+status: 9,																	
+typeLoc: 10,																	
+statusLoc: 11,																	
+																	
+nnameLoc: 12,																	
+nameLoc: 13,																	
+ucsName: 14,																	
+block: 16,																	
+																	
+othertranscriptions: [[6, 'Joint Sino-Mongolian'], [15, 'Cyrillic']]																	
+}																	
+																	
+// Joint Sino-Mongolian																	
+// https://zh.wikipedia.org/wiki/%E4%B8%AD%E8%92%99%E8%81%94%E5%90%88%E8%BD%AC%E5%86%99																	
