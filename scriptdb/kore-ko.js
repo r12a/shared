7c466b7964cda0e67eef9313f3b2eebb63c8fa97
r12a <ishida@w3.org>
2024-03-27 14:21:21 +0000

    scriptdb/kore-ko: Feb template update.

--- scriptdb/kore-ko.js
+++ scriptdb/kore-ko.js
@@ -1,86 +1,327 @@
-var spreadsheet = `
-ㄱ	k	k		g- -k	k		Lo		consonant		U+3131: HANGUL LETTER KIYEOK	
-ㄲ	k	k͈		kk- -k	k͈		Lo		consonant		U+3132: HANGUL LETTER SSANGKIYEOK	
-ㄴ	n	n		n	n		Lo		consonant		U+3134: HANGUL LETTER NIEUN	
-ㄷ	t	t		d- -t	t		Lo		consonant		U+3137: HANGUL LETTER TIKEUT	
-ㄸ	t	t͈		tt-	t͈		Lo		consonant		U+3138: HANGUL LETTER SSANGTIKEUT	
-ㄹ	l	l~ɾ		r- -l	l~ɾ		Lo		consonant		U+3139: HANGUL LETTER RIEUL	
-ㅁ	m	m		m	m		Lo		consonant		U+3141: HANGUL LETTER MIEUM	
-ㅂ	p	p		b- -p	p		Lo		consonant		U+3142: HANGUL LETTER PIEUP	
-ㅃ	p	p͈		pp-	p͈		Lo		consonant		U+3143: HANGUL LETTER SSANGPIEUP	
-ㅅ	s	s		s- -t	s		Lo		consonant		U+3145: HANGUL LETTER SIOS	
-ㅆ	s	s͈		ss- -t	s͈		Lo		consonant		U+3146: HANGUL LETTER SSANGSIOS	
-ㅇ	n	ŋ		-ng	ŋ		Lo		consonant		U+3147: HANGUL LETTER IEUNG	
-ㅈ	c	t͡ɕ		j- -t	t͡ɕ		Lo		consonant		U+3148: HANGUL LETTER CIEUC	
-ㅉ	c	t͈͡ɕ		jj-	t͈͡ɕ		Lo		consonant		U+3149: HANGUL LETTER SSANGCIEUC	
-ㅊ	c	tɕʰ		ch- -t	tɕʰ		Lo		consonant		U+314A: HANGUL LETTER CHIEUCH	
-ㅋ	k	kʰ		k	kʰ		Lo		consonant		U+314B: HANGUL LETTER KHIEUKH	
-ㅌ	t	tʰ		t	tʰ		Lo		consonant		U+314C: HANGUL LETTER THIEUTH	
-ㅍ	p	pʰ		p	pʰ		Lo		consonant		U+314D: HANGUL LETTER PHIEUPH	
-ㅎ	h	h		h	h		Lo		consonant		U+314E: HANGUL LETTER HIEUH	
-												
-												
-ㅏ	a	ɐ		a	ɐ		Lo		vowel		U+314F: HANGUL LETTER A	
-ㅐ	a	ɛ		ae	ɛ		Lo		vowel		U+3150: HANGUL LETTER AE	
-ㅑ	a	jɐ		ya	jᵄ		Lo		vowel		U+3151: HANGUL LETTER YA	
-ㅒ	a	jɛ		yae	jᵋ		Lo		vowel		U+3152: HANGUL LETTER YAE	
-ㅓ	e	ʌ̹		eo	ʌ		Lo		vowel		U+3153: HANGUL LETTER EO	
-ㅔ	e	e̞		eo	e		Lo		vowel		U+3154: HANGUL LETTER E	
-ㅕ	e	jʌ		yeo	jᶺ		Lo		vowel		U+3155: HANGUL LETTER YEO	
-ㅖ	e	je		yeo	jᵉ		Lo		vowel		U+3156: HANGUL LETTER YE	
-ㅗ	o	o		o	o		Lo		vowel		U+3157: HANGUL LETTER O	
-ㅘ	w	wɐ		wa	wᵄ		Lo		vowel		U+3158: HANGUL LETTER WA	
-ㅙ	w	wɛ		wae	wᵋ		Lo		vowel		U+3159: HANGUL LETTER WAE	
-ㅚ	o			oe	–		Lo		vowel		U+315A: HANGUL LETTER OE	
-ㅛ	o	jo		yo	jᵒ		Lo		vowel		U+315B: HANGUL LETTER YO	
-ㅜ	u	u		u	u		Lo		vowel		U+315C: HANGUL LETTER U	
-ㅝ	o	wʌ		wo	wᶺ		Lo		vowel		U+315D: HANGUL LETTER WEO	
-ㅞ	e	we		we	wᵉ		Lo		vowel		U+315E: HANGUL LETTER WE	
-ㅟ	i	ɥi		wi	ɥⁱ		Lo		vowel		U+315F: HANGUL LETTER WI	
-ㅠ	u	ju		yu	jᵘ		Lo		vowel		U+3160: HANGUL LETTER YU	
-ㅡ	e	ɯ		eu	ɯ		Lo		vowel		U+3161: HANGUL LETTER EU	
-ㅢ	u	ɰi		ui	ɰⁱ		Lo		vowel		U+3162: HANGUL LETTER YI	
-ㅣ	i	i		i	i		Lo		vowel		U+3163: HANGUL LETTER I	
-
-
-
-`
-
-
-
-latinPanel = 'e̞ kʰ k͈ pʰ p͈ tɕʰ tʰ t͈ t͈͡ɕ t͡ɕ ŋ ɐ ɛ ɥi ɯ ɰi ɾ ʌ ʌ̹'
-
-
-
-var cols = {
-"key": 1,
-"ipaLoc": 2,
-"ipaPlus": 3,
-"transcription": 4,
-"transLoc": 5,
-"kbd": 6,
-"class": 7,
-"status": 8,
-"typeLoc": 9,
-"statusLoc": 10,
-
-"equiv": 0,
-"dvowel": 0,
-"ivowel": 0,
-"subj":0,
-"ltone":0,
-"htone":0,
-"lc":0,
-"uc":0,
-"meaning":0,
-"shape": 0,
-"transckey": 0,
-"numLoc": 0,
-
-"nnameLoc": 0,
-"nameLoc": 0,
-"ucsName": 11,
-"block": 12,
-
-"othertranscriptions": [[4, 'Latin']]
-}
+var spreadsheet = `kore-ko	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	name	ucs name				
+\u0020	 	 		 	 		Zs		space				U+0020: SPACE				
+\u200B	=			ᶻʷˢᵖ			Cf		zero-width space				U+200B: ZERO WIDTH SPACE				
+// onsets																	
+ᄇ	p	p-		p	b		Lo		jamo onset consonant		비읍	bi-eup	U+1107: HANGUL CHOSEONG PIEUP				
+ᄈ	p	p͈-		p͈	pp		Lo		jamo onset consonant		쌍비읍	ssang-bi-eup	U+1108: HANGUL CHOSEONG SSANGPIEUP				
+ᄑ	p	pʰ-		pʰ	p		Lo		jamo onset consonant		피읖 	p̣i-eup 	U+1111: HANGUL CHOSEONG PHIEUPH				
+ᄃ	t	t-		t	d		Lo		jamo onset consonant		디귿	digeut	U+1103: HANGUL CHOSEONG TIKEUT				
+ᄄ	t	t͈-		t͈	tt		Lo		jamo onset consonant		쌍디귿	ssang-digeut	U+1104: HANGUL CHOSEONG SSANGTIKEUT				
+ᄐ	t	tʰ-		tʰ	t		Lo		jamo onset consonant		티읕 	ṭi-eut 	U+1110: HANGUL CHOSEONG THIEUTH				
+ᄌ	c	t͡ɕ-		c	j		Lo		jamo onset consonant		지읒 	ji-eut 	U+110C: HANGUL CHOSEONG CIEUC				
+ᄍ	c	t͈͡ɕ-		c	jj		Lo		jamo onset consonant		쌍지읒	ssang-ji-eut 	U+110D: HANGUL CHOSEONG SSANGCIEUC				
+ᄎ	c	tɕʰ-		cʰ	ch		Lo		jamo onset consonant		치읓 	chi-eut 	U+110E: HANGUL CHOSEONG CHIEUCH				
+ᄀ	k	k-		k	g		Lo		jamo onset consonant		기역	gi-yeok	U+1100: HANGUL CHOSEONG KIYEOK				
+ᄁ	k	k͈-		k͈	kk		Lo		jamo onset consonant		쌍기역	ssang-giyeok	U+1101: HANGUL CHOSEONG SSANGKIYEOK				
+ᄏ	k	kʰ-		kʰ	k		Lo		jamo onset consonant		키읔 	ḳi-euk 	U+110F: HANGUL CHOSEONG KHIEUKH				
+ᄉ	s	s-		s	s		Lo		jamo onset consonant		시옷	si-ot (shi-ot)	U+1109: HANGUL CHOSEONG SIOS				
+ᄊ	s	s͈-		s͈	ss		Lo		jamo onset consonant		쌍시옷	ssang-si-ot (ssang-shi-ot)	U+110A: HANGUL CHOSEONG SSANGSIOS				
+ᄒ	h	h-		h	h		Lo		jamo onset consonant		히읗 	hi-eut 	U+1112: HANGUL CHOSEONG HIEUH				
+ᄆ	m	m-		m	m		Lo		jamo onset consonant		미음	mi-eum	U+1106: HANGUL CHOSEONG MIEUM				
+ᄂ	n	n-		n	n		Lo		jamo onset consonant		니은	ni-eun	U+1102: HANGUL CHOSEONG NIEUN				
+ᄋ	n	∅-		ŋ			Lo		jamo onset consonant		이응	'i-eung 	U+110B: HANGUL CHOSEONG IEUNG				
+ᄅ	l	ɾ-		l	r		Lo		jamo onset consonant		리을	ri-eul	U+1105: HANGUL CHOSEONG RIEUL				
+// vowels																	
+ᅵ	i	i		i	i		Lo		jamo plain vowel				U+1175: HANGUL JUNGSEONG I	✓			
+ᅳ	w	ɯ		ɯ	eu		Lo		jamo plain vowel				U+1173: HANGUL JUNGSEONG EU	✓			
+ᅮ	u	u		u	u		Lo		jamo plain vowel				U+116E: HANGUL JUNGSEONG U	✓			
+ᅦ	e	e		e	eo		Lo		jamo plain vowel				U+1166: HANGUL JUNGSEONG E	✓			
+ᅩ	o	o		o	o		Lo		jamo plain vowel				U+1169: HANGUL JUNGSEONG O	✓			
+ᅢ	a	ɛ		ɛ	ae		Lo		jamo plain vowel				U+1162: HANGUL JUNGSEONG AE	✓			
+ᅥ	e	ʌ		ʌ	eo		Lo		jamo plain vowel				U+1165: HANGUL JUNGSEONG EO	✓			
+ᅡ	a	a		ɐ	a		Lo		jamo plain vowel				U+1161: HANGUL JUNGSEONG A	✓			
+// diphthongs																	
+ᅲ	Y	ju		jᵘ	yu		Lo		jamo diphthong				U+1172: HANGUL JUNGSEONG YU	✓			
+ᅨ	Y	je		jᵉ	yeo		Lo		jamo diphthong				U+1168: HANGUL JUNGSEONG YE	✓			
+ᅭ	Y	jo		jᵒ	yo		Lo		jamo diphthong				U+116D: HANGUL JUNGSEONG YO	✓			
+ᅤ	Y	jɛ		jᵋ	yae		Lo		jamo diphthong				U+1164: HANGUL JUNGSEONG YAE	✓			
+ᅧ	Y	jʌ		jᶺ	yeo		Lo		jamo diphthong				U+1167: HANGUL JUNGSEONG YEO	✓			
+ᅣ	Y	ja		jᵄ	ya		Lo		jamo diphthong				U+1163: HANGUL JUNGSEONG YA	✓			
+ᅴ	W	ɰi		ɰⁱ	ui		Lo		jamo diphthong				U+1174: HANGUL JUNGSEONG YI	✓			
+ᅱ	W	ɥi y		ɥⁱ	wi		Lo		jamo diphthong				U+1171: HANGUL JUNGSEONG WI	✓			
+ᅰ	W	we		wᵉ	we		Lo		jamo diphthong				U+1170: HANGUL JUNGSEONG WE	✓			
+ᅬ	o	we ø		–	oe		Lo		jamo diphthong				U+116C: HANGUL JUNGSEONG OE	✓			
+ᅫ	W	wɛ		wᵋ	wae		Lo		jamo diphthong				U+116B: HANGUL JUNGSEONG WAE	✓			
+ᅯ	W	wʌ		wᶺ	wo		Lo		jamo diphthong				U+116F: HANGUL JUNGSEONG WEO	✓			
+ᅪ	W	wa		wᵄ	wa		Lo		jamo diphthong				U+116A: HANGUL JUNGSEONG WA	✓			
+// finals																	
+ᆸ	p	-p̚		p	p		Lo		jamo final consonant		비읍	bi-eup	U+11B8: HANGUL JONGSEONG PIEUP	✓			
+ᆹ		-p -ps			bs		Lo		jamo final consonant				U+11B9: HANGUL JONGSEONG PIEUP-SIOS	✓			
+ᇁ	p	-p̚		pʰ	p		Lo		jamo final consonant		피읖 	p̣i-eup 	U+11C1: HANGUL JONGSEONG PHIEUPH	✓			
+ᆮ	t	-t		t	t		Lo		jamo final consonant		디귿	digeut	U+11AE: HANGUL JONGSEONG TIKEUT	✓			
+ᇀ	t	-t		tʰ	t		Lo		jamo final consonant		티읕 	ṭi-eut 	U+11C0: HANGUL JONGSEONG THIEUTH	✓			
+ᆽ	c	-t		c	t		Lo		jamo final consonant		지읒 	ji-eut 	U+11BD: HANGUL JONGSEONG CIEUC	✓			
+ᆾ	c	-t		cʰ	t		Lo		jamo final consonant		치읓 	chi-eut 	U+11BE: HANGUL JONGSEONG CHIEUCH	✓			
+ᆨ	k	-k̚		k	k		Lo		jamo final consonant		기역	gi-yeok	U+11A8: HANGUL JONGSEONG KIYEOK	✓			
+ᆩ	k	-k̚		k͈	k		Lo		jamo final consonant		쌍기역	ssang-giyeok	U+11A9: HANGUL JONGSEONG SSANGKIYEOK	✓			
+ᆪ	k	-k -ks			ks		Lo		jamo complex final consonant				U+11AA: HANGUL JONGSEONG KIYEOK-SIOS	✓			
+ᆿ	k	-k̚		kʰ	k		Lo		jamo final consonant		키읔 	ḳi-euk 	U+11BF: HANGUL JONGSEONG KHIEUKH	✓			
+ᆺ	s	-t		s	t		Lo		jamo final consonant		시옷	si-ot (shi-ot)	U+11BA: HANGUL JONGSEONG SIOS	✓			
+ᆻ	s	-t		s͈	t		Lo		jamo final consonant		쌍시옷	ssang-si-ot (ssang-shi-ot)	U+11BB: HANGUL JONGSEONG SSANGSIOS	✓			
+ᇂ	h	-t		h	h		Lo		jamo final consonant		히읗 	hi-eut 	U+11C2: HANGUL JONGSEONG HIEUH	✓			
+ᆷ	m	-m		m	m		Lo		jamo final consonant		미음	mi-eum	U+11B7: HANGUL JONGSEONG MIEUM	✓			
+ᆫ	n	-n		n	n		Lo		jamo final consonant		니은	ni-eun	U+11AB: HANGUL JONGSEONG NIEUN	✓			
+ᆬ	n	-n -nt͡ɕ			nj		Lo		jamo complex final consonant				U+11AC: HANGUL JONGSEONG NIEUN-CIEUC	✓			
+ᆭ		-n -nh			nh		Lo		jamo complex final consonant				U+11AD: HANGUL JONGSEONG NIEUN-HIEUH	✓			
+ᆼ	n	-ŋ		ŋ	ng		Lo		jamo final consonant		이응	'i-eung 	U+11BC: HANGUL JONGSEONG IEUNG	✓			
+ᆯ	l	-ɭ		l	l		Lo		jamo final consonant		리을	ri-eul	U+11AF: HANGUL JONGSEONG RIEUL	✓			
+ᆰ		-k -l -lk			lg		Lo		jamo complex final consonant				U+11B0: HANGUL JONGSEONG RIEUL-KIYEOK	✓			
+ᆱ		-m -lm			lm		Lo		jamo complex final consonant				U+11B1: HANGUL JONGSEONG RIEUL-MIEUM	✓			
+ᆲ		-l -p -lp			lb		Lo		jamo complex final consonant				U+11B2: HANGUL JONGSEONG RIEUL-PIEUP	✓			
+ᆳ		-l -ls			ls		Lo		jamo complex final consonant				U+11B3: HANGUL JONGSEONG RIEUL-SIOS	✓			
+ᆴ		-l -lt			lt		Lo		jamo complex final consonant				U+11B4: HANGUL JONGSEONG RIEUL-THIEUTH	✓			
+ᆵ		-p -lp			lp		Lo		jamo complex final consonant				U+11B5: HANGUL JONGSEONG RIEUL-PHIEUPH	✓			
+ᆶ		-l -lh			lh		Lo		jamo complex final consonant				U+11B6: HANGUL JONGSEONG RIEUL-HIEUH	✓			
+																	
+																	
+																	
+// compat							Lo										
+ㅂ		p- -p̚		p	b- -p		Lo	x	jamo compatibility consonant		비읍	bi-eup	U+3142: HANGUL LETTER PIEUP	✓			
+ㅃ		p͈-		p͈	pp-		Lo	x	jamo compatibility consonant		쌍비읍	ssang-bi-eup	U+3143: HANGUL LETTER SSANGPIEUP				
+ㅍ		pʰ- -p̚		pʰ	p		Lo	x	jamo compatibility consonant		피읖 	p̣i-eup 	U+314D: HANGUL LETTER PHIEUPH				
+ㄷ		t		t	d- -t		Lo	x	jamo compatibility consonant		디귿	digeut	U+3137: HANGUL LETTER TIKEUT				
+ㄸ		t͈-		t͈	tt-		Lo	x	jamo compatibility consonant		쌍디귿	ssang-digeut	U+3138: HANGUL LETTER SSANGTIKEUT				
+ㅌ		tʰ- -t		tʰ	t		Lo	x	jamo compatibility consonant		티읕 	ṭi-eut 	U+314C: HANGUL LETTER THIEUTH				
+ㅈ		t͡ɕ- -t		c	j- -t		Lo	x	jamo compatibility consonant		지읒 	ji-eut 	U+3148: HANGUL LETTER CIEUC				
+ㅉ		t͈͡ɕ-		c	jj-		Lo	x	jamo compatibility consonant		쌍지읒	ssang-ji-eut 	U+3149: HANGUL LETTER SSANGCIEUC	✓			
+ㅊ		tɕʰ- -t		cʰ	ch- -t		Lo	x	jamo compatibility consonant		치읓 	chi-eut 	U+314A: HANGUL LETTER CHIEUCH				
+ㄱ		k- -k̚		k	g- -k		Lo	x	jamo compatibility consonant		기역	gi-yeok	U+3131: HANGUL LETTER KIYEOK				
+ㄲ		k͈- -k̚		k͈	kk- -k		Lo	x	jamo compatibility consonant		쌍기역	ssang-giyeok	U+3132: HANGUL LETTER SSANGKIYEOK				
+ㅋ		kʰ- -k̚		kʰ	k		Lo	x	jamo compatibility consonant		키읔 	ḳi-euk 	U+314B: HANGUL LETTER KHIEUKH				
+ㅅ		s- -t		s	s- -t		Lo	x	jamo compatibility consonant		시옷	si-ot (shi-ot)	U+3145: HANGUL LETTER SIOS	✓			
+ㅆ		s͈- -t		s͈	ss- -t		Lo	x	jamo compatibility consonant		쌍시옷	ssang-si-ot (ssang-shi-ot)	U+3146: HANGUL LETTER SSANGSIOS				
+ㅎ		h- -t		h	h		Lo	x	jamo compatibility consonant		히읗 	hi-eut 	U+314E: HANGUL LETTER HIEUH	✓			
+ㅁ		m		m	m		Lo	x	jamo compatibility consonant		미음	mi-eum	U+3141: HANGUL LETTER MIEUM				
+ㄴ		n		n	n		Lo	x	jamo compatibility consonant		니은	ni-eun	U+3134: HANGUL LETTER NIEUN				
+ㅇ		∅- -ŋ		ŋ	-ng		Lo	x	jamo compatibility consonant		이응	'i-eung 	U+3147: HANGUL LETTER IEUNG				
+ㄹ		ɾ- -ɭ		l	r- -l		Lo	x	jamo compatibility consonant		리을	ri-eul	U+3139: HANGUL LETTER RIEUL				
+																	
+ㅣ		i		i	i		Lo	x	jamo compatibility vowel				U+3163: HANGUL LETTER I	✓			
+ㅡ		ɯ		ɯ	eu		Lo	x	jamo compatibility vowel				U+3161: HANGUL LETTER EU	✓			
+ㅜ		u		u	u		Lo	x	jamo compatibility vowel				U+315C: HANGUL LETTER U				
+ㅔ		e		e	eo		Lo	x	jamo compatibility vowel				U+3154: HANGUL LETTER E	✓			
+ㅗ		o		o	o		Lo	x	jamo compatibility vowel				U+3157: HANGUL LETTER O	✓			
+ㅐ		ɛ		ɛ	ae		Lo	x	jamo compatibility vowel				U+3150: HANGUL LETTER AE	✓			
+ㅓ		ʌ		ʌ	eo		Lo	x	jamo compatibility vowel				U+3153: HANGUL LETTER EO				
+ㅏ		a		ɐ	a		Lo	x	jamo compatibility vowel				U+314F: HANGUL LETTER A	✓			
+																	
+ㅠ		ju		jᵘ	yu		Lo	x	jamo compatibility vowel				U+3160: HANGUL LETTER YU	✓			
+ㅖ		je		jᵉ	yeo		Lo	x	jamo compatibility vowel				U+3156: HANGUL LETTER YE	✓			
+ㅛ		jo		jᵒ	yo		Lo	x	jamo compatibility vowel				U+315B: HANGUL LETTER YO				
+ㅒ		jɛ		jᵋ	yae		Lo	x	jamo compatibility vowel				U+3152: HANGUL LETTER YAE	✓			
+ㅕ		jʌ		jᶺ	yeo		Lo	x	jamo compatibility vowel				U+3155: HANGUL LETTER YEO	✓			
+ㅑ		ja		jᵄ	ya		Lo	x	jamo compatibility vowel				U+3151: HANGUL LETTER YA	✓			
+ㅟ		ɥi y		ɥⁱ	wi		Lo	x	jamo compatibility vowel				U+315F: HANGUL LETTER WI	✓			
+ㅢ		ɰi		ɰⁱ	ui		Lo	x	jamo compatibility vowel				U+3162: HANGUL LETTER YI	✓			
+ㅞ		we		wᵉ	we		Lo	x	jamo compatibility vowel				U+315E: HANGUL LETTER WE	✓			
+ㅚ		we ø		–	oe		Lo	x	jamo compatibility vowel				U+315A: HANGUL LETTER OE	✓			
+ㅙ		wɛ		wᵋ	wae		Lo	x	jamo compatibility vowel				U+3159: HANGUL LETTER WAE	✓			
+ㅝ		wʌ		wᶺ	wo		Lo	x	jamo compatibility vowel				U+315D: HANGUL LETTER WEO	✓			
+ㅘ		wa		wᵄ	wa		Lo	x	jamo compatibility vowel				U+3158: HANGUL LETTER WA	✓			
+																	
+\u115F				x			Lo		filler character				U+115F: HANGUL CHOSEONG FILLER				
+\u1160				y			Lo		filler character				U+1160: HANGUL JUNGSEONG FILLER				
+○				–			So		replacement character				U+25CB: WHITE CIRCLE				
+×				–			Sm		replacement character				U+00D7: MULTIPLICATION SIGN				
+□				–			So		replacement character				U+25A1: WHITE SQUARE				
+// punctuation																	
+・				–			Po		counterpoint marker				U+30FB: KATAKANA MIDDLE DOT				
+%				%			Po		percent sign				U+0025: PERCENT SIGN				
+℃				℃			So		degree celsius sign				U+2103: DEGREE CELSIUS				
+~				~			Sm		range indicator				U+007E: TILDE				
+																	
+,	,			,	,		Po		comma				U+002C: COMMA		⹁	،	
+、							Po			Used in vertical text.			U+3001: IDEOGRAPHIC COMMA				
+;	;			;	;		Po		semicolon				U+003B: SEMICOLON		⁏ ;	؛	
+；							Po			Used in vertical text.			U+FF1B: FULLWIDTH SEMICOLON				
+!	!			!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK		𞥞 !		
+！							Po			Used in vertical text.			U+FF01: FULLWIDTH EXCLAMATION MARK				
+?	?			?	?		Po		question mark				U+003F: QUESTION MARK		𞥟 ؟	؟	
+？							Po			Used in vertical text.			U+FF1F: FULLWIDTH QUESTION MARK				
+:	:			:	:		Po		colon				U+003A: COLON		:	:	
+：							Po			Used in vertical text.			U+FF1A: FULLWIDTH COLON				
+.	.			.	.		Po		full stop				U+002E: FULL STOP			✓	
+．							Po						U+FF0E: FULLWIDTH FULL STOP				
+。							Po						U+3002: IDEOGRAPHIC FULL STOP				
+…	.			…	…		Po		ellipsis				U+2026: HORIZONTAL ELLIPSIS			✓	
+‥				‥			Po		ellipsis marker				U+2025: TWO DOT LEADER				
+“	<			“	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK			✓	
+”	>			”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK			✓	
+‘	<			‘	‘		Pi		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK			✓	
+’	>			’	’		Pf		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK			✓	
+「							Ps			Used in vertical text.			U+300C: LEFT CORNER BRACKET				
+」							Pe			Used in vertical text.			U+300D: RIGHT CORNER BRACKET				
+『							Ps			Used in vertical text.			U+300E: LEFT WHITE CORNER BRACKET				
+』							Pe			Used in vertical text.			U+300F: RIGHT WHITE CORNER BRACKET				
+《							Ps						U+300A: LEFT DOUBLE ANGLE BRACKET				
+》							Pe						U+300B: RIGHT DOUBLE ANGLE BRACKET				
+〈							Ps						U+3008: LEFT ANGLE BRACKET				
+〉							Pe						U+3009: RIGHT ANGLE BRACKET				
+(	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS			(	
+)	)			)	)		Pe		parenthesis				U+0029: RIGHT PARENTHESIS			)	
+[	(			[	[		Ps		bracket				U+005B: LEFT SQUARE BRACKET			[	
+]	)			] 	]		Pe		bracket				U+005D: RIGHT SQUARE BRACKET			]	
+{				{			Ps		bracket				U+007B: LEFT CURLY BRACKET				
+}				}			Pe		bracket				U+007D: RIGHT CURLY BRACKET				
+〔				〔			Ps		bracket				U+3014: LEFT TORTOISE SHELL BRACKET				
+〕				〕			Pe		bracket				U+3015: RIGHT TORTOISE SHELL BRACKET				
+-	-			-	-				hyphen				: 		-	-	
+‑	-			‑	‑		Pd		non-breaking hyphen				U+2011: NON-BREAKING HYPHEN		‑		
+–	-			–	–		Pd		en dash				U+2013: EN DASH		–	–	
+—	-			—	—		Pd		em dash				U+2014: EM DASH		—	—	
+―				―			Pd		dash				U+2015: HORIZONTAL BAR				
+〜				〜			Pd		dash				U+301C: WAVE DASH				
+〃	&			&			Po		repetition				U+3003: DITTO MARK				
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+0	0			0	0		Nd		digit				U+0030: DIGIT ZERO				
+1	1			1	1		Nd		digit				U+0031: DIGIT ONE				
+2	2			2	2		Nd		digit				U+0032: DIGIT TWO				
+3	3			3	3		Nd		digit				U+0033: DIGIT THREE				
+4	4			4	4		Nd		digit				U+0034: DIGIT FOUR				
+5	5			5	5		Nd		digit				U+0035: DIGIT FIVE				
+6	6			6	6		Nd		digit				U+0036: DIGIT SIX				
+7	7			7	7		Nd		digit				U+0037: DIGIT SEVEN				
+8	8			8	8		Nd		digit				U+0038: DIGIT EIGHT				
+9	9			9	9		Nd		digit				U+0039: DIGIT NINE				
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+																	
+\u2003		  			  				em space				U+2003 EM SPACE				
+`																	
+																	
+latinPanel = 'e̞ kʰ k͈ pʰ p͈ tɕʰ tʰ t͈ t͈͡ɕ t͡ɕ ŋ ɐ ɛ ɥi ɯ ɰi ɾ ʌ ʌ̹'																	
+																	
+var cols = {																	
+key: 1,																	
+ipaLoc: 2,																	
+ipaPlus: 3,																	
+transLoc: 4,																	
+transcription: 5,																	
+kbd: 6,																	
+class: 7,																	
+status: 8,																	
+typeLoc: 9,																	
+statusLoc: 10,																	
+																	
+nnameLoc: 11,																	
+nameLoc: 12,																	
+ucsName: 13,																	
+block: 14,																	
+																	
+//othertranscriptions: [[5, 'UNGEN']]																	
+}																	
