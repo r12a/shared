0d93db7921260e99f09215a2ed0f35dec93804a7
r12a <ishida@w3.org>
2024-06-21 07:29:28 +0100

    scriptdb/arab-sd

--- scriptdb/arab-sd.js
+++ scriptdb/arab-sd.js
@@ -1,87 +1,205 @@
-var spreadsheet = `arab-sd	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	ucs name	block
-جهہ		ɟʱ											U+062C U+0647 U+06C1: ARABIC LETTER JEEM, LETTER HEH, LETTER HEH GOAL	
-ڄ		ʄ					Lo						U+0684: ARABIC LETTER DYEH	
-ج		ɟ					Lo						U+062C: ARABIC LETTER JEEM	
-پ		p					Lo						U+067E: ARABIC LETTER PEH	
-ث		s					Lo						U+062B: ARABIC LETTER THEH	
-ٺ		ʈʰ					Lo						U+067A: ARABIC LETTER TTEHEH	
-ٽ		ʈ					Lo						U+067D: ARABIC LETTER TEH WITH THREE DOTS ABOVE DOWNWARDS	
-ٿ		tʰ					Lo						U+067F: ARABIC LETTER TEHEH	
-ت		t					Lo						U+062A: ARABIC LETTER TEH	
-ڀ		bʱ					Lo						U+0680: ARABIC LETTER BEHEH	
-ٻ		ɓ					Lo						U+067B: ARABIC LETTER BEEH	
-ب		b					Lo						U+0628: ARABIC LETTER BEH	
-ا		ɑː ʔ ∅					Lo						U+0627: ARABIC LETTER ALEF	
-ڙ		ɽ					Lo						U+0699: ARABIC LETTER REH WITH FOUR DOTS ABOVE	
-ر		r					Lo						U+0631: ARABIC LETTER REH	
-ذ		z					Lo						U+0630: ARABIC LETTER THAL	
-ڍ		ɖʱ					Lo						U+068D: ARABIC LETTER DDAHAL	
-ڊ		ɖ					Lo						U+068A: ARABIC LETTER DAL WITH DOT BELOW	
-ڏ		ɗ					Lo						U+068F: ARABIC LETTER DAL WITH THREE DOTS ABOVE DOWNWARDS	
-ڌ		dʱ					Lo						U+068C: ARABIC LETTER DAHAL	
-د		d					Lo						U+062F: ARABIC LETTER DAL	
-خ		x					Lo						U+062E: ARABIC LETTER KHAH	
-ح		h					Lo						U+062D: ARABIC LETTER HAH	
-ڇ		cʰ					Lo						U+0687: ARABIC LETTER TCHEHEH	
-چ		c					Lo						U+0686: ARABIC LETTER TCHEH	
-ڃ		ɲ					Lo						U+0683: ARABIC LETTER NYEH	
-ڪ		k					Lo						U+06AA: ARABIC LETTER SWASH KAF	
-ق		q					Lo						U+0642: ARABIC LETTER QAF	
-ڦ		pʰ					Lo						U+06A6: ARABIC LETTER PEHEH	
-ف		f					Lo						U+0641: ARABIC LETTER FEH	
-غ		ɣ					Lo						U+063A: ARABIC LETTER GHAIN	
-ع		ɑːoːeːʔ∅					Lo						U+0639: ARABIC LETTER AIN	
-ظ		z					Lo						U+0638: ARABIC LETTER ZAH	
-ط		t					Lo						U+0637: ARABIC LETTER TAH	
-ض		z					Lo						U+0636: ARABIC LETTER DAD	
-ص		s					Lo						U+0635: ARABIC LETTER SAD	
-ش		ʂ					Lo						U+0634: ARABIC LETTER SHEEN	
-س		s					Lo						U+0633: ARABIC LETTER SEEN	
-ز		z					Lo						U+0632: ARABIC LETTER ZAIN	
-ي		j iː					Lo						U+064A: ARABIC LETTER YEH	
-ء		ʔ ∅					Lo						U+0621: ARABIC LETTER HAMZA	
-ھ		h					Lo						U+06BE: ARABIC LETTER HEH DOACHASHMEE	
-و		ʋ ʊ oː ɔː uː					Lo						U+0648: ARABIC LETTER WAW	
-ڻ		ɳ					Lo						U+06BB: ARABIC LETTER RNOON	
-ن		n					Lo						U+0646: ARABIC LETTER NOON	
-م		m					Lo						U+0645: ARABIC LETTER MEEM	
-ل		l					Lo						U+0644: ARABIC LETTER LAM	
-ڱ		ŋ					Lo						U+06B1: ARABIC LETTER NGOEH	
-گهہ		ɡʱ											U+06AF U+0647 U+06C1: ARABIC LETTER GAF, LETTER HEH, LETTER HEH GOAL	
-ڳ		ɠ					Lo						U+06B3: ARABIC LETTER GUEH	
-گ		ɡ					Lo						U+06AF: ARABIC LETTER GAF	
-ک		kʰ					Lo						U+06A9: ARABIC LETTER KEHEH	
-`														
-														
-latinPanel = 'bʱ cʰ dʱ eː iː kʰ oː pʰ tʰ uː ŋ ɑː ɓ ɔː ɖ ɖʱ ɗ ɟ ɟʱ ɠ ɡ ɡʱ ɣ ɲ ɳ ɽ ʂ ʄ ʈ ʈʰ ʊ ʋ ʔ ∅'														
-														
-var cols = {														
-key: 1,														
-ipaLoc: 2,														
-ipaPlus: 3,														
-transLoc: 4,														
-transcription: 5,														
-kbd: 6,														
-class: 7,														
-status: 8,														
-typeLoc: 9,														
-statusLoc: 10,														
-														
-dvowel: 0,														
-ivowel: 0,														
-subj:0,														
-ltone:0,														
-htone:0,														
-lc:0,														
-uc:0,														
-meaning:0,														
-shape: 0,														
-numLoc: 0,														
-														
-nnameLoc: 11,														
-nameLoc: 12,														
-ucsName: 13,														
-block: 14,														
-														
-//othertranscriptions: [[5, 'Latin']]														
-}														
+var spreadsheet = `arab-sd	key	ipa	ipa+	ipa++	translit	transc	kbd	class	status	type	usage	shape	native name	latin name	ucs name	block
+																
+پ	p	p			p			Lo		consonant		4		pe	U+067E: ARABIC LETTER PEH	
+ڦ	P	pʰ			pʰ			Lo		consonant		4		pʱe	U+06A6: ARABIC LETTER PEHEH	
+ب	b	b			b			Lo		consonant		4		be	U+0628: ARABIC LETTER BEH	
+ڀ	B	bʰ			bʰ			Lo		consonant		4		bʱe	U+0680: ARABIC LETTER BEHEH	
+ٻ	b	ɓ			ɓ			Lo		consonant		4		ɓe	U+067B: ARABIC LETTER BEEH	
+ت	t	t			t			Lo		consonant		4		t̪e	U+062A: ARABIC LETTER TEH	
+ط	t	t			ṫ			Lo	l	consonant		4		t̪oːy	U+0637: ARABIC LETTER TAH	
+ٿ	T	tʰ			tʰ			Lo		consonant		4		tʱe	U+067F: ARABIC LETTER TEHEH	
+چ	c	t͡ɕ			ʨ			Lo		consonant		4		t͡ʃe	U+0686: ARABIC LETTER TCHEH	
+ڇ	C	t͡ɕʰ			ʨʰ			Lo		consonant		4		t͡ʃʰe	U+0687: ARABIC LETTER TCHEHEH	
+د	d	d			d			Lo		consonant		2		dal	U+062F: ARABIC LETTER DAL	
+ڌ	D	dʰ			dʰ			Lo		consonant		2		dʱal	U+068C: ARABIC LETTER DAHAL	
+ج	j	d͡ʑ			ʥ			Lo		consonant		4		d͡ʒiːm	U+062C: ARABIC LETTER JEEM	
+جھ	J	d͡ʑʰ			ʥʰ					consonant		4		d͡ʒʱe	U+062C U+06BE: ARABIC LETTER JEEM, LETTER HEH DOACHASHMEE	
+جھہ	J	d͡ʑʰ			ʥʰ				?	consonant					U+062C U+06BE U+06C1: ARABIC LETTER JEEM, LETTER HEH DOACHASHMEE, LETTER HEH GOAL	
+ڏ	d	ɗ			ɗ			Lo		consonant		2		ᶑal	U+068F: ARABIC LETTER DAL WITH THREE DOTS ABOVE DOWNWARDS	
+ٽ	t	ʈ			ʈ			Lo		consonant		4		ʈe	U+067D: ARABIC LETTER TEH WITH THREE DOTS ABOVE DOWNWARDS	
+ٺ	T	ʈʰ			ʈʰ			Lo		consonant		4		ʈʰe	U+067A: ARABIC LETTER TTEHEH	
+ڊ	d	ɖ			ɖ			Lo		consonant		2		ɖal	U+068A: ARABIC LETTER DAL WITH DOT BELOW	
+ڍ	D	ɖʰ			ɖʰ			Lo		consonant		2		ɖʱal	U+068D: ARABIC LETTER DDAHAL	
+ڄ	j	ʄ			ʄ			Lo		consonant		4		ʄiːm	U+0684: ARABIC LETTER DYEH	
+ڪ	k	k			k			Lo		consonant		4		kaf	U+06AA: ARABIC LETTER SWASH KAF	
+ک	K	kʰ			kʰ			Lo		consonant		4		kʰaf	U+06A9: ARABIC LETTER KEHEH	
+گ	g	ɡ			ɡ			Lo		consonant		4		ɡaf	U+06AF: ARABIC LETTER GAF	
+گھ	G	ɡʰ			ɡʰ					consonant		4		ɡʱaf	U+06AF U+06BE U+06C1: ARABIC LETTER GAF, LETTER HEH DOACHASHMEE	
+گھہ	G	ɡʰ			ɡʰ				?	consonant					U+06AF U+06BE U+06C1: ARABIC LETTER GAF, LETTER HEH DOACHASHMEE, LETTER HEH GOAL	
+ڳ	g	ɠ			ɠ			Lo		consonant		4		ɠaf	U+06B3: ARABIC LETTER GUEH	
+ق	q	q			q			Lo		consonant		4		qaf	U+0642: ARABIC LETTER QAF	
+ف	f	f			f			Lo		consonant		4		fe	U+0641: ARABIC LETTER FEH	
+س	s	s			s			Lo		consonant		4		siːn	U+0633: ARABIC LETTER SEEN	
+ص	s	s			ṣ			Lo	l	consonant		4		swaːd	U+0635: ARABIC LETTER SAD	
+ث	s	s			s̱			Lo	l	consonant		4		se	U+062B: ARABIC LETTER THEH	
+ز	z	z			z͈			Lo		consonant		2		ze	U+0632: ARABIC LETTER ZAIN	
+ذ	z	z			z			Lo	l	consonant		2		zal	U+0630: ARABIC LETTER THAL	
+ظ	z	z			ẓ			Lo	l	consonant		4		zoːy	U+0638: ARABIC LETTER ZAH	
+ض	z	z			ẕ			Lo	l	consonant		4		zwaːd	U+0636: ARABIC LETTER DAD	
+ش	s	ʂ			ʂ			Lo		consonant		4		ʃiːn	U+0634: ARABIC LETTER SHEEN	
+خ	x	x			x			Lo		consonant		4		xe	U+062E: ARABIC LETTER KHAH	
+غ	x	ɣ			ɣ			Lo		consonant		4		ɣɛːn	U+063A: ARABIC LETTER GHAIN	
+ه	h	h			h			Lo		consonant		4			U+0647: ARABIC LETTER HEH	
+ح	h	h			h			Lo		consonant		4		he	U+062D: ARABIC LETTER HAH	
+ھ	h	ʰ			ʰ			Lo		aspiration marker	Only used for aspiration.	4			U+06BE: ARABIC LETTER HEH DOACHASHMEE	
+ہ	h	ʰ			ḥ			Lo	i	silent he	Only used in word-final position.	2			U+06C1: ARABIC LETTER HEH GOAL	
+م	m	m			m			Lo		consonant		4		miːm	U+0645: ARABIC LETTER MEEM	
+مھ	M	mʰ			mʰ					aspirated consonant		4			U+0645, U+06BE: ARABIC LETTER MEEM, LETTER HEH DOACHASHMEE	
+ن	n	n ̃			n			Lo		consonant		4		nuːn	U+0646: ARABIC LETTER NOON	
+نھ	N	nʰ			nʰ					aspirated consonant		4			U+0646, U+06BE: ARABIC LETTER NOON, LETTER HEH DOACHASHMEE	
+ڻ	n	ɳ			ɳ			Lo		consonant		4		ɳuːn	U+06BB: ARABIC LETTER RNOON	
+ڻھ	N	ɳʰ			ɳʰ					aspirated consonant		4			U+06BB, U+06BE: ARABIC LETTER RNOON, LETTER HEH DOACHASHMEE	
+ڃ	n	ɲ			ɲ			Lo		consonant		4		ɲe	U+0683: ARABIC LETTER NYEH	
+ڱ	N	ŋ			ŋ			Lo		consonant		4		ŋaf	U+06B1: ARABIC LETTER NGOEH	
+و	u	ʋ u		ʋ ʊ o oː ɔː uː	u			Lo		consonant/vowel		2			U+0648: ARABIC LETTER WAW	
+ر	r	r			r			Lo		consonant		2		re	U+0631: ARABIC LETTER REH	
+ڙ	r	ɽ			ɽ			Lo		consonant		2		ɽe	U+0699: ARABIC LETTER REH WITH FOUR DOTS ABOVE	
+ڙھ	R	ɽʰ			ɽʰ					aspirated consonant		4		ɽʱe	U+0699, U+06BE: ARABIC LETTER REH WITH FOUR DOTS ABOVE, LETTER HEH DOACHASHMEE	
+ل	l	l			l			Lo		consonant		4		lam	U+0644: ARABIC LETTER LAM	
+لھ	L	lʰ			lʰ					aspirated consonant		4			U+0644, U+06BE: ARABIC LETTER LAM, LETTER HEH DOACHASHMEE	
+ي	y	j iː			j			Lo		consonant/vowel		4			U+064A: ARABIC LETTER YEH	
+																
+ء	[	ɪ ʊ ə			ʔ					word-final standalone vowel carrier					U+0621: ARABIC LETTER HAMZA	
+ئ	[	i ə ∅			y					word-medial standalone vowel carrier		4			U+0626: ARABIC LETTER YEH WITH HAMZA ABOVE	
+																
+																
+// post-consonant vowels																
+ِي		i				i			r	vowel in vocalised text					U+0650 U+064A: ARABIC KASRA, LETTER YEH	
+ِ	i	ɪ				ɪ		Mn	r	vowel in vocalised text					U+0650: ARABIC KASRA	
+ُ	u	ʊ				ʊ		Mn	r	vowel in vocalised text					U+064F: ARABIC DAMMA	
+ُو		u				u			r	vowel in vocalised text					U+064F U+0648: ARABIC DAMMA, LETTER WAW	
+َ	a	ə				ə		Mn	r	vowel in vocalised text					U+064E: ARABIC FATHA	
+ع	[	æ		ɑːoːeːʔ∅		o		Lo		vowel		4		ɛːn	U+0639: ARABIC LETTER AIN	
+ا	a	a		ə ʔ		ɑ		Lo		vowel		2			U+0627: ARABIC LETTER ALEF	
+آ	A	ɑ ɑː			ạ	a		Lo		standalone vowel					U+0622: ARABIC LETTER ALEF WITH MADDA ABOVE	
+																
+// standalone vowels																
+اِي		i				i			r	standalone vowel in vocalised text					U+0627 U+0650 U+064A: ARABIC LETTER ALEF, KASRA, LETTER YEH	
+ئِي		i				i			r	standalone vowel in vocalised text					U+0626 U+064E U+064A: ARABIC LETTER YEH WITH HAMZA ABOVE, FATHA, LETTER YEH	
+اِ		ɪ				ɪ			r	standalone vowel in vocalised text					U+0627 U+0650: ARABIC LETTER ALEF, KASRA	
+ئِ		ɪ				ɪ			r	standalone vowel in vocalised text					U+0626 U+0650: ARABIC LETTER YEH WITH HAMZA ABOVE, KASRA	
+ءِ		ɪ				ɪ			r	standalone vowel in vocalised text					U+0621 U+0650: ARABIC LETTER HAMZA, KASRA	
+اُ		ʊ				ʊ			r	standalone vowel in vocalised text					U+0627 U+064F: ARABIC LETTER ALEF, DAMMA	
+ئُ		ʊ				ʊ			r	standalone vowel in vocalised text					U+0626 U+064F: ARABIC LETTER YEH WITH HAMZA ABOVE, DAMMA	
+ءُ		ʊ				ʊ			r	standalone vowel in vocalised text					U+0621 U+064F: ARABIC LETTER HAMZA, DAMMA	
+اُو		u				u			r	standalone vowel in vocalised text					U+0627 U+064F U+0648: ARABIC LETTER ALEF, DAMMA, LETTER WAW	
+ئو		u				u				standalone vowel in vocalised text					U+0626 U+0648: ARABIC LETTER YEH WITH HAMZA ABOVE, LETTER WAW	
+او		o				o				standalone vowel in vocalised text					U+0627 U+0648: ARABIC LETTER ALEF, LETTER WAW	
+ئو		o				o				standalone vowel in vocalised text					U+0626 U+0648: ARABIC LETTER YEH WITH HAMZA ABOVE, LETTER WAW	
+ئَ		ə				ə			r	standalone vowel in vocalised text					U+0626 U+064E: ARABIC LETTER YEH WITH HAMZA ABOVE, FATHA	
+ءَ		ə				ə			r	standalone vowel in vocalised text					U+0621 U+064E: ARABIC LETTER HAMZA, FATHA	
+																
+۽	&				&			So		ampersand					U+06FD: ARABIC SIGN SINDHI AMPERSAND	
+۾	&	mẽ			{loc}			So		locative case marker					U+06FE: ARABIC SIGN SINDHI POSTPOSITION MEN	
+																
+// space																
+\u0020	 							Zs		space					U+0020: SPACE	
+\u200B	=				ᶻʷˢᵖ			Cf		zero-width space					U+200B: ZERO WIDTH SPACE	
+// baseline extender																
+ـ	_				_	_			?	baseline extender					U+0640: ARABIC TATWEEL	
+// phrase dividers																
+،	,				,̣	⹁		Po	x	comma					U+060C: ARABIC COMMA	
+,	,				,	,		Po	?	comma					U+002C: COMMA	
+؛	;				;̣	;		Po	x	semicolon					U+061B: ARABIC SEMICOLON	
+⹁	,				,	⹁		Po		comma					U+2E41: REVERSED COMMA	
+⁏	;				;	;		Po		semicolon					U+204F: REVERSED SEMICOLON	
+!	!				!	!		Po		exclamation mark					U+0021: EXCLAMATION MARK	
+؟	?				?	?		Po		question mark					U+061F: ARABIC QUESTION MARK	
+// colons & length marks																
+:	:				:	:		Po		colon					U+003A: COLON	
+// period & ellipsis																
+.	.				.	.		Po		full stop					U+002E: FULL STOP	
+…	.				…	…		Po	?	ellipsis					U+2026: HORIZONTAL ELLIPSIS	
+«	<				«	“		Pi	?	quotation mark					U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+»	>				»	”		Pf	?	quotation mark					U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	<				‹			Pi	?	quotation mark					U+2018: LEFT SINGLE QUOTATION MARK	
+›	>				›			Pf	?	quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	
+“	<				“	“		Pi	?	quotation mark					U+201C: LEFT DOUBLE QUOTATION MARK	
+”	>				”	”		Pf	?	quotation mark					U+201D: RIGHT DOUBLE QUOTATION MARK	
+‘	<				‘	‘		Pi	?	quotation mark					U+2018: LEFT SINGLE QUOTATION MARK	
+’	>				’	’		Pf	?	quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	
+// brackets																
+(	(				( (˙ (¯ (¨ (˭	(		Ps		parenthesis					U+0028: LEFT PARENTHESIS	
+)	)				) )˙ )¯ )¨ )˭	)		Ps		parenthesis					U+0029: RIGHT PARENTHESIS	
+[	(				[	[		Pe		bracket					U+005B: LEFT SQUARE BRACKET	
+]	)				] 	]		Pe		bracket					U+005D: RIGHT SQUARE BRACKET	
+﴾	(				[˙			Pe	?	ornate parenthesis					U+FD3E: ORNATE LEFT PARENTHESIS	
+﴿	)				]˙			Ps	?	ornate parenthesis					U+FD3F: ORNATE RIGHT PARENTHESIS	
+// dashes																
+-	-				-	-		Pd	?	hyphen					U+2010: HYPHEN	
+‑	-				‑	‑		Pd	?	non-breaking hyphen					U+2011: NON-BREAKING HYPHEN	
+–	-				–	–		Pd	?	en dash					U+2013: EN DASH	
+—	-				—	—		Pd	?	em dash					U+2014: EM DASH	
+// virama, sukun, silencers, etc																
+ْ	*				˟				r	vowel absence indicator					U+0652: ARABIC SUKUN	
+// formatting characters																
+\u200C	=				ᶻʷⁿʲ			Cf		zero-width non-joiner					U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=				ᶻʷʲ			Cf		zero-width joiner					U+200D: ZERO WIDTH JOINER	
+\u034F	=				ᶜᵍʲ			Mn		combining grapheme joiner					U+034F: COMBINING GRAPHEME JOINER	
+\u2060	=				ʷʲ			Cf		word joiner					U+2060 WORD JOINER	
+\u2067	=				ʳˡⁱ			Cf		rtl isolate					U+2067: RIGHT-TO-LEFT ISOLATE	
+\u2066	=				ˡʳⁱ			Cf		ltr isolate					U+2066: LEFT-TO-RIGHT ISOLATE	
+\u2068	=				ᶠˢⁱ			Cf		first-strong isolate					U+2068: FIRST STRONG ISOLATE	
+\u2069	=				ᵖᵈⁱ			Cf		pop direction isolate					U+2069: POP DIRECTIONAL ISOLATE	
+\u202B	=				ʳˡᵉ			Cf	x	rtl embed					U+202B: RIGHT-TO-LEFT EMBEDDING	
+\u202A	=				ˡʳᵉ			Cf	x	ltr embed					U+202A: LEFT-TO-RIGHT EMBEDDING	
+\u202C	=				ᵖᵈᶠ			Cf	x	pop direction					U+202C: POP DIRECTIONAL FORMATTING	
+\u200F	=				ʳˡᵐ			Cf		rtl mark					U+200F: RIGHT-TO-LEFT MARK	
+\u200E	=				ˡʳᵐ			Cf		ltr mark					U+200E: LEFT-TO-RIGHT MARK	
+\u061C	=				ᵃˡᵐ			Cf		arabic letter mark					U+061C: ARABIC LETTER MARK	
+// digits																
+۰	±	0			0̣	0		Nd		digit			ٻُڙِي	ɓuṛi	U+06F0: EXTENDED ARABIC-INDIC DIGIT ZERO	
+۱	±	1			1̣	1		Nd		digit			هِڪُ	hiku	U+06F1: EXTENDED ARABIC-INDIC DIGIT ONE	
+۲	±	2			2̣	2		Nd		digit			ٻَه	ɓa	U+06F2: EXTENDED ARABIC-INDIC DIGIT TWO	
+۳	±	3			3̣	3		Nd		digit			ٽِي	ṭī	U+06F3: EXTENDED ARABIC-INDIC DIGIT THREE	
+۴	±	4			4̣	4		Nd		digit			چَارِ	cāri	U+06F4: EXTENDED ARABIC-INDIC DIGIT FOUR	
+۵	±	5			5̣	5		Nd		digit			پَنج	pañja	U+06F5: EXTENDED ARABIC-INDIC DIGIT FIVE	
+۶	±	6			6̣	6		Nd		digit			ڇَهَه	chaha	U+06F6: EXTENDED ARABIC-INDIC DIGIT SIX	
+۷	±	7			7̣	7		Nd		digit			سَتَ	sata	U+06F7: EXTENDED ARABIC-INDIC DIGIT SEVEN	
+۸	±	8			8̣	8		Nd		digit			اَٺَ	aṭha	U+06F8: EXTENDED ARABIC-INDIC DIGIT EIGHT	
+۹	±	9			9̣	9		Nd		digit			نَوَ	nava	U+06F9: EXTENDED ARABIC-INDIC DIGIT NINE	
+																
+ٔ					ٴ			Mn​	r	combining hamza	Orphan. Only found in decomposed text with YEH.				U+0654: ARABIC HAMZA ABOVE	
+ٓ					˜			Mn	r	combining madd	Orphan. Only found in decomposed text with ALEF.				U+0653: ARABIC MADDAH ABOVE	
+																
+																
+																
+																
+																
+																
+																
+`																
+																
+latinPanel = 'bʰ cʰ dʰ eː iː kʰ oː pʰ tʰ uː ŋ ɑː ɓ ɔː ɖ ɖʰ ɗ ɟ ɟʰ ɠ ɡ ɡʰ ɣ ɲ ɳ ɽ ʂ ʄ ʈ ʈʰ ʊ ʋ ʔ ∅'																
+																
+var cols = {																
+key: 1,																
+ipaLoc: 2,																
+ipaPlus: 3,																
+ipaMore: 4,																
+transLoc: 5,																
+transcription: 6,																
+kbd: 7,																
+class: 8,																
+status: 9,																
+typeLoc: 10,																
+statusLoc: 11,																
+																
+dvowel: 0,																
+ivowel: 0,																
+subj:0,																
+ltone:0,																
+htone:0,																
+lc:0,																
+uc:0,																
+meaning:0,																
+shape: 12,																
+numLoc: 0,																
+																
+nnameLoc: 13,																
+nameLoc: 14,																
+ucsName: 15,																
+block: 16,																
+																
+//othertranscriptions: [[5, 'Latin']]																
+}																
