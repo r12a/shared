738ea5d8f7ae8a6061345ad84a925b9cfe5a599a
r12a <ishida@w3.org>
2024-10-26 17:33:01 +0100

    + tols-kru

---
+++ scriptdb/tols-kru.js
@@ -0,0 +1,163 @@
+var spreadsheet = `tols-kru	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	block
+\u{11DB6}	p	p		p	p		Lo		consonant				U+11DB6: TOLONG SIKI LETTER PA	
+\u{11DB7}	P	pʰ		pʰ	ph		Lo		consonant				U+11DB7: TOLONG SIKI LETTER PHA	
+\u{11DB8}	b	b		b	b		Lo		consonant				U+11DB8: TOLONG SIKI LETTER BA	
+\u{11DB9}	B	bʰ		bʰ	bh		Lo		consonant				U+11DB9: TOLONG SIKI LETTER BHA	
+\u{11DBB}	t	t		t	t		Lo		consonant				U+11DBB: TOLONG SIKI LETTER TA	
+\u{11DBC}	T	tʰ		tʰ	th		Lo		consonant				U+11DBC: TOLONG SIKI LETTER THA	
+\u{11DBD}	d	d		d	d		Lo		consonant				U+11DBD: TOLONG SIKI LETTER DA	
+\u{11DBE}	D	dʰ		dʰ	dh		Lo		consonant				U+11DBE: TOLONG SIKI LETTER DHA	
+\u{11DC0}	t	ʈ		ʈ			Lo		consonant				U+11DC0: TOLONG SIKI LETTER TTA	
+\u{11DC1}	T	ʈʰ		ʈʰ			Lo		consonant				U+11DC1: TOLONG SIKI LETTER TTHA	
+\u{11DC2}	d	ɖ		ɖ			Lo		consonant				U+11DC2: TOLONG SIKI LETTER DDA	
+\u{11DC3}	D	ɖʰ		ɖʰ			Lo		consonant				U+11DC3: TOLONG SIKI LETTER DDHA	
+\u{11DC5}	c	t͡ʃ		ʧ			Lo		consonant				U+11DC5: TOLONG SIKI LETTER CA	
+\u{11DC6}	C	t͡ʃʰ		ʧʰ			Lo		consonant				U+11DC6: TOLONG SIKI LETTER CHA	
+\u{11DC7}	j	d͡ʒ		ʤ			Lo		consonant				U+11DC7: TOLONG SIKI LETTER JA	
+\u{11DC8}	J	d͡ʒʰ		ʤʰ			Lo		consonant				U+11DC8: TOLONG SIKI LETTER JHA	
+\u{11DCA}	k	k		k			Lo		consonant				U+11DCA: TOLONG SIKI LETTER KA	
+\u{11DCB}	K	kʰ		kʰ			Lo		consonant				U+11DCB: TOLONG SIKI LETTER KHA	
+\u{11DCC}	g	ɡ		ɡ			Lo		consonant				U+11DCC: TOLONG SIKI LETTER GA	
+\u{11DCD}	G	ɡʰ		ɡʰ			Lo		consonant				U+11DCD: TOLONG SIKI LETTER GHA	
+														
+\u{11DD4}	s	s		s			Lo		consonant				U+11DD4: TOLONG SIKI LETTER SA	
+\u{11DD6}	x	x		x			Lo		consonant				U+11DD6: TOLONG SIKI LETTER XA	
+\u{11DD5}	h	h		h			Lo		consonant				U+11DD5: TOLONG SIKI LETTER HA	
+														
+\u{11DBA}	m	m		m			Lo		consonant				U+11DBA: TOLONG SIKI LETTER MA	
+\u{11DBF}	n	n		n			Lo		consonant				U+11DBF: TOLONG SIKI LETTER NA	
+\u{11DC4}	n	ɳ		ɳ			Lo		consonant				U+11DC4: TOLONG SIKI LETTER NNA	
+\u{11DC9}	n	ɲ		ɲ			Lo		consonant				U+11DC9: TOLONG SIKI LETTER NYA	
+\u{11DD3}	n	ɲ		ɲʲ			Lo		consonant				U+11DD3: TOLONG SIKI LETTER NNYA	
+\u{11DCE}	n	ŋ		ŋ			Lo		consonant				U+11DCE: TOLONG SIKI LETTER NGA	
+														
+\u{11DD2}	v	w		v			Lo		consonant				U+11DD2: TOLONG SIKI LETTER VA	
+\u{11DD0}	r	r		r			Lo		consonant				U+11DD0: TOLONG SIKI LETTER RA	
+\u{11DD7}	r	ɽ		ɽ			Lo		consonant				U+11DD7: TOLONG SIKI LETTER RRA	
+\u{11DD8}	r	ɽʰ		ɽʰ			Lo		consonant				U+11DD8: TOLONG SIKI LETTER RRHA	
+\u{11DD1}	l	l		l			Lo		consonant				U+11DD1: TOLONG SIKI LETTER LA	
+\u{11DCF}	y	j		j			Lo		consonant				U+11DCF: TOLONG SIKI LETTER YA	
+														
+\u{11DB0}	i	i		i			Lo		consonant				U+11DB0: TOLONG SIKI LETTER I	
+\u{11DB2}	u	u		u			Lo		consonant				U+11DB2: TOLONG SIKI LETTER U	
+\u{11DB1}	e	e		e			Lo		consonant				U+11DB1: TOLONG SIKI LETTER E	
+\u{11DB3}	o	o		o			Lo		consonant				U+11DB3: TOLONG SIKI LETTER O	
+\u{11DB4}	a	a		a			Lo		consonant				U+11DB4: TOLONG SIKI LETTER A	
+\u{11DB5}	A	aː		aː			Lo		consonant				U+11DB5: TOLONG SIKI LETTER AA	
+														
+\u{11DB5}̆	o	ɔ						l	vowel				U+11DB5 U+0306: TOLONG SIKI LETTER AA, COMBINING BREVE	
+														
+\u{11DDA}	}	ʔ		ʔ			Lo		glottal stop				U+11DDA: TOLONG SIKI SIGN HECAKA	
+														
+// finals														
+̰	r	r		r̆			Mn​		medial r			revā	U+0330: COMBINING TILDE BELOW	
+̇	n	ɴ		ᴎ			Mn​		homorganic final nasal			mitalā	U+0307: COMBINING DOT ABOVE	
+̆	[			˘			Mn​		vowel modifier			phavā	U+0306: COMBINING BREVE	
+														
+// signs														
+\u{11DDB}							Lo		auspicious sign				U+11DDB: TOLONG SIKI UNGGA	
+														
+														
+														
+// space														
+\u0020	 						Zs		space				U+0020: SPACE	
+\u200B	=			ᶻʷˢᵖ			Cf		zero-width space				U+200B: ZERO WIDTH SPACE	
+\u202F	]			ⁿⁿᵇˢᵖ			Zs		nnbsp				U+202F NARROW NO-BREAK SPACE	
+// word separator														
+ʼ	'			•	ʼ		Lm		syllable separator			ghetalā	U+02BC: MODIFIER LETTER APOSTROPHE	✓
+// nasalisation														
+̃	[¶	̃		˜	̃		Mn​		nasalisation			evā	U+0303: COMBINING TILDE	
+// section dividers														
+।	|			|	.		Po	i	section divider				U+0964: DEVANAGARI DANDA	
+॥	|			¶			Po	i	section divider				U+0965: DEVANAGARI DOUBLE DANDA	
+// phrase dividers														
+,	,			,	,		Po		comma			ucari ṭuṛā	U+002C: COMMA	
+;	;			;	;	;;⁏⸵	Po		semicolon			micari ṭuṛā	U+003B: SEMICOLON	
+!	!			!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK	
+?	?			?	?		Po		question mark				U+003F: QUESTION MARK	
+// colons & length marks														
+\u{11DD9}	:			ː			Lo		vowel length mark				U+11DD9: TOLONG SIKI SIGN SELA	✓
+:	:			:	:		Po		colon				U+003A: COLON	✓
+// period & ellipsis														
+.	.			.	.		Po		full stop			gahala ṭuṛā	U+002E: FULL STOP	
+…	.			…	…		Po	?	ellipsis				U+2026: HORIZONTAL ELLIPSIS	
+// quote marks														
+“	<			“	“		Pi	?	quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
+”	>			”	”		Pf	?	quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
+‘	<			‘	‘		Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+’	>			’	’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+// brackets														
+(	(			(	(		Ps	?	parenthesis				U+0028: LEFT PARENTHESIS	
+)	)			)	)		Ps	?	parenthesis				U+0029: RIGHT PARENTHESIS	
+[	(			[	[		Pe	?	bracket				U+005B: LEFT SQUARE BRACKET	
+]	)			] 	]		Pe	?	bracket				U+005D: RIGHT SQUARE BRACKET	
+// dashes														
+-	-			-	-		Pd		hyphen			pacha	U+2010: HYPHEN	
+‑	-			‑	‑		Pd	?	non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd		en dash				U+2013: EN DASH	
+—	-			—	—		Pd		em dash				U+2014: EM DASH	
+// nuktas														
+̈	/			ˑ			Mn		nukta			ilā	U+0308: COMBINING DIAERESIS	
+̤	/			ˑ̣			Mn		nukta			uphā	U+0324: COMBINING DIAERESIS BELOW	
+•	•			•			Po		bullet				U+2022: BULLET	
+// digits														
+\u{11DE0}	±			1̣	1		Nd		digit				U+11DE0: TOLONG SIKI DIGIT ZERO	
+\u{11DE1}	±			2̣	2		Nd		digit				U+11DE1: TOLONG SIKI DIGIT ONE	
+\u{11DE2}	±			3̣	3		Nd		digit				U+11DE2: TOLONG SIKI DIGIT TWO	
+\u{11DE3}	±			4̣	4		Nd		digit				U+11DE3: TOLONG SIKI DIGIT THREE	
+\u{11DE4}	±			5̣	5		Nd		digit				U+11DE4: TOLONG SIKI DIGIT FOUR	
+\u{11DE5}	±			6̣	6		Nd		digit				U+11DE5: TOLONG SIKI DIGIT FIVE	
+\u{11DE6}	±			7̣	7		Nd		digit				U+11DE6: TOLONG SIKI DIGIT SIX	
+\u{11DE7}	±			8̣	8		Nd		digit				U+11DE7: TOLONG SIKI DIGIT SEVEN	
+\u{11DE8}	±			9̣	9		Nd		digit				U+11DE8: TOLONG SIKI DIGIT EIGHT	
+\u{11DE9}	±			0̣	0		Nd		digit				U+11DE9: TOLONG SIKI DIGIT NINE	
+// number related														
+%	%			%			Po		percentage mark				U+0025: PERCENT SIGN	
+‰	%			‰			Po	?	per mille mark				U+2030: PER MILLE SIGN	
+// ascii digits														
+0	0			0	0		Nd		digit				U+0030: DIGIT ZERO	
+1	1			1	1		Nd		digit				U+0031: DIGIT ONE	
+2	2			2	2		Nd		digit				U+0032: DIGIT TWO	
+3	3			3	3		Nd		digit				U+0033: DIGIT THREE	
+4	4			4	4		Nd		digit				U+0034: DIGIT FOUR	
+5	5			5	5		Nd		digit				U+0035: DIGIT FIVE	
+6	6			6	6		Nd		digit				U+0036: DIGIT SIX	
+7	7			7	7		Nd		digit				U+0037: DIGIT SEVEN	
+8	8			8	8		Nd		digit				U+0038: DIGIT EIGHT	
+9	9			9	9		Nd		digit				U+0039: DIGIT NINE	
+`														
+														
+latinPanel = 'ä bʷ č'														
+														
+var cols = {														
+key: 1,														
+ipaLoc: 2,														
+ipaPlus: 3,														
+transLoc: 4,														
+transcription: 5,														
+kbd: 6,														
+class: 7,														
+status: 8,														
+typeLoc: 9,														
+statusLoc: 10,														
+														
+dvowel: 0,														
+ivowel: 0,														
+onset:0,														
+final:0,														
+subj:0,														
+ltone:0,														
+htone:0,														
+lc:0,														
+uc:0,														
+meaning:0,														
+shape: 0,														
+numLoc: 0,														
+														
+nnameLoc: 11,														
+nameLoc: 12,														
+ucsName: 13,														
+block: 14,														
+														
+//othertranscriptions: [[5, 'Latin']]														
+}														
