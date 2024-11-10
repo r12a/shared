c828915ce8fae26cb2b8ae57b86a4a64d6611ee3
r12a <ishida@w3.org>
2024-11-10 07:32:38 +0000

    + tayo

---
+++ scriptdb/tayo-tyj.js
@@ -0,0 +1,204 @@
+var spreadsheet = `tayo-tyj	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	block
+														
+\u{1E6D1}	p	p		p			Lo		low class consonant				U+1E6D1: TAI YO LETTER LOW PO	
+\u{1E6D2}	p	p		p̣			Lo		high class consonant				U+1E6D2: TAI YO LETTER HIGH PO	
+\u{1E6D3}	P	pʰ		pʰ			Lo		consonant				U+1E6D3: TAI YO LETTER PHO	
+\u{1E6D0}	b	ɓ -p		b			Lo		consonant				U+1E6D0: TAI YO LETTER BO	
+\u{1E6CC}	t	t		t			Lo		low class consonant				U+1E6CC: TAI YO LETTER LOW TO	
+\u{1E6CD}	t	t		ṭ			Lo		high class consonant				U+1E6CD: TAI YO LETTER HIGH DO	
+\u{1E6CE}	T	tʰ		tʰ			Lo		consonant				U+1E6CE: TAI YO LETTER THO	
+\u{1E6CB}	d	ɗ -t		d			Lo		consonant				U+1E6CB: TAI YO LETTER DO	
+\u{1E6C0}	k	k		k			Lo		low class consonant				U+1E6C0: TAI YO LETTER LOW KO	
+\u{1E6C1}	k	k		ḳ			Lo		high class consonant				U+1E6C1: TAI YO LETTER HIGH KO	
+\u{1E6C2}	K	kʰ		kʰ			Lo		low class consonant				U+1E6C2: TAI YO LETTER LOW KHO	
+\u{1E6C3}	K	kʰ		ḳʰ			Lo	i	high class consonant				U+1E6C3: TAI YO LETTER HIGH KHO	
+\u{1E6C4}	g	ɣ		ɡ			Lo		consonant				U+1E6C4: TAI YO LETTER GO	
+\u{1E6DC}	}	ʔ		ʔ			Lo		consonant				U+1E6DC: TAI YO LETTER QO	
+														
+\u{1E6C6}	c	t͡ʃ		ʧ			Lo		consonant				U+1E6C6: TAI YO LETTER CHO	
+														
+\u{1E6D4}	f	f		f			Lo		low class consonant				U+1E6D4: TAI YO LETTER LOW FO	
+\u{1E6D5}	f	f		f̣			Lo		high class consonant				U+1E6D5: TAI YO LETTER HIGH FO	
+\u{1E6C7}	x	s		s			Lo		low class consonant				U+1E6C7: TAI YO LETTER LOW XO	
+\u{1E6C8}	x	s		ṣ			Lo		high class consonant				U+1E6C8: TAI YO LETTER HIGH XO	
+\u{1E6DA}	h	h		h			Lo		low class consonant				U+1E6DA: TAI YO LETTER LOW HO	
+\u{1E6DB}	h	h		ḥ			Lo		high class consonant				U+1E6DB: TAI YO LETTER HIGH HO	
+														
+\u{1E6D6}	m	m		m			Lo		consonant				U+1E6D56: TAI YO LETTER MO	
+\u{1E6CF}	n	n		n			Lo		consonant				U+1E6CF: TAI YO LETTER NO	
+\u{1E6C9}	n	ɲ -j		ɲ			Lo		low class consonant				U+1E6C9: TAI YO LETTER LOW NYO	
+\u{1E6CA}	n	ɲ		ɲ̣			Lo		high class consonant				U+1E6CA: TAI YO LETTER HIGH NYO	
+\u{1E6C5}	N	ŋ -i		ŋ			Lo		consonant				U+1E6C5: TAI YO LETTER NGO	
+														
+\u{1E6D9}	v	v -w		w			Lo		consonant				U+1E6D29: TAI YO LETTER VO	
+\u{1E6D8}	l	l		l			Lo		consonant				U+1E6D8: TAI YO LETTER LO	
+\u{1E6D7}	y	j		j			Lo		consonant				U+1E6D7: TAI YO LETTER YO	
+														
+														
+\u{1E6DD}	k	kʷ		kʷ			Lo		low class consonant				U+1E6DD: TAI YO LETTER LOW KVO	
+\u{1E6DE}	k	kʷ		ḳʷ			Lo		high class consonant				U+1E6DE: TAI YO LETTER HIGH KVO	
+														
+\u{1E6E1}	i	i		i			Lo		vowel				U+1E6E1: TAI YO LETTER I	
+\u{1E6E2}	i	ɨ		ɨ			Lo		vowel				U+1E6E2: TAI YO LETTER EU	
+\u{1E6E3}	i	ɨ		ɨ̣			Mn		vowel sign				U+1E6E3: TAI YO SIGN EU	
+\u{1E6E4}	u	u		u			Lo		vowel				U+1E6E4: TAI YO LETTER U	
+														
+\u{1E6E8}	e	e		e			Lo		vowel				U+1E6E8: TAI YO SIGN E	
+\u{1E6E7}	o	o		o			Lo		vowel				U+1E6E7: TAI YO LETTER O	
+														
+\u{1E6ED}	a	ə		ə			Lo		vowel				U+1E6ED: TAI YO LETTER AUE	
+														
+\u{1E6E5}	e	ɛ		ɛ			Lo		vowel				U+1E6E5: TAI YO LETTER AE	
+\u{1E6EC}	o	ɔː		ɔː			Lo		vowel				U+1E6EC: TAI YO LETTER OO	
+														
+\u{1E6E0}	a	aː		a			Lo		vowel				U+1E6E0: TAI YO LETTER AA	
+														
+\u{1E6E9}	i	iə̯		iᵊ			Lo		diphthong				U+1E6E9: TAI YO LETTER IA	
+\u{1E6EA}	u	ɨə		ɨᵊ			Lo		diphthong				U+1E6EA: TAI YO LETTER UEA	
+\u{1E6EB}	u	uə̯		uᵊ			Lo		diphthong				U+1E6EB: TAI YO LETTER UA	
+														
+														
+\u{1E6F4}	A	ap		aᵖ			Lo		rhyme				U+1E6F4: TAI YO LETTER AP	
+\u{1E6F3}	A	at		aᵗ			Lo		rhyme				U+1E6F3: TAI YO LETTER AT	
+\u{1E6F2}	A	ak		aᵏ			Lo		rhyme				U+1E6F2: TAI YO LETTER AK	
+\u{1E6F1}	A	am		aᵐ			Lo		rhyme				U+1E6F1: TAI YO LETTER AM	
+\u{1E6F0}	A	an		aⁿ			Lo		rhyme				U+1E6F0: TAI YO LETTER AN	
+\u{1E6EF}	A	aŋ		aᵑ			Mn		rhyme				U+1E6EF: TAI YO SIGN ANG	
+\u{1E6E6}	a	au̯		aᵘ			Mn		rhyme				U+1E6E6: TAI YO SIGN AU	
+\u{1E6EE}	A	aj -w-		aʲ			Mn		rhyme; medial w				U+1E6EE: TAI YO SIGN AY	
+\u{1E6F5}	O	om		oᵐ			Mn		rhyme				U+1E6F5: TAI YO SIGN OM	
+														
+\u{1E6FE}	M	mɨəŋ		{mɨəŋ}			Lo		logogram for village			mương	U+1E6FE: TAI YO SYMBOL MEUANG	
+														
+// repetition														
+\u{1E6FF}	&			&			Lm		repetition				U+1E6FF: TAI YO XAM LAI	
+														
+														
+														
+// space														
+\u0020	 						Zs	?	space					
+\u200B	=			ᶻʷˢᵖ			Cf	?	zero-width space				U+200B: ZERO WIDTH SPACE	
+\u202F	]			ⁿⁿᵇˢᵖ			Zs	?	nnbsp					
+// nasalisation														
+‍ଁ	[¶	̃		˜	̃			?	nasalisation				U+200D U+0B01: ZERO WIDTH JOINER, ORIYA SIGN CANDRABINDU	
+// phrase dividers														
+、	,			,	⹁		Po		comma				U+3001: IDEOGRAPHIC COMMA	
+,	,			,̣	,		Po		comma				U+002C: COMMA	
+;	;			; ;̣ ;̱ ;͈ ;͇	;	;;⁏⸵	Po	?	semicolon				U+003B: SEMICOLON	
+！	!			!	!		Po		exclamation mark				U+FF01: FULLWIDTH EXCLAMATION MARK	
+？	?			?	?		Po		question mark				U+FF1F: FULLWIDTH QUESTION MARK	
+// colons & length marks														
+：	:			:	:		Po		colon				U+FF1A: FULLWIDTH COLON	
+// period & ellipsis														
+。	.			.	.		Po		full stop				U+3002: IDEOGRAPHIC FULL STOP	
+…	.			…	…		Po	?	ellipsis				U+2026: HORIZONTAL ELLIPSIS	
+⋯	.			⋯̇ ⋯̄ ⋯̎ ⋯̿	⋯		Sm	?	midline ellipsis				U+22EF: MIDLINE HORIZONTAL ELLIPSIS	
+// quote marks														
+«	<			«	“		Pi	?	quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+»	>			»	”		Pf	?	quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	<			‹			Pi		quotation mark				U+2039: SINGLE LEFT-POINTING ANGLE QUOTATION MARK	
+›	>			›			Pf		quotation mark				U+203A: SINGLE RIGHT-POINTING ANGLE QUOTATION MARK	
+“	<			“	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
+”	>			”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
+‘	<			‘	‘		Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+’	>			’	’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+// brackets														
+（	(			(	(		Ps		parenthesis				U+FF08: FULLWIDTH LEFT PARENTHESIS	
+）	)			)	)		Ps		parenthesis				U+FF09: FULLWIDTH RIGHT PARENTHESIS	
+［	(				[		Pe		bracket				U+FF3B: FULLWIDTH LEFT SQUARE BRACKET	
+］	)			] 	]		Pe		bracket				U+FF3D: FULLWIDTH RIGHT SQUARE BRACKET	
+// dashes														
+-	-			-	-		Pd	?	hyphen				: 	
+‑	-			‑	‑		Pd	?	non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd	?	en dash				U+2013: EN DASH	
+—	-			—	—		Pd	?	em dash				U+2014: EM DASH	
+// apostrophe														
+'	\'			ʼ	ʼ		Lm	?	apostrophe				U+0027: APOSTROPHE	
+// abbrev.														
+	+			¨				?	abbreviation marker					
+// misc diacritics														
+	[			+				?	vowel carrier					
+	[			˘				?	vowel shortener					
+	[			ᵊ				?	vowel retainer					
+	[							?	consonant shifter					
+	[			ˈ				?	superscript alef					
+	[			ʿ				?	hamza above					
+	[			˓				?	hamza below					
+٭	[			*				?	star				U+066D: ARABIC FIVE POINTED STAR	
+	[			~				?	maddah					
+	[			″				?	register change					
+	[			°				?	special					
+	[			˙				?	combining dot above					
+	[			ʼ				?	combining hook above					
+	[			ˆ				?	combining circumflex					
+	[			ˉ				?	combining macron					
+	[			̨				?	combining ogonek					
+	[			ᶠ				?	feminine marker					
+// other														
+•	•			•			Po	?	bullet				U+2022: BULLET	
+// formatting characters														
+\u200C	=			ᶻʷⁿʲ			Cf	?	zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=			ᶻʷʲ			Cf	?	zero-width joiner				U+200D: ZERO WIDTH JOINER	
+\u034F	=			ᶜᵍʲ			Mn	?	combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER	
+\u2060	=			ʷʲ			Cf	?	word joiner				U+2060: WORD JOINER	
+// digits														
+1	±			1̣	1		Nd	?	digit				U+0031: DIGIT ONE	
+2	±			2̣	2		Nd	?	digit				U+0032: DIGIT TWO	
+3	±			3̣	3		Nd	?	digit				U+0033: DIGIT THREE	
+4	±			4̣	4		Nd	?	digit				U+0034: DIGIT FOUR	
+5	±			5̣	5		Nd	?	digit				U+0035: DIGIT FIVE	
+6	±			6̣	6		Nd	?	digit				U+0036: DIGIT SIX	
+7	±			7̣	7		Nd	?	digit				U+0037: DIGIT SEVEN	
+8	±			8̣	8		Nd	?	digit				U+0038: DIGIT EIGHT	
+9	±			9̣	9		Nd	?	digit				U+0039: DIGIT NINE	
+0	±			0̣	0		Nd	?	digit				U+0030: DIGIT ZERO	
+// number related														
+%	%			%			Po	?	percentage mark				U+0025: PERCENT SIGN	
+‰	%			‰			Po	?	per mille mark				U+2030: PER MILLE SIGN	
+// ascii digits														
+0	0			0	0		Nd		digit				U+0030: DIGIT ZERO	
+1	1			1	1		Nd		digit				U+0031: DIGIT ONE	
+2	2			2	2		Nd		digit				U+0032: DIGIT TWO	
+3	3			3	3		Nd		digit				U+0033: DIGIT THREE	
+4	4			4	4		Nd		digit				U+0034: DIGIT FOUR	
+5	5			5	5		Nd		digit				U+0035: DIGIT FIVE	
+6	6			6	6		Nd		digit				U+0036: DIGIT SIX	
+7	7			7	7		Nd		digit				U+0037: DIGIT SEVEN	
+8	8			8	8		Nd		digit				U+0038: DIGIT EIGHT	
+9	9			9	9		Nd		digit				U+0039: DIGIT NINE	
+`														
+														
+latinPanel = 'aː kʰ kʷ oː pʰ tʰ t͡ʃ u̯ ŋ ə ə̯ ɛ ɡ ɨ ɲ ɲ ʔ'														
+														
+var cols = {														
+key: 1,														
+ipaLoc: 2,														
+ipaPlus: 3,														
+transLoc: 4,														
+transcription: 5,														
+kbd: 6,														
+class: 7,														
+status: 8,														
+typeLoc: 9,														
+statusLoc: 10,														
+														
+dvowel: 0,														
+ivowel: 0,														
+onset:0,														
+final:0,														
+subj:0,														
+ltone:0,														
+htone:0,														
+lc:0,														
+uc:0,														
+meaning:0,														
+shape: 0,														
+numLoc: 0,														
+														
+nnameLoc: 11,														
+nameLoc: 12,														
+ucsName: 13,														
+block: 14,														
+														
+//othertranscriptions: [[5, 'Latin']]														
+}														
