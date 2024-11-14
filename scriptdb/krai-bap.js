64b2b9ed23358faec135253cd52bb2a6c6f0587d
r12a <ishida@w3.org>
2024-11-14 08:48:24 +0000

    + krai

---
+++ scriptdb/krai-bap.js
@@ -0,0 +1,104 @@
+var spreadsheet = `krai-bap	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	block
+														
+														
+\u{16D57}	p	p	a	p			Lo		consonant				U+16D57: KIRAT RAI LETTER PA	
+\u{16D58}	P	pʰ	a	pʰ			Lo		consonant				U+16D58: KIRAT RAI LETTER PHA	
+\u{16D59}	b	b	a	b			Lo		consonant				U+16D59: KIRAT RAI LETTER BA	
+\u{16D5A}	B	bʰ	a	bʰ			Lo		consonant				U+16D5A: KIRAT RAI LETTER BHA	
+\u{16D52}	t	t	a	t			Lo		consonant				U+16D52: KIRAT RAI LETTER TA	
+\u{16D53}	T	tʰ	a	tʰ			Lo		consonant				U+16D53: KIRAT RAI LETTER THA	
+\u{16D54}	d	d	a	d			Lo		consonant				U+16D54: KIRAT RAI LETTER DA	
+\u{16D55}	D	dʰ	a	dʰ			Lo		consonant				U+16D55: KIRAT RAI LETTER DHA	
+\u{16D4E}	t	ʈ	a	ʈ			Lo		consonant				U+16D4E: KIRAT RAI LETTER TTA	
+\u{16D4F}	T	ʈʰ	a	ʈʰ			Lo		consonant				U+16D4F: KIRAT RAI LETTER TTHA	
+\u{16D50}	d	ɖ	a	ɖ			Lo		consonant				U+16D50: KIRAT RAI LETTER DDA	
+\u{16D51}	D	ɖʰ	a	ɖʰ			Lo	i	consonant				U+16D51: KIRAT RAI LETTER DDHA	
+\u{16D44}	k	k	a	k			Lo		consonant				U+16D44: KIRAT RAI LETTER KA	
+\u{16D45}	K	kʰ	a	kʰ			Lo		consonant				U+16D45: KIRAT RAI LETTER KHA	
+\u{16D46}	g	ɡ	a	ɡ			Lo		consonant				U+16D46: KIRAT RAI LETTER GA	
+\u{16D47}	G	ɡʰ	a	ɡʰ			Lo		consonant				U+16D47: KIRAT RAI LETTER GHA	
+\u{16D42}	}	ʔ		ʔ			Lm		glottal stop				U+16D42: KIRAT RAI SIGN VISARGA	
+\u{16D49}	c	t͡s	a	ʦ			Lo		consonant				U+16D49: KIRAT RAI LETTER CA	
+\u{16D4A}	C	t͡sʰ	a	ʦʰ			Lo		consonant				U+16D4A: KIRAT RAI LETTER CHA	
+\u{16D4B}	j	d͡z	a	ʣ			Lo		consonant				U+16D4B: KIRAT RAI LETTER JA	
+\u{16D4C}	J	d͡zʰ	a	ʣʰ			Lo		consonant				U+16D4C: KIRAT RAI LETTER JHA	
+\u{16D60}	s	s	a	s			Lo		consonant				U+16D60: KIRAT RAI LETTER SA	
+\u{16D61}	S	ʃ	a	ʃ			Lo	i	consonant				U+16D61: KIRAT RAI LETTER SHA	
+\u{16D62}	h	h	a	h			Lo		consonant				U+16D62: KIRAT RAI LETTER HA	
+\u{16D5B}	m	m	a	m			Lo		consonant				U+16D5B: KIRAT RAI LETTER MA	
+\u{16D56}	n	n	a	n			Lo		consonant				U+16D56: KIRAT RAI LETTER NA	
+\u{16D4D}	n	ɲ	a	ɲ			Lo	i	consonant				U+16D4D: KIRAT RAI LETTER NYA	
+\u{16D48}	N	ŋ	a	ŋ			Lo		consonant				U+16D48: KIRAT RAI LETTER NGA	
+\u{16D40}	N	ɴ		ᴎ			Lm		nasal coda				U+16D40: KIRAT RAI SIGN ANUSVARA	
+\u{16D5F}	w	w	a	w			Lo		consonant				U+16D5F: KIRAT RAI LETTER VA	
+\u{16D5D}	r	r	a	r			Lo		consonant				U+16D5D: KIRAT RAI LETTER RA	
+\u{16D5E}	l	l	a	l			Lo		consonant				U+16D5E: KIRAT RAI LETTER LA	
+\u{16D5C}	y	j	a	j			Lo		consonant				U+16D5C: KIRAT RAI LETTER YA	
+														
+\u{16D64}	i	i		i			Lo		vowel				U+16D64: KIRAT RAI VOWEL SIGN I	
+\u{16D66}	I	ɨ		ɨ			Lo		vowel				U+16D66: KIRAT RAI VOWEL SIGN UE	
+\u{16D65}	u	u		u			Lo		vowel				U+16D65: KIRAT RAI VOWEL SIGN U	
+\u{16D67}	e	e		e			Lo		vowel				U+16D67: KIRAT RAI VOWEL SIGN E	
+\u{16D69}	o	o		o			Lo		vowel				U+16D69: KIRAT RAI VOWEL SIGN O	
+\u{16D43}	A	a ʔ		a			Lo		vowel carrier				U+16D43: KIRAT RAI LETTER A	
+\u{16D63}	a	aː		aː			Lo		vowel				U+16D63: KIRAT RAI VOWEL SIGN AA	
+\u{16D68}	E	aɪ		ɛ			Lo		vowel				U+16D68: KIRAT RAI VOWEL SIGN AI	
+\u{16D6A}	a	aʊ		au			Lo		vowel				U+16D6A: KIRAT RAI VOWEL SIGN AU	
+														
+\u{16D41}	[¶	̃		˜			Lm		nasalisation marker				U+16D41: KIRAT RAI SIGN TONPI	
+\u{16D6B}	*			͞			Lm		vowel killer	Used for 1st letter in word only.			U+16D6B: KIRAT RAI SIGN VIRAMA	
+\u{16D6C}	*			˟			Lm		vowel killer	Used for non-initial letters in a word.			U+16D6C: KIRAT RAI SIGN SAAT	
+\u{16D6D}	+			¨			Po		abbreviation marker				U+16D6D: KIRAT RAI SIGN YUPI	
+\u{16D6E}	|			|			Po		sentence delimiter				U+16D6E: KIRAT RAI DANDA	
+\u{16D6F}	|			¶			Po		sentence delimiter				U+16D6F: KIRAT RAI DOUBLE DANDA	
+														
+\u{16D70}	±	0		1̣			Nd		digit				U+16D70: KIRAT RAI DIGIT ZERO	
+\u{16D71}	±	1		2̣			Nd		digit				U+16D71: KIRAT RAI DIGIT ONE	
+\u{16D72}	±	2		3̣			Nd		digit				U+16D72: KIRAT RAI DIGIT TWO	
+\u{16D73}	±	3		4̣			Nd		digit				U+16D73: KIRAT RAI DIGIT THREE	
+\u{16D74}	±	4		5̣			Nd		digit				U+16D74: KIRAT RAI DIGIT FOUR	
+\u{16D75}	±	5		6̣			Nd		digit				U+16D75: KIRAT RAI DIGIT FIVE	
+\u{16D76}	±	6		7̣			Nd		digit				U+16D76: KIRAT RAI DIGIT SIX	
+\u{16D77}	±	7		8̣			Nd		digit				U+16D77: KIRAT RAI DIGIT SEVEN	
+\u{16D78}	±	8		9̣			Nd		digit				U+16D78: KIRAT RAI DIGIT EIGHT	
+\u{16D79}	±	9		0̣			Nd		digit				U+16D79: KIRAT RAI DIGIT NINE	
+														
+														
+														
+														
+`														
+														
+latinPanel = 'ʰ ̃ ʈ ɖ ɡ ʔ t͡s d͡z ʃ ɲ ŋ ɴ ɨ ɛ aː ː'														
+														
+var cols = {														
+key: 1,														
+ipaLoc: 2,														
+ipaPlus: 3,														
+transLoc: 4,														
+transcription: 5,														
+kbd: 6,														
+class: 7,														
+status: 8,														
+typeLoc: 9,														
+statusLoc: 10,														
+														
+dvowel: 0,														
+ivowel: 0,														
+onset:0,														
+final:0,														
+subj:0,														
+ltone:0,														
+htone:0,														
+lc:0,														
+uc:0,														
+meaning:0,														
+shape: 0,														
+numLoc: 0,														
+														
+nnameLoc: 11,														
+nameLoc: 12,														
+ucsName: 13,														
+block: 14,														
+														
+//othertranscriptions: [[5, 'Latin']]														
+}														
