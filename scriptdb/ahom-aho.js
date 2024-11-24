c2f0fcd45a66c783b8779d02adb8507ef56a8068
r12a <ishida@w3.org>
2024-11-24 11:43:48 +0000

    + ahom    first bank

---
+++ scriptdb/ahom-aho.js
@@ -0,0 +1,223 @@
+var spreadsheet = `ahom-aho	key	ipa	ipa+		translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	block
+															
+𑜆	p	p	a		p	p		Lo						U+11706: AHOM LETTER PA	
+𑜇	P	pʰ	a		pʰ	ph		Lo						U+11707: AHOM LETTER PHA	
+𑜈	b	b	a		b	b		Lo						U+11708: AHOM LETTER BA	
+𑜚	b	b	a		b	b		Lo						U+1171A: AHOM LETTER ALTERNATE BA	
+𑜘	B	bʱ	a		bʱ	bh		Lo						U+11718: AHOM LETTER BHA	
+𑜄	t	t	a		t	t		Lo						U+11704: AHOM LETTER TA	
+𑜅	t	t	a		t	t		Lo						U+11705: AHOM LETTER ALTERNATE TA	
+𑜌	T	tʰ	a		tʰ	th		Lo						U+1170C: AHOM LETTER THA	
+𑜓	d	d	a		d	d		Lo						U+11713: AHOM LETTER DA	
+𑜔	D	dʱ	a		dʱ	ḍh		Lo						U+11714: AHOM LETTER DHA	
+𑝁	t	ʈ	a		ʈ	ṭ		Lo		consonant				U+11741: AHOM LETTER TTA	
+𑝂	T	ʈʰ	a		ʈʰ	ṭh		Lo		consonant				U+11742: AHOM LETTER TTHA	
+𑝃	d	ɖ	a		ɖ	ḍ		Lo		consonant				U+11743: AHOM LETTER DDA	
+𑝄	D	ɖʱ	a		ɖʱ	ḍh		Lo		consonant				U+11744: AHOM LETTER DDHA	
+𑝀	c	c	a		c	c		Lo		consonant				U+11740: AHOM LETTER CA	
+𑜋	C	cʰ	a		cʰ	ch		Lo						U+1170B: AHOM LETTER CHA	
+𑜊	j	ɟ	a		ɟ	j		Lo						U+1170A: AHOM LETTER JA	
+𑜙	J	ɟʱ	a		ɟʱ	jh		Lo						U+11719: AHOM LETTER JHA	
+𑜀	k	k	a		k	k		Lo						U+11700: AHOM LETTER KA	
+𑜁	K	kʰ	a		kʰ	kh		Lo						U+11701: AHOM LETTER KHA	
+𑜕	g	ɡ	a		ɡ	g		Lo						U+11715: AHOM LETTER GA	
+𑜖	g	ɡ	a		ɡ	g		Lo						U+11716: AHOM LETTER ALTERNATE GA	
+𑜗	G	ɡʱ	a		ɡʱ	gh		Lo						U+11717: AHOM LETTER GHA	
+𑜏	s	s	a		s	s		Lo						U+1170F: AHOM LETTER SA	
+𑜑	h	h	a		h	h		Lo						U+11711: AHOM LETTER HA	
+𑜉	m	m	a		m	m		Lo						U+11709: AHOM LETTER MA	
+𑜃	n	n	a		n	n		Lo						U+11703: AHOM LETTER NA	
+𑝅	n	ɳ	a		ɳ	ṇ		Lo		consonant				U+11745: AHOM LETTER NNA	
+𑜐	n	ɲ	a		ɲ	ny		Lo						U+11710: AHOM LETTER NYA	
+𑜂	N	ŋ	a		ŋ	ng		Lo						U+11702: AHOM LETTER NGA	
+𑜍	r	r	a		r	r		Lo						U+1170D: AHOM LETTER RA	
+𑜞	R	r	a		r	r		Mc​						U+1171E: AHOM CONSONANT SIGN MEDIAL RA	
+𑜟	R	r	a		r	r		Mn​						U+1171F: AHOM CONSONANT SIGN MEDIAL LIGATING RA	
+𑜎	l	l	a		l	l		Lo						U+1170E: AHOM LETTER LA	
+𑜝	L	l	a		l	l		Mn​						U+1171D: AHOM CONSONANT SIGN MEDIAL LA	
+𑝆	l	ɭ	a		ɭ	ḷ		Lo		consonant				U+11746: AHOM LETTER LLA	
+															
+𑜢	i	i			i	i		Mn​						U+11722: AHOM VOWEL SIGN I	
+𑜣	I	iː			iː	ii		Mn​						U+11723: AHOM VOWEL SIGN II	
+𑜤	u	u			u	u		Mn​						U+11724: AHOM VOWEL SIGN U	
+𑜥	U	uː			uː	uu		Mn​						U+11725: AHOM VOWEL SIGN UU	
+𑜦	e	e			e	e		Mc​						U+11726: AHOM VOWEL SIGN E	
+𑜨	o	o			o	o		Mn​						U+11728: AHOM VOWEL SIGN O	
+𑜠	a	a			a	a		Mc​						U+11720: AHOM VOWEL SIGN A	
+𑜡	A	aː			aː	aa		Mc​						U+11721: AHOM VOWEL SIGN AA	
+															
+𑜩	a	ai			ai	ai		Mn​						U+11729: AHOM VOWEL SIGN AI	
+𑜧	a	aw			aw	aw		Mn​						U+11727: AHOM VOWEL SIGN AW	
+𑜪	a	am			am	am		Mn​						U+1172A: AHOM VOWEL SIGN AM	
+															
+𑜒	}	a			a	a		Lo		vowel carrier				U+11712: AHOM LETTER A	
+𑜫	*				͞			Mn​		vowel killer; standalone vowel carrier				U+1172B: AHOM SIGN KILLER	
+															
+𑜱	±	1			1̣	1		Nd		digit				U+11731: AHOM DIGIT ONE	
+𑜲	±	2			2̣	2		Nd		digit				U+11732: AHOM DIGIT TWO	
+𑜳	±	3			3̣	3		Nd		digit				U+11733: AHOM DIGIT THREE	
+𑜴	±	4			4̣	4		Nd		digit				U+11734: AHOM DIGIT FOUR	
+𑜵	±	5			5̣	5		Nd		digit				U+11735: AHOM DIGIT FIVE	
+𑜶	±	6			6̣	6		Nd		digit				U+11736: AHOM DIGIT SIX	
+𑜷	±	7			7̣	7		Nd		digit				U+11737: AHOM DIGIT SEVEN	
+𑜸	±	8			8̣	8		Nd		digit				U+11738: AHOM DIGIT EIGHT	
+𑜹	±	9			9̣	9		Nd		digit				U+11739: AHOM DIGIT NINE	
+𑜰	±	0			0̣	0		Nd		digit				U+11730: AHOM DIGIT ZERO	
+𑜺	$	10			{10}	10		No		number				U+1173A: AHOM NUMBER TEN	
+𑜻	$	20			{20}	20		No		number				U+1173B: AHOM NUMBER TWENTY	
+															
+𑜼	|				|	.		Po		small section separator				U+1173C: AHOM SIGN SMALL SECTION	
+𑜽	|				¶			Po		section separator				U+1173D: AHOM SIGN SECTION	
+𑜾	|				|˙	.		Po		paragraph separator				U+1173E: AHOM SIGN RULAI	
+𑜿	!				!			So		exclamation mark				U+1173F: AHOM SYMBOL VI	
+															
+\u0020	 				 			Zs		space				U+0020: SPACE	
+															
+															
+// special letters															
+	q				θ				?						
+	{				ʕ				?						
+	}				ʔ				?						
+// space															
+\u200B	=				ᶻʷˢᵖ			Cf	?	zero-width space				U+200B: ZERO WIDTH SPACE	
+\u202F	]				ⁿⁿᵇˢᵖ			Zs	?	nnbsp				U+202F NARROW NO-BREAK SPACE	
+// word separator															
+	]				• §-				?	word separator					
+// nasalisation															
+‍ଁ	[¶	̃			˜	̃			?	nasalisation					
+// phrase dividers															
+,	,				,	,		Po	?	comma				U+002C: COMMA	
+⹁	,				,̣ ,̱ ,͈ ,͇	⹁		Po	?	comma				U+2E41: REVERSED COMMA	
+;	;				; ;̣ ;̱ ;͈ ;͇	;	;;⁏⸵	Po	?	semicolon				U+003B: SEMICOLON	
+!	!				! !̣ !̱ !͈ !͇	!		Po	?	exclamation mark				U+0021: EXCLAMATION MARK	
+	!				¡	¡			?	exclamation mark					
+?	?				? ?̣ ?̱ ?͈ ?͇	?		Po	?	question mark				U+003F: QUESTION MARK	
+	?				¿	¿			?	question mark					
+// colons & length marks															
+:	:				: :̣ :̱ :͈ :͇	:		Po	?	colon				U+003A: COLON	
+	:				ː ː̣ ː̱ ː͈ ː͇				?	length mark					
+// period & ellipsis															
+.	.				. .̣ .̱ .͈ .͇	.		Po	?	full stop				U+002E: FULL STOP	
+…	.				…	…		Po	?	ellipsis				U+2026: HORIZONTAL ELLIPSIS	
+⋯	.				⋯̇ ⋯̄ ⋯̎ ⋯̿	⋯		Sm	?	midline ellipsis				U+22EF: MIDLINE HORIZONTAL ELLIPSIS	
+// quote marks															
+«	<				«	“		Pi	?	quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+»	>				»	”		Pf	?	quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	<				‹			Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+›	>				›			Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+“	<				“	“		Pi	?	quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
+”	>				”	”		Pf	?	quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
+‘	<				‘	‘		Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+’	>				’	’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+// brackets															
+(	(				( (˙ (¯ (¨ (˭	(		Ps	?	parenthesis				U+0028: LEFT PARENTHESIS	
+)	)				) )˙ )¯ )¨ )˭	)		Ps	?	parenthesis				U+0029: RIGHT PARENTHESIS	
+[	(				[	[		Pe	?	bracket				U+005B: LEFT SQUARE BRACKET	
+]	)				] 	]		Pe	?	bracket				U+005D: RIGHT SQUARE BRACKET	
+// dashes															
+-	-				- -̣ -̱ -͈ -͇	-		Pd	?	hyphen				U+2010: HYPHEN	
+‑	-				‑	‑		Pd	?	non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
+–	-				–	–		Pd	?	en dash				U+2013: EN DASH	
+—	-				—	—		Pd	?	em dash				U+2014: EM DASH	
+// apostrophe															
+'	\'				ʼ	ʼ		Lm	?	apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE	
+// secondary viramas															
+	\				ˣ ˣ̣ ˣ̱ ˣ͈ ˣ͇				?						
+// nuktas															
+‍଼	/				ˑ ˑ̣ ˑ̱ ˑ͈ ˑ͇				?	nukta					
+// repetition, gemination															
+	&				& &̣ &̱ &͈ &͇ &̥				?	repetition					
+	&				˖				?	gemination					
+// abbrev.															
+	+				¨				?	abbreviation marker					
+// misc diacritics															
+	[				+				?	vowel carrier					
+	[				˘				?	vowel shortener					
+	[				ᵊ				?	vowel retainer					
+	[								?	consonant shifter					
+	[				ˈ				?	superscript alef					
+	[				ʿ				?	hamza above					
+	[				˓				?	hamza below					
+٭	[				*				?	star					
+	[				~				?	maddah					
+	[				″				?	register change					
+	[				°				?	special					
+	[				˙				?	combining dot above					
+	[				ʼ				?	combining hook above					
+	[				ˆ				?	combining circumflex					
+	[				ˉ				?	combining macron					
+	[				̨				?	combining ogonek					
+	[				ᶠ				?	feminine marker					
+// head marks, honorifics, etc															
+	@				⟦				?	opening head mark					
+	@				⟧				?	closing head mark					
+	@				⟨				?	2nd opening head					
+	@				⟩				?	2 closing head					
+	@				§ᵃ				?	honorific					
+	@				§ᵇ				?	honorific					
+	@				§ᶜ				?	honorific					
+	@				°				?	extra					
+// formatting characters															
+\u200C	=				ᶻʷⁿʲ			Cf	?	zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=				ᶻʷʲ			Cf	?	zero-width joiner				U+200D: ZERO WIDTH JOINER	
+\u034F	=				ᶜᵍʲ			Mn	?	combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER	
+\u2060	=				ʷʲ			Cf	?	word joiner				U+2060 WORD JOINER	
+// number related															
+	#				ᵗ				?	thousands separator					
+	#				ᵈ				?	decimal separator					
+	#				/				?	date separator					
+	#				¤				?	currency symbol					
+	#				¢				?						
+	#				# #̣ #̱ #͈ #͇				?	number symbol					
+	#				¼				?	quarter					
+	#				½				?	half					
+	#				¾				?	three-quarters					
+%	%				%			Po	?	percentage mark				U+0025: PERCENT SIGN	
+‰	%				‰			Po	?	per mille mark				U+2030: PER MILLE SIGN	
+// ascii digits															
+0	0				0	0		Nd	?	digit				U+0030: DIGIT ZERO	
+1	1				1	1		Nd	?	digit				U+0031: DIGIT ONE	
+2	2				2	2		Nd	?	digit				U+0032: DIGIT TWO	
+3	3				3	3		Nd	?	digit				U+0033: DIGIT THREE	
+4	4				4	4		Nd	?	digit				U+0034: DIGIT FOUR	
+5	5				5	5		Nd	?	digit				U+0035: DIGIT FIVE	
+6	6				6	6		Nd	?	digit				U+0036: DIGIT SIX	
+7	7				7	7		Nd	?	digit				U+0037: DIGIT SEVEN	
+8	8				8	8		Nd	?	digit				U+0038: DIGIT EIGHT	
+9	9				9	9		Nd	?	digit				U+0039: DIGIT NINE	
+`															
+															
+latinPanel = 'ä bʷ č'															
+															
+var cols = {															
+key: 1,															
+ipaLoc: 2,															
+ipaPlus: 3,															
+transLoc: 5,															
+transcription: 6,															
+kbd: 7,															
+class: 8,															
+status: 9,															
+typeLoc: 10,															
+statusLoc: 11,															
+															
+dvowel: 0,															
+ivowel: 0,															
+onset:0,															
+final:0,															
+subj:0,															
+ltone:0,															
+htone:0,															
+lc:0,															
+uc:0,															
+meaning:0,															
+shape: 0,															
+numLoc: 0,															
+															
+nnameLoc: 12,															
+nameLoc: 13,															
+ucsName: 14,															
+block: 15,															
+															
+//othertranscriptions: [[5, 'Latin']]															
+}															
