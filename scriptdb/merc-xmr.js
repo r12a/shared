b8b327b46e4ab845998e2674b052ee0014d307a5
r12a <ishida@w3.org>
2024-12-01 10:50:29 +0000

    + scriptdb/merc-xmr.js

---
+++ scriptdb/merc-xmr.js
@@ -0,0 +1,157 @@
+var spreadsheet = `merc-xmr	key	ipa	ipa+		translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	block	0		
+𐦧	p	p	a		p	p		Lo		consonant				U+109A7: MEROITIC CURSIVE LETTER PA				
+𐦦	b	b	a		b	b		Lo		consonant				U+109A6: MEROITIC CURSIVE LETTER BA				
+𐦴	t	t	a		t	e		Lo		consonant				U+109B4: MEROITIC CURSIVE LETTER TA				
+𐦵	t	t(ə)			tᵉ	te		Lo		syllable/final consonant				U+109B5: MEROITIC CURSIVE LETTER TE				
+𐦶	t	tu			tᵒ	to		Lo		syllable/final consonant				U+109B6: MEROITIC CURSIVE LETTER TO				
+𐦷	d	ɾ d	a		d	d		Lo		consonant	d when initial. ɾ between vowels.			U+109B7: MEROITIC CURSIVE LETTER DA				
+𐦲	k	k	a		k	k		Lo		consonant				U+109B2: MEROITIC CURSIVE LETTER KA				
+𐦳	q	q	a		q	q		Lo		consonant				U+109B3: MEROITIC CURSIVE LETTER QA				
+𐦯	s	s	a		s	s		Lo		consonant				U+109AF: MEROITIC CURSIVE LETTER SA				
+𐦰	s	s	a		ṣ	s		Lo	a	consonant	Archaic form.			U+109B0: MEROITIC CURSIVE LETTER ARCHAIC SA				
+𐦱	s	s(ə)			sᵉ	se		Lo		syllable/final consonant				U+109B1: MEROITIC CURSIVE LETTER SE				
+𐦭	x	x	a		x	ḫ		Lo		consonant				U+109AD: MEROITIC CURSIVE LETTER KHA				
+𐦮	h	ɣ	a		ɣ	ẖ		Lo		consonant				U+109AE: MEROITIC CURSIVE LETTER HHA				
+𐦨	m	m	a		m	m		Lo		consonant				U+109A8: MEROITIC CURSIVE LETTER MA				
+𐦩	n	n	a		n	n		Lo		consonant				U+109A9: MEROITIC CURSIVE LETTER NA				
+𐦪	n	n(ə)			nᵉ	ne		Lo		syllable/final consonant				U+109AA: MEROITIC CURSIVE LETTER NE				
+𐦥	w	w	a		w	w		Lo		consonant				U+109A5: MEROITIC CURSIVE LETTER WA				
+𐦫	r	r	a		r	r		Lo		consonant				U+109AB: MEROITIC CURSIVE LETTER RA				
+𐦬	l	l	a		l	l		Lo		consonant				U+109AC: MEROITIC CURSIVE LETTER LA				
+𐦤	y	j	a		j	y		Lo		consonant				U+109A4: MEROITIC CURSIVE LETTER YA				
+																		
+𐦢	i	i			i	i		Lo		vowel				U+109A2: MEROITIC CURSIVE LETTER I				
+𐦡	e	ə			e	e		Lo		vowel				U+109A1: MEROITIC CURSIVE LETTER E				
+𐦣	o	u			o	o		Lo		vowel				U+109A3: MEROITIC CURSIVE LETTER O				
+𐦠	a	a			a	a		Lo		vowel				U+109A0: MEROITIC CURSIVE LETTER A				
+																		
+																		
+𐦾	g	rmt				rmt		Lo		logogram				U+109BE: MEROITIC CURSIVE LOGOGRAM RMT				
+𐦿	g	imn				imn		Lo		logogram				U+109BF: MEROITIC CURSIVE LOGOGRAM IMN				
+																		
+// word separator																		
+:					 			Po		cursive word separator				U+003A: COLON				
+⁝	]				•			Po		hieroglyph word separator				U+205D: TRICOLON				
+																		
+// space																		
+\u0020	 							Zs		space				U+0020: SPACE				
+\u200B	=				ᶻʷˢᵖ			Cf		zero-width space				U+200B: ZERO WIDTH SPACE				
+\u202F	]				ⁿⁿᵇˢᵖ			Zs		nnbsp				U+202F NARROW NO-BREAK SPACE				
+// formatting characters																		
+\u200C	=				ᶻʷⁿʲ			Cf		zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER			✓	
+\u200D	=				ᶻʷʲ			Cf		zero-width joiner				U+200D: ZERO WIDTH JOINER			✓	
+\u034F	=				ᶜᵍʲ			Mn		combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER			✓	
+\u2060	=				ʷʲ			Cf		word joiner				U+2060 WORD JOINER			✓	
+\u2067	=				ʳˡⁱ			Cf		rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE			✓	
+\u2066	=				ˡʳⁱ			Cf		ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE			✓	
+\u2068	=				ᶠˢⁱ			Cf		first-strong isolate				U+2068: FIRST STRONG ISOLATE			✓	
+\u2069	=				ᵖᵈⁱ			Cf		pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE			✓	
+\u202B	=				ʳˡᵉ			Cf	x	rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING			✓	
+\u202A	=				ˡʳᵉ			Cf	x	ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING			✓	
+\u202C	=				ᵖᵈᶠ			Cf	x	pop direction				U+202C: POP DIRECTIONAL FORMATTING			✓	
+\u200F	=				ʳˡᵐ			Cf		rtl mark				U+200F: RIGHT-TO-LEFT MARK			✓	
+\u200E	=				ˡʳᵐ			Cf		ltr mark				U+200E: LEFT-TO-RIGHT MARK			✓	
+\u061C	=				ᵃˡᵐ			Cf		arabic letter mark				U+061C: ARABIC LETTER MARK			✓	
+// digits																		
+𐧀	±				1̣	1		No		digit				U+109C0: MEROITIC CURSIVE NUMBER ONE			١	
+𐧁	±				2̣	2		No		digit				U+109C1: MEROITIC CURSIVE NUMBER TWO			٢	
+𐧂	±				3̣	3		No		digit				U+109C2: MEROITIC CURSIVE NUMBER THREE			٣	
+𐧃	±				4̣	4		No		digit				U+109C3: MEROITIC CURSIVE NUMBER FOUR			٤	
+𐧄	±				5̣	5		No		digit				U+109C4: MEROITIC CURSIVE NUMBER FIVE			٥	
+𐧅	±				6̣	6		No		digit				U+109C5: MEROITIC CURSIVE NUMBER SIX			٦	
+𐧆	±				7̣	7		No		digit				U+109C6: MEROITIC CURSIVE NUMBER SEVEN			٧	
+𐧇	±				8̣	8		No		digit				U+109C7: MEROITIC CURSIVE NUMBER EIGHT			٨	
+𐧈	±				9̣	9		No		digit				U+109C8: MEROITIC CURSIVE NUMBER NINE			٩	
+// logograms, extended numbers, etc																		
+𐧉	$				{10}			No		extended number				U+109C9: MEROITIC CURSIVE NUMBER TEN				
+𐧊					{20}			No		extended number				U+109CA: MEROITIC CURSIVE NUMBER TWENTY				
+𐧋					{30}			No		extended number				U+109CB: MEROITIC CURSIVE NUMBER THIRTY				
+𐧌					{40}			No		extended number				U+109CC: MEROITIC CURSIVE NUMBER FORTY				
+𐧍					{50}			No		extended number				U+109CD: MEROITIC CURSIVE NUMBER FIFTY				
+𐧎					{60}			No		extended number				U+109CE: MEROITIC CURSIVE NUMBER SIXTY				
+𐧏					{70}			No		extended number				U+109CF: MEROITIC CURSIVE NUMBER SEVENTY				
+𐧒					{100}			No		extended number				U+109D2: MEROITIC CURSIVE NUMBER ONE HUNDRED				
+𐧓					{200}			No		extended number				U+109D3: MEROITIC CURSIVE NUMBER TWO HUNDRED				
+𐧔					{300}			No		extended number				U+109D4: MEROITIC CURSIVE NUMBER THREE HUNDRED				
+𐧕					{400}			No		extended number				U+109D5: MEROITIC CURSIVE NUMBER FOUR HUNDRED				
+𐧖					{500}			No		extended number				U+109D6: MEROITIC CURSIVE NUMBER FIVE HUNDRED				
+𐧗					{600}			No		extended number				U+109D7: MEROITIC CURSIVE NUMBER SIX HUNDRED				
+𐧘					{700}			No		extended number				U+109D8: MEROITIC CURSIVE NUMBER SEVEN HUNDRED				
+𐧙					{800}			No		extended number				U+109D9: MEROITIC CURSIVE NUMBER EIGHT HUNDRED				
+𐧚					{900}			No		extended number				U+109DA: MEROITIC CURSIVE NUMBER NINE HUNDRED				
+𐧛								No		extended number				U+109DB: MEROITIC CURSIVE NUMBER ONE THOUSAND				
+𐧜								No		extended number				U+109DC: MEROITIC CURSIVE NUMBER TWO THOUSAND				
+𐧝								No		extended number				U+109DD: MEROITIC CURSIVE NUMBER THREE THOUSAND				
+𐧞								No		extended number				U+109DE: MEROITIC CURSIVE NUMBER FOUR THOUSAND				
+𐧟								No		extended number				U+109DF: MEROITIC CURSIVE NUMBER FIVE THOUSAND				
+𐧠								No		extended number				U+109E0: MEROITIC CURSIVE NUMBER SIX THOUSAND				
+𐧡								No		extended number				U+109E1: MEROITIC CURSIVE NUMBER SEVEN THOUSAND				
+𐧢								No		extended number				U+109E2: MEROITIC CURSIVE NUMBER EIGHT THOUSAND				
+𐧣								No		extended number				U+109E3: MEROITIC CURSIVE NUMBER NINE THOUSAND				
+𐧤								No		extended number				U+109E4: MEROITIC CURSIVE NUMBER TEN THOUSAND				
+𐧥								No		extended number				U+109E5: MEROITIC CURSIVE NUMBER TWENTY THOUSAND				
+𐧦								No		extended number				U+109E6: MEROITIC CURSIVE NUMBER THIRTY THOUSAND				
+𐧧								No		extended number				U+109E7: MEROITIC CURSIVE NUMBER FORTY THOUSAND				
+𐧨								No		extended number				U+109E8: MEROITIC CURSIVE NUMBER FIFTY THOUSAND				
+𐧩								No		extended number				U+109E9: MEROITIC CURSIVE NUMBER SIXTY THOUSAND				
+𐧪								No		extended number				U+109EA: MEROITIC CURSIVE NUMBER SEVENTY THOUSAND				
+𐧫								No		extended number				U+109EB: MEROITIC CURSIVE NUMBER EIGHTY THOUSAND				
+𐧬								No		extended number				U+109EC: MEROITIC CURSIVE NUMBER NINETY THOUSAND				
+𐧭								No		extended number				U+109ED: MEROITIC CURSIVE NUMBER ONE HUNDRED THOUSAND				
+𐧮								No		extended number				U+109EE: MEROITIC CURSIVE NUMBER TWO HUNDRED THOUSAND				
+𐧯								No		extended number				U+109EF: MEROITIC CURSIVE NUMBER THREE HUNDRED THOUSAND				
+𐧰								No		extended number				U+109F0: MEROITIC CURSIVE NUMBER FOUR HUNDRED THOUSAND				
+𐧱								No		extended number				U+109F1: MEROITIC CURSIVE NUMBER FIVE HUNDRED THOUSAND				
+𐧲								No		extended number				U+109F2: MEROITIC CURSIVE NUMBER SIX HUNDRED THOUSAND				
+𐧳								No		extended number				U+109F3: MEROITIC CURSIVE NUMBER SEVEN HUNDRED THOUSAND				
+𐧴								No		extended number				U+109F4: MEROITIC CURSIVE NUMBER EIGHT HUNDRED THOUSAND				
+𐧵								No		extended number				U+109F5: MEROITIC CURSIVE NUMBER NINE HUNDRED THOUSAND				
+																		
+𐧶								No		fraction				U+109F6: MEROITIC CURSIVE FRACTION ONE TWELFTH				
+𐧷								No		fraction				U+109F7: MEROITIC CURSIVE FRACTION TWO TWELFTHS				
+𐧸								No		fraction				U+109F8: MEROITIC CURSIVE FRACTION THREE TWELFTHS				
+𐧹								No		fraction				U+109F9: MEROITIC CURSIVE FRACTION FOUR TWELFTHS				
+𐧺								No		fraction				U+109FA: MEROITIC CURSIVE FRACTION FIVE TWELFTHS				
+𐧻								No		fraction				U+109FB: MEROITIC CURSIVE FRACTION SIX TWELFTHS				
+𐧼								No		fraction				U+109FC: MEROITIC CURSIVE FRACTION SEVEN TWELFTHS				
+𐧽								No		fraction				U+109FD: MEROITIC CURSIVE FRACTION EIGHT TWELFTHS				
+𐧾								No		fraction				U+109FE: MEROITIC CURSIVE FRACTION NINE TWELFTHS				
+𐧿								No		fraction				U+109FF: MEROITIC CURSIVE FRACTION TEN TWELFTHS				
+𐦼								No		fraction				U+109BC: MEROITIC CURSIVE FRACTION ELEVEN TWELFTHS				
+𐦽					½			No		fraction				U+109BD: MEROITIC CURSIVE FRACTION ONE HALF				
+`																		
+																		
+latinPanel = 'ə ɾ ḫ ẖ'																		
+																		
+var cols = {																		
+key: 1,																		
+ipaLoc: 2,																		
+ipaPlus: 3,																		
+transLoc: 5,																		
+transcription: 6,																		
+kbd: 7,																		
+class: 8,																		
+status: 9,																		
+typeLoc: 10,																		
+statusLoc: 11,																		
+																		
+dvowel: 0,																		
+ivowel: 0,																		
+onset:0,																		
+final:0,																		
+subj:0,																		
+ltone:0,																		
+htone:0,																		
+lc:0,																		
+uc:0,																		
+meaning:0,																		
+shape: 0,																		
+numLoc: 0,																		
+																		
+nnameLoc: 12,																		
+nameLoc: 13,																		
+ucsName: 14,																		
+block: 15,																		
+																		
+//othertranscriptions: [[5, 'Latin']]																		
+}																		
