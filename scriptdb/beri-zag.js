5734a119f3fbf2efd1c81c3f4c551751b2865d9c
r12a <ishida@w3.org>
2024-12-03 08:35:01 +0000

    + beria

---
+++ scriptdb/beri-zag.js
@@ -0,0 +1,186 @@
+var spreadsheet = `beri-zag	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	block
+														
+\u{16ECB}	p	p		p			Ll		lowercase				U+16ECB: BERIA ERFE SMALL LETTER PI	
+\u{16EB0}	P	p		P			Lu		uppercase				16EB0: BERIA ERFE CAPITAL LETTER PI	
+\u{16EBC}	b	b		b			Ll		lowercase				16EBC: BERIA ERFE SMALL LETTER BASIGNA	
+\u{16EA1}	B	b		B			Lu		uppercase				U+16EA1: BERIA ERFE CAPITAL LETTER BASIGNA	
+\u{16ED0}	t	t		t			Ll		lowercase				16ED0: BERIA ERFE SMALL LETTER TATASOUE	
+\u{16EB5}	T	t		T			Lu		uppercase				16EB5: BERIA ERFE CAPITAL LETTER TATASOUE	
+\u{16EBD}	d	d		d			Ll		lowercase				16EBD: BERIA ERFE SMALL LETTER DARBAI	
+\u{16EA2}	D	d		D			Lu		uppercase				16EA2: BERIA ERFE CAPITAL LETTER DARBAI	
+\u{16EC3}	j	ɟ		j			Ll		lowercase				16EC3: BERIA ERFE SMALL LETTER DJAI	
+\u{16EA8}	J	ɟ		J			Lu		uppercase				16EA8: BERIA ERFE CAPITAL LETTER DJAI	
+\u{16EC4}	k	k		k			Ll		lowercase				16EC4: BERIA ERFE SMALL LETTER KOBO	
+\u{16EA9}	K	k		K			Lu		uppercase				16EA9: BERIA ERFE CAPITAL LETTER KOBO	
+\u{16EC0}	g	ɡ		g			Ll		lowercase				16EC0: BERIA ERFE SMALL LETTER GOWAY	
+\u{16EA5}	G	ɡ		G			Lu		uppercase				16EA5: BERIA ERFE CAPITAL LETTER GOWAY	
+\u{16EBF}	f	f		f			Ll		lowercase				16EBF: BERIA ERFE SMALL LETTER FITKO	
+\u{16EA4}	F	f		F			Lu		uppercase				16EA4: BERIA ERFE CAPITAL LETTER FITKO	
+\u{16ECE}	s	s		s			Ll		lowercase				16ECE: BERIA ERFE SMALL LETTER SERI	
+\u{16EB3}	S	s		S			Lu		uppercase				16EB3: BERIA ERFE CAPITAL LETTER SERI	
+\u{16ECF}	s	ʃ		ʃ			Ll		lowercase				16ECF: BERIA ERFE SMALL LETTER SHEP	
+\u{16EB4}	S	ʃ		Ʃ			Lu		uppercase				16EB4: BERIA ERFE CAPITAL LETTER SHEP	
+\u{16EC1}	h	h		h			Ll		lowercase				16EC1: BERIA ERFE SMALL LETTER HIRDEABO	
+\u{16EA6}	H	h		H			Lu		uppercase				16EA6: BERIA ERFE CAPITAL LETTER HIRDEABO	
+\u{16EC6}	m	m		m			Ll		lowercase				16EC6: BERIA ERFE SMALL LETTER MERI	
+\u{16EAB}	M	m		M			Lu		uppercase				16EAB: BERIA ERFE CAPITAL LETTER MERI	
+\u{16EC7}	n	n		n			Ll		lowercase				16EC7: BERIA ERFE SMALL LETTER NINI	
+\u{16EAC}	N	n		N			Lu		uppercase				16EAC: BERIA ERFE CAPITAL LETTER NINI	
+\u{16EC8}	n	ɲ		ɲ			Ll		lowercase				16EC8: BERIA ERFE SMALL LETTER GNA	
+\u{16EAD}	N	ɲ		Ɲ			Lu		uppercase				16EAD: BERIA ERFE CAPITAL LETTER GNA	
+\u{16EC9}	n	ŋ		ŋ			Ll		lowercase				16EC9: BERIA ERFE SMALL LETTER NGAY	
+\u{16EAE}	N	ŋ		Ŋ			Lu		uppercase				16EAE: BERIA ERFE CAPITAL LETTER NGAY	
+\u{16ED2}	w	w		w			Ll		lowercase				16ED2: BERIA ERFE SMALL LETTER WASSE	
+\u{16EB7}	W	w		W			Lu		uppercase				16EB7: BERIA ERFE CAPITAL LETTER WASSE	
+\u{16ECC}	r	ɾ		ɾ			Ll		lowercase				16ECC: BERIA ERFE SMALL LETTER ERIGO	
+\u{16EB1}	R	ɾ		R			Lu		uppercase				16EB1: BERIA ERFE CAPITAL LETTER ERIGO	
+\u{16ECD}	r	ɻ		ɻ			Ll		lowercase				16ECD: BERIA ERFE SMALL LETTER ERIGO TAMURA	
+\u{16EB2}	R	ɻ		ᴚ			Lu		uppercase				16EB2: BERIA ERFE CAPITAL LETTER ERIGO TAMURA	
+\u{16EC5}	l	l		l			Ll		lowercase				16EC5: BERIA ERFE SMALL LETTER LAKKO	
+\u{16EAA}	L	l		L			Lu		uppercase				16EAA: BERIA ERFE CAPITAL LETTER LAKKO	
+\u{16ED3}	y	j		j			Ll		lowercase				16ED3: BERIA ERFE SMALL LETTER AY	
+\u{16EB8}	Y	j		J			Lu		uppercase				16EB8: BERIA ERFE CAPITAL LETTER AY	
+														
+														
+\u{16EC2}	i	ɪ		ɪ			Ll		lowercase				16EC2: BERIA ERFE SMALL LETTER I	
+\u{16EA7}	I	ɪ		Ɪ			Lu		uppercase				16EA7: BERIA ERFE CAPITAL LETTER I	
+\u{16EC2}̄		i		i					lowercase ATR+				16EC2 0304: BERIA ERFE SMALL LETTER I, COMBINING MACRON	
+\u{16EA7}̄		I		I					uppercase ATR+				16EA7 0304: BERIA ERFE CAPITAL LETTER I, COMBINING MACRON	
+\u{16ED1}	u	ʊ		ʊ			Ll		lowercase				16ED1: BERIA ERFE SMALL LETTER UI	
+\u{16EB6}	U	ʊ		Ʊ			Lu		uppercase				16EB6: BERIA ERFE CAPITAL LETTER UI	
+\u{16ED1}̄		u		u					lowercase ATR+				16ED1 0304: BERIA ERFE SMALL LETTER UI, COMBINING MACRON	
+\u{16EB6}̄		u		u					uppercase ATR+				16EB6 0304: BERIA ERFE CAPITAL LETTER UI, COMBINING MACRON	
+\u{16EBE}	e	ɛ		ɛ			Ll		lowercase				16EBE: BERIA ERFE SMALL LETTER EH	
+\u{16EA3}	E	ɛ		Ɛ			Lu		uppercase				16EA3: BERIA ERFE CAPITAL LETTER EH	
+\u{16EBE}̄		e		e					lowercase ATR+				16EBE 0304: BERIA ERFE SMALL LETTER EH, COMBINING MACRON	
+\u{16EA3}̄		e		E					uppercase ATR+				16EA3 0304: BERIA ERFE CAPITAL LETTER EH, COMBINING MACRON	
+\u{16ECA}	o	ɔ		ɔ			Ll		lowercase				16ECA: BERIA ERFE SMALL LETTER OI	
+\u{16EAF}	O	ɔ		Ɔ			Lu		uppercase				16EAF: BERIA ERFE CAPITAL LETTER OI	
+\u{16ECA}̄		o		o					lowercase ATR+				16ECA 0304: BERIA ERFE SMALL LETTER OI, COMBINING MACRON	
+\u{16EAF}̄		o		O					uppercase ATR+				16EAF 0304: BERIA ERFE CAPITAL LETTER OI, COMBINING MACRON	
+\u{16EBB}	a	a		a			Ll		lowercase				16EBB: BERIA ERFE SMALL LETTER ARKAB	
+\u{16EA0}	A	a		A			Lu		uppercase				16EA0: BERIA ERFE CAPITAL LETTER ARKAB	
+\u{16EBB}̄		ә		ә					lowercase ATR+				16EBB 0304: BERIA ERFE SMALL LETTER ARKAB, COMBINING MACRON	
+\u{16EA0}̄		ә		Ә					uppercase ATR+				16EA0 0304: BERIA ERFE CAPITAL LETTER ARKAB, COMBINING MACRON	
+														
+̄	[			¯			Mn		ATR+ indicator				0304: COMBINING MACRON	
+́	^			¹			Mn		tone mark				U+0301: COMBINING ACUTE ACCENT	
+̀	^			²			Mn		tone mark				U+0300: COMBINING GRAVE ACCENT	
+̇	^			³			Mn		tone mark				U+0307: COMBINING DOT ABOVE	
+														
+														
+														
+// space														
+\u0020	 						Zs	?	space				U+0020: SPACE	
+\u200B	=			ᶻʷˢᵖ			Cf	?	zero-width space				U+200B: ZERO WIDTH SPACE	
+\u202F	]			ⁿⁿᵇˢᵖ			Zs	?	nnbsp				U+202F NARROW NO-BREAK SPACE	
+// phrase dividers														
+,	,			,	,		Po		comma				U+002C: COMMA	
+;	;			; ;̣ ;̱ ;͈ ;͇	;	;;⁏⸵	Po		semicolon				U+003B: SEMICOLON	
+!	!			! !̣ !̱ !͈ !͇	!		Po		exclamation mark				U+0021: EXCLAMATION MARK	
+?	?			? ?̣ ?̱ ?͈ ?͇	?		Po		question mark				U+003F: QUESTION MARK	
+// colons & length marks														
+:	:			: :̣ :̱ :͈ :͇	:		Po		colon				U+003A: COLON	
+// period & ellipsis														
+.	.			. .̣ .̱ .͈ .͇	.		Po		full stop				U+002E: FULL STOP	
+…	.			…	…		Po	?	ellipsis				U+2026: HORIZONTAL ELLIPSIS	
+⋯	.			⋯̇ ⋯̄ ⋯̎ ⋯̿	⋯		Sm	?	midline ellipsis				U+22EF: MIDLINE HORIZONTAL ELLIPSIS	
+// quote marks														
+«	<			«	“		Pi	?	quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+»	>			»	”		Pf	?	quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	<			‹			Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+›	>			›			Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+“	<			“	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
+”	>			”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
+‘	<			‘	‘		Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+’	>			’	’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+// brackets														
+(	(			( (˙ (¯ (¨ (˭	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS	
+)	)			) )˙ )¯ )¨ )˭	)		Ps		parenthesis				U+0029: RIGHT PARENTHESIS	
+[	(			[	[		Pe	?	bracket				U+005B: LEFT SQUARE BRACKET	
+]	)			] 	]		Pe	?	bracket				U+005D: RIGHT SQUARE BRACKET	
+// dashes														
+-	-			- -̣ -̱ -͈ -͇	-		Pd		hyphen				U+2010: HYPHEN	
+‑	-			‑	‑		Pd	?	non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd		en dash				U+2013: EN DASH	
+—	-			—	—		Pd	?	em dash				U+2014: EM DASH	
+// apostrophe														
+'	\'			ʼ	ʼ		Lm		apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE	
+•	•			•			Po	?	bullet				U+2022: BULLET	
+// formatting characters														
+\u200C	=			ᶻʷⁿʲ			Cf	?	zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=			ᶻʷʲ			Cf	?	zero-width joiner				U+200D: ZERO WIDTH JOINER	
+\u034F	=			ᶜᵍʲ			Mn	?	combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER	
+\u2060	=			ʷʲ			Cf	?	word joiner				U+2060 WORD JOINER	
+\u2067	=			ʳˡⁱ			Cf	?	rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE	
+\u2066	=			ˡʳⁱ			Cf	?	ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE	
+\u2068	=			ᶠˢⁱ			Cf	?	first-strong isolate				U+2068: FIRST STRONG ISOLATE	
+\u2069	=			ᵖᵈⁱ			Cf	?	pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE	
+\u202B	=			ʳˡᵉ			Cf	x	rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING	
+\u202A	=			ˡʳᵉ			Cf	x	ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING	
+\u202C	=			ᵖᵈᶠ			Cf	x	pop direction				U+202C: POP DIRECTIONAL FORMATTING	
+\u200F	=			ʳˡᵐ			Cf	?	rtl mark				U+200F: RIGHT-TO-LEFT MARK	
+\u200E	=			ˡʳᵐ			Cf	?	ltr mark				U+200E: LEFT-TO-RIGHT MARK	
+\u061C	=			ᵃˡᵐ			Cf	?	arabic letter mark				U+061C: ARABIC LETTER MARK	
+// digits														
+1	±			1̣	1		Nd	?	digit					
+2	±			2̣	2		Nd	?	digit					
+3	±			3̣	3		Nd	?	digit					
+4	±			4̣	4		Nd	?	digit					
+5	±			5̣	5		Nd	?	digit					
+6	±			6̣	6		Nd	?	digit					
+7	±			7̣	7		Nd	?	digit					
+8	±			8̣	8		Nd	?	digit					
+9	±			9̣	9		Nd	?	digit					
+0	±			0̣	0		Nd	?	digit					
+%	%			%			Po	?	percentage mark				U+0025: PERCENT SIGN	
+‰	%			‰			Po	?	per mille mark				U+2030: PER MILLE SIGN	
+// logograms, extended numbers, etc														
+	$			{10} etc				?	extended number					
+// ascii digits														
+0	0			0	0		Nd	?	digit				U+0030: DIGIT ZERO	
+1	1			1	1		Nd	?	digit				U+0031: DIGIT ONE	
+2	2			2	2		Nd	?	digit				U+0032: DIGIT TWO	
+3	3			3	3		Nd	?	digit				U+0033: DIGIT THREE	
+4	4			4	4		Nd	?	digit				U+0034: DIGIT FOUR	
+5	5			5	5		Nd	?	digit				U+0035: DIGIT FIVE	
+6	6			6	6		Nd	?	digit				U+0036: DIGIT SIX	
+7	7			7	7		Nd	?	digit				U+0037: DIGIT SEVEN	
+8	8			8	8		Nd	?	digit				U+0038: DIGIT EIGHT	
+9	9			9	9		Nd	?	digit				U+0039: DIGIT NINE	
+`														
+														
+latinPanel = 'ɪ ʊ ɛ ɔ ɟ ʃ ɾ ɻ ɡ ́ ̄'														
+														
+var cols = {														
+key: 1,														
+ipaLoc: 2,														
+ipaPlus: 3,														
+transLoc: 4,														
+transcription: 5,														
+kbd: 6,														
+class: 7,														
+status: 8,														
+typeLoc: 9,														
+statusLoc: 10,														
+														
+dvowel: 0,														
+ivowel: 0,														
+onset:0,														
+final:0,														
+subj:0,														
+ltone:0,														
+htone:0,														
+lc:0,														
+uc:0,														
+meaning:0,														
+shape: 0,														
+numLoc: 0,														
+														
+nnameLoc: 11,														
+nameLoc: 12,														
+ucsName: 13,														
+block: 14,														
+														
+//othertranscriptions: [[5, 'Latin']]														
+}														
