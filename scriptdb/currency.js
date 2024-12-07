00fb0ce7662106a0e8310f0b6c3a0cee8793f9e7
r12a <ishida@w3.org>
2024-12-07 16:31:53 +0000

    + scriptdb/currency.js

---
+++ scriptdb/currency.js
@@ -0,0 +1,72 @@
+var spreadsheet = `khmr-km	key	ipa	ipa+		translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	block	0		
+₠								Sc						U+20A0: EURO-CURRENCY SIGN				
+₡								Sc						U+20A1: COLON SIGN				
+₢								Sc						U+20A2: CRUZEIRO SIGN				
+₣								Sc						U+20A3: FRENCH FRANC SIGN				
+₤								Sc						U+20A4: LIRA SIGN				
+₥								Sc						U+20A5: MILL SIGN				
+₦								Sc						U+20A6: NAIRA SIGN				
+₧								Sc						U+20A7: PESETA SIGN				
+₨								Sc						U+20A8: RUPEE SIGN				
+₩								Sc						U+20A9: WON SIGN				
+₪								Sc						U+20AA: NEW SHEQEL SIGN				
+₫								Sc						U+20AB: DONG SIGN				
+€								Sc						U+20AC: EURO SIGN				
+₭								Sc						U+20AD: KIP SIGN				
+₮								Sc						U+20AE: TUGRIK SIGN				
+₯								Sc						U+20AF: DRACHMA SIGN				
+₰								Sc						U+20B0: GERMAN PENNY SIGN				
+₱								Sc						U+20B1: PESO SIGN				
+₲								Sc						U+20B2: GUARANI SIGN				
+₳								Sc						U+20B3: AUSTRAL SIGN				
+₴								Sc						U+20B4: HRYVNIA SIGN				
+₵								Sc						U+20B5: CEDI SIGN				
+₶								Sc						U+20B6: LIVRE TOURNOIS SIGN				
+₷								Sc						U+20B7: SPESMILO SIGN				
+₸								Sc						U+20B8: TENGE SIGN				
+₹								Sc						U+20B9: INDIAN RUPEE SIGN				
+₺								Sc						U+20BA: TURKISH LIRA SIGN				
+₻								Sc						U+20BB: NORDIC MARK SIGN				
+₼								Sc						U+20BC: MANAT SIGN				
+₽								Sc						U+20BD: RUBLE SIGN				
+₾								Sc						U+20BE: LARI SIGN				
+₿								Sc						U+20BF: BITCOIN SIGN				
+⃀								Sc						U+20C0: SOM SIGN				
+																		
+																		
+`																		
+																		
+latinPanel = 'ä bʷ č'																		
+																		
+var cols = {																		
+key: 1,																		
+ipaLoc: 2,																		
+ipaPlus: 3,																		
+transLoc: 4,																		
+transcription: 5,																		
+kbd: 6,																		
+class: 7,																		
+status: 8,																		
+typeLoc: 9,																		
+statusLoc: 10,																		
+																		
+dvowel: 0,																		
+ivowel: 0,																		
+onset:0,																		
+final:0,																		
+subj:0,																		
+ltone:0,																		
+htone:0,																		
+lc:0,																		
+uc:0,																		
+meaning:0,																		
+shape: 0,																		
+numLoc: 0,																		
+																		
+nnameLoc: 11,																		
+nameLoc: 12,																		
+ucsName: 13,																		
+block: 14,																		
+																		
+//othertranscriptions: [[5, 'Latin']]																		
+}																		
