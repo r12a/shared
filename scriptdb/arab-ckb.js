4ea46f636db707ed3464d2485c1667c390c301cf
r12a <ishida@w3.org>
2024-12-09 17:22:19 +0000

    scriptdb/arab-ckb   Reorganise

--- scriptdb/arab-ckb.js
+++ scriptdb/arab-ckb.js
@@ -1,4 +1,54 @@
 var spreadsheet = `arab-ckb	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	shape	native name	latin name	ucs name	block
+پ	p	p		p	p		Lo		consonant		4			U+067E: ARABIC LETTER PEH	
+ب	b	b		b	b		Lo		consonant		4			U+0628: ARABIC LETTER BEH	
+ت	t	t	tʰ	t	t		Lo		consonant		4			U+062A: ARABIC LETTER TEH	
+چ	c	t͡ʃ		c	ç		Lo		consonant		4			U+0686: ARABIC LETTER TCHEH	
+د	d	d		d	d		Lo		consonant		2			U+062F: ARABIC LETTER DAL	
+ج	J	d͡ʒ		ʤ	c		Lo		consonant		4			U+062C: ARABIC LETTER JEEM	
+ک	k	k	c	k	k		Lo		consonant		4			U+06A9: ARABIC LETTER KEHEH	
+گ	g	ɡ		g	g		Lo		consonant		4			U+06AF: ARABIC LETTER GAF	
+ق	q	q		q	q		Lo		consonant		4			U+0642: ARABIC LETTER QAF	
+ئ	{	ʔ		ʔ	–		Lo		word-initial vowel carrier					U+0626: ARABIC LETTER YEH WITH HAMZA ABOVE	
+ف	f	f		f	f		Lo		consonant		4			U+0641: ARABIC LETTER FEH	
+ڤ	v	v		v	v		Lo		consonant		4			U+06A4: ARABIC LETTER VEH	
+س	s	s		s	s		Lo		consonant		4			U+0633: ARABIC LETTER SEEN	
+ز	z	z		z	z		Lo		consonant		4			U+0632: ARABIC LETTER ZAIN	
+ش	S	ʃ		ʃ	ş		Lo		consonant		4			U+0634: ARABIC LETTER SHEEN	
+ژ	j	ʒ		ʒ	j		Lo		consonant		2			U+0698: ARABIC LETTER JEH	
+خ	x	x		x	x		Lo		consonant		4			U+062E: ARABIC LETTER KHAH	
+غ	X	ɣ		ɣ	ẍ		Lo		consonant		4			U+063A: ARABIC LETTER GHAIN	
+ح	H	ħ		ħ	h		Lo		consonant		4			U+062D: ARABIC LETTER HAH	
+ع	{	ʕ		ʕ	–		Lo		consonant		4			U+0639: ARABIC LETTER AIN	
+ه	h	h		h	h		Lo		consonant		4			U+0647: ARABIC LETTER HEH	
+ھ	h	h		h	h		Lo		consonant		4			U+06BE: ARABIC LETTER HEH DOACHASHMEE	
+م	m	m		m	m		Lo		consonant		4			U+0645: ARABIC LETTER MEEM	
+ن	n	n		n	n		Lo		consonant		4			U+0646: ARABIC LETTER NOON	
+نٚ	N	ŋ		ŋ	ň			r	consonant					U+0646 U+065A: ARABIC LETTER NOON, VOWEL SIGN SMALL V ABOVE	
+ر	r	ɾ		ɾ	r		Lo		consonant		2			U+0631: ARABIC LETTER REH	
+ڕ	R	r		r	rr		Lo		consonant		2			U+0695: ARABIC LETTER REH WITH SMALL V BELOW	
+ل	l	l		l	l		Lo		consonant		4			U+0644: ARABIC LETTER LAM	
+ڵ	L	ɫ		ɫ	ll		Lo		consonant		4			U+06B5: ARABIC LETTER LAM WITH SMALL V	
+ی	y	j iː		i	y i		Lo		consonant		4			U+06CC: ARABIC LETTER FARSI YEH	
+															
+و	u	ʊ w		ʊ	u		Lo		vowel		2			U+0648: ARABIC LETTER WAW	
+وو	U	uː		u	û				vowel		2			U+0648 U+0648: ARABIC LETTER WAW, LETTER WAW	
+ۆ	o	oː		o	o		Lo		vowel		2			U+06C6: ARABIC LETTER OE	
+ێ	E	eː		e	ê		Lo		vowel		4			U+06CE: ARABIC LETTER YEH WITH SMALL V	
+ە	e	æ ə ɛ		ɛ	e		Lo		vowel		2			U+06D5: ARABIC LETTER AE	
+ا	a	ɑː		a	a		Lo		vowel, glottal stop		2			U+0627: ARABIC LETTER ALEF	
+															
+ي				í			Lo	r	decomposed part of standalone vowel carrier	not normally used				U+064A: ARABIC LETTER YEH	
+ٔ				ʾ			Mn​	r	decomposed part of standalone vowel carrier	not normally used				U+0654: ARABIC HAMZA ABOVE	
+\u065A				^			Mn​	u	inappropriate orphan					U+065A: ARABIC VOWEL SIGN SMALL V ABOVE	
+ك		k		ḳ			Lo	u	arabic k	not normally used				U+0643: ARABIC LETTER KAF	
+ڶ		ɫ		ĺ			Lo	u	consonant	poorly attested alternative				U+06B6: ARABIC LETTER LAM WITH DOT ABOVE	
+ڷ		ɫ		ľ			Lo	u	consonant	poorly attested alternative				U+06B7: ARABIC LETTER LAM WITH THREE DOTS ABOVE	
+ڒ		r		ṙ			Lo	u	consonant	poorly attested alternative				U+0692: ARABIC LETTER REH WITH SMALL V	
+ڔ		r		ṝ			Lo	u	consonant	poorly attested alternative				U+0694: ARABIC LETTER REH WITH DOT BELOW	
+ۊ	y	yː		y	–		Lo	r	vowel		2			U+06CA: ARABIC LETTER WAW WITH TWO DOTS ABOVE	
+ّ				–			Mn	u	gemination diacritic	not used				U+0651: ARABIC SHADDA	
+ْ				–			Mn	u	absent vowel indicator	not used				U+0652: ARABIC SUKUN	
+															
 \u0020				 	 		Sp		space					U+0020: SPACE	
 -	-			-	-		Pd	?	hyphen					U+2010: HYPHEN	
 ‑	-			‑	‑		Pd	?	non-breaking hyphen					U+2011: NON-BREAKING HYPHEN	
@@ -25,55 +75,6 @@ var spreadsheet = `arab-ckb	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 ›	>			›			Pf	?	quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	
 ”	>			”	”		Pf		quotation mark					U+201D: RIGHT DOUBLE QUOTATION MARK	
 ’	>			’	’		Pf	?	quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	
-ا	a	ɑː		a	a		Lo		vowel, glottal stop		ا ـا			U+0627: ARABIC LETTER ALEF	
-ب	b	b		b	b		Lo		consonant		ب ببب			U+0628: ARABIC LETTER BEH	
-چ	c	t͡ʃ		c	ç		Lo		consonant		چ چچچ			U+0686: ARABIC LETTER TCHEH	
-د	d	d		d	d		Lo		consonant		د ـد			U+062F: ARABIC LETTER DAL	
-ە	e	æ ə ɛ		ɛ	e		Lo		vowel		ە ـە			U+06D5: ARABIC LETTER AE	
-ێ	E	eː		e	ê		Lo		vowel		 ێ ێێێ			U+06CE: ARABIC LETTER YEH WITH SMALL V	
-ف	f	f		f	f		Lo		consonant		ف ففف			U+0641: ARABIC LETTER FEH	
-گ	g	ɡ		g	g		Lo		consonant		گ گگگ			U+06AF: ARABIC LETTER GAF	
-ه	h	h		h	h		Lo		consonant		ه ههـ			U+0647: ARABIC LETTER HEH	
-ح	H	ħ		ħ	h		Lo		consonant		ح ححح			U+062D: ARABIC LETTER HAH	
-ھ	h	h		h	h		Lo		consonant		ھ ھھـ			U+06BE: ARABIC LETTER HEH DOACHASHMEE	
-ژ	j	ʒ		ʒ	j		Lo		consonant		ژ ـژ			U+0698: ARABIC LETTER JEH	
-ج	J	d͡ʒ		ʤ	c		Lo		consonant		 ج ججج			U+062C: ARABIC LETTER JEEM	
-ک	k	k	c	k	k		Lo		consonant		ک ککک			U+06A9: ARABIC LETTER KEHEH	
-ل	l	l		l	l		Lo		consonant		ل للل			U+0644: ARABIC LETTER LAM	
-ڵ	L	ɫ		ɫ	ll		Lo		consonant		ڵ ـڵڵ			U+06B5: ARABIC LETTER LAM WITH SMALL V	
-م	m	m		m	m		Lo		consonant		م ممم			U+0645: ARABIC LETTER MEEM	
-ن	n	n		n	n		Lo		consonant		ن ننن			U+0646: ARABIC LETTER NOON	
-ۆ	o	oː		o	o		Lo		vowel		ۆ ـۆ			U+06C6: ARABIC LETTER OE	
-پ	p	p		p	p		Lo		consonant		پ پپپ			U+067E: ARABIC LETTER PEH	
-ق	q	q		q	q		Lo		consonant		ق ققق			U+0642: ARABIC LETTER QAF	
-ر	r	ɾ		ɾ	r		Lo		consonant		ر ـر			U+0631: ARABIC LETTER REH	
-ڕ	R	r		r	rr		Lo		consonant		ڕ ـڕ			U+0695: ARABIC LETTER REH WITH SMALL V BELOW	
-س	s	s		s	s		Lo		consonant		س سسس			U+0633: ARABIC LETTER SEEN	
-ش	S	ʃ		ʃ	ş		Lo		consonant		ش ششش			U+0634: ARABIC LETTER SHEEN	
-ت	t	t	tʰ	t	t		Lo		consonant		ت تتت			U+062A: ARABIC LETTER TEH	
-و	u	ʊ w		ʊ	u		Lo		vowel		و ـو			U+0648: ARABIC LETTER WAW	
-وو	U	uː		u	û				vowel		وو ـوو			U+0648 U+0648: ARABIC LETTER WAW, LETTER WAW	
-ڤ	v	v		v	v		Lo		consonant		ڤ ڤڤڤ			U+06A4: ARABIC LETTER VEH	
-خ	x	x		x	x		Lo		consonant		خ خخخ			U+062E: ARABIC LETTER KHAH	
-غ	X	ɣ		ɣ	ẍ		Lo		consonant		غ غغغ			U+063A: ARABIC LETTER GHAIN	
-ی	y	j iː		i	y i		Lo		consonant		ی ییی			U+06CC: ARABIC LETTER FARSI YEH	
-ز	z	z		z	z		Lo		consonant		ز ـز			U+0632: ARABIC LETTER ZAIN	
-ع	{	ʕ		ʕ	–		Lo		consonant		ع ععع			U+0639: ARABIC LETTER AIN	
-نٚ	N	ŋ		ŋ	ň			r	consonant					U+0646 U+065A: ARABIC LETTER NOON, VOWEL SIGN SMALL V ABOVE	
-ئ	{	ʔ		ʔ	–		Lo		word-initial vowel carrier					U+0626: ARABIC LETTER YEH WITH HAMZA ABOVE	
-															
-ي				í			Lo	r	decomposed part of standalone vowel carrier	not normally used				U+064A: ARABIC LETTER YEH	
-ٔ				ʾ			Mn​	r	decomposed part of standalone vowel carrier	not normally used				U+0654: ARABIC HAMZA ABOVE	
-\u065A				^			Mn​	u	inappropriate orphan					U+065A: ARABIC VOWEL SIGN SMALL V ABOVE	
-ك		k		ḳ			Lo	u	arabic k	not normally used				U+0643: ARABIC LETTER KAF	
-ڶ		ɫ		ĺ			Lo	u	consonant	poorly attested alternative				U+06B6: ARABIC LETTER LAM WITH DOT ABOVE	
-ڷ		ɫ		ľ			Lo	u	consonant	poorly attested alternative				U+06B7: ARABIC LETTER LAM WITH THREE DOTS ABOVE	
-ڒ		r		ṙ			Lo	u	consonant	poorly attested alternative				U+0692: ARABIC LETTER REH WITH SMALL V	
-ڔ		r		ṝ			Lo	u	consonant	poorly attested alternative				U+0694: ARABIC LETTER REH WITH DOT BELOW	
-ۊ	y	yː		y	–		Lo	r	vowel		ۊ ـۊ			U+06CA: ARABIC LETTER WAW WITH TWO DOTS ABOVE	
-ّ				–			Mn	u	gemination diacritic	not used				U+0651: ARABIC SHADDA	
-ْ				–			Mn	u	absent vowel indicator	not used				U+0652: ARABIC SUKUN	
-															
 ۱		1		1	1				digit					U+06F1: EXTENDED ARABIC-INDIC DIGIT ONE	
 ۲		2		2	2				digit					U+06F2: EXTENDED ARABIC-INDIC DIGIT TWO	
 ۳		3		3	3				digit					U+06F3: EXTENDED ARABIC-INDIC DIGIT THREE	
