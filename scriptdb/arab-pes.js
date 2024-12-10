b08ad5f09289e077f3d29ef9a056006b4966be74
r12a <ishida@w3.org>
2024-12-10 17:02:55 +0000

    scriptdb/arab-pes   Reorder

--- scriptdb/arab-pes.js
+++ scriptdb/arab-pes.js
@@ -1,4 +1,74 @@
 var spreadsheet = `arab-pes	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	shape	native name	latin name	ucs name	block
+															
+پ	p	p		p	p	m	Lo		stop		4	پِ	pe	U+067E: ARABIC LETTER PEH	✓
+ب	b	b		b	b	f	Lo		stop		4	بِ	be	U+0628: ARABIC LETTER BEH	✓
+چ	c	t͡ʃ		ʧ	č	]	Lo		affricate		4	چِ	če	U+0686: ARABIC LETTER TCHEH	✓
+ت	t	t		t	t	j	Lo		stop		4	تِ	te	U+062A: ARABIC LETTER TEH	✓
+ط	T	t		t̂	t	x	Lo		stop		4	طی	ṭā	U+0637: ARABIC LETTER TAH	✓
+د	d	d		d	d	n	Lo		stop		2	دال	dāl	U+062F: ARABIC LETTER DAL	✓
+ج	j	d͡ʒ		ʤ	j	[	Lo		affricate		4	جیم	jim	U+062C: ARABIC LETTER JEEM	✓
+ک	k	k		k	k	;	Lo		stop		4	کاف	kāf	U+06A9: ARABIC LETTER KEHEH	✓
+گ	g¶	g		g	g		Lo		stop		4	گاف	ɡāf	U+06AF: ARABIC LETTER GAF	✓
+ق	q	ɢ		ɢ	q	r	Lo		stop		4	قاف	qāf	U+0642: ARABIC LETTER QAF	✓
+ؤ	w	ʔ		w͑		A	Lo		glottal stop		2	همزه	hæmze	U+0624: ARABIC LETTER WAW WITH HAMZA ABOVE	✓
+یٔ	y	ʔ		yʿ					glottal stop		4	همزه	hæmze	U+06CC U+0654: ARABIC LETTER FARSI YEH, HAMZA ABOVE	
+ئ	Y	ʔ		y͑		S	Lo	u	glottal stop	incorrect character	4	همزه	hæmze	U+0626: ARABIC LETTER YEH WITH HAMZA ABOVE	✓
+ع	{	ʔ ∅		ʔ̇	a ’	u	Lo		fricative		4	عین	ʿeyn	U+0639: ARABIC LETTER AIN	✓
+ء	}	ʔ		ʔ		M	Lo		glottal stop			همزه	hæmze	U+0621: ARABIC LETTER HAMZA	✓
+ڤ		β					Lo	a	fricative		4			U+06A4: ARABIC LETTER VEH	✓
+ف	f	f		f	f	t	Lo		fricative		4	فِ	fe	U+0641: ARABIC LETTER FEH	✓
+و	v	v u o ow		v	v u	,	Lo		fricative/mater lectionis		2	واو	vāv	U+0648: ARABIC LETTER WAW	✓
+ث	s	s		ṡ	s	e	Lo		fricative		4	ثِ	se	U+062B: ARABIC LETTER THEH	✓
+ص	S	s		ŝ	s	w	Lo		fricative		4	صاد	sād	U+0635: ARABIC LETTER SAD	✓
+س	s¶	s		s	s	s	Lo		fricative		4	سین	sin	U+0633: ARABIC LETTER SEEN	✓
+ذ	z	z		ż	z	b	Lo		fricative		2	ذال	zāl	U+0630: ARABIC LETTER THAL	✓
+ض	Z	z		ẑ	z	q	Lo		fricative		4	ضاد	zād	U+0636: ARABIC LETTER DAD	✓
+ظ	Z	z		ž	z	z	Lo		consonant		4	ظی	ẓā	U+0638: ARABIC LETTER ZAH	✓
+ز	z¶	z		z	z	c	Lo		fricative		2	زِ	ze	U+0632: ARABIC LETTER ZAIN	✓
+ش	s	ʃ		ʃ	š	a	Lo		fricative		4	شین	šin	U+0634: ARABIC LETTER SHEEN	✓
+ژ	z	ʒ		ʒ	ž	C	Lo		fricative		2	ژِ	že	U+0698: ARABIC LETTER JEH	✓
+خ	x	x		x	x	o	Lo		fricative		4	خِ	xe	U+062E: ARABIC LETTER KHAH	✓
+غ	g	ɢ ɣ		ɣ	ġ	y	Lo		consonant		4	غین	ġeyn	U+063A: ARABIC LETTER GHAIN	✓
+ح	h	h		ħ	h	p	Lo		fricative		4	حِ	he	U+062D: ARABIC LETTER HAH	✓
+ه	h¶	h e		h	h e	i	Lo		fricative		4	هِ	he do-češm	U+0647: ARABIC LETTER HEH	✓
+ة	t	h - ɛ æ		ẗ		J	Lo	i	consonant+vowel		2			U+0629: ARABIC LETTER TEH MARBUTA	✓
+م	m	m		m	m	l	Lo		nasal		4	میم	mim	U+0645: ARABIC LETTER MEEM	✓
+ن	n	n		n	n	k	Lo		nasal		4	نون	nun	U+0646: ARABIC LETTER NOON	✓
+ر	r	r		r	r	v	Lo		trill		2	رِ	re	U+0631: ARABIC LETTER REH	✓
+ل	l	l		l	l	g	Lo		lateral		4	لام	lām	U+0644: ARABIC LETTER LAM	✓
+ي	Y	j iː		ŷ		D	Lo	l	consonant+vowel	non-native	4			U+064A: ARABIC LETTER YEH	✓
+ی	y¶	j iː		y	y i	d	Lo		approximant/mater lectionis		4	یِ	ye	U+06CC: ARABIC LETTER FARSI YEH	✓
+															
+															
+ِ	i	e		i		Y	Mn		vowel			زیر	zir	U+0650: ARABIC KASRA	✓
+ُ	u	o		u		T	Mn		vowel			پیش	piš	U+064F: ARABIC DAMMA	✓
+ٌ	u	un		uⁿ		W	Mn	r	vowel			تنوین رفع	tanvin e rafe	U+064C: ARABIC DAMMATAN	✓
+ٍ	i	in		iⁿ		E	Mn	r	vowel			تنوین جرّ	tanvin e jarr	U+064D: ARABIC KASRATAN	✓
+ا	A	ɒː æ e o ∅		ɑ	ā a o e	h	Lo		vowel carrier/vowel		2	الف	ʾalef	U+0627: ARABIC LETTER ALEF	✓
+آ	A	ɒː		ɑ̄	ā	H	Lo		glottal stop		2			U+0622: ARABIC LETTER ALEF WITH MADDA ABOVE	✓
+أ	A	ʔ		ɑ͑		G	Lo	r	glottal stop		2	همزه	hæmze	U+0623: ARABIC LETTER ALEF WITH HAMZA ABOVE	✓
+َ	a	æ		a		U	Mn		vowel			زِبَر	zebar	U+064E: ARABIC FATHA	✓
+ً	a	an		aⁿ		R	Mn		vowel			تنوین نصب	tanvin e nasb	U+064B: ARABIC FATHATAN	✓
+															
+															
+ۀ				–			Lo	u	confusable	do not use for Persian				U+06C0: ARABIC LETTER HEH WITH YEH ABOVE	
+ە				–			Lo	u	confusable	do not use for Persian				U+06D5: ARABIC LETTER AE	
+ہ				–			Lo	u	confusable	do not use for Persian				U+06C1: ARABIC LETTER HEH GOAL	
+ۂ				–			Lo	u	confusable	do not use for Persian				U+06C2: ARABIC LETTER HEH GOAL WITH HAMZA ABOVE	
+ئ				–			Lo	u	confusable	do not use for Persian				U+0626: ARABIC LETTER YEH WITH HAMZA ABOVE	
+ࢨ				–			Lo	u	confusable	do not use for Persian				U+08A8: ARABIC LETTER YEH WITH TWO DOTS BELOW AND HAMZA ABOVE	
+ي				–			Lo	u	confusable	do not use for Persian				U+064A: ARABIC LETTER YEH	
+ۃ				–			Lo	u	confusable	do not use for Persian				U+06C3: ARABIC LETTER TEH MARBUTA GOAL	
+ك				–			Lo	u	confusable	do not use for Persian				U+0643: ARABIC LETTER KAF	
+ى				–			Lo	u	confusable	do not use for Persian				U+0649: ARABIC LETTER ALEF MAKSURA	
+															
+لا				lɑ					ligature					U+0644 U+0627: ARABIC LETTER LAM, LETTER ALEF	
+لآ				lɑ̄					ligature					U+0644 U+0622: ARABIC LETTER LAM, LETTER ALEF WITH MADDA ABOVE	
+لأ				lɑ͑					ligature					U+0644 U+0623: ARABIC LETTER LAM, LETTER ALEF WITH HAMZA ABOVE	
+لإ				lɑ̜					ligature					U+0644 U+0625: ARABIC LETTER LAM, LETTER ALEF WITH HAMZA BELOW	
+															
+ّ				˖		I	Mn		gemination mark			تشدید	tašdid	U+0651: ARABIC SHADDA	✓
+															
 ـ	_			_			Lm	i	baseline extender					U+0640: ARABIC TATWEEL	✓
 -	-			-	-			?	hyphen					U+2010: HYPHEN	
 ‑	-			‑	‑		Pd	?	non-breaking hyphen					U+2011: NON-BREAKING HYPHEN	
@@ -18,8 +88,6 @@ var spreadsheet = `arab-pes	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 ]	)			]	]	O	Pe	?	bracket					U+005D: RIGHT SQUARE BRACKET	
 ٔ	[	ʔ		ʿ		N	Mn		hamza					U+0654: ARABIC HAMZA ABOVE	✓
 ٭	[			*			Po	?	punctuation					U+066D: ARABIC FIVE POINTED STAR	✓
-ع	{	ʔ ∅		ʔ̇	a ’	u	Lo		fricative		ع ععع	عین	ʿeyn	U+0639: ARABIC LETTER AIN	✓
-ء	}	ʔ		ʔ		M	Lo		glottal stop		ء	همزه	hæmze	U+0621: ARABIC LETTER HAMZA	✓
 ٓ	}			~		X	Mn	r	maddah diacritic	used with ا				U+0653: ARABIC MADDAH ABOVE	✓
 ٕ	}			˓			Mn	u	hamza	not used				U+0655: ARABIC HAMZA BELOW	✓
 ْ	*			͓		Q	Mn		vowel absence marker			سکون	sokun	U+0652: ARABIC SUKUN	✓
@@ -66,81 +134,10 @@ var spreadsheet = `arab-pes	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 ›	>			›			Pf	?	quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	✓
 ”	>			”	”		Pf	?	quotation mark					U+201D: RIGHT DOUBLE QUOTATION MARK	✓
 ’	>			\‘	\‘		Pf	?	quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	✓
-															
-ا	A	ɒː æ e o ∅		ɑ	ā a o e	h	Lo		vowel carrier/vowel		ا ـا	الف	ʾalef	U+0627: ARABIC LETTER ALEF	✓
-آ	A	ɒː		ɑ̄	ā	H	Lo		glottal stop		آ ـآ			U+0622: ARABIC LETTER ALEF WITH MADDA ABOVE	✓
-أ	A	ʔ		ɑ͑		G	Lo	r	glottal stop		أ ـأ	همزه	hæmze	U+0623: ARABIC LETTER ALEF WITH HAMZA ABOVE	✓
-َ	a	æ		a		U	Mn		vowel			زِبَر	zebar	U+064E: ARABIC FATHA	✓
-ً	a	an		aⁿ		R	Mn		vowel			تنوین نصب	tanvin e nasb	U+064B: ARABIC FATHATAN	✓
-ب	b	b		b	b	f	Lo		stop		ب ببب	بِ	be	U+0628: ARABIC LETTER BEH	✓
-چ	c	t͡ʃ		ʧ	č	]	Lo		affricate		چ چچچ	چِ	če	U+0686: ARABIC LETTER TCHEH	✓
-د	d	d		d	d	n	Lo		stop		د ـد	دال	dāl	U+062F: ARABIC LETTER DAL	✓
-ف	f	f		f	f	t	Lo		fricative		ف ففف	فِ	fe	U+0641: ARABIC LETTER FEH	✓
-غ	g	ɢ ɣ		ɣ	ġ	y	Lo		consonant		ع ععع	غین	ġeyn	U+063A: ARABIC LETTER GHAIN	✓
-گ	g¶	g		g	g		Lo		stop		گ گگگ	گاف	ɡāf	U+06AF: ARABIC LETTER GAF	✓
-ح	h	h		ħ	h	p	Lo		fricative		ح ححح	حِ	he	U+062D: ARABIC LETTER HAH	✓
-ه	h¶	h e		h	h e	i	Lo		fricative		ه ههه	هِ	he do-češm	U+0647: ARABIC LETTER HEH	✓
-ِ	i	e		i		Y	Mn		vowel			زیر	zir	U+0650: ARABIC KASRA	✓
-ٍ	i	in		iⁿ		E	Mn	r	vowel			تنوین جرّ	tanvin e jarr	U+064D: ARABIC KASRATAN	✓
-ج	j	d͡ʒ		ʤ	j	[	Lo		affricate		ج ججج	جیم	jim	U+062C: ARABIC LETTER JEEM	✓
-ک	k	k		k	k	;	Lo		stop		ک ککک	کاف	kāf	U+06A9: ARABIC LETTER KEHEH	✓
-ل	l	l		l	l	g	Lo		lateral		ل للل	لام	lām	U+0644: ARABIC LETTER LAM	✓
-م	m	m		m	m	l	Lo		nasal		م ممم	میم	mim	U+0645: ARABIC LETTER MEEM	✓
-ن	n	n		n	n	k	Lo		nasal		ن ننن	نون	nun	U+0646: ARABIC LETTER NOON	✓
-پ	p	p		p	p	m	Lo		stop		پ پپپ	پِ	pe	U+067E: ARABIC LETTER PEH	✓
-ق	q	ɢ		ɢ	q	r	Lo		stop		ق ققق	قاف	qāf	U+0642: ARABIC LETTER QAF	✓
-ر	r	r		r	r	v	Lo		trill		ر ـر	رِ	re	U+0631: ARABIC LETTER REH	✓
-ث	s	s		ṡ	s	e	Lo		fricative		ث ثثث	ثِ	se	U+062B: ARABIC LETTER THEH	✓
-ص	S	s		ŝ	s	w	Lo		fricative		ص صصص	صاد	sād	U+0635: ARABIC LETTER SAD	✓
-ش	s	ʃ		ʃ	š	a	Lo		fricative		ش ششش	شین	šin	U+0634: ARABIC LETTER SHEEN	✓
-س	s¶	s		s	s	s	Lo		fricative		س سسس	سین	sin	U+0633: ARABIC LETTER SEEN	✓
-ت	t	t		t	t	j	Lo		stop		ت تتت	تِ	te	U+062A: ARABIC LETTER TEH	✓
-ط	T	t		t̂	t	x	Lo		stop		ط ططط	طی	ṭā	U+0637: ARABIC LETTER TAH	✓
-ة	t	h - ɛ æ		ẗ		J	Lo	i	consonant+vowel		ة ـة			U+0629: ARABIC LETTER TEH MARBUTA	✓
-ُ	u	o		u		T	Mn		vowel			پیش	piš	U+064F: ARABIC DAMMA	✓
-ٌ	u	un		uⁿ		W	Mn	r	vowel			تنوین رفع	tanvin e rafe	U+064C: ARABIC DAMMATAN	✓
-و	v	v u o ow		v	v u	,	Lo		fricative/mater lectionis		و ـو	واو	vāv	U+0648: ARABIC LETTER WAW	✓
-ؤ	w	ʔ		w͑		A	Lo		glottal stop		ؤ ـؤ	همزه	hæmze	U+0624: ARABIC LETTER WAW WITH HAMZA ABOVE	✓
-خ	x	x		x	x	o	Lo		fricative		خ خخخ	خِ	xe	U+062E: ARABIC LETTER KHAH	✓
-یٔ	y	ʔ		yʿ					glottal stop		ئ ئئئ	همزه	hæmze	U+06CC U+0654: ARABIC LETTER FARSI YEH, HAMZA ABOVE	
-ئ	Y	ʔ		y͑		S	Lo	u	glottal stop	incorrect character	ئ ئئئ	همزه	hæmze	U+0626: ARABIC LETTER YEH WITH HAMZA ABOVE	✓
-ي	Y	j iː		ŷ		D	Lo	l	consonant+vowel	non-native	ي ييي			U+064A: ARABIC LETTER YEH	✓
-ی	y¶	j iː		y	y i	d	Lo		approximant/mater lectionis		ی ییی	یِ	ye	U+06CC: ARABIC LETTER FARSI YEH	✓
-ذ	z	z		ż	z	b	Lo		fricative		ذ ـذ	ذال	zāl	U+0630: ARABIC LETTER THAL	✓
-ض	Z	z		ẑ	z	q	Lo		fricative		ض ضضض	ضاد	zād	U+0636: ARABIC LETTER DAD	✓
-ظ	Z	z		ž	z	z	Lo		consonant		ظ ظظظ	ظی	ẓā	U+0638: ARABIC LETTER ZAH	✓
-ژ	z	ʒ		ʒ	ž	C	Lo		fricative		ژ ـژ	ژِ	že	U+0698: ARABIC LETTER JEH	✓
-ز	z¶	z		z	z	c	Lo		fricative		ز ـز	زِ	ze	U+0632: ARABIC LETTER ZAIN	✓
-															
-ڤ		β					Lo	a	fricative					U+06A4: ARABIC LETTER VEH	✓
-															
-ۀ				–			Lo	u	confusable	do not use for Persian				U+06C0: ARABIC LETTER HEH WITH YEH ABOVE	
-ە				–			Lo	u	confusable	do not use for Persian				U+06D5: ARABIC LETTER AE	
-ہ				–			Lo	u	confusable	do not use for Persian				U+06C1: ARABIC LETTER HEH GOAL	
-ۂ				–			Lo	u	confusable	do not use for Persian				U+06C2: ARABIC LETTER HEH GOAL WITH HAMZA ABOVE	
-ئ				–			Lo	u	confusable	do not use for Persian				U+0626: ARABIC LETTER YEH WITH HAMZA ABOVE	
-ࢨ				–			Lo	u	confusable	do not use for Persian				U+08A8: ARABIC LETTER YEH WITH TWO DOTS BELOW AND HAMZA ABOVE	
-ي				–			Lo	u	confusable	do not use for Persian				U+064A: ARABIC LETTER YEH	
-ۃ				–			Lo	u	confusable	do not use for Persian				U+06C3: ARABIC LETTER TEH MARBUTA GOAL	
-ك				–			Lo	u	confusable	do not use for Persian				U+0643: ARABIC LETTER KAF	
-ى				–			Lo	u	confusable	do not use for Persian				U+0649: ARABIC LETTER ALEF MAKSURA	
-															
-لا				lɑ					ligature					U+0644 U+0627: ARABIC LETTER LAM, LETTER ALEF	
-لآ				lɑ̄					ligature					U+0644 U+0622: ARABIC LETTER LAM, LETTER ALEF WITH MADDA ABOVE	
-لأ				lɑ͑					ligature					U+0644 U+0623: ARABIC LETTER LAM, LETTER ALEF WITH HAMZA ABOVE	
-لإ				lɑ̜					ligature					U+0644 U+0625: ARABIC LETTER LAM, LETTER ALEF WITH HAMZA BELOW	
-															
-ّ				˖		I	Mn		gemination mark			تشدید	tašdid	U+0651: ARABIC SHADDA	✓
-															
 {				{	{	}	Ps	?	bracket					U+007B: LEFT CURLY BRACKET	
 }				}	}	{	Pe	?	bracket					U+007D: RIGHT CURLY BRACKET	
 															
 ʼ				ʼ	ʼ		Lm	?	apostrophe					U+02BC: MODIFIER LETTER APOSTROPHE	
-§				§	§		Po	?	section sign					U+00A7: SECTION SIGN	
-†				†	†		Po	?	dagger					U+2020: DAGGER	
-‡				‡	‡		Po	?	double dagger					U+2021: DOUBLE DAGGER	
-′				′	′		Po	?	prime					U+2032: PRIME	
-″				″	″		Po	?	double prime					U+2033: DOUBLE PRIME	
 															
 ٰ				–		V	Mn	?		for the keyboard				U+0670: ARABIC LETTER SUPERSCRIPT ALEF	✓
 `															
