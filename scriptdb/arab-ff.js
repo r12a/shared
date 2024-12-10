a1afd28e92d5b7009370d380a6db349328d08e3c
r12a <ishida@w3.org>
2024-12-10 06:42:07 +0000

    scriptdb/arab-ff   Reorder

--- scriptdb/arab-ff.js
+++ scriptdb/arab-ff.js
@@ -1,133 +1,137 @@
-var spreadsheet = `arab-ff	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	latin name	ucs name	block
--	-			-	-		Pd		hyphen				U+2010: HYPHEN	
-‑	-			‑	‑		Pd		non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
-–	-			–	–		Pd		en dash				U+2013: EN DASH	
-—	-			—	—		Pd		em dash				U+2014: EM DASH	
-،	,			,	,		Po		comma				U+060C: ARABIC COMMA	
-؛	;			;			Po		semicolon				U+061B: ARABIC SEMICOLON	
-:	:			:	:		Po		colon				U+003A: COLON	
-!	!			!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK	
-؟	?			?	?		Po		question mark				U+061F: ARABIC QUESTION MARK	
-⸫	.			.̣	.		Po		full stop				U+2E2B: ONE DOT OVER TWO DOTS PUNCTUATION	
-.	.			.	.		Po		full stop				U+002E: FULL STOP	
-…	.			…	…		Po		ellipsis				U+2026: HORIZONTAL ELLIPSIS	
-(	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS	
-[	(			[	[		Pe		bracket				U+005B: LEFT SQUARE BRACKET	
-)	)			)	)		Ps		parenthesis				U+0029: RIGHT PARENTHESIS	
-]	)			]	]		Pe		bracket				U+005D: RIGHT SQUARE BRACKET	
-%	%			%			Po		percentage mark				U+0025: PERCENT SIGN	
-‰	%			‰			Po	u	per mille mark				U+2030: PER MILLE SIGN	
-«	<			«			Pi		quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	✓
-‹	<			‹			Pi	u	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
-“	<			“	“		Pi	u	quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
-‘	<			\‘	\‘		Pi	u	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
-»	>			»			Pf		quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	✓
-›	>			›			Pf	u	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
-”	>			”	”		Pf	u	quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
-’	>			\’	\’		Pf	u	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
-ْ	*			͞			Mn		vowel absence marker			howde / hownde	U+0652: ARABIC SUKUN	
-ّ	&			˖			Mn		gemination mark			siddere	U+0651: ARABIC SHADDA	
-ٰ	[			̍			Mn		vowel	used as part of ٜىٰ			U+0670: ARABIC LETTER SUPERSCRIPT ALEF	
-ا		–		ā			Lo		vowel lengthener			aliifi	U+0627: ARABIC LETTER ALEF	
-َ	a	a		a	a		Mn​		vowel			maasi dow	U+064E: ARABIC FATHA	
-َا	A	aː		aā	a				vowel support			aliifi balewol / bodeewol	U+064E U+0627: ARABIC FATHA, LETTER ALEF	
-ب	b	b		b	b		Lo		consonant			bee	U+0628: ARABIC LETTER BEH	
-ࢡ	B	ɓ		ɓ	ɓ		Lo		consonant			ɓee moɗu	U+08A1: ARABIC LETTER BEH WITH HAMZA ABOVE	✓
-مب	b	ᵐb		mb	mb				pre-nasalised consonant				U+0645 U+0628: ARABIC LETTER MEEM, LETTER BEH	
-ش	c	t͡ʃ ʃ		c	c		Lo		consonant			ciini	U+0634: ARABIC LETTER SHEEN	
-ث	c	θ		θ	th		Lo	l	consonant	Arabic loan words only		camamlu	U+062B: ARABIC LETTER THEH	
-د	d	d		d	d		Lo		consonant			deeli	U+062F: ARABIC LETTER DAL	
-ند	d	ⁿd		nd	nd				pre-nasalised consonant				U+0646 U+062F: ARABIC LETTER NOON, LETTER DAL	
-ض	d	d		ḍ	d		Lo	l	consonant			baadi / daadi	U+0636: ARABIC LETTER DAD	
-ط	D	ɗ		ɗ			Lo		consonant			ɗaadi	U+0637: ARABIC LETTER TAH	✓
-ٜ	e	e		e	e		Mn​		vowel			yamaleere	U+065C: ARABIC VOWEL SIGN DOT BELOW	
-ٜىٰ	E	eː		ē	ee				long vowel			yamaleere bee aliifi bodewol	U+065C U+0649 U+0670: ARABIC VOWEL SIGN DOT BELOW, LETTER ALEF MAKSURA, LETTER SUPERSCRIPT ALEF	
-ٜى	E	eː		ē	ee				long vowel	final form only			U+065C U+0649: ARABIC VOWEL SIGN DOT BELOW, LETTER ALEF MAKSURA	
-ى	E	eː		ẹ			Lo		long vowel				U+0649: ARABIC LETTER ALEF MAKSURA	
-ڢ	f	f		f	f		Lo		consonant			fee	U+06A2: ARABIC LETTER FEH WITH DOT MOVED BELOW	
-غ	g	ɡ		g	g		Lo		consonant			aŋgani	U+063A: ARABIC LETTER GHAIN	
-ح	h	h		h	h		Lo		consonant			haa baaluul	U+062D: ARABIC LETTER HAH	
-ه	h	h		ḥ	h		Lo	r	consonant	important Qur'anic names only		haa peetel	U+0647: ARABIC LETTER HEH	
-ِ	i	i		i	i		Mn​		vowel			maasi les	U+0650: ARABIC KASRA	
-ِي	I	iː		iy	ii				long vowel			maasi les ii	U+0650 U+064A: ARABIC KASRA, LETTER YEH	
-ج	j	d͡ʒ		ʤ	j		Lo		consonant			jiimi	U+062C: ARABIC LETTER JEEM	
-ذ	j	d͡ʒ		ʤ̣	j		Lo	l	consonant	Arabic loan words only		zaali	U+0630: ARABIC LETTER THAL	
-ز	j	d͡ʒ		z	j		Lo	l	consonant	Arabic loan words only		zayra	U+0632: ARABIC LETTER ZAIN	
-ظ	j	d͡ʒ		ẓ	j		Lo	l	consonant	Arabic loan words only		zaadi	U+0638: ARABIC LETTER ZAH	
-نج	j	ᶮd͡ʒ		nʤ	nj				pre-nasalised consonant				U+0646 U+062C: ARABIC LETTER NOON, LETTER JEEM	
-ک	k	k		k	k		Lo		consonant			keefu	U+06A9: ARABIC LETTER KEHEH	
-ل	l	l		l	l		Lo		consonant			laamu	U+0644: ARABIC LETTER LAM	
-م	m	m		m	m		Lo		consonant			miimi	U+0645: ARABIC LETTER MEEM	
-ن	n	n		n	n		Lo		consonant			nuunu	U+0646: ARABIC LETTER NOON	
-نغ	n	ᵑɡ ŋ		ng	ŋ				consonant			nuunu e aŋgini	U+0646 U+063A: ARABIC LETTER NOON, LETTER GHAIN	
-ࢩ	N	ɲ		ɲ	ny		Lo		consonant			nya	U+08A9: ARABIC LETTER YEH WITH TWO DOTS BELOW AND DOT ABOVE	✓
-ٛ	o	o		o	o		Mn​		vowel			turnɗe o / esoode	U+065B: ARABIC VOWEL SIGN INVERTED SMALL V ABOVE	
-ٛو	O	oː		ow	oo				long vowel			turnɗe oo	U+065B U+0648: ARABIC VOWEL SIGN INVERTED SMALL V ABOVE, LETTER WAW	
-ݠ	p	p		p	p		Lo		consonant			pee	U+0760: ARABIC LETTER FEH WITH TWO DOTS BELOW	
-ر	r	ɾ		ɾ	r		Lo		consonant			arre	U+0631: ARABIC LETTER REH	
-رّ	r	r		r	rr				consonant				U+0631 U+0651: ARABIC LETTER REH, SHADDA	
-س	s	s		s	s		Lo		consonant			siini	U+0633: ARABIC LETTER SEEN	
-ص	s	s sˤ		ṣ	s		Lo	l	consonant	Arabic loan words only		saadi	U+0635: ARABIC LETTER SAD	
-ت	t	t		t	t		Lo		consonant			tee	U+062A: ARABIC LETTER TEH	
-ُ	u	u		u	u		Mn​		vowel			turnɗe / too turi	U+064F: ARABIC DAMMA	
-ُو	U	uː		uw	uu				long vowel			turnɗe uu	U+064F U+0648: ARABIC DAMMA, LETTER WAW	
-و	w	w		w	w		Lo		consonant			waawu	U+0648: ARABIC LETTER WAW	
-خ	x	x		x	kh		Lo	l	consonant	Arabic loan words only		haa toɓɓuŋgol	U+062E: ARABIC LETTER KHAH	
-ق	q	χ		χ	k g		Lo	l	consonant	Arabic loan words or Qur'anic names only		naafu / gaafu	U+0642: ARABIC LETTER QAF	
-ي	y	j		y	y		Lo		consonant			yah	U+064A: ARABIC LETTER YEH	
-ࢨ	y	ʔʲ		ƴ	ƴ		Lo		consonant	Cameroon		ƴah moɗi	U+08A8: ARABIC LETTER YEH WITH TWO DOTS BELOW AND HAMZA ABOVE	✓
-ع	{	ʔ-		ʔ	’		Lo		glottal stop	before standalone a, i, u		ayni	U+0639: ARABIC LETTER AIN	
-ء	{	ʔ		ʔ̣	’		Lo	l	glottal stop	Arabic loan words or Qur'anic names only		hamaza	U+0621: ARABIC LETTER HAMZA	
-أ	}	ʔ		ɑ͑	a u	H	Lo	l	glottal stop				U+0623: ARABIC LETTER ALEF WITH HAMZA ABOVE	
-إ	}	ʔ		i̜	i	Y	Lo	l	glottal stop				U+0625: ARABIC LETTER ALEF WITH HAMZA BELOW	
-ٔ	}	ʔ		ʿ			Mn	r	hamza				U+0654: ARABIC HAMZA ABOVE	
-ٕ	}	ʔ		˓			Mn	r	hamza				U+0655: ARABIC HAMZA BELOW	
-														
-١	±	1		1̣	1		Nd		digit				U+0661: ARABIC-INDIC DIGIT ONE	
-٢	±	2		2̣	2		Nd		digit				U+0662: ARABIC-INDIC DIGIT TWO	
-٣	±	3		3̣	3		Nd		digit				U+0663: ARABIC-INDIC DIGIT THREE	
-٤	±	4		4̣	4		Nd		digit				U+0664: ARABIC-INDIC DIGIT FOUR	
-٥	±	5		5̣	5		Nd		digit				U+0665: ARABIC-INDIC DIGIT FIVE	
-٦	±	6		6̣	6		Nd		digit				U+0666: ARABIC-INDIC DIGIT SIX	
-٧	±	7		7̣	7		Nd		digit				U+0667: ARABIC-INDIC DIGIT SEVEN	
-٨	±	8		8̣	8		Nd		digit				U+0668: ARABIC-INDIC DIGIT EIGHT	
-٩	±	9		9̣	9		Nd		digit				U+0669: ARABIC-INDIC DIGIT NINE	
-٠	±	0		0̣	0		Nd		digit				U+0660: ARABIC-INDIC DIGIT ZERO	
-														
-\u200C	=			ₓ			Cf		zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
-\u200D	=			₊			Cf		zero-width joiner				U+200D: ZERO WIDTH JOINER	
-\u2067	=			ʳˡⁱ			Cf		rtl isolate				U+2067: RIGHT-TO-LEFT ISOLATE	
-\u2066	=			ˡʳⁱ			Cf		ltr isolate				U+2066: LEFT-TO-RIGHT ISOLATE	
-\u2068	=			ᶠˢⁱ			Cf		first-strong isolate				U+2068: FIRST STRONG ISOLATE	
-\u2069	=			ᵖᵈⁱ			Cf		pop direction isolate				U+2069: POP DIRECTIONAL ISOLATE	
-\u202B	=			ʳˡᵉ			Cf		rtl embed				U+202B: RIGHT-TO-LEFT EMBEDDING	
-\u202A	=			ˡʳᵉ			Cf		ltr embed				U+202A: LEFT-TO-RIGHT EMBEDDING	
-\u202C	=			ᵖᵈᶠ			Cf		pop direction				U+202C: POP DIRECTIONAL FORMATTING	
-\u200F	=			ʳˡᵐ			Cf		rtl mark				U+200F: RIGHT-TO-LEFT MARK	
-\u200E	=			ˡʳᵐ			Cf		ltr mark				U+200E: LEFT-TO-RIGHT MARK	
-\u034F	=			ᶜᵍʲ			Mn		combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER	
-\u061C	=			ᵃˡᵐ			Cf		arabic letter mark				U+061C: ARABIC LETTER MARK	
-◌				◌	◌			u					U+25CC: DOTTED CIRCLE	
-`														
-														
-latinPanel = 'aː d͡ʒ eː ẹ ḥ iː oː t͡ʃ uː ā ē ŋ ƴ ɓ ɗ ɡ ɲ ɾ ʃ ʔ ʔʲ θ χ ᵐb ᵑɡ ᶮd͡ʒ ḍ ṣ ’ ⁿd'														
-														
-var cols = {														
-key: 1,														
-ipaLoc: 2,														
-ipaPlus: 3,														
-transLoc: 4,														
-transcription: 5,														
-kbd: 6,														
-class: 7,														
-status: 8,														
-typeLoc: 9,														
-statusLoc: 10,														
-														
-nnameLoc: 11,														
-nameLoc: 12,														
-ucsName: 13,														
-block: 14,														
-														
-//othertranscriptions: [[5, 'LOC']]														
-}														
+var spreadsheet = `arab-ff	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	shape	native name	latin name	ucs name	block
+ݠ	p	p		p	p		Lo		consonant		4		pee	U+0760: ARABIC LETTER FEH WITH TWO DOTS BELOW	
+ب	b	b		b	b		Lo		consonant		4		bee	U+0628: ARABIC LETTER BEH	
+ࢡ	B	ɓ		ɓ	ɓ		Lo		consonant		4		ɓee moɗu	U+08A1: ARABIC LETTER BEH WITH HAMZA ABOVE	✓
+ت	t	t		t	t		Lo		consonant		4		tee	U+062A: ARABIC LETTER TEH	
+ش	c	t͡ʃ ʃ		c	c		Lo		consonant		4		ciini	U+0634: ARABIC LETTER SHEEN	
+د	d	d		d	d		Lo		consonant		2		deeli	U+062F: ARABIC LETTER DAL	
+ض	d	d		ḍ	d		Lo	l	consonant		4		baadi / daadi	U+0636: ARABIC LETTER DAD	
+ج	j	d͡ʒ		ʤ	j		Lo		consonant		4		jiimi	U+062C: ARABIC LETTER JEEM	
+ذ	j	d͡ʒ		ʤ̣	j		Lo	l	consonant	Arabic loan words only	2		zaali	U+0630: ARABIC LETTER THAL	
+ز	j	d͡ʒ		z	j		Lo	l	consonant	Arabic loan words only	2		zayra	U+0632: ARABIC LETTER ZAIN	
+ظ	j	d͡ʒ		ẓ	j		Lo	l	consonant	Arabic loan words only	4		zaadi	U+0638: ARABIC LETTER ZAH	
+ط	D	ɗ		ɗ			Lo		consonant		4		ɗaadi	U+0637: ARABIC LETTER TAH	✓
+ک	k	k		k	k		Lo		consonant		4		keefu	U+06A9: ARABIC LETTER KEHEH	
+غ	g	ɡ		g	g		Lo		consonant		4		aŋgani	U+063A: ARABIC LETTER GHAIN	
+ࢨ	y	ʔʲ		ƴ	ƴ		Lo		consonant	Cameroon	4		ƴah moɗi	U+08A8: ARABIC LETTER YEH WITH TWO DOTS BELOW AND HAMZA ABOVE	✓
+ع	{	ʔ-		ʔ	’		Lo		glottal stop	before standalone a, i, u	4		ayni	U+0639: ARABIC LETTER AIN	
+ء	{	ʔ		ʔ̣	’		Lo	l	glottal stop	Arabic loan words or Qur'anic names only			hamaza	U+0621: ARABIC LETTER HAMZA	
+أ	}	ʔ		ɑ͑	a u	H	Lo	l	glottal stop		2			U+0623: ARABIC LETTER ALEF WITH HAMZA ABOVE	
+إ	}	ʔ		i̜	i	Y	Lo	l	glottal stop		2			U+0625: ARABIC LETTER ALEF WITH HAMZA BELOW	
+ٔ	}	ʔ		ʿ			Mn	r	hamza					U+0654: ARABIC HAMZA ABOVE	
+ٕ	}	ʔ		˓			Mn	r	hamza					U+0655: ARABIC HAMZA BELOW	
+مب	b	ᵐb		mb	mb				pre-nasalised consonant		2			U+0645 U+0628: ARABIC LETTER MEEM, LETTER BEH	
+ند	d	ⁿd		nd	nd				pre-nasalised consonant		2			U+0646 U+062F: ARABIC LETTER NOON, LETTER DAL	
+نج	j	ᶮd͡ʒ		nʤ	nj				pre-nasalised consonant		2			U+0646 U+062C: ARABIC LETTER NOON, LETTER JEEM	
+نغ	n	ᵑɡ ŋ		ng	ŋ				consonant		2		nuunu e aŋgini	U+0646 U+063A: ARABIC LETTER NOON, LETTER GHAIN	
+ڢ	f	f		f	f		Lo		consonant		4		fee	U+06A2: ARABIC LETTER FEH WITH DOT MOVED BELOW	
+ث	c	θ		θ	th		Lo	l	consonant	Arabic loan words only	4		camamlu	U+062B: ARABIC LETTER THEH	
+س	s	s		s	s		Lo		consonant		4		siini	U+0633: ARABIC LETTER SEEN	
+ص	s	s sˤ		ṣ	s		Lo	l	consonant	Arabic loan words only	4		saadi	U+0635: ARABIC LETTER SAD	
+خ	x	x		x	kh		Lo	l	consonant	Arabic loan words only	4		haa toɓɓuŋgol	U+062E: ARABIC LETTER KHAH	
+ق	q	χ		χ	k g		Lo	l	consonant	Arabic loan words or Qur'anic names only	4		naafu / gaafu	U+0642: ARABIC LETTER QAF	
+ح	h	h		h	h		Lo		consonant		4		haa baaluul	U+062D: ARABIC LETTER HAH	
+ه	h	h		ḥ	h		Lo	r	consonant	important Qur'anic names only	4		haa peetel	U+0647: ARABIC LETTER HEH	
+م	m	m		m	m		Lo		consonant		4		miimi	U+0645: ARABIC LETTER MEEM	
+ن	n	n		n	n		Lo		consonant		4		nuunu	U+0646: ARABIC LETTER NOON	
+ࢩ	N	ɲ		ɲ	ny		Lo		consonant		4		nya	U+08A9: ARABIC LETTER YEH WITH TWO DOTS BELOW AND DOT ABOVE	✓
+و	w	w		w	w		Lo		consonant		2		waawu	U+0648: ARABIC LETTER WAW	
+ر	r	ɾ		ɾ	r		Lo		consonant		2		arre	U+0631: ARABIC LETTER REH	
+رّ	r	r		r	rr				consonant		2			U+0631 U+0651: ARABIC LETTER REH, SHADDA	
+ل	l	l		l	l		Lo		consonant		4		laamu	U+0644: ARABIC LETTER LAM	
+ي	y	j		y	y		Lo		consonant		4		yah	U+064A: ARABIC LETTER YEH	
+															
+ِ	i	i		i	i		Mn​		vowel				maasi les	U+0650: ARABIC KASRA	
+ِي	I	iː		iy	ii				long vowel				maasi les ii	U+0650 U+064A: ARABIC KASRA, LETTER YEH	
+ُ	u	u		u	u		Mn​		vowel				turnɗe / too turi	U+064F: ARABIC DAMMA	
+ُو	U	uː		uw	uu				long vowel				turnɗe uu	U+064F U+0648: ARABIC DAMMA, LETTER WAW	
+ٜ	e	e		e	e		Mn​		vowel				yamaleere	U+065C: ARABIC VOWEL SIGN DOT BELOW	
+ٜىٰ	E	eː		ē	ee				long vowel				yamaleere bee aliifi bodewol	U+065C U+0649 U+0670: ARABIC VOWEL SIGN DOT BELOW, LETTER ALEF MAKSURA, LETTER SUPERSCRIPT ALEF	
+ٜى	E	eː		ē	ee				long vowel	final form only				U+065C U+0649: ARABIC VOWEL SIGN DOT BELOW, LETTER ALEF MAKSURA	
+ى	E	eː		ẹ			Lo		long vowel					U+0649: ARABIC LETTER ALEF MAKSURA	
+ٛ	o	o		o	o		Mn​		vowel				turnɗe o / esoode	U+065B: ARABIC VOWEL SIGN INVERTED SMALL V ABOVE	
+ٛو	O	oː		ow	oo				long vowel				turnɗe oo	U+065B U+0648: ARABIC VOWEL SIGN INVERTED SMALL V ABOVE, LETTER WAW	
+ٰ	[			̍			Mn		vowel	used as part of ٜىٰ				U+0670: ARABIC LETTER SUPERSCRIPT ALEF	
+ا		–		ā			Lo		vowel lengthener				aliifi	U+0627: ARABIC LETTER ALEF	
+َ	a	a		a	a		Mn​		vowel				maasi dow	U+064E: ARABIC FATHA	
+َا	A	aː		aā	a				vowel support				aliifi balewol / bodeewol	U+064E U+0627: ARABIC FATHA, LETTER ALEF	
+															
+ْ	*			͞			Mn		vowel absence marker				howde / hownde	U+0652: ARABIC SUKUN	
+ّ	&			˖			Mn		gemination mark				siddere	U+0651: ARABIC SHADDA	
+															
+-	-			-	-		Pd		hyphen					U+2010: HYPHEN	
+‑	-			‑	‑		Pd		non-breaking hyphen					U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd		en dash					U+2013: EN DASH	
+—	-			—	—		Pd		em dash					U+2014: EM DASH	
+،	,			,	,		Po		comma					U+060C: ARABIC COMMA	
+؛	;			;			Po		semicolon					U+061B: ARABIC SEMICOLON	
+:	:			:	:		Po		colon					U+003A: COLON	
+!	!			!	!		Po		exclamation mark					U+0021: EXCLAMATION MARK	
+؟	?			?	?		Po		question mark					U+061F: ARABIC QUESTION MARK	
+⸫	.			.̣	.		Po		full stop					U+2E2B: ONE DOT OVER TWO DOTS PUNCTUATION	
+.	.			.	.		Po		full stop					U+002E: FULL STOP	
+…	.			…	…		Po		ellipsis					U+2026: HORIZONTAL ELLIPSIS	
+(	(			(	(		Ps		parenthesis					U+0028: LEFT PARENTHESIS	
+[	(			[	[		Pe		bracket					U+005B: LEFT SQUARE BRACKET	
+)	)			)	)		Ps		parenthesis					U+0029: RIGHT PARENTHESIS	
+]	)			]	]		Pe		bracket					U+005D: RIGHT SQUARE BRACKET	
+%	%			%			Po		percentage mark					U+0025: PERCENT SIGN	
+‰	%			‰			Po	u	per mille mark					U+2030: PER MILLE SIGN	
+«	<			«			Pi		quotation mark					U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	✓
+‹	<			‹			Pi	u	quotation mark					U+2018: LEFT SINGLE QUOTATION MARK	
+“	<			“	“		Pi	u	quotation mark					U+201C: LEFT DOUBLE QUOTATION MARK	
+‘	<			\‘	\‘		Pi	u	quotation mark					U+2018: LEFT SINGLE QUOTATION MARK	
+»	>			»			Pf		quotation mark					U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	✓
+›	>			›			Pf	u	quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	
+”	>			”	”		Pf	u	quotation mark					U+201D: RIGHT DOUBLE QUOTATION MARK	
+’	>			\’	\’		Pf	u	quotation mark					U+2019: RIGHT SINGLE QUOTATION MARK	
+١	±	1		1̣	1		Nd		digit					U+0661: ARABIC-INDIC DIGIT ONE	
+٢	±	2		2̣	2		Nd		digit					U+0662: ARABIC-INDIC DIGIT TWO	
+٣	±	3		3̣	3		Nd		digit					U+0663: ARABIC-INDIC DIGIT THREE	
+٤	±	4		4̣	4		Nd		digit					U+0664: ARABIC-INDIC DIGIT FOUR	
+٥	±	5		5̣	5		Nd		digit					U+0665: ARABIC-INDIC DIGIT FIVE	
+٦	±	6		6̣	6		Nd		digit					U+0666: ARABIC-INDIC DIGIT SIX	
+٧	±	7		7̣	7		Nd		digit					U+0667: ARABIC-INDIC DIGIT SEVEN	
+٨	±	8		8̣	8		Nd		digit					U+0668: ARABIC-INDIC DIGIT EIGHT	
+٩	±	9		9̣	9		Nd		digit					U+0669: ARABIC-INDIC DIGIT NINE	
+٠	±	0		0̣	0		Nd		digit					U+0660: ARABIC-INDIC DIGIT ZERO	
+															
+\u200C	=			ₓ			Cf		zero-width non-joiner					U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=			₊			Cf		zero-width joiner					U+200D: ZERO WIDTH JOINER	
+\u2067	=			ʳˡⁱ			Cf		rtl isolate					U+2067: RIGHT-TO-LEFT ISOLATE	
+\u2066	=			ˡʳⁱ			Cf		ltr isolate					U+2066: LEFT-TO-RIGHT ISOLATE	
+\u2068	=			ᶠˢⁱ			Cf		first-strong isolate					U+2068: FIRST STRONG ISOLATE	
+\u2069	=			ᵖᵈⁱ			Cf		pop direction isolate					U+2069: POP DIRECTIONAL ISOLATE	
+\u202B	=			ʳˡᵉ			Cf		rtl embed					U+202B: RIGHT-TO-LEFT EMBEDDING	
+\u202A	=			ˡʳᵉ			Cf		ltr embed					U+202A: LEFT-TO-RIGHT EMBEDDING	
+\u202C	=			ᵖᵈᶠ			Cf		pop direction					U+202C: POP DIRECTIONAL FORMATTING	
+\u200F	=			ʳˡᵐ			Cf		rtl mark					U+200F: RIGHT-TO-LEFT MARK	
+\u200E	=			ˡʳᵐ			Cf		ltr mark					U+200E: LEFT-TO-RIGHT MARK	
+\u034F	=			ᶜᵍʲ			Mn		combining grapheme joiner					U+034F: COMBINING GRAPHEME JOINER	
+\u061C	=			ᵃˡᵐ			Cf		arabic letter mark					U+061C: ARABIC LETTER MARK	
+◌				◌	◌			u						U+25CC: DOTTED CIRCLE	
+`															
+															
+latinPanel = 'aː d͡ʒ eː ẹ ḥ iː oː t͡ʃ uː ā ē ŋ ƴ ɓ ɗ ɡ ɲ ɾ ʃ ʔ ʔʲ θ χ ᵐb ᵑɡ ᶮd͡ʒ ḍ ṣ ’ ⁿd'															
+															
+var cols = {															
+key: 1,															
+ipaLoc: 2,															
+ipaPlus: 3,															
+transLoc: 4,															
+transcription: 5,															
+kbd: 6,															
+class: 7,															
+status: 8,															
+typeLoc: 9,															
+statusLoc: 10,															
+															
+shape: 11,															
+															
+nnameLoc: 12,															
+nameLoc: 13,															
+ucsName: 14,															
+block: 15,															
+															
+//othertranscriptions: [[5, 'LOC']]															
+}															
