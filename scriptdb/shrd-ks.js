6d1933a7e54f8bb2c97a7bb64547776934a6830f
r12a <ishida@w3.org>
2025-02-15 09:18:15 +0000

    + shrd-ks

---
+++ scriptdb/shrd-ks.js
@@ -0,0 +1,178 @@
+var spreadsheet = `shrd-ks	key	ipa	ipa+	ipaOther	translit	transc	kbd	class	status	type	usage	dvowel	ivowel	native name	name transcription	ucs name	map	block	0		
+𑆥	p	p	ɐ		p	p		Lo		consonant						U+111A5: SHARADA LETTER PA					
+𑆦	P	pʱ	ɐ		pʱ	ph		Lo		consonant						U+111A6: SHARADA LETTER PHA					
+𑆧	b	b	ɐ		b	b		Lo		consonant						U+111A7: SHARADA LETTER BA					
+𑆨	B	bʱ	ɐ		bʱ	bh		Lo		consonant						U+111A8: SHARADA LETTER BHA					
+𑆠	t	t	ɐ		t	t		Lo		consonant						U+111A0: SHARADA LETTER TA					
+𑆡	T	tʱ	ɐ		tʱ	th		Lo		consonant						U+111A1: SHARADA LETTER THA					
+𑆢	d	d	ɐ		d	d		Lo		consonant						U+111A2: SHARADA LETTER DA					
+𑆣	D	dʱ	ɐ		dʱ	dh		Lo		consonant						U+111A3: SHARADA LETTER DHA					
+𑆛	t	ʈ	ɐ		ʈ	ṭ		Lo		consonant						U+1119B: SHARADA LETTER TTA					
+𑆜	T	ʈʰ	ɐ		ʈʰ	ṭh		Lo		consonant						U+1119C: SHARADA LETTER TTHA					
+𑆝	d	ɖ	ɐ		ɖ	ḍ		Lo		consonant						U+1119D: SHARADA LETTER DDA					
+𑆞	D	ɖʱ	ɐ		ɖʱ	ḍh		Lo		consonant						U+1119E: SHARADA LETTER DDHA					
+𑆖	c	t͡ɕ	ɐ		t͡ɕ	c		Lo		consonant						U+11196: SHARADA LETTER CA					
+𑆗	C	t͡ɕʰ	ɐ		t͡ɕʰ	ch		Lo		consonant						U+11197: SHARADA LETTER CHA					
+𑆘	j	d͡ʑ	ɐ		d͡ʑ	j		Lo		consonant						U+11198: SHARADA LETTER JA					
+𑆙	J	d͡ʑʰ	ɐ		d͡ʑʰ	jh		Lo		consonant						U+11199: SHARADA LETTER JHA					
+𑆑	k	k	ɐ		k	k		Lo		consonant					kov ka	U+11191: SHARADA LETTER KA					
+𑆒	K	kʰ	ɐ		kʰ	kh		Lo		consonant					khoni kha	U+11192: SHARADA LETTER KHA					
+𑆓	g	ɡ	ɐ		ɡ	g		Lo		consonant						U+11193: SHARADA LETTER GA					
+𑆔	G	ɡʰ	ɐ		ɡʰ	gh		Lo		consonant						U+11194: SHARADA LETTER GHA					
+𑇃	f	ɸ			ᶠ			Lo		stacked consonant						U+111C3: SHARADA SIGN UPADHMANIYA					
+𑆱	s	s	ɐ		s	s		Lo		consonant						U+111B1: SHARADA LETTER SA					
+𑆯	s	ɕ	ɐ		ɕ	ś		Lo		consonant						U+111AF: SHARADA LETTER SHA					
+𑆰	s	ʂ	ɐ		ʂ	ṣ		Lo		consonant						U+111B0: SHARADA LETTER SSA					
+𑇂	x	x			ˣ			Lo		stacked consonant						U+111C2: SHARADA SIGN JIHVAMULIYA					
+𑆲	h	ɦ	ɐ		ɦ	h		Lo		consonant						U+111B2: SHARADA LETTER HA					
+𑆩	m	m	ɐ		m	m		Lo		consonant						U+111A9: SHARADA LETTER MA					
+𑆤	n	n	ɐ		n	n		Lo		consonant						U+111A4: SHARADA LETTER NA					
+𑆚	n	ɲ	ɐ		ɲ	ñ		Lo		consonant						U+1119A: SHARADA LETTER NYA					
+𑆟	n	ɳ	ɐ		ɳ	ṇ		Lo		consonant						U+1119F: SHARADA LETTER NNA					
+𑆕	N	ŋ	ɐ		ŋ	ṅ		Lo		consonant						U+11195: SHARADA LETTER NGA					
+𑆮	w	ʋ	ɐ		ʋ	v		Lo		consonant						U+111AE: SHARADA LETTER VA					
+𑆫	r	r ɽ	ɐ		r	r		Lo		consonant						U+111AB: SHARADA LETTER RA					
+𑆬	l	l	ɐ		l	l		Lo		consonant						U+111AC: SHARADA LETTER LA					
+𑆭	l	ɭ	ɐ		ɭ	ḷ		Lo		consonant						U+111AD: SHARADA LETTER LLA					
+𑆪	y	j	ɐ		j	y		Lo		consonant						U+111AA: SHARADA LETTER YA					
+																					
+𑆴	i	ɪ			ɪ	i		Mc​		dependent vowel			𑆅			U+111B4: SHARADA VOWEL SIGN I					
+𑆅	I	ɪ			ɪ	i		Lo		independent vowel		𑆴				U+11185: SHARADA LETTER I					
+𑆵	i	iː			iː	ī		Mc​		dependent vowel			𑆆			U+111B5: SHARADA VOWEL SIGN II					
+𑆆	I	iː			iː	ī		Lo		independent vowel		𑆵				U+11186: SHARADA LETTER II					
+\u{11B62}	i	ɨ			ɨ	ü		Mn​		dependent vowel	Used for extra vowels for Kashmiri.		𑆄\u{11B62}			U+11B62: SHARADA VOWEL SIGN UE					
+𑆄\u{11B62}	I	ɨ								independent vowel	Used for extra vowels for Kashmiri.	\u{11B62}				U+11184 U+11B62: SHARADA LETTER AA, SHARADA VOWEL SIGN UE					
+\u{11B63}	i	ɨː			ɨ̣	ü		Mn​		dependent vowel	Used for extra vowels for Kashmiri.		\u{11B63}			U+11B63: SHARADA VOWEL SIGN UUE					
+𑆄\u{11B63}	I	ɨː								independent vowel	Used for extra vowels for Kashmiri.	\u{11B63}				U+11184 U+11B623: SHARADA LETTER AA, SHARADA VOWEL SIGN UUE					
+𑆶	u	ʊ			ʊ	u		Mn​		dependent vowel			𑆇			U+111B6: SHARADA VOWEL SIGN U					
+𑆇	U	ʊ			ʊ	u		Lo		independent vowel		𑆶				U+11187: SHARADA LETTER U					
+𑆷	u	uː			uː	ū		Mn​		dependent vowel			𑆈			U+111B7: SHARADA VOWEL SIGN UU					
+𑆈	U	uː			uː	ū		Lo		independent vowel		𑆷				U+11188: SHARADA LETTER UU					
+\u{11B64}	e	e			e	e		Mn​		dependent vowel	Used for extra vowels for Kashmiri.		𑆍\u{11B64}			U+11B64: SHARADA VOWEL SIGN SHORT E					
+𑆍\u{11B64}	E	e								independent vowel	Used for extra vowels for Kashmiri.	\u{11B64}				U+1118D U+11B64: SHARADA LETTER AA, SHARADA VOWEL SIGN SHORT E					
+𑆼	e	eː			eː	ē		Mn​		dependent vowel			𑆍			U+111BC: SHARADA VOWEL SIGN E					
+𑆍	E	eː			eː	ē		Lo		independent vowel		𑆼				U+1118D: SHARADA LETTER E					
+\u{11B65}	o	o			o	o		Mc		dependent vowel	Used for extra vowels for Kashmiri.		𑆏\u{11B65}			U+11B65: SHARADA VOWEL SIGN SHORT O					
+𑆏\u{11B65}	O	o								independent vowel	Used for extra vowels for Kashmiri.	\u{11B65}				U+1118F U+11B65: SHARADA LETTER O, SHARADA VOWEL SIGN SHORT O					
+𑆾	o	oː			oː	ō		Mn​		dependent vowel			𑆏			U+111BE: SHARADA VOWEL SIGN O					
+𑆏	O	oː			oː	ō		Lo		independent vowel		𑆾				U+1118F: SHARADA LETTER O					
+\u{11B60}	q	ə			ə	ö		Mn​		dependent vowel	Used for extra vowels for Kashmiri.		𑆃\u{11B60}			U+11B60: SHARADA VOWEL SIGN OE					
+𑆃\u{11B60}	Q	ə								independent vowel	Used for extra vowels for Kashmiri.	\u{11B60}				U+11183 U+11B60: SHARADA LETTER A, SHARADA VOWEL SIGN OE					
+\u{11B61}	q	əː			ə̄	ȫ		Mc		dependent vowel	Used for extra vowels for Kashmiri.		𑆄\u{11B61}			U+11B61: SHARADA VOWEL SIGN OOE					
+𑆄\u{11B61}	Q	əː								independent vowel	Used for extra vowels for Kashmiri.	\u{11B61}				U+11184 U+11B601: SHARADA LETTER AA, SHARADA VOWEL SIGN OOE					
+\u{11B66}	o	ɔ			ɔ̣			Mn​		dependent vowel	Used for extra vowels for Kashmiri.		𑆃\u{11B66}			U+11B66: SHARADA VOWEL SIGN CANDRA E					
+𑆃\u{11B66}	O	ɔ								independent vowel	Used for extra vowels for Kashmiri.	\u{11B66}				U+11183 U+11B66: SHARADA LETTER A, SHARADA VOWEL SIGN CANDRA E					
+\u{11B67}	o	ɔː						Mc		dependent vowel	Used for extra vowels for Kashmiri.		𑆃\u{11B67}			U+11B67: SHARADA VOWEL SIGN CANDRA O					
+𑆃\u{11B67}	O	ɔː								independent vowel	Used for extra vowels for Kashmiri.	\u{11B67}				U+11183 U+11B67: SHARADA LETTER A, SHARADA VOWEL SIGN CANDRA O					
+𑆃	A	ɐ			ɐ	a		Lo		independent vowel						U+11183: SHARADA LETTER A					
+𑆳	a	ɐ			ɐ	a		Mc​		dependent vowel			𑆄			U+111B3: SHARADA VOWEL SIGN AA					
+𑆄	A	aː			aː	ā		Lo		independent vowel		𑆳				U+11184: SHARADA LETTER AA					
+𑇎	^				^			Mc​		supplementary vowel sign						U+111CE: SHARADA VOWEL SIGN PRISHTHAMATRA E					
+																					
+𑆽	a	aːi̯		ai ɐi ɛi	aʲ	ai		Mn​		diphthong			𑆎			U+111BD: SHARADA VOWEL SIGN AI					
+𑆎	A	aːi̯		ai ɐi ɛi	aʲ	ai		Lo		diphthong		𑆽				U+1118E: SHARADA LETTER AI					
+𑆿	a	aːu̯		au ɐu ɔu	aʷ	au		Mc​		diphthong			𑆐			U+111BF: SHARADA VOWEL SIGN AU					
+𑆐	A	aːu̯		au ɐu ɔu	aʷ	au		Lo		diphthong		𑆿				U+11190: SHARADA LETTER AU					
+																					
+𑆸	R	r̩			r̩	r̥		Mn​		vocalic						U+111B8: SHARADA VOWEL SIGN VOCALIC R					
+𑆉	R	r̩			r̩	r̥		Lo		vocalic						U+11189: SHARADA LETTER VOCALIC R					
+𑆹	R	r̩ː			r̩ː	r̥̄		Mn​		vocalic						U+111B9: SHARADA VOWEL SIGN VOCALIC RR					
+𑆊	R	r̩ː			r̩ː	r̥̄		Lo		vocalic						U+1118A: SHARADA LETTER VOCALIC RR					
+𑆺	L	l̩			l̩	l̥		Mn​		vocalic						U+111BA: SHARADA VOWEL SIGN VOCALIC L					
+𑆋	L	l̩			l̩	l̥		Lo		vocalic						U+1118B: SHARADA LETTER VOCALIC L					
+𑆻	L	l̩ː			l̩ː	l̥̄		Mn​		vocalic						U+111BB: SHARADA VOWEL SIGN VOCALIC LL					
+𑆌	L	l̩ː			l̩ː	l̥̄		Lo		vocalic						U+1118C: SHARADA LETTER VOCALIC LL					
+																					
+																					
+𑆀	[¶	◌̃			˜			Mn​		nasalisation						U+11180: SHARADA SIGN CANDRABINDU					
+𑇏	[	◌̃						Mn​		nasalisation	Alternative shape.					U+111CF: SHARADA SIGN INVERTED CANDRABINDU					
+𑆁	n	n m			ᶰ			Mn​		nasal coda						U+11181: SHARADA SIGN ANUSVARA					
+𑆂	h	h			ᵸ			Mc​		h coda						U+11182: SHARADA SIGN VISARGA					
+																					
+																					
+																					
+𑇐	±				0̣	0		Nd		digit				𑆤𑆾𑇌𑆡	noth	U+111D0: SHARADA DIGIT ZERO					
+𑇑	±				1̣	1		Nd		digit				𑆃𑆒	akh	U+111D1: SHARADA DIGIT ONE					
+𑇒	±				2̣	2		Nd		digit				𑆘𑇊𑆶𑇌	zû	U+111D2: SHARADA DIGIT TWO					
+𑇓	±				3̣	3		Nd		digit				𑆠𑇀𑆫𑆼𑇌	tré	U+111D3: SHARADA DIGIT THREE					
+𑇔	±				4̣	4		Nd		digit				𑆘𑇊𑆾𑆫	zor	U+111D4: SHARADA DIGIT FOUR					
+𑇕	±				5̣	5		Nd		digit				𑆥𑆳𑆀𑆗𑇊	pàtsh	U+111D5: SHARADA DIGIT FIVE					
+𑇖	±				6̣	6		Nd		digit				𑆯𑆾𑇌	śé	U+111D6: SHARADA DIGIT SIX					
+𑇗	±				7̣	7		Nd		digit				𑆱𑆡	sath	U+111D7: SHARADA DIGIT SEVEN					
+𑇘	±				8̣	8		Nd		digit				𑆐𑆜	auṭh	U+111D8: SHARADA DIGIT EIGHT					
+𑇙	±				9̣	9		Nd		digit				𑆤𑆮	nav	U+111D9: SHARADA DIGIT NINE					
+																					
+																					
+																					
+𑇅	|				|	.		Po		section divider						U+111C5: SHARADA DANDA					
+𑇆	|				¶			Po		section divider						U+111C6: SHARADA DOUBLE DANDA					
+𑇞	@				⟧			Po		section end						U+111DE: SHARADA SECTION MARK-1					
+𑇟	@				⟩			Po		section end						U+111DF: SHARADA SECTION MARK-2					
+𑇚	@				§ᵃ			Lo		invocation sign						U+111DA: SHARADA EKAM					
+𑇛	@				⟦			Po		opening head mark						U+111DB: SHARADA SIGN SIDDHAM					
+𑇍	@				°			Po		sutra end mark						U+111CD: SHARADA SUTRA MARK					
+																					
+𑇄					{om}			Lo	d	OM symbol	Use the decomposed sequence.					U+111C4: SHARADA OM					
+𑆏𑆀										OM symbol						U+1118F U+11180: SHARADA LETTER O, SIGN CANDRABINDU					
+																					
+𑇀	*				͞			Mc		vowel-killer, virama						U+111C0: SHARADA SIGN VIRAMA					
+																					
+𑇁								Lo		avagraha sandhi mark						U+111C1: SHARADA SIGN AVAGRAHA					
+𑇉								Mn​		sandhi mark						U+111C9: SHARADA SANDHI MARK					
+																					
+𑇇	+				¨			Po		abbreviation marker						U+111C7: SHARADA ABBREVIATION SIGN					
+𑇈	]				•			Po		word separator						U+111C8: SHARADA SEPARATOR					
+																					
+𑇊	/				ˑ			Mn​		nukta	Used to write Kashmiri sounds.					U+111CA: SHARADA SIGN NUKTA					
+𑇌	[				˘			Mn​		vowel shortener	Used to write Kashmiri sounds.					U+111CC: SHARADA EXTRA SHORT VOWEL MARK					
+𑇋	[				ˈ			Mn​		regressive metaphony	Used to write Kashmiri sounds.					U+111CB: SHARADA VOWEL MODIFIER MARK					
+																					
+𑇜	_				_	_		Lo		headstroke continuation mark						U+111DC: SHARADA HEADSTROKE					
+𑇝	-				-̣			Po		hyphenation marker						U+111DD: SHARADA CONTINUATION SIGN					
+																					
+\u0020	 							Zs		space						U+0020: SPACE					
+																					
+																					
+																					
+																					
+																					
+																					
+																					
+`																					
+																					
+latinPanel = 'aː aːi̯ aːu̯ bʱ dʱ d͡ʑ d͡ʑʰ eː ɡ iː kʰ l̥ l̥̄ l̩ l̩ː oː pʱ r̥ r̥̄ r̩ r̩ː tʱ t͡ɕ t͡ɕʰ uː ñ ö ü ā ē ī ŋ ō ś ū ȫ ɐ ɔ ɔː ɕ ɖ ɖʱ ə əː ɛ ɡ ɡʰ ɦ ɨ ɨː ɪ ɭ ɲ ɳ ɸ ɽ ʂ ʈ ʈʰ ʊ ʋ ḍ ḍh ḷ ṅ ṇ ṣ ṭ ◌̃'																					
+																					
+var cols = {																					
+key: 1,																					
+ipaLoc: 2,																					
+ipaPlus: 3,																					
+ipaOther: 4,																					
+transLoc: 5,																					
+transcription: 6,																					
+kbd: 7,																					
+class: 8,																					
+status: 9,																					
+typeLoc: 10,																					
+statusLoc: 11,																					
+																					
+dvowel: 12,																					
+ivowel: 13,																					
+onset:0,																					
+final:0,																					
+subj:0,																					
+ltone:0,																					
+htone:0,																					
+lc:0,																					
+uc:0,																					
+meaning:0,																					
+shape: 0,																					
+numLoc: 0,																					
+																					
+nnameLoc: 14,																					
+nameLoc: 15,																					
+ucsName: 16,																					
+map: 17,																					
+block: 18,																					
+																					
+//othertranscriptions: [[6, 'Latin']]																					
+}																					
