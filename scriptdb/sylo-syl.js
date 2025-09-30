0c41439c53aab7f2f70e19ea915874d9fd8d5f0b
r12a <ishida@w3.org>
2025-09-30 08:45:10 +0100

    + scriptdb/sylo-syl.js

---
+++ scriptdb/sylo-syl.js
@@ -0,0 +1,161 @@
+var spreadsheet = `khmr-km	key	ipa	ipa+	ipaOther	translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	map	block
+																✓
+ꠛ	b	b	ɔ		b	b		Lo		consonant				U+A81B: SYLOTI NAGRI LETTER BO		
+ꠜ	b	b	ɔ́		ḅ	bh		Lo		high tone				U+A81C: SYLOTI NAGRI LETTER BHO		
+ꠔ	t	t̪	ɔ		t	t		Lo		consonant				U+A814: SYLOTI NAGRI LETTER TO		
+ꠕ	t	t̪	ɔ́		ṭ	th		Lo		high tone				U+A815: SYLOTI NAGRI LETTER THO		
+ꠖ	d	d̪	ɔ		d	d		Lo		consonant				U+A816: SYLOTI NAGRI LETTER DO		
+ꠗ	d	d̪	ɔ́		ḍ	dh		Lo		high tone				U+A817: SYLOTI NAGRI LETTER DHO		
+ꠐ	t	ʈ	ɔ		ʈ	ṭ		Lo		consonant				U+A810: SYLOTI NAGRI LETTER TTO		
+ꠑ	t	ʈ	ɔ́		ʈ̣	ṭh		Lo		high tone				U+A811: SYLOTI NAGRI LETTER TTHO		
+ꠒ	d	ɖ	ɔ		ɖ	ḍ		Lo		consonant				U+A812: SYLOTI NAGRI LETTER DDO		
+ꠓ	d	ɖ	ɔ́		ɖ̣	ḍh		Lo		high tone				U+A813: SYLOTI NAGRI LETTER DDHO		
+ꠉ	g	ɡ	ɔ		g	g		Lo		consonant				U+A809: SYLOTI NAGRI LETTER GO		
+ꠊ	g	ɡ	ɔ́		g̣	gh		Lo		high tone				U+A80A: SYLOTI NAGRI LETTER GHO		
+ꠙ	p	ɸ~f	ɔ	p	ɸ	p		Lo		consonant				U+A819: SYLOTI NAGRI LETTER PO		
+ꠚ	p	ɸ~f	ɔ́		ɸ̣	ph		Lo		high tone				U+A81A: SYLOTI NAGRI LETTER PHO		
+ꠌ	c	s	ɔ	t͡ʃ	s	ch		Lo		consonant				U+A80C: SYLOTI NAGRI LETTER CO		
+ꠍ	c	s	ɔ́	t͡ʃ	ṣ	chh		Lo		high tone				U+A80D: SYLOTI NAGRI LETTER CHO		
+ꠎ	j	z	ɔ	d͡ʒ	z	j		Lo		consonant				U+A80E: SYLOTI NAGRI LETTER JO		
+ꠏ	j	z	ɔ́	d͡ʒ	ẓ	jh		Lo		high tone				U+A80F: SYLOTI NAGRI LETTER JHO		
+ꠡ	s	ʃ	ɔ		ʃ	sh		Lo		consonant				U+A821: SYLOTI NAGRI LETTER SO		
+ꠇ	k	x~k	ɔ	k	x	k		Lo		consonant				U+A807: SYLOTI NAGRI LETTER KO		
+ꠈ	k	x~k	ɔ́	k	x̣	kh		Lo		high tone				U+A808: SYLOTI NAGRI LETTER KHO		
+ꠢ	h	ɦ	ɔ		ɦ	h		Lo		consonant				U+A822: SYLOTI NAGRI LETTER HO		
+ꠝ	m	m	ɔ		m	m		Lo		consonant				U+A81D: SYLOTI NAGRI LETTER MO		
+ꠘ	n	n	ɔ		n	n		Lo		consonant				U+A818: SYLOTI NAGRI LETTER NO		
+ꠋ	n	ŋ			ŋ	ŋ		Mn​		consonant				U+A80B: SYLOTI NAGRI SIGN ANUSVARA		
+ꠞ	r	ɾ	ɔ		ɾ	r		Lo		consonant				U+A81E: SYLOTI NAGRI LETTER RO		
+ꠠ	r	ɽ	ɔ		ɽ	ṛ		Lo		consonant				U+A820: SYLOTI NAGRI LETTER RRO		
+ꠟ	l	l	ɔ		l	l		Lo		consonant				U+A81F: SYLOTI NAGRI LETTER LO		
+																
+ꠤ	i	i			i	i		Mc​		vowel sign				U+A824: SYLOTI NAGRI VOWEL SIGN I		
+ꠁ	I	i			ị	i		Lo		standalone vowel				U+A801: SYLOTI NAGRI LETTER I		
+ꠥ	u	u~ʊ			u	u		Mn​		vowel sign				U+A825: SYLOTI NAGRI VOWEL SIGN U		
+ꠃ	U	u~ʊ			ụ	u		Lo		standalone vowel				U+A803: SYLOTI NAGRI LETTER U		
+ꠦ	e	e~ɛ		æ	e	e		Mn​		vowel sign				U+A826: SYLOTI NAGRI VOWEL SIGN E		
+ꠄ	E	e~ɛ			ẹ	e		Lo		standalone vowel				U+A804: SYLOTI NAGRI LETTER E		
+ꠧ	o	ɔ~o		-ʊ	ɔ	o		Mc​		vowel sign				U+A827: SYLOTI NAGRI VOWEL SIGN OO		
+ꠅ	O	ɔ~o		-ʊ	ɔ̣	o		Lo		standalone vowel				U+A805: SYLOTI NAGRI LETTER O		
+ꠣ	a	a			a	a		Mc​		vowel sign				U+A823: SYLOTI NAGRI VOWEL SIGN A		
+ꠀ	A	a			ạ	a		Lo		standalone vowel				U+A800: SYLOTI NAGRI LETTER A		
+																
+ꠂ	o	ɔi~oi			oⁱ	oi		Mn​		diphthong vowel sign				U+A802: SYLOTI NAGRI SIGN DVISVARA		
+																
+꠆	*				͞			Mn​		vowel-killer				U+A806: SYLOTI NAGRI SIGN HASANTA		
+꠬	*				˟			Mn​		virama				U+A82C: SYLOTI NAGRI SIGN ALTERNATE HASANTA		
+																
+꠨	]							So		poetry mark				U+A828: SYLOTI NAGRI POETRY MARK-1		
+꠩	]							So		poetry mark				U+A829: SYLOTI NAGRI POETRY MARK-2		
+꠪	]							So		poetry mark				U+A82A: SYLOTI NAGRI POETRY MARK-3		
+꠫	]							So		poetry mark				U+A82B: SYLOTI NAGRI POETRY MARK-4		
+⁕	]							Po		poetry mark				U+2055: FLOWER PUNCTUATION MARK		
+																
+																
+// space																
+\u0020	 							Zs		space				U+0020: SPACE		
+\u200B	=				ᶻʷˢᵖ			Cf	?	zero-width space				U+200B: ZERO WIDTH SPACE		
+\u202F	]				ⁿⁿᵇˢᵖ			Zs	?	nnbsp				U+202F NARROW NO-BREAK SPACE		
+// section dividers																
+।	|				|	.		Po		full stop				U+0964: DEVANAGARI DANDA		
+॥	|				¶			Po		section divider				U+0965: DEVANAGARI DOUBLE DANDA		
+// phrase dividers																
+,	,				,	,		Po		comma				U+002C: COMMA		
+;	;				;	;	;;⁏⸵	Po		semicolon				U+003B: SEMICOLON		
+!	!				!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK		
+?	?				?	?		Po		question mark				U+003F: QUESTION MARK		
+// colons & length marks																
+:	:				:	:		Po		colon				U+003A: COLON		
+// period & ellipsis																
+.	.				.	.		Po		full stop				U+002E: FULL STOP		
+…	.				…	…		Po		ellipsis				U+2026: HORIZONTAL ELLIPSIS		
+// quote marks																
+“	<				“	“		Pi		quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK		
+”	>				”	”		Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK		
+‘	<				‘	‘		Pi		quotation mark				U+2018: LEFT SINGLE QUOTATION MARK		
+’	>				’	’		Pf		quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK		
+// brackets																
+(	(				(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS		
+)	)				)	)		Ps		parenthesis				U+0029: RIGHT PARENTHESIS		
+[	(				[	[		Pe		bracket				U+005B: LEFT SQUARE BRACKET		
+]	)				] 	]		Pe		bracket				U+005D: RIGHT SQUARE BRACKET		
+// dashes																
+-	-				-	-		Pd		hyphen				U+2010: HYPHEN		
+–	-				–	–		Pd		en dash				U+2013: EN DASH		
+—	-				—	—		Pd		em dash				U+2014: EM DASH		
+// apostrophe																
+ʼ	\'				ʼ	ʼ		Lm		apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE		
+•	•				•			Po	?	bullet				U+2022: BULLET		
+// formatting characters																
+\u200C	=				ᶻʷⁿʲ			Cf	?	zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER		
+\u200D	=				ᶻʷʲ			Cf	?	zero-width joiner				U+200D: ZERO WIDTH JOINER		
+\u034F	=				ᶜᵍʲ			Mn	?	combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER		
+\u2060	=				ʷʲ			Cf	?	word joiner				U+2060 WORD JOINER		
+// number related																
+%	%				%			Po		percentage mark				U+0025: PERCENT SIGN		
+‰	%				‰			Po	?	per mille mark				U+2030: PER MILLE SIGN		
+৳	#				৳			Sc		rupee mark				U+09F3: BENGALI RUPEE SIGN		
+// digits																
+১	±				1̣	1		Nd		digit				U+09E7: BENGALI DIGIT ONE		
+২	±				2̣	2		Nd		digit				U+09E8: BENGALI DIGIT TWO		
+৩	±				3̣	3		Nd		digit				U+09E9: BENGALI DIGIT THREE		
+৪	±				4̣	4		Nd		digit				U+09EA: BENGALI DIGIT FOUR		
+৫	±				5̣	5		Nd		digit				U+09EB: BENGALI DIGIT FIVE		
+৬	±				6̣	6		Nd		digit				U+09EC: BENGALI DIGIT SIX		
+৭	±				7̣	7		Nd		digit				U+09ED: BENGALI DIGIT SEVEN		
+৮	±				8̣	8		Nd		digit				U+09EE: BENGALI DIGIT EIGHT		
+৯	±				9̣	9		Nd		digit				U+09EF: BENGALI DIGIT NINE		
+০	±				0̣	0		Nd		digit				U+09E6: BENGALI DIGIT ZERO		
+// ascii digits																
+0	0				0	0		Nd	?	digit				U+0030: DIGIT ZERO		
+1	1				1	1		Nd	?	digit				U+0031: DIGIT ONE		
+2	2				2	2		Nd	?	digit				U+0032: DIGIT TWO		
+3	3				3	3		Nd	?	digit				U+0033: DIGIT THREE		
+4	4				4	4		Nd	?	digit				U+0034: DIGIT FOUR		
+5	5				5	5		Nd	?	digit				U+0035: DIGIT FIVE		
+6	6				6	6		Nd	?	digit				U+0036: DIGIT SIX		
+7	7				7	7		Nd	?	digit				U+0037: DIGIT SEVEN		
+8	8				8	8		Nd	?	digit				U+0038: DIGIT EIGHT		
+9	9				9	9		Nd	?	digit				U+0039: DIGIT NINE		
+																
+																
+ⓘ														Inherent vowel marker		
+																
+`																
+																
+latinPanel = ' ́ ɔ ɖ ɛ ɦ ɸ ɽ ɾ ʃ ʈ ʊ ʒ d͡ʒ t͡ʃ ḍ ṛ ṭ ạ ŋ æ'																
+																
+var cols = {																
+key: 1,																
+ipaLoc: 2,																
+ipaPlus: 3,																
+ipaOther: 4,																
+transLoc: 5,																
+transcription: 6,																
+kbd: 7,																
+class: 8,																
+status: 9,																
+typeLoc: 10,																
+statusLoc: 11,																
+																
+dvowel: 0,																
+ivowel: 0,																
+onset:0,																
+final:0,																
+subj:0,																
+ltone:0,																
+htone:0,																
+lc:0,																
+uc:0,																
+meaning:0,																
+shape: 0,																
+numLoc: 0,																
+																
+nnameLoc: 12,																
+nameLoc: 13,																
+ucsName: 14,																
+map: 15,																
+block: 16,																
+																
+//othertranscriptions: [[6, 'Latin']]																
+}																
