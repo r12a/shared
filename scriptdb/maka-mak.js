a9d5683868ae74ad0e799601aef68f0c0e5df07c
r12a <ishida@w3.org>
2025-10-05 17:05:29 +0100

    + Makasar

---
+++ scriptdb/maka-mak.js
@@ -0,0 +1,109 @@
+var spreadsheet = `maka-mak	key	ipa	ipa+	ipaOther	translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	map	block	0		
+𑻣	p	p	a	pʰ	p	p	p	Lo		consonant				U+11EE3: MAKASAR LETTER PA					
+𑻤	b	b	a	ɓ	b	b	b	Lo		consonant				U+11EE4: MAKASAR LETTER BA					
+𑻦	t	t	a	tʰ	t	t	t	Lo		consonant				U+11EE6: MAKASAR LETTER TA					
+𑻧	d	d	a	ɗ	d	d	d	Lo		consonant				U+11EE7: MAKASAR LETTER DA					
+𑻩	c	c	a		c	c	c	Lo		consonant				U+11EE9: MAKASAR LETTER CA					
+𑻪	j	ɟ	a		ɟ	j	j	Lo		consonant				U+11EEA: MAKASAR LETTER JA					
+𑻠	k	k	a	kʰ	k	k	k	Lo		consonant				U+11EE0: MAKASAR LETTER KA					
+𑻡	g	ɡ	a		ɡ	g	g	Lo		consonant				U+11EE1: MAKASAR LETTER GA					
+𑻰	s	s	a		s	s	s	Lo		consonant				U+11EF0: MAKASAR LETTER SA					
+𑻥	m	m	a		m	m	m	Lo		consonant				U+11EE5: MAKASAR LETTER MA					
+𑻨	n	n	a		n	n	n	Lo		consonant				U+11EE8: MAKASAR LETTER NA					
+𑻫	n	ɲ	a		ɲ	ɲ	n	Lo		consonant				U+11EEB: MAKASAR LETTER NYA					
+𑻢	N	ŋ	a		ŋ	ŋ	n	Lo		consonant				U+11EE2: MAKASAR LETTER NGA					
+𑻯	w	w	a		w	w	w	Lo		consonant				U+11EEF: MAKASAR LETTER VA					
+𑻭	r	r	a		r	r	r	Lo		consonant				U+11EED: MAKASAR LETTER RA					
+𑻮	l	l	a		l	l	l	Lo		consonant				U+11EEE: MAKASAR LETTER LA					
+𑻬	y	j	a		j	y	j	Lo		consonant				U+11EEC: MAKASAR LETTER YA					
+																✓	6	u	unused
+𑻳	i	i			i	i	i	Mn​		vowel		𑻦𑻳𑻳𑻱𑻳𑻭𑻦𑻵	tittiʼ i rate	U+11EF3: MAKASAR VOWEL SIGN I					
+𑻴	u	u			u	u	u	Mn​		vowel		𑻦𑻳𑻳𑻱𑻳𑻭𑻯	tittiʼ  i rawa	U+11EF4: MAKASAR VOWEL SIGN U					
+𑻵	e	e	ɛ		e	e	e	Mc​		vowel		𑻱𑻨𑻭𑻳𑻱𑻶𑻮𑻶	anaʼ ri olo	U+11EF5: MAKASAR VOWEL SIGN E					
+𑻶	o	o	ɔ		o	o	o	Mc​		vowel		𑻱𑻨𑻭𑻳𑻤𑻶𑻠𑻶	anaʼ ri boko	U+11EF6: MAKASAR VOWEL SIGN O					
+																			
+𑻱𑻳		i								standalone vowel				U+11EF1 U+11EF3: MAKASAR LETTER A, MAKASAR VOWEL SIGN I					
+𑻱𑻴		u								standalone vowel				U+11EF1 U+11EF4: MAKASAR LETTER A, MAKASAR VOWEL SIGN U					
+𑻱𑻵		e								standalone vowel				U+11EF1 U+11EF5: MAKASAR LETTER A, MAKASAR VOWEL SIGN E					
+𑻱𑻶		o								standalone vowel				U+11EF1 U+11EF6: MAKASAR LETTER A, MAKASAR VOWEL SIGN O					
+𑻱	a	a			a	a	a	Lo		standalone vowel / vowel carrier				U+11EF1: MAKASAR LETTER A					
+																			
+𑻷	.				•		,	Po		phrase delimiter				U+11EF7: MAKASAR PASSIMBANG				o	obsolete
+𑻸	|				¶		.	Po		section delimiter				U+11EF8: MAKASAR END OF SECTION				g	allograph
+																			
+𑻲	&				˖˖		q	Lo		repetition symbol				U+11EF2: MAKASAR ANGKA				l	extension for foreign sounds
+																			
+ت					#			Lo		part of tammat section terminator				U+062A: ARABIC LETTER TEH					
+م					#			Lo		part of tammat section terminator				U+0645: ARABIC LETTER MEEM					
+\u0651					#			Mn​	i	part of tammat section terminator				U+0651: ARABIC SHADDA					
+ت					#			Lo		part of tammat section terminator				U+062A: ARABIC LETTER TEH					
+تمّت										tammat section terminator				U+062A U+0645 U+0651 U+062A: ARABIC LETTER TEH, ARABIC LETTER MEEM, ARABIC SHADDA, ARABIC LETTER TEH					
+																			
+																			
+// space																			
+\u0020	 				 			Zs	?	space				U+0020: SPACE					
+\u200B	=				ᶻʷˢᵖ			Cf	?	zero-width space				U+200B: ZERO WIDTH SPACE					
+\u202F	]				ⁿⁿᵇˢᵖ			Zs	?	nnbsp				U+202F NARROW NO-BREAK SPACE					
+ء	#				¤			Lo	r	calendar indicator				U+0621: ARABIC LETTER HAMZA				﷼	
+\u0600	#				#			Cf	i	number symbol				U+0600: ARABIC NUMBER SIGN					
+\u0601	#				#̣			Cf	i	number symbol				U+0601: ARABIC SIGN SANAH					
+// digits																			
+0	0				0	0		Nd		digit				U+0030: DIGIT ZERO					
+1	1				1	1		Nd		digit				U+0031: DIGIT ONE					
+2	2				2	2		Nd		digit				U+0032: DIGIT TWO					
+3	3				3	3		Nd		digit				U+0033: DIGIT THREE					
+4	4				4	4		Nd		digit				U+0034: DIGIT FOUR					
+5	5				5	5		Nd		digit				U+0035: DIGIT FIVE					
+6	6				6	6		Nd		digit				U+0036: DIGIT SIX					
+7	7				7	7		Nd		digit				U+0037: DIGIT SEVEN					
+8	8				8	8		Nd		digit				U+0038: DIGIT EIGHT					
+9	9				9	9		Nd		digit				U+0039: DIGIT NINE					
+١	±				1̣	1		Nd		digit				U+0661: ARABIC-INDIC DIGIT ONE			١		
+٢	±				2̣	2		Nd		digit				U+0662: ARABIC-INDIC DIGIT TWO			٢		
+٣	±				3̣	3		Nd		digit				U+0663: ARABIC-INDIC DIGIT THREE			٣		
+٤	±				4̣	4		Nd		digit				U+0664: ARABIC-INDIC DIGIT FOUR			٤		
+٥	±				5̣	5		Nd		digit				U+0665: ARABIC-INDIC DIGIT FIVE			٥		
+٦	±				6̣	6		Nd		digit				U+0666: ARABIC-INDIC DIGIT SIX			٦		
+٧	±				7̣	7		Nd		digit				U+0667: ARABIC-INDIC DIGIT SEVEN			٧		
+٨	±				8̣	8		Nd		digit				U+0668: ARABIC-INDIC DIGIT EIGHT			٨		
+٩	±				9̣	9		Nd		digit				U+0669: ARABIC-INDIC DIGIT NINE			٩		
+٠	±				0̣	0		Nd		digit				U+0660: ARABIC-INDIC DIGIT ZERO			٠		
+																			
+`																			
+																			
+latinPanel = 'ɡ ɳ ŋ'																			
+																			
+var cols = {																			
+key: 1,																			
+ipaLoc: 2,																			
+ipaPlus: 3,																			
+ipaOther: 4,																			
+transLoc: 5,																			
+transcription: 6,																			
+kbd: 7,																			
+class: 8,																			
+status: 9,																			
+typeLoc: 10,																			
+statusLoc: 11,																			
+																			
+dvowel: 0,																			
+ivowel: 0,																			
+onset:0,																			
+final:0,																			
+subj:0,																			
+ltone:0,																			
+htone:0,																			
+lc:0,																			
+uc:0,																			
+meaning:0,																			
+shape: 0,																			
+numLoc: 0,																			
+																			
+nnameLoc: 12,																			
+nameLoc: 13,																			
+ucsName: 14,																			
+map: 15,																			
+block: 16,																			
+																			
+//othertranscriptions: [[6, 'Latin']]																			
+}																			
