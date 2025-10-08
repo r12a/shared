dee9c079aaf9bb1d8b5a013209c01cb376367cd0
r12a <ishida@w3.org>
2025-10-08 05:45:40 +0100

    + Tangsa

---
+++ scriptdb/tnsa.js
@@ -0,0 +1,238 @@
+var spreadsheet = `tnsa-nst	key	ipa	ipa+	ipaOther	translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	map	block
+𖪀	i	i˩			i¹	iz		Lo		vowel with low tone				U+16A80: TANGSA LETTER IZ		
+𖪃	i	i˧			i²	ix		Lo		vowel with mid tone				U+16A83: TANGSA LETTER IX		
+𖪁	i	i˦			i³	ic		Lo		vowel with high tone				U+16A81: TANGSA LETTER IC		
+𖪂	i	iˀ			i⁴	iq		Lo		vowel with glottal stop				U+16A82: TANGSA LETTER IQ		
+𖪌	w	ɯ˩			ɯ¹	uiz		Lo		vowel with low tone				U+16A8C: TANGSA LETTER UIZ		
+𖪏	w	ɯ˧			ɯ²	uix		Lo		vowel with mid tone				U+16A8F: TANGSA LETTER UIX		
+𖪍	w	ɯ˦			ɯ³	uic		Lo		vowel with high tone				U+16A8D: TANGSA LETTER UIC		
+𖪎	w	ɯˀ			ɯ⁴	uiq		Lo		vowel with glottal stop				U+16A8E: TANGSA LETTER UIQ		
+𖪄	u	u˩			u¹	uz		Lo		vowel with low tone				U+16A84: TANGSA LETTER UZ		
+𖪇	u	u˧			u²	ux		Lo		vowel with mid tone				U+16A87: TANGSA LETTER UX		
+𖪅	u	u˦			u³	uc		Lo		vowel with high tone				U+16A85: TANGSA LETTER UC		
+𖪆	u	uˀ			u⁴	uq		Lo		vowel with glottal stop				U+16A86: TANGSA LETTER UQ		
+𖩼	e	e˩			e¹	ez		Lo		vowel with low tone				U+16A7C: TANGSA LETTER EZ		
+𖩿	e	e˧			e²	ex		Lo		vowel with mid tone				U+16A7F: TANGSA LETTER EX		
+𖩽	e	e˦			e³	ec		Lo		vowel with high tone				U+16A7D: TANGSA LETTER EC		
+𖩾	e	eˀ			e⁴	eq		Lo		vowel with glottal stop				U+16A7E: TANGSA LETTER EQ		
+𖪔	v	ɤ˩			ɤ¹	uec		Lo		vowel with low tone				U+16A94: TANGSA LETTER UEC		
+𖪗	v	ɤ˧			ɤ²	uex		Lo		vowel with mid tone				U+16A97: TANGSA LETTER UEX		
+𖪑	v	ɤː˧			ɤ̣²	uex		Lo		long vowel with mid tone	Applies prosodic length.			U+16A91: TANGSA LETTER LONG UEX		
+𖪕	v	ɤ˦			ɤ³	uez		Lo		vowel with high tone				U+16A95: TANGSA LETTER UEZ		
+𖪖	v	ɤˀ			ɤ⁴	ueq		Lo		vowel with glottal stop				U+16A96: TANGSA LETTER UEQ		
+𖩰	o	o˩			o¹	oz		Lo		vowel with low tone				U+16A70: TANGSA LETTER OZ		
+𖩳	o	o˧			o²	ox		Lo		vowel with mid tone				U+16A73: TANGSA LETTER OX		
+𖩱	o	o˦			o³	oc		Lo		vowel with high tone				U+16A71: TANGSA LETTER OC		
+𖩲	o	oˀ			o⁴	oq		Lo		vowel with glottal stop				U+16A72: TANGSA LETTER OQ		
+𖩸	E	ə˩			ə¹	vz		Lo		vowel with low tone				U+16A78: TANGSA LETTER VZ		
+𖩻	E	ə˧			ə²	vx		Lo		vowel with mid tone				U+16A7B: TANGSA LETTER VX		
+𖩹	E	ə˦			ə³	vc		Lo		vowel with high tone				U+16A79: TANGSA LETTER VC		
+𖩺	E	əˀ			ə⁴	vq		Lo		vowel with glottal stop				U+16A7A: TANGSA LETTER VQ		
+𖪒	v	ə			ə	v		Lo		short vowel with low tone	Used for toneless prefixes.			U+16A92: TANGSA LETTER SHORT UEZ		
+𖪈	O	ɔ˩			ɔ¹	awz		Lo		vowel with low tone				U+16A88: TANGSA LETTER AWZ		
+𖪋	O	ɔ˧			ɔ²	awx		Lo		vowel with mid tone				U+16A8B: TANGSA LETTER AWX		
+𖪓	O	ɔ̆˧			ɔ²	awx		Lo		short vowel with mid tone				U+16A93: TANGSA LETTER SHORT AWX		
+𖪉	O	ɔ˦			ɔ³	awc		Lo		vowel with high tone				U+16A89: TANGSA LETTER AWC		
+𖪊	O	ɔˀ			ɔ⁴	awq		Lo		vowel with glottal stop				U+16A8A: TANGSA LETTER AWQ		
+𖩴	a	a˩			a¹	az		Lo		vowel with low tone				U+16A74: TANGSA LETTER AZ		
+𖩷	a	a˧			a²	ax		Lo		vowel with mid tone				U+16A77: TANGSA LETTER AX		
+𖩵	a	a˦			a³	ac		Lo		vowel with high tone				U+16A75: TANGSA LETTER AC		
+𖩶	a	aˀ			a⁴	aq		Lo		vowel with glottal stop				U+16A76: TANGSA LETTER AQ		
+																
+𖪘	W	ɯu˩			ɯᵘ¹	uiuz		Lo		diphthong with low tone				U+16A98: TANGSA LETTER UIUZ		
+𖪛	W	ɯu˧			ɯᵘ²	uiux		Lo		diphthong with mid tone				U+16A9B: TANGSA LETTER UIUX		
+𖪙	W	ɯu˦			ɯᵘ³	uiuc		Lo		diphthong with high tone				U+16A99: TANGSA LETTER UIUC		
+𖪚	W	ɯuˀ			ɯᵘ⁴	uiuq		Lo		diphthong with glottal stop				U+16A9A: TANGSA LETTER UIUQ		
+																
+𖪧	p	p			p	p		Lo		consonant				U+16AA7: TANGSA LETTER PA		
+𖪩	P	pʰ			pʰ	ph		Lo		consonant				U+16AA9: TANGSA LETTER PHA		
+𖪪	b	b			b	b		Lo		consonant				U+16AAA: TANGSA LETTER BA		
+𖪰	t	t			t	t		Lo		consonant				U+16AB0: TANGSA LETTER TA		
+𖪯	T	tʰ			tʰ	ht		Lo		consonant				U+16AAF: TANGSA LETTER HTA		
+𖪹	t	t̪			t̪	th		Lo		consonant				U+16AB9: TANGSA LETTER THA		
+𖪸	T	t̪ʰ			t̪ʰ	htt		Lo		consonant				U+16AB8: TANGSA LETTER HTTA		
+𖪶	c	t͡s			t͡s	ts		Lo		consonant				U+16AB6: TANGSA LETTER TSA		
+𖪵	c	t͡ɕ			t͡ɕ	c		Lo		consonant				U+16AB5: TANGSA LETTER CA		
+𖪽	C	t͡ɕʰ			t͡ɕʰ	ch		Lo		consonant				U+16ABD: TANGSA LETTER CHA		
+𖪱	d	d			d	d		Lo		consonant				U+16AB1: TANGSA LETTER DA		
+𖪠	k	k			k	k		Lo		consonant				U+16AA0: TANGSA LETTER KA		
+𖪡	K	kʰ			kʰ	kh		Lo		consonant				U+16AA1: TANGSA LETTER KHA		
+𖪢	g	ɡ			ɡ	g		Lo		consonant				U+16AA2: TANGSA LETTER GA		
+𖪻	f	f			f	f		Lo		consonant				U+16ABB: TANGSA LETTER FA		
+𖪼	D	ð			ð	dh		Lo		consonant				U+16ABC: TANGSA LETTER DHA		
+𖪤	s	s			s	s		Lo		consonant				U+16AA4: TANGSA LETTER SA		
+𖪾	z	z			z	z		Lo		consonant				U+16ABE: TANGSA LETTER ZA		
+𖪴	S	ʃ			ʃ	sh		Lo		consonant				U+16AB4: TANGSA LETTER SHA		
+𖪺	x	x			x	x		Lo		consonant				U+16ABA: TANGSA LETTER XA		
+𖪷	X	ɣ			ɣ	gh		Lo		consonant				U+16AB7: TANGSA LETTER GHA		
+𖪭	h	h			h	h		Lo		consonant				U+16AAD: TANGSA LETTER HA		
+𖪫	m	m			m	m		Lo		consonant				U+16AAB: TANGSA LETTER MA		
+𖪜	m	m̩˩			m̥¹	mx		Lo		syllabic m				U+16A9C: TANGSA LETTER MZ		
+𖪟	m	m̩˧			m̥²	mx		Lo		syllabic m				U+16A9F: TANGSA LETTER MX		
+𖪝	m	m̩˦			m̥³	mc		Lo		syllabic m				U+16A9D: TANGSA LETTER MC		
+𖪞	m	m̩ˀ			m̥⁴	mq		Lo		syllabic m				U+16A9E: TANGSA LETTER MQ		
+𖪬	n	n			n	n		Lo		consonant				U+16AAC: TANGSA LETTER NA		
+𖪳	n	n̪			n̪	nh		Lo		consonant				U+16AB3: TANGSA LETTER NHA		
+𖪨	n	ɲ			ɲ	ny		Lo		consonant				U+16AA8: TANGSA LETTER NYA		
+𖪣	n	ŋ			ŋ	ng		Lo		consonant				U+16AA3: TANGSA LETTER NGA		
+𖪐	n	ŋ			ŋ̣	ng		Lo		coda				U+16A90: TANGSA LETTER FINAL NG		
+𖪦	w	w			w	w		Lo		consonant				U+16AA6: TANGSA LETTER WA		
+𖪲	r	r			r	r		Lo		consonant				U+16AB2: TANGSA LETTER RA		
+𖪮	l	l			l	l		Lo		consonant				U+16AAE: TANGSA LETTER LA		
+𖪥	y	j			j	y		Lo		consonant				U+16AA5: TANGSA LETTER YA		
+																
+𖫁	±				1̣	1		Nd		digit				U+16AC1: TANGSA DIGIT ONE		
+𖫂	±				2̣	2		Nd		digit				U+16AC2: TANGSA DIGIT TWO		
+𖫃	±				3̣	3		Nd		digit				U+16AC3: TANGSA DIGIT THREE		
+𖫄	±				4̣	4		Nd		digit				U+16AC4: TANGSA DIGIT FOUR		
+𖫅	±				5̣	5		Nd		digit				U+16AC5: TANGSA DIGIT FIVE		
+𖫆	±				6̣	6		Nd		digit				U+16AC6: TANGSA DIGIT SIX		
+𖫇	±				7̣	7		Nd		digit				U+16AC7: TANGSA DIGIT SEVEN		
+𖫈	±				8̣	8		Nd		digit				U+16AC8: TANGSA DIGIT EIGHT		
+𖫉	±				9̣	9		Nd		digit				U+16AC9: TANGSA DIGIT NINE		
+𖫀	±				0̣	0		Nd		digit				U+16AC0: TANGSA DIGIT ZERO		
+																
+\u0020	 				 			Zs		space				U+0020: SPACE		
+																
+,	,				,	,		Po		comma				U+002C: COMMA		
+;	;				;	;		Po	?	semicolon				U+003B: SEMICOLON		
+!	!				!	!		Po	?	exclamation mark				U+0021: EXCLAMATION MARK		
+?	?				?	?		Po	?	question mark				U+003F: QUESTION MARK		
+:	:				:	:		Po	?	colon				U+003A: COLON		
+.	.				.	.		Po		full stop				U+002E: FULL STOP		
+…	.				…	…		Po	?	ellipsis				U+2026: HORIZONTAL ELLIPSIS		
+																
+-	-				-	-		Pd		hyphen				U+2010: HYPHEN		
+																
+																
+																
+																
+// space																
+\u200B	=				ᶻʷˢᵖ			Cf	?	zero-width space				U+200B: ZERO WIDTH SPACE		
+\u202F	]				ⁿⁿᵇˢᵖ			Zs	?	nnbsp				U+202F NARROW NO-BREAK SPACE		
+// tone marks & variation selectors																
+	^				¹				?	tone mark	main					
+	^				²				?	tone mark	main					
+	^				³				?	tone mark	main					
+	^				⁴				?	tone mark	main					
+	^				⁵				?	tone mark	main					
+	^				⁶				?	tone mark	main					
+	^				⁷				?	tone mark	main					
+	^				⁸				?	tone mark	main					
+	^				⁹				?	tone mark	main					
+	^				̀						main					
+// section dividers																
+।	|				| |˙ |¯ |¨ |˭	.		Po	?	section divider				U+0964: DEVANAGARI DANDA		
+॥	|				¶			Po	?	section divider				U+0965: DEVANAGARI DOUBLE DANDA		
+// quote marks																
+“	<				“	“		Pi	?	quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK		
+”	>				”	”		Pf	?	quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK		
+‘	<				‘	‘		Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK		
+’	>				’	’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK		
+// brackets																
+(	(				( (˙ (¯ (¨ (˭	(		Ps	?	parenthesis				U+0028: LEFT PARENTHESIS		
+)	)				) )˙ )¯ )¨ )˭	)		Ps	?	parenthesis				U+0029: RIGHT PARENTHESIS		
+[	(				[	[		Pe	?	bracket				U+005B: LEFT SQUARE BRACKET		
+]	)				] 	]		Pe	?	bracket				U+005D: RIGHT SQUARE BRACKET		
+// dashes																
+‑	-				‑	‑		Pd	?	non-breaking hyphen				U+2011: NON-BREAKING HYPHEN		
+–	-				–	–		Pd	?	en dash				U+2013: EN DASH		
+—	-				—	—		Pd	?	em dash				U+2014: EM DASH		
+// apostrophe																
+'	\'				ʼ	ʼ		Lm	?	apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE		
+// secondary viramas																
+	\				ˣ ˣ̣ ˣ̱ ˣ͈ ˣ͇				?							
+// abbrev.																
+	+				¨				?	abbreviation marker						
+// misc diacritics																
+	[				+				?	vowel carrier						
+	[				˘				?	vowel shortener						
+	[				ᵊ				?	vowel retainer						
+	[								?	consonant shifter						
+	[				ˈ				?	superscript alef						
+	[				ʿ				?	hamza above						
+	[				˓				?	hamza below						
+٭	[				*				?	star						
+	[				~				?	maddah						
+	[				″				?	register change						
+	[				°				?	special						
+	[				˙				?	combining dot above						
+	[				ʼ				?	combining hook above						
+	[				ˆ				?	combining circumflex						
+	[				ˉ				?	combining macron						
+	[				̨				?	combining ogonek						
+	[				ᶠ				?	feminine marker						
+// head marks, honorifics, etc																
+	@				⟦				?	opening head mark						
+	@				⟧				?	closing head mark						
+	@				⟨				?	2nd opening head						
+	@				⟩				?	2 closing head						
+	@				§ᵃ				?	honorific						
+	@				§ᵇ				?	honorific						
+	@				§ᶜ				?	honorific						
+	@				°				?	extra						
+// other																
+′	\'				′	′		Po	?	prime				U+2032: PRIME		
+″	"				″	″		Po	?	double prime				U+2033: DOUBLE PRIME		
+•	•				•			Po	?	bullet				U+2022: BULLET		
+// number related																
+	#				ᵗ				?	thousands separator						
+	#				ᵈ				?	decimal separator						
+	#				/				?	date separator						
+	#				¤				?	currency symbol						
+	#				¢				?							
+	#				# #̣ #̱ #͈ #͇				?	number symbol						
+	#				¼				?	quarter						
+	#				½				?	half						
+	#				¾				?	three-quarters						
+%	%				%			Po	?	percentage mark				U+0025: PERCENT SIGN		
+‰	%				‰			Po	?	per mille mark				U+2030: PER MILLE SIGN		
+// ascii digits																
+0	0				0	0		Nd	?	digit				U+0030: DIGIT ZERO		
+1	1				1	1		Nd	?	digit				U+0031: DIGIT ONE		
+2	2				2	2		Nd	?	digit				U+0032: DIGIT TWO		
+3	3				3	3		Nd	?	digit				U+0033: DIGIT THREE		
+4	4				4	4		Nd	?	digit				U+0034: DIGIT FOUR		
+5	5				5	5		Nd	?	digit				U+0035: DIGIT FIVE		
+6	6				6	6		Nd	?	digit				U+0036: DIGIT SIX		
+7	7				7	7		Nd	?	digit				U+0037: DIGIT SEVEN		
+8	8				8	8		Nd	?	digit				U+0038: DIGIT EIGHT		
+9	9				9	9		Nd	?	digit				U+0039: DIGIT NINE		
+`																
+																
+latinPanel = '˩ ˧ ˦ ˀ ɯ ɤ ə ɔ ʰ t̪ t͡s t͡ɕ ɡ ð ʃ ɣ m̩ n̪ ɲ ŋ ¹ ² ³ ⁴'																
+																
+var cols = {																
+key: 1,																
+ipaLoc: 2,																
+ipaPlus: 3,																
+ipaOther: 4,																
+transLoc: 5,																
+transcription: 6,																
+kbd: 7,																
+class: 8,																
+status: 9,																
+typeLoc: 10,																
+statusLoc: 11,																
+																
+dvowel: 0,																
+ivowel: 0,																
+onset:0,																
+final:0,																
+subj:0,																
+ltone:0,																
+htone:0,																
+lc:0,																
+uc:0,																
+meaning:0,																
+shape: 0,																
+numLoc: 0,																
+																
+nnameLoc: 12,																
+nameLoc: 13,																
+ucsName: 14,																
+map: 15,																
+block: 16,																
+																
+//othertranscriptions: [[6, 'Latin']]																
+}																
