74667f372ca808042abe4982479b7defd16665d4
r12a <ishida@w3.org>
2025-10-10 06:04:40 +0100

    + Gunjala Gondi

---
+++ scriptdb/gong.js
@@ -0,0 +1,231 @@
+var spreadsheet = `gong-wsg	key	ipa	ipa+	ipaOther	translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	map	block
+																
+𑶅	p	p	a		p	p		Lo		consonant				U+11D85: GUNJALA GONDI LETTER PA		
+𑶆	P	pʰ	a		pʰ	ph		Lo		consonant				U+11D86: GUNJALA GONDI LETTER PHA		
+𑵮	b	b	a		b	b		Lo		consonant				U+11D6E: GUNJALA GONDI LETTER BA		
+𑵯	B	bʰ	a		bʰ	bh		Lo		consonant				U+11D6F: GUNJALA GONDI LETTER BHA		
+𑵳	t	t	a		t	t		Lo		consonant				U+11D73: GUNJALA GONDI LETTER TA		
+𑵴	T	tʰ	a		tʰ	th		Lo		consonant				U+11D74: GUNJALA GONDI LETTER THA		
+𑵻	c	t͡ʃ	a		ʧ	c		Lo		consonant				U+11D7B: GUNJALA GONDI LETTER CA		
+𑵼	C	t͡ʃʰ	a		ʧʰ	ch		Lo		consonant				U+11D7C: GUNJALA GONDI LETTER CHA		
+𑵽	t	ʈ	a		ʈ	ṭ		Lo		consonant				U+11D7D: GUNJALA GONDI LETTER TTA		
+𑵾	T	ʈʰ	a		ʈʰ	ṭh		Lo		consonant				U+11D7E: GUNJALA GONDI LETTER TTHA		
+𑵸	d	d	a		d	d		Lo		consonant				U+11D78: GUNJALA GONDI LETTER DA		
+𑵹	D	dʰ	a		dʰ	dh		Lo		consonant				U+11D79: GUNJALA GONDI LETTER DHA		
+𑶀	j	d͡ʒ	a		ʤ	j		Lo		consonant				U+11D80: GUNJALA GONDI LETTER JA		
+𑶁	J	d͡ʒʰ	a		ʤʰ	jh		Lo		consonant				U+11D81: GUNJALA GONDI LETTER JHA		
+𑶂	d	ɖ	a		ɖ	ḍ‍		Lo		consonant				U+11D82: GUNJALA GONDI LETTER DDA		
+𑶃	D	ɖʰ	a		ɖʰ	ḍ‍h		Lo		consonant				U+11D83: GUNJALA GONDI LETTER DDHA		
+𑵱	k	k	a		k	k		Lo		consonant				U+11D71: GUNJALA GONDI LETTER KA		
+𑵲	K	kʰ	a		kʰ	kh		Lo		consonant				U+11D72: GUNJALA GONDI LETTER KHA		
+𑵶	g	ɡ	a		ɡ	g		Lo		consonant				U+11D76: GUNJALA GONDI LETTER GA		
+𑵷	G	ɡʰ	a		ɡʰ	gh		Lo		consonant				U+11D77: GUNJALA GONDI LETTER GHA		
+𑶉	s	s	a	ʂ	s	s		Lo		consonant				U+11D89: GUNJALA GONDI LETTER SA		
+𑶇	h	h	a		h	h		Lo		consonant				U+11D87: GUNJALA GONDI LETTER HA		
+𑶖		h			ʰ	h		Mc​						U+11D96: GUNJALA GONDI SIGN VISARGA		
+𑵰	m	m	a		m	m		Lo		consonant				U+11D70: GUNJALA GONDI LETTER MA		
+𑵺	n	n	a	ɲ ɳ	n	n		Lo		consonant				U+11D7A: GUNJALA GONDI LETTER NA		
+𑶄	N	ŋ	a		ŋ	ṅ		Lo		consonant				U+11D84: GUNJALA GONDI LETTER NGA		
+𑶕	N	ŋ		ɲ ɳ	˜	̃		Mn		nasal coda				U+11D95: GUNJALA GONDI SIGN ANUSVARA		
+𑵭	v	ʋ	a	w	v	v		Lo		consonant				U+11D6D: GUNJALA GONDI LETTER VA		
+𑶈	r	r	a	ɾ	r	r		Lo		consonant				U+11D88: GUNJALA GONDI LETTER RA		
+𑵵	l	l	a		l	l		Lo		consonant				U+11D75: GUNJALA GONDI LETTER LA		
+𑵿		ɭ	a		ɭ	ḷ		Lo		consonant				U+11D7F: GUNJALA GONDI LETTER LLA		
+𑵬	j	j	a		j	y		Lo		consonant				U+11D6C: GUNJALA GONDI LETTER YA		
+																
+𑶋	i	i			i	i		Mc​		vowel sign				U+11D8B: GUNJALA GONDI VOWEL SIGN I		
+𑶌	i	iː			iː	ī		Mc​		vowel sign				U+11D8C: GUNJALA GONDI VOWEL SIGN II		
+𑶍	u	u			u	u		Mc​		vowel sign				U+11D8D: GUNJALA GONDI VOWEL SIGN U		
+𑶎	u	uː			uː	ū		Mc​		vowel sign				U+11D8E: GUNJALA GONDI VOWEL SIGN UU		
+𑶐	e	eː			eː	ē		Mn​		vowel sign				U+11D90: GUNJALA GONDI VOWEL SIGN EE		
+𑶓	o	o:			o:	ō		Mc​		vowel sign				U+11D93: GUNJALA GONDI VOWEL SIGN OO		
+𑶊	a	aː			aː	ā		Mc​		vowel sign				U+11D8A: GUNJALA GONDI VOWEL SIGN AA		
+𑶑	a	ai			aⁱ	ai		Mn​		vowel sign				U+11D91: GUNJALA GONDI VOWEL SIGN AI		
+𑶔	a	au			aᵘ	au		Mc​		vowel sign				U+11D94: GUNJALA GONDI VOWEL SIGN AU		
+																
+𑵢	i	i			ị	i		Lo		independent vowel				U+11D62: GUNJALA GONDI LETTER I		
+𑵣	I	iː			ịː	ī		Lo		independent vowel				U+11D63: GUNJALA GONDI LETTER II		
+𑵤	u	u			ụ	u		Lo		independent vowel				U+11D64: GUNJALA GONDI LETTER U		
+𑵥	U	uː			ụː	ū		Lo		independent vowel				U+11D65: GUNJALA GONDI LETTER UU		
+𑵧	e	eː			ẹː	ē		Lo		independent vowel				U+11D67: GUNJALA GONDI LETTER EE		
+𑵪	o	o:			ọ:	ō		Lo		independent vowel				U+11D6A: GUNJALA GONDI LETTER OO		
+𑵠	a	a			ạ	a		Lo		independent vowel				U+11D60: GUNJALA GONDI LETTER A		
+𑵡	A	aː			ạː	ā		Lo		independent vowel				U+11D61: GUNJALA GONDI LETTER AA		
+𑵨	A	ai			ạⁱ	ai		Lo		independent vowel				U+11D68: GUNJALA GONDI LETTER AI		
+𑵫	A	au			ạᵘ	au		Lo		independent vowel				U+11D6B: GUNJALA GONDI LETTER AU		
+																
+𑶠	±	0			0̣	0		Nd		digit				U+11DA0: GUNJALA GONDI DIGIT ZERO		
+𑶡	±	1			1̣	1		Nd		digit		𑵤𑶕𑵸𑶋	uṇdi	U+11DA1: GUNJALA GONDI DIGIT ONE		
+𑶢	±	2			2̣	2		Nd		digit		𑶈𑶕𑶂	raṇdd	U+11DA2: GUNJALA GONDI DIGIT TWO		
+𑶣	±	3			3̣	3		Nd		digit		𑵰𑶎𑶕𑵸	muuṇda	U+11DA3: GUNJALA GONDI DIGIT THREE		
+𑶤	±	4			4̣	4		Nd		digit		𑵺𑶍𑵵𑶍𑶄	nuluṇnga	U+11DA4: GUNJALA GONDI DIGIT FOUR		
+𑶥	±	5			5̣	5		Nd		digit		𑶉𑶋𑵬𑶍𑶕𑶄	siyuṇnga	U+11DA5: GUNJALA GONDI DIGIT FIVE		
+𑶦	±	6			6̣	6		Nd		digit		𑶉𑶍𑶈𑶍𑶕𑶄	suruṇnga	U+11DA6: GUNJALA GONDI DIGIT SIX		
+𑶧	±	7			7̣	7		Nd		digit		𑵬𑶐𑶀𑶕𑶄	yejuṇnga	U+11DA7: GUNJALA GONDI DIGIT SEVEN		
+𑶨	±	8			8̣	8		Nd		digit		𑵠𑵺	ana	U+11DA8: GUNJALA GONDI DIGIT EIGHT		
+𑶩	±	9			9̣	9		Nd		digit		𑶅𑶔𑵺	pauna	U+11DA9: GUNJALA GONDI DIGIT NINE		
+																
+																
+𑶗	*				͞			Mn		vowel-killer, virama				U+11D97: GUNJALA GONDI VIRAMA		
+𑶘	O	oːm		𑶘		om		Lo		om				U+11D98: GUNJALA GONDI OM		
+																
+।	|				|	.		Po		section divider				U+0964: DEVANAGARI DANDA		
+॥	|				¶			Po		section divider				U+0965: DEVANAGARI DOUBLE DANDA		
+																
+\u0020	 							Zs		space				U+0020: SPACE		
+,	,				,	,		Po		comma				U+002C: COMMA		
+:	:				:	:		Po		colon				U+003A: COLON		
+.	.				,	.		Po		full stop				U+002E: FULL STOP		
+																
+\u200C	=				ᶻʷⁿʲ			Cf		zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER		
+\u200D	=				ᶻʷʲ			Cf		zero-width joiner				U+200D: ZERO WIDTH JOINER		
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+// space																
+\u200B	=				ᶻʷˢᵖ			Cf	?	zero-width space				U+200B: ZERO WIDTH SPACE		
+\u202F	]				ⁿⁿᵇˢᵖ			Zs	?	nnbsp				U+202F NARROW NO-BREAK SPACE		
+// word separator																
+	]				• §-				?	word separator						
+// phrase dividers																
+;	;				; ;̣ ;̱ ;͈ ;͇	;	;;⁏⸵	Po	?	semicolon				U+003B: SEMICOLON		
+!	!				! !̣ !̱ !͈ !͇	!		Po	?	exclamation mark				U+0021: EXCLAMATION MARK		
+?	?				? ?̣ ?̱ ?͈ ?͇	?		Po	?	question mark				U+003F: QUESTION MARK		
+// period & ellipsis																
+…	.				…	…		Po	?	ellipsis				U+2026: HORIZONTAL ELLIPSIS		
+// quote marks																
+«	<				«	“		Pi	?	quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK		
+»	>				»	”		Pf	?	quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK		
+‹	<				‹			Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK		
+›	>				›			Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK		
+“	<				“	“		Pi	?	quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK		
+”	>				”	”		Pf	?	quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK		
+‘	<				‘	‘		Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK		
+’	>				’	’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK		
+// brackets																
+(	(				( (˙ (¯ (¨ (˭	(		Ps	?	parenthesis				U+0028: LEFT PARENTHESIS		
+)	)				) )˙ )¯ )¨ )˭	)		Ps	?	parenthesis				U+0029: RIGHT PARENTHESIS		
+[	(				[	[		Pe	?	bracket				U+005B: LEFT SQUARE BRACKET		
+]	)				] 	]		Pe	?	bracket				U+005D: RIGHT SQUARE BRACKET		
+// dashes																
+-	-				- -̣ -̱ -͈ -͇	-		Pd	?	hyphen				U+2010: HYPHEN		
+‑	-				‑	‑		Pd	?	non-breaking hyphen				U+2011: NON-BREAKING HYPHEN		
+–	-				–	–		Pd	?	en dash				U+2013: EN DASH		
+—	-				—	—		Pd	?	em dash				U+2014: EM DASH		
+// apostrophe																
+'	\'				ʼ	ʼ		Lm	?	apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE		
+// nuktas																
+‍଼	/				ˑ ˑ̣ ˑ̱ ˑ͈ ˑ͇				?	nukta						
+// repetition, gemination																
+	&				& &̣ &̱ &͈ &͇ &̥				?	repetition						
+	&				˖				?	gemination						
+// abbrev.																
+	+				¨				?	abbreviation marker						
+// misc diacritics																
+	[				+				?	vowel carrier						
+	[				˘				?	vowel shortener						
+	[				ᵊ				?	vowel retainer						
+	[								?	consonant shifter						
+	[				ˈ				?	superscript alef						
+	[				ʿ				?	hamza above						
+	[				˓				?	hamza below						
+٭	[				*				?	star						
+	[				~				?	maddah						
+	[				″				?	register change						
+	[				°				?	special						
+	[				˙				?	combining dot above						
+	[				ʼ				?	combining hook above						
+	[				ˆ				?	combining circumflex						
+	[				ˉ				?	combining macron						
+	[				̨				?	combining ogonek						
+	[				ᶠ				?	feminine marker						
+// head marks, honorifics, etc																
+	@				⟦				?	opening head mark						
+	@				⟧				?	closing head mark						
+	@				⟨				?	2nd opening head						
+	@				⟩				?	2 closing head						
+	@				§ᵃ				?	honorific						
+	@				§ᵇ				?	honorific						
+	@				§ᶜ				?	honorific						
+	@				°				?	extra						
+•	•				•			Po	?	bullet				U+2022: BULLET		
+// formatting characters																
+\u034F	=				ᶜᵍʲ			Mn	?	combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER		
+\u2060	=				ʷʲ			Cf	?	word joiner				U+2060 WORD JOINER		
+// number related																
+	#				ᵗ				?	thousands separator						
+	#				ᵈ				?	decimal separator						
+	#				/				?	date separator						
+	#				¤				?	currency symbol						
+	#				¢				?							
+	#				# #̣ #̱ #͈ #͇				?	number symbol						
+	#				¼				?	quarter						
+	#				½				?	half						
+	#				¾				?	three-quarters						
+%	%				%			Po	?	percentage mark				U+0025: PERCENT SIGN		
+‰	%				‰			Po	?	per mille mark				U+2030: PER MILLE SIGN		
+// logograms, extended numbers, etc																
+	$				{10} etc				?	extended number						
+// ascii digits																
+0	0				0	0		Nd	?	digit				U+0030: DIGIT ZERO		
+1	1				1	1		Nd	?	digit				U+0031: DIGIT ONE		
+2	2				2	2		Nd	?	digit				U+0032: DIGIT TWO		
+3	3				3	3		Nd	?	digit				U+0033: DIGIT THREE		
+4	4				4	4		Nd	?	digit				U+0034: DIGIT FOUR		
+5	5				5	5		Nd	?	digit				U+0035: DIGIT FIVE		
+6	6				6	6		Nd	?	digit				U+0036: DIGIT SIX		
+7	7				7	7		Nd	?	digit				U+0037: DIGIT SEVEN		
+8	8				8	8		Nd	?	digit				U+0038: DIGIT EIGHT		
+9	9				9	9		Nd	?	digit				U+0039: DIGIT NINE		
+`																
+																
+latinPanel = 'ː ʰ ɡ ʈ ɖ ŋ t͡ʃ d͡ʒ ʧ ʤ ḷ ḍ ṅ ṭ ā ē ī ō ū ɭ ɽ ʋ'																
+																
+var cols = {																
+key: 1,																
+ipaLoc: 2,																
+ipaPlus: 3,																
+ipaOther: 4,																
+transLoc: 5,																
+transcription: 6,																
+kbd: 7,																
+class: 8,																
+status: 9,																
+typeLoc: 10,																
+statusLoc: 11,																
+																
+dvowel: 0,																
+ivowel: 0,																
+onset:0,																
+final:0,																
+subj:0,																
+ltone:0,																
+htone:0,																
+lc:0,																
+uc:0,																
+meaning:0,																
+shape: 0,																
+numLoc: 0,																
+																
+nnameLoc: 12,																
+nameLoc: 13,																
+ucsName: 14,																
+map: 15,																
+block: 16,																
+																
+//othertranscriptions: [[6, 'Latin']]																
+}																
