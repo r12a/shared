4dd9f6e163e33c2d44f2cfd7e182f841d58d834b
r12a <ishida@w3.org>
2025-10-13 18:56:56 +0100

    gonm files

---
+++ scriptdb/gonm.js
@@ -0,0 +1,227 @@
+var spreadsheet = `gonm-gon	key	ipa	ipa+	ipaOther	translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	map	block
+																
+𑴲	i	i			i	i		Mn​		vowel sign				U+11D32: MASARAM GONDI VOWEL SIGN I	vowel sign	
+𑴳	i	iː			ī	ī		Mn​		vowel sign				U+11D33: MASARAM GONDI VOWEL SIGN II	vowel sign	
+𑴴	u	u			u	u		Mn​		vowel sign				U+11D34: MASARAM GONDI VOWEL SIGN U	vowel sign	
+𑴵	u	uː			ū	ū		Mn​		vowel sign				U+11D35: MASARAM GONDI VOWEL SIGN UU	vowel sign	
+𑴺	e	eː			ē	e		Mn​		vowel sign				U+11D3A: MASARAM GONDI VOWEL SIGN E	vowel sign	
+𑴽	o	oː			ō	o		Mn​		vowel sign				U+11D3D: MASARAM GONDI VOWEL SIGN O	vowel sign	
+𑴱	a	aː			ā	ā		Mn​		vowel sign				U+11D31: MASARAM GONDI VOWEL SIGN AA	vowel sign	
+																
+𑴼	a	ai			aⁱ	ai		Mn​		diphthong vowel sign				U+11D3C: MASARAM GONDI VOWEL SIGN AI	vowel sign	
+𑴿	a	au			aᵘ	au		Mn​		diphthong vowel sign				U+11D3F: MASARAM GONDI VOWEL SIGN AU	vowel sign	
+																
+𑴂	I	i			ị	i		Lo		plain standalone vowel				U+11D02: MASARAM GONDI LETTER I	standalone	
+𑴃	I	iː			ị̄	ī		Lo		plain standalone vowel				U+11D03: MASARAM GONDI LETTER II	standalone	
+𑴄	U	u			ụ	u		Lo		plain standalone vowel				U+11D04: MASARAM GONDI LETTER U	standalone	
+𑴅	U	uː			ụ̄	ū		Lo		plain standalone vowel				U+11D05: MASARAM GONDI LETTER UU	standalone	
+𑴆	E	e			ẹ	e		Lo		plain standalone vowel				U+11D06: MASARAM GONDI LETTER E	standalone	
+𑴉	O	o			ọ	o		Lo		plain standalone vowel				U+11D09: MASARAM GONDI LETTER O	standalone	
+𑴀	A	a			ạ	a		Lo		plain standalone vowel				U+11D00: MASARAM GONDI LETTER A	standalone	
+𑴁	A	aː			ạ̄	ā		Lo		plain standalone vowel				U+11D01: MASARAM GONDI LETTER AA	standalone	
+																
+𑴈	A	ai			ạⁱ	ai		Lo		diphthong standalone vowel				U+11D08: MASARAM GONDI LETTER AI	standalone	
+𑴋	A	au			ạᵘ	au		Lo		diphthong standalone vowel				U+11D0B: MASARAM GONDI LETTER AU	standalone	
+																
+𑴠	p	p	a		p	p		Lo		consonant				U+11D20: MASARAM GONDI LETTER PA		
+𑴡	P	pʰ	a		pʰ	ph		Lo		consonant				U+11D21: MASARAM GONDI LETTER PHA		
+𑴢	b	b	a		b	b		Lo		consonant				U+11D22: MASARAM GONDI LETTER BA		
+𑴣	B	bʰ	a		bʰ	bh		Lo		consonant				U+11D23: MASARAM GONDI LETTER BHA		
+𑴛	t	t	a		t	t		Lo		consonant				U+11D1B: MASARAM GONDI LETTER TA		
+𑴜	T	tʰ	a		tʰ	th		Lo		consonant				U+11D1C: MASARAM GONDI LETTER THA		
+𑴑	c	t͡ʃ	a		ʧ	c		Lo		consonant				U+11D11: MASARAM GONDI LETTER CA		
+𑴒	C	t͡ʃʰ	a		ʧʰ	ch		Lo		consonant				U+11D12: MASARAM GONDI LETTER CHA		
+𑴝	d	d	a		d	d		Lo		consonant				U+11D1D: MASARAM GONDI LETTER DA		
+𑴞	D	dʰ	a		dʰ	dh		Lo		consonant				U+11D1E: MASARAM GONDI LETTER DHA		
+𑴓	j	d͡ʒ	a		ʤ	j		Lo		consonant				U+11D13: MASARAM GONDI LETTER JA		
+𑴔	J	d͡ʒʰ	a		ʤʰ	jh		Lo		consonant				U+11D14: MASARAM GONDI LETTER JHA		
+𑴖	t	ʈ	a		ʈ	ṭ		Lo		consonant				U+11D16: MASARAM GONDI LETTER TTA		
+𑴗	T	ʈʰ	a		ʈʰ	ṭh		Lo		consonant				U+11D17: MASARAM GONDI LETTER TTHA		
+𑴘	d	ɖ	a		ɖ	ḍ		Lo		consonant				U+11D18: MASARAM GONDI LETTER DDA		
+𑴙	D	ɖʰ	a		ɖʰ	ḍh		Lo		consonant				U+11D19: MASARAM GONDI LETTER DDHA		
+𑴌	k	k	a		k	k		Lo		consonant				U+11D0C: MASARAM GONDI LETTER KA		
+𑴍	K	kʰ	a		kʰ	kh		Lo		consonant				U+11D0D: MASARAM GONDI LETTER KHA		
+𑴎	g	ɡ	a		ɡ	g		Lo		consonant				U+11D0E: MASARAM GONDI LETTER GA		
+𑴏	G	ɡʰ	a		ɡʰ	gh		Lo		consonant				U+11D0F: MASARAM GONDI LETTER GHA		
+𑴨	v	v	a		v	v		Lo		consonant				U+11D28: MASARAM GONDI LETTER VA		
+𑴫	s	s	a		s	s		Lo		consonant				U+11D2B: MASARAM GONDI LETTER SA		
+𑴪	S	ʂ	a		ʂ	?		Lo		consonant				U+11D2A: MASARAM GONDI LETTER SSA		
+𑴩	S	ʃ	a		ʃ	sh		Lo		consonant				U+11D29: MASARAM GONDI LETTER SHA		
+𑴬	h	h	a		h	h		Lo		consonant				U+11D2C: MASARAM GONDI LETTER HA		
+𑵁	h	h			h̽	h		Mn​		visarga				U+11D41: MASARAM GONDI SIGN VISARGA		
+𑴤	m	m	a		m	m		Lo		consonant				U+11D24: MASARAM GONDI LETTER MA		
+𑴟	n	n	a		n	n		Lo		consonant				U+11D1F: MASARAM GONDI LETTER NA		
+𑴚	n	ɳ	a		ɳ	ṇ		Lo		consonant				U+11D1A: MASARAM GONDI LETTER NNA		
+𑴕	n	ɲ	a		ɲ	ny		Lo		consonant				U+11D15: MASARAM GONDI LETTER NYA		
+𑴐	N	ŋ	a		ŋ	ṅ		Lo		consonant				U+11D10: MASARAM GONDI LETTER NGA		
+𑵀	[¶	ŋ		ɳ ɲ	˜	n		Mn​		nasal coda				U+11D40: MASARAM GONDI SIGN ANUSVARA		
+𑴦	r	r	a		r	r		Lo		consonant				U+11D26: MASARAM GONDI LETTER RA		
+𑵆	r	r			r̽	r		Lo		RA coda in consonant clusters.				U+11D46: MASARAM GONDI REPHA		
+𑵇	r	r			r̆	r		Mn​		Medial RA.				U+11D47: MASARAM GONDI RA-KARA		
+𑴦𑵂	r	ɽ			ɽ	ṛ				Retroflex r				U+11D26 U+11D42: MASARAM GONDI LETTER RA, SIGN NUKTA		
+𑴧	l	l	a		l	l		Lo		consonant				U+11D27: MASARAM GONDI LETTER LA		
+𑴭	l	ɭ	a		ɭ	ḷ		Lo		consonant				U+11D2D: MASARAM GONDI LETTER LLA		
+𑴥	y	j	a		j	y		Lo		consonant				U+11D25: MASARAM GONDI LETTER YA		
+𑴮	k	ks	a		ks	ks		Lo		predefined consonant cluster				U+11D2E: MASARAM GONDI LETTER KSSA		
+𑴯	g	gɲ	a		gɲ	gny		Lo		predefined consonant cluster				U+11D2F: MASARAM GONDI LETTER JNYA		
+𑴰	t	tr	a		tr	tr		Lo		predefined consonant cluster				U+11D30: MASARAM GONDI LETTER TRA		
+																
+𑴶	r	ri						Mn​		vocalic vowel sign				U+11D36: MASARAM GONDI VOWEL SIGN VOCALIC R	vowel sign	
+																
+𑵐	±	0			0̣	0		Nd		digit				U+11D50: MASARAM GONDI DIGIT ZERO		
+𑵑	±	1			1̣	1		Nd		digit				U+11D51: MASARAM GONDI DIGIT ONE		
+𑵒	±	2			2̣	2		Nd		digit				U+11D52: MASARAM GONDI DIGIT TWO		
+𑵓	±	3			3̣	3		Nd		digit				U+11D53: MASARAM GONDI DIGIT THREE		
+𑵔	±	4			4̣	4		Nd		digit				U+11D54: MASARAM GONDI DIGIT FOUR		
+𑵕	±	5			5̣	5		Nd		digit				U+11D55: MASARAM GONDI DIGIT FIVE		
+𑵖	±	6			6̣	6		Nd		digit				U+11D56: MASARAM GONDI DIGIT SIX		
+𑵗	±	7			7̣	7		Nd		digit				U+11D57: MASARAM GONDI DIGIT SEVEN		
+𑵘	±	8			8̣	8		Nd		digit				U+11D58: MASARAM GONDI DIGIT EIGHT		
+𑵙	±	9			9̣	9		Nd		digit				U+11D59: MASARAM GONDI DIGIT NINE		
+																
+𑵂	/				ˑ			Mn​		nukta				U+11D42: MASARAM GONDI SIGN NUKTA		
+𑵃	/				ˑ̣			Mn​		vowel nukta	Used for transcribing foriegn vowels.			U+11D43: MASARAM GONDI SIGN CANDRA		
+𑵄	*				˟			Mn​		word-final vowel killer	Silences the inherent vowel at word-end.			U+11D44: MASARAM GONDI SIGN HALANTA		
+𑵅	*				͞			Mn​		conjunct former	Forms conjuncts only.			U+11D45: MASARAM GONDI VIRAMA		
+																
+।	|				|	.		Po		section divider				U+0964: DEVANAGARI DANDA		
+॥	|				¶			Po		section divider				U+0965: DEVANAGARI DOUBLE DANDA		
+																
+\u0020	 	 			 	 		Zs		space				U+0020: SPACE		
+,	,				,	,		Po		comma				U+002C: COMMA		
+;	;				;	;	;	Po		semicolon				U+003B: SEMICOLON		
+!	!				!	!		Po		exclamation mark				U+0021: EXCLAMATION MARK		
+?	?				?	?		Po		question mark				U+003F: QUESTION MARK		
+:	:				:	:		Po		colon				U+003A: COLON		
+.	.				.	.		Po		full stop				U+002E: FULL STOP		
+(	(				(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS		
+)	)				)	)		Ps		parenthesis				U+0029: RIGHT PARENTHESIS		
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+																
+// space																
+\u200B	=				ᶻʷˢᵖ			Cf	?	zero-width space				U+200B: ZERO WIDTH SPACE		
+\u202F	]				ⁿⁿᵇˢᵖ			Zs	?	nnbsp				U+202F NARROW NO-BREAK SPACE		
+// period & ellipsis																
+…	.				…	…		Po	?	ellipsis				U+2026: HORIZONTAL ELLIPSIS		
+// quote marks																
+“	<				“	“		Pi	?	quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK		
+”	>				”	”		Pf	?	quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK		
+‘	<				‘	‘		Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK		
+’	>				’	’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK		
+// brackets																
+[	(				[	[		Pe	?	bracket				U+005B: LEFT SQUARE BRACKET		
+]	)				] 	]		Pe	?	bracket				U+005D: RIGHT SQUARE BRACKET		
+// dashes																
+-	-				- -̣ -̱ -͈ -͇	-		Pd	?	hyphen				U+2010: HYPHEN		
+‑	-				‑	‑		Pd	?	non-breaking hyphen				U+2011: NON-BREAKING HYPHEN		
+–	-				–	–		Pd	?	en dash				U+2013: EN DASH		
+—	-				—	—		Pd	?	em dash				U+2014: EM DASH		
+// apostrophe																
+'	\'				ʼ	ʼ		Lm	?	apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE		
+// other																
+•	•				•			Po	?	bullet				U+2022: BULLET		
+// formatting characters																
+\u200C	=				ᶻʷⁿʲ			Cf	?	zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER		
+\u200D	=				ᶻʷʲ			Cf	?	zero-width joiner				U+200D: ZERO WIDTH JOINER		
+\u034F	=				ᶜᵍʲ			Mn	?	combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER		
+\u2060	=				ʷʲ			Cf	?	word joiner				U+2060 WORD JOINER		
+// number related																
+	#				ᵗ				?	thousands separator						
+	#				ᵈ				?	decimal separator						
+	#				/				?	date separator						
+	#				¤				?	currency symbol						
+	#				¢				?							
+	#				# #̣ #̱ #͈ #͇				?	number symbol						
+	#				¼				?	quarter						
+	#				½				?	half						
+	#				¾				?	three-quarters						
+%	%				%			Po	?	percentage mark				U+0025: PERCENT SIGN		
+‰	%				‰			Po	?	per mille mark				U+2030: PER MILLE SIGN		
+// logograms, extended numbers, etc																
+	$				{10} etc				?	extended number						
+// ascii digits																
+0	0				0	0		Nd	?	digit				U+0030: DIGIT ZERO		
+1	1				1	1		Nd	?	digit				U+0031: DIGIT ONE		
+2	2				2	2		Nd	?	digit				U+0032: DIGIT TWO		
+3	3				3	3		Nd	?	digit				U+0033: DIGIT THREE		
+4	4				4	4		Nd	?	digit				U+0034: DIGIT FOUR		
+5	5				5	5		Nd	?	digit				U+0035: DIGIT FIVE		
+6	6				6	6		Nd	?	digit				U+0036: DIGIT SIX		
+7	7				7	7		Nd	?	digit				U+0037: DIGIT SEVEN		
+8	8				8	8		Nd	?	digit				U+0038: DIGIT EIGHT		
+9	9				9	9		Nd	?	digit				U+0039: DIGIT NINE		
+`																
+																
+latinPanel = 'ː ʰ ɡ ŋ t͡ʃ d͡ʒ ɲ ʈ ɖ ɳ ṇ ḍ ṭ ʃ ɭ ʧ ʤ iː uː eː oː aː ā ī ū ē ō r̥'																
+																
+var cols = {																
+key: 1,																
+ipaLoc: 2,																
+ipaPlus: 3,																
+ipaOther: 4,																
+transLoc: 5,																
+transcription: 6,																
+kbd: 7,																
+class: 8,																
+status: 9,																
+typeLoc: 10,																
+statusLoc: 11,																
+																
+dvowel: 0,																
+ivowel: 0,																
+onset:0,																
+final:0,																
+subj:0,																
+ltone:0,																
+htone:0,																
+lc:0,																
+uc:0,																
+meaning:0,																
+shape: 0,																
+numLoc: 0,																
+																
+nnameLoc: 12,																
+nameLoc: 13,																
+ucsName: 14,																
+map: 15,																
+block: 16,																
+																
+//othertranscriptions: [[6, 'Latin']]																
+}																
